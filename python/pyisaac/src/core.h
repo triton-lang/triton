5ef01f041a212143b887d0b024f7a1e6ac3bfe78
Philippe Tillet <ptillet@g.harvard.edu>
2015-04-29 17:48:57 -0400

    Python: Refactored wrapper

---
+++ python/pyisaac/src/core.h
@@ -0,0 +1,6 @@
+#ifndef PYISAAC_SRC_CORE_H
+#define PYISAAC_SRC_CORE_H
+
+void export_core();
+
+#endif
---
+++ python/pyisaac/src/model.h
@@ -0,0 +1,6 @@
+#ifndef PYISAAC_SRC_MODEL_H
+#define PYISAAC_SRC_MODEL_H
+
+void export_model();
+
+#endif
