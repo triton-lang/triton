552f64c9ebb6f6aa828850e4703092ac527eb475
Philippe Tillet <ptillet@g.harvard.edu>
2015-08-07 20:56:11 -0700

    Driver: Fixed bug in NDRange

--- include/isaac/driver/ndrange.h
+++ include/isaac/driver/ndrange.h
@@ -14,11 +14,14 @@ namespace driver
 class ISAACAPI NDRange
 {
 public:
-  NDRange(size_t size0 = 1, size_t size1 = 1, size_t size2 = 1);
-  size_t dimension() const;
+  NDRange(size_t size0);
+  NDRange(size_t size0, size_t size1);
+  NDRange(size_t size0, size_t size1, size_t size2);
+  int dimension() const;
   operator const size_t*() const;
 private:
   size_t sizes_[3];
+  int dimension_;
 };
 
 }
