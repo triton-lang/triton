9d92b16cc002173a1ce03cfd4f6210c59e7598bf
Philippe Tillet <ptillet@g.harvard.edu>
2015-08-14 01:11:46 -0700

    Android: restored compatibility

--- tests/android/test.cpp
+++ tests/android/test.cpp
@@ -2,14 +2,15 @@
 #include <android_native_app_glue.h>
 #include <sstream>
 #include "isaac/array.h"
-#include "isaac/tools/to_string.hpp"
 
 void android_main(struct android_app* state)
 {
     app_dummy(); // Make sure glue isn't stripped
     __android_log_print(ANDROID_LOG_INFO, "IsaacAndroidTest", "This is a test");
     isaac::array test(5, isaac::FLOAT_TYPE);
-    __android_log_print(ANDROID_LOG_INFO, "IsaacAndroidTest", isaac::tools::to_string(test).c_str());
+    std::ostringstream oss;
+    oss << test;
+    __android_log_print(ANDROID_LOG_INFO, "IsaacAndroidTest", oss.str().c_str());
     ANativeActivity_finish(state->activity);
 }
 
