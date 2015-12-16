ae11f46f8f67eafefb28d3f5c745141e194a271f
Philippe Tillet <ptillet@g.harvard.edu>
2015-12-16 01:32:46 -0500

    Core: refurbished array printing

--- examples/indexing.cpp
+++ examples/indexing.cpp
@@ -11,7 +11,7 @@ int main()
     std::cout << "Tutorial: Indexing " << std::endl;
     std::cout << dline << std::endl;
 
-    sc::int_t M = 10, N = 7;
+    sc::int_t M = 5, N = 12;
 
     std::vector<float> data(M*N);
     for(unsigned int i = 0 ; i < data.size(); ++i)
@@ -21,25 +21,16 @@ int main()
     std::cout << "A:" << std::endl;
     std::cout << sline << std::endl;
     std::cout << A << std::endl;
-    std::cout << std::endl;
 
-    std::cout << "A[3, 2:end]:" << std::endl;
-    std::cout << sline << std::endl;
-    std::cout << A(3, {2,sc::end}) << std::endl;
-    std::cout << std::endl;
+//    std::cout << sline << std::endl;
+//    std::cout << "A[3, 2:end]:" << A(3, {2,sc::end}) << std::endl;
 
-    std::cout << "A[2:end, 4]:" << std::endl;
-    std::cout << sline << std::endl;
-    std::cout << A({2,sc::end}, 4) << std::endl;
-    std::cout << std::endl;
+//    std::cout << sline << std::endl;
+//    std::cout << "A[2:end, 4]:" << A({2,sc::end}, 4) << std::endl;
 
-    std::cout << "diag(A,  1): " << std::endl;
-    std::cout << sline << std::endl;
-    std::cout << sc::diag(A, 1) << std::endl;
-    std::cout << std::endl;
+//    std::cout << sline << std::endl;
+//    std::cout << "diag(A,  1): " << sc::diag(A, 1) << std::endl;
 
-    std::cout << "diag(A, -7): " << std::endl;
-    std::cout << sline << std::endl;
-    std::cout << sc::diag(A, -7) << std::endl;
-    std::cout << std::endl;
+//    std::cout << sline << std::endl;
+//    std::cout << "diag(A, -7): " << sc::diag(A, -7) << std::endl;
 }
