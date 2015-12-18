ea607bccedc693276c40cf208ef3d0247d972b67
Philippe Tillet <ptillet@g.harvard.edu>
2015-12-18 03:32:37 -0500

    Presets: better Maxwell profile

--- lib/profiles/presets.cpp
+++ lib/profiles/presets.cpp
@@ -2,6 +2,7 @@
 #include "isaac/profiles/profiles.h"
 
 #include "presets/broadwell.hpp"
+#include "presets/maxwell.hpp"
 
 namespace isaac
 {
@@ -11,7 +12,12 @@ namespace isaac
             {std::make_tuple(driver::Device::Type::TYPE, driver::Device::Vendor::VENDOR, driver::Device::Architecture::ARCHITECTURE), STRING}
 
 const profiles::presets_type profiles::presets_ =
-            {  };
+{
+    //INTEL
+    DATABASE_ENTRY(GPU, INTEL, BROADWELL, presets::broadwell),
+    //NVIDIA
+    DATABASE_ENTRY(GPU, NVIDIA, MAXWELL, presets::maxwell),
+};
 
 
 #undef DATABASE_ENTRY
