fa4cb6866d12ec38c1ac7e1bd107b53eed56ceea
Philippe Tillet <ptillet@g.harvard.edu>
2016-09-29 14:41:29 -0400

    Bench: Now displaying results in a table

--- bench/blas.cpp
+++ bench/blas.cpp
@@ -49,7 +49,7 @@ void bench(sc::numeric_type dtype, std::string operation)
     total_time+=times.back();\
   }\
   double t = min(times);\
-  std::cout << " " << (int)(PERF) << std::flush;\
+  std::cout << "\t" << (int)PERF << std::flush;\
   }
 
 #define BENCHMARK_CLBLAS(OP, PERF) \
@@ -66,7 +66,7 @@ void bench(sc::numeric_type dtype, std::string operation)
     total_time+=times.back();\
   }\
   double t = min(times);\
-  std::cout << " " << (int)(PERF) << std::flush;\
+  std::cout << "\t" << PERF << std::flush;\
   }
 
 #define BENCHMARK_HOST(OP, PERF) \
@@ -82,7 +82,7 @@ void bench(sc::numeric_type dtype, std::string operation)
     total_time += time;\
   }\
   double t = min(times);\
-  std::cout << " " << (int)(PERF) << std::flush;\
+  std::cout << "\t" << PERF << std::flush;\
   }
 
 #define BENCHMARK_CUDA(OP, PERF) \
@@ -99,27 +99,14 @@ void bench(sc::numeric_type dtype, std::string operation)
       total_time+=times.back();\
     }\
     double t = min(times);\
-    std::cout << " " << (int)(PERF) << std::flush;\
+    std::cout << "\t" << PERF << std::flush;\
   }
 
   unsigned int dtsize = sc::size_of(dtype);
   sc::driver::CommandQueue & queue = sc::driver::backend::queues::get(sc::driver::backend::contexts::get_default(),0);
   std::map<std::string, std::string> metric{ {"axpy", "GB/s"}, {"dot", "GB/s"}, {"gemv", "GB/s"}, {"gemm", "GFLOPS"}};
   sc::array flush((int)1e6, sc::FLOAT_TYPE);
-  std::cout << "#" << operation << " (" << metric[operation] << ")" << std::endl;
-  std::cout << "\"N\"";
-  std::cout << " \"ISAAC\"";
-//  std::cout << " \"ISAAC (Best impl.)\"";
-#ifdef BENCH_CLBLAS
-  std::cout << " \"clBLAS\"";
-#endif
-#ifdef BENCH_CBLAS
-  std::cout << " \"BLAS\"";
-#endif
-#ifdef BENCH_CUBLAS
-  std::cout << " \"cuBLAS\"";
-#endif
-  std::cout << std::endl;
+
   //
   // RUN BENCHMARKS
   //
@@ -250,53 +237,51 @@ void bench(sc::numeric_type dtype, std::string operation)
 
   if(operation.substr(0,4)=="gemm")
   {
-    std::vector<std::tuple<std::string, char, char, int_t, int_t, int_t> > MNKs;
+    std::vector<std::tuple<std::string, int_t, int_t, int_t, char, char> > MNKs;
     //Square
-    MNKs.push_back(std::make_tuple("square896",'N','T',896,896,896));
-    MNKs.push_back(std::make_tuple("square2560",'N','T',2560,2560,2560));
-
-    //Convolution
-    MNKs.push_back(std::make_tuple("conv1",'N','N',3025,64,363));
-    MNKs.push_back(std::make_tuple("conv2",'N','N',729,192,1600));
-    MNKs.push_back(std::make_tuple("conv3",'N','N',169,384,1728));
-    MNKs.push_back(std::make_tuple("conv4",'N','N',169,256,3456));
-    MNKs.push_back(std::make_tuple("conv5",'N','N',169,128,2304));
-
-//    //Convolution Gradient-1
-//    MNKs.push_back(std::make_tuple("convgrad5-1]",'T','N',2304,256,169));
-//    MNKs.push_back(std::make_tuple("convgrad4-1]",'T','N',3456,256,169));
-//    MNKs.push_back(std::make_tuple("convgrad3-1]",'T','N',1728,384,169));
-//    MNKs.push_back(std::make_tuple("convgrad2-1]",'T','N',1600,192,729));
-//    MNKs.push_back(std::make_tuple("convgrad1-1]",'T','N',363,64,3025));
-
-//    //Convolution Gradient-2
-//    MNKs.push_back(std::make_tuple("convgrad5-2]",'N','T',169,2304,256));
-//    MNKs.push_back(std::make_tuple("convgrad4-2]",'N','T',169,3456,256));
-//    MNKs.push_back(std::make_tuple("convgrad3-2]",'N','T',169,1728,384));
-//    MNKs.push_back(std::make_tuple("convgrad2-2]",'N','T',729,1600,192));
-//    MNKs.push_back(std::make_tuple("convgrad1-2]",'N','T',3025,363,64));
-
+    MNKs.push_back(std::make_tuple("Square",896,896,896,'N','T'));
+    MNKs.push_back(std::make_tuple("Square",2560,2560,2560,'N','T'));
+    //DeepBench
+    for(size_t MK: std::vector<size_t>{1760, 2048, 2560})
+      for(size_t N: std::vector<size_t>{16, 32, 64, 128, MK})
+        MNKs.push_back(std::make_tuple("Deep", MK, N, MK, 'N', 'N'));
+    for(size_t MK: std::vector<size_t>{1760, 2048, 2560})
+      for(size_t N: std::vector<size_t>{16, 32, 64, 128, MK})
+        MNKs.push_back(std::make_tuple("Deep", MK, N, MK, 'T', 'N'));
+    for(size_t MK: std::vector<size_t>{1760})
+      MNKs.push_back(std::make_tuple("Deep", MK, 1733, MK, 'N', 'T'));
     //Covariance (e.g., ICA, 10minutes/100Hz)
-    MNKs.push_back(std::make_tuple("ica32",'N','T',32,32,60000));
-    MNKs.push_back(std::make_tuple("ica256",'N','T',256,256,60000));
+    MNKs.push_back(std::make_tuple("Cov",32,32,60000,'N','T'));
+    MNKs.push_back(std::make_tuple("Cov",256,256,60000,'N','T'));
+    //Bi-diagonalization
+    MNKs.push_back(std::make_tuple("Lapack",4096,4096,32,'N','T'));
+    MNKs.push_back(std::make_tuple("Lapack",3456,3456,32,'N','T'));
+    MNKs.push_back(std::make_tuple("Lapack",896,896,32,'N','T'));
 
-//    //Bi-diagonalization
-    MNKs.push_back(std::make_tuple("32rank1-4096",'N','T',4096,4096,32));
-    MNKs.push_back(std::make_tuple("32rank1-3456",'N','T',3456,3456,32));
-    MNKs.push_back(std::make_tuple("32rank1-896",'N','T',896,896,32));
+    std::cout << "BENCH\tM\tN\tK\ta_t\tb_t\tISAAC";;
+#ifdef BENCH_CLBLAS
+  std::cout << "\tclBLAS";
+#endif
+#ifdef BENCH_CBLAS
+  std::cout << "\tBLAS";
+#endif
+#ifdef BENCH_CUBLAS
+  std::cout << "\tcuBLAS";
+#endif
+    std::cout << std::endl;
 
     /*---------*/
     /*--BLAS3--*/
     /*---------*/
-    for(std::tuple<std::string, char, char, int_t, int_t, int_t> MNK: MNKs)
+    for(auto MNK: MNKs)
     {
-        bool AT = std::get<1>(MNK)=='T';
-        bool BT = std::get<2>(MNK)=='T';
-        int_t M = std::get<3>(MNK);
-        int_t N = std::get<4>(MNK);
-        int_t K = std::get<5>(MNK);
-        std::cout << "\"" << std::get<0>(MNK) << "\"";
-        std::cout << std::flush;
+        bool AT = std::get<4>(MNK)=='T';
+        bool BT = std::get<5>(MNK)=='T';
+        int_t M = std::get<1>(MNK);
+        int_t N = std::get<2>(MNK);
+        int_t K = std::get<3>(MNK);
+        std::cout << std::get<0>(MNK) << "\t" << M << "\t" << N
+                  << "\t" << K << "\t" << std::get<4>(MNK) << "\t" << std::get<5>(MNK);
         /* ISAAC */
         int_t As1 = M, As2 = K;
         if(AT) std::swap(As1, As2);
@@ -307,13 +292,13 @@ void bench(sc::numeric_type dtype, std::string operation)
     #ifdef HAS_A_BLAS
         int_t lda = A.stride()[1], ldb = B.stride()[1], ldc = C.stride()[1];
     #endif
-        BENCHMARK_ISAAC(C = AT?(BT?dot(A.T,B.T):dot(A.T,B)):(BT?dot(A,B.T):dot(A,B)), (double)2*M*N*K/t);
+        BENCHMARK_ISAAC(C = AT?(BT?dot(A.T,B.T):dot(A.T,B)):(BT?dot(A,B.T):dot(A,B)), (double)2*M*N*K/t*1e-3);
 //        BENCHMARK_ISAAC(C = sc::execution_handler(AT?(BT?dot(A.T,B.T):dot(A.T,B)):(BT?dot(A,B.T):dot(A,B)), sc::execution_options_type(0), sc::dispatcher_options_type(true)), (double)2*M*N*K/t);
         /* clblas */
     #ifdef BENCH_CLBLAS
         if(C.context().backend()==sc::driver::OPENCL)
             BENCHMARK_CLBLAS(clblasSgemm(clblasColumnMajor, AT?clblasTrans:clblasNoTrans, BT?clblasTrans:clblasNoTrans, M, N, K, 1, CL_HANDLE(A.data()), 0, lda, CL_HANDLE(B.data()), 0, ldb,
-                                                0, CL_HANDLE(C.data()), 0, ldc, 1, &CL_HANDLE(queue),0, NULL, NULL), (double)2*M*N*K/t)
+                                                0, CL_HANDLE(C.data()), 0, ldc, 1, &CL_HANDLE(queue),0, NULL, NULL), (double)2*M*N*K/t*1e-3)
     #endif
         /* BLAS */
     #ifdef BENCH_CBLAS
@@ -321,10 +306,10 @@ void bench(sc::numeric_type dtype, std::string operation)
         sc::copy(C, cC);
         sc::copy(A, cA);
         sc::copy(B, cB);
-        BENCHMARK_HOST(cblas_sgemm(CblasColMajor, AT?CblasTrans:CblasNoTrans, BT?CblasTrans:CblasNoTrans, M, N, K, 1, cA.data(), lda, cB.data(), ldb, 1, cC.data(), ldc), (double)2*M*N*K/t);
+        BENCHMARK_HOST(cblas_sgemm(CblasColMajor, AT?CblasTrans:CblasNoTrans, BT?CblasTrans:CblasNoTrans, M, N, K, 1, cA.data(), lda, cB.data(), ldb, 1, cC.data(), ldc), (double)2*M*N*K/t*1e-3);
     #endif
     #ifdef BENCH_CUBLAS
-        BENCHMARK_CUDA(cublasSgemm(AT?'t':'n', BT?'t':'n', M, N, K, 1, (T*)CU_HANDLE(A.data()), lda, (T*)CU_HANDLE(B.data()), ldb, 1, (T*)CU_HANDLE(C.data()), ldc), (double)2*M*N*K/t)
+        BENCHMARK_CUDA(cublasSgemm(AT?'t':'n', BT?'t':'n', M, N, K, 1, (T*)CU_HANDLE(A.data()), lda, (T*)CU_HANDLE(B.data()), ldb, 1, (T*)CU_HANDLE(C.data()), ldc), (double)2*M*N*K/t*1e-3)
     #endif
         std::cout << std::endl;
       }
@@ -373,8 +358,7 @@ int main(int argc, char* argv[])
   }
 
   sc::driver::backend::default_device = device_idx;
-  std::cout << "#Benchmark : BLAS" << std::endl;
-  std::cout << "#----------------" << std::endl;
+  std::cout << std::fixed << std::setprecision(2);
   bench<float>(sc::FLOAT_TYPE, operation);
 
 #ifdef BENCH_CLBLAS
