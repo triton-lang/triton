e4d9ae6efab7adff3489d263cafe0740078647ef
Wang, Tiantian <tiantian.wang@intel.com>
2016-12-05 18:44:23 +0800

    simple_half warning is fixed and simple_half is formated.
    Change-Id: I6040dbf2b4372dcb7304566242fb39d377681459

--- include/isaac/common/simple_half.h
+++ include/isaac/common/simple_half.h
@@ -11,195 +11,71 @@ class half{
 public:
   unsigned short us;
   operator int() const;
-//   operator float()const ;
-//   operator double() const ;
-//   operator unsigned int() const ;
-//   operator short() const ;
-//   operator unsigned short() const ;
-//   operator char() const;
 
-  half operator+(half &h1);
-  half operator-(half &h1);
-  half operator*(half &h1);
-  half operator/(half &h1);
-  half operator+=(half &h1);
-  half operator>(half &h1);
-  half operator>=(half &h1);
-  half operator<(half &h1);
-  half operator<=(half &h1);
-  half operator==(half &h1);
-  half operator!=(half &h1);
+  half operator+(half &);
+  half operator-(half &);
+  half operator*(half &);
+  half operator/(half &);
+  half operator+=(half &);
+  half operator>(half &);
+  half operator>=(half &);
+  half operator<(half &);
+  half operator<=(half &);
+  half operator==(half &);
+  half operator!=(half &);
 
-  half & operator=(const char x);
-  half & operator=(const unsigned char x);
-  half & operator=(const int x);
-  half & operator=(const unsigned int x);
-  half & operator=(const short x);
-  half & operator=(const unsigned short x);
-  half & operator=(const float x);
-  half & operator=(const double x);
-  half & operator=(const long x);
-  half & operator=(const unsigned long x);
-  half & operator=(const long long x);
-  half & operator=(const unsigned long long x);
-  //half & operator=(const int8_t x);
-  //half & operator=(const uint8_t x);
+  half & operator=(const char);
+  half & operator=(const unsigned char);
+  half & operator=(const int);
+  half & operator=(const unsigned int);
+  half & operator=(const short);
+  half & operator=(const unsigned short);
+  half & operator=(const float);
+  half & operator=(const double);
+  half & operator=(const long);
+  half & operator=(const unsigned long);
+  half & operator=(const long long);
+  half & operator=(const unsigned long long);
 
-  std::ostream & operator<<(std::ostream & os);
-  half(char &x);
-  half(unsigned char &x);
-  half(int &x);
-  half(unsigned int &x);
-  half(short &x);
-  half(unsigned short &x);
-  half(float &x);
-  half(double &x);
-  half(long &x);
-  half(unsigned long &x);
+  std::ostream & operator<<(std::ostream &);
+  half(char &);
+  half(unsigned char &);
+  half(int &);
+  half(unsigned int &);
+  half(short &);
+  half(unsigned short &);
+  half(float &);
+  half(double &);
+  half(long &);
+  half(unsigned long &);
   half();
-
-// half ( char x);
-// half (unsigned char x);
-// half ( int x);
-// half (unsigned int x);
-// half( short x);
-// half ( unsigned short x);
-// half ( float x);
-// half ( double x);
-// half( long x);
-// half ( unsigned long x);
 
 };
 
 std::ostream & operator<<(std::ostream & os,half h);
-value_scalar operator+(char h1, half h2);
-value_scalar operator-(char h1, half h2);
-value_scalar operator*(char h1, half h2);
-value_scalar operator/(char h1, half h2);
-value_scalar operator+(half h2, char h1);
-value_scalar operator-(half h2, char h1);
-value_scalar operator*(half h2, char h1);
-value_scalar operator/(half h2, char h1);
-value_scalar pow(half h2, char h1);
-value_scalar pow(char h1, half h2);
 
-value_scalar operator+(unsigned char h1, half h2);
-value_scalar operator-(unsigned char h1, half h2);
-value_scalar operator*(unsigned char h1, half h2);
-value_scalar operator/(unsigned char h1, half h2);
-value_scalar operator+(half h2, unsigned char h1);
-value_scalar operator-(half h2, unsigned char h1);
-value_scalar operator*(half h2, unsigned char h1);
-value_scalar operator/(half h2, unsigned char h1);
+#define INSTANTIATE(TYPE)\
+  value_scalar operator+(TYPE, half);\
+  value_scalar operator-(TYPE, half);\
+  value_scalar operator*(TYPE, half);\
+  value_scalar operator/(TYPE, half);\
+  value_scalar operator+(half, TYPE);\
+  value_scalar operator-(half, TYPE);\
+  value_scalar operator*(half, TYPE);\
+  value_scalar operator/(half, TYPE);
 
-value_scalar operator+(short h1, half h2);
-value_scalar operator-(short h1, half h2);
-value_scalar operator*(short h1, half h2);
-value_scalar operator/(short h1, half h2);
-value_scalar operator+(half h2, short h1);
-value_scalar operator-(half h2, short h1);
-value_scalar operator*(half h2, short h1);
-value_scalar operator/(half h2, short h1);
-
-value_scalar operator+(unsigned short h1, half h2);
-value_scalar operator-(unsigned short h1, half h2);
-value_scalar operator*(unsigned short h1, half h2);
-value_scalar operator/(unsigned short h1, half h2);
-value_scalar operator+(half h2, unsigned short h1);
-value_scalar operator-(half h2, unsigned short h1);
-value_scalar operator*(half h2, unsigned short h1);
-value_scalar operator/(half h2, unsigned short h1);
-
-value_scalar operator+(int h1, half h2);
-value_scalar operator-(int h1, half h2);
-value_scalar operator*(int h1, half h2);
-value_scalar operator/(int h1, half h2);
-value_scalar operator+(half h2, int h1);
-value_scalar operator-(half h2, int h1);
-value_scalar operator*(half h2, int h1);
-value_scalar operator/(half h2, int h1);
-
-value_scalar operator+(unsigned int h1, half h2);
-value_scalar operator-(unsigned int h1, half h2);
-value_scalar operator*(unsigned int h1, half h2);
-value_scalar operator/(unsigned int h1, half h2);
-value_scalar operator+(half h2, unsigned int h1);
-value_scalar operator-(half h2, unsigned int h1);
-value_scalar operator*(half h2, unsigned int h1);
-value_scalar operator/(half h2, unsigned int h1);
-
-value_scalar operator+(long h1, half h2);
-value_scalar operator-(long h1, half h2);
-value_scalar operator*(long h1, half h2);
-value_scalar operator/(long h1, half h2);
-value_scalar operator+(half h2, long h1);
-value_scalar operator-(half h2, long h1);
-value_scalar operator*(half h2, long h1);
-value_scalar operator/(half h2, long h1);
-
-value_scalar operator+(unsigned long h1, half h2);
-value_scalar operator-(unsigned long h1, half h2);
-value_scalar operator*(unsigned long h1, half h2);
-value_scalar operator/(unsigned long h1, half h2);
-value_scalar operator+(half h2, unsigned long h1);
-value_scalar operator-(half h2, unsigned long h1);
-value_scalar operator*(half h2, unsigned long h1);
-value_scalar operator/(half h2, unsigned long h1);
-
-value_scalar operator+(long long h1, half h2);
-value_scalar operator-(long long h1, half h2);
-value_scalar operator*(long long h1, half h2);
-value_scalar operator/(long long h1, half h2);
-value_scalar operator+(half h2, long long h1);
-value_scalar operator-(half h2, long long h1);
-value_scalar operator*(half h2, long long h1);
-value_scalar operator/(half h2, long long h1);
-
-value_scalar operator+(unsigned long long h1, half h2);
-value_scalar operator-(unsigned long long h1, half h2);
-value_scalar operator*(unsigned long long h1, half h2);
-value_scalar operator/(unsigned long long h1, half h2);
-value_scalar operator+(half h2, unsigned long long h1);
-value_scalar operator-(half h2, unsigned long long h1);
-value_scalar operator*(half h2, unsigned long long h1);
-value_scalar operator/(half h2, unsigned long long h1);
-
-value_scalar operator+(float h1, half h2);
-value_scalar operator-(float h1, half h2);
-value_scalar operator*(float h1, half h2);
-value_scalar operator/(float h1, half h2);
-value_scalar operator+(half h2, float h1);
-value_scalar operator-(half h2, float h1);
-value_scalar operator*(half h2, float h1);
-value_scalar operator/(half h2, float h1);
-
-value_scalar operator+(double h1, half h2);
-value_scalar operator-(double h1, half h2);
-value_scalar operator*(double h1, half h2);
-value_scalar operator/(double h1, half h2);
-value_scalar operator+(half h2, double h1);
-value_scalar operator-(half h2, double h1);
-value_scalar operator*(half h2, double h1);
-value_scalar operator/(half h2, double h1);
-
-// value_scalar operator/(unsigned char h1, half h2);
-// value_scalar operator+=(short h1, half h2);
-// value_scalar operator+=(unsigned short h1, half h2);
-// value_scalar operator+(int h1, half h2);
-// value_scalar operator*(unsigned int h1, half h2);
-// value_scalar operator+=(long h1, half h2);
-// value_scalar operator+=(unsigned long h1, half h2);
-// value_scalar operator+=(long long h1, half h2);
-// value_scalar operator+=(double h1, half h2);
-// value_scalar operator-( half h2,char h1);
-// value_scalar operator/( half h2,unsigned char h1);
-// value_scalar operator+=( half h2,short h1);
-// value_scalar operator+=( half h2,unsigned short h1);
-// value_scalar operator+( half h2,int h1);
-// value_scalar operator*( half h2,unsigned int h1);
-// value_scalar  operator+=( half h2,long h1);
-// value_scalar operator+=( half h2,unsigned long h1);
-// value_scalar operator+=( half h2,long long h1);
+INSTANTIATE(char)
+INSTANTIATE(unsigned char)
+INSTANTIATE(short)
+INSTANTIATE(unsigned short)
+INSTANTIATE(int)
+INSTANTIATE(unsigned int)
+INSTANTIATE(long)
+INSTANTIATE(unsigned long)
+INSTANTIATE(long long)
+INSTANTIATE(unsigned long long)
+INSTANTIATE(float)
+INSTANTIATE(double)
 
 }
 #endif
--- lib/simple_half.cpp
+++ lib/simple_half.cpp
@@ -1,56 +1,38 @@
-
-#include"isaac/common/simple_half.h"
-#include"isaac/common/numeric_type.h"
-#include"isaac/value_scalar.h"
+#include "isaac/common/simple_half.h"
+#include "isaac/common/numeric_type.h"
+#include "isaac/value_scalar.h"
 namespace isaac{
 
 
 half::operator int() const{return (int)(us);}
-// half::  operator float()const {return (float)us;}
-// half:: operator double() const {return (double)us;}
-//   operator int() const {return 0;}
-//  half:: operator unsigned int() const {return (unsigned int)us;}
-// half:: operator short() const {return (short)us;}
-// half::  operator unsigned short() const {return us;}
-//  half:: operator char() const{return (char)us;}
-//   operator int() const{}
-//   operator int() const{}
 
-half half::operator+(half &h1)
+half half::operator+(half &h)
 {
-  return h1;
+  return h;
 }
-half half::operator-(half &h1 )
+half half::operator-(half &h)
 {
-  return h1;
+  return h;
 }
-half half::operator*(half &h1)
+half half::operator*(half &h)
 {
-  return h1;
+  return h;
 }
-half half::operator/(half &h1)
+half half::operator/(half &h)
 {
-  return h1;
+  return h;
 }
-half half::operator+=(half &h1)
+half half::operator+=(half &h)
 {
-  return h1;
+  return h;
 }
 
-half half::operator>(half &h1) { return h1; }
-half half::operator>=(half &h1) { return h1; }
-half half::operator<(half &h1) { return h1; }
-half half::operator<=(half &h1) { return h1; }
-half half::operator==(half &h1) { return h1; }
-half half::operator!=(half &h1) { return h1; }
-
-//half::half(char &x):us(x){}
-//half::half(int &x):us(x){}
-//half::half(unsigned short &x):us(x){}
-//half::half( short &x):us(x){}
-//half::half(unsigned int &x):us(x){}
-//half::half(float &x):us(x){}
-//half::half(double &x):us(x){}
+half half::operator>(half &h) { return h; }
+half half::operator>=(half &h) { return h; }
+half half::operator<(half &h) { return h; }
+half half::operator<=(half &h) { return h; }
+half half::operator==(half &h) { return h; }
+half half::operator!=(half &h) { return h; }
 
 half& half::operator=(const char x) { us = x; return *this; }
 half& half::operator=(const unsigned char x) { us = x; return *this; }
@@ -82,609 +64,64 @@ half::half(long &x) { us = x; }
 half::half(unsigned long &x) { us = x; }
 half::half() { }
 
-// half::half ( char x){us=x;}
-// half::half (unsigned char x){us=x;}
-// half::half ( int x){us=x;}
-// half::half (unsigned int x){us=x;}
-// half::half( short x){us=x;}
-// half::half ( unsigned short x){us=x;}
-// half::half ( float x){us=x;}
-// half::half ( double x){us=x;}
-// half::half( long x){us=x;}
-// half::half ( unsigned long x){us=x;}
 std::ostream & operator<<(std::ostream & os, half h)
 {
   return os<<h.us<<std::endl;
 }
 
-value_scalar operator+(char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
+#define INSTANTIATEVS  \
+  std::cout << "WARNING: This function has no practical significance, it will not give correct output." << std::endl;\
+  values_holder vh;\
+  vh.float16 = h;\
   return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
 
-value_scalar operator+(half h2, char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
+#define INSTANTIATE(TYPE)\
+  value_scalar operator+(TYPE, half h)\
+  {\
+    INSTANTIATEVS\
+  }\
+  value_scalar operator-(TYPE, half h)\
+  {\
+    INSTANTIATEVS\
+  }\
+  value_scalar operator*(TYPE, half h)\
+  {\
+    INSTANTIATEVS\
+  }\
+  value_scalar operator/(TYPE, half h)\
+  {\
+    INSTANTIATEVS\
+  }\
+   \
+  value_scalar operator+(half h, TYPE)\
+  {\
+    INSTANTIATEVS\
+  }\
+  value_scalar operator-(half h, TYPE)\
+  {\
+    INSTANTIATEVS\
+  }\
+  value_scalar operator*(half h, TYPE)\
+  {\
+    INSTANTIATEVS\
+  }\
+  value_scalar operator/(half h, TYPE)\
+  {\
+    INSTANTIATEVS\
+  }
 
-value_scalar operator+(unsigned char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(unsigned char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(unsigned char h1, half h2)
-{
-   values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(unsigned char h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, unsigned char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, unsigned char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, unsigned char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, unsigned char h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(unsigned short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(unsigned short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(unsigned short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(unsigned short h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, unsigned short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, unsigned short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, unsigned short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, unsigned short h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2,int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(unsigned int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(unsigned int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(unsigned int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(unsigned int h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, unsigned int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, unsigned int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, unsigned int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, unsigned int h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(unsigned long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(unsigned long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(unsigned long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(unsigned long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, unsigned long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, unsigned long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, unsigned long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, unsigned long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(unsigned long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(unsigned long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(unsigned long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(unsigned long long h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, unsigned long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, unsigned long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, unsigned long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, unsigned long long h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(float h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(float h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(float h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(float h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, float h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, float h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, float h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, float h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-
-value_scalar operator+(double h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(double h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(double h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(double h1, half h2)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator+(half h2, double h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator-(half h2, double h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator*(half h2, double h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
-value_scalar operator/(half h2, double h1)
-{
-  values_holder vh;
-  vh.float16 = h2;
-  return value_scalar(vh, HALF_TYPE);
-}
+INSTANTIATE(char)
+INSTANTIATE(unsigned char)
+INSTANTIATE(short)
+INSTANTIATE(unsigned short)
+INSTANTIATE(int)
+INSTANTIATE(unsigned int)
+INSTANTIATE(long)
+INSTANTIATE(unsigned long)
+INSTANTIATE(long long)
+INSTANTIATE(unsigned long long)
+INSTANTIATE(float)
+INSTANTIATE(double)
 
 }
 
