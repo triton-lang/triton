a68061aa9620342685dc61dd280a678f01c1ae18
Philippe Tillet <phil.tillet@gmail.com>
2017-01-17 20:25:28 -0500

    LICENSING: Switched to MIT

--- documentation/add-license.sh
+++ documentation/add-license.sh
@@ -1,4 +1,4 @@
-for i in $(find ../lib/ ../include/isaac/ ../python/src/bind -name '*.cpp' -or -name '*.hpp' -or -name '*.h' | egrep -v "../lib/external");
+for i in $(find ../lib/ ../include/isaac/ ../python/src/bind -name '*.cpp' -or -name '*.hpp' -or -name '*.h' | grep -v "../lib/external" | grep -v "../include/isaac/driver/external/");
 do
   if ! grep -q Copyright $i
   then
