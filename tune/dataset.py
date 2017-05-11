d3eb9c3b84685086b3353cd3e27c659941363520
Philippe Tillet <ptillet@g.harvard.edu>
2017-05-10 23:30:42 -0700

    Tune: No longer using default_context and default_stream

--- tune/dataset.py
+++ tune/dataset.py
@@ -18,9 +18,9 @@ def benchmarks(prefix, OpType, device, nsamples):
     step = 200
     
     X, Y = load(path, [('X', OpType.nparams), ( 'Y', 1)])
-    ctx = sc.driver.default_context()
-    stream = sc.driver.default_stream()
-    
+    ctx = sc.driver.Context(device)
+    stream = sc.driver.Stream(ctx)
+
     bufX, bufY = np.empty((step, X.shape[1])), np.empty((step, Y.shape[1]))
     param_ranges = OpType.param_ranges()
     
