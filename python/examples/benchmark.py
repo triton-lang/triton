8b38bab44c91be054c34f13334fa824a727229b2
Philippe Tillet <ptillet@g.harvard.edu>
2017-11-11 02:14:37 -0500

    [Python] Added conv3d wrapper in tensorflow

--- python/examples/benchmark.py
+++ python/examples/benchmark.py
@@ -7,23 +7,27 @@ from tensorflow.python.client import timeline
 
 isaac = tf.load_op_library(sc.tensorflow)
 
-
 # Session
 sess = tf.Session()
 options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 run_metadata = tf.RunMetadata()
 
 # Shapes to benchmark
-shapes = [(7, 7, 512, 8, 512, 3, 3)]
-print('Shapes\t\t\t\tISAAC\tcuDNN')
+shapes = [([7, 7], 512, 8, 512, [3, 3])]
+print('Shapes\t\t\t\tISAAC\tcuDN\tError')
 for shape in shapes:
-    (W, H, C, N, K, R, S) = shape
+    # Extract shapes
+    Ishape, C, N, K, Fshape = shape
+    dim = len(Ishape)
+    fmt = {2: 'NCHW', 3: 'NCDHW'}[dim]
+    tf_op = {2: tf.nn.conv2d, 3: tf.nn.conv3d}[dim]
+    sc_op = {2: isaac.conv2d, 3: isaac.conv3d}[dim]
     # Graph
-    A = tf.Variable(tf.random_uniform(shape=[N, C, H, W], seed=1), dtype=tf.float32)
-    filters = tf.Variable(tf.random_uniform(shape=[R, S, C, K], seed=1), dtype=tf.float32)
-    trans_filters = tf.Variable(tf.transpose(filters, [2, 0, 1, 3]))
-    y_tf = tf.nn.conv2d(input=A, filter=filters, strides=[1, 1, 1, 1], padding="SAME", data_format='NCHW')
-    y_sc = isaac.conv(input=A, filter=trans_filters, strides=[1, 1, 1, 1], padding="SAME", data_format='NCHW')
+    A = tf.Variable(tf.random_uniform(shape=[N, C] + Ishape, seed=1), dtype=tf.float32)
+    filters = tf.Variable(tf.random_uniform(shape= Fshape + [C, K], seed=1), dtype=tf.float32)
+    trans_filters = tf.Variable(tf.transpose(filters, [dim] + list(range(dim)) + [dim + 1]))
+    y_tf = tf_op(input=A, filter=filters, strides=[1]*(2 + dim), padding="SAME", data_format=fmt)
+    y_sc = sc_op(input=A, filter=trans_filters, strides=[1]*(2 + dim), padding="SAME", data_format=fmt)
     # Initialize
     sess.run(tf.global_variables_initializer())
     # Compute
@@ -33,6 +37,6 @@ for shape in shapes:
     t_sc = timeit.repeat(lambda: sess.run(tf.group(y_sc)), repeat=10, number=1)
     t_tf = timeit.repeat(lambda: sess.run(tf.group(y_tf)), repeat=10, number=1)
     # Log
-    num_ops = 2*N*C*H*W*R*S*K*1e-12
-    print('{}\t{:.5f}\t{:.5f}'.format(shape, min(t_sc), min(t_tf)))
+    num_ops = 2*C*N*K*np.prod(Ishape)*np.prod(Fshape)*1e-12
+    print('{}\t{:.2f}\t{:.2f}\t{:.3f}'.format(shape, min(t_sc)*1e3, min(t_tf)*1e3, error))
 
--- python/src/extensions/tensorflow.cpp
+++ python/src/extensions/tensorflow.cpp
@@ -18,7 +18,7 @@ using namespace tensorflow;
 using GPUDevice = Eigen::GpuDevice;
 namespace drv = isaac::driver;
 
-REGISTER_OP("Conv")
+REGISTER_OP("Conv2d")
     .Attr("T: {float}")
     .Input("input: T")
     .Input("filter: T")
@@ -28,61 +28,93 @@ REGISTER_OP("Conv")
     .Output("output: T");
 
 
+REGISTER_OP("Conv3d")
+    .Attr("T: {float}")
+    .Input("input: T")
+    .Input("filter: T")
+    .Attr("strides: list(int)")
+    .Attr("data_format: string")
+    .Attr("padding:  string")
+    .Output("output: T");
+
+template<size_t DIM>
 class ConvOp : public OpKernel {
  public:
-  explicit ConvOp(OpKernelConstruction* context) : OpKernel(context) {}
+  explicit ConvOp(OpKernelConstruction* context) : OpKernel(context) {
+    // Get attributes
+    OP_REQUIRES_OK(context, context->GetAttr("strides", &strides_));
+    string data_format;
+    OP_REQUIRES_OK(context, context->GetAttr("data_format", &data_format));
+    OP_REQUIRES(context, FormatFromString(data_format, &data_format_), errors::InvalidArgument("Invalid data format"));
+    OP_REQUIRES_OK(context, context->GetAttr("padding", &pad_));
+  }
 
   void Compute(OpKernelContext* context){
     GPUDevice device =  context->eigen_device<GPUDevice>();
     drv::Stream stream(device.stream(), false);
     isaac::DType dtype = isaac::FLOAT_TYPE;
 
+    /* Get inputs */
     const Tensor& inputs = context->input(0);
     const Tensor& filter = context->input(1);
 
-
+    /* Extract shapes */
+    // Input
+    int64 D = 1, H = 1, W = 1;
     const int64 N = inputs.dim_size(0);
-    const int64 D = 1;
     const int64 Ci = inputs.dim_size(1);
-    const int64 H = inputs.dim_size(2);
-    const int64 W = inputs.dim_size(3);
-
-    const int64 T = 1;
+    if(DIM > 2) D = inputs.dim_size(2);
+    if(DIM > 1) H = inputs.dim_size(2 + (DIM > 2));
+    if(DIM > 0) W = inputs.dim_size(2 + (DIM > 2) + (DIM > 1));
+    // Filter
+    int64 T = 1, R = 1, S = 1;
     const int64 Cf = filter.dim_size(0);
-    const int64 R = filter.dim_size(1);
-    const int64 S = filter.dim_size(2);
-    const int64 K = filter.dim_size(3);
+    if(DIM > 2) T = filter.dim_size(1);
+    if(DIM > 1) R = filter.dim_size(1 + (DIM > 2));
+    if(DIM > 0) S = filter.dim_size(1 + (DIM > 2) + (DIM > 1));
+    const int64 K = filter.dim_size(1 + DIM);
+    // Strides
+    int64_t stride_d = 1, stride_h = 1, stride_w = 1;
+    if(DIM > 2) stride_d = strides_[2];
+    if(DIM > 1) stride_h = strides_[2 + (DIM > 2)];
+    if(DIM > 0) stride_w = strides_[2 + (DIM > 2) + (DIM > 1)];
+    // Output
+    int64 M, P, Q, pad_d, pad_h, pad_w;
+    GetWindowedOutputSize(D, T, stride_d, pad_, &M, &pad_d);
+    GetWindowedOutputSize(H, R, stride_h, pad_, &P, &pad_h);
+    GetWindowedOutputSize(W, S, stride_w, pad_, &Q, &pad_w);
 
-    assert(Ci == Cf);
+
+    /* Requirements */
+    OP_REQUIRES(context, Ci==Cf, errors::InvalidArgument("input and filter must have the same depth: ", Ci, " vs ", Cf));
     const int64_t C = Ci;
 
-    const int64 stride_d = 1;
-    const int64 stride_h = 1;
-    const int64 stride_w = 1;
-    int64 M = 1, P, Q, pad_d, pad_h, pad_w;
-
-    Padding pad = SAME;
-    TensorFormat data_format = FORMAT_NCHW;
-    GetWindowedOutputSize(M, T, stride_d, pad, &M, &pad_d);
-    GetWindowedOutputSize(H, R, stride_h, pad, &P, &pad_h);
-    GetWindowedOutputSize(W, S, stride_w, pad, &Q, &pad_w);
-
+    /* Allocate output */
     Tensor* output = nullptr;
-    TensorShape out_shape = ShapeFromFormat(data_format, N, P, Q, K);
+    TensorShape out_shape;
+    if(DIM==3) out_shape = TensorShape({N, K, M, P, Q});
+    else if(DIM == 2) out_shape = TensorShape({N, K, P, Q});
+    else if(DIM == 1) out_shape = TensorShape({N, K, Q});
     OP_REQUIRES_OK(context, context->allocate_output(0, out_shape, &output));
 
+    /* Return early if possible */
     if (out_shape.num_elements() == 0)
       return;
 
+    /* Compute convolution */
     isaac::scalar alpha(1., dtype), beta(0., dtype);
     isaac::driver::Buffer I(stream.context(), (CUdeviceptr)inputs.flat<float>().data(), false);
     isaac::driver::Buffer F(stream.context(), (CUdeviceptr)filter.flat<float>().data(), false);
     isaac::driver::Buffer O(stream.context(), (CUdeviceptr)output->flat<float>().data(), false);
-
     isaac::CONV(stream.context().device(), stream, dtype,
                 N, K, M, P, Q, C, T, R, S, D, H, W, pad_d, pad_h, pad_w, stride_d, stride_h, stride_w, alpha, I, F, beta, O);
   }
 
+private:
+  Padding pad_;
+  TensorFormat data_format_;
+  std::vector<int32> strides_;
 };
 
-REGISTER_KERNEL_BUILDER(Name("Conv").Device(DEVICE_GPU), ConvOp);
+REGISTER_KERNEL_BUILDER(Name("Conv2d").Device(DEVICE_GPU), ConvOp<2>);
+REGISTER_KERNEL_BUILDER(Name("Conv3d").Device(DEVICE_GPU), ConvOp<3>);
