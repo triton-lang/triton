8dbb5652004f9080e6b86aa5ef36a822cb2df4ca
Philippe Tillet <ptillet@g.harvard.edu>
2019-01-03 00:55:24 -0500

    [general] added missing file

---
+++ include/ir/context_impl.h
@@ -0,0 +1,32 @@
+#ifndef TDL_INCLUDE_IR_CONTEXT_IMPL_H
+#define TDL_INCLUDE_IR_CONTEXT_IMPL_H
+
+#include <memory>
+#include <map>
+#include "ir/type.h"
+
+namespace tdl{
+namespace ir{
+
+class context;
+
+/* Context impl */
+class context_impl {
+public:
+  // constructors
+  context_impl(context &ctx);
+
+public:
+  // primitive types
+  type void_ty, label_ty, half_ty, float_ty, double_ty;
+  // derived types
+  integer_type int1_ty, int8_ty, int16_ty, int32_ty, int64_ty, int128_ty;
+  // Pointer types
+  std::map<std::pair<type*, unsigned>, pointer_type*> ptr_tys;
+  std::map<std::pair<type*,std::vector<unsigned>>, tile_type*> tile_tys;
+};
+
+}
+}
+
+#endif
