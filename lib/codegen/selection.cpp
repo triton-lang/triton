a6d39138b6ce6ceaad95eb124dff88ee42826f26
Philippe Tillet <ptillet@g.harvard.edu>
2019-07-18 16:35:48 -0700

    [dnn/gemm]: fixed  leading dimension in transposed variants

--- lib/codegen/selection.cpp
+++ lib/codegen/selection.cpp
@@ -1149,6 +1149,7 @@ void selection::lower_tile_instruction(ir::instruction *ins, llvm::IRBuilder<> &
       unsigned alignment = std::min(starting_multiple, max_contiguous);
       unsigned vector_size = std::min<unsigned>(result->axis(0).contiguous, alignment);
 //      vector_size = result->axis(0).contiguous;
+//      vector_size = 1;
       std::map<unsigned, Value*> packets;
       distributed_tile *TP = (distributed_tile*)tmap_.at(ld->get_pointer_operand());
       result->for_each([&](indices_t idx){
