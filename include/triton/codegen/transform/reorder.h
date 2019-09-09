0cbbcce5c0e8cd356704da71cc7c876e15813e36
Philippe Tillet <ptillet@g.harvard.edu>
2019-09-08 21:38:08 -0400

    added missing file

---
+++ include/triton/codegen/transform/reorder.h
@@ -0,0 +1,39 @@
+#ifndef TDL_INCLUDE_CODEGEN_OPTIMIZE_REORDER_H
+#define TDL_INCLUDE_CODEGEN_OPTIMIZE_REORDER_H
+
+#include <map>
+#include <vector>
+
+namespace triton {
+
+namespace ir {
+  class module;
+  class value;
+}
+
+namespace codegen{
+
+namespace analysis{
+  class align;
+  class meminfo;
+}
+
+namespace transform{
+
+class reorder {
+public:
+  reorder(analysis::align* algin, analysis::meminfo* mem);
+  std::vector<unsigned> get_order(ir::value* v);
+  void run(ir::module &mod);
+
+private:
+  analysis::align* align_;
+  analysis::meminfo* mem_;
+  std::map<ir::value*, std::vector<unsigned>> order_;
+};
+
+}
+}
+}
+
+#endif
