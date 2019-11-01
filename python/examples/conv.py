212e0175ec143c4a60fad35e2f0af14479dd63cc
Philippe Tillet <ptillet@g.harvard.edu>
2019-11-01 11:20:00 -0400

    [PYTHON][OPS] Convolution: Some cleaning of Triton-C kernel

--- python/examples/conv.py
+++ python/examples/conv.py
@@ -2,7 +2,7 @@ import torch
 import triton
 
 N, C, K = 32, 8, 32
-H, W = 4, 4
+H, W = 16, 16
 R, S = 3, 3
 torch.manual_seed(0)
 a = torch.randn(N, C, H, W).cuda()
@@ -11,6 +11,5 @@ b = torch.ones(C, R, S, K).cuda()
 rc = torch.nn.functional.conv2d(a, b.permute(3, 0, 1, 2))
 tc = triton.ops.conv(a, b)
 print((rc - tc).abs().max())
-print((tc[:,:,0,0] - rc[:,:,0,0]).abs())
 #print((rc[:30,:30,:,:] - tc[:30, :30, :, :]).abs().max())
 #print(tc[31, 31,:,:])
