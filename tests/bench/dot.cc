56330f7c83fe695b89dc77f44a2de6c266c28e3a
Philippe Tillet <ptillet@g.harvard.edu>
2020-08-07 00:49:04 -0400

    [CODEGEN][ANALYSIS] Fixed issue in layout inference

--- tests/bench/dot.cc
+++ tests/bench/dot.cc
@@ -9,11 +9,40 @@ int main() {
   // shapes to benchmark
   typedef std::tuple<std::vector<int>, bool, bool, int, int, int> config_t;
   std::vector<config_t> configs;
-  for(auto ord: std::vector<std::vector<int>>{{1, 0}})
-    for(auto x: std::vector<std::array<bool, 2>>{{false, false}}){
+  for(auto ord: std::vector<std::vector<int>>{{0, 1}})
+      for(auto x: std::vector<std::array<bool, 2>>{{false, true}, {false, false}, {true, false}, {true, true}}){
     std::vector<config_t> tmp = {
+//      config_t{ord, x[0], x[1], 128, 128, 128},
+//      config_t{ord, x[0], x[1], 256, 256, 256},
+//      config_t{ord, x[0], x[1], 384, 384, 384},
 //      config_t{ord, x[0], x[1], 512, 512, 512},
-      config_t{ord, x[0], x[1], 1024, 1024, 1024},
+//      config_t{ord, x[0], x[1], 768, 768, 768},
+//      config_t{ord, x[0], x[1], 1024, 1024, 1024},
+//      config_t{ord, x[0], x[1], 1280, 1280, 1280},
+//      config_t{ord, x[0], x[1], 1536, 1536, 1536},
+//      config_t{ord, x[0], x[1], 2048, 2048, 2048},
+      config_t{ord, x[0], x[1], 8192, 8192, 8192},
+
+//      config_t{ord, x[0], x[1], 256, 16, 256},
+//      config_t{ord, x[0], x[1], 512, 16, 512},
+//      config_t{ord, x[0], x[1], 768, 16, 768},
+//      config_t{ord, x[0], x[1], 1024, 16, 1024},
+//      config_t{ord, x[0], x[1], 1280, 16, 1280},
+//      config_t{ord, x[0], x[1], 1536, 16, 1536},
+//      config_t{ord, x[0], x[1], 2048, 16, 2048},
+//      config_t{ord, x[0], x[1], 3072, 16, 3072},
+//      config_t{ord, x[0], x[1], 4096, 16, 4096},
+//      config_t{ord, x[0], x[1], 5120, 16, 5120},
+//      config_t{ord, x[0], x[1], 6144, 16, 6144},
+//      config_t{ord, x[0], x[1], 7168, 16, 7168},
+
+//      config_t{ord, x[0], x[1], 64, 64, 4096},
+//      config_t{ord, x[0], x[1], 64, 64, 8192},
+//      config_t{ord, x[0], x[1], 64, 64, 16384},
+//      config_t{ord, x[0], x[1], 64, 64, 32768},
+//      config_t{ord, x[0], x[1], 64, 64, 65536},
+//      config_t{ord, x[0], x[1], 64, 64, 131072}
+
 //      config_t{ord, x[0], x[1], 127008, 768, 576},
 //      config_t{ord, x[0], x[1], 8192, 8192, 8192}
 //      config_t{ord, x[0], x[1], 16, 2048, 2048},
@@ -36,7 +65,7 @@ int main() {
   for(const auto& c: configs){
     std::tie(ord, AT, BT, M, N, K) = c;
     std::cout << "// " << c ;
-    for(auto perf: bench_dot(stream, FLOAT, AT, BT, M, N, K, ord, ord))
+    for(auto perf: bench_dot(stream, HALF, AT, BT, M, N, K, ord, ord))
       std::cout << ", " << perf << std::flush;
     std::cout << std::endl;
   }
--- tests/common/src/dot.h
+++ tests/common/src/dot.h
@@ -6,13 +6,15 @@ __global__ void dot(TYPE * A __noalias __readonly __aligned(16),
                     TYPE * B __noalias __readonly __aligned(16),
                     TYPE * C __noalias __aligned(16),
                     float alpha,
-                    int M, int N, int K,
+                    int M, int N, int K __multipleof(16),
                     int lda __multipleof(8),
                     int ldb __multipleof(8),
-                    int ldc __multipleof(8)) {
+                    int ldc __multipleof(8),
+                    int* locks) {
       // prologue
       int ridx = get_program_id(0);
       int ridy = get_program_id(1);
+      int ridz = get_program_id(2);
       int gridx = M / TM;
       int gridy = N / TN;
       int rid = ridx + ridy * gridx;
@@ -20,7 +22,10 @@ __global__ void dot(TYPE * A __noalias __readonly __aligned(16),
       ridy = rid % gridy;
       int rm[TM] = ridx * TM + 0 ... TM;
       int rn[TN] = ridy * TN + 0 ... TN;
-      int rk[TK] = 0 ... TK;
+
+      // reduction splitting
+      K           = K / TZ;
+      int rk[TK]  = ridz * K + 0 ... TK;
 
       // pointers to operands
       int offa[SHAPE_A] = rk[BROADCAST_AK] * STRIDE_AK + rm[BROADCAST_AM] * STRIDE_AM;
@@ -35,9 +40,9 @@ __global__ void dot(TYPE * A __noalias __readonly __aligned(16),
       TYPE b[SHAPE_B] = checkb ? *pb : 0;
 
       // reduction loop
-      float c[TM, TN] = 0;
+      float acc[TM, TN] = 0;
       for(int k = K; k > 0; k -= TK){
-        c += USEA @ USEB;
+        acc += USEA @ USEB;
         bool checka[SHAPE_A] = k > TK;
         bool checkb[SHAPE_B] = k > TK;
         pa += TK * STRIDE_AK;
@@ -45,7 +50,8 @@ __global__ void dot(TYPE * A __noalias __readonly __aligned(16),
         a = *?(checka)pa;
         b = *?(checkb)pb;
       }
-      //c = c * alpha;
+      acc = acc * alpha;
+      TYPE c[TM, TN] = acc;
 
       // epilogue
       int rxm[TM] = get_program_id(0) * TM + 0 ... TM;
@@ -53,7 +59,22 @@ __global__ void dot(TYPE * A __noalias __readonly __aligned(16),
       int offc[TM, TN] = rxm[:, newaxis] * ldc + rxn[newaxis, :];
       TYPE* pc[TM, TN] = C + offc;
       bool checkc[TM, TN] = (rxm[:, newaxis] < M) && (rxn[newaxis, :] < N);
-      *?(checkc)pc = (TYPE[TM, TN])c;
+
+#if (TZ==1)
+      *?(checkc) pc = c;
+#else
+      // accumulate partial result using spin-locks
+      int *plock  = locks + rid;
+      int *pcount = plock + get_num_programs(0) * get_num_programs(1);
+      for(int repeat = 1; repeat == 1; repeat = atomic_cas(plock, 0, 1));
+      int count = *pcount;
+      if(count == 0)
+        *?(checkc) pc = c;
+      else
+        *?(checkc) pc = c + *?(checkc)pc;
+      atomic_xchg(pcount, (count + 1) % TZ);
+      atomic_xchg(plock, 0);
+#endif
 }
 )";
 
