526a3468e01f399b852cd2c0facdebced63239b3
Philippe Tillet <ptillet@g.harvard.edu>
2020-09-03 14:36:30 -0400

    [Version] Now version 0.2.3

--- python/setup.py
+++ python/setup.py
@@ -105,7 +105,7 @@ for d in directories:
 
 setup(
     name='triton',
-    version='0.2.2',
+    version='0.2.3',
     author='Philippe Tillet',
     author_email='ptillet@g.harvard.edu',
     description='A language and compiler for custom Deep Learning operations',
