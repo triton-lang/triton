f81da73b6a912dfb130d9210a87d05515fcd3a3a
Philippe Tillet <phil@openai.com>
2021-01-30 18:09:49 -0500

    [PYTHON] Added utility to read single Triton kernel from provided file in triton.read

--- python/triton/ops/softmax.py
+++ python/triton/ops/softmax.py
@@ -2,23 +2,22 @@ import torch
 import triton
 import os
 
-kernels = dict()
-def get_kernel(block, dtype, device):
+fwd_src = triton.read(os.path.join(os.path.dirname(__file__), 'softmax.c'), kernel_names=['forward'])
+fwd_kernels = dict()
+def get_fwd_kernel(block, dtype, device):
     key = (block, dtype, device)
-    if key not in kernels:
-        src = triton.read(os.path.join(os.path.dirname(__file__), 'softmax.c'))
+    if key not in fwd_kernels:
         defines = {'BLOCK': block, 'TYPE': dtype}
-        kernels[key] = triton.kernel(src, device = device, defines = defines)
-    return kernels[key]
+        fwd_kernels[key] = triton.kernel(fwd_src, device = device, defines = defines)
+    return fwd_kernels[key]
 
 
 class _softmax(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, x):
         y = torch.empty_like(x)
         M, N = x.shape
-        kernel = get_kernel(N, x.dtype, x.device)
+        kernel = get_fwd_kernel(N, x.dtype, x.device)
         kernel(x.data_ptr(), y.data_ptr(), grid = lambda opt: [M, ])
         return y
 
