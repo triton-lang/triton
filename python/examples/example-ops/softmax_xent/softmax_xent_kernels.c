bc37fe81c0582478957a15b5b7248cf7b6da0bef
Jared <jareddk@gmail.com>
2021-02-05 14:09:26 -0500

    updated kernels

--- python/examples/example-ops/softmax_xent/softmax_xent_kernels.c
+++ python/examples/example-ops/softmax_xent/softmax_xent_kernels.c
@@ -23,11 +23,13 @@ __global__ void softmax_fwd(TYPE *logit,
 
 __global__ void softmax_bwd(TYPE *neg_logprobs,
                     long *indices,
-                    TYPE *dneg_logprobs) {
+                    TYPE *dneg_logprobs,
+                    int n_vocab) {
 
         int token_idx = get_program_id(0);
 
-        int offset[TILE] = token_idx * TILE + 0 ... TILE;
+        bool check[TILE] = ((0 ... TILE) < n_vocab);
+        int offset[TILE] = token_idx * n_vocab + 0 ... TILE;
         TYPE* px[TILE]  = neg_logprobs + offset;
         long local_ind = *(indices + token_idx);
         TYPE local_dn = *(dneg_logprobs + token_idx);
@@ -35,14 +37,17 @@ __global__ void softmax_bwd(TYPE *neg_logprobs,
         // We know d(-log(p[i])/dlogit[k] = -id_mat[i,k] + p[k]
         // and we have -log(p[k]) stored, so this is easy
 
-        *px = exp(-(float[TILE]) *px);
+        TYPE intermediate[TILE] = check ? exp(-(float[TILE]) *px) : 0;
 
         // Do one hot
 
         // selected_logit_idx is selected logit index for our token
         bool find_one[TILE] = ((0 ... TILE) == local_ind);
-        *px = *px - (TYPE[TILE]) find_one;
+        intermediate = intermediate - ((TYPE[TILE]) find_one);
+        //*?(check)px = *px - ((TYPE[TILE]) find_one);
 
         // multiply by dneg_logprobs
-        *px = (*px) * local_dn;
+        __debug_barrier();
+        *?(check)px = intermediate * local_dn;
+        //*?(check)px = (*px) * local_dn;
     }
