dffd66bc83ffc995e1719d472f14bbf837a84018
Philippe Tillet <phil@openai.com>
2021-02-07 15:06:57 -0500

    [PYTHON] Made codebase pep8 compliant

--- python/triton/ops/softmax.py
+++ python/triton/ops/softmax.py
@@ -4,13 +4,13 @@ import os
 
 fwd_src = triton.read(os.path.join(os.path.dirname(__file__), 'softmax.c'), kernel_names=['forward'])
 fwd_kernels = dict()
+
 def get_fwd_kernel(block, dtype, device):
     key = (block, dtype, device)
     if key not in fwd_kernels:
         defines = {'BLOCK': block, 'TYPE': dtype}
-        fwd_kernels[key] = triton.kernel(fwd_src, device = device, defines = defines)
+        fwd_kernels[key] = triton.kernel(fwd_src, device=device, defines=defines)
     return fwd_kernels[key]
-
 
 class _softmax(torch.autograd.Function):
     @staticmethod
@@ -18,9 +18,8 @@ class _softmax(torch.autograd.Function):
         y = torch.empty_like(x)
         M, N = x.shape
         kernel = get_fwd_kernel(N, x.dtype, x.device)
-        kernel(x.data_ptr(), y.data_ptr(), grid = lambda opt: [M, ])
+        grid = lambda opt: (M, )
+        kernel(x.data_ptr(), y.data_ptr(), grid=grid)
         return y
 
 softmax = _softmax.apply
-        
-
