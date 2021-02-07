861c46c43bdee212ff91a9b8bdb6c75b13e4753b
Philippe Tillet <phil@openai.com>
2021-02-07 16:46:47 -0500

    [PYTHON] Removed support for dense softmax
    Interest seems limited now that it is fused in cross_entropy. Will
    likely re-add once it's easier to share code between ops

--- python/tests/test_softmax.py
+++
@@ -1,8 +0,0 @@
-import torch
-import triton
-
-def test_op(M = 1024, N = 1024, dtype = torch.float32):
-    x = torch.randn(M, N, dtype=dtype, device='cuda')
-    th_y = torch.softmax(x, dim=-1)
-    tt_y = triton.ops.softmax(x)
-    assert torch.allclose(tt_y, th_y)
