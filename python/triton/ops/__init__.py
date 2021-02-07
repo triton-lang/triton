66c94f21d7ac817dfd3ae27ab9058c4eb1e4da78
Philippe Tillet <phil@openai.com>
2021-02-07 17:02:11 -0500

    [PYTHON] Removed .softmax from ops/__init__.py following previous commit

--- python/triton/ops/__init__.py
+++ python/triton/ops/__init__.py
@@ -1,5 +1,4 @@
 from .conv import _conv, conv
 from .matmul import _matmul, matmul
-from .softmax import _softmax, softmax
 from .cross_entropy import _cross_entropy, cross_entropy
 from . import blocksparse
