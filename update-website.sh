d35d6cd77f8b4cec70cc1b780d681aedb873801d
Philippe Tillet <phil@openai.com>
2021-03-06 22:09:04 -0500

    [GH-PAGES] Added small script to automatically update website (assuming it's been compiled in master)

---
+++ update-website.sh
@@ -0,0 +1,10 @@
+rm -r /tmp/triton-docs; 
+mkdir /tmp/triton-docs;
+cp -r CNAME /tmp/triton-docs/
+cp -r .nojekyll /tmp/triton-docs/
+cp -r update-website.sh /tmp/triton-docs/
+cp -r docs/_build/html/* /tmp/triton-docs/
+rm -r *
+cp -r /tmp/triton-docs/* .
+git add .
+git commit -am "[GH-PAGES] Updated website"
