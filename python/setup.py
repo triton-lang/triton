f1999eb7c091c3d01cd0ecf2b95c1b10c0dad18c
Philippe Tillet <phil@openai.com>
2021-03-21 16:11:51 -0400

    Creating pipeline for building wheels

--- python/setup.py
+++ python/setup.py
@@ -103,10 +103,10 @@ setup(
     url="https://github.com/ptillet/triton/",
     download_url="https://github.com/ptillet/triton/archive/v0.1.tar.gz",
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
     ],
 )
