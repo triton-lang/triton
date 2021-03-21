9a0ed19994032df4ab72b42896ed2bf60d74c9e1
Philippe Tillet <phil@openai.com>
2021-03-21 16:59:41 -0400

    Removed symlinks

--- python/setup.py
+++ python/setup.py
@@ -68,7 +68,7 @@ class CMakeBuild(build_ext):
         env = os.environ.copy()
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        sourcedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "src"))
+        sourcedir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
         subprocess.check_call(["cmake", sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
 
