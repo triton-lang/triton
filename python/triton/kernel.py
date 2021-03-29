5236524f1cc1560c9c18f34f5a5a71c96e4ac10c
Philippe Tillet <ptillet@g.harvard.edu>
2021-03-29 02:35:13 -0400

    [DOCS] Various improvements and typo fixes

--- python/triton/kernel.py
+++ python/triton/kernel.py
@@ -1,32 +1,44 @@
 import os
 import struct
-from typing import Optional, Dict, List
+from typing import Optional, Dict, List, Callable
 import torch
-# C bindings
 import triton._C.libtriton.triton as _triton
 
 codes = {
-    _triton.runtime.arg_type.int1: 'B', _triton.runtime.arg_type.int8: 'B', _triton.runtime.arg_type.int32: 'I',
-    _triton.runtime.arg_type.int64: 'Q', _triton.runtime.arg_type.half: 'H', _triton.runtime.arg_type.float: 'f',
-    _triton.runtime.arg_type.double: 'd', _triton.runtime.arg_type.buffer: 'P'
+    _triton.runtime.arg_type.int1: 'B',
+    _triton.runtime.arg_type.int8: 'B',
+    _triton.runtime.arg_type.int32: 'I',
+    _triton.runtime.arg_type.int64: 'Q',
+    _triton.runtime.arg_type.half: 'H',
+    _triton.runtime.arg_type.float: 'f',
+    _triton.runtime.arg_type.double: 'd',
+    _triton.runtime.arg_type.buffer: 'P',
 }
 
 
 def th_to_triton(obj):
+    """ Convert a `torch.dtype` to a Triton-C type string. """
     tys = {
-        torch.int8: 'char', torch.int16: 'short', torch.int32: 'int', torch.int64: 'long',\
-        torch.float16: 'half', torch.float32: 'float', torch.float64: 'double'
+        torch.int8: 'char',
+        torch.int16: 'short',
+        torch.int32: 'int',
+        torch.int64: 'long',
+        torch.float16: 'half',
+        torch.float32: 'float',
+        torch.float64: 'double',
     }
     if isinstance(obj, torch.dtype):
         return tys[obj]
     return str(obj)
 
 
-def cdiv(a, b):
+def cdiv(a: int, b: int) -> int:
+    """ Ceil division (a + b - 1) // b"""
     return (a + b - 1) // b
 
 
-def read(path, kernel_names: Optional[List] = None):
+def read(path: str, kernel_names: Optional[List] = None) -> str:
+    """ Extracts the source code for `kernel_names` from the given `path` file."""
     if kernel_names is None:
         kernel_names = []
     with open(path, 'r') as f:
@@ -39,19 +51,31 @@ config = _triton.runtime.config
 
 
 class kernel:
+    """
+    A class used to represent a Triton kernel.
+    """
     def __init__(
         self,
-        src,
-        device,
+        src: str,
+        device: torch.device,
         defines: Optional[Dict] = None,
         num_warps: int = 4,
-        autotune_vals: Optional[List] = None,
+        autotune_configs: Optional[List] = None,
         autotune_key: Optional[List] = None
     ):
+        """
+        :param src: The source code of the kernel.
+        :param device: The device to compile the kernel for.
+        :param defines: A dictionary of preprocessor #define for the compiler.
+        :param num_warps: Optimization flag for the compiler's internal auto-parallelization engine.
+        :param autotune_configs: A list of triton.config objects for the autotuner to try.
+        :param autotune_key: A list of kernel argument names whose change in value should trigger the autotuner to re-run.
+        """
+
         if defines is None:
             defines = {}
-        if autotune_vals is None:
-            autotune_vals = []
+        if autotune_configs is None:
+            autotune_configs = []
         if autotune_key is None:
             autotune_key = []
         # check if src is empty
@@ -74,11 +98,17 @@ class kernel:
         self.opt = _triton.runtime.options()
         self.opt.defines = {k: th_to_triton(v) for k, v in defines.items()}
         self.opt.num_warps = num_warps
-        # autotune_vals = [({}, 4)]
-        self.fn = _triton.runtime.function(self.src, self.opt, self.device, autotune_vals, autotune_key)
+        # autotune_configs = [({}, 4)]
+        self.fn = _triton.runtime.function(self.src, self.opt, self.device, autotune_configs, autotune_key)
         self.tys = ''.join([codes[x] for x in self.fn.signature()])
 
-    def __call__(self, *args, grid):
+    def __call__(self, *args, grid: Callable[[_triton.runtime.options], tuple]):
+        """
+        Runs the kernel on the given arguments and launch grid.
+        :param args: The arguments to the kernel in the orders that they appear in the Triton-C source.
+        :param grid: The launch grid for the kernel, i.e., callable that transform compilation options into a tuple of at most 3 integers.
+        :return: None
+        """
         # make sure that the executing thread is on the right device
         torch.cuda.set_device(self.device_id)
         # pack parameters into a byte buffer
