c4fbeeea679a3e169f5651af88f113bfc749f71b
Philippe Tillet <phil@openai.com>
2021-05-16 18:17:14 -0400

    [PYTHON] Fixed-up `setup.py`

--- python/setup.py
+++ python/setup.py
@@ -96,7 +96,7 @@ setup(
     author_email="phil@openai.com",
     description="A language and compiler for custom Deep Learning operations",
     long_description="",
-    packages=["triton", "triton/_C", "triton/ops", "triton/ops/blocksparse"],
+    packages=["triton", "triton/_C", "triton/tools", "triton/ops", "triton/ops/blocksparse"],
     install_requires=["numpy", "torch"],
     package_data={"triton/ops": ["*.c"], "triton/ops/blocksparse": ["*.c"]},
     include_package_data=True,
