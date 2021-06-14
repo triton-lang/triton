55011d90edf35b84b4d72490751e1f84ceda8d5a
Philippe Tillet <ptillet@g.harvard.edu>
2021-06-14 12:22:48 -0400

    [CODEGEN] Bugfix in membar pass (#124)
    Membar pass on top of master is buggy with asynchronous copy. For example, it doesn't wait for asynchronous copies to complete before recoalescing accumulator in GEMM, which leads to undefined behavior when the program doesn't enter the loop. This PR proposes

--- lib/codegen/pass.cc
+++ lib/codegen/pass.cc
@@ -92,7 +92,9 @@ void add_passes_to_emit_bin(ir::module &ir, driver::device *dev, int num_warps,
   liveness.run(ir);
   allocation.run(ir);
   prefetch_s.run(ir);
+//  ir::print(ir, std::cout);
   barriers.run(ir);
+//  ir::print(ir, std::cout);
   // ir::print(ir, std::cout);
   isel.visit(ir, *llvm);
   mod = driver::module::create(dev, std::move(llvm));
