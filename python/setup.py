8c732eb024364ce534e51cfc4b642a5f3a6a737d
Philippe Tillet <phil@openai.com>
2021-07-28 01:51:17 -0700

    [GENERAL] Some minor improvements here and there to build systems and docs (#148)

--- python/setup.py
+++ python/setup.py
@@ -23,12 +23,13 @@ def get_llvm():
     paths = [distutils.spawn.find_executable(cfg) for cfg in supported]
     paths = [p for p in paths if p is not None]
     if paths:
-      return paths[0]
+      return '', ''
     # download if nothing is installed
     name = 'clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04'
     dir = '/tmp'
-    llvm_config = '{dir}/{name}/bin/llvm-config'.format(dir=dir, name=name)
-    if not os.path.exists(llvm_config):
+    llvm_include_dir = '{dir}/{name}/include'.format(dir=dir, name=name)
+    llvm_library_dir = '{dir}/{name}/lib'.format(dir=dir, name=name)
+    if not os.path.exists(llvm_library_dir):
         try:
             shutil.rmtree(os.path.join(dir, name))
         except:
@@ -38,7 +39,7 @@ def get_llvm():
         ftpstream = urllib.request.urlopen(url)
         file = tarfile.open(fileobj=ftpstream, mode="r|xz")
         file.extractall(path=dir)
-    return llvm_config
+    return llvm_include_dir, llvm_library_dir
 
 
 class CMakeExtension(Extension):
@@ -76,7 +77,7 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        llvm_config = get_llvm()
+        llvm_include_dir, llvm_library_dir = get_llvm()
         # self.debug = True
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.path)))
         # create build directories
@@ -88,12 +89,12 @@ class CMakeBuild(build_ext):
             os.makedirs(llvm_build_dir)
         # python directories
         python_include_dirs = [distutils.sysconfig.get_python_inc()] + ['/usr/local/cuda/include']
-        python_lib_dirs = distutils.sysconfig.get_config_var("LIBDIR")
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
             "-DBUILD_TUTORIALS=OFF",
             "-DBUILD_PYTHON_MODULE=ON",
-            "-DLLVM_CONFIG=" + llvm_config,
+            "-DLLVM_INCLUDE_DIRS=" + llvm_include_dir,
+            "-DLLVM_LIBRARY_DIR=" + llvm_library_dir,
             #'-DPYTHON_EXECUTABLE=' + sys.executable,
             #'-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON',
             "-DTRITON_LLVM_BUILD_DIR=" + llvm_build_dir,
@@ -126,7 +127,7 @@ setup(
     description="A language and compiler for custom Deep Learning operations",
     long_description="",
     packages=["triton", "triton/_C", "triton/tools", "triton/ops", "triton/ops/blocksparse"],
-    install_requires=["numpy", "torch"],
+    install_requires=["torch"],
     package_data={"triton/ops": ["*.c"], "triton/ops/blocksparse": ["*.c"]},
     include_package_data=True,
     ext_modules=[CMakeExtension("triton", "triton/_C/")],
