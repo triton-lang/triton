f371d04d6d229c88cbd390fd4e0af8173bce5987
Philippe Tillet <phil@openai.com>
2021-09-10 11:05:44 -0700

    [PYTHON] Now providing triton.next_power_of_2 (#273)

--- python/triton/code_gen.py
+++ python/triton/code_gen.py
@@ -833,6 +833,16 @@ def jit(fn):
 def cdiv(x, y):
     return (x + y - 1) // y
 
+def next_power_of_2(n):
+    """Return the smallest power of 2 greater than or equal to n"""
+    n -= 1
+    n |= n >> 1
+    n |= n >> 2
+    n |= n >> 4
+    n |= n >> 8
+    n |= n >> 16
+    n += 1
+    return n
 
 ######
 
