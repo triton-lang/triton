0f3ac94cf7e3f2fd3acd72216ce3544b74550858
Philippe Tillet <phil@openai.com>
2021-09-14 01:33:13 -0700

    .

--- python/test/regression/test_performance.py
+++ python/test/regression/test_performance.py
@@ -16,8 +16,6 @@ def nvsmi(attrs):
     ret = out.decode(sys.stdout.encoding).split(',')
     ret = [int(x) for x in ret]
     return ret
-do_bench = lambda fn: triton.testing.do_bench(fn, percentiles=None, warmup=10, rep=250)
-
 
 
 #######################
@@ -26,22 +24,22 @@ do_bench = lambda fn: triton.testing.do_bench(fn, percentiles=None, warmup=10, r
 
 matmul_data = {
   # square
-  (256 , 256 , 256  ) : {'v100': 0.026},
-  (512 , 512 , 512  ) : {'v100': 0.140},
-  (1024, 1024, 1024 ) : {'v100': 0.461},
-  (2048, 2048, 2048 ) : {'v100': 0.670},
-  (4096, 4096, 4096 ) : {'v100': 0.801},
-  (8192, 8192, 8192 ) : {'v100': 0.858},
+  (256 , 256 , 256  ) : {'v100': 0.027},
+  (512 , 512 , 512  ) : {'v100': 0.141},
+  (1024, 1024, 1024 ) : {'v100': 0.466},
+  (2048, 2048, 2048 ) : {'v100': 0.680},
+  (4096, 4096, 4096 ) : {'v100': 0.831},
+  (8192, 8192, 8192 ) : {'v100': 0.841},
   # tall-skinny
-  (16  , 1024, 1024 ) : {'v100': 0.0126},
-  (16  , 4096, 4096 ) : {'v100': 0.0536},
-  (16  , 8192, 8192 ) : {'v100': 0.0957},
-  (64  , 1024, 1024 ) : {'v100': 0.0486},
-  (64  , 4096, 4096 ) : {'v100': 0.204},
-  (64  , 8192, 8192 ) : {'v100': 0.337},
-  (1024, 64  , 1024 ) : {'v100': 0.0458},
-  (4096, 64  , 4096 ) : {'v100': 0.152},
-  (8192, 64  , 8192 ) : {'v100': 0.302},
+  (16  , 1024, 1024 ) : {'v100': 0.0128},
+  (16  , 4096, 4096 ) : {'v100': 0.0558},
+  (16  , 8192, 8192 ) : {'v100': 0.101},
+  (64  , 1024, 1024 ) : {'v100': 0.049},
+  (64  , 4096, 4096 ) : {'v100': 0.211},
+  (64  , 8192, 8192 ) : {'v100': 0.360},
+  (1024, 64  , 1024 ) : {'v100': 0.0469},
+  (4096, 64  , 4096 ) : {'v100': 0.198},
+  (8192, 64  , 8192 ) : {'v100': 0.323},
 #   # deep reductions
 #   (64  , 64  , 16384) : {'v100': 0.},
 #   (64  , 64  , 65536) : {'v100': 0.},
@@ -57,7 +55,8 @@ def test_matmul(M, N, K):
     assert cur_sm_clock == ref_sm_clock, f'GPU SMs must run at {ref_sm_clock} MHz'
     a = torch.randn((M, K), dtype=torch.float16, device='cuda')
     b = torch.randn((K, N), dtype=torch.float16, device='cuda')
-    ms = do_bench(lambda: triton.ops.matmul(a, b))
+    fn = lambda: triton.ops.matmul(a, b)
+    ms = triton.testing.do_bench(fn, percentiles=None, warmup=10, rep=1000)
     cur_gpu_perf = 2.*M*N*K/ms * 1e-9
     cur_gpu_util = cur_gpu_perf / max_gpu_perf
     triton.testing.assert_almost_equal(cur_gpu_util, ref_gpu_util, decimal=2)
@@ -82,12 +81,12 @@ def _add(x_ptr, y_ptr, output_ptr, n_elements, **meta):
 
 elementwise_data = {
   1024*16   : {'v100': 0.0219},
-  1024*64   : {'v100': 0.0843},
-  1024*256  : {'v100': 0.248},
-  1024*1024 : {'v100': 0.527},
-  1024*4096 : {'v100': 0.776},
-  1024*16384: {'v100': 0.880},
-  1024*65536: {'v100': 0.911},
+  1024*64   : {'v100': 0.0791},
+  1024*256  : {'v100': 0.243},
+  1024*1024 : {'v100': 0.534},
+  1024*4096 : {'v100': 0.796},
+  1024*16384: {'v100': 0.905},
+  1024*65536: {'v100': 0.939},
 }
 
 @pytest.mark.parametrize('N', elementwise_data.keys())
@@ -101,7 +100,8 @@ def test_elementwise(N):
     x = torch.randn_like(z)
     y = torch.randn_like(z)
     grid = lambda meta: (triton.cdiv(N, meta['BLOCK_SIZE']), )
-    ms = do_bench(lambda: _add[grid](x, y, z, N, BLOCK_SIZE=1024))
+    fn = lambda: _add[grid](x, y, z, N, BLOCK_SIZE=1024)
+    ms = triton.testing.do_bench(fn, percentiles=None, warmup=10, rep=250)
     cur_gpu_perf = 3.*N*z.element_size()/ms*1e-6
     cur_gpu_util = cur_gpu_perf / max_gpu_perf
     triton.testing.assert_almost_equal(cur_gpu_util, ref_gpu_util, decimal=2)
