68c7d1db2edfc1a27232811fcdf77f7cd383c5c4
Yan Da <dyanab@connect.ust.hk>
2021-09-22 16:12:30 +0000

    Remove num_stages in the config

--- python/triton/ops/block_reduce.py
+++ python/triton/ops/block_reduce.py
@@ -7,16 +7,16 @@ import triton
 '''
 @triton.autotune(
     configs=[
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 256}, num_stages=3, num_warps=8),
-        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 128}, num_stages=3, num_warps=8),
-        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 64},  num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64 , 'BLOCK_N': 256}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 64},  num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64 , 'BLOCK_N': 128}, num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 32},  num_stages=4, num_warps=4),
-        triton.Config({'BLOCK_M': 64 , 'BLOCK_N': 32},  num_stages=5, num_warps=2),
-        triton.Config({'BLOCK_M': 32 , 'BLOCK_N': 64},  num_stages=5, num_warps=2),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 256}, num_warps=8),
+        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 128}, num_warps=8),
+        triton.Config({'BLOCK_M': 256, 'BLOCK_N': 64},  num_warps=4),
+        triton.Config({'BLOCK_M': 64 , 'BLOCK_N': 256}, num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 128}, num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 64},  num_warps=4),
+        triton.Config({'BLOCK_M': 64 , 'BLOCK_N': 128}, num_warps=4),
+        triton.Config({'BLOCK_M': 128, 'BLOCK_N': 32},  num_warps=4),
+        triton.Config({'BLOCK_M': 64 , 'BLOCK_N': 32},  num_warps=2),
+        triton.Config({'BLOCK_M': 32 , 'BLOCK_N': 64},  num_warps=2),
     ],
     key=['SPLIT_K', 'M', 'N'],
 )
