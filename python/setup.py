3959a06cb7e19581593fe00ac2567836f9588784
Philippe Tillet <phil@openai.com>
2021-09-27 21:41:59 -0700

    [VERSION] Bumped to 1.1

--- python/setup.py
+++ python/setup.py
@@ -121,7 +121,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="triton",
-    version="1.0.1",
+    version="1.1.0",
     author="Philippe Tillet",
     author_email="phil@openai.com",
     description="A language and compiler for custom Deep Learning operations",
