11101f081607b15adad7faeaaf30791adca06167
Victor Nova <lostfreeman@gmail.com>
2021-12-03 11:24:24 -0800

    fixed shared library file name under Windows

--- python/setup.py
+++ python/setup.py
@@ -107,7 +107,7 @@ class CMakeBuild(build_ext):
         build_args = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
+            cmake_args += ["-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
             if sys.maxsize > 2**32:
                 cmake_args += ["-A", "x64"]
             build_args += ["--", "/m"]
