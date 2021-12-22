e03ca1fbacba5b7f7d3fb3feb0bb86f3fbb1d7ac
Philippe Tillet <phil@openai.com>
2021-12-21 18:57:16 -0800

    .

--- include/triton/codegen/transform/inline.h
+++ include/triton/codegen/transform/inline.h
@@ -5,6 +5,9 @@ namespace triton {
 
 namespace ir {
   class module;
+  class function;
+  class call_inst;
+  class builder;
 }
 
 namespace codegen{
@@ -13,6 +16,7 @@ namespace transform{
 class inliner {
 public:
   inliner() {}
+  void do_inline(ir::function* fn, ir::call_inst* callsite, ir::builder& builder);
   void run(ir::module &mod);
 };
 
--- lib/codegen/transform/inline.cc
+++ lib/codegen/transform/inline.cc
@@ -6,15 +6,52 @@ namespace triton{
 namespace codegen{
 namespace transform{
 
+void inliner::do_inline(ir::function* fn, ir::call_inst* callsite, ir::builder& builder){
+  ir::function* parent_fn = callsite->get_fn();
+  // create new basic block to branch to when the called function return
+  // and optionally also a phi node to hold the return values
+  ir::basic_block* after_call_bb = ir::basic_block::create(callsite->get_parent()->get_context(), "after_call", parent_fn);
+  ir::phi_node* ret_vals = builder.create_phi(fn->get_fn_type()->get_return_ty(), 0);
+  // get arguments `fn` is called with
+  std::vector<ir::value*> tgt_args(callsite->op_begin(), callsite->op_end());
+  std::vector<ir::argument*> src_args(fn->args().begin(), fn->args().end());
+  for(ir::basic_block* old_block: fn->blocks()){
+    ir::basic_block* new_block = ir::basic_block::create(old_block->get_context(), old_block->get_name(), callsite->get_fn());
+    builder.set_insert_point(new_block);
+    for(ir::instruction* old_instr: old_block->get_inst_list()){
+      // `ret` instruction is a special case:
+      // instead of returning we need to branch to after the function call
+      if(ir::return_inst* ret = dynamic_cast<ir::return_inst*>(old_instr)){
+
+
+        continue;
+      }
+      //
+      ir::instruction* new_instr = old_instr->clone();
+      for(size_t i = 0; i < src_args.size(); i++)
+        new_instr->replace_uses_of_with(src_args[i], tgt_args[i]);
+      builder.insert(new_instr);
+    }
+  }
+}
+
 void inliner::run(ir::module &mod) {
 
-  std::map<ir::function*, std::vector<ir::call_inst*>> call_sites;
-
+  // gather all call sites
+  std::map<ir::function*, std::vector<ir::call_inst*>> callsites;
   for(ir::function* fn: mod.get_function_list())
   for(ir::basic_block* block: fn->blocks())
   for(ir::instruction* instr: block->get_inst_list())
   if(ir::call_inst* call = dynamic_cast<ir::call_inst*>(instr)){
-    call_sites[call->get_fn()].push_back(call);
+    callsites[call->get_fn()].push_back(call);
+  }
+
+  // replace call sites with function bodies, one by one
+  for(auto& x: callsites){
+    ir::function* fn = x.first;
+    for(ir::call_inst* callsite: x.second)
+      do_inline(fn, callsite, mod.get_builder());
+
   }
 }
 
