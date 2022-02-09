2fe544297eebd5a81843bba7d32afca8c2a88541
Philippe Tillet <phil@openai.com>
2022-02-08 16:51:48 -0800

    Updated `update-website.sh`

--- update-website.sh
+++ update-website.sh
@@ -6,5 +6,6 @@ cp -r update-website.sh /tmp/triton-docs/
 cp -r docs/_build/html/* /tmp/triton-docs/
 rm -r *
 cp -r /tmp/triton-docs/* .
+ln -s master docs
 git add .
 git commit -am "[GH-PAGES] Updated website"
