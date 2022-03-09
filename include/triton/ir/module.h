61dbce5e91903fb7e58e608c31d75447140fb733
Yan Da <dyanab@connect.ust.hk>
2022-03-09 14:48:54 +0800

    Remove unused code

--- include/triton/ir/module.h
+++ include/triton/ir/module.h
@@ -60,21 +60,6 @@ public:
   module(const std::string &name);
   const std::string& get_name();
 
-  // Setters
-  void set_value(const std::string& name, basic_block* block, value *x);
-  void set_value(const std::string& name, value* x);
-  // Getters
-  const std::map<val_key_t, value*>& get_values() { return values_; }
-  const std::map<std::string, type*>& get_types() { return types_; }
-  void set_values(const std::map<val_key_t, value*>& values) { values_ = values; }
-  void set_types(const std::map<std::string, type*>& types) { types_ = types; }
-
-  value *get_value(const std::string& name, basic_block* block);
-  value *get_value(const std::string& name);
-  void set_type(const std::string& name, ir::type* ty) { types_[name] = ty; }
-  // Seal block -- no more predecessors will be added
-  void seal_block(basic_block *block);
-
   // Functions
   const functions_list_t &get_function_list() const { return functions_; }
   functions_list_t &get_function_list()             { return functions_; }
@@ -92,13 +77,8 @@ public:
 
 private:
   std::string name_;
-  // std::map<val_key_t, value*> values_;
-  // std::map<std::string, type*> types_;
-  // std::set<basic_block*> sealed_blocks_;
-  // std::map<basic_block*, std::map<std::string, phi_node*>> incomplete_phis_;
   functions_list_t functions_;
   symbols_map_t symbols_;
-  std::map<value*, value**> current_phi_;
   std::vector<ir::alloc_const*> allocs_;
   std::map<std::string, ir::value*> globals_;
   std::map<std::string, md_pair_t> metadatas_;
