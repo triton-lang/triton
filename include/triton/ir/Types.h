13aead480825f07f5f70120e853431c14029872c
Yan Da <dyanab@connect.ust.hk>
2022-04-08 16:32:46 +0800

    Use TableGen to define new types

--- include/triton/ir/Types.h
+++ include/triton/ir/Types.h
@@ -4,43 +4,7 @@
 #include "mlir/IR/TypeSupport.h"
 #include "mlir/IR/Types.h"
 
-namespace mlir {
-namespace triton {
-
-namespace detail {
-struct PointerTypeStorage;
-} // namespace detail
-
-// TODO: Should be base class be FloatType?
-class Float8Type : public Type::TypeBase<Float8Type, Type, TypeStorage> {
-public:
-  using Base::Base;
-
-  static Float8Type get(MLIRContext *context);
-};
-
-class BFloat8Type : public Type::TypeBase<BFloat8Type, Type, TypeStorage> {
-public:
-  using Base::Base;
-
-  static BFloat8Type get(MLIRContext *context);
-};
-
-class PointerType : public Type::TypeBase<PointerType, Type, 
-                                          detail::PointerTypeStorage> {
-public:
-  using Base::Base;
-
-  static PointerType get(Type pointeeType);
-
-  static PointerType get(Type pointeeType, unsigned addressSpace);
-
-  Type getPointeeType() const;
-
-  unsigned getAddressSpace() const;
-};
-
-} // namespace triton
-} // namespace mlir
+#define GET_TYPEDEF_CLASSES
+#include "triton/ir/Types.h.inc"
 
 #endif // TRITON_IR_TYPES_H_
