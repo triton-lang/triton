2da4486691405a7c1980e5a4c3089ac552132468
Philippe Tillet <Phil.Tillet@gmail.com>
2022-04-21 18:06:33 -0700

    .

--- python/triton/testing.py
+++ python/triton/testing.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 import psutil, functools
 import torch
+from contextlib import contextmanager
 
 import triton._C.libtriton.triton as _triton
 from .code_gen import OutOfResources
@@ -380,6 +381,39 @@ def cuda_memcheck(**target_kwargs):
                 test_fn(*args, **kwargs)
         return wrapper
     return decorator
+
+
+@contextmanager
+def set_gpu_clock(ref_sm_clock=1350, ref_mem_clock=1215):
+    try:
+        subprocess.check_output(["nvidia-smi", "-i", "0", "-pm", "1"])
+        subprocess.check_output(
+            [
+                "nvidia-smi",
+                "-i",
+                "0",
+                f"--lock-gpu-clocks={ref_sm_clock},{ref_sm_clock}",
+            ]
+        )
+        subprocess.check_output(
+            [
+                "nvidia-smi",
+                "-i",
+                "0",
+                f"--lock-memory-clocks={ref_mem_clock},{ref_mem_clock}",
+            ]
+        )
+        cur_sm_clock = nvsmi_attr(["clocks.current.sm"])[0]
+        cur_mem_clock = nvsmi_attr(["clocks.current.memory"])[0]
+        assert abs(cur_sm_clock - ref_sm_clock) < 10, f"GPU SMs must run at {ref_sm_clock} MHz"
+        assert abs(cur_mem_clock - ref_mem_clock) < 10, f"GPU SMs must run at {ref_mem_clock} MHz"
+        tflops = 1e-6 * 2 * 108 * 4 * 256 * ref_sm_clock
+        gbps = 640 * 2 * ref_mem_clock * 1e-3
+        yield tflops, gbps
+    finally:
+        subprocess.check_output(["nvidia-smi", "-i", "0", "-pm", "0"])
+        subprocess.check_output(["nvidia-smi", "-i", "0", "-rgc"])
+        subprocess.check_output(["nvidia-smi", "-i", "0", "-rmc"])
 
 
 def get_max_simd_tflops(dtype: torch.dtype, backend=None, device=None):
