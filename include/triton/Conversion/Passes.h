3939af3c784b563084ff7005bee6c927653525de
Phil Tillet <phil@openai.com>
2022-04-30 14:31:18 -0700

    [TRITONGPU] Added template for Triton -> TritonGPU conversion

---
+++ include/triton/Conversion/Passes.h
@@ -0,0 +1,19 @@
+#ifndef TRITON_CONVERSION_PASSES_H
+#define TRITON_CONVERSION_PASSES_H
+
+#include "triton/Conversion/TritonToTritonGPU/TritonToTritonGPU.h"
+
+namespace mlir
+{
+namespace triton
+{
+
+#define GEN_PASS_REGISTRATION
+#include "triton/Conversion/Passes.h.inc"
+
+
+} // namespace triton
+} // namespace mlir
+
+
+#endif
