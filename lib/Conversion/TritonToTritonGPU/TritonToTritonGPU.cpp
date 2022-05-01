4ece9fd1f37520c91e5ab91279c61f9ffcbd5442
Yan Da <dyanab@connect.ust.hk>
2022-05-01 13:06:51 +0800

    Move dependentDialects from .cpp to .td

--- lib/Conversion/TritonToTritonGPU/TritonToTritonGPU.cpp
+++ lib/Conversion/TritonToTritonGPU/TritonToTritonGPU.cpp
@@ -51,15 +51,6 @@ class ConvertTritonToTritonGPU:
     public ConvertTritonToTritonGPUBase<ConvertTritonToTritonGPU> {
 
 public:
-    void getDependentDialects(DialectRegistry& registry) const override {
-        registry.insert<arith::ArithmeticDialect>();
-        registry.insert<StandardOpsDialect>();
-        registry.insert<scf::SCFDialect>();
-        // LLVM15
-        // registry.insert<cf::ControlFlowDialect>()
-        // registry.insert<func::FuncDialect>()
-    }
-
     void runOnOperation() override {
         MLIRContext *context = &getContext();
         ConversionTarget target(*context);
