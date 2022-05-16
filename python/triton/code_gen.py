5b0bb11151f7cda85cb584747393d1ed2d0dd7f2
Yan Da <dyanab@connect.ust.hk>
2022-05-16 19:16:01 +0800

    TritonGPU combiner

--- python/triton/code_gen.py
+++ python/triton/code_gen.py
@@ -1316,6 +1316,7 @@ class JITFunction:
         pm.add_convert_triton_to_tritongpu_pass()
         pm.add_tritongpu_pipeline_pass()
         pm.add_canonicalizer_pass()
+        pm.add_triton_gpu_combine_pass()
         pm.run(mod)
         return mod
 
