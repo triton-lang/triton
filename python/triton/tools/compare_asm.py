7ccae58879206649612e5057bd5bb49642769834
Yan Da <dyanab@connect.ust.hk>
2022-05-24 12:43:21 +0800

    formatting

--- python/triton/tools/compare_asm.py
+++ python/triton/tools/compare_asm.py
@@ -31,46 +31,46 @@ asm = args.asm
 dir0_files = {}
 dir1_files = {}
 for root, _, files in os.walk(dir0):
-  for file in files:
-    if not file.endswith('.lock'):
-      path = os.path.join(root, file)
-      with open(path, 'rb') as f:
-        loaded_file = pickle.load(f)
-        bin = loaded_file['binary']
-        key = loaded_file['key']
-        info = key.split('-')[-3:]  # num_warps, num_stages, signature
-        dict_key = bin.name + '-'.join(info)
-        dir0_files[dict_key] = bin.asm
+    for file in files:
+        if not file.endswith('.lock'):
+            path = os.path.join(root, file)
+            with open(path, 'rb') as f:
+                loaded_file = pickle.load(f)
+                bin = loaded_file['binary']
+                key = loaded_file['key']
+                info = key.split('-')[-3:]  # num_warps, num_stages, signature
+                dict_key = bin.name + '-'.join(info)
+                dir0_files[dict_key] = bin.asm
 
 for root, _, files in os.walk(dir1):
-  for file in files:
-    if not file.endswith('.lock'):
-      path = os.path.join(root, file)
-      with open(path, 'rb') as f:
-        loaded_file = pickle.load(f)
-        bin = loaded_file['binary']
-        key = loaded_file['key']
-        info = key.split('-')[-3:]  # num_warps, num_stages, signature
-        dict_key = bin.name + '-'.join(info)
-        dir1_files[dict_key] = bin.asm
+    for file in files:
+        if not file.endswith('.lock'):
+            path = os.path.join(root, file)
+            with open(path, 'rb') as f:
+                loaded_file = pickle.load(f)
+                bin = loaded_file['binary']
+                key = loaded_file['key']
+                info = key.split('-')[-3:]  # num_warps, num_stages, signature
+                dict_key = bin.name + '-'.join(info)
+                dir1_files[dict_key] = bin.asm
 
 diff_keys = []
 for key in dir0_files:
-  asm0 = dir0_files[key]
-  if key not in dir1_files:
-    continue
-  asm1 = dir1_files[key]
-  if asm0[asm] != asm1[asm]:
-    diff_keys.append(key)
+    asm0 = dir0_files[key]
+    if key not in dir1_files:
+        continue
+    asm1 = dir1_files[key]
+    if asm0[asm] != asm1[asm]:
+        diff_keys.append(key)
 
 if args.early_stops:
-  diff_keys = diff_keys[:1]
+    diff_keys = diff_keys[:1]
 if diff_keys:
-  with open(args.diff_out0, 'w') as f0, open(args.diff_out1, 'w') as f1:
-    for key in diff_keys:
-      f0.write(f'{asm} mismatch at {key}')
-      f0.write(dir0_files[key][asm])
-      f0.write('\n')
-      f1.write(f'{asm} mismatch at {key}')
-      f1.write(dir1_files[key][asm])
-      f1.write('\n')
+    with open(args.diff_out0, 'w') as f0, open(args.diff_out1, 'w') as f1:
+        for key in diff_keys:
+            f0.write(f'{asm} mismatch at {key}')
+            f0.write(dir0_files[key][asm])
+            f0.write('\n')
+            f1.write(f'{asm} mismatch at {key}')
+            f1.write(dir1_files[key][asm])
+            f1.write('\n')
