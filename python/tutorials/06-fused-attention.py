c4de7fbe606f082286708604a32c2d98e454c86c
Phil Wang <lucidrains@gmail.com>
2022-08-15 16:27:54 -0700

    assert proper head dimensions

--- python/tutorials/06-fused-attention.py
+++ python/tutorials/06-fused-attention.py
@@ -206,6 +206,7 @@ class _attention(torch.autograd.Function):
         # shape constraints
         Lq, Lk, Lv = q.shape[-1], k.shape[-1], v.shape[-1]
         assert Lq == Lk and Lk == Lv
+        assert Lk in {16, 32, 64, 128}
         o = torch.empty_like(q)
         grid = (triton.cdiv(q.shape[2], BLOCK), q.shape[0] * q.shape[1])
         tmp = torch.empty((q.shape[0] * q.shape[1], q.shape[2]), device=q.device, dtype=torch.float32)
