ea175f689e9f6aaea0a6e468cc0d83920dc8f110
Jun Yang <yangjunpro@gmail.com>
2022-09-04 12:50:27 +0800

    [CI]Added initial framework of CXX unittest (#98)
    Based on the discussion in #53 , I just added the initial flow of CXX unittests for this repo, with providing two dummy UTs as placeholder to show the usage, feel free to add your own CXX unittests. 
    @Superjomn  @ptillet 
    
    @ptillet , in this PR, I also configure the integration-tests.yml to add the unittest into github CI check. 
    
    Thanks

---
+++ unittest/Analysis/UtilityTest.cpp
@@ -0,0 +1,14 @@
+//===- UtilityTest.cpp - Tests for
+// Utility----------------------------------===//
+//
+//===----------------------------------------------------------------------===//
+
+#include "triton/Analysis/Utility.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace mlir {
+
+TEST(UtilityTest, DummyTest) { EXPECT_EQ(true, true); }
+
+} // namespace mlir
