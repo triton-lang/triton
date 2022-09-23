d05d4e60b04fb9d18d451940303b22d570a26441
Philippe Tillet <phil@openai.com>
2022-09-23 16:09:43 -0700

    [FRONTEND] Backport new runtime from `master` (#706)
    This PR merges the new runtime back into the `triton-mlir` branch. This
    adds caching and just-in-time compilation functionality to the
    triton-mlir project, and paves the way for re-using tests from the
    master branch.

--- python/examples/empty.py
+++ python/examples/empty.py
@@ -1,3 +1,5 @@
+import torch
+
 import triton
 import triton.language as tl
 
@@ -7,4 +9,5 @@ def kernel(X, stride_xm, stride_xn, BLOCK: tl.constexpr):
     pass
 
 
-ret = triton.compile(kernel, "*fp32,i32,i32", constants={"BLOCK": 256}, output="ttgir")
+X = torch.randn(1, device="cuda")
+pgm = kernel[(1,)](X, 1, 1, BLOCK=1024)
