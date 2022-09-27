f5ba51e20c551512de488ceb1b796af3c2106cba
Phil Tillet <phil@openai.com>
2022-09-27 08:54:31 -0700

    .

--- lib/Dialect/TritonGPU/Transforms/Combine.cpp
+++ lib/Dialect/TritonGPU/Transforms/Combine.cpp
@@ -120,9 +120,8 @@ public:
 
     auto blacklist = [](Operation *op) {
       if (isa<triton::gpu::ExtractSliceOp, triton::gpu::AllocTensorOp,
-              triton::gpu::InsertSliceAsyncOp, triton::LoadOp, triton::StoreOp, 
-              triton::DotOp>(
-              op))
+              triton::gpu::InsertSliceAsyncOp, triton::LoadOp, triton::StoreOp,
+              triton::DotOp>(op))
         return true;
       if (isa<scf::YieldOp, scf::ForOp>(op))
         return true;
