4dd4214c5267082a202e63c00d2db566d17555ff
Michael Melesse <micmelesse@gmail.com>
2022-10-17 17:28:48 +0000

    add scripts

---
+++ scripts/amd/backtrace.sh
@@ -0,0 +1,14 @@
+sudo apt install gdb -y
+
+# export AMD_OCL_WAIT_COMMAND=1
+# export AMD_LOG_LEVEL=3
+# export HIP_LAUNCH_BLOCKING=1
+
+
+gdb -ex "set pagination off" \
+    -ex "file python" \
+    -ex 'run -m pytest --capture=tee-sys --verbose "python/test/unit/language/test_core.py::test_program_id[float16]"' \
+    -ex "backtrace" \
+    -ex "set confirm off" \
+    -ex "q" \
+    2>&1 | tee /dockerx/pytorch/test_core_gdb.log
