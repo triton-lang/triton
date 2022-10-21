768aae5137325faa36ecef6feb1158e38c2bc490
goostavz <109190422+goostavz@users.noreply.github.com>
2022-10-21 11:46:28 +0800

    [Triton-MLIR] Minor fixes related with scf/swizzling support (#791)
    1, Disable static loop unrolling in the frontend by default;
    2, A minor fix in axisAnalysis in order to support scf;
    3, A minor fix in TritonGPUToLLVM to support swizzling.

--- lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVM.cpp
@@ -1867,8 +1867,8 @@ LogicalResult ConvertLayoutOpConversion::lowerBlockedToShared(
     unsigned linearIdxInNanoTile = i % srcAccumSizeInThreads;
     auto multiDimIdxInNanoTile = getMultiDimIndex<unsigned>(
         linearIdxInNanoTile, srcBlockedLayout.getSizePerThread());
-    multiDimIdxInNanoTile[inOrd[0]] /= minVec;
     unsigned pos = multiDimIdxInNanoTile[inOrd[0]] % minVec;
+    multiDimIdxInNanoTile[inOrd[0]] /= minVec;
     unsigned wordVecIdx =
         getLinearIndex<unsigned>(multiDimIdxInNanoTile, wordsInEachRep);
     wordVecs[wordVecIdx] =
