6a31c43774d3686e08dfbf6a0b8770df7c8d1d90
Michael Melesse <micmelesse@gmail.com>
2022-10-21 19:56:19 +0000

    update batcktrace

--- scripts/amd/backtrace.sh
+++ scripts/amd/backtrace.sh
@@ -7,7 +7,7 @@ sudo apt install gdb -y
 
 gdb -ex "set pagination off" \
     -ex "file python" \
-    -ex 'run -m pytest --capture=tee-sys --verbose "python/test/unit/language/test_core.py::test_program_id[float16]"' \
+    -ex 'run -m pytest --capture=tee-sys --verbose "python/test/unit/language/test_core.py::test_empty_kernel[float32]"' \
     -ex "backtrace" \
     -ex "set confirm off" \
     -ex "q" \
