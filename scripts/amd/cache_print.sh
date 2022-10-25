df925f71877e785726cfa61ccf20d7dd59c7232c
Michael Melesse <micmelesse@gmail.com>
2022-10-25 20:48:36 +0000

    add cache print script

--- scripts/amd/cache_print.sh
+++ scripts/amd/cache_print.sh
@@ -4,6 +4,10 @@ CACHED_FILES=$(find /root/.triton/cache/ -type f -name "*.*")
 
 for file in ${CACHED_FILES[@]}; do
 	echo "$file"
-	sed -i -e '$a\' $file
-	cat $file
+	if [[ $file == *.so ]]; then
+		echo "Skipping printing .so file"
+	else
+		sed -i -e '$a\' $file
+		cat $file
+	fi
 done
