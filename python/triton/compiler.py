166c0fe9ad2f0daf77fb109df1e7de66b24519a6
Yanbo Liang <ybliang8@gmail.com>
2022-10-24 19:41:25 -0700

    Add bf16/fp16/fp64 support for ty_to_cpp (#800)
    In ```torch._inductor```, we [convert 0d CPU tensor to scalar during
    triton codegen](https://github.com/pytorch/pytorch/pull/87329), so need
    add missing triton support for bf16/fp16/fp64.

--- python/triton/compiler.py
+++ python/triton/compiler.py
@@ -913,7 +913,10 @@ def ty_to_cpp(ty):
         "i64": "int64_t",
         "u32": "uint32_t",
         "u64": "uint64_t",
+        "fp16": "float",
+        "bf16": "float",
         "fp32": "float",
+        "fp64": "double",
     }[ty]
 
 
@@ -943,6 +946,8 @@ def generate_launcher(identifier, constants, signature):
             'i64': 'int64_t',
             'u32': 'uint32_t',
             'u64': 'uint64_t',
+            'fp16': 'float',
+            'bf16': 'float',
             'fp32': 'float',
             'fp64': 'double',
         }[ty]
