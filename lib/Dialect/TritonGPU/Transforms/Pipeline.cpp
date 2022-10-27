e7760eea2ac079589267382a7e16d92a3dae87f7
Jokeren <kerenzhou@openai.com>
2022-10-27 00:48:47 -0700

    Fix pipeline

--- lib/Dialect/TritonGPU/Transforms/Pipeline.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeline.cpp
@@ -78,6 +78,9 @@ public:
   /// emit pipelined loads (before loop body)
   void emitPrologue();
 
+  /// emit pipelined loads (after loop body)
+  void emitEpilogue();
+
   /// create the new ForOp (add new args & insert prefetched ops)
   scf::ForOp createNewForOp();
 
@@ -368,6 +371,15 @@ void LoopPipeliner::emitPrologue() {
       builder.create<arith::ConstantIntOp>(loopIterIdx.getLoc(), 1, 32));
 }
 
+void LoopPipeliner::emitEpilogue() {
+  // If there's any dangling async copies, we need to wait for them.
+  OpBuilder builder(forOp);
+  OpBuilder::InsertionGuard g(builder);
+  builder.setInsertionPointAfter(forOp);
+  Operation *asyncWait = builder.create<triton::gpu::AsyncWaitOp>(
+      forOp.getLoc(), 0);
+}
+
 scf::ForOp LoopPipeliner::createNewForOp() {
   OpBuilder builder(forOp);
 
@@ -584,6 +596,8 @@ struct PipelinePass : public TritonGPUPipelineBase<PipelinePass> {
       pipeliner.emitPrologue();
 
       scf::ForOp newForOp = pipeliner.createNewForOp();
+
+      pipeliner.emitEpilogue();
 
       // replace the original loop
       for (unsigned i = 0; i < forOp->getNumResults(); ++i)
