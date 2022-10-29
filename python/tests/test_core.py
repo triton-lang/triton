70a5ee1cd24b230a3f736ad3d2b4763f11cd52fc
Phil Tillet <phil@openai.com>
2022-10-28 21:27:16 -0700

    .

--- python/tests/test_core.py
+++ python/tests/test_core.py
@@ -411,12 +411,12 @@ def test_where(dtype):
     assert (z == to_numpy(z_tri)).all()
 
 
-# TODO: wrong result
 def test_where_broadcast():
     @triton.jit
     def where_kernel(cond_ptr, a_ptr, out_ptr, BLOCK_SIZE: tl.constexpr):
         xoffsets = tl.arange(0, BLOCK_SIZE)[:, None]
         yoffsets = tl.arange(0, BLOCK_SIZE)[None, :]
+
         mask = tl.load(cond_ptr + yoffsets)
         vals = tl.load(a_ptr + yoffsets + BLOCK_SIZE * xoffsets)
         res = tl.where(mask, vals, 0.)
@@ -426,11 +426,12 @@ def test_where_broadcast():
     def where_scalar_condition(a_ptr, out_ptr, BLOCK_SIZE: tl.constexpr):
         xoffsets = tl.arange(0, BLOCK_SIZE)[:, None]
         yoffsets = tl.arange(0, BLOCK_SIZE)[None, :]
+        mask = 0
         vals = tl.load(a_ptr + yoffsets + BLOCK_SIZE * xoffsets)
         res = tl.where(mask, vals, 0.)
         tl.store(out_ptr + yoffsets + BLOCK_SIZE * xoffsets, res)
 
-    SIZE = 16
+    SIZE = 32
     dtype = 'float32'
     rs = RandomState(17)
     x = numpy_random((SIZE, SIZE), dtype_str=dtype, rs=rs)
@@ -438,14 +439,12 @@ def test_where_broadcast():
     z = np.where(mask, x, 0)
     cond_tri = to_triton(mask, device="cuda")
     x_tri = to_triton(x, device='cuda', dst_type=dtype)
-    z_tri = to_triton(np.zeros((SIZE, SIZE), dtype=z.dtype), device='cuda', dst_type=dtype)
-    # print(z)
-    # print(z_tri)
-    where_kernel[(1,)](cond_tri, x_tri, z_tri, SIZE, num_warps=1)
+    z_tri = to_triton(np.empty((SIZE, SIZE), dtype=z.dtype), device='cuda', dst_type=dtype)
+    where_kernel[(1,)](cond_tri, x_tri, z_tri, SIZE)
     assert (z == to_numpy(z_tri)).all()
-    # where_scalar_condition[(1,)](x_tri, z_tri, SIZE)
-    # z = np.where(0, x, 0)
-    # assert (z == to_numpy(z_tri)).all()
+    where_scalar_condition[(1,)](x_tri, z_tri, SIZE)
+    z = np.where(0, x, 0)
+    assert (z == to_numpy(z_tri)).all()
 
 # # ---------------
 # # test unary ops
