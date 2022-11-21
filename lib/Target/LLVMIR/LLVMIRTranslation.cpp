b23c128f62fb8e3797a8ca020a1e35b91f716103
Jokeren <kerenzhou@openai.com>
2022-11-20 16:25:12 -0800

    Fix

--- lib/Target/LLVMIR/LLVMIRTranslation.cpp
+++ lib/Target/LLVMIR/LLVMIRTranslation.cpp
@@ -99,9 +99,6 @@ translateLLVMToLLVMIR(llvm::LLVMContext *llvmContext, mlir::ModuleOp module) {
     return nullptr;
   }
 
-  // Initialize LLVM targets.
-  mlir::ExecutionEngine::setupTargetTriple(llvmModule.get());
-
   auto optPipeline = mlir::makeOptimizingTransformer(
       /*optLevel=*/3, /*sizeLevel=*/0,
       /*targetMachine=*/nullptr);
