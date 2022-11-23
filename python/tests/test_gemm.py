1f1b91100d6a4a286e33b78fb9156bd024a5a981
Jokeren <kerenzhou@openai.com>
2022-11-22 20:02:54 -0800

    Add test cases

--- python/tests/test_gemm.py
+++ python/tests/test_gemm.py
@@ -172,7 +172,8 @@ def get_proper_err(a, b, golden):
     [128, 64, 128, 4, 128, 64, 128, False, False],
     [16, 16, 16, 16, 16, 16, 16, False, False],  # wpt overflow issue
     # K-Forloop
-    [64, 64, 128, 4, 64, 64, 64, False, False], # Single shared encoding
+    [32, 32, 64, 4, 32, 32, 32, False, False], # Single shared encoding
+    [16, 16, 128, 4, 16, 16, 16, False, False], # Single shared encoding and small k
     [64, 32, 128, 4, 64, 32, 64, False, False],
     [128, 16, 128, 4, 128, 16, 32, False, False],
     [32, 16, 128, 4, 32, 16, 32, False, False],
