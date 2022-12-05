e0072d210ae647732aac4509673cf1965e279cd7
Crutcher Dunnavant <crutcher@gmail.com>
2022-12-05 14:41:02 -0800

    [FRONTEND] Propagate mypy types through @jit, @builtin, etc (#915)
    Changes to make decorated API methods no longer type-opaque.
    
    ```
    $ echo 'import triton; reveal_type(triton.language.max)' | mypy /dev/stdin
    /dev/stdin:1: note: Revealed type is "def (input: Any, axis: Any, _builder: Any =) -> Any"
    Success: no issues found in 1 source file
    ```

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from enum import Enum
 from functools import wraps
-from typing import List
+from typing import List, Callable, TypeVar
 
 import triton
 from . import semantic
 from triton._C.libtriton.triton import ir
 
+T = TypeVar('T')
 
 def _to_tensor(x, builder):
     if isinstance(x, bool):
@@ -33,7 +34,7 @@ def _to_tensor(x, builder):
     assert False, f'cannot convert {x} to tensor'
 
 
-def builtin(fn):
+def builtin(fn: T) -> T:
     @wraps(fn)
     def wrapper(*args, **kwargs):
         if '_builder' not in kwargs or \
@@ -851,9 +852,9 @@ def store(pointer, value, mask=None, _builder=None):
 # Atomic Memory Operations
 # -----------------------
 
-def _add_atomic_docstr(name):
+def _add_atomic_docstr(name: str) -> Callable[[T], T]:
 
-    def _decorator(func):
+    def _decorator(func: T) -> T:
         docstr = """
     Performs an atomic {name} at the memory location specified by :code:`pointer`.
 
@@ -974,9 +975,9 @@ def fdiv(x, y, ieee_rounding=False, _builder=None):
     return semantic.fdiv(x, y, ieee_rounding, _builder)
 
 
-def _add_math_1arg_docstr(name):
+def _add_math_1arg_docstr(name: str) -> Callable[[T], T]:
 
-    def _decorator(func):
+    def _decorator(func: T) -> T:
         docstr = """
     Computes the element-wise {name} of :code:`x`
 
@@ -1023,9 +1024,9 @@ def sqrt(x, _builder=None):
 # Reductions
 # -----------------------
 
-def _add_reduction_docstr(name):
+def _add_reduction_docstr(name: str) -> Callable[[T], T]:
 
-    def _decorator(func):
+    def _decorator(func: T) -> T:
         docstr = """
     Returns the {name} of all elements in the :code:`input` tensor along the provided :code:`axis`
 
