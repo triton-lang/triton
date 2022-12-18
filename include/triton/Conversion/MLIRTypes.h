0e995980bb2c6ffd50371ad5af6af719e7f6997e
Chenggang Zhao <lyricz@yeah.net>
2022-12-18 13:41:24 +0800

    Add a TODO

--- include/triton/Conversion/MLIRTypes.h
+++ include/triton/Conversion/MLIRTypes.h
@@ -10,6 +10,7 @@ namespace triton {
 namespace type {
 
 // Integer types
+// TODO(Superjomn): may change `static` into better implementations
 static Type i32Ty(MLIRContext *ctx) { return IntegerType::get(ctx, 32); }
 static Type i8Ty(MLIRContext *ctx) { return IntegerType::get(ctx, 8); }
 static Type u32Ty(MLIRContext *ctx) {
