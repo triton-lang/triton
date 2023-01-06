b16aeb6541adc4ca1d8f9a21c6ac703874c218eb
Phil Tillet <phil@openai.com>
2023-01-06 13:39:54 -0800

    added missing file

---
+++ lib/Dialect/TritonGPU/Transforms/SinkConversionsFromShared.cpp
@@ -0,0 +1,38 @@
+#include "mlir/Analysis/SliceAnalysis.h"
+#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/IR/BlockAndValueMapping.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/Verifier.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Transforms/Passes.h"
+#include "mlir/Transforms/RegionUtils.h"
+#include "triton/Analysis/Utility.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/Transforms/Passes.h"
+#include "triton/Dialect/TritonGPU/Transforms/TritonGPUConversion.h"
+#define GEN_PASS_CLASSES
+#include "triton/Dialect/TritonGPU/Transforms/Passes.h.inc"
+
+using namespace mlir;
+
+
+class TritonGPUSinkConversionsFromSharedPass
+    : public TritonGPUSinkConversionsFromSharedBase<TritonGPUSinkConversionsFromSharedPass> {
+public:
+  TritonGPUSinkConversionsFromSharedPass() = default;
+
+  void runOnOperation() override {
+    return;
+  }
+};
+
+std::unique_ptr<Pass>
+mlir::createTritonGPUSinkConversionsFromSharedPass() {
+  return std::make_unique<TritonGPUSinkConversionsFromSharedPass>();
+}
