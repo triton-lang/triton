917d477acc548a8f301e7c9d04c880f53dd353fb
thexler <g.axler@gmail.com>
2023-01-10 19:37:56 -0800

    test code for kernel launching

---
+++ aot_kernel_tests/build.py
@@ -0,0 +1,63 @@
+from typing import Sequence
+import os
+import subprocess
+import shutil
+
+def libcuda_dirs():
+    locs = subprocess.check_output(["whereis", "libcuda.so"]).decode().strip().split()[1:]
+    return [os.path.dirname(loc) for loc in locs]
+
+if not libcuda_dirs():
+    print("No libcuda.so found")
+    # exit(1)   
+
+def default_cuda_dir():
+    default_dir = "/usr/local/cuda"
+    return os.getenv("CUDA_HOME", default=default_dir)
+
+if not os.path.exists(default_cuda_dir()):
+    print("No cuda directory found, set CUDA_HOME env var")
+    # exit(1)
+
+def find_compiler():
+  cc = os.environ.get("CC")
+  if cc is None:
+      # TODO: support more things here.
+      clang = shutil.which("clang")
+      gcc = shutil.which("gcc")
+      cc = gcc if gcc is not None else clang
+  return cc
+
+
+def build_cuda_bin(out:str, *src_files, include_dirs: Sequence[str]=None, pic: bool = False):
+  cuda_lib_dirs = libcuda_dirs()
+  
+  cuda_path = os.environ.get('CUDA_PATH', default_cuda_dir())
+  cu_include_dir = os.path.join(cuda_path, "include")
+
+  cc = find_compiler()
+  if include_dirs is None:
+    include_dirs = []
+
+  includes = [f"-I{inc_dir}" for inc_dir in include_dirs]  
+  
+  options = []
+  if pic:
+    options.append("-fPIC")
+  
+  cc_cmd = [cc, *src_files, "-O3", f"-I{cu_include_dir}", *includes, *options, "-lcuda", "-o", out]
+  cc_cmd += [f"-L{dir}" for dir in cuda_lib_dirs]
+  print(" ".join(cc_cmd))
+#   ret = subprocess.check_call(cc_cmd)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("--out", "-o", type=str)
+    parser.add_argument("src", nargs="+")
+    parser.add_argument("--include-dirs", "-I", nargs="+")
+
+    args = parser.parse_args()
+
+    build_cuda_bin(args.out, *args.src, include_dirs=args.include_dirs)
---
+++ aot_kernel_tests/main.c
@@ -0,0 +1,58 @@
+#include <cuda.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void errMsg(char* msg, int code) {
+  if (code != CUDA_SUCCESS) {
+   printf("%s with %d\n", msg, code); 
+  }
+}
+
+#define CHECK_CUDA(code, msg) errMsg(msg, code)
+
+#define GET_DEVICE(id) 
+
+void arange(double *arr, size_t start, size_t size) {
+
+  double val = (double)start;
+
+  for (size_t i = 0; i < size; i++) {
+    arr[i] = (double)val;
+    val++;
+  }
+}
+
+#define AllocFloatVec(N) (double *)malloc(N * sizeof(double));
+#define cuAllocFloatVec(N,ptr) CHECK_CUDA(cuMemAlloc(ptr, N * sizeof(double)), "Allocate Float");
+
+#define VectoDevice(N, src, dst) CHECK_CUDA(cuMemcpyHtoD(dst, src, N*sizeof(double)), "Copy HtoD"); 
+
+void main() {
+
+  int device_id = 0;
+  int max_shared_mem;
+
+  double *u =  AllocFloatVec(100);
+  double *v =  AllocFloatVec(100);
+
+  CHECK_CUDA(cuInit(0), "Cuda init failed");
+
+  CUdevice device;
+  CHECK_CUDA(cuDeviceGet(&device, device_id), "Get device failed");
+  cuDeviceGetAttribute(&max_shared_mem,
+                       CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK_OPTIN,
+                       device);
+  
+  printf("Max shared mem: %u \n", max_shared_mem);
+  CUcontext ctx;
+  cuCtxCreate(&ctx, CU_CTX_SCHED_AUTO, device);
+  
+  CUdeviceptr u_cu, v_cu;
+  cuAllocFloatVec(100, &u_cu);
+  cuAllocFloatVec(100, &v_cu);
+
+  VectoDevice(100, u, u_cu);
+  VectoDevice(100, v, v_cu);
+}
