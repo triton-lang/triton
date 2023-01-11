b1b35dd52c2d133651fe219f7ac2ae00edf35884
Superjomn <yanchunwei@outlook.com>
2023-01-11 10:28:47 +0800

    seperate utility

--- lib/Dialect/TritonGPU/Transforms/Combine.cpp
+++ lib/Dialect/TritonGPU/Transforms/Combine.cpp
@@ -1,3 +1,4 @@
+#include "Utility.h"
 #include "mlir/Analysis/SliceAnalysis.h"
 #include "mlir/Dialect/SCF/SCF.h"
 #include "mlir/IR/BlockAndValueMapping.h"
@@ -1146,50 +1147,6 @@ public:
   }
 };
 
-class FixupLoop : public mlir::RewritePattern {
-
-public:
-  explicit FixupLoop(mlir::MLIRContext *context)
-      : mlir::RewritePattern(scf::ForOp::getOperationName(), 2, context) {}
-
-  mlir::LogicalResult
-  matchAndRewrite(mlir::Operation *op,
-                  mlir::PatternRewriter &rewriter) const override {
-    auto forOp = cast<scf::ForOp>(op);
-
-    // Rewrite init argument
-    SmallVector<Value, 4> newInitArgs = forOp.getInitArgs();
-    bool shouldRematerialize = false;
-    for (size_t i = 0; i < newInitArgs.size(); i++) {
-      auto initArg = newInitArgs[i];
-      auto regionArg = forOp.getRegionIterArgs()[i];
-      if (newInitArgs[i].getType() != forOp.getRegionIterArgs()[i].getType() ||
-          newInitArgs[i].getType() != forOp.getResultTypes()[i]) {
-        shouldRematerialize = true;
-        break;
-      }
-    }
-    if (!shouldRematerialize)
-      return failure();
-
-    scf::ForOp newForOp = rewriter.create<scf::ForOp>(
-        forOp.getLoc(), forOp.getLowerBound(), forOp.getUpperBound(),
-        forOp.getStep(), newInitArgs);
-    newForOp->moveBefore(forOp);
-    rewriter.setInsertionPointToStart(newForOp.getBody());
-    BlockAndValueMapping mapping;
-    for (const auto &arg : llvm::enumerate(forOp.getRegionIterArgs()))
-      mapping.map(arg.value(), newForOp.getRegionIterArgs()[arg.index()]);
-    mapping.map(forOp.getInductionVar(), newForOp.getInductionVar());
-
-    for (Operation &op : forOp.getBody()->getOperations()) {
-      rewriter.clone(op, mapping);
-    }
-    rewriter.replaceOp(forOp, newForOp.getResults());
-    return success();
-  }
-};
-
 // This pattern collects the wrong Mma those need to update and create the right
 // ones for each.
 class CollectMmaToUpdateForVolta : public mlir::RewritePattern {
@@ -1529,7 +1486,7 @@ public:
     }
 
     mlir::RewritePatternSet loopFixup(context);
-    loopFixup.add<FixupLoop>(context);
+    addFixupLoopPattern(context, loopFixup);
     if (applyPatternsAndFoldGreedily(m, std::move(loopFixup)).failed()) {
       signalPassFailure();
     }
