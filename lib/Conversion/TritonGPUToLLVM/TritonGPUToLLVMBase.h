9ae48a22e35e629911d09523bb702b3f1663087d
Phil Tillet <phil@openai.com>
2023-01-12 19:58:26 -0800

    Added documentation

--- lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMBase.h
+++ lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMBase.h
@@ -227,27 +227,53 @@ public:
                                          SmallVectorImpl<Value>& offsetVals,
                                          SmallVectorImpl<Value>& srcStrides
                                          ) const {
-
+    // This utililty computes the pointers for accessing the provided swizzled
+    // shared memory layout `resSharedLayout`. More specifically, it computes, for all
+    // indices (row, col) of `srcEncoding` such that idx % inVec = 0, the pointer:
+    // ptr[(row, col)] = base + (rowOff * strides[ord[1]] + colOff)
+    // where : 
+    //   compute phase = (row // perPhase) % maxPhase
+    //   rowOff = row
+    //   colOff = colOffSwizzled + colOffOrdered
+    //     colOffSwizzled = ((col // outVec) ^ phase) * outVec
+    //     colOffOrdered = (col % outVec) // minVec * minVec
+    //
+    // Note 1: 
+    // -------
+    // Because swizzling happens at a granularity of outVec, we need to
+    // decompose the offset into a swizzled factor and a non-swizzled (ordered) 
+    // factor
+    //
+    // Note 2:
+    // -------
+    // If we have x, y, z of the form:
+    // x = 0b00000xxxx
+    // y = 0byyyyy0000
+    // z = 0b00000zzzz
+    // then (x + y) XOR z = 0byyyyxxxx XOR 0b00000zzzz = (x XOR z) + y
+    // This means that we can use some immediate offsets for shared memory operations.
+    // More specifically
     auto dstPtrTy = ptr_ty(resElemTy, 3);
     auto dstOffset = dot(rewriter, loc, offsetVals, smemObj.strides);
     Value dstPtrBase = gep(dstPtrTy, smemObj.base, dstOffset);
 
     auto srcEncoding = srcTy.getEncoding();
     auto srcShape = srcTy.getShape();
-    unsigned outVec = resSharedLayout.getVec();
-    unsigned minVec = std::min(outVec, inVec);
     unsigned numElems = triton::gpu::getElemsPerThread(srcTy);
+    // swizzling params as described in TritonGPUAttrDefs.td
+    unsigned outVec = resSharedLayout.getVec();
     unsigned perPhase = resSharedLayout.getPerPhase();
     unsigned maxPhase = resSharedLayout.getMaxPhase();
+    // order
     auto inOrder = triton::gpu::getOrder(srcEncoding);
     auto outOrder = triton::gpu::getOrder(resSharedLayout);
-
+    // tensor indices held by the current thread, as LLVM values
     auto srcIndices = emitIndices(loc, rewriter, srcEncoding, srcShape);
-
-
+    // return values
     DenseMap<unsigned, Value> ret;
+    // cache for non-immediate offsets
     DenseMap<unsigned, Value> cacheCol, cacheRow;
-
+    unsigned minVec = std::min(outVec, inVec);
     for (unsigned elemIdx = 0; elemIdx < numElems; elemIdx += minVec) {
       // extract multi dimensional index for current element
       auto idx = srcIndices[elemIdx];
@@ -255,8 +281,8 @@ public:
       Value idxRow = idx[inOrder[1]]; // discontiguous dimension
       Value strideCol = srcStrides[inOrder[0]]; 
       Value strideRow = srcStrides[inOrder[1]]; 
-      // extract dynamic/static offset
-      unsigned baseOffsetCol = 0;
+      // extract dynamic/static offset for immediate offseting
+      unsigned immedateOffCol = 0;
       if (auto add = dyn_cast_or_null<LLVM::AddOp>(idxCol.getDefiningOp()))
       if (auto _cst = dyn_cast_or_null<LLVM::ConstantOp>(add.getRhs().getDefiningOp())) {
         unsigned cst = _cst.getValue().cast<IntegerAttr>()
@@ -264,9 +290,10 @@ public:
         unsigned key = cst % (outVec * maxPhase);
         cacheCol.insert({key, idxCol});
         idxCol = cacheCol[key];
-        baseOffsetCol = cst / (outVec * maxPhase) * (outVec * maxPhase);
+        immedateOffCol = cst / (outVec * maxPhase) * (outVec * maxPhase);
       }
-      unsigned baseOffsetRow = 0;
+      // extract dynamic/static offset for immediate offseting
+      unsigned immedateOffRow = 0;
       if (auto add = dyn_cast_or_null<LLVM::AddOp>(idxRow.getDefiningOp()))
       if (auto _cst = dyn_cast_or_null<LLVM::ConstantOp>(add.getRhs().getDefiningOp())) {
         unsigned cst = _cst.getValue().cast<IntegerAttr>()
@@ -274,41 +301,31 @@ public:
         unsigned key = cst % (perPhase * maxPhase);
         cacheRow.insert({key, idxRow});
         idxRow = cacheRow[key];
-        baseOffsetRow = cst / (perPhase * maxPhase) * (perPhase * maxPhase);
+        immedateOffRow = cst / (perPhase * maxPhase) * (perPhase * maxPhase);
       }
-      // Swizzling
-      // Since the swizzling index is related to outVec, and we know minVec
-      // already, inVec doesn't matter
-      //
-      // (Numbers represent row indices)
-      // Example1:
-      // outVec = 2, inVec = 2, minVec = 2
-      // outVec = 2, inVec = 4, minVec = 2
-      //     | [1 2] [3 4] [5 6] ... |
-      //     | [3 4] [1 2] [7 8] ... |
-      //     | [5 6] [7 8] [1 2] ... |
-      // Example2:
-      // outVec = 4, inVec = 2, minVec = 2
-      //     | [1 2 3 4] [5 6 7 8] [9 10 11 12] ... |
-      //     | [5 6 7 8] [1 2 3 4] [13 14 15 16] ... |
-      //     | [9 10 11 12] [13 14 15 16] [1 2 3 4] ... |
+      // compute phase = (row // perPhase) % maxPhase
       Value phase = urem(udiv(idxRow, i32_val(perPhase)), i32_val(maxPhase));
-      Value rowOffset = mul(idxRow, strideRow);
-      Value colOffset = xor_(udiv(idxCol, i32_val(outVec)), phase);
-      colOffset = mul(colOffset, i32_val(outVec));
-      Value remained = urem(idxCol, i32_val(outVec));
-      remained = udiv(remained, i32_val(minVec));
-      colOffset = add(colOffset, mul(remained, i32_val(minVec)));
-
-      Value tileOffset = add(rowOffset, mul(colOffset, strideCol));
-      tileOffset = gep(dstPtrTy, dstPtrBase, tileOffset);
-
-      Value baseOffset =
-          add(mul(i32_val(baseOffsetRow), srcStrides[outOrder[1]]),
-              i32_val(baseOffsetCol));
-      Value basePtr = gep(dstPtrTy, tileOffset, baseOffset);
-
-      ret[elemIdx] = basePtr;
+      // row offset is simply row index
+      Value rowOff = mul(idxRow, strideRow);
+      // because swizzling happens at a granularity of outVec, we need to
+      // decompose the offset into a swizzled factor and a non-swizzled (ordered) 
+      // factor:
+      // colOffSwizzled = ((col // outVec) ^ phase) * outVec
+      // colOffOrdered = (col % outVec) // minVec * minVec
+      Value colOffSwizzled = xor_(udiv(idxCol, i32_val(outVec)), phase);
+      colOffSwizzled = mul(colOffSwizzled, i32_val(outVec));
+      Value colOffOrdered =  urem(idxCol, i32_val(outVec));
+      colOffOrdered = udiv(colOffOrdered, i32_val(minVec));
+      colOffOrdered = mul(colOffOrdered, i32_val(minVec));
+      Value colOff = add(colOffSwizzled, colOffOrdered);
+      // compute non-immediate offset
+      Value offset = add(rowOff, mul(colOff, strideCol));
+      Value currPtr = gep(dstPtrTy, dstPtrBase, offset);
+      // compute immediate offset
+      Value immedateOff =
+          add(mul(i32_val(immedateOffRow), srcStrides[inOrder[1]]),
+              i32_val(immedateOffCol));
+      ret[elemIdx] = gep(dstPtrTy, currPtr, immedateOff);
     }
     return ret;
   }
