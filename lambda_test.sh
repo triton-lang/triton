d8f7822fcba0a4deb99cda194e6b5b92432c1bf2
gaxler <g.axler@gmail.com>
2023-01-14 06:05:32 +0000

    kernel testing code

--- lambda_test.sh
+++ lambda_test.sh
@@ -16,7 +16,7 @@ cp $TT/aot_kernel_tests /tmp/aot_test
 cd /tmp/aot_test
 # python $TTPY/triton/tools/ttc.py vector_addition.py --infer >> dummy.yml
 python $TTC vector_addition.py --infer >> auto_infer_test.yml
-
-mkdir build
 python $TTC vector_addition.py --config test.yml -o /tmp/aot_test/build
+
+cd build
 $(python build.py main.c add_kernel0.c -I /tmp/aot_test/build/ -o main); ./main
--- python/examples/vector_addition.py
+++ python/examples/vector_addition.py
@@ -29,33 +29,3 @@ def add_kernel(
     output = x + y
     # Write x + y back to DRAM
     tl.store(output_ptr + offsets, output, mask=mask)
-
-
-@triton.jit
-def infered_ann_add_kernel(
-    x_ptr: "*fp32(16)",  # *Pointer* to first input vector
-    y_ptr: "*fp32(16)",  # *Pointer* to second input vector
-    output_ptr: "*fp32(16)",  # *Pointer* to output vector
-    n_elements: "i32(16)",  # Size of the vector
-    BLOCK_SIZE: tl.constexpr = [32, 64]  # Number of elements each program should process
-    # NOTE: `constexpr` so it can be used as a shape value
-):
-    # There are multiple 'program's processing different data. We identify which program
-    # we are here
-    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0
-    # This program will process inputs that are offset from the initial data.
-    # for instance, if you had a vector of length 256 and block_size of 64, the programs
-    # would each access the elements [0:64, 64:128, 128:192, 192:256].
-    # Note that offsets is a list of pointers
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    # Create a mask to guard memory operations against out-of-bounds accesses
-    mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not a
-    # multiple of the block size
-    x = tl.load(x_ptr + offsets, mask=mask)
-    y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
-    # Write x + y back to DRAM
-    tl.store(output_ptr + offsets, output, mask=mask)
-
