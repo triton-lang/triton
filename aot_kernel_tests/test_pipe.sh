b59c320c817452e157c8ba488323fe82eabedae2
thexler <g.axler@gmail.com>
2023-01-13 23:15:38 -0800

    clean branch

--- aot_kernel_tests/test_pipe.sh
+++ aot_kernel_tests/test_pipe.sh
@@ -3,7 +3,7 @@ TTC=~/triton/python/triton/tools/ttc.py
 
 git clone https://github.com/gaxler/triton.git
 cd triton/python
-git checkout tmp
+git checkout aot-mlir-backend
 pip install cmake
 pip install -e .
 
