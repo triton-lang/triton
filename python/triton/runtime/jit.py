c82daebddc504f3c497aba4a07f980bbd8f82ce3
nishantsikarwar <nsikarwar@ch.iitr.ac.in>
2023-01-29 15:42:24 +0530

    fix: using set comprehension
    Signed-off-by: nishantsikarwar <nsikarwar@ch.iitr.ac.in>

--- python/triton/runtime/jit.py
+++ python/triton/runtime/jit.py
@@ -301,7 +301,7 @@ def {self.fn.__name__}({', '.join(self.arg_names)}, grid, num_warps=4, num_stage
         self.has_defaults = any(v.default != inspect._empty for v in signature.parameters.values())
         # specialization hints
         self.do_not_specialize = [] if do_not_specialize is None else do_not_specialize
-        self.do_not_specialize = set([self.arg_names.index(arg) if isinstance(arg, str) else arg for arg in self.do_not_specialize])
+        self.do_not_specialize = {self.arg_names.index(arg) if isinstance(arg, str) else arg for arg in self.do_not_specialize}
         # function source code (without decorators)
         self.src = textwrap.dedent(inspect.getsource(fn))
         self.src = self.src[self.src.find("def"):]
