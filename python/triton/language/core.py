9f253dc3684e85431c9712df3c06cebf8ad160b3
Philippe Tillet <phil@openai.com>
2023-01-31 18:04:19 -0800

    [FRONTEND] Adding static range (#1130)
    Included: Revert "[BACKEND] Replace `mlir::topologicalSort` with a
    custom implementation (#1113)"

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -1307,3 +1307,33 @@ def printf(prefix, *args, _builder=None):
     for arg in args:
         new_args.append(_to_tensor(arg, _builder))
     return semantic.printf(new_prefix, new_args, _builder)
+
+# -----------------------
+# Iterators
+# -----------------------
+
+
+class static_range:
+
+    """Iterator that counts upward forever."""
+
+    def __init__(self, arg1, arg2=None, step=None):
+        assert isinstance(arg1, constexpr)
+        if step is None:
+            self.step = constexpr(1)
+        else:
+            assert isinstance(step, constexpr)
+            self.step = step
+        if arg2 is None:
+            self.start = constexpr(0)
+            self.end = arg1
+        else:
+            assert isinstance(arg2, constexpr)
+            self.start = arg1
+            self.end = arg2
+
+    def __iter__(self):
+        raise RuntimeError("static_range can only be used in @triton.jit'd functions")
+
+    def __next__(self):
+        raise RuntimeError("static_range can only be used in @triton.jit'd functions")
