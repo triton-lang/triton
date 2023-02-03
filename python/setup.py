7bcf7cdb6cbab0b76156253d8013f38829a61957
Phil Tillet <phil@openai.com>
2023-02-03 01:07:32 -0800

    .

--- python/setup.py
+++ python/setup.py
@@ -53,7 +53,7 @@ def get_llvm_package_info():
     system = platform.system()
     vglibc = tuple(map(int, platform.libc_ver()[1].split('.')))
     vglibc = vglibc[0]*100 + vglibc[1]
-    linux_suffix = 'ubuntu-18.04' if vglibc > 217 else 'centos7'
+    linux_suffix = 'ubuntu-18.04' if vglibc > 217 else 'centos-7'
     system_suffix = {"Linux": "linux-gnu-{}".format(linux_suffix), "Darwin": "apple-darwin"}[system]
     use_assert_enabled_llvm = check_env_flag("TRITON_USE_ASSERT_ENABLED_LLVM", "False")
     release_suffix = "assert" if use_assert_enabled_llvm else "release"
