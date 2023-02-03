539b1fafc57c08393bd307deb2386983d8284738
Phil Tillet <phil@openai.com>
2023-02-02 23:25:27 -0800

    .

--- python/setup.py
+++ python/setup.py
@@ -59,6 +59,21 @@ def get_llvm_package_info():
     url = "https://github.com/ptillet/triton-llvm-releases/releases/download/llvm-14.0.6-f28c006a5895/{}.tar.xz".format(name)
     return Package("llvm", name, url, "lib", "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH")
 
+# def get_llvm_package_info():
+#     # download if nothing is installed
+#     system = platform.system()
+#     system_suffix = {"Linux": "linux-gnu-ubuntu-18.04", "Darwin": "apple-darwin"}[system]
+#     use_assert_enabled_llvm = check_env_flag("TRITON_USE_ASSERT_ENABLED_LLVM", "False")
+#     if use_assert_enabled_llvm:
+#         name = 'llvm+mlir-14.0.0-x86_64-{}-assert'.format(system_suffix)
+#         url = "https://github.com/shintaro-iwasaki/llvm-releases/releases/download/llvm-14.0.0-329fda39c507/{}.tar.xz".format(name)
+#     else:
+#         name = 'clang+llvm-14.0.0-x86_64-{}'.format(system_suffix)
+#         url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/{}.tar.xz".format(name)
+#     return Package("llvm", name, url, "lib", "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH")
+
+
+
 def get_thirdparty_packages(triton_cache_path):
     packages = [get_pybind11_package_info(), get_llvm_package_info()]
     thirdparty_cmake_args = []
