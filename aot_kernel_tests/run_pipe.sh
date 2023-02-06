f2aa54b846100804e385c08df2b135428d4aa642
thexler <g.axler@gmail.com>
2023-02-06 14:48:20 -0800

    tt-linker takes output param

--- aot_kernel_tests/run_pipe.sh
+++ aot_kernel_tests/run_pipe.sh
@@ -11,7 +11,7 @@ cp $TT/aot_kernel_tests/build_ex/* /tmp/aot_test/build/.
 cd /tmp/aot_test 
 python $TTC vector_addition.py -n add_kernel --signature *fp32:16 *fp32:16 *fp32:16 i32:16 --BLOCK_SIZE 64 --out-name vec_add_64 -o /tmp/aot_test/build/add_kernel64
 python $TTC vector_addition.py -n add_kernel --signature *fp32:16 *fp32:16 *fp32:16 i32:16 --BLOCK_SIZE 128 --out-name vec_add_128 -o /tmp/aot_test/build/add_kernel128
-python $TTL /tmp/aot_test/build/add_kernel64.h /tmp/aot_test/build/add_kernel128.h >> /tmp/aot_test/build/dispatcher.c 
+python $TTL /tmp/aot_test/build/add_kernel64.h /tmp/aot_test/build/add_kernel128.h -o /tmp/aot_test/build/dispatcher
  
 cd build 
 # Use preprocessor to dump the linked dispatcher.c into main.c (thats why no dispatcher passed to compiler)
