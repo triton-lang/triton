bcf723a670fb7b466ad663054d94681668d0e8f3
Phil Tillet <phil@openai.com>
2023-02-09 17:42:14 -0800

    .

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -1952,69 +1952,6 @@ module attributes {"triton_gpu.num-warps" = 4 : i32} {
     assert torch.equal(z, x)
 
 
-# layouts = [
-#     BlockedLayout([1, 8], [2, 16], [4, 1], [1, 0]),
-#     BlockedLayout([1, 4], [4, 8], [2, 2], [1, 0]),
-#     BlockedLayout([1, 1], [1, 32], [2, 2], [1, 0]),
-#     BlockedLayout([8, 1], [16, 2], [1, 4], [0, 1]),
-#     BlockedLayout([4, 1], [8, 4], [2, 2], [0, 1]),
-#     BlockedLayout([1, 1], [32, 1], [2, 2], [0, 1]),
-#     BlockedLayout([4, 4], [1, 32], [4, 1], [1, 0])
-# ]
-
-
-# @pytest.mark.parametrize("shape", [(128, 128)])
-# @pytest.mark.parametrize("dtype", ['float16'])
-# @pytest.mark.parametrize("src_layout", layouts)
-# @pytest.mark.parametrize("dst_layout", layouts)
-# def test_convert2d(dtype, shape, src_layout, dst_layout, device='cuda'):
-#     if str(src_layout) == str(dst_layout):
-#         pytest.skip()
-#     if 'mma' in str(src_layout) and 'mma' in str(dst_layout):
-#         pytest.skip()
-
-#     ir = f"""
-# #src = {src_layout}
-# #dst = {dst_layout}
-# """ + """
-# module attributes {"triton_gpu.num-warps" = 4 : i32} {
-#   func public @kernel_0d1d(%arg0: !tt.ptr<f16> {tt.divisibility = 16 : i32}, %arg1: !tt.ptr<f16> {tt.divisibility = 16 : i32}) {
-#     %cst = arith.constant dense<128> : tensor<128x1xi32, #src>
-#     %0 = tt.make_range {end = 128 : i32, start = 0 : i32} : tensor<128xi32, #triton_gpu.slice<{dim = 1, parent = #src}>>
-#     %1 = tt.make_range {end = 128 : i32, start = 0 : i32} : tensor<128xi32, #triton_gpu.slice<{dim = 0, parent = #src}>>
-#     %2 = tt.splat %arg0 : (!tt.ptr<f16>) -> tensor<128x128x!tt.ptr<f16>, #src>
-#     %4 = tt.expand_dims %0 {axis = 1 : i32} : (tensor<128xi32, #triton_gpu.slice<{dim = 1, parent = #src}>>) -> tensor<128x1xi32, #src>
-#     %5 = arith.muli %4, %cst : tensor<128x1xi32, #src>
-#     %6 = tt.expand_dims %1 {axis = 0 : i32} : (tensor<128xi32, #triton_gpu.slice<{dim = 0, parent = #src}>>) -> tensor<1x128xi32, #src>
-#     %7 = tt.broadcast %6 : (tensor<1x128xi32, #src>) -> tensor<128x128xi32, #src>
-#     %8 = tt.broadcast %5 : (tensor<128x1xi32, #src>) -> tensor<128x128xi32, #src>
-#     %9 = arith.addi %8, %7 : tensor<128x128xi32, #src>
-#     %10 = tt.addptr %2, %9 : tensor<128x128x!tt.ptr<f16>, #src>, tensor<128x128xi32, #src>
-#     %11 = tt.load %10 {cache = 1 : i32, evict = 1 : i32, isVolatile = false} : tensor<128x128xf16, #src>
-#     %3 = tt.splat %arg1 : (!tt.ptr<f16>) -> tensor<128x128x!tt.ptr<f16>, #dst>
-#     %12 = triton_gpu.convert_layout %9 : (tensor<128x128xi32, #src>) -> tensor<128x128xi32, #dst>
-#     %13 = triton_gpu.convert_layout %11 : (tensor<128x128xf16, #src>) -> tensor<128x128xf16, #dst>
-#     %14 = tt.addptr %3, %12 : tensor<128x128x!tt.ptr<f16>, #dst>, tensor<128x128xi32, #dst>
-#     tt.store %14, %13 : tensor<128x128xf16, #dst>
-#     return
-#   }
-# }
-# """
-
-#     x = to_triton(numpy_random(shape, dtype_str=dtype))
-#     z = torch.empty_like(x)
-
-#     # write the IR to a temporary file using mkstemp
-#     import tempfile
-#     with tempfile.NamedTemporaryFile(mode='w', suffix='.ttgir') as f:
-#         f.write(ir)
-#         f.flush()
-#         kernel = triton.compile(f.name)
-#     kernel[(1, 1, 1)](x.data_ptr(), z.data_ptr())
-
-#     assert torch.equal(z, x)
-
-
 def test_load_scalar_with_mask():
     @triton.jit
     def kernel(Input, Index, Out, N: int):
