1ee649d2bad3d07ecf08b4ec39b19be3016266c2
Phil Tillet <phil@openai.com>
2023-02-12 11:40:46 -0800

    .

--- lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMBase.h
+++ lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMBase.h
@@ -7,6 +7,7 @@
 #include "triton/Analysis/Allocation.h"
 
 //
+#include "DotOpHelpers.h"
 #include "Utility.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "triton/Analysis/AxisInfo.h"
@@ -695,7 +696,86 @@ private:
   emitBaseIndexForMmaLayoutV1(Location loc, ConversionPatternRewriter &rewriter,
                               const MmaEncodingAttr &mmaLayout,
                               ArrayRef<int64_t> shape) const {
-    llvm_unreachable("emitIndicesForMmaLayoutV1 not implemented");
+
+    auto wpt = mmaLayout.getWarpsPerCTA();
+    auto fpw = LLVM::DotOpMmaV1ConversionHelper::fpw;
+    auto [isARow, isBRow, isAVec4, isBVec4, id] =
+        mmaLayout.decodeVoltaLayoutStates();
+
+    Value thread = getThreadId(rewriter, loc);
+    auto *ctx = thread.getContext();
+    Value _1 = i32_val(1);
+    Value _2 = i32_val(2);
+    Value _4 = i32_val(4);
+    Value _16 = i32_val(16);
+    Value _32 = i32_val(32);
+    Value _fpw0 = i32_val(fpw[0]);
+    Value _fpw1 = i32_val(fpw[1]);
+
+    LLVM::DotOpMmaV1ConversionHelper::AParam aParam(isARow, isAVec4);
+    LLVM::DotOpMmaV1ConversionHelper::BParam bParam(isBRow, isBVec4);
+
+    SmallVector<int, 2> rep({aParam.rep[0], bParam.rep[1]});
+    SmallVector<int, 2> spw({aParam.spw[0], bParam.spw[1]});
+    SmallVector<unsigned, 2> shapePerCTA({spw[0] * wpt[0], spw[1] * wpt[1]});
+
+    Value lane = urem(thread, _32);
+    Value warp = udiv(thread, _32);
+
+    Value warp0 = urem(warp, i32_val(wpt[0]));
+    Value warp12 = udiv(warp, i32_val(wpt[0]));
+    Value warp1 = urem(warp12, i32_val(wpt[1]));
+
+    // warp offset
+    Value offWarpM = mul(warp0, i32_val(spw[0]));
+    Value offWarpN = mul(warp1, i32_val(spw[1]));
+    // quad offset
+    Value offQuadM = mul(udiv(and_(lane, _16), _4), _fpw0);
+    Value offQuadN = mul(udiv(and_(lane, _16), _4), _fpw1);
+    // pair offset
+    Value offPairM = udiv(urem(lane, _16), _4);
+    offPairM = urem(offPairM, _fpw0);
+    offPairM = mul(offPairM, _4);
+    Value offPairN = udiv(urem(lane, _16), _4);
+    offPairN = udiv(offPairN, _fpw0);
+    offPairN = urem(offPairN, _fpw1);
+    offPairN = mul(offPairN, _4);
+
+    // sclare
+    offPairM = mul(offPairM, i32_val(rep[0] / 2));
+    offQuadM = mul(offQuadM, i32_val(rep[0] / 2));
+    offPairN = mul(offPairN, i32_val(rep[1] / 2));
+    offQuadN = mul(offQuadN, i32_val(rep[1] / 2));
+
+    // quad pair offset
+    Value offLaneM = add(offPairM, offQuadM);
+    Value offLaneN = add(offPairN, offQuadN);
+    // a, b offset
+    Value offsetAM = add(offWarpM, offLaneM);
+    Value offsetBN = add(offWarpN, offLaneN);
+    // m indices
+    Value offsetCM = add(and_(lane, _1), offsetAM);
+    // SmallVector<Value> idxM;
+    // for (unsigned m = 0; m < shape[0]; m += shapePerCTA[0])
+    //   for (unsigned mm = 0; mm < rep[0]; ++mm)
+    //     idxM.push_back(add(offsetCM, i32_val(m + mm * 2)));
+
+    // n indices
+    Value offsetCN = add((and_(lane, _2)), (add(offWarpN, offPairN)));
+    // SmallVector<Value> idxN;
+    // for (int n = 0; n < shape[1]; n += shapePerCTA[1]) {
+    //   for (int nn = 0; nn < rep[1]; ++nn) {
+    //     idxN.push_back(add(offsetCN, i32_val(n + nn / 2 * 4 +
+    //                                          (nn % 2) * 2 * fpw[1] *
+    //                                          rep[1])));
+    //     idxN.push_back(
+    //         add(offsetCN,
+    //             i32_val(n + nn / 2 * 4 + (nn % 2) * 2 * fpw[1] * rep[1] +
+    //             1)));
+    //   }
+    // }
+
+    return {offsetCM, offsetCN};
   }
 
   SmallVector<SmallVector<unsigned>>
@@ -769,9 +849,9 @@ private:
   SmallVector<SmallVector<Value>> emitIndicesForDistributedLayout(
       Location loc, ConversionPatternRewriter &rewriter,
       const Attribute &layout, ArrayRef<int64_t> shape) const {
-    if (auto mmaLayout = layout.template dyn_cast<MmaEncodingAttr>()) {
-      assert(!mmaLayout.isVolta());
-    }
+    // if (auto mmaLayout = layout.template dyn_cast<MmaEncodingAttr>()) {
+    //   assert(!mmaLayout.isVolta());
+    // }
 
     // step 1, delinearize threadId to get the base index
     auto multiDimBase = emitBaseIndexForLayout(loc, rewriter, layout, shape);
