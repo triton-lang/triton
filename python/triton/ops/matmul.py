fc4c391ba0e66f675526413a0ddb4bbd98125bf2
Horace He <chilli@fb.com>
2023-02-16 02:27:34 +0000

    Changed for loop to iterate by 1 in matmuls

--- python/triton/ops/matmul.py
+++ python/triton/ops/matmul.py
@@ -71,8 +71,8 @@ def _kernel(A, B, C, M, N, K,
     # matrix multiplication
     pid = tl.program_id(0)
     pid_z = tl.program_id(1)
-    grid_m = (M + BLOCK_M - 1) // BLOCK_M
-    grid_n = (N + BLOCK_N - 1) // BLOCK_N
+    grid_m = tl.cdiv(M, BLOCK_M)
+    grid_n = tl.cdiv(N, BLOCK_N)
     # re-order program ID for better L2 performance
     width = GROUP_M * grid_n
     group_id = pid // width
@@ -89,7 +89,7 @@ def _kernel(A, B, C, M, N, K,
     A = A + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
     B = B + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
     acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=ACC_TYPE)
-    for k in range(K, 0, -BLOCK_K * SPLIT_K):
+    for k in range(0, tl.cdiv(K, BLOCK_K * SPLIT_K)):
         if EVEN_K:
             a = tl.load(A)
             b = tl.load(B)
