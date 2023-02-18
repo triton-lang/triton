351ffc72d084159158e8a595071ff2f774e20dda
Jokeren <kerenzhou@openai.com>
2023-02-17 23:45:19 -0500

    Add comment

--- lib/Analysis/Utility.cpp
+++ lib/Analysis/Utility.cpp
@@ -354,6 +354,10 @@ public:
                                        value, op->getDialect())));
       return success();
     }
+    // Dead code analysis requires every operands has initialized ConstantValue
+    // state before it is visited.
+    // https://github.com/llvm/llvm-project/blob/2ec1aba2b69faa1de5f71832a48e25aa3b5d5314/mlir/lib/Analysis/DataFlow/DeadCodeAnalysis.cpp#L322
+    // That's why we need to set all operands to unknown constants.
     setAllToUnknownConstants(op->getResults());
     for (Region &region : op->getRegions()) {
       for (Block &block : region.getBlocks())
