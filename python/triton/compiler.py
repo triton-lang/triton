711370f008486aff8602f64d2855badf145a1f0a
Rohit Santhanam <rohit.santhanam@amd.com>
2023-02-18 05:02:46 +0000

    Upgrade HIP launcher code to be at parity with CUDA.

--- python/triton/compiler.py
+++ python/triton/compiler.py
@@ -1196,109 +1196,122 @@ def generate_launcher(constants, signature):
     #define __HIP_PLATFORM_AMD__
     #include <hip/hip_runtime.h>
     #include <Python.h>
-
     static inline void gpuAssert(hipError_t code, const char *file, int line)
     {{
-    if (code != HIP_SUCCESS)
-    {{
-        const char* prefix = "Triton Error [HIP]: ";
-        const char* str = hipGetErrorString(code);
-        char err[1024] = {{0}};
-        strcat(err, prefix);
-        strcat(err, str);
-        PyErr_SetString(PyExc_RuntimeError, err);
+      if (code != HIP_SUCCESS)
+      {{
+         const char* prefix = "Triton Error [HIP]: ";
+         const char* str = hipGetErrorString(code);
+         char err[1024] = {{0}};
+         strcat(err, prefix);
+         strcat(err, str);
+         PyErr_SetString(PyExc_RuntimeError, err);
+      }}
     }}
-    }}
-
     #define HIP_CHECK(ans) {{ gpuAssert((ans), __FILE__, __LINE__); }}
-
     void _launch(int gridX, int gridY, int gridZ, int num_warps, int shared_memory, hipStream_t stream, hipFunction_t function, {arg_decls}) {{
-    void *params[] = {{ {', '.join(f"&arg{i}" for i in signature.keys() if i not in constants)} }};
-    if(gridX*gridY*gridZ > 0){{
-        HIP_CHECK(hipModuleLaunchKernel(function, gridX, gridY, gridZ, 32*num_warps, 1, 1, shared_memory, stream, params, 0));
+      void *params[] = {{ {', '.join(f"&arg{i}" for i in signature.keys() if i not in constants)} }};
+      if(gridX*gridY*gridZ > 0){{
+          HIP_CHECK(hipModuleLaunchKernel(function, gridX, gridY, gridZ, 32*num_warps, 1, 1, shared_memory, stream, params, 0));
+      }}
     }}
-    }}
-
-    static inline hipDeviceptr_t getPointer(PyObject *obj, int idx) {{
-    if (PyLong_Check(obj)) {{
-        return (hipDeviceptr_t)PyLong_AsUnsignedLongLong(obj);
-    }}
-    if (obj == Py_None) {{
-        return (hipDeviceptr_t)0;
-    }}
-    PyObject *ptr = PyObject_GetAttrString(obj, "data_ptr");
-    if(ptr){{
+    typedef struct _DevicePtrInfo {{
+      hipDeviceptr_t dev_ptr;
+      bool valid;
+    }} DevicePtrInfo;
+    static inline DevicePtrInfo getPointer(PyObject *obj, int idx) {{
+      DevicePtrInfo ptr_info;
+      ptr_info.dev_ptr = 0;
+      ptr_info.valid = true;
+      if (PyLong_Check(obj)) {{
+        ptr_info.dev_ptr = (hipDeviceptr_t)PyLong_AsUnsignedLongLong(obj);
+        return ptr_info;
+      }}
+      if (obj == Py_None) {{
+        // valid nullptr
+        return ptr_info;
+      }}
+      PyObject *ptr = PyObject_GetAttrString(obj, "data_ptr");
+      if(ptr){{
         PyObject *empty_tuple = PyTuple_New(0);
         PyObject *ret = PyObject_Call(ptr, empty_tuple, NULL);
         Py_DECREF(empty_tuple);
         Py_DECREF(ptr);
         if (!PyLong_Check(ret)) {{
-        PyErr_SetString(PyExc_TypeError, "data_ptr method of Pointer object must return 64-bit int");
+          PyErr_SetString(PyExc_TypeError, "data_ptr method of Pointer object must return 64-bit int");
+          ptr_info.valid = false;
+          return ptr_info;
         }}
-        return (hipDeviceptr_t)PyLong_AsUnsignedLongLong(ret);
+        ptr_info.dev_ptr = (hipDeviceptr_t)PyLong_AsUnsignedLongLong(ret);
+        if(!ptr_info.dev_ptr)
+          return ptr_info;
+        uint64_t dev_ptr;
+        hipError_t status = hipPointerGetAttribute(&dev_ptr, HIP_POINTER_ATTRIBUTE_DEVICE_POINTER, ptr_info.dev_ptr);
+        if (status == hipErrorInvalidValue) {{
+            PyErr_Format(PyExc_ValueError,
+                         "Pointer argument (at %d) cannot be accessed from Triton (cpu tensor?)", idx);
+            ptr_info.valid = false;
+        }}
+        ptr_info.dev_ptr = (hipDeviceptr_t)dev_ptr;
+        return ptr_info;
+      }}
+      PyErr_SetString(PyExc_TypeError, "Pointer argument must be either uint64 or have data_ptr method");
+      return ptr_info;
     }}
-    PyErr_SetString(PyExc_TypeError, "Pointer argument must be either uint64 or have data_ptr method");
-    return (hipDeviceptr_t)0;
-    }}
-
     static PyObject* launch(PyObject* self, PyObject* args) {{
-    int gridX, gridY, gridZ;
-    uint64_t _stream;
-    uint64_t _function;
-    int num_warps;
-    int shared_memory;
-    PyObject *launch_enter_hook = NULL;
-    PyObject *launch_exit_hook = NULL;
-    PyObject *compiled_kernel = NULL;
-    PyObject *hook_ret = NULL;
-    {' '.join([f"{_extracted_type(ty)} _arg{i}; " for i, ty in signature.items()])}
-    if(!PyArg_ParseTuple(args, \"{format}\", &gridX, &gridY, &gridZ, &num_warps, &shared_memory, &_stream, &_function, &launch_enter_hook, &launch_exit_hook, &compiled_kernel, {', '.join(f"&_arg{i}" for i, ty in signature.items())})) {{
-        return NULL;
-    }}
+      int gridX, gridY, gridZ;
+      uint64_t _stream;
+      uint64_t _function;
+      int num_warps;
+      int shared_memory;
+      PyObject *launch_enter_hook = NULL;
+      PyObject *launch_exit_hook = NULL;
+      PyObject *compiled_kernel = NULL;
+      PyObject *hook_ret = NULL;
+      {' '.join([f"{_extracted_type(ty)} _arg{i}; " for i, ty in signature.items()])}
+      if(!PyArg_ParseTuple(args, \"{format}\", &gridX, &gridY, &gridZ, &num_warps, &shared_memory, &_stream, &_function, &launch_enter_hook, &launch_exit_hook, &compiled_kernel, {', '.join(f"&_arg{i}" for i, ty in signature.items())})) {{
+          return NULL;
+      }}
 
-    if (launch_enter_hook != Py_None) {{
-        PyObject *new_args = PyTuple_Pack(1, compiled_kernel);
-        hook_ret = PyObject_CallObject(launch_enter_hook, new_args);
-        Py_DECREF(new_args);
+      if (launch_enter_hook != Py_None) {{
+          PyObject *new_args = PyTuple_Pack(1, compiled_kernel);
+          hook_ret = PyObject_CallObject(launch_enter_hook, new_args);
+          Py_DECREF(new_args);
+      }}
+      // raise exception asap
+      {"; ".join([f"DevicePtrInfo ptr_info{i} = getPointer(_arg{i}, {i}); if (!ptr_info{i}.valid) return NULL;" if ty[0] == "*" else "" for i, ty in signature.items()])};
+      _launch(gridX, gridY, gridZ, num_warps, shared_memory, (hipStream_t)_stream, (hipFunction_t)_function, {', '.join(f"ptr_info{i}.dev_ptr" if ty[0]=="*" else f"_arg{i}"for i, ty in signature.items())});
+      if (launch_exit_hook != Py_None) {{
+          PyObject *new_args = NULL;
+          if (hook_ret) {{
+              new_args = PyTuple_Pack(2, compiled_kernel, hook_ret);
+          }} else {{
+              new_args = PyTuple_Pack(1, compiled_kernel);
+          }}
+          hook_ret = PyObject_CallObject(launch_exit_hook, new_args);
+          Py_DECREF(new_args);
+      }}
+      if (hook_ret) {{
+          Py_DECREF(hook_ret);
+      }}
+      if(PyErr_Occurred()) {{
+          return NULL;
+      }}
+      // return None
+      Py_INCREF(Py_None);
+      return Py_None;
     }}
-
-    _launch(gridX, gridY, gridZ, num_warps, shared_memory, (hipStream_t)_stream, (hipFunction_t)_function, {', '.join(f"getPointer(_arg{i},{i})" if ty[0]=="*" else f"_arg{i}"for i, ty in signature.items())});
-
-    if (launch_exit_hook != Py_None) {{
-        PyObject *new_args = NULL;
-        if (hook_ret) {{
-            new_args = PyTuple_Pack(2, compiled_kernel, hook_ret);
-        }} else {{
-            new_args = PyTuple_Pack(1, compiled_kernel);
-        }}
-        hook_ret = PyObject_CallObject(launch_exit_hook, new_args);
-        Py_DECREF(new_args);
-    }}
-
-    if (hook_ret) {{
-        Py_DECREF(hook_ret);
-    }}
-    if(PyErr_Occurred()) {{
-        return NULL;
-    }}
-    // return None
-    Py_INCREF(Py_None);
-    return Py_None;
-    }}
-
     static PyMethodDef ModuleMethods[] = {{
     {{"launch", launch, METH_VARARGS, "Entry point for all kernels with this signature"}},
     {{NULL, NULL, 0, NULL}} // sentinel
     }};
-
     static struct PyModuleDef ModuleDef = {{
     PyModuleDef_HEAD_INIT,
-    \"launcher\",
+    \"__triton_launcher\",
     NULL, //documentation
     -1, //size
     ModuleMethods
     }};
-
     PyMODINIT_FUNC PyInit_launcher(void) {{
     PyObject *m = PyModule_Create(&ModuleDef);
     if(m == NULL) {{
