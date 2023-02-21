d54c04abe2c3e67b2139c68cdbda87b59e8dd01b
Natalia Gimelshein <ngimel@fb.com>
2023-02-21 14:01:14 -0800

    Revert "Minor fix to support sm_90 (#1125)" (#1226)
    To support sm_89 without warnings
    Confirmed with @ptrblck that it works. 
    This reverts commit 3e8d83b7cc11b1d26e7f1fb7f0022d03aa2377ae.

--- python/src/triton.cc
+++ python/src/triton.cc
@@ -1495,9 +1495,7 @@ void init_triton_translation(py::module &m) {
           std::string cmd;
           int err;
           cmd = ptxasPath + " -v --gpu-name=sm_" + std::to_string(capability) +
-                (capability == 90 ? "a " : " ") + _fsrc + " -o " + _fsrc +
-                ".o 2> " + _flog;
-
+                " " + _fsrc + " -o " + _fsrc + ".o 2> " + _flog;
           err = system(cmd.c_str());
           if (err != 0) {
             std::ifstream _log(_flog);
