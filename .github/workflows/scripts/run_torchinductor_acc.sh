7f52d14e652f872b97e40a5a5ccf997013c18e3c
Jokeren <kerenzhou@openai.com>
2023-02-22 17:40:59 -0800

    Update

--- .github/workflows/scripts/run_torchinductor_acc.sh
+++ .github/workflows/scripts/run_torchinductor_acc.sh
@@ -11,15 +11,21 @@ TEST_REPORTS_DIR=$TEST_REPORTS_DIR/acc
 mkdir -p "$TEST_REPORTS_DIR"
 
 for model in "${MODELS[@]}"; do
-  echo "Running performance test for $model"
-  python benchmarks/dynamo/"$model".py --ci --training --performance --disable-cudagraphs\
-    --device cuda --inductor --amp --output "$TEST_REPORTS_DIR"/"$model".csv
+  echo "Running accuracy test for $model"
+  python benchmarks/dynamo/"$model".py --ci --accuracy --timing --explain --inductor --device cuda \
+    --output "$TEST_REPORTS_DIR"/inference_"$model".csv
+  python benchmarks/dynamo/"$model".py --ci --accuracy --timing --explain --inductor --training --amp --device cuda \
+    --output "$TEST_REPORTS_DIR"/training_"$model".csv
+  python benchmarks/dynamo/"$model".py --ci --accuracy --timing --explain --inductor --dynamic-shapes --device cuda \
+    --output "$TEST_REPORTS_DIR"/dynamic_shapes_"$model".csv
 done
 
 cd "$ROOT" || exit
 for model in "${MODELS[@]}"; do
-  echo "Checking performance test for $model"
-  python .github/workflows/scripts/check_perf.py "$TEST_REPORTS_DIR"/"$model".csv
+  echo "Checking accuracy test for $model"
+  python .github/workflows/scripts/check_acc.py "$TEST_REPORTS_DIR"/inference_"$model".csv
+  python .github/workflows/scripts/check_acc.py "$TEST_REPORTS_DIR"/training_"$model".csv
+  python .github/workflows/scripts/check_acc.py "$TEST_REPORTS_DIR"/dynamic_shapes_"$model".csv
 done
 
 # go back to where we started
--- .github/workflows/scripts/run_torchinductor_perf.sh
+++ .github/workflows/scripts/run_torchinductor_perf.sh
@@ -12,20 +12,14 @@ mkdir -p "$TEST_REPORTS_DIR"
 
 for model in "${MODELS[@]}"; do
   echo "Running performance test for $model"
-  python benchmarks/dynamo/"$model".py --ci --accuracy --timing --explain --inductor --device cuda \
-    --output "$TEST_REPORTS_DIR"/inference_"$model".csv
-  python benchmarks/dynamo/"$model".py --ci --accuracy --timing --explain --inductor --training --amp --device cuda \
-    --output "$TEST_REPORTS_DIR"/training_"$model".csv
-  python benchmarks/dynamo/"$model".py --ci --accuracy --timing --explain --inductor --dynamic-shapes --device cuda \
-    --output "$TEST_REPORTS_DIR"/dynamic_shapes_"$model".csv
+  python benchmarks/dynamo/"$model".py --ci --training --performance --disable-cudagraphs\
+    --device cuda --inductor --amp --output "$TEST_REPORTS_DIR"/"$model".csv
 done
 
 cd "$ROOT" || exit
 for model in "${MODELS[@]}"; do
   echo "Checking performance test for $model"
-  python .github/workflows/scripts/check_perf.py --new "$TEST_REPORTS_DIR"/inference_"$model".csv --baseline .github/workflows/scripts/baseline_inference_"$model".csv
-  python .github/workflows/scripts/check_perf.py --new "$TEST_REPORTS_DIR"/training_"$model".csv --baseline .github/workflows/scripts/baseline_training_"$model".csv
-  python .github/workflows/scripts/check_perf.py --new "$TEST_REPORTS_DIR"/dynamic_shapes_"$model".csv --baseline .github/workflows/scripts/baseline_dynamic_shapes_"$model".csv
+  python .github/workflows/scripts/check_perf.py --new "$TEST_REPORTS_DIR"/"$model".csv --baseline .github/workflows/scripts/"$model".csv
 done
 
 # go back to where we started
