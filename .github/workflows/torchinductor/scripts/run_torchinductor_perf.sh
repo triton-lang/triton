07e71e4a260710dccbb87f3c0c0b8f6a4ca6dba5
Jokeren <kerenzhou@openai.com>
2023-02-24 10:12:12 -0800

    CI last fixes

--- .github/workflows/torchinductor/scripts/run_torchinductor_perf.sh
+++ .github/workflows/torchinductor/scripts/run_torchinductor_perf.sh
@@ -9,6 +9,10 @@ source /opt/torchinductor_venv/bin/activate
 # shellcheck source=/dev/null
 source "$INDUCTOR"/scripts/common.sh
 
+# lock GPU clocks to 1350 MHz
+sudo nvidia-smi -i 0 -pm 1
+sudo nvidia-smi -i 0 --lock-gpu-clocks=1350,1350
+
 cd "$PYTORCH_DIR" || exit
 TEST_REPORTS_DIR=$TEST_REPORTS_DIR/perf
 mkdir -p "$TEST_REPORTS_DIR"
@@ -24,6 +28,9 @@ for model in "${MODELS[@]}"; do
   echo "Checking performance test for $model"
   python3 "$INDUCTOR"/scripts/check_perf.py --new "$TEST_REPORTS_DIR"/"$model".csv --baseline "$INDUCTOR"/data/"$model".csv
 done
+
+# unlock GPU clocks
+sudo nvidia-smi -i 0 -rgc
 
 # go back to where we started
 cd "$ROOT" || exit
