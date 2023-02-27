c823c8fcb421c2496ce93d365ffebdbc081e802e
Jokeren <kerenzhou@openai.com>
2023-02-27 13:57:20 -0800

    Update

--- .github/workflows/torchinductor/scripts/check_perf.py
+++ .github/workflows/torchinductor/scripts/check_perf.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 # Create a named tuple for the output of the benchmark
 BenchmarkOutput = namedtuple(
-    'BenchmarkOutput', ['dev', 'name', 'batch_size', 'speedup'])
+    'BenchmarkOutput', ['dev', 'name', 'batch_size', 'speedup', 'latency'])
 
 
 def parse_output(file_path: str) -> dict:
@@ -12,13 +12,14 @@ def parse_output(file_path: str) -> dict:
     with open(file_path) as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
-            if i == 0:
+            if i == 0 or len(row) < 5:
                 continue
             dev = row[0]
             name = row[1]
             batch_size = row[2]
             speedup = float(row[3])
-            entries[name] = BenchmarkOutput(dev, name, batch_size, speedup)
+            latency = float(row[4])
+            entries[name] = BenchmarkOutput(dev, name, batch_size, speedup, latency)
     return entries
 
 
@@ -26,21 +27,21 @@ def compare(baseline: dict, new: dict, threshold: float) -> bool:
     for key in new:
         if key not in baseline:
             print(f"New benchmark {key} not found in baseline")
-        baseline_speedup = baseline[key].speedup
-        new_speedup = new[key].speedup
-        if new_speedup < baseline_speedup * (1 - threshold):
+        baseline_latency = baseline[key].latency
+        new_latency = new[key].latency
+        if new_latency < baseline_latency * (1 - threshold):
             print(
-                f"New benchmark {key} is slower than baseline: {new_speedup} vs {baseline_speedup}")
-        elif new_speedup > baseline_speedup * (1 + threshold):
+                f"New benchmark {key} is slower than baseline: {new_latency} vs {baseline_latency}")
+        elif new_latency > baseline_latency * (1 + threshold):
             print(
-                f"New benchmark {key} is faster than baseline: {new_speedup} vs {baseline_speedup}")
+                f"New benchmark {key} is faster than baseline: {new_latency} vs {baseline_latency}")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--baseline', required=True)
     parser.add_argument('--new', required=True)
-    parser.add_argument('--threshold', type=float, default=0.02)
+    parser.add_argument('--threshold', type=float, default=0.05)
     args = parser.parse_args()
     baseline = parse_output(args.baseline)
     new = parse_output(args.new)
