f8d3211d88c43f7e5517bcf9f82fe4b4f3713a34
Jokeren <kerenzhou@openai.com>
2023-02-28 12:26:04 -0800

    Add geomean

--- .github/workflows/torchinductor/scripts/check_perf.py
+++ .github/workflows/torchinductor/scripts/check_perf.py
@@ -23,7 +23,9 @@ def parse_output(file_path: str) -> dict:
     return entries
 
 
-def compare(baseline: dict, new: dict, threshold: float) -> bool:
+def compare(baseline: dict, new: dict, threshold: float, geomean_threshold: float) -> bool:
+    baseline_geomean = 1.0
+    new_geomean = 1.0
     for key in new:
         if key not in baseline:
             print(f"New benchmark {key} not found in baseline")
@@ -35,6 +37,15 @@ def compare(baseline: dict, new: dict, threshold: float) -> bool:
         elif new_latency > baseline_latency * (1 + threshold):
             print(
                 f"New benchmark {key} is slower than baseline: {new_latency} vs {baseline_latency}")
+        baseline_geomean *= baseline[key].speedup
+        new_geomean *= new[key].speedup
+
+    baseline_geomean = baseline_geomean ** (1 / len(baseline))
+    new_geomean = new_geomean ** (1 / len(new))
+    print(f"Baseline geomean: {baseline_geomean}")
+    print(f"New geomean: {new_geomean}")
+    assert new_geomean > baseline_geomean * (1 - geomean_threshold), \
+        f"New geomean is slower than baseline: {new_geomean} vs {baseline_geomean}"
 
 
 def main():
@@ -42,10 +53,11 @@ def main():
     parser.add_argument('--baseline', required=True)
     parser.add_argument('--new', required=True)
     parser.add_argument('--threshold', type=float, default=0.1)
+    parser.add_argument('--geomean-threshold', type=float, default=0.02)
     args = parser.parse_args()
     baseline = parse_output(args.baseline)
     new = parse_output(args.new)
-    compare(baseline, new, args.threshold)
+    compare(baseline, new, args.threshold, args.geomean_threshold)
 
 
 main()
