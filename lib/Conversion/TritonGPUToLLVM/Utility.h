20ef9a0908b973d302b9a3c12253e046732ccde3
Rohit Santhanam <rohit.santhanam@amd.com>
2023-03-03 08:01:19 +0000

    Fix for LLVM compiler's inability to auto vectorize float16 loads and stores on AMDGPU.

--- lib/Conversion/TritonGPUToLLVM/Utility.h
+++ lib/Conversion/TritonGPUToLLVM/Utility.h
@@ -35,6 +35,8 @@
 #define xor_(...) rewriter.create<LLVM::XOrOp>(loc, __VA_ARGS__)
 #define bitcast(val__, type__)                                                 \
   rewriter.create<LLVM::BitcastOp>(loc, type__, val__)
+#define addrspacecast(val__, type__)                                           \
+  rewriter.create<LLVM::AddrSpaceCastOp>(loc, type__, val__)
 #define gep(...) rewriter.create<LLVM::GEPOp>(loc, __VA_ARGS__)
 #define ptr_ty(...) LLVM::LLVMPointerType::get(__VA_ARGS__)
 #define insert_val(...) rewriter.create<LLVM::InsertValueOp>(loc, __VA_ARGS__)
