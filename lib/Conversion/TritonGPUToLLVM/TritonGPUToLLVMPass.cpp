1dcb08674bb039f078a2ce405f16e5c5ab20d646
Michael Melesse <micmelesse@gmail.com>
2023-03-16 12:28:04 -0500

    fix clang error

--- lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMPass.cpp
+++ lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMPass.cpp
@@ -121,7 +121,7 @@ struct FuncOpConversion : public FuncOpConversionBase {
       // Set an attribute to indicate this function is a kernel entry.
       newFuncOp->setAttr("rocdl.kernel",
                          rewriter.getIntegerAttr(type::u1Ty(ctx), 1));
-      
+
       // Set an attribute for maxntidx, it could be used in latter LLVM codegen
       newFuncOp->setAttr("rocdl.maxntid",
                          rewriter.getIntegerAttr(i32_ty, 32 * numWarps));
