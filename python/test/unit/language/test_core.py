b70e95254b6454a68e4bb145a678259d2bd6b451
Phil Tillet <phil@openai.com>
2023-03-23 20:00:40 -0700

    .

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -1511,6 +1511,7 @@ def test_masked_load(dtype_str, size, size_diff, device='cuda'):
     kernel[(1,)](input, output, input_size, output_size)
 
     reference_out = torch.cat((input, torch.ones((size_diff,), dtype=dtype, device=device)))
+    # print((output - reference_out).nonzero())
     torch.testing.assert_allclose(output, reference_out)
 
 # Testing masked loads with an intermate copy to shared memory run.
@@ -1542,15 +1543,15 @@ def test_masked_load_shared_memory(dtype, device='cuda'):
         in2_offsets = K_offsets[:, None] * in2_stride + N_offsets[None, :]
 
         # Load inputs.
-        x = tl.load(in1_ptr + in_offsets, mask=in_offsets < in_numel)
-        w = tl.load(in2_ptr + in2_offsets, mask=in2_offsets < in2_numel)
+        x = tl.load(in1_ptr + in_offsets, mask=in_offsets < M*K)
+        w = tl.load(in2_ptr + in2_offsets, mask=in2_offsets < K*N)
 
         # Without a dot product the memory doesn't get promoted to shared.
         o = tl.dot(x, w, out_dtype=tl.float32)
 
         # Store output
         output_offsets = M_offsets[:, None] * out_stride + N_offsets[None, :]
-        tl.store(output_ptr + output_offsets, o, mask=output_offsets < in2_numel)
+        tl.store(output_ptr + output_offsets, o, mask=output_offsets < M*N)
 
     pgm = _kernel[(1,)](in1, in2, out,
                         in1.stride()[0],
