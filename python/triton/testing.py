71da0df2175063e2aae4f1d1af1064b74f29addd
Phil Tillet <phil@openai.com>
2023-04-04 18:08:04 -0700

    .

--- python/triton/testing.py
+++ python/triton/testing.py
@@ -315,6 +315,20 @@ def nvsmi_attr(attrs):
     return ret
 
 
+def assert_almost_equal(x, y, decimal=2, err_msg=''):
+    import numpy.testing as npt
+    import torch
+    if isinstance(x, torch.Tensor):
+        if x.dtype == torch.bfloat16:
+            x = x.float()
+        x = x.cpu().detach().numpy()
+    if isinstance(y, torch.Tensor):
+        if y.dtype == torch.bfloat16:
+            y = y.float()
+        y = y.cpu().detach().numpy()
+    npt.assert_array_almost_equal(x, y, err_msg=err_msg, decimal=decimal)
+
+
 @contextmanager
 def set_gpu_clock(ref_sm_clock=1350, ref_mem_clock=1215):
     try:
