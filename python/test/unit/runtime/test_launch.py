a41bfbfb3d18f84a0d300980a95d439b0da137bc
Phil Tillet <phil@openai.com>
2023-04-11 10:27:27 -0700

    [TESTS] disable launch latency test

--- python/test/unit/runtime/test_launch.py
+++ python/test/unit/runtime/test_launch.py
@@ -1,19 +1,18 @@
 import gc
-import importlib
-import os
-import sys
-import tempfile
-import textwrap
-import time
+# import importlib
+# import os
+# import sys
+# import tempfile
+# import textwrap
+# import time
 import tracemalloc
-from typing import Tuple
 
 import torch
 
 import triton
 import triton.language as tl
 
-LATENCY_THRESHOLD_US = 46
+# from typing import Tuple
 
 
 def test_memory_leak() -> None:
@@ -44,62 +43,64 @@ def test_memory_leak() -> None:
         tracemalloc.stop()
 
 
-def test_kernel_launch_latency() -> None:
-    def define_kernel(kernel_name: str, num_tensor_args: int) -> str:
-        arg_str = ",".join([f"arg{i}: torch.Tensor" for i in range(num_tensor_args)])
-        arg_str += ", n_elements: int, BLOCK_SIZE: tl.constexpr"
-        func_str = f"""
-        import torch
+# LATENCY_THRESHOLD_US = 46
 
-        import triton
-        import triton.language as tl
+# def test_kernel_launch_latency() -> None:
+#     def define_kernel(kernel_name: str, num_tensor_args: int) -> str:
+#         arg_str = ",".join([f"arg{i}: torch.Tensor" for i in range(num_tensor_args)])
+#         arg_str += ", n_elements: int, BLOCK_SIZE: tl.constexpr"
+#         func_str = f"""
+#         import torch
 
-        @triton.jit
-        def {kernel_name}({arg_str}):
-            pass
-        """
-        with tempfile.NamedTemporaryFile(mode="w+t", suffix=".py", delete=False) as temp_file:
-            temp_file.write(textwrap.dedent(func_str))
-            temp_file_path = temp_file.name
+#         import triton
+#         import triton.language as tl
 
-        return temp_file_path
+#         @triton.jit
+#         def {kernel_name}({arg_str}):
+#             pass
+#         """
+#         with tempfile.NamedTemporaryFile(mode="w+t", suffix=".py", delete=False) as temp_file:
+#             temp_file.write(textwrap.dedent(func_str))
+#             temp_file_path = temp_file.name
 
-    def import_kernel(file_path, kernel_name):
-        directory, filename = os.path.split(file_path)
-        module_name, _ = os.path.splitext(filename)
-        sys.path.insert(0, directory)
+#         return temp_file_path
 
-        module = importlib.import_module(module_name)
-        kernel = getattr(module, kernel_name)
-        return kernel
+#     def import_kernel(file_path, kernel_name):
+#         directory, filename = os.path.split(file_path)
+#         module_name, _ = os.path.splitext(filename)
+#         sys.path.insert(0, directory)
 
-    def empty(*kernel_args: Tuple[torch.Tensor]):
-        first_arg = kernel_args[0]
-        n_elements = first_arg.numel()
-        grid = (triton.cdiv(n_elements, 1024),)
-        device = torch.cuda.current_device()
-        # Warmup
-        empty_kernel[grid](*kernel_args, n_elements, BLOCK_SIZE=1024, device=device)
-        torch.cuda.synchronize()
-        # Measure launch overhead at steady state
-        num_runs = 1000
-        start_time = time.time()
-        for i in range(num_runs):
-            empty_kernel[grid](*kernel_args, n_elements, BLOCK_SIZE=1024, device=device)
-        end_time = time.time()
-        latency_us = (end_time - start_time) / num_runs * 1e6
+#         module = importlib.import_module(module_name)
+#         kernel = getattr(module, kernel_name)
+#         return kernel
 
-        assert latency_us < LATENCY_THRESHOLD_US, "Kernel launch time has increased!"
+#     def empty(*kernel_args: Tuple[torch.Tensor]):
+#         first_arg = kernel_args[0]
+#         n_elements = first_arg.numel()
+#         grid = (triton.cdiv(n_elements, 1024),)
+#         device = torch.cuda.current_device()
+#         # Warmup
+#         empty_kernel[grid](*kernel_args, n_elements, BLOCK_SIZE=1024, device=device)
+#         torch.cuda.synchronize()
+#         # Measure launch overhead at steady state
+#         num_runs = 1000
+#         start_time = time.time()
+#         for i in range(num_runs):
+#             empty_kernel[grid](*kernel_args, n_elements, BLOCK_SIZE=1024, device=device)
+#         end_time = time.time()
+#         latency_us = (end_time - start_time) / num_runs * 1e6
 
-    num_tensor_args = 40
-    kernel_name = 'empty_kernel'
-    file_path = define_kernel(kernel_name, num_tensor_args)
-    empty_kernel = import_kernel(file_path, kernel_name)
+#         assert latency_us < LATENCY_THRESHOLD_US, "Kernel launch time has increased!"
 
-    # Initialize random tensors for the empty_kernel
-    torch.manual_seed(0)
-    size = 1024
-    kernel_args = (torch.rand(size, device='cuda') for i in range(num_tensor_args))
+#     num_tensor_args = 40
+#     kernel_name = 'empty_kernel'
+#     file_path = define_kernel(kernel_name, num_tensor_args)
+#     empty_kernel = import_kernel(file_path, kernel_name)
 
-    # Run empty, which would run empty_kernel internally
-    empty(*kernel_args)
+#     # Initialize random tensors for the empty_kernel
+#     torch.manual_seed(0)
+#     size = 1024
+#     kernel_args = (torch.rand(size, device='cuda') for i in range(num_tensor_args))
+
+#     # Run empty, which would run empty_kernel internally
+#     empty(*kernel_args)
