c0d86d3b04129a16a2491a5eadf5570c50acc4bd
Philippe Tillet <phil@openai.com>
2023-04-12 23:50:44 -0700

    [RUNTIME] refactor driver (#1515)
    Improved separation between different backends

--- python/triton/language/math.py
+++ python/triton/language/math.py
@@ -1,14 +1,9 @@
 import os
 
-import torch
-
+from ..runtime import driver
 from . import core, extern
 
-if torch.version.hip is not None:
-    LOCAL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cuda2gcn.bc")
-else:
-    LOCAL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "third_party", "cuda", "lib", "libdevice.10.bc")
-LIBDEVICE_PATH = os.getenv("TRITON_LIBDEVICE_PATH", LOCAL_PATH)
+LIBDEVICE_PATH = os.getenv("TRITON_LIBDEVICE_PATH", driver.libdevice_path)
 
 
 @extern.extern
--- python/triton/runtime/driver/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cuda import get_cuda_utils
-from .hip import get_hip_utils
-
-__all__ = ['get_cuda_utils', 'get_hip_utils']
