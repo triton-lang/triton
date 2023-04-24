6a119d9789646d20e0c85dd180999b800c04c20a
Ian O Connell <ianoconnell@openai.com>
2023-04-24 10:57:42 -0600

    Formatting

--- python/triton/runtime/cache.py
+++ python/triton/runtime/cache.py
@@ -1,10 +1,10 @@
 import json
 import os
+import random
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Dict, Optional
 
-import random
 
 def default_cache_dir():
     return os.path.join(Path.home(), ".triton", "cache")
