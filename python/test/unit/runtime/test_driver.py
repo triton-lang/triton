66a88e0c0fac7ce968927b4c345e06ffe0fcf4cc
Phil Tillet <phil@openai.com>
2023-04-24 13:07:48 -0700

    .

--- python/test/unit/runtime/test_driver.py
+++ python/test/unit/runtime/test_driver.py
@@ -5,4 +5,4 @@ import triton
 
 def test_is_lazy():
     mod = sys.modules[triton.runtime.driver.__module__]
-    assert isinstance(triton.runtime.driver, getattr(mod, "LazyProxy"))
+    assert not isinstance(triton.runtime.driver, getattr(mod, "DriverBase"))
