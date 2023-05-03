a5013148550cd0fd220b98119f558e7a33caabba
pommedeterresautee <pommedeterresautee@msn.com>
2023-05-03 09:08:52 +0200

    fix: simplify mechanism to make torch optional

--- python/triton/debugger/torch_wrapper.py
+++ python/triton/debugger/torch_wrapper.py
@@ -3,15 +3,16 @@ try:
 except ImportError:
     _torch = None
 
-import sys
-from types import ModuleType
 
+class TorchWrapper:
+    """
+    Helps in making torch an optional dependency
+    """
 
-class _Wrapper(ModuleType):
     def __getattr__(self, name):
         if _torch is None:
             raise ImportError("Triton requires PyTorch to be installed")
         return getattr(_torch, name)
 
 
-sys.modules[__name__] = _Wrapper(__name__)
+torch = TorchWrapper()
