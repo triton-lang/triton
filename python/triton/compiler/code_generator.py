f29838a3ea4954576bd6c77ebe2f4784ae7f5431
Keren Zhou <kerenzhou@openai.com>
2023-05-26 12:44:03 -0400

    [FRONTEND] Emit return error message in the frontend (#1708)

--- python/triton/compiler/code_generator.py
+++ python/triton/compiler/code_generator.py
@@ -560,7 +560,11 @@ class CodeGenerator(ast.NodeVisitor):
         cond = self.visit(node.test)
         if _is_triton_tensor(cond):
             cond = cond.to(language.int1, _builder=self.builder)
-            if self.scf_stack or not ContainsReturnChecker(self.gscope).visit(node):
+            contains_return = ContainsReturnChecker(self.gscope).visit(node)
+            if self.scf_stack and contains_return:
+                raise UnsupportedLanguageConstruct(None, node,
+                                                   "Cannot have `return` statements inside `while` or `for` statements in triton")
+            elif self.scf_stack or not contains_return:
                 self.visit_if_scf(cond, node)
             else:
                 self.visit_if_top_level(cond, node)
