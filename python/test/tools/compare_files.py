061f78f319fe0f9c9d95a07e94e3da531d4f5fe5
Zahi Moudallal <zahi@openai.com>
2023-05-26 11:23:49 -0700

    Some more changes to the comparison

--- python/test/tools/compare_files.py
+++ python/test/tools/compare_files.py
@@ -21,30 +21,77 @@ def compare_files(file1, file2):
         content1 = f1.read()
         content2 = f2.read()
 
-    assert content1 == content2, f"Files {file1} and {file2} are not identical."
+    return content1 == content2
+
+
+def get_json_file(path):
+    # get json file in directory
+    json_files = list_files_with_extension(path, "json")
+    if len(json_files) == 0:
+        return None
+    # filter out json files with grp in their name
+    json_files = [f for f in json_files if "grp" not in f]
+    assert len(json_files) == 1, f"Found {len(json_files)} files in {path} with extension json!"
+    json_file = os.path.join(path, json_files[0])
+    return json_file
+
+
+def get_name_to_hashes_dict(path):
+    name_to_hashes = {}
+    for hash in os.listdir(path):
+        full_path = os.path.join(path, hash)
+        assert os.path.isdir(full_path), f"Path {full_path} is not a directory!"
+        json_file = get_json_file(full_path)
+        if json_file is None:
+            continue
+        # load json file
+        with open(json_file, 'r') as file:
+            content = yaml.safe_load(file)
+            # get name
+            name = content["name"]
+            name_to_hashes.setdefault(name, []).append(hash)
+    return name_to_hashes
+
+
+def compare_matching_files(name, extension, name_to_hashes1, name_to_hashes2, args):
+    hashes1 = name_to_hashes1[name]
+    hashes2 = name_to_hashes2[name]
+    for hash1 in hashes1:
+        json1 = get_json_file(os.path.join(args.path1, hash1))
+        assert json1 is not None
+        for hash2 in hashes2:
+            json2 = get_json_file(os.path.join(args.path2, hash2))
+            assert json2 is not None
+            if not compare_files(json1, json2):
+                continue
+            ext_file1 = list_files_with_extension(os.path.join(args.path1, hash1), extension)[0]
+            ext_file2 = list_files_with_extension(os.path.join(args.path2, hash2), extension)[0]
+            if not compare_files(ext_file1, ext_file2):
+                return (ext_file1, ext_file2)
+    return ()
 
 
 def main(args):
-    yaml_file_path = args.hash
-    assert os.path.exists(yaml_file_path), f"Path {yaml_file_path} does not exist!"
-    hashes_and_extensions = load_yaml_file(yaml_file_path)["hashes_and_extensions"]
-
     assert args.path1 != args.path2, "Cannot compare files in the same directory!"
+    name_to_hashes1 = get_name_to_hashes_dict(args.path1)
+    name_to_hashes2 = get_name_to_hashes_dict(args.path2)
 
-    for d in hashes_and_extensions:
-        hash = d["hash"]
+    yaml_file_path = args.kernels
+    assert os.path.exists(yaml_file_path), f"Path {yaml_file_path} does not exist!"
+    name_and_extension = load_yaml_file(yaml_file_path)["name_and_extension"]
+
+    mismatches = {}
+    for d in name_and_extension:
+        name = d["name"]
         extension = d["extension"]
-        full_path1 = os.path.join(args.path1, hash)
-        full_path2 = os.path.join(args.path2, hash)
-        assert os.path.exists(full_path1), f"Path {full_path1} does not exist!"
-        assert os.path.exists(full_path2), f"Path {full_path2} does not exist!"
-        files1 = list_files_with_extension(full_path1, extension)
-        files2 = list_files_with_extension(full_path2, extension)
-        assert len(files1) == 1, f"Found {len(files1)} files in {full_path1} with extension {extension}!"
-        assert len(files2) == 1, f"Found {len(files2)} files in {full_path2} with extension {extension}!"
-        file1 = os.path.join(full_path1, files1[0])
-        file2 = os.path.join(full_path2, files2[0])
-        compare_files(file1, file2)
+        result = compare_matching_files(name, extension, name_to_hashes1, name_to_hashes2, args)
+        if len(result) > 0:
+            mismatches[name] = result
+    if len(mismatches) > 0:
+        print(f"Found {len(mismatches)} mismatches:")
+        for name, mismatch in mismatches.items():
+            print(f"{name}: {mismatch}")
+        assert False, "Found mismatches!"
 
 
 if __name__ == "__main__":
@@ -64,11 +111,11 @@ if __name__ == "__main__":
         help=("Path to second cache directory"),
     )
     parser.add_argument(
-        "--hash",
+        "--kernels",
         type=str,
         default=None,
         required=True,
-        help=("Path to hash file"),
+        help=("Path to kernels yaml file"),
     )
     args = parser.parse_args()
     main(args)
