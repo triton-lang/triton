d28429bb844b92aac6dc48fff3bb757bc6135b85
Keren Zhou <kerenzhou@openai.com>
2023-06-01 12:07:43 -0700

    [BACKEND] Pipeline pass rewrite part 1: functionality fixes (#1716)
    Support the following three cases:
    1. Operands of `load` depend on induction variables before `load`s.
    2. Mixed use of induction variables and offset to update the `ptr`.
    3. Cross iteration (>1) dependency values.

--- lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMPass.cpp
+++ lib/Conversion/TritonGPUToLLVM/TritonGPUToLLVMPass.cpp
@@ -307,8 +307,7 @@ public:
     // Preprocess
     decomposeMmaToDotOperand(mod, numWarps);
     decomposeBlockedToDotOperand(mod);
-    if (failed(decomposeInsertSliceAsyncOp(mod)))
-      return signalPassFailure();
+    decomposeInsertSliceAsyncOp(mod);
 
     // Allocate shared memory and set barrier
     ModuleAllocation allocation(mod);
@@ -487,7 +486,7 @@ private:
     });
   }
 
-  LogicalResult decomposeInsertSliceAsyncOp(ModuleOp mod) const {
+  void decomposeInsertSliceAsyncOp(ModuleOp mod) const {
     ModuleAxisInfoAnalysis axisInfoAnalysis(mod);
     // TODO(Keren): This is a hacky knob that may cause performance regression
     // when decomposition has been performed. We should remove this knob once we
@@ -515,6 +514,7 @@ private:
       // Get the vectorized load size
       auto src = insertSliceAsyncOp.getSrc();
       auto dst = insertSliceAsyncOp.getDst();
+      auto mask = insertSliceAsyncOp.getMask();
       auto srcTy = src.getType().cast<RankedTensorType>();
       auto dstTy = dst.getType().cast<RankedTensorType>();
       auto srcBlocked =
@@ -523,6 +523,9 @@ private:
           dstTy.getEncoding().dyn_cast<triton::gpu::SharedEncodingAttr>();
       auto resElemTy = dstTy.getElementType();
       unsigned inVec = axisInfoAnalysis.getPtrContiguity(src);
+      if (mask)
+        inVec =
+            std::min<unsigned>(axisInfoAnalysis.getMaskAlignment(mask), inVec);
       unsigned outVec = resSharedLayout.getVec();
       unsigned minVec = std::min(outVec, inVec);
       auto maxBitWidth =
@@ -586,7 +589,6 @@ private:
         asyncWaitOp.erase();
       }
     });
-    return success();
   }
 };
 
