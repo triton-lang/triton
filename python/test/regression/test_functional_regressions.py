9bc8b0b777c8304c89d2f55aa0fd69f724cc4fdc
Oleg Shyshkov <shyshkov@google.com>
2023-06-06 17:41:26 +0200

    [TESTS] Add a test for mixed-precision matmuls.
    python/tutorials/03-matrix-multiplication.py was used as a base for matmul kernel.
    
    Co-authored-by: Goran Flegar <gflegar@google.com>

--- python/test/regression/test_functional_regressions.py
+++ python/test/regression/test_functional_regressions.py
@@ -6,6 +6,178 @@ from numpy.random import RandomState
 import triton
 import triton.language as tl
 
+# Tests matmul with different argument types and memory layouts.
+@pytest.mark.parametrize("lhs_row_major", [False, True])
+@pytest.mark.parametrize("rhs_row_major", [False, True])
+@pytest.mark.parametrize(("lhs_dtype", "rhs_dtype"),
+    [("float16", "float16"), ("float16", "float8"),
+     ("float16", "int8"), ("float8", "float16"),
+     ("float8", "float8"), ("int8", "float16"),
+     ("int8", "int8")])
+def test_matmul(lhs_dtype, lhs_row_major, rhs_dtype, rhs_row_major):
+    if (lhs_row_major and not rhs_row_major and lhs_dtype == rhs_dtype and
+          (lhs_dtype == "float8" or lhs_dtype == "int8")):
+        pytest.skip("Fails because of a known issue: "
+            "https://github.com/openai/triton/issues/1397#issuecomment-1564409407")
+
+    def matmul_reference(a, b):
+        return torch.matmul(a, b)
+
+    def f8_to_f16(x):
+        assert x.is_contiguous(), "Kernel only works for contiguous tensors"
+        @triton.jit
+        def kernel(Y, X, N, BLOCK_SIZE: tl.constexpr):
+            pid = tl.program_id(0)
+            offs = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+            mask = offs < N
+            x = tl.load(X + offs, mask=mask)
+            y = x.to(tl.float16)
+            tl.store(Y + offs, y, mask=mask)
+        ret = torch.empty(x.shape, dtype=torch.float16, device=x.device)
+        grid = lambda META: (triton.cdiv(x.numel(), META['BLOCK_SIZE']),)
+        kernel[grid](ret, triton.reinterpret(x, tl.float8e5), ret.numel(), BLOCK_SIZE=1024)
+        return ret
+
+    def f16_to_f8(x):
+        assert x.is_contiguous(), "Kernel only works for contiguous tensors"
+        @triton.jit
+        def kernel(Y, X, N, BLOCK_SIZE: tl.constexpr):
+            pid = tl.program_id(0)
+            offs = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+            mask = offs < N
+            x = tl.load(X + offs, mask=mask)
+            y = x.to(tl.float8e5)
+            tl.store(Y + offs, y, mask=mask)
+        ret = torch.empty(x.shape, dtype=torch.int8, device=x.device)
+        grid = lambda META: (triton.cdiv(x.numel(), META['BLOCK_SIZE']),)
+        kernel[grid](triton.reinterpret(ret, tl.float8e5), x, x.numel(), BLOCK_SIZE=1024)
+        return ret
+
+    @triton.jit
+    def matmul_kernel(
+        # Pointers to matrices
+        a_ptr, b_ptr, c_ptr,
+        # Matrix dimensions
+        M, N, K,
+        # The stride variables represent how much to increase the ptr by when moving by 1
+        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+        # by to get the element one row down (A has M rows).
+        stride_am, stride_ak,
+        stride_bk, stride_bn,
+        stride_cm, stride_cn,
+        # Meta-parameters
+        BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
+        GROUP_SIZE_M: tl.constexpr,
+        IS_LHS_FP8: tl.constexpr,
+        IS_RHS_FP8: tl.constexpr,
+    ):
+        """Kernel for computing the matmul C = A x B.
+        A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+        """
+        # -----------------------------------------------------------
+        # Map program ids `pid` to the block of C it should compute.
+        # This is done in a grouped ordering to promote L2 data reuse.
+        # See above `L2 Cache Optimizations` section for details.
+        pid = tl.program_id(axis=0)
+        num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+        num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+        num_pid_in_group = GROUP_SIZE_M * num_pid_n
+        group_id = pid // num_pid_in_group
+        first_pid_m = group_id * GROUP_SIZE_M
+        group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+        pid_m = first_pid_m + (pid % group_size_m)
+        pid_n = (pid % num_pid_in_group) // group_size_m
+
+        # ----------------------------------------------------------
+        # Create pointers for the first blocks of A and B.
+        # We will advance this pointer as we move in the K direction
+        # and accumulate
+        # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
+        # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
+        # See above `Pointer Arithmetics` section for details
+        offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+        offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+        offs_k = tl.arange(0, BLOCK_SIZE_K)
+        a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+        b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+        # -----------------------------------------------------------
+        # Iterate to compute a block of the C matrix.
+        # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+        # of fp32 values for higher accuracy.
+        # `accumulator` will be converted back to fp16 after the loop.
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+        for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+            # Load the next block of A and B, generate a mask by checking the K dimension.
+            # If it is out of bounds, set it to 0.
+            a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+            if IS_LHS_FP8:
+              a = a.to(tl.float8e5, bitcast=True)
+            a = a.to(tl.float16)
+            b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+            if IS_RHS_FP8:
+              b = b.to(tl.float8e5, bitcast=True)
+            b = b.to(tl.float16)
+            # We accumulate along the K dimension.
+            accumulator += tl.dot(a, b)
+            # Advance the ptrs to the next K block.
+            a_ptrs += BLOCK_SIZE_K * stride_ak
+            b_ptrs += BLOCK_SIZE_K * stride_bk
+        c = accumulator.to(tl.float16)
+
+        # -----------------------------------------------------------
+        # Write back the block of the output matrix C with masks.
+        offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+        offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+        c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+        c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+        tl.store(c_ptrs, c, mask=c_mask)
+
+    m, n, k = 32, 64, 128
+    block_m, block_n, block_k = 16, 32, 64
+    group_size_m = 8
+
+    def get_input(n, m, dtype, row_major):
+      if not row_major:
+        return get_input(m, n, dtype, row_major=True).T
+
+      res = torch.randn((n, m), device='cuda', dtype=torch.float16)
+      if dtype == "int8":
+        res = res.to(torch.int8)
+      elif dtype == "float8":
+        res = f16_to_f8(res)
+      return res
+
+    torch.manual_seed(0)
+    a = get_input(m, k, lhs_dtype, lhs_row_major)
+    b = get_input(k, n, rhs_dtype, rhs_row_major)
+
+    grid = (triton.cdiv(m, block_m) * triton.cdiv(n, block_n),)
+    triton_output = torch.empty((m, n), device=a.device, dtype=torch.float16)
+    matmul_kernel[grid](
+        a, b, triton_output,
+        m, n, k,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        triton_output.stride(0), triton_output.stride(1),
+        block_m, block_n, block_k,
+        group_size_m,
+        IS_LHS_FP8=(lhs_dtype == "float8"),
+        IS_RHS_FP8=(rhs_dtype == "float8")
+    )
+
+    def maybe_upcast(x, dtype):
+      if not x.is_contiguous():
+          return maybe_upcast(x.T, dtype).T
+
+      if dtype == "float8":
+          return f8_to_f16(x)
+      else:
+          return x.to(torch.float16)
+
+    torch_output = torch.matmul(maybe_upcast(a, lhs_dtype), maybe_upcast(b, rhs_dtype))
+    assert torch.allclose(triton_output, torch_output, atol=1e-2, rtol=1e-2)
+
 
 def test_chained_matmul():
     # Regression test for issue #1601
