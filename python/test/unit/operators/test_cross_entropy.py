0d01ce298f1db67265e76e3bd2f3a1e4809c8be8
Thomas Raoux <thomas.raoux@openai.com>
2023-07-06 08:42:12 -0700

    skip hopper

--- python/test/unit/operators/test_cross_entropy.py
+++ python/test/unit/operators/test_cross_entropy.py
@@ -1,8 +1,8 @@
 import pytest
 import torch
 
-# import triton
-# import triton.ops
+import triton
+import triton.ops
 
 
 @pytest.mark.parametrize("M, N, dtype, mode",
@@ -10,27 +10,29 @@ import torch
                              (M, N, dtype, mode) for M in [1024]
                              for N in [512]
                              for dtype in ['float16']
-                             for mode in ['backward']
+                             for mode in ['backward', 'forward']
                          ]
                          )
 def test_op(M, N, dtype, mode):
     capability = torch.cuda.get_device_capability()
     if capability[0] < 8 and dtype == "bfloat16":
         pytest.skip("Only test bfloat16 on devices with sm >= 80")
+    if capability[0] > 8 and mode == "backward":
+        pytest.skip("Skip hopper")
     dtype = {'bfloat16': torch.bfloat16, 'float16': torch.float16, 'float32': torch.float32}[dtype]
     # create inputs
     x = torch.randn(M, N, dtype=dtype, device='cuda', requires_grad=True)
     idx = 4 + torch.ones(M, dtype=torch.int64, device='cuda')
     # forward pass
-    # tt_y = triton.ops.cross_entropy(x, idx)
+    tt_y = triton.ops.cross_entropy(x, idx)
     th_y = torch.nn.CrossEntropyLoss(reduction="none")(x, idx)
     if mode == 'forward':
-        torch.testing.assert_allclose(th_y, th_y)
+        torch.testing.assert_allclose(th_y, tt_y)
     # backward pass
     elif mode == 'backward':
-        dy = torch.randn_like(th_y)
+        dy = torch.randn_like(tt_y)
         # triton backward
-        th_y.backward(dy)
+        tt_y.backward(dy)
         tt_dx = x.grad.clone()
         # torch backward
         x.grad.zero_()
