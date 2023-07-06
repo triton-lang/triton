d82835a28c1eb090e246839d61c8fa6f3d7de938
Izzy Putterman <iputterman@nvidia.com>
2023-07-05 17:00:57 -0700

    Credit to HazyResearch

--- python/triton/ops/flash_attention.py
+++ python/triton/ops/flash_attention.py
@@ -3,6 +3,9 @@ Fused Attention
 ===============
 This is a Triton implementation of the Flash Attention algorithm
 (see: Dao et al., https://arxiv.org/pdf/2205.14135v2.pdf; Rabe and Staats https://arxiv.org/pdf/2112.05682v2.pdf)
+
+Sequence Parallel implementation inspired by HazyResearch
+(see https://github.com/HazyResearch/flash-attention/blob/main/flash_attn/flash_attn_triton.py)
 """
 
 import torch
