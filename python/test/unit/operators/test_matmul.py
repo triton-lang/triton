f4e536272ad7f7d14a9ba9fe8123605d3fc71852
Phil Tillet <phil@openai.com>
2023-07-09 21:26:32 -0700

    .

--- python/test/unit/operators/test_matmul.py
+++ python/test/unit/operators/test_matmul.py
@@ -8,7 +8,7 @@ import triton.language as tl
 import triton.ops
 
 
-def f8_to_f16(x):
+def f8_to_f16(x, dtype):
 
     @triton.jit
     def kernel(Y, X, N, BLOCK_SIZE: tl.constexpr):
@@ -20,7 +20,7 @@ def f8_to_f16(x):
 
     ret = torch.empty(x.shape, dtype=torch.float16, device=x.device)
     grid = lambda META: (triton.cdiv(x.numel(), META['BLOCK_SIZE']),)
-    dtype = getattr(tl, 'float8e5')
+    dtype = getattr(tl, dtype)
     kernel[grid](ret, triton.reinterpret(x, dtype), ret.numel(), BLOCK_SIZE=1024)
     return ret
 
@@ -89,9 +89,9 @@ def f8_to_f16(x):
                 (128, 256, 32, 1, 8, 2, None, None, None, AT, BT, ADTYPE, BDTYPE),
                 (32, 64, 32, 1, 1, 2, 64, 128, 32, AT, BT, ADTYPE, BDTYPE),
                 (128, 128, 32, 8, 4, 2, 256, 256, 128, AT, BT, ADTYPE, BDTYPE),
-            ] for ADTYPE, BDTYPE in [("float8e4b15", "float8e5"),
+            ] for ADTYPE, BDTYPE in [("float8e5", "float8e5"),
                                      ("float8e5", "float16"),
-                                     ("float16", "float8e4b15"),
+                                     ("float16", "float8e5"),
                                      ("float16", "float32"),
                                      ("float32", "float16"),
                                      ("bfloat16", "float32"),
@@ -123,9 +123,9 @@ def test_op(BLOCK_M, BLOCK_N, BLOCK_K, SPLIT_K, NWARP, NSTAGE, M, N, K, AT, BT, 
     a_fp8 = "float8" in ADTYPE
     b_fp8 = "float8" in BDTYPE
 
-    def maybe_upcast(x, is_float8):
+    def maybe_upcast(x, dtype, is_float8):
         if is_float8:
-            return f8_to_f16(x)
+            return f8_to_f16(x, dtype)
         return x
 
     def init_input(n, m, t, dtype, is_float8):
@@ -140,10 +140,10 @@ def test_op(BLOCK_M, BLOCK_N, BLOCK_K, SPLIT_K, NWARP, NSTAGE, M, N, K, AT, BT, 
     a = init_input(M, K, AT, ADTYPE, a_fp8)
     b = init_input(K, N, BT, BDTYPE, b_fp8)
     # run test
-    th_a = maybe_upcast(a, a_fp8).to(torch.float32)
+    th_a = maybe_upcast(a, ADTYPE, a_fp8).to(torch.float32)
     if AT and a_fp8:
         th_a = th_a.view(th_a.shape[::-1]).T
-    th_b = maybe_upcast(b, b_fp8).to(torch.float32)
+    th_b = maybe_upcast(b, BDTYPE, b_fp8).to(torch.float32)
     if BT and b_fp8:
         th_b = th_b.view(th_b.shape[::-1]).T
     th_c = torch.matmul(th_a, th_b)
