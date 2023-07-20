9c422e260b3fe68a5f45c938593a64b88771a254
David Berard <dberard@fb.com>
2023-07-19 17:40:46 -0700

    [OPTIMIZER] AxisInfoVisitor for LoadOp constancy calculation (#1968)
    If you call `result = load(x, mask)` where `x` and `mask` have some
    constancy properties, then you can infer some constancy properties for
    `result`.

--- lib/Analysis/AxisInfo.cpp
+++ lib/Analysis/AxisInfo.cpp
@@ -469,6 +469,36 @@ public:
   }
 };
 
+class LoadOpAxisInfoVisitor final : public AxisInfoVisitorImpl<triton::LoadOp> {
+public:
+  using AxisInfoVisitorImpl<triton::LoadOp>::AxisInfoVisitorImpl;
+
+  AxisInfo
+  getAxisInfo(triton::LoadOp op,
+              ArrayRef<const dataflow::Lattice<AxisInfo> *> operands) override {
+    // If pointers and mask both have constancy properties, those properties
+    // will also extend to output.
+    AxisInfo ptrInfo = operands[0]->getValue();
+    std::optional<AxisInfo> maskInfo;
+    if (operands.size() > 1) {
+      maskInfo = operands[1]->getValue();
+    }
+    AxisInfo::DimVectorT contiguity;
+    AxisInfo::DimVectorT divisibility;
+    AxisInfo::DimVectorT constancy;
+
+    for (int d = 0; d < ptrInfo.getRank(); ++d) {
+      contiguity.push_back(1);
+      divisibility.push_back(1);
+      constancy.push_back(
+          gcd(ptrInfo.getConstancy(d),
+              maskInfo.has_value() ? maskInfo->getConstancy(d) : 0));
+    }
+
+    return AxisInfo(contiguity, divisibility, constancy);
+  }
+};
+
 class ExpandDimsOpAxisInfoVisitor final
     : public AxisInfoVisitorImpl<triton::ExpandDimsOp> {
 public:
@@ -871,6 +901,7 @@ AxisInfoAnalysis::AxisInfoAnalysis(DataFlowSolver &solver)
                   MaxMinOpAxisInfoVisitor<arith::MaxUIOp>,
                   MaxMinOpAxisInfoVisitor<arith::MinSIOp>,
                   MaxMinOpAxisInfoVisitor<arith::MinUIOp>>();
+  visitors.append<LoadOpAxisInfoVisitor>();
 }
 
 void AxisInfoAnalysis::visitOperation(
