f7d1505b2012cfd0a77b6bf4c1c4bb786bef8985
mikex86 <michael.keiblinger@gmail.com>
2023-07-28 19:38:44 +0200

    Fix memory leak caused by target->createTargetMachine

--- lib/Target/PTX/PTXTranslation.cpp
+++ lib/Target/PTX/PTXTranslation.cpp
@@ -13,6 +13,7 @@
 
 #include <mutex>
 #include <optional>
+#include <memory>
 
 namespace triton {
 
@@ -77,9 +78,12 @@ std::string translateLLVMIRToPTX(llvm::Module &module, int cc, int version) {
   opt.UnsafeFPMath = false;
   opt.NoInfsFPMath = false;
   opt.NoNaNsFPMath = true;
-  llvm::TargetMachine *machine = target->createTargetMachine(
+  std::unique_ptr<llvm::TargetMachine> machine{
+    target->createTargetMachine(
       module.getTargetTriple(), proc, features, opt, llvm::Reloc::PIC_,
-      std::nullopt, llvm::CodeGenOpt::Aggressive);
+      std::nullopt, llvm::CodeGenOpt::Aggressive
+    )
+  };
   // set data layout
   if (layout.empty())
     module.setDataLayout(machine->createDataLayout());
