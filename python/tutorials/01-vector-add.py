458b45dd6eeff517bcc32d41c1ce4e4f70f656cb
youkaichao <youkaichao@126.com>
2023-07-28 20:44:43 +0800

    add n vectors to show the benefit of kernel fusion and the name origin of triton

--- python/tutorials/01-vector-add.py
+++ python/tutorials/01-vector-add.py
@@ -12,6 +12,8 @@ In doing so, you will learn about:
 
 * The best practices for validating and benchmarking your custom ops against native reference implementations.
 
+* How fusing kernels using `triton` can greatly surpass the corresponding pytorch code.
+
 """
 
 # %%
@@ -137,3 +139,138 @@ def benchmark(size, provider):
 # We can now run the decorated function above. Pass `print_data=True` to see the performance number, `show_plots=True` to plot them, and/or
 # `save_path='/path/to/results/' to save them to disk along with raw CSV data:
 benchmark.run(print_data=True, show_plots=True)
+
+
+# %%
+# We can also vary the number of arguments to see the power of kernel fusion.
+# We will create a fused kernel function to replace the pytorch style code:
+# `tensor1 + tensor2 + ... + tensorn`.
+# In fact, `triton` gets its name because triton (the hydrogen atom with one
+# proton and two neutrons) is often used for nuclear fusion. We use triton
+# for nuclear fusion, and use `triton` for kernel fusion!
+# The `triton` function does not support variable number of tensors as input,
+# and we have to manually construct the code.
+#
+# Remember to change the `n_input_tensors` variable and re-run the following
+# code to see the comparison!
+template = '''
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def addn_kernel(
+{signature}
+    output_ptr,  # *Pointer* to output vector.
+    n_elements,  # Size of the vector.
+    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+                 # NOTE: `constexpr` so it can be used as a shape value.
+):
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load data from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+{loads}
+    output = {output}
+    # Write output back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+def addn({signature2}):
+    # We need to preallocate the output.
+    output = torch.empty_like(data1)
+    assert {asserts} and output.is_cuda
+    n_elements = output.numel()
+    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
+    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
+    # In this case, we use a 1D grid where the size is the number of blocks:
+    grid = lambda meta: (triton.cdiv(meta['n_elements'], meta['BLOCK_SIZE']),)
+    # NOTE:
+    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+    #  - Don't forget to pass meta-parameters as keywords arguments.
+    addn_kernel[grid]({args}, output, n_elements, BLOCK_SIZE=1024)
+    # We return a handle to output but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
+    # running asynchronously at this point.
+    return output
+'''
+
+def generate_n(n=2):
+    assert n >= 2
+    signature = '\n'.join([f'    ptr{i},  # *Pointer* to {i}-th input vector.' for i in range(1, n + 1)])
+    loads = '\n'.join([f'    data{i} = tl.load(ptr{i} + offsets, mask=mask)' for i in range(1, n + 1)])
+    output = ' + '.join([f'data{i}' for i in range(1, n + 1)])
+    signature2 = ', '.join([f'data{i}: torch.Tensor' for i in range(1, n + 1)])
+    asserts = ' and '.join([f'data{i}.is_cuda' for i in range(1, n + 1)])
+    args = ', '.join([f'data{i}' for i in range(1, n + 1)])
+    return template.format(signature=signature, loads=loads, output=output, signature2=signature2, asserts=asserts, args=args)
+
+n_input_tensors = 3
+func_string = generate_n(n_input_tensors)
+with open('addn.py', 'w') as f:
+    f.write(func_string)
+import addn
+import importlib
+importlib.reload(addn)
+from addn import *
+
+
+# %%
+# Sanity check for the function.
+size = 98432
+tensors = [torch.rand(size, device='cuda') for i in range(n_input_tensors)]
+output_torch = sum(tensors[1:], tensors[0])
+output_triton = addn(*tensors)
+print(output_torch)
+print(output_triton)
+print(
+    f'The maximum difference between torch and triton is '
+    f'{torch.max(torch.abs(output_torch - output_triton))}'
+)
+
+
+# %%
+# Benchmarking the performance to see the benefit of `triton` (kernel fusion)!
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['size'],  # Argument names to use as an x-axis for the plot.
+        x_vals=[
+            2 ** i for i in range(12, 28, 1)
+        ],  # Different possible values for `x_name`.
+        x_log=True,  # x axis is logarithmic.
+        line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
+        line_vals=['triton', 'torch'],  # Possible values for `line_arg`.
+        line_names=['Triton', 'Torch'],  # Label name for the lines.
+        styles=[('blue', '-'), ('green', '-')],  # Line styles.
+        ylabel='GB/s',  # Label name for the y-axis.
+        plot_name=f'performance of adding {n_input_tensors} vectors',  # Name for the plot. Used also as a file name for saving the plot.
+        args={},  # Values for function arguments not in `x_names` and `y_name`.
+    )
+)
+def benchmark(size, provider):
+    tensors = [torch.rand(size, device='cuda') for i in range(n_input_tensors)]
+    quantiles = [0.5, 0.2, 0.8]
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: sum(tensors[1:], tensors[0]), quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: addn(*tensors), quantiles=quantiles)
+    gbps = lambda ms: 4 * (n_input_tensors + 1) * size / ms * 1e-6
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+import os
+output_dir = f'./output{n_input_tensors}'
+os.makedirs(output_dir, exist_ok=True)
+benchmark.run(save_path=output_dir)
+
+
+# %%
+# Observation:
+# By varying the `n_input_tensors` argument, we can see that the computation speed of triton keeps almost untouched, while the naive pytorch expression `tensor1 + tensor2 + tensor3 + ...` becomes more and more slowlier, because it has to write all the intermediate variables into and out of GPU memory.
