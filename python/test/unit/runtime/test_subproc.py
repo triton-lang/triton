32d7c6d64657a39ce68d879c474d620d27027d1f
jayfurmanek <Jason.Furmanek@amd.com>
2023-08-07 10:30:36 -0500

    Fix runtime/test_subproc.py for hip devices (#284)
    * Fix runtime/test_subproc.py for hip devices
    
    * address review comments

--- python/test/unit/runtime/test_subproc.py
+++ python/test/unit/runtime/test_subproc.py
@@ -20,7 +20,22 @@ def reset_tmp_dir():
 instance_descriptor = namedtuple("instance_descriptor", ["divisible_by_16", "equal_to_1"])
 
 
-def compile_fn(config, cc):
+def get_device_type():
+    try:
+        import torch
+    except ImportError:
+        raise ImportError("Triton requires PyTorch to be installed")
+    if torch.version.hip is None:
+        major, minor = torch.cuda.get_device_capability(0)
+        cc = major * 10 + minor
+        device_type="cuda"
+    else:
+        cc = None
+        device_type="hip"
+    return cc, device_type
+
+
+def compile_fn(config, device_type, cc):
     @triton.jit
     def kernel_sub(a, b, o, N: tl.constexpr):
         idx = tl.arange(0, N)
@@ -29,6 +44,7 @@ def compile_fn(config, cc):
         fn=kernel_sub,
         signature={0: "*fp32", 1: "*fp32", 2: "*fp32"},
         device=0,
+        device_type=device_type,
         constants={3: 32},
         configs=[config],
         warm_cache_only=True,
@@ -37,20 +53,19 @@ def compile_fn(config, cc):
 
 
 def test_compile_in_subproc() -> None:
-    major, minor = torch.cuda.get_device_capability(0)
-    cc = major * 10 + minor
+    cc, device_type = get_device_type()
     config = instance_descriptor(tuple(range(4)), ())
 
     multiprocessing.set_start_method('fork')
     proc = multiprocessing.Process(
         target=compile_fn,
-        args=(config, cc))
+        args=(config, device_type, cc))
     proc.start()
     proc.join()
     assert proc.exitcode == 0
 
 
-def compile_fn_dot(config, cc):
+def compile_fn_dot(config, device_type, cc):
     @triton.jit
     def kernel_dot(Z):
         offs = tl.arange(0, 16)[:, None] * 16 + tl.arange(0, 16)[None, :]
@@ -62,6 +77,7 @@ def compile_fn_dot(config, cc):
         fn=kernel_dot,
         signature={0: "*fp32"},
         device=0,
+        device_type=device_type,
         configs=[config],
         warm_cache_only=True,
         cc=cc,
@@ -70,14 +86,13 @@ def compile_fn_dot(config, cc):
 
 def test_compile_in_forked_subproc() -> None:
     reset_tmp_dir()
-    major, minor = torch.cuda.get_device_capability(0)
-    cc = major * 10 + minor
+    cc, device_type = get_device_type()
     config = instance_descriptor(tuple(range(1)), ())
 
     assert multiprocessing.get_start_method() == 'fork'
     proc = multiprocessing.Process(
         target=compile_fn_dot,
-        args=(config, cc))
+        args=(config, device_type, cc))
     proc.start()
     proc.join()
     assert proc.exitcode == 0
