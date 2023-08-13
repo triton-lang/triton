dfe7f5b0d8d9a2e7b628b7a10ebc7bd054464971
Phil Tillet <phil@openai.com>
2023-08-13 12:20:21 -0700

    .

--- python/test/unit/operators/test_matmul.py
+++ python/test/unit/operators/test_matmul.py
@@ -61,7 +61,7 @@ def f8_to_f16(x, dtype):
                 # variable input
                 (128, 128, 32, 1, 4, 2, 256, 384, 160, AT, BT, DTYPE, DTYPE),
                 (128, 128, 32, 1, 4, 2, 107, 233, 256, AT, BT, DTYPE, DTYPE),
-                (128, 128, 32, 1, 4, 2, 107, 233, 311, AT, BT, DTYPE, DTYPE),
+                (128, 128, 32, 1, 4, 2, 107, 233, 83, AT, BT, DTYPE, DTYPE),
                 (128, 256, 64, 1, 8, 3, 256, 512, 160, AT, BT, DTYPE, DTYPE),
             ] for DTYPE in ["float16", "bfloat16", "float32"] for AT in [False, True] for BT in [False, True]
         ],
