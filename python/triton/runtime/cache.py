3ff708ca16c05303590f80ff18098449886cd82e
Jon Chuang <9093549+jon-chuang@users.noreply.github.com>
2023-08-28 17:57:26 +0800

    fix cache dir

--- python/triton/runtime/cache.py
+++ python/triton/runtime/cache.py
@@ -41,7 +41,7 @@ class FileCacheManager(CacheManager):
         self.lock_path = None
         # create cache directory if it doesn't exist
         self.cache_dir = os.environ.get('TRITON_CACHE_DIR', default_cache_dir())
-        if self.cache_dir:
+        if not self.cache_dir:
             self.cache_dir = os.path.join(self.cache_dir, self.key)
             self.lock_path = os.path.join(self.cache_dir, "lock")
             os.makedirs(self.cache_dir, exist_ok=True)
