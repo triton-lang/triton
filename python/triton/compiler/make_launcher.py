triton-lang/triton

commit a9b8c8c37d134d2cb196b36141d4ab2c1b1961f2

[FRONTEND] drop GIL for launch, and set value=false upon pointer error (#2185)

python/triton/compiler/make_launcher.py
@@ -183,6 +183,7 @@ def generate_launcher(constants, signature, ids):
       }}
 
       PyErr_SetString(PyExc_TypeError, "Pointer argument must be either uint64 or have data_ptr method");
+      ptr_info.valid = false;
       return ptr_info;
     }}
 
@@ -208,11 +209,10 @@ def generate_launcher(constants, signature, ids):
 
       // raise exception asap
       {"; ".join([f"DevicePtrInfo ptr_info{i} = getPointer(_arg{i}, {i}); if (!ptr_info{i}.valid) return NULL;" if ty[0] == "*" else "" for i, ty in signature.items()])};
+      Py_BEGIN_ALLOW_THREADS;
       _launch(gridX, gridY, gridZ, num_warps, shared_memory, (hipStream_t)_stream, (hipFunction_t)_function{', ' + ', '.join(f"ptr_info{i}.dev_ptr" if ty[0]=="*" else f"_arg{i}" for i, ty in signature.items()) if len(signature) > 0 else ''});
+      Py_END_ALLOW_THREADS;
 
-      if (PyErr_Occurred()) {{
-        return NULL;
-      }}
       if (launch_exit_hook != Py_None && !PyObject_CallObject(launch_exit_hook, args)) {{
         return NULL;
       }}
@@ -366,6 +366,7 @@ static inline DevicePtrInfo getPointer(PyObject *obj, int idx) {{
     return ptr_info;
   }}
   PyErr_SetString(PyExc_TypeError, "Pointer argument must be either uint64 or have data_ptr method");
+  ptr_info.valid = false;
   return ptr_info;
 }}
 
@@ -394,11 +395,10 @@ static PyObject* launch(PyObject* self, PyObject* args) {{
 
   // raise exception asap
   {"; ".join([f"DevicePtrInfo ptr_info{i} = getPointer(_arg{i}, {i}); if (!ptr_info{i}.valid) return NULL;" if ty[0] == "*" else "" for i, ty in signature.items()])};
+  Py_BEGIN_ALLOW_THREADS;
   _launch(gridX, gridY, gridZ, num_warps, num_ctas, clusterDimX, clusterDimY, clusterDimZ, shared_memory, (CUstream)_stream, (CUfunction)_function{', ' + ', '.join(f"ptr_info{i}.dev_ptr" if ty[0]=="*" else f"_arg{i}"for i, ty in signature.items()) if len(signature) > 0 else ''});
+  Py_END_ALLOW_THREADS;
 
- if (PyErr_Occurred()) {{
-    return NULL;
-  }}
   if (launch_exit_hook != Py_None && !PyObject_CallObject(launch_exit_hook, args)) {{
     return NULL;
   }}

commit fe90f275df66464ec7e4aa317c215c43bf57f9df

[FRONTEND] Fix Python error handling in launch
Similar to #2225, this was regressed by #2185 because we didn't realise
CUDA_CHECK macro could do Python calls. I think the PyErr_Occurred got
removed in that PR because there was missing error handling before the
call to _launch.

It looks like there are also potentially a couple places in cuda.c that
are missing error handling, e.g. getDeviceProperties, memAlloc,
memcpyHtoD, memFree, tensorMapEncodeTiled etc, but those are all
pre-existing and not affected by recent changes.

python/triton/compiler/make_launcher.py
@@ -255,6 +255,9 @@ static PyObject* launch(PyObject* self, PyObject* args) {{
   Py_BEGIN_ALLOW_THREADS;
   _launch(gridX, gridY, gridZ, num_warps, num_ctas, clusterDimX, clusterDimY, clusterDimZ, shared_memory, (CUstream)_stream, (CUfunction)_function{', ' + ', '.join(f"ptr_info{i}.dev_ptr" if ty[0]=="*" else f"_arg{i}"for i, ty in signature.items()) if len(signature) > 0 else ''});
   Py_END_ALLOW_THREADS;
+  if (PyErr_Occurred()) {{
+    return NULL;
+  }}
 
   if (launch_exit_hook != Py_None && !PyObject_CallObject(launch_exit_hook, args)) {{
     return NULL;