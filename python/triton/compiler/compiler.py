bba24bb1915a3b6bf33828982461d92536db5d72
Phil Tillet <phil@openai.com>
2023-09-25 23:00:51 -0700

    .

--- python/triton/compiler/compiler.py
+++ python/triton/compiler/compiler.py
@@ -406,7 +406,7 @@ def compile(fn, **kwargs):
                           lambda src: ttgir_to_llir(src, extern_libs, target, tma_infos))
         add_cuda_stages(target, extern_libs, stages)
     elif device_type == "hip":
-        _device_backend.add_stages(arch, extern_libs, stages, num_warps=num_warps, num_stages=num_stages)
+        _device_backend.add_stages(target, extern_libs, stages, num_warps=num_warps, num_stages=num_stages)
     else:
         # pass the user's configuration to the backend device.
         target["num_warps"] = num_warps
