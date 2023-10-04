6a173eab8a55fae1ae5f3b89cd3bf7afaa0511c9
oplavsic <130548569+oplavsic@users.noreply.github.com>
2023-10-04 21:10:07 +0200

    Remove redundant fp32->fp16 conversion in FA (#349)

--- python/tutorials/06-fused-attention.py
+++ python/tutorials/06-fused-attention.py
@@ -90,7 +90,7 @@ def _fwd_kernel(
         qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float16)
         if IS_CAUSAL:
             qk = tl.where(P_SEQ + offs_m[:, None] >= (start_n + offs_n[None, :]), qk, float("-inf"))
-        qk += tl.dot(q, k, out_dtype=tl.float16)
+        qk += tl.dot(q, k)
         # -- compute scaling constant ---
         m_i_new = tl.maximum(m_i, tl.max(qk, 1))
         alpha = tl.math.exp2(m_i - m_i_new)
