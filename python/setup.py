5d1ca98e8db083395737e194ea14e38b6397c31b
Goran Flegar <gflegar@google.com>
2023-10-23 15:07:45 +0200

    Bump LLVM version to https://github.com/llvm/llvm-project/commit/49af6502c6dcb4a7f7520178bd14df396f78240c (#2530)

--- python/setup.py
+++ python/setup.py
@@ -86,7 +86,7 @@ def get_llvm_package_info():
         return Package("llvm", "LLVM-C.lib", "", "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH")
     # use_assert_enabled_llvm = check_env_flag("TRITON_USE_ASSERT_ENABLED_LLVM", "False")
     # release_suffix = "assert" if use_assert_enabled_llvm else "release"
-    rev = "b1115f8c"
+    rev = "49af6502"
     name = f"llvm-{rev}-{system_suffix}"
     url = f"https://tritonlang.blob.core.windows.net/llvm-builds/{name}.tar.gz"
     return Package("llvm", name, url, "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH")
