1d686adb6126d2895b0c0284751533d825e363a8
Zahi Moudallal <zahi@openai.com>
2023-10-23 18:27:49 -0700

    Big refactor/cleanup

--- lib/Dialect/TritonGPU/Transforms/OptimizeThreadLocality.cpp
+++ lib/Dialect/TritonGPU/Transforms/OptimizeThreadLocality.cpp
@@ -10,83 +10,30 @@
 
 using namespace mlir;
 
-// Replace ForOp with a new ForOp with extra operands. The YieldOp is not
-// updated and needs to be updated separatly for the loop to be correct.
-static scf::ForOp replaceForOpWithNewSignature(OpBuilder &rewriter,
-                                               scf::ForOp loop,
-                                               ValueRange newIterOperands) {
-  OpBuilder::InsertionGuard g(rewriter);
-  rewriter.setInsertionPoint(loop);
-
-  // Create a new loop before the existing one, with the extra operands.
-  // rewriter.setInsertionPoint(loop);
-  auto operands = llvm::to_vector<4>(loop.getInitArgs());
-  operands.append(newIterOperands.begin(), newIterOperands.end());
-  scf::ForOp newLoop = rewriter.create<scf::ForOp>(
-      loop.getLoc(), loop.getLowerBound(), loop.getUpperBound(), loop.getStep(),
-      operands);
-  newLoop.getBody()->erase();
-  newLoop.getRegion().getBlocks().splice(
-      newLoop.getRegion().getBlocks().begin(), loop.getRegion().getBlocks());
-  for (Value operand : newIterOperands)
-    newLoop.getBody()->addArgument(operand.getType(), operand.getLoc());
-
-  for (auto it : llvm::zip(loop.getResults(), newLoop.getResults().take_front(
-                                                  loop.getNumResults())))
-    std::get<0>(it).replaceAllUsesWith(std::get<1>(it));
-  return newLoop;
-}
-
 class TritonGPUOptimizeThreadLocalityPass
     : public TritonGPUOptimizeThreadLocalityBase<
           TritonGPUOptimizeThreadLocalityPass> {
-  template <typename T>
-  SmallVector<T> insertValue(ArrayRef<T> vec, unsigned index, int value) const {
-    SmallVector<T> res(vec.begin(), vec.end());
-    res.insert(res.begin() + index, static_cast<T>(value));
-    return res;
-  }
   void runOnOperation() override {
     ModuleOp mod = getOperation();
-    int counter = 0;
     DenseSet<triton::ReduceOp> reduceOps;
-    mod.walk([&](triton::ReduceOp reduce) -> void {
-      if (counter > 0)
-        return;
-      counter++;
-      std::cout << "reduce op found" << std::endl;
-      reduceOps.insert(reduce);
-    });
+    mod.walk(
+        [&](triton::ReduceOp reduce) -> void { reduceOps.insert(reduce); });
     for (auto reduce : reduceOps) {
       OpBuilder builder(reduce);
       auto srcType = reduce.getOperands()[0].getType().cast<RankedTensorType>();
       auto srcShape = srcType.getShape();
       auto srcEncoding = srcType.getEncoding();
-      if (!srcEncoding.isa<triton::gpu::BlockedEncodingAttr>())
-        return;
+      assert(srcEncoding.isa<triton::gpu::BlockedEncodingAttr>() &&
+             "Thread locality optimization only supports blocked encoding");
       auto blocked = srcEncoding.dyn_cast<triton::gpu::BlockedEncodingAttr>();
       auto elemsPerThread =
           triton::gpu::getElemsPerThread(srcType)[reduce.getAxis()];
       auto rank = srcShape.size();
       // create new layouts
-      auto sizePerThread3d =
-          insertValue(blocked.getSizePerThread(), rank,
-                      blocked.getSizePerThread()[reduce.getAxis()]);
-      sizePerThread3d[reduce.getAxis()] = 1;
-      auto threadsPerWarp3d = insertValue(blocked.getThreadsPerWarp(), rank, 1);
-      auto warsPerCTA3d = insertValue(blocked.getWarpsPerCTA(), rank, 1);
-      auto order3d = insertValue(blocked.getOrder(), 0, 2);
-      auto ctasPerCGA3d =
-          insertValue(blocked.getCTALayout().getCTAsPerCGA(), rank, 1);
-      auto ctasSplitNum3d =
-          insertValue(blocked.getCTALayout().getCTASplitNum(), rank, 1);
-      auto ctaOrder3d =
-          insertValue(blocked.getCTALayout().getCTAOrder(), rank, 2);
-      auto ctaLayout3d = triton::gpu::CTALayoutAttr::get(
-          mod.getContext(), ctasPerCGA3d, ctasSplitNum3d, ctaOrder3d);
-      auto blocked3d = triton::gpu::BlockedEncodingAttr::get(
-          mod.getContext(), sizePerThread3d, threadsPerWarp3d, warsPerCTA3d,
-          order3d, ctaLayout3d);
+      auto blocked3d = getThreadLocalityOptimizedEncoding(reduce);
+      auto viewOpTensorShape = getThreadLocalityOptimizedShape(reduce);
+      auto viewOpTensorType = RankedTensorType::get(
+          viewOpTensorShape, srcType.getElementType(), blocked3d);
       auto slice2d =
           triton::gpu::SliceEncodingAttr::get(mod.getContext(), 2, blocked3d);
       auto slice1d = triton::gpu::SliceEncodingAttr::get(
@@ -97,96 +44,205 @@ class TritonGPUOptimizeThreadLocalityPass
       auto operandNumber = use.getOperandNumber();
       auto user = use.getOwner();
       assert(user->getNumOperands() == 2);
-      assert(operandNumber == 1);
-      auto firstOperand = user->getOperand(0);
-      assert(firstOperand.isa<BlockArgument>());
-      auto blockArg = firstOperand.dyn_cast<BlockArgument>();
+      auto accumOperandNumber = (operandNumber == 0) ? 1 : 0;
+      auto accumOperand = user->getOperand(accumOperandNumber);
+      assert(accumOperand.isa<BlockArgument>());
+      auto blockArg = accumOperand.dyn_cast<BlockArgument>();
       auto blockArgNum = blockArg.getArgNumber();
       auto forOp = dyn_cast<scf::ForOp>(blockArg.getOwner()->getParentOp());
-
-      // get initArg
-      auto initArg =
+      // get oldAccum
+      auto oldAccum =
           forOp.getInitArgs()[blockArgNum - forOp.getNumInductionVars()];
-      // create newAccum initialization
-      SmallVector<int64_t> accumShape(srcShape.begin(), srcShape.end());
-      accumShape[reduce.getAxis()] /= elemsPerThread;
-      auto accumType = RankedTensorType::get(
-          accumShape,
-          initArg.getType().cast<RankedTensorType>().getElementType(), slice2d);
-      builder.setInsertionPointAfter(initArg.getDefiningOp());
-      auto newAccum = builder.create<arith::ConstantOp>(
-          initArg.getLoc(), accumType, builder.getZeroAttr(accumType));
       // get old loop user
-      OpOperand &loopUse = *(forOp.getResult(0).getUses().begin());
-      auto loopUser = loopUse.getOwner();
+      Value loopResult =
+          forOp.getResult(blockArgNum - forOp.getNumInductionVars());
+      assert(loopResult.hasOneUse());
+      OpOperand &loopUse = *(loopResult.getUses().begin());
+      Operation *loopUser = loopUse.getOwner();
       // get old loop yield
       auto oldYield = cast<scf::YieldOp>(forOp.getBody()->getTerminator());
+      // create newAccum initialization
+      auto newAccum =
+          createAccum(builder, oldAccum, viewOpTensorShape, slice2d);
       // create new loop
       auto newLoop = replaceForOpWithNewSignature(
-          builder, forOp, ValueRange{newAccum.getResult()});
+          builder, forOp, ValueRange{newAccum->getResult(0)});
       // create new reduce
-      auto viewOpTensorShape = insertValue(srcShape, rank, 1);
-      viewOpTensorShape[reduce.getAxis()] /= elemsPerThread;
-      viewOpTensorShape[rank] = elemsPerThread;
-      auto viewOpTensorType = RankedTensorType::get(
-          viewOpTensorShape, srcType.getElementType(), blocked3d);
-      builder.setInsertionPointAfter(reduce);
-      IRMapping mapping;
-      for (auto operand : reduce.getOperands()) {
-        auto viewOp = builder.create<triton::ViewOp>(reduce.getLoc(),
-                                                     viewOpTensorType, operand);
-        mapping.map(operand, viewOp);
-      }
+      auto newReduce = createReduce(builder, reduce, viewOpTensorType);
 
-      auto newReduce = cloneWithInferType(builder, &(*reduce), mapping);
-      newReduce->setAttr("axis", builder.getI32IntegerAttr(rank));
-      auto typeInfer = dyn_cast<InferTypeOpInterface>(newReduce);
-      if (typeInfer) {
-        SmallVector<Type, 1> newTypes;
-        auto success = typeInfer.inferReturnTypes(
-            newReduce->getContext(), newReduce->getLoc(),
-            newReduce->getOperands(), newReduce->getAttrDictionary(),
-            newReduce->getPropertiesStorage(), newReduce->getRegions(),
-            newTypes);
-        if (succeeded(success)) {
-          for (size_t i = 0; i < newTypes.size(); i++)
-            newReduce->getResult(i).setType(newTypes[i]);
-        }
-      }
       // create new accum update
-      auto newArg = newLoop.getBody()->getArgument(blockArgNum + 1);
-      builder.setInsertionPointAfter(user);
-      auto newUpdate = builder.create<arith::AddFOp>(reduce.getLoc(), newArg,
-                                                     newReduce->getResult(0));
+      auto newUpdate = createUpdate(builder, newLoop, newReduce, user);
       // create new yield
-      builder.setInsertionPointToEnd(newLoop.getBody());
-      auto newLoopBlockArg = newLoop.getBody()->getArgument(blockArgNum);
-      SmallVector<Value> yieldValues = {newLoopBlockArg, newUpdate};
-      auto newYield =
-          builder.create<scf::YieldOp>(newLoop.getLoc(), yieldValues);
+      auto newYield = createYield(builder, newLoop, oldYield,
+                                  newUpdate->getResult(0), blockArgNum);
       // Add one more reduce after loop
-      auto newLoopResult = newLoop.getResult(1);
-      builder.setInsertionPointAfter(newLoop);
-      IRMapping newMapping;
-      newMapping.map(*(reduce.getOperands().begin()), newLoopResult);
-      auto newReduce2 = cloneWithInferType(builder, &(*reduce), newMapping);
+      auto newReduce2 = createPostLoopReduce(builder, newLoop, reduce);
+
       // Replace loop user with new reduce
-      builder.setInsertionPointAfter(loopUser);
-      IRMapping newMapping1;
-      newMapping1.map(loopUser->getOperands()[0], newReduce2->getResult(0));
-      auto finalOp = builder.clone(*loopUser, newMapping1);
-      // Replace uses of loopUser with finalOp
-      loopUser->replaceAllUsesWith(finalOp);
+      replaceLoopUser(builder, loopUse, newReduce2);
 
       oldYield.erase();
       forOp.erase();
-      user->erase();
-      reduce.erase();
+    }
+  };
+
+private:
+  void replaceLoopUser(OpBuilder &builder, OpOperand &loopUse,
+                       Operation *newReduce2) const {
+    Operation *loopUser = loopUse.getOwner();
+    if (auto cvtLayout = dyn_cast<triton::gpu::ConvertLayoutOp>(loopUser)) {
+      builder.setInsertionPointAfter(loopUser);
+      IRMapping mapping;
+      mapping.map(loopUser->getOperands()[0], newReduce2->getResult(0));
+      auto newCvt = builder.clone(*loopUser, mapping);
+      // Replace uses of loopUser with finalOp
+      loopUser->replaceAllUsesWith(newCvt);
+      loopUser->erase();
+    } else {
+      builder.setInsertionPoint(loopUser);
+      auto operandIdx = loopUse.getOperandNumber();
+      Value operand = loopUser->getOperand(operandIdx);
+      Type destType = operand.getType();
+      auto newCvt = builder.create<triton::gpu::ConvertLayoutOp>(
+          loopUser->getLoc(), destType, newReduce2->getResult(0));
+      IRMapping mapping;
+      mapping.map(operand, newCvt->getResult(0));
+      auto newUser = builder.clone(*loopUser, mapping);
+      // Replace uses of loopUser with finalOp
+      loopUser->replaceAllUsesWith(newUser);
       loopUser->erase();
     }
-    // std::cout << "Printing module after pass" << std::endl;
-    // mod.dump();
-  };
+  }
+
+  Operation *createPostLoopReduce(OpBuilder &builder, scf::ForOp &loop,
+                                  triton::ReduceOp &reduce) const {
+    auto resultIndex =
+        loop.getBody()->getNumArguments() - 1 - loop.getNumInductionVars();
+    auto newLoopResult = loop.getResult(resultIndex);
+    builder.setInsertionPointAfter(loop);
+    IRMapping mapping;
+    mapping.map(*(reduce.getOperands().begin()), newLoopResult);
+    auto newReduce2 = cloneWithInferType(builder, &(*reduce), mapping);
+    return newReduce2;
+  }
+
+  Operation *createYield(OpBuilder &builder, scf::ForOp &loop,
+                         scf::YieldOp &oldYield, Value newUpdate,
+                         int oldAccumBlockArgNum) const {
+    builder.setInsertionPoint(oldYield);
+    SmallVector<Value> yieldValues = llvm::to_vector(oldYield.getOperands());
+    yieldValues[oldAccumBlockArgNum - 1] =
+        loop.getBody()->getArgument(oldAccumBlockArgNum);
+    yieldValues.push_back(newUpdate);
+    auto newYield =
+        builder.create<scf::YieldOp>(oldYield.getLoc(), yieldValues);
+    return newYield;
+  }
+
+  Operation *createUpdate(OpBuilder &builder, scf::ForOp &loop,
+                          Operation *newReduce, Operation *oldUpdate) const {
+    auto blockArgNum = loop.getBody()->getNumArguments() - 1;
+    auto newArg = loop.getBody()->getArgument(blockArgNum);
+    builder.setInsertionPointAfter(newReduce);
+    IRMapping mapping;
+    mapping.map(oldUpdate->getOperand(0), newArg);
+    mapping.map(oldUpdate->getOperand(1), newReduce->getResult(0));
+    auto newUpdate = cloneWithInferType(builder, oldUpdate, mapping);
+    // auto newUpdate = builder.create<arith::AddFOp>(reduce.getLoc(), newArg,
+    //                                                newReduce->getResult(0));
+    return newUpdate;
+  }
+
+  Operation *createReduce(OpBuilder &builder, triton::ReduceOp reduce,
+                          Type viewOpTensorType) const {
+    auto srcType = reduce.getOperands()[0].getType().cast<RankedTensorType>();
+    auto rank = srcType.getShape().size();
+    builder.setInsertionPointAfter(reduce);
+    IRMapping mapping;
+    for (auto operand : reduce.getOperands()) {
+      auto viewOp = builder.create<triton::ViewOp>(reduce.getLoc(),
+                                                   viewOpTensorType, operand);
+      mapping.map(operand, viewOp);
+    }
+
+    auto newReduce = cloneWithInferType(builder, &(*reduce), mapping);
+    newReduce->setAttr("axis", builder.getI32IntegerAttr(rank));
+    auto typeInfer = dyn_cast<InferTypeOpInterface>(newReduce);
+    if (typeInfer) {
+      SmallVector<Type, 1> newTypes;
+      auto success = typeInfer.inferReturnTypes(
+          newReduce->getContext(), newReduce->getLoc(),
+          newReduce->getOperands(), newReduce->getAttrDictionary(),
+          newReduce->getPropertiesStorage(), newReduce->getRegions(), newTypes);
+      if (succeeded(success)) {
+        for (size_t i = 0; i < newTypes.size(); i++)
+          newReduce->getResult(i).setType(newTypes[i]);
+      }
+    }
+    return newReduce;
+  }
+
+  Operation *createAccum(OpBuilder &builder, Value &oldAccum,
+                         SmallVector<int64_t> &shape,
+                         Attribute &slice2d) const {
+    // Drop the last dimension (thread locality dimension)
+    SmallVector<int64_t> accumShape(shape.begin(), shape.end() - 1);
+    // Create tensor type for the new accumulator
+    auto accumType = RankedTensorType::get(
+        accumShape,
+        oldAccum.getType().cast<RankedTensorType>().getElementType(), slice2d);
+    // Create new accumulator
+    builder.setInsertionPointAfter(oldAccum.getDefiningOp());
+    auto newAccum = builder.create<arith::ConstantOp>(
+        oldAccum.getLoc(), accumType, builder.getZeroAttr(accumType));
+    return newAccum;
+  }
+
+  SmallVector<int64_t>
+  getThreadLocalityOptimizedShape(triton::ReduceOp reduce) const {
+    auto srcType = reduce.getOperands()[0].getType().cast<RankedTensorType>();
+    auto srcShape = srcType.getShape();
+    auto rank = srcShape.size();
+    auto elemsPerThread =
+        triton::gpu::getElemsPerThread(srcType)[reduce.getAxis()];
+    auto viewOpTensorShape = insertValue(srcShape, rank, 1);
+    viewOpTensorShape[reduce.getAxis()] /= elemsPerThread;
+    viewOpTensorShape[rank] = elemsPerThread;
+    return viewOpTensorShape;
+  }
+
+  Attribute getThreadLocalityOptimizedEncoding(triton::ReduceOp reduce) const {
+    auto srcType = reduce.getOperands()[0].getType().cast<RankedTensorType>();
+    auto rank = srcType.getShape().size();
+    auto srcEncoding = srcType.getEncoding();
+    auto blocked = srcEncoding.dyn_cast<triton::gpu::BlockedEncodingAttr>();
+    auto sizePerThread3d =
+        insertValue(blocked.getSizePerThread(), rank,
+                    blocked.getSizePerThread()[reduce.getAxis()]);
+    sizePerThread3d[reduce.getAxis()] = 1;
+    auto threadsPerWarp3d = insertValue(blocked.getThreadsPerWarp(), rank, 1);
+    auto warsPerCTA3d = insertValue(blocked.getWarpsPerCTA(), rank, 1);
+    auto order3d = insertValue(blocked.getOrder(), 0, 2);
+    auto ctasPerCGA3d =
+        insertValue(blocked.getCTALayout().getCTAsPerCGA(), rank, 1);
+    auto ctasSplitNum3d =
+        insertValue(blocked.getCTALayout().getCTASplitNum(), rank, 1);
+    auto ctaOrder3d =
+        insertValue(blocked.getCTALayout().getCTAOrder(), rank, 2);
+    auto ctaLayout3d = triton::gpu::CTALayoutAttr::get(
+        reduce.getContext(), ctasPerCGA3d, ctasSplitNum3d, ctaOrder3d);
+    auto blocked3d = triton::gpu::BlockedEncodingAttr::get(
+        reduce.getContext(), sizePerThread3d, threadsPerWarp3d, warsPerCTA3d,
+        order3d, ctaLayout3d);
+    return blocked3d;
+  }
+
+  template <typename T>
+  SmallVector<T> insertValue(ArrayRef<T> vec, unsigned index, int value) const {
+    SmallVector<T> res(vec.begin(), vec.end());
+    res.insert(res.begin() + index, static_cast<T>(value));
+    return res;
+  }
 };
 
 std::unique_ptr<Pass> mlir::createTritonGPUOptimizeThreadLocalityPass() {
--- lib/Dialect/TritonGPU/Transforms/Utility.cpp
+++ lib/Dialect/TritonGPU/Transforms/Utility.cpp
@@ -357,6 +357,32 @@ bool canFoldIntoConversion(Operation *op, Attribute targetEncoding) {
 
 //
 
+// Replace ForOp with a new ForOp with extra operands. The YieldOp is not
+// updated and needs to be updated separatly for the loop to be correct.
+scf::ForOp replaceForOpWithNewSignature(OpBuilder &rewriter, scf::ForOp loop,
+                                        ValueRange newIterOperands) {
+  OpBuilder::InsertionGuard g(rewriter);
+  rewriter.setInsertionPoint(loop);
+
+  // Create a new loop before the existing one, with the extra operands.
+  // rewriter.setInsertionPoint(loop);
+  auto operands = llvm::to_vector<4>(loop.getInitArgs());
+  operands.append(newIterOperands.begin(), newIterOperands.end());
+  scf::ForOp newLoop = rewriter.create<scf::ForOp>(
+      loop.getLoc(), loop.getLowerBound(), loop.getUpperBound(), loop.getStep(),
+      operands);
+  newLoop.getBody()->erase();
+  newLoop.getRegion().getBlocks().splice(
+      newLoop.getRegion().getBlocks().begin(), loop.getRegion().getBlocks());
+  for (Value operand : newIterOperands)
+    newLoop.getBody()->addArgument(operand.getType(), operand.getLoc());
+
+  for (auto it : llvm::zip(loop.getResults(), newLoop.getResults().take_front(
+                                                  loop.getNumResults())))
+    std::get<0>(it).replaceAllUsesWith(std::get<1>(it));
+  return newLoop;
+}
+
 Operation *cloneWithInferType(mlir::OpBuilder &rewriter, Operation *op,
                               IRMapping &mapping) {
   Operation *newOp = rewriter.clone(*op, mapping);
