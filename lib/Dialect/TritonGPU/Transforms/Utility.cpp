ca8f110617c0996f9db4966eca286dc4bef91fc3
Thomas Raoux <thomas.raoux@openai.com>
2023-11-02 09:56:39 -0700

    [BACKEND] Pipeliner refactoring (#2565)
    Refactor the pipeliner pass in order to make it more generic. The main
    change is that the pipeliner is now broken into 2 pieces one calculating
    a modulo schedule and create async ops based on the IR and an expander
    that will generate the pipelined IR based on the modulo schedule.
    The advantage of separating the two pieces is that it will allow us to
    create different schedule without having to change the expander and it
    will allow for more complex schedules.
    For now the schedule generated for matmul case matches rougly the
    schedule picked by the previous pipeliner in order to avoid changes.
    
    This also creates a different sequence of insert/extract slice for the
    alloc. We should probably change shared alloc to use memory semantic.

--- lib/Dialect/TritonGPU/Transforms/Utility.cpp
+++ lib/Dialect/TritonGPU/Transforms/Utility.cpp
@@ -355,10 +355,6 @@ bool canFoldIntoConversion(Operation *op, Attribute targetEncoding) {
              triton::MakeRangeOp, triton::SplatOp>(op);
 }
 
-//
-
-// Replace ForOp with a new ForOp with extra operands. The YieldOp is not
-// updated and needs to be updated separatly for the loop to be correct.
 scf::ForOp replaceForOpWithNewSignature(OpBuilder &rewriter, scf::ForOp loop,
                                         ValueRange newIterOperands) {
   OpBuilder::InsertionGuard g(rewriter);
