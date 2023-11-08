f77764533f3320b5660e4eeafc0c6443fd6a06ef
Phil Tillet <phil@openai.com>
2023-11-08 09:28:45 -0800

    .

--- python/triton/language/semantic.py
+++ python/triton/language/semantic.py
@@ -1201,25 +1201,26 @@ def dot(lhs: tl.tensor, rhs: tl.tensor, acc: tl.tensor, allow_tf32: bool, max_nu
                 return
             assert lhs_dtype == rhs_dtype, f"First input ({lhs_dtype}) and second input ({rhs_dtype}) must have the same dtype!"
         else:
-            assert not lhs_dtype.is_fp8e4b15() and not rhs_dtype.is_fp8e4b15(
-            ), "Dot op does not support fp8e4b15 on CUDA arch >= 90"
-            assert not lhs_dtype.is_fp8e4b15x4() and not rhs_dtype.is_fp8e4b15x4(
-            ), "Dot op does not support fp8e4b15x4 on CUDA arch >= 90"
-            if lhs_dtype.is_int() or rhs_dtype.is_int():
-                assert lhs_dtype == rhs_dtype, f"Both operands must be same type. First operand ({lhs_dtype}) and second operand ({rhs_dtype})"
-                assert lhs_dtype.is_int8() or lhs_dtype.is_uint8(
-                ), f"Both operands must be either int8 or uint8. Operand type ({lhs_dtype})"
-            elif lhs_dtype.is_fp8() or rhs_dtype.is_fp8():
-                assert lhs_dtype.is_fp8e4nv() or lhs_dtype.is_fp8e5(
-                ), f"Only supports fp8e4nv or fp8e5. First operand ({lhs_dtype})"
-                assert rhs_dtype.is_fp8e4nv() or rhs_dtype.is_fp8e5(
-                ), f"Only supports fp8e4nv or fp8e5. Second operand ({rhs_dtype})"
-            else:
-                assert lhs_dtype.is_fp16() or lhs_dtype.is_bf16() or lhs_dtype.is_fp32() or lhs_dtype.is_int1(
-                ), f"Unsupported dtype {lhs_dtype}"
-                assert rhs_dtype.is_fp16() or rhs_dtype.is_bf16() or rhs_dtype.is_fp32() or rhs_dtype.is_int1(
-                ), f"Unsupported dtype {rhs_dtype}"
-                assert lhs_dtype == rhs_dtype, f"First input ({lhs_dtype}) and second input ({rhs_dtype}) must have the same dtype!"
+            pass
+            # assert not lhs_dtype.is_fp8e4b15() and not rhs_dtype.is_fp8e4b15(
+            # ), "Dot op does not support fp8e4b15 on CUDA arch >= 90"
+            # assert not lhs_dtype.is_fp8e4b15x4() and not rhs_dtype.is_fp8e4b15x4(
+            # ), "Dot op does not support fp8e4b15x4 on CUDA arch >= 90"
+            # if lhs_dtype.is_int() or rhs_dtype.is_int():
+            #     assert lhs_dtype == rhs_dtype, f"Both operands must be same type. First operand ({lhs_dtype}) and second operand ({rhs_dtype})"
+            #     assert lhs_dtype.is_int8() or lhs_dtype.is_uint8(
+            #     ), f"Both operands must be either int8 or uint8. Operand type ({lhs_dtype})"
+            # elif lhs_dtype.is_fp8() or rhs_dtype.is_fp8():
+            #     assert lhs_dtype.is_fp8e4nv() or lhs_dtype.is_fp8e5(
+            #     ), f"Only supports fp8e4nv or fp8e5. First operand ({lhs_dtype})"
+            #     assert rhs_dtype.is_fp8e4nv() or rhs_dtype.is_fp8e5(
+            #     ), f"Only supports fp8e4nv or fp8e5. Second operand ({rhs_dtype})"
+            # else:
+            #     assert lhs_dtype.is_fp16() or lhs_dtype.is_bf16() or lhs_dtype.is_fp32() or lhs_dtype.is_int1(
+            #     ), f"Unsupported dtype {lhs_dtype}"
+            #     assert rhs_dtype.is_fp16() or rhs_dtype.is_bf16() or rhs_dtype.is_fp32() or rhs_dtype.is_int1(
+            #     ), f"Unsupported dtype {rhs_dtype}"
+            #     assert lhs_dtype == rhs_dtype, f"First input ({lhs_dtype}) and second input ({rhs_dtype}) must have the same dtype!"
 
     assert lhs.type.is_block() and rhs.type.is_block()
 
