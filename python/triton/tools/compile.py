7e122f710b00c193d89e08f8788d403d73172795
jeromeku <jerome.ku@gmail.com>
2023-11-10 12:51:48 -0800

    add debug flags to aot compiler script

--- python/triton/tools/compile.py
+++ python/triton/tools/compile.py
@@ -1,6 +1,7 @@
 import binascii
 import hashlib
 import importlib.util
+import json
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
@@ -37,20 +38,60 @@ used to run this `compile.py` script
 """
 
 if __name__ == "__main__":
-
     # command-line arguments
     parser = ArgumentParser(description=desc)
-    parser.add_argument("path",
-                        help="Path to Python source containing desired kernel in its scope. File will be executed.")
-    parser.add_argument("--kernel-name", "-n", type=str, default="", help="Name of the kernel to compile",
-                        required=True)
-    parser.add_argument("--num-warps", "-w", type=int, default=1, help="Number of warps to launch the kernel")
-    parser.add_argument("--num-stages", "-ns", type=int, default=3,
-                        help="Number of stages (meta-parameter of the kernel)")
-    parser.add_argument("--out-name", "-on", type=str, default=None, help="Out name for the compiled kernel")
-    parser.add_argument("--out-path", "-o", type=Path, default=None, help="Out filename")
-    parser.add_argument("--signature", "-s", type=str, help="Signature of the kernel", required=True)
-    parser.add_argument("--grid", "-g", type=str, help="Launch grid of the kernel", required=True)
+    parser.add_argument(
+        "path",
+        help="Path to Python source containing desired kernel in its scope. File will be executed.",
+    )
+    parser.add_argument(
+        "--kernel-name",
+        "-n",
+        type=str,
+        default="",
+        help="Name of the kernel to compile",
+        required=True,
+    )
+    parser.add_argument(
+        "--num-warps",
+        "-w",
+        type=int,
+        default=1,
+        help="Number of warps to launch the kernel",
+    )
+    parser.add_argument(
+        "--num-stages",
+        "-ns",
+        type=int,
+        default=3,
+        help="Number of stages (meta-parameter of the kernel)",
+    )
+    parser.add_argument(
+        "--out-name",
+        "-on",
+        type=str,
+        default=None,
+        help="Out name for the compiled kernel",
+    )
+    parser.add_argument(
+        "--out-path", "-o", type=Path, default=None, help="Out filename"
+    )
+    parser.add_argument(
+        "--signature", "-s", type=str, help="Signature of the kernel", required=True
+    )
+    parser.add_argument(
+        "--grid", "-g", type=str, help="Launch grid of the kernel", required=True
+    )
+    parser.add_argument(
+        "--save_args",
+        action="store_true",
+        help="Save args passed to `triton.compiler.compile`, primarily for use with `triton.tools.aot.compiler`",
+    )
+    parser.add_argument(
+        "--save_params",
+        action="store_true",
+        help="Save params passed to kernel template, primarily for use with `triton.tools.aot.compiler`",
+    )
     args = parser.parse_args()
 
     out_name = args.out_name if args.out_name else args.kernel_name
@@ -68,6 +109,10 @@ if __name__ == "__main__":
 
     # validate and parse signature
     signature = list(map(lambda s: s.strip(" "), args.signature.split(",")))
+
+    # Save original signature -- mainly for debugging and testing of `triton.tools.aot.compiler`
+    if args.save_args:
+        original_signature = {i: v for i, v in enumerate(signature)}
 
     def hash_signature(signature: List[str]):
         m = hashlib.sha256()
@@ -94,8 +139,17 @@ if __name__ == "__main__":
     hints = {k: v for k, v in hints.items() if v is not None}
     constexprs = {i: constexpr(s) for i, s in enumerate(signature)}
     constexprs = {k: v for k, v in constexprs.items() if v is not None}
-    signature = {i: s.split(":")[0] for i, s in enumerate(signature) if i not in constexprs}
-    const_sig = 'x'.join([str(v) for v in constexprs.values()])
+
+    # Save original constants -- mainly for debugging and testing of AOT Compiler class
+    if args.save_params:
+        from copy import deepcopy
+
+        original_constants = deepcopy(constexprs)
+
+    signature = {
+        i: s.split(":")[0] for i, s in enumerate(signature) if i not in constexprs
+    }
+    const_sig = "x".join([str(v) for v in constexprs.values()])
     doc_string = [f"{kernel.arg_names[i]}={constexprs[i]}" for i in constexprs.keys()]
     doc_string += [f"num_warps={args.num_warps}", f"num_stages={args.num_stages}"]
 
@@ -104,11 +158,19 @@ if __name__ == "__main__":
         assert h in [1, 16], f"Only 1 and 16 are valid hints, got {h}"
     divisible_by_16 = [i for i, h in hints.items() if h == 16]
     equal_to_1 = [i for i, h in hints.items() if h == 1]
-    config = triton.compiler.instance_descriptor(divisible_by_16=divisible_by_16, equal_to_1=equal_to_1)
+    config = triton.compiler.instance_descriptor(
+        divisible_by_16=divisible_by_16, equal_to_1=equal_to_1
+    )
     for i in equal_to_1:
         constexprs.update({i: 1})
-    ccinfo = triton.compile(kernel, signature=signature, constants=constexprs, configs=[config],
-                            num_warps=args.num_warps, num_stages=args.num_stages)
+    ccinfo = triton.compile(
+        kernel,
+        signature=signature,
+        constants=constexprs,
+        configs=[config],
+        num_warps=args.num_warps,
+        num_stages=args.num_stages,
+    )
     arg_names = []
     arg_types = []
     for i in signature.keys():
@@ -118,28 +180,67 @@ if __name__ == "__main__":
 
     # dump C stub code
     suffix = kernel_suffix(signature.values(), config)
-    func_name = '_'.join([out_name, sig_hash, suffix])
-    triton_kernel_name = '_'.join([args.kernel_name, suffix])
+    func_name = "_".join([out_name, sig_hash, suffix])
+    triton_kernel_name = "_".join([args.kernel_name, suffix])
     hex_ = str(binascii.hexlify(ccinfo.asm["cubin"]))[2:-1]
     params = {
         "kernel_name": func_name,
         "triton_kernel_name": triton_kernel_name,
         "bin_size": len(hex_),
         "bin_data": ", ".join([f"0x{x}{y}" for x, y in zip(hex_[::2], hex_[1::2])]),
-        "signature": ", ".join([f"{ty_to_cpp(ty)} {name}" for name, ty in zip(arg_names, arg_types)]),
-        "full_signature": ", ".join([f"{ty_to_cpp(signature[i])} {kernel.arg_names[i]}" for i in signature.keys()]),
+        "signature": ", ".join(
+            [f"{ty_to_cpp(ty)} {name}" for name, ty in zip(arg_names, arg_types)]
+        ),
+        "full_signature": ", ".join(
+            [
+                f"{ty_to_cpp(signature[i])} {kernel.arg_names[i]}"
+                for i in signature.keys()
+            ]
+        ),
         "arg_pointers": ", ".join([f"&{arg}" for arg in arg_names]),
         "num_args": len(arg_names),
         "kernel_docstring": doc_string,
         "shared": ccinfo.shared,
         "num_warps": args.num_warps,
-        "algo_info": '_'.join([const_sig, meta_sig]),
+        "algo_info": "_".join([const_sig, meta_sig]),
         "gridX": grid[0],
         "gridY": grid[1],
         "gridZ": grid[2],
         "_placeholder": "",
     }
-    for ext in ['h', 'c']:
+    for ext in ["h", "c"]:
         template_path = Path(__file__).parent / f"compile.{ext}"
         with out_path.with_suffix(f".{sig_hash}_{suffix}.{ext}").open("w") as fp:
             fp.write(Path(template_path).read_text().format(**params))
+
+    # Dump compile args
+    if args.save_args:
+        with out_path.with_suffix(f".{sig_hash}_{suffix}.jit_args.json").open(
+            "w"
+        ) as fp:
+            serialized_sig = json.dumps(signature)
+            serialized_const = json.dumps(constexprs)
+
+            # Unpack configs to dicts for serialization
+            unpacked_configs = []
+            for c in [config]:
+                unpacked_configs.append({k: list(v) for k, v in c._asdict().items()})
+
+            serialized_args = {
+                "signature": signature,
+                "constants": constexprs,
+                "configs": unpacked_configs,
+                "num_warps": args.num_warps,
+                "num_stages": args.num_stages,
+                "grid": grid,
+                "_original_signature": original_signature,
+                "_original_constants": original_constants,
+            }
+            json.dump(serialized_args, fp, indent=2)
+    # Dump params
+    if args.save_params:
+        with out_path.with_suffix(f".{sig_hash}_{suffix}.params.json").open("w") as fp:
+            params["gridX"] = params["gridX"].strip()
+            params["gridY"] = params["gridY"].strip()
+            params["gridZ"] = params["gridZ"].strip()
+            json.dump(params, fp, indent=2)
