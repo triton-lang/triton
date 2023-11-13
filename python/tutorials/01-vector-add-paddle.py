101ca55d2a27ce1e3f56f167ce0b647a5aa85095
nishirong <1847623494@qq.com>
2023-11-13 08:25:57 +0000

    add tutorials-add-paddle

---
+++ python/tutorials/01-vector-add-paddle.py
@@ -0,0 +1,118 @@
+"""
+Vector Addition
+===============
+
+In this tutorial, you will write a simple vector addition using Triton.
+
+In doing so, you will learn about:
+
+* The basic programming model of Triton.
+
+* The `triton.jit` decorator, which is used to define Triton kernels.
+
+* The best practices for validating and benchmarking your custom ops against native reference implementations.
+
+"""
+
+# %%
+# Compute Kernel
+# -----------
+import triton
+import triton.language as tl
+
+import paddle
+
+
+@triton.jit
+def add_kernel(x_ptr,  # *Pointer* to first input vector.
+               y_ptr,  # *Pointer* to second input vector.
+               output_ptr,  # *Pointer* to output vector.
+               n_elements,  # Size of the vector.
+               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+               # NOTE: `constexpr` so it can be used as a shape value.
+               ):
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load x and y from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    # Write x + y back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+
+# %%
+# Let's also declare a helper function to (1) allocate the `z` tensor
+# and (2) enqueue the above kernel with appropriate grid/block sizes:
+
+
+def add_paddle(x: paddle.Tensor, y: paddle.Tensor):
+    # We need to preallocate the output.
+    # output = torch.empty_like(x)
+    output = paddle.empty_like(x)
+
+    # 在paddle官网当中找不到类似的接口
+    # assert x.is_cuda and y.is_cuda and output.is_cuda
+
+    
+    n_elements = len(output)
+    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
+    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
+    # In this case, we use a 1D grid where the size is the number of blocks:
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    # NOTE:
+    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+    #  - Don't forget to pass meta-parameters as keywords arguments.
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
+    # running asynchronously at this point.
+    return output
+
+
+# %%
+# We can now use the above function to compute the element-wise sum of two `torch.tensor` objects and test its correctness:
+
+# torch.manual_seed(0)
+# size = 98432
+# x = torch.rand(size, device='cuda')
+# y = torch.rand(size, device='cuda')
+# output_torch = x + y
+# output_triton = add(x, y)
+# print(output_torch)
+# print(output_triton)
+# print(f'The maximum difference between torch and triton is '
+#       f'{torch.max(torch.abs(output_torch - output_triton))}')
+
+size = 98432
+paddle.set_device('gpu:0')
+shape_tensor = paddle.to_tensor([size])
+x = paddle.randn(shape_tensor)
+y = paddle.randn(shape_tensor)
+output_paddle = x + y
+output_triton = add_paddle(x, y)
+print(output_paddle)
+print(output_triton)
+print(f'The maximum difference between paddle and triton is '
+      f'{paddle.max(paddle.abs(output_paddle - output_triton))}')
+
+# %%
+# Seems like we're good to go!
+
+# %%
+# Benchmark
+# ---------
+#
+# We can now benchmark our custom op on vectors of increasing sizes to get a sense of how it does relative to PyTorch.
+# To make things easier, Triton has a set of built-in utilities that allow us to concisely plot the performance of our custom ops.
+# for different problem sizes.
