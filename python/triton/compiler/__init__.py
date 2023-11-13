adfdb35997781346346a399053b2812b960140d5
Phil Tillet <phil@openai.com>
2023-11-12 17:45:59 -0800

    some cleaning

--- python/triton/compiler/__init__.py
+++ python/triton/compiler/__init__.py
@@ -1,8 +1,12 @@
 from .compiler import (CompiledKernel, compile, get_arch_default_num_stages, get_arch_default_num_warps,
                        instance_descriptor)
 from .errors import CompilationError
+from .backends.cuda import CUDABackend
+from ..common.backend import register_backend
 
 __all__ = [
     "compile", "instance_descriptor", "CompiledKernel", "CompilationError", "get_arch_default_num_warps",
     "get_arch_default_num_stages"
 ]
+
+register_backend("cuda", CUDABackend)
