d719c2078218c84cc4d491d1c10880ed55675d5a
Nhat Nguyen <hoangnhat2911@gmail.com>
2023-11-14 19:36:34 +0700

    update

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,8 @@
 cd /home/nhat/github/triton/python
 export TRITON_SHARED_OPT_PATH="$(pwd)/build/$(ls $(pwd)/build | grep -i cmake)/third_party/triton_shared/tools/triton-shared-opt/triton-shared-opt"
 export LLVM_BINARY_DIR="${HOME}/.triton/llvm/$(ls ${HOME}/.triton/llvm/ | grep -i llvm)/bin"
-python3 ../third_party/triton_shared/python/examples/reduce.py
+
+echo "TRITON_SHARED_OPT_PATH=$TRITON_SHARED_OPT_PATH"
+echo "LLVM_BINARY_DIR=$LLVM_BINARY_DIR"
+
+# python3 ../third_party/triton_shared/python/examples/reduce.py
