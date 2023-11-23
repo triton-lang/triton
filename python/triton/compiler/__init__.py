b40ae9797b572003f81e9e4e1801bcab62c2bc01
Phil Tillet <phil@openai.com>
2023-11-22 22:53:22 -0800

    .

--- python/triton/compiler/__init__.py
+++ python/triton/compiler/__init__.py
@@ -1,7 +1,7 @@
-from .compiler import (CompiledKernel, compile)
+from .compiler import (CompiledKernel, compile, InstanceDescriptor)
 from .errors import CompilationError
 
 __all__ = [
-    "compile", "instance_descriptor", "CompiledKernel", "CompilationError", "get_arch_default_num_warps",
+    "compile", "InstanceDescriptor", "CompiledKernel", "CompilationError", "get_arch_default_num_warps",
     "get_arch_default_num_stages"
 ]
