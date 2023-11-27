1e00e4a5aa4bd65f4f165a23a1ad0b8a475e9eb1
Phil Tillet <phil@openai.com>
2023-11-26 21:50:54 -0800

    .

--- python/test/unit/runtime/test_subproc.py
+++ python/test/unit/runtime/test_subproc.py
@@ -1,7 +1,6 @@
 import multiprocessing
 import os
 import shutil
-from collections import namedtuple
 
 import torch
 
@@ -17,10 +16,6 @@ def reset_tmp_dir():
         shutil.rmtree(tmpdir, ignore_errors=True)
 
 
-instance_descriptor = namedtuple("instance_descriptor",
-                                 ["divisible_by_16", "equal_to_1", "ids_of_folded_args", "divisible_by_8"])
-
-
 def compile_fn(config, cc):
 
     @triton.jit
@@ -29,20 +24,18 @@ def compile_fn(config, cc):
         tl.store(o + idx, tl.load(a + idx) - tl.load(b + idx) * 777)
 
     triton.compile(
-        fn=kernel_sub,
+        src=kernel_sub,
         signature={0: "*fp32", 1: "*fp32", 2: "*fp32"},
-        device=0,
+        device_type=("cuda", cc),
         constants={3: 32},
         configs=[config],
-        warm_cache_only=True,
-        cc=cc,
     )
 
 
 def test_compile_in_subproc() -> None:
     major, minor = torch.cuda.get_device_capability(0)
     cc = major * 10 + minor
-    config = instance_descriptor(tuple(range(4)), (), (), ())
+    config = triton.compiler.InstanceDescriptor(tuple(range(4)), (), (), ())
 
     multiprocessing.set_start_method('fork')
     proc = multiprocessing.Process(target=compile_fn, args=(config, cc))
@@ -61,12 +54,10 @@ def compile_fn_dot(config, cc):
         tl.store(Z + offs, z)
 
     triton.compile(
-        fn=kernel_dot,
+        src=kernel_dot,
         signature={0: "*fp32"},
-        device=0,
+        device_type=("cuda", cc),
         configs=[config],
-        warm_cache_only=True,
-        cc=cc,
     )
 
 
@@ -74,7 +65,7 @@ def test_compile_in_forked_subproc() -> None:
     reset_tmp_dir()
     major, minor = torch.cuda.get_device_capability(0)
     cc = major * 10 + minor
-    config = instance_descriptor(tuple(range(1)), (), (), ())
+    config = triton.compiler.InstanceDescriptor(tuple(range(1)), (), (), ())
 
     assert multiprocessing.get_start_method() == 'fork'
     proc = multiprocessing.Process(target=compile_fn_dot, args=(config, cc))
