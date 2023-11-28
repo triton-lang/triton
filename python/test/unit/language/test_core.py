acf02eea87684ec6f3baf8b7fc92e959df826455
Shikai Li <levendlee@gmail.com>
2023-11-28 15:59:40 -0800

    Resolves a yapf format failure.

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -785,8 +785,8 @@ def test_where_broadcast(num_ctas, device):
 @pytest.mark.parametrize("dtype", dtypes_with_bfloat16)
 @pytest.mark.parametrize("op", ["maximum", "minimum"])
 def test_maximum_minium(dtype, op):
-  expr = f'tl.{op}(x, y)'
-  _test_binary(dtype, dtype, expr)
+    expr = f'tl.{op}(x, y)'
+    _test_binary(dtype, dtype, expr)
 
 
 # ---------------
