8fe071b06b593392f6fafcb89c9c69833c18b7a4
Christos Perivolaropoulos <cperivol@google.com>
2023-12-05 14:02:30 +0000

    Made the test slightly better

--- python/test/regression/test_dedup_mma.py
+++ python/test/regression/test_dedup_mma.py
@@ -7,6 +7,8 @@
  assigned contiguous blocks of elements to be computed.
 
 """
+
+import os
 import pytest
 import torch
 import numpy as np
@@ -19,30 +21,39 @@ out_dtypes = ["float16", "float32"]
 
 
 def test_elementwise_dedupe():
-
+    # XXX: Could probably be achieved with interpreter mode aswell.
     def numpy_kernel():
         mask = np.ones((16, 16), dtype=bool)
-        mask[:2, :] = False
-        masked = np.where(mask, np.zeros((16, 16)), 1)
-        return masked @ np.ones((16, 16))
+        mask &= (np.arange(0, 16) < 2)[:, None]
+        x = np.zeros((16, 16), dtype=np.float32)
+        dot = x @ x
+        masked = np.where(mask, dot, 1)
+        return masked @ np.ones((16, 16), dtype=np.float32)
 
     @jit
     def kernel(out_ptr):
         # Make a 16x16 mask where `mask[0:2,:] == False`
-        mask_qk = tl.full([16, 16], 1, dtype=tl.int1)
-        mask_qk &= (tl.arange(0, 16) < 2)[:,None]
+        mask = tl.full([16, 16], 1, dtype=tl.int1)
+        mask &= (tl.arange(0, 16) < 2)[:, None]
         # Force the mask encoding to #mma so it needs convert to #dot.
-        x = tl.zeros([16,16], tl.float32)
+        x = tl.zeros([16, 16], tl.float32)
+        # make sure it converts to dot
         dot = tl.dot(x, x)
-        # make sure it converts to dot. If the layout is not properly
-        # taken into account (8, 0) may not be masked.
-        res = tl.where(mask_qk, dot, 1)
+        res = tl.where(mask, dot, 1)
         res = tl.dot(res, tl.full(x.shape, 1, x.dtype))
-        tl.store(to_uniblock16(out_ptr), res)
+        out_ptr = tl.make_block_ptr(
+            out_ptr,
+            shape=(16, 16),
+            strides=(16, 1),
+            offsets=(0, 0),
+            block_shape=(16, 16),
+            order=(0, 1),
+        )
+        tl.store(out_ptr, res)
 
-    grid = (1,)
-    triton_res = torch.zeros((16, 16))
-    kernel[grid](triton_res)
-    numpy_res = numpy_kernel()
+    baseline_res = numpy_kernel()
 
-    torch.testing.assert_close(triton_res, numpy_res)
+    gpu_res = torch.zeros((16, 16))
+    kernel[(1,)](gpu_res)
+
+    torch.testing.assert_close(triton_res, baseline_res)
