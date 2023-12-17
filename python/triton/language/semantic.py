a813cd8c84fab4c97bfbd1afbefba48f84a67428
Philippe Tillet <phil@openai.com>
2023-12-16 22:12:26 -0800

    [FRONTEND] split pybind11 src into multiple files (#2810)
    this will make it easier to track what is nvidia-specific vs what can be
    used by future plugins

--- python/triton/language/semantic.py
+++ python/triton/language/semantic.py
@@ -3,7 +3,7 @@ from __future__ import annotations  # remove after python 3.11
 from functools import wraps
 from typing import List, Optional, Sequence, Tuple, TypeVar
 
-from .._C.libtriton.triton import ir
+from .._C.libtriton import ir
 from ..common.build import is_hip
 from . import core as tl
 
--- python/triton/runtime/jit.py
+++ python/triton/runtime/jit.py
@@ -10,7 +10,7 @@ from collections import defaultdict, namedtuple
 from functools import cached_property
 from typing import Callable, Generic, Iterable, List, Optional, TypeVar, Union, cast, overload
 
-from .._C.libtriton.triton import TMAInfos
+from .._C.libtriton.translation import TMAInfos
 from ..common.backend import get_backend, get_cuda_version_key
 from .interpreter import InterpretedFunction
 from ..runtime.driver import driver
