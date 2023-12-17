0c66349f17e0bdf7cbea7198812113efe3b6b884
Phil Tillet <phil@openai.com>
2023-12-16 21:34:35 -0800

    separate triton python module into multiple files

---
+++ python/src/runtime.cc
@@ -0,0 +1,23 @@
+﻿#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+// enum backend_t {
+//   HOST,
+//   CUDA,
+//   ROCM,
+// };
+
+void init_triton_runtime(py::module &&m) {
+  // wrap backend_t
+  // py::enum_<backend_t>(m, "backend", py::module_local())
+  //     .value("HOST", HOST)
+  //     .value("CUDA", CUDA)
+  //     .value("ROCM", ROCM)
+  //     .export_values();
+
+  // py::enum_<mlir::triton::Target>(m, "TARGET")
+  //     .value("NVVM", mlir::triton::NVVM)
+  //     .value("ROCDL", mlir::triton::ROCDL)
+  //     .export_values();
+}
