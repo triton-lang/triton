40a651d7cc07087e53cb038ac0ec72f58e6df3e0
Phil Tillet <phil@openai.com>
2024-01-08 00:40:22 -0800

    .

--- python/triton/runtime/driver.py
+++ python/triton/runtime/driver.py
@@ -8,4 +8,39 @@ def _create_driver():
     return actives[0]()
 
 
-driver = _create_driver()
+class LazyProxy:
+
+    def __init__(self, init_fn):
+        self._init_fn = init_fn
+        self._obj = None
+
+    def _initialize_obj(self):
+        if self._obj is None:
+            self._obj = self._init_fn()
+
+    def __getattr__(self, name):
+        self._initialize_obj()
+        return getattr(self._obj, name)
+
+    def __setattr__(self, name, value):
+        if name in ["_init_fn", "_obj"]:
+            super().__setattr__(name, value)
+        else:
+            self._initialize_obj()
+            setattr(self._obj, name, value)
+
+    def __delattr__(self, name):
+        self._initialize_obj()
+        delattr(self._obj, name)
+
+    def __repr__(self):
+        if self._obj is None:
+            return f"<{self.__class__.__name__} for {self._init_fn} not yet initialized>"
+        return repr(self._obj)
+
+    def __str__(self):
+        self._initialize_obj()
+        return str(self._obj)
+
+
+driver = LazyProxy(_create_driver)
