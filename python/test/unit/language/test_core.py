01a38eaa13c054f63fbd85b7b15769888bd98379
Sasha Rush <srush.research@gmail.com>
2024-01-16 18:56:08 -0500

    Add a flip operator and a test

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -4431,3 +4431,24 @@ def test_sort(M, N, descending, dtype_str, device):
     z = torch.empty_like(x)
     sort_kernel[(1, )](x, z, N, M, descending, num_warps=8)
     assert (y == z).all(), (y, z)
+
+
+@pytest.mark.parametrize("M, N", [[1, 512], [8, 64], [256, 16], [512, 8]])
+@pytest.mark.parametrize("dtype_str", ['int32', 'float16', 'float32'])
+def test_flip(M, N, dtype_str, device):
+
+    @triton.jit
+    def flip_kernel(X, Z, N: tl.constexpr, M: tl.constexpr):
+        offx = tl.arange(0, M)
+        offy = tl.arange(0, N) * M
+        off2d = offx[None, :] + offy[:, None]
+        x = tl.load(X + off2d)
+        x = tl.flip(x)
+        tl.store(Z + off2d, x)
+
+    x = numpy_random((N, M), dtype_str=dtype_str)
+    x = torch.from_numpy(x).to("cuda")
+    y = torch.flip(x, (1,))
+    z = torch.empty_like(x)
+    flip_kernel[(1, )](x, z, N, M, num_warps=8)
+    assert (y == z).all(), (y, z)
