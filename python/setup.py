8375c520dd9d3a4a31d9e17fb03376d6c9e965bd
Justin Lebar <justin.lebar@gmail.com>
2024-01-16 11:12:17 -0800

    Update version to 3.0.0.
    The release branch is on 2.2.0, and we plan for our next release to
    break bw compatibility, so calling it 3.0.0.
    
    Fixes #2940.

--- python/setup.py
+++ python/setup.py
@@ -384,7 +384,7 @@ package_data.update({f"triton/backends/{b.name}": b.package_data for b in backen
 
 setup(
     name=os.environ.get("TRITON_WHEEL_NAME", "triton"),
-    version="2.1.0" + os.environ.get("TRITON_WHEEL_VERSION_SUFFIX", ""),
+    version="3.0.0" + os.environ.get("TRITON_WHEEL_VERSION_SUFFIX", ""),
     author="Philippe Tillet",
     author_email="phil@openai.com",
     description="A language and compiler for custom Deep Learning operations",
