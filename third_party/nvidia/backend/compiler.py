977cd07626b035fe44dee7ecee65f9707cfd4506
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-01-19 17:49:05 -0500

    try

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -1,6 +1,7 @@
 from triton.backends.compiler import BaseBackend
 from triton._C.libtriton import ir, passes, llvm, nvidia
-from triton.backends.nvidia.driver import CudaUtils
+# from triton.backends.nvidia.driver import CudaUtils
+from triton.runtime import driver
 
 from dataclasses import dataclass
 import functools
@@ -80,7 +81,6 @@ class InfoFromBackendForTensorMap:
     ntma = 0
 
     def __init__(self, infos=None, dummy=False):
-        self.utils = CudaUtils()
         self.dummy = dummy
         self.ids_of_folded_args = ()
         if not dummy and not isinstance(infos, dict):
@@ -93,33 +93,33 @@ class InfoFromBackendForTensorMap:
     def _dummy(self):
         assert InfoFromBackendForTensorMap.n < InfoFromBackendForTensorMap.N
         if InfoFromBackendForTensorMap.n == 0:
-            self.tensorDataType = self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT16"]
+            self.tensorDataType = driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT16"]
             self.tensorRank = 4
             self.globalAddressArgIdx = 0
             self.globalStridesArgIdx = [7, 6, -1, -1]
             self.globalDimsArgIdx = [5, 3, -1, -1]
             self.boxDims = [16, 64, 1, 1]
             self.elementStrides = [1, 1, 1, 1]
-            self.interleave = self.utils.CUtensorMapInterleave["CU_TENSOR_MAP_INTERLEAVE_NONE"]
-            self.swizzle = self.utils.CUtensorMapSwizzle["CU_TENSOR_MAP_SWIZZLE_32B"]
-            self.l2Promotion = self.utils.CUtensorMapL2promotion["CU_TENSOR_MAP_L2_PROMOTION_L2_128B"]
+            self.interleave = driver.active.CUtensorMapInterleave["CU_TENSOR_MAP_INTERLEAVE_NONE"]
+            self.swizzle = driver.active.CUtensorMapSwizzle["CU_TENSOR_MAP_SWIZZLE_32B"]
+            self.l2Promotion = driver.active.CUtensorMapL2promotion["CU_TENSOR_MAP_L2_PROMOTION_L2_128B"]
             self.TMADescArgIdx = 11
-            self.oobFill = self.utils.CUtensorMapFloatOOBfill["CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE"]
+            self.oobFill = driver.active.CUtensorMapFloatOOBfill["CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE"]
             InfoFromBackendForTensorMap.n += 1
             return
         if InfoFromBackendForTensorMap.n == 1:
-            self.tensorDataType = self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT16"]
+            self.tensorDataType = driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT16"]
             self.tensorRank = 4
             self.globalAddressArgIdx = 1
             self.globalStridesArgIdx = [7, 6, -1, -1]
             self.globalDimsArgIdx = [5, 3, -1, -1]
             self.boxDims = [16, 64, 1, 1]
             self.elementStrides = [1, 1, 1, 1]
-            self.interleave = self.utils.CUtensorMapInterleave["CU_TENSOR_MAP_INTERLEAVE_NONE"]
-            self.swizzle = self.utils.CUtensorMapSwizzle["CU_TENSOR_MAP_SWIZZLE_32B"]
-            self.l2Promotion = self.utils.CUtensorMapL2promotion["CU_TENSOR_MAP_L2_PROMOTION_L2_128B"]
+            self.interleave = driver.active.CUtensorMapInterleave["CU_TENSOR_MAP_INTERLEAVE_NONE"]
+            self.swizzle = driver.active.CUtensorMapSwizzle["CU_TENSOR_MAP_SWIZZLE_32B"]
+            self.l2Promotion = driver.active.CUtensorMapL2promotion["CU_TENSOR_MAP_L2_PROMOTION_L2_128B"]
             self.TMADescArgIdx = 12
-            self.oobFill = self.utils.CUtensorMapFloatOOBfill["CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE"]
+            self.oobFill = driver.active.CUtensorMapFloatOOBfill["CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE"]
             InfoFromBackendForTensorMap.n += 1
             return
 
@@ -164,19 +164,19 @@ class InfoFromBackendForTensorMap:
     # dtype:cuda.CUtensorMapDataType | int
     def bytes_from_type(self, dtype):
         return {
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT8"]: 1,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT16"]: 2,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT32"]: 4,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_INT32"]: 4,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT64"]: 8,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_INT64"]: 8,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT16"]: 2,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT32"]: 4,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT64"]: 8,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_BFLOAT16"]: 2,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT32_FTZ"]: 4,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_TFLOAT32"]: 4,
-            self.utils.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_TFLOAT32_FTZ"]: 4
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT8"]: 1,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT16"]: 2,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT32"]: 4,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_INT32"]: 4,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_UINT64"]: 8,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_INT64"]: 8,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT16"]: 2,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT32"]: 4,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT64"]: 8,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_BFLOAT16"]: 2,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_FLOAT32_FTZ"]: 4,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_TFLOAT32"]: 4,
+            driver.active.CUtensorMapDataType["CU_TENSOR_MAP_DATA_TYPE_TFLOAT32_FTZ"]: 4
         }[dtype]
 
     def getTensorMapDataType(self):
@@ -255,7 +255,7 @@ class InfoFromBackendForTensorMap:
             return idx
 
     def tensormap(self, args):
-        return self.utils.cuTensorMapEncodeTiled(
+        return driver.active.cuTensorMapEncodeTiled(
             self.getTensorMapDataType(),
             self.getTensorRank(),
             self.getGlobalAddress(args),
