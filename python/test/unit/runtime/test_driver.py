6a3ade628b29d70dc2ca06762a6c0ee0e4f9b552
Nhat Nguyen <hoangnhat2911@gmail.com>
2024-01-19 18:06:29 -0500

    fix is_lazy

--- python/test/unit/runtime/test_driver.py
+++ python/test/unit/runtime/test_driver.py
@@ -7,8 +7,8 @@ def test_is_lazy():
     from importlib import reload
     reload(sys.modules["triton.runtime.driver"])
     reload(sys.modules["triton.runtime"])
-    mod = sys.modules[triton.runtime.driver.__module__]
-    assert isinstance(triton.runtime.driver, getattr(mod, "LazyProxy"))
-    assert triton.runtime.driver._obj is None
-    utils = triton.runtime.driver.utils  # noqa: F841
-    assert issubclass(triton.runtime.driver._obj.__class__, getattr(triton.backends.driver, "DriverBase"))
+    mod = sys.modules[triton.runtime.driver.active.__module__]
+    assert isinstance(triton.runtime.driver.active, getattr(mod, "LazyProxy"))
+    assert triton.runtime.driver.active._obj is None
+    utils = triton.runtime.driver.active.utils  # noqa: F841
+    assert issubclass(triton.runtime.driver.active._obj.__class__, getattr(triton.backends.driver, "DriverBase"))
