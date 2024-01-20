5422b2c15cca251b0eaa7a5898220565e8123400
Phil Tillet <phil@openai.com>
2024-01-19 22:16:44 -0800

    .

--- include/triton/Conversion/TritonGPUToLLVM/Passes.h
+++ include/triton/Conversion/TritonGPUToLLVM/Passes.h
@@ -2,6 +2,7 @@
 #define TRITONGPU_CONVERSION_TRITONGPUTOLLVM_PASSES_H
 
 #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "triton/Target/PTX/TmaMetadata.h"
 
@@ -28,6 +29,9 @@ std::unique_ptr<OperationPass<ModuleOp>> createConvertTritonGPUToLLVMPass();
 std::unique_ptr<OperationPass<ModuleOp>>
 createConvertTritonGPUToLLVMPass(int32_t computeCapability, Target target,
                                  mlir::triton::gpu::TMAMetadataTy *tmaMetadata);
+
+#define GEN_PASS_REGISTRATION
+#include "triton/Conversion/TritonGPUToLLVM/Passes.h.inc"
 
 } // namespace triton
 
