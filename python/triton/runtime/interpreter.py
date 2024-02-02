007d6a65aaf2b389834cccb4ef06a76038900353
Guoliang He <61049384+hgl71964@users.noreply.github.com>
2024-02-02 17:00:01 +0000

    [interpreter] add handler for slice (#3069)
    fix #3068

--- python/triton/runtime/interpreter.py
+++ python/triton/runtime/interpreter.py
@@ -382,7 +382,14 @@ def _patch_lang_tensor(tensor, builder):
     tensor.__index__ = lambda self: int(self.handle.data)
     tensor.__bool__ = lambda self: True
     tensor.__str__ = lambda self: str(self.handle.data)
-    tensor.__getitem__ = lambda self, slices: self.handle.data.__getitem__(slices)
+
+    def handle_slice(self, slices):
+        data = self.handle.data.__getitem__(slices)
+        tensor_handle = TensorHandle(data, self.dtype)
+        tensor_type = tl.block_type(self.dtype, data.shape)
+        return tl.core.tensor(tensor_handle, tensor_type)
+
+    tensor.__getitem__ = handle_slice
 
 
 def _patch_lang_core(lang, builder):
