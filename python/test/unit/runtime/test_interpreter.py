8bdccf3f6829c353d072c18268db1226f0a9f6ed
Guoliang He <guolianghe1996@gmail.com>
2024-02-04 20:10:42 +0000

    add test_interpreter

---
+++ python/test/unit/runtime/test_interpreter.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import pytest
+
+# activate interpreter mode
+os.environ['TRITON_INTERPRET'] = '1'
+
+# resolve the relative path to test_core.py 
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_CORE = os.path.join(TEST_DIR, '..', 'language')
+sys.path.append(TEST_CORE)
+
+
+from test_core import test_bin_op
