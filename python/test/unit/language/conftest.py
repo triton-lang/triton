0b8a1cb2c5691f7e99008b0d1d0246da39f6f2ab
Guoliang He <guolianghe1996@gmail.com>
2024-02-07 20:25:33 +0000

    add interpreter test

--- python/test/unit/language/conftest.py
+++ python/test/unit/language/conftest.py
@@ -1,5 +1,6 @@
 # content of conftest.py
 
+import os
 import pytest
 
 
@@ -10,3 +11,11 @@ def pytest_addoption(parser):
 @pytest.fixture
 def device(request):
     return request.config.getoption("--device")
+
+
+@pytest.fixture(scope="function", params=[False, True])
+def add_interpreter_test(request):
+    mode = request.param
+    os.environ['TRITON_INTERPRET'] = '1' if mode else '0'
+    yield
+    os.environ['TRITON_INTERPRET'] = '0'
