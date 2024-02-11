bbf4b80bfe184af82696249fbd6ec7c0f674b05f
Phil Tillet <phil@openai.com>
2024-02-11 00:55:39 -0800

    progress

--- python/tutorials/sort-bench.py
+++ python/tutorials/sort-bench.py
@@ -5,21 +5,19 @@ import triton.language as tl
 
 
 @triton.jit
-def compare_and_swap(x, i: tl.constexpr, n_dims: tl.constexpr):
+def compare_and_swap(x, desc_mask, i: tl.constexpr, n_dims: tl.constexpr):
     shape: tl.constexpr = [2**i, 2, 2**(n_dims - i - 1)]
     y = tl.reshape(x, shape)
     mask = tl.arange(0, 2)[None, :, None]
     left = tl.broadcast_to(tl.sum(y * (1 - mask), 1)[:, None, :], shape)
     right = tl.broadcast_to(tl.sum(y * mask, 1)[:, None, :], shape)
+    # reshapes
     left = tl.reshape(left, x.shape)
     right = tl.reshape(right, x.shape)
-    ileft = left.to(tl.int32, bitcast=True)
-    iright = right.to(tl.int32, bitcast=True)
-    ix = x.to(tl.int32, bitcast=True)
-    _0 = tl.zeros_like(ix)
-    ret = ix ^ tl.where((ileft > iright), ileft ^ iright, _0)
-    ret = ret.to(x.dtype, bitcast=True)
-    return left, right, ret
+    desc_mask = tl.reshape(desc_mask, x.shape)
+    _0 = tl.zeros_like(left)
+    ret = x ^ tl.where((left > right) ^ desc_mask, left ^ right, _0)
+    return left, right, ret, desc_mask
 
 
 @triton.jit
@@ -27,14 +25,21 @@ def sort_rows(X, Y1, Y2, R, N: tl.constexpr, N_DIMS: tl.constexpr):
     pid = tl.program_id(0)
     Xs = X + pid * N + tl.arange(0, N)
     x = tl.load(Xs)
-    y1, y2, r = compare_and_swap(x, 2, N_DIMS)
-    tl.store(Y1 + pid * N + tl.arange(0, N), y1)
-    tl.store(Y2 + pid * N + tl.arange(0, N), y2)
+    i: tl.constexpr = 2
+    # desc_mask
+    shape: tl.constexpr = [2**(i - 1), 2, 2**(N_DIMS - i)]
+    desc_mask = tl.broadcast_to(tl.arange(0, 2)[None, :, None], shape)
+    #
+    y1, y2, r, m = compare_and_swap(x, desc_mask, i, N_DIMS)
+    # tl.store(Y1 + pid * N + tl.arange(0, N), y1)
+    # tl.store(Y2 + pid * N + tl.arange(0, N), y2)
+
     tl.store(R + pid * N + tl.arange(0, N), r)
+    # tl.store(Y2 + pid * N + tl.arange(0, N), m)
 
 
 N = 8
-X = torch.randint(10, (N, ), dtype=torch.int32, device="cuda")
+X = torch.tensor([3, 9, 1, 4, 2, 1, 9, 3], dtype=torch.int32, device="cuda")
 Y1 = torch.empty_like(X)
 Y2 = torch.empty_like(X)
 R = torch.empty_like(X)
@@ -43,3 +48,14 @@ print(X)
 print(Y1)
 print(Y2)
 print(R)
+
+# x x
+# x x
+# x x
+# x x
+
+# we want
+# 0 0
+# 1 1
+# 0 0
+# 1 1
