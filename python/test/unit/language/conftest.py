5a4e04a66ef6bf2db914b6eb23b58ffc94c7ed6d
Jokeren <robinho364@gmail.com>
2024-02-14 22:26:53 -0500

    Update
    Update
    
    Unpatch
    
    Update
    
    Update
    
    UPdate
    
    Try monkey patch
    
    UPdate
    
    [TEST] Generalize `test_core.py` for different backends (#3061)
    
    1. Do not check generated ptx asm for non cuda devices.
    2. Use `device` parameter when possible.
    3. Guide `torch.cuda.get_device_capability()` with
    `triton.runtime.driver.active.get_current_target()[0] == "cuda"`.
    
    ---------
    
    Signed-off-by: Whitney Tsang <whitney.tsang@intel.com>
    
    Update
    
    Update
    
    UPdate
    
    Update
    
    Update
    
    Update
    
    Update

--- python/test/unit/language/conftest.py
+++ python/test/unit/language/conftest.py
@@ -1,6 +1,5 @@
 # content of conftest.py
 
-import os
 import pytest
 
 
@@ -8,14 +7,10 @@ def pytest_addoption(parser):
     parser.addoption("--device", action="store", default='cuda')
 
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "interpreter: indicate whether interpreter supports the test")
+
+
 @pytest.fixture
 def device(request):
     return request.config.getoption("--device")
-
-
-@pytest.fixture(scope="function", params=[False, True])
-def add_interpreter_test(request):
-    mode = request.param
-    os.environ['TRITON_INTERPRET'] = '1' if mode else '0'
-    yield
-    os.environ['TRITON_INTERPRET'] = '0'
