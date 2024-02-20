0e681c03557c5a0db16e338e86a18adaa78c018c
Lixun Zhang <lixun.zhang@amd.com>
2024-02-20 09:36:46 -0600

    Add failed tests in summary only

--- python/check_test_core.sh
+++ python/check_test_core.sh
@@ -11,6 +11,7 @@ TEST_CORE=./test/unit/language/test_core.py
 pytest --co -q ${TEST_CORE} | sed "s/\[.*\]//g" | awk '!a[$0]++' | sed -n "/test_core.py/p" > test_names.txt
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=${BRANCH/\//-}
 COMMIT=$(git rev-parse --short HEAD)
 
 SUMMARY=${TEST_OUTPUT}/summary_${BRANCH}@${COMMIT}_$(date '+%m-%d-%Y-%H-%M-%S').txt
@@ -20,15 +21,21 @@ echo "Running test_core.py on ${BRANCH}@${COMMIT}" | tee -a ${SUMMARY}
 while read -r line; do
     test_name=${line##*::}
     if [[ ${line:0:1} != "#" ]];then
-        printf '%-45s' "${test_name}: " | tee -a $SUMMARY
+        printf '%-45s' "${test_name}: "
         Msg=$(pytest -v $line > ${TEST_OUTPUT}/${test_name}.output 2>&1)
         if [ $? -ne 0 ] && grep -Fq "Aborted" ${TEST_OUTPUT}/${test_name}.output; then
+            printf '%-45s' "${test_name}: " >> $SUMMARY
             printf "Aborted\n" | tee -a $SUMMARY
         else
             last_line=$(sed -n '/====/p' ${TEST_OUTPUT}/${test_name}.output | tail -n 1)
             report=${last_line#=* }
             report=${report%%in *}
-            printf "$report \n" | tee -a $SUMMARY
+            if [[ $report = *'failed'* ]] || [[ $report = *'skipped'* ]]; then
+                printf '%-45s' "${test_name}: " >> $SUMMARY
+                printf "$report \n" | tee -a $SUMMARY
+            else
+                printf "$report \n"
+            fi
         fi
     fi
 done < test_names.txt
