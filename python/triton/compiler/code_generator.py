91c0ccf255a8c8f72295fef612474feafda9fb9b
Keren Zhou <kerenzhou@openai.com>
2024-02-21 12:19:49 -0500

    [INTERPRETER] Revive flash attention test (#3158)

--- python/triton/compiler/code_generator.py
+++ python/triton/compiler/code_generator.py
@@ -6,7 +6,7 @@ import warnings
 from typing import Any, Callable, Dict, Optional, Tuple, Type, Union
 from .. import language
 from .._C.libtriton import ir
-from ..language import constexpr, tensor
+from ..language import constexpr, tensor, str_to_ty
 # ideally we wouldn't need any runtime component
 from ..runtime import JITFunction
 from .errors import (CompilationError, CompileTimeAssertionFailure, UnsupportedLanguageConstruct)
@@ -1163,34 +1163,6 @@ class CodeGenerator(ast.NodeVisitor):
         int: static_executor(int),
         len: static_executor(len),
     }
-
-
-def str_to_ty(name):
-    if name[0] == "*":
-        ty = str_to_ty(name[1:])
-        return language.pointer_type(ty)
-    tys = {
-        "fp8e4nv": language.float8e4nv,
-        "fp8e5": language.float8e5,
-        "fp8e4b15": language.float8e4b15,
-        "fp8e4b15x4": language.float8e4b15x4,
-        "fp16": language.float16,
-        "bf16": language.bfloat16,
-        "fp32": language.float32,
-        "fp64": language.float64,
-        "i1": language.int1,
-        "i8": language.int8,
-        "i16": language.int16,
-        "i32": language.int32,
-        "i64": language.int64,
-        "u1": language.int1,
-        "u8": language.uint8,
-        "u16": language.uint16,
-        "u32": language.uint32,
-        "u64": language.uint64,
-        "B": language.int1,
-    }
-    return tys[name]
 
 
 def kernel_suffix(signature, specialization):
