d821d7104ebe303bc8efb51aee444f24c564a1b3
Zahi Moudallal <zahi@openai.com>
2024-02-28 11:09:59 -0800

    Added instrinsic call to amd ballot op

--- third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.cpp
@@ -1,4 +1,5 @@
 #include "TargetInfo.h"
+#include "amd/include/TritonAMDGPUToLLVM/GCNAsmFormat.h"
 #include "triton/Conversion/TritonGPUToLLVM/Utility.h"
 
 namespace AMD {
@@ -6,6 +7,11 @@ bool TargetInfo::isSupported() const { return false; }
 Value TargetInfo::callBallotOp(ConversionPatternRewriter &rewriter,
                                Location loc, Value threadMask,
                                Value cmp) const {
-  return i32_val(0);
+  auto stringAttr = rewriter.getStringAttr("llvm.amdgcn.ballot");
+  SmallVector<Value> operands = {threadMask, cmp};
+  Value asmResult =
+      rewriter.create<LLVM::CallIntrinsicOp>(loc, i32_ty, stringAttr, operands)
+          ->getResult(0);
+  return asmResult;
 }
 } // namespace AMD
