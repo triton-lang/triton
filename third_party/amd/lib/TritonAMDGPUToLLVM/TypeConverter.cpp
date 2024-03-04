c123dc2dba91dec0398be57b3599a8f14a840f36
Lixun Zhang <Lixun.Zhang@amd.com>
2024-03-04 16:33:41 -0600

    [AMD] Refactor SharedToDotOperandMFMA  (#3264)
    This PR updates SharedToDotOperandMFMA.cpp and MFMA.cpp.
    - SharedToDotOperandMFMA.cpp is up to date with triton-mlir as of today,
    which includes changes until https://github.com/ROCm/triton/pull/482
      - Fixed issue with opaque pointers
    - Fixed API for `getMFMAElemsPerInstrForOperands` and
    `getMFMARepForOperands`
    - MFMA.cpp is synced with triton-mlir@6bb04d, which includes changes
    until https://github.com/ROCm/triton/pull/469
    
    Note to @binarman: changes in other files from
    https://github.com/ROCm/triton/pull/469 are not included in this PR. We
    can bring up the support for mfma 64x4 and 4x64 later.

--- third_party/amd/lib/TritonAMDGPUToLLVM/TypeConverter.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TypeConverter.cpp
@@ -128,16 +128,7 @@ Type TritonGPUToLLVMTypeConverter::getElementTypeForStruct(
     return elemTy;
 #ifdef USE_ROCM
   if (auto mfmaParent = dotOpLayout.getParent().dyn_cast<AMDMfmaEncodingAttr>()) {
-    if (elemTy.isF32())
-      return elemTy;
-    if (elemTy.isInteger(16)) // aka BF16
-      return vec_ty(elemTy, dotOpLayout.getKWidth());
-    if (elemTy.isF16())
-      return vec_ty(elemTy, 4);
-    if (elemTy.isInteger(8) && dotOpLayout.getKWidth() == 4)
-      return IntegerType::get(ctx, 32);
-    if (elemTy.isInteger(8) && dotOpLayout.getKWidth() == 8)
-      return IntegerType::get(ctx, 64);
+    return vec_ty(elemTy, dotOpLayout.getKWidth());
   }
 #endif
   auto mmaParent = dotOpLayout.getParent().dyn_cast<NvidiaMmaEncodingAttr>();
