e814545272cde5610e3d620a6ede4ca500b8f74a
Shucai Xiao <shucai.xiao@amd.com>
2024-03-06 13:53:45 -0600

    backup changes

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -2170,9 +2170,6 @@ def test_histogram(M, N, device):
 @pytest.mark.parametrize("N", [512, 1024, 2048])
 @pytest.mark.parametrize("num_pid_n", [2, 4])
 def test_locality(op, BLOCK_N, N, num_pid_n, device):
-    if is_hip():
-        pytest.skip('TODO test_locality is WIP on HIP')
-
     @triton.jit
     def kernel(X, Y, N, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr):
         start_m = tl.program_id(0)
@@ -2203,6 +2200,7 @@ def test_locality(op, BLOCK_N, N, num_pid_n, device):
         'min': np.min,
     }[op]
     kernel = patch_kernel(kernel, {'ACCUMULATE_PATCH': reduce_patch, 'INITIALIZE_PATCH': initialize_patch})
+    print(f"kernel_src = \n{kernel.src}")
     torch.manual_seed(0)
     BLOCK_M = 32
     x = torch.randn((BLOCK_M, N), dtype=torch.float32, device=device)
