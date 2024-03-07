15fec0a4503f28d5082a83a2e32f890559ef6e46
Zahi Moudallal <zahi@openai.com>
2024-03-07 11:44:02 -0800

    Delete dead code

--- third_party/amd/lib/TritonAMDGPUToLLVM/TritonGPUToLLVMBase.h
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TritonGPUToLLVMBase.h
@@ -22,10 +22,10 @@
 using namespace mlir;
 using namespace mlir::triton;
 
+using ::mlir::triton::gpu::AMDMfmaEncodingAttr;
 using ::mlir::triton::gpu::BlockedEncodingAttr;
 using ::mlir::triton::gpu::CTALayoutAttr;
 using ::mlir::triton::gpu::DotOperandEncodingAttr;
-using ::mlir::triton::gpu::AMDMfmaEncodingAttr;
 using ::mlir::triton::gpu::NvidiaMmaEncodingAttr;
 using ::mlir::triton::gpu::SliceEncodingAttr;
 namespace ttng = ::mlir::triton::nvidia_gpu;
@@ -167,11 +167,6 @@ struct CacheKeyDenseMapInfo {
   }
 };
 
-namespace mlir::triton {
-class ReduceOp;
-class ScanOp;
-} // namespace mlir::triton
-
 namespace AMD {
 class ConvertTritonGPUOpToLLVMPatternBase {
 public:
@@ -268,7 +263,6 @@ public:
             smem, offVal);
     return base;
   }
-
 
   SmallVector<Value>
   loadSharedToDistributed(Value dst, ArrayRef<SmallVector<Value>> dstIndices,
@@ -470,54 +464,5 @@ protected:
   }
 };
 
-template <typename SourceOp>
-class ConvertTritonGPUReduceScanToLLVMPattern
-    : public ConvertTritonGPUOpToLLVMPattern<SourceOp> {
-public:
-  // Make sure the class is only instantiated with Reduce and Scan
-  static_assert(std::is_same_v<SourceOp, ReduceOp> ||
-                std::is_same_v<SourceOp, ScanOp>);
-
-  using ConvertTritonGPUOpToLLVMPatternBase::getSharedMemoryBase;
-  using ConvertTritonGPUOpToLLVMPatternBase::getTypeConverter;
-  using ConvertTritonGPUOpToLLVMPattern<
-      SourceOp>::ConvertTritonGPUOpToLLVMPattern;
-
-  // Return the pointee type of the shared memory pointer for operand i.
-  Type getElementType(SourceOp op, int i) const {
-    auto ty = op.getInputTypes()[i].getElementType();
-    return getTypeConverter()->convertType(ty);
-  }
-
-  // Helper to compute the smem bases in both reductions and scans
-  SmallVector<Value> getSmemBases(SourceOp op, unsigned elems,
-                                  ConversionPatternRewriter &rewriter) const {
-    auto loc = op.getLoc();
-    // indices will store the index of the op operands in descending order
-    // of their bitwidths
-    std::vector<unsigned> indices(op.getNumOperands());
-    std::iota(indices.begin(), indices.end(), 0);
-
-    std::sort(indices.begin(), indices.end(), [&](unsigned i, unsigned j) {
-      return op.getElementTypes()[i].getIntOrFloatBitWidth() >
-             op.getElementTypes()[j].getIntOrFloatBitWidth();
-    });
-    // Assign base index to each operand in their order in indices
-    std::map<unsigned, Value> indexToBase;
-    indexToBase[indices[0]] =
-        getSharedMemoryBase(loc, rewriter, op.getOperation());
-    for (unsigned i = 1; i < op.getNumOperands(); ++i) {
-      indexToBase[indices[i]] = gep(
-          ptr_ty(rewriter.getContext(), 3), getElementType(op, indices[i - 1]),
-          indexToBase[indices[i - 1]], i32_val(elems));
-    }
-    // smemBases[k] is the base pointer for the k-th operand
-    SmallVector<Value> smemBases(op.getNumOperands());
-    for (unsigned i = 0; i < op.getNumOperands(); ++i) {
-      smemBases[i] = indexToBase[i];
-    }
-    return smemBases;
-  }
-};
 } // namespace AMD
 #endif
