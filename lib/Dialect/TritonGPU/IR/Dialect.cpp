22783335c1df7f956d6ac6a1fe7f1e571b62030f
Lixun Zhang <lixun.zhang@amd.com>
2024-03-08 15:53:32 -0600

    Replace amd::DecomposeConversion with common::ReduceDataDuplication
    and address review comments

--- lib/Dialect/TritonGPU/IR/Dialect.cpp
+++ lib/Dialect/TritonGPU/IR/Dialect.cpp
@@ -1489,7 +1489,6 @@ SmallVector<unsigned> AMDMfmaEncodingAttr::getSizePerThread() const {
 
 SmallVector<int64_t>
 AMDMfmaEncodingAttr::getMFMAInstrShapeForOperands(int kWidth, int opIdx) const {
-  // TODO(lixun): should we extend MFMAInstrShape to 3D?
   unsigned mDim = getMDim();
   unsigned nDim = getNDim();
   assert((mDim == nDim) && (mDim == 32 || mDim == 16 || mDim == 4) ||
