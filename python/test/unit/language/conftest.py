8a26ba18d568fabd1c05dddfe691b0b091f35639
Bert Maher <bertmaher@users.noreply.github.com>
2024-03-10 18:45:53 -0400

    [tests] Move --device argument up a level (#3267)
    When trying to run the unit tests from the top level directory, was
    getting:
    ```
    ValueError: option names {'--device'} already added
    ```
    since this was defined in both `operators` and `language` tests.

--- python/test/unit/language/conftest.py
+++ python/test/unit/language/conftest.py
@@ -1,16 +1,5 @@
 # content of conftest.py
 
-import pytest
-
-
-def pytest_addoption(parser):
-    parser.addoption("--device", action="store", default='cuda')
-
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "interpreter: indicate whether interpreter supports the test")
-
-
-@pytest.fixture
-def device(request):
-    return request.config.getoption("--device")
