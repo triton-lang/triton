72cba380aa64336c90cd98fc9c74cc5a5b205e05
Lixun Zhang <Lixun.Zhang@amd.com>
2024-03-10 18:49:29 -0500

    [AMD] Add amd f8 datatype (#3322)
    Upstream https://github.com/ROCm/triton/pull/357

--- python/src/ir.cc
+++ python/src/ir.cc
@@ -712,6 +712,10 @@ void init_triton_ir(py::module &&m) {
            [](TritonOpBuilder &self) -> Type {
              return self.getBuilder().getType<Float8E4M3FNUZType>();
            })
+      .def("get_fp8e4b8_ty",
+           [](TritonOpBuilder &self) -> mlir::Type {
+             return self.getBuilder().getType<mlir::Float8E4M3FNUZType>();
+           })
       .def("get_fp8e4b15_ty",
            [](TritonOpBuilder &self) -> Type {
              // TODO: upstream FP8E4B15 into MLIR, or find a way to externally
@@ -727,6 +731,10 @@ void init_triton_ir(py::module &&m) {
       .def("get_fp8e5_ty",
            [](TritonOpBuilder &self) -> Type {
              return self.getBuilder().getType<Float8E5M2Type>();
+           })
+      .def("get_fp8e5b16_ty",
+           [](TritonOpBuilder &self) -> mlir::Type {
+             return self.getBuilder().getType<mlir::Float8E5M2FNUZType>();
            })
       .def("get_half_ty",
            [](TritonOpBuilder &self) -> Type {
