da3b55690184adf6b7b8ced2e3144074eabc6411
Pawel Szczerbuk <pawel.szczerbuk@openai.com>
2024-03-13 09:36:29 -0700

    Fixing interpreter

--- python/triton/runtime/interpreter.py
+++ python/triton/runtime/interpreter.py
@@ -673,11 +673,11 @@ def _patch_lang(fn):
 # TODO: wrap everything in triton tensors
 def _implicit_cvt(arg):
     if isinstance(arg, int):
-        ty = tl.str_to_ty(arg.mangle_type())
+        ty = tl.str_to_ty(triton.runtime.jit.JITFunction._type_of(triton.runtime.jit.JITFunction._key_of(arg)))
         handle = TensorHandle(np.array([arg], dtype=np.int32), ty)
         return tl.tensor(handle, ty)
     if hasattr(arg, "data_ptr"):
-        ty = tl.str_to_ty(arg.mangle_type())
+        ty = tl.str_to_ty(triton.runtime.jit.JITFunction._type_of(triton.runtime.jit.JITFunction._key_of(arg)))
         handle = TensorHandle(np.array([arg.data_ptr()], dtype=np.uint64), ty)
         return tl.tensor(handle, ty)
     return arg
