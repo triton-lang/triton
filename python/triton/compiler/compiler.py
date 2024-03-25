c2f615fa945d1b09539fc3169fc3ed5ff925f194
Michael Voznesensky <voznesenskym@gmail.com>
2024-03-25 13:27:28 -0700

    Fix mixed key constants breaking sorted_constants for hashing ASTSource (#3434)
    Fixes bug introduced in:
    https://github.com/openai/triton/commit/2b6762c15c3d42fa92ed60d20c9fd058855ddf85
    
    Reproed via dynamo with a manually updated pin 
    `test_functions.py` running test `test_triton_kernel_various_args`
    creates constants of ` {'RANDOM_SIZE': 0, 5: 128}` which breaks this
    hashing.
    
    A simple step to first string the keys, and then sort works.
    
    cc: @bertmaher @jlebar @ptillet

--- python/triton/compiler/compiler.py
+++ python/triton/compiler/compiler.py
@@ -102,7 +102,9 @@ class ASTSource:
 
     def hash(self):
         sorted_sig = [v for k, v in sorted(self.signature.items())]
-        sorted_constants = [(k, v) for k, v in sorted(self.constants.items())]
+        # Note - we stringify the keys here to allow sorting to work for cases
+        # where constants have mixed int/str keys.
+        sorted_constants = sorted((str(k), v) for k, v in self.constants.items())
         key = f"{self.fn.cache_key}-{self.attrs.hash()}-{sorted_sig}-{sorted_constants}"
         return hashlib.sha256(key.encode("utf-8")).hexdigest()
 
