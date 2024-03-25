triton-lang/triton

commit 1ecbc27a8169cd47ef81e061789f05b913a740b1

[FRONTEND] Sort signature/constants when hashing (#3258)
I'm trying to maximize the number of (distributed) cache hits I'm
getting on kernels, and dictionary iteration order seems like a possible
randomizing factor.

I don't think there's *technically* a problem here, because Python now
guarantees insertion ordering and I don't see a way the ordering here
could vary, since they're argument-index ordered.

But it also feels like a reasonable sort of belt-and-suspenders
protection against this being randomized, and this sort should be
low-cost since it's just done at kernel compile time.

python/triton/compiler/compiler.py
@@ -101,7 +101,9 @@ class ASTSource:
             self.attrs = AttrsDescriptor()
 
     def hash(self):
-        key = f"{self.fn.cache_key}-{self.attrs.hash()}-{self.signature.values()}-{self.constants}"
+        sorted_sig = [v for k, v in sorted(self.signature.items())]
+        sorted_constants = [(k, v) for k, v in sorted(self.constants.items())]
+        key = f"{self.fn.cache_key}-{self.attrs.hash()}-{sorted_sig}-{sorted_constants}"
         return hashlib.sha256(key.encode("utf-8")).hexdigest()
 
     def make_ir(self, options, context):

commit 65937252f09adc4051224af0aefea664268b8c7c

Fix mixed key constants breaking sorted_constants for hashing ASTSource (#3434)
Fixes bug introduced in:
https://github.com/openai/triton/commit/1ecbc27a8169cd47ef81e061789f05b913a740b1

Reproed via dynamo with a manually updated pin 
`test_functions.py` running test `test_triton_kernel_various_args`
creates constants of ` {'RANDOM_SIZE': 0, 5: 128}` which breaks this
hashing.

A simple step to first string the keys, and then sort works.

cc: @bertmaher @jlebar @ptillet

python/triton/compiler/compiler.py
@@ -102,7 +102,9 @@ class ASTSource:
 
     def hash(self):
         sorted_sig = [v for k, v in sorted(self.signature.items())]
-        sorted_constants = [(k, v) for k, v in sorted(self.constants.items())]
+        # Note - we stringify the keys here to allow sorting to work for cases
+        # where constants have mixed int/str keys.
+        sorted_constants = sorted((str(k), v) for k, v in self.constants.items())
         key = f"{self.fn.cache_key}-{self.attrs.hash()}-{sorted_sig}-{sorted_constants}"
         return hashlib.sha256(key.encode("utf-8")).hexdigest()
 