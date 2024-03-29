cc2dc4a1e0edfe830fd9adac1ab5f7c3788496ce
Lei Zhang <antiagainst@gmail.com>
2024-03-29 06:32:51 +0000

    [backend][amd] Format C/C++ code and turn on presubmit check

--- third_party/amd/lib/TritonAMDGPUToLLVM/DotOpToLLVM/MFMA.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/DotOpToLLVM/MFMA.cpp
@@ -269,7 +269,8 @@ struct DotOpMFMAConversionHelper {
         Type ty = vec_ty(elemTy, kWidth);
         Value rawElems = undef(ty);
         for (int k = 0; k < kWidth; ++k) {
-          rawElems = insert_element(ty, rawElems, elems[kWidth * (n1 * i + j) + k], i32_val(k));
+          rawElems = insert_element(
+              ty, rawElems, elems[kWidth * (n1 * i + j) + k], i32_val(k));
         }
 
         Value convertedElems;
