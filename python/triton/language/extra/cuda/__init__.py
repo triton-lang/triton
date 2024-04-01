88542e5010ea9ffc1345a7b2c8b19737e5321e35
Thomas Raoux <thomas.raoux@openai.com>
2024-04-01 16:16:05 -0700

    [FRONTEND] Fix float8 sw conversion for V100

--- python/triton/language/extra/cuda/__init__.py
+++ python/triton/language/extra/cuda/__init__.py
@@ -1,5 +1,8 @@
 from . import libdevice
 
-from .utils import (globaltimer, num_threads, num_warps, smid, convert_custom_float8)
+from .utils import (globaltimer, num_threads, num_warps, smid, convert_custom_float8_sm70, convert_custom_float8_sm80)
 
-__all__ = ["libdevice", "globaltimer", "num_threads", "num_warps", "smid", "convert_custom_float8"]
+__all__ = [
+    "libdevice", "globaltimer", "num_threads", "num_warps", "smid", "convert_custom_float8_sm70",
+    "convert_custom_float8_sm80"
+]
