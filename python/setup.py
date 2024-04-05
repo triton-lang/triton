79c5b549f30549653b6eb9ed73416d1b4d15cdb1
Faycel Kouteib <faycel.kouteib@gmail.com>
2024-04-05 13:46:56 -0700

    Re-apply fixes post merge conflict. Update docs for cpython 3.12 support.

--- python/setup.py
+++ python/setup.py
@@ -484,6 +484,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     test_suite="tests",
     extras_require={
