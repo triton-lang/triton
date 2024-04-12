ea8f37244f8df95a3c85ddbf10d816792d15484a
Jokeren <robinho364@gmail.com>
2024-04-12 15:09:17 -0400

    Update

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -5031,7 +5031,6 @@ def test_fp8_dot_acc(in_type_str, low_precision_acc, device):
     BLOCK_M, BLOCK_N, BLOCK_K = 128, 256, 128
     A = numpy_random((M, K), dtype_str=in_type_str)
     B = numpy_random((K, N), dtype_str=in_type_str)
-    Bt = B.T
     C = torch.empty((M, N), dtype=torch.float32, device=device)
     num_warps = 8
     a = to_triton(A, device=device, dst_type=in_type_str)
@@ -5039,10 +5038,10 @@ def test_fp8_dot_acc(in_type_str, low_precision_acc, device):
     grid = (triton.cdiv(M, BLOCK_M), 1)
     matmul_kernel[grid](a, b, C, M, N, K, a.stride(0), a.stride(1), b.stride(0), b.stride(1), C.stride(0), C.stride(1),
                         BLOCK_M, BLOCK_N, BLOCK_K, low_precision_acc, num_warps=num_warps)
-    torch_a = torch.from_numpy(A)
-    th_a = f8_to_f16(torch_a if is_interpreter() else torch_a.cuda(), in_type_str)
-    torch_b = torch.from_numpy(B)
-    th_b = f8_to_f16(torch_b if is_interpreter() else torch_b.cuda(), in_type_str)
+    torch_a = torch.from_numpy(A).to(device=device)
+    th_a = f8_to_f16(torch_a, in_type_str)
+    torch_b = torch.from_numpy(B).to(device=device)
+    th_b = f8_to_f16(torch_b, in_type_str)
     ref_out = torch.matmul(th_a, th_b).to(torch.float32)
     if in_type_str == 'float8e4nv':
         torch.testing.assert_close(ref_out, C, rtol=0.01, atol=0.01)
