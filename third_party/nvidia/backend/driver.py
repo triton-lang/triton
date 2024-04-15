2319a6dee43ad76c8c7ec2f9169a8fdebff1c455
Thomas Raoux <thomas.raoux@openai.com>
2024-04-14 22:46:49 -0700

    [BACKEND] Add TTGIR operations to support TMA load
    This adds the low level pieces to support TMA load in triton. This is
    not hooked to the front end yet but can be tested with TTGIR test.

--- third_party/nvidia/backend/driver.py
+++ third_party/nvidia/backend/driver.py
@@ -81,6 +81,7 @@ class CudaUtils(object):
         self.get_device_properties = mod.get_device_properties
         self.cuOccupancyMaxActiveClusters = mod.cuOccupancyMaxActiveClusters
         self.set_printf_fifo_size = mod.set_printf_fifo_size
+        self.fill1DTMADescriptor = mod.fill1DTMADescriptor
 
 
 # ------------------------
