305ca0e944cbf60fa925ca836db4d85c23ae7bf8
Pawel Szczerbuk <pawel.szczerbuk@openai.com>
2024-04-25 17:27:02 -0700

    Bringing back reverted change and limiting the hack to ReduceOp

--- lib/Dialect/TritonGPU/Transforms/RemoveLayoutConversions.cpp
+++ lib/Dialect/TritonGPU/Transforms/RemoveLayoutConversions.cpp
@@ -13,6 +13,7 @@
 #include "mlir/Transforms/Passes.h"
 #include "mlir/Transforms/RegionUtils.h"
 #include "triton/Analysis/Utility.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Passes.h"
 #include "triton/Dialect/TritonGPU/Transforms/TritonGPUConversion.h"
@@ -28,6 +29,8 @@
 
 namespace mlir::triton::gpu {
 namespace {
+
+namespace tt = mlir::triton;
 
 // -----------------------------------------------------------------------------
 //
@@ -207,6 +210,26 @@ bool hasConvertToMMATransisitiveUse(Operation *op, Attribute encoding) {
     queue.pop_back();
     getForwardSlice(currentValue, &forwardSlice);
     for (Operation *op : forwardSlice) {
+      // HACK: Stop propagation if the op is using mma layout but is producing
+      // tensor smaller than the layout we would like to propagate. This is to
+      // avoid stepping into the known bug.
+      if (isa<tt::ReduceOp>(op)) {
+        for (auto result : op->getResults()) {
+          auto tensorType = result.getType().dyn_cast<RankedTensorType>();
+          if (tensorType &&
+              tensorType.getEncoding().isa<NvidiaMmaEncodingAttr>()) {
+            auto mmaInstrShape =
+                encoding.cast<NvidiaMmaEncodingAttr>().getInstrShape();
+            if (tensorType.getShape()[tensorType.getRank() - 2] <
+                    mmaInstrShape[0] ||
+                tensorType.getShape()[tensorType.getRank() - 1] <
+                    mmaInstrShape[1]) {
+              return false;
+            }
+          }
+        }
+      }
+
       if (auto convertOp = dyn_cast<ConvertLayoutOp>(op)) {
         Attribute dstEncoding = convertOp.getType().getEncoding();
         if (auto mmaLayout = dstEncoding.dyn_cast<NvidiaMmaEncodingAttr>())
@@ -220,12 +243,21 @@ bool hasConvertToMMATransisitiveUse(Operation *op, Attribute encoding) {
       auto yield = dyn_cast<scf::YieldOp>(op);
       if (!yield)
         continue;
+      if (auto ifOp = dyn_cast<scf::IfOp>(yield->getParentOp())) {
+        for (OpOperand &operand : yield->getOpOperands()) {
+          Operation *def = operand.get().getDefiningOp();
+          if (def &&
+              (forwardSlice.count(def) || operand.get() == currentValue) &&
+              (seen.insert(operand.get()).second == true))
+            queue.push_back(ifOp.getResult(operand.getOperandNumber()));
+        }
+      }
       auto forOp = dyn_cast<scf::ForOp>(yield.getOperation()->getParentOp());
       if (!forOp)
         continue;
       for (OpOperand &operand : yield->getOpOperands()) {
         Operation *def = operand.get().getDefiningOp();
-        if (def && forwardSlice.count(def) &&
+        if (def && (forwardSlice.count(def) || operand.get() == currentValue) &&
             (seen.insert(operand.get()).second == true))
           queue.push_back(forOp.getRegionIterArg(operand.getOperandNumber()));
       }
