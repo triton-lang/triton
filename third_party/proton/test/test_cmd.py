f8461a3a8e66c2fdeb017397984ea63a356b2558
Jokeren <robinho364@gmail.com>
2024-04-29 13:41:13 -0400

    Update

--- third_party/proton/test/test_cmd.py
+++ third_party/proton/test/test_cmd.py
@@ -1,4 +1,6 @@
 import subprocess
+import tempfile
+import json
 
 
 def test_help():
@@ -8,8 +10,13 @@ def test_help():
 
 
 def test_python():
-    ret = subprocess.check_call(["proton", "-n", "test", "helper.py"])
-    assert ret == 0
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".hatchet") as f:
+        ret = subprocess.check_call(["proton", "-n", "test", "helper.py"])
+        assert ret == 0
+        data = json.load(f)
+        kernels = data[0]["children"]
+        assert len(kernels) == 2
+        assert kernels[1]["frame"]["name"] == "test"
 
 
 def test_pytest():
