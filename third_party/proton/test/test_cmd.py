f3b22faa3c86615faec00eda254ef13076111029
Jokeren <robinho364@gmail.com>
2024-04-29 22:22:17 -0400

    Update

--- third_party/proton/test/test_cmd.py
+++ third_party/proton/test/test_cmd.py
@@ -12,16 +12,17 @@ def test_help():
 
 @pytest.mark.parametrize("mode", ["script", "python", "pytest"])
 def test_exec(mode):
+    file_path = __file__
+    helper_file = file_path.replace("test_cmd.py", "helper.py")
     with tempfile.NamedTemporaryFile(delete=True, suffix=".hatchet") as f:
         name = f.name.split(".")[0]
         if mode == "script":
-            ret = subprocess.check_call(["proton", "-n", name, "helper.py", "test"], stdout=subprocess.DEVNULL)
+            ret = subprocess.check_call(["proton", "-n", name, helper_file, "test"], stdout=subprocess.DEVNULL)
         elif mode == "python":
-            ret = subprocess.check_call(["python", "-m", "triton.profiler.proton", "-n", name, "helper.py", "test"],
+            ret = subprocess.check_call(["python", "-m", "triton.profiler.proton", "-n", name, helper_file, "test"],
                                         stdout=subprocess.DEVNULL)
         elif mode == "pytest":
-            ret = subprocess.check_call(["proton", "-n", name, "pytest", "helper.py", "test"],
-                                        stdout=subprocess.DEVNULL)
+            ret = subprocess.check_call(["proton", "-n", name, "pytest", helper_file], stdout=subprocess.DEVNULL)
         assert ret == 0
         data = json.load(f, )
         kernels = data[0]["children"]
