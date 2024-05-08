2c6c5533845c6c4774ea6916047c3117c8ba131f
ravil-mobile <ravil.aviva.com@gmail.com>
2024-05-08 17:41:37 +0000

    Refactored `extra.libdevice`

--- python/triton/language/extra/__init__.py
+++ python/triton/language/extra/__init__.py
@@ -1,3 +1,13 @@
-from . import cuda
+from ...runtime.driver import driver
 
-__all__ = ['cuda']
+backend = driver.active.get_current_target().backend
+if backend== 'cuda':
+  from .cuda import libdevice
+  from . import cuda
+  __all__ = ['cuda']
+elif backend == 'hip':
+  from .hip import libdevice
+  from . import hip
+  __all__ = ['hip']
+else:
+  raise RuntimeError('unknown backend')
