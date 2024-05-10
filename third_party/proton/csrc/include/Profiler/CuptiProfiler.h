88de56024253503cb6d941860d6b3d535d48cbf6
Keren Zhou <kerenzhou@openai.com>
2024-05-09 21:49:46 -0400

    Update third_party/proton/csrc/include/Profiler/CuptiProfiler.h
    Co-authored-by: Justin Lebar <justin.lebar@gmail.com>

--- third_party/proton/csrc/include/Profiler/CuptiProfiler.h
+++ third_party/proton/csrc/include/Profiler/CuptiProfiler.h
@@ -29,7 +29,7 @@ protected:
   void doStop() override;
 
 private:
-  // pimpl-idiom to hide the implementation details
+  // Use the pimpl idiom to hide the implementation details.  This lets us avoid including the cupti header from this header.  The cupti header and the equivalent header from AMD define conflicting macros, so we want to use those headers only within cc files.
   struct CuptiCallback;
   std::unique_ptr<CuptiCallback> cuptiCallback;
 };
