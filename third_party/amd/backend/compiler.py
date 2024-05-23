8fbe47859fe26adfc7ec2f3d8ea8a21558b85a7d
Sijia Chen <sijiac@meta.com>
2024-05-23 11:32:00 -0700

    check the TRITON_HIP_LLD_PATH first

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -85,6 +85,12 @@ class HIPBackend(BaseBackend):
     @staticmethod
     def path_to_rocm_lld():
         # First check backend for ld.lld (used for pytorch wheels)
+        lld_env_path = os.getenv("TRITON_HIP_LLD_PATH")
+        if lld_env_path is not None:
+            lld = Path(lld_env_path)
+            if lld.is_file():
+                return lld
+
         lld = Path(__file__).parent / "llvm/bin/ld.lld"
         if lld.is_file():
             return lld
@@ -94,10 +100,6 @@ class HIPBackend(BaseBackend):
         lld = Path("/usr/bin/ld.lld")
         if lld.is_file():
             return lld
-        if "TRITON_HIP_LLD_PATH" in os.environ:
-            lld = Path(os.environ["TRITON_HIP_LLD_PATH"])
-            if lld.is_file():
-                return lld
         raise Exception("ROCm linker /opt/rocm/llvm/bin/ld.lld not found")
 
     @staticmethod
