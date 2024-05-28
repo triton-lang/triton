fbdf6719bf3580667bd776764838a7f3913e648f
jungpark-mlir <jungwook.park@amd.com>
2024-05-28 14:51:42 +0000

    replace the last wave in comment

--- lib/Dialect/TritonGPU/IR/Dialect.cpp
+++ lib/Dialect/TritonGPU/IR/Dialect.cpp
@@ -1551,7 +1551,7 @@ AMDMfmaEncodingAttr::getMFMAInstrShapeForOperands(int kWidth, int opIdx) const {
   unsigned nDim = getNDim();
   assert((mDim == nDim) && (mDim == 32 || mDim == 16 || mDim == 4) ||
          (mDim == 64 && nDim == 4) || (mDim == 4 && nDim == 64));
-  constexpr int warpSize = 64; // MFMA is used on wave64 architectures only
+  constexpr int warpSize = 64; // MFMA is always based on the 64-wide warps.
   int kGroups = -1;
   if (mDim == nDim)
     kGroups = warpSize / mDim;
