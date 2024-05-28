f69beb80987bd94b4e28f87db31fde0688cccecb
Minjang Kim <minjang@meta.com>
2024-05-28 14:49:37 -0700

    [BACKEND][CPU] Make it buildable and runnable in a different environment (#8)
    * [BACKEND][CPU] Make it buildable and runnable in a different environment
    
    * Revert seemingly inconsistent python code formatting

--- python/triton/runtime/build.py
+++ python/triton/runtime/build.py
@@ -47,6 +47,9 @@ def _build(name, src, srcdir, library_dirs, include_dirs, libraries):
     cc_cmd += [f'-l{lib}' for lib in libraries]
     cc_cmd += [f"-L{dir}" for dir in library_dirs]
     cc_cmd += [f"-I{dir}" for dir in include_dirs if dir is not None]
+    # CPU backend uses C++ (driver.cpp). Some old version compilers need a specific C++17 flag.
+    if src.endswith(".cpp") or src.endswith(".cc"):
+        cc_cmd += ["-std=c++17"]
     ret = subprocess.check_call(cc_cmd)
     if ret == 0:
         return so
