328b86d726de9179559823050f0baa11eebc6c4e
Keren Zhou <kerenzhou@openai.com>
2024-06-04 13:57:33 -0400

    [PROTON] Refactor GPU profilers (#4056)
    1. Extract duplicated code into GPUProfiler.h
    2. Track finished correlation ids for both cupti and amd profilers

--- third_party/proton/csrc/include/Profiler/CuptiProfiler.h
+++ third_party/proton/csrc/include/Profiler/CuptiProfiler.h
@@ -1,40 +1,17 @@
 #ifndef PROTON_PROFILER_CUPTI_PROFILER_H_
 #define PROTON_PROFILER_CUPTI_PROFILER_H_
 
-#include "Context/Context.h"
-#include "Profiler.h"
-
-#include <atomic>
-#include <map>
+#include "GPUProfiler.h"
 
 namespace proton {
 
-class CuptiProfiler : public Profiler,
-                      public OpInterface,
-                      public Singleton<CuptiProfiler> {
+class CuptiProfiler : public GPUProfiler<CuptiProfiler> {
 public:
   CuptiProfiler();
   virtual ~CuptiProfiler();
 
-protected:
-  // OpInterface
-  void startOp(const Scope &scope) override final;
-  void stopOp(const Scope &scope) override final;
-  void setOpInProgress(bool value) override final;
-  bool isOpInProgress() override final;
-
-  // Profiler
-  void doStart() override;
-  void doFlush() override;
-  void doStop() override;
-
 private:
-  // Use the pimpl idiom to hide the implementation details. This lets us avoid
-  // including the cupti header from this header. The cupti header and the
-  // equivalent header from AMD define conflicting macros, so we want to use
-  // those headers only within cc files.
   struct CuptiProfilerPimpl;
-  std::unique_ptr<CuptiProfilerPimpl> pImpl;
 };
 
 } // namespace proton
