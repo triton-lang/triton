bfcf45ecdd39a1234b419a7bad8d490b674ba0dc
Jokeren <robinho364@gmail.com>
2024-06-07 11:05:20 -0400

    Lint

--- third_party/proton/csrc/lib/Driver/GPU/HsaApi.cpp
+++ third_party/proton/csrc/lib/Driver/GPU/HsaApi.cpp
@@ -14,8 +14,8 @@ struct ExternLibHsa : public ExternLibBase {
 
 void *ExternLibHsa::lib = nullptr;
 
-DEFINE_DISPATCH(ExternLibHsa, agentGetInfo, hsa_agent_get_info,
-                hsa_agent_t, hsa_agent_info_t, void *);
+DEFINE_DISPATCH(ExternLibHsa, agentGetInfo, hsa_agent_get_info, hsa_agent_t,
+                hsa_agent_info_t, void *);
 
 hsa_status_t iterateAgents(hsa_status_t (*callback)(hsa_agent_t agent,
                                                     void *data),
