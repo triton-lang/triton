2875c871241c146660ab8710b6fa2ed689585ce6
pawelszczerbuk <153013546+pawelszczerbuk@users.noreply.github.com>
2024-06-07 09:21:08 -0700

    Minimal triton cuBLAS wrapper for fp8 matmul (#4085)
    This is a *minimal* wrapper around cuBLAS library that allows for
    testing Nvidia's fp8 matmul implementation with the environment
    consistent with other triton tests.
    Being minimal, there are some tradeoffs that we may potentially want to
    address in the future:
    - When creating cublasLt instance the user needs to provide a
    "workspace" buffer used by cuBLAS as a scratchpad memory. This
    allocation could have been handled internally by the implementation,
    however it would require linking against CUDA runtime, which is
    completely avoided in the current implementation.
    - To avoid downloading cuBLAS includes and their dependencies (libcu++)
    I've added a file with necessary declarations. This isn't so bad at the
    moment, but if the wrapper is to be expanded beyond simple matmul
    functionality this may need to be revised.
    - Wrapper does not perform dimensionality checks on the matrices, while
    requiring that matrix B is transposed. This is because to avoid adding
    dependency to torch the interface accepts raw tensor device pointers,
    instead of torch tensors with rich metadata.

---
+++ python/test/unit/runtime/test_cublas.py
@@ -0,0 +1,45 @@
+import pytest
+import torch
+import triton
+import os
+
+
+def is_interpreter():
+    return os.environ.get('TRITON_INTERPRET', '0') == '1'
+
+
+def is_cuda():
+    return not is_interpreter() and \
+        triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+
+@pytest.mark.parametrize("m, n, k", [(16, 16, 16), (32, 16, 16), (16, 32, 16), (16, 16, 32)])
+def test_cublas_fp8(m, n, k, device):
+    if not (is_cuda() and torch.cuda.get_device_capability()[0] >= 9):
+        pytest.skip("test_cublas_fp8 is only supported on CUDA with cc >= 90")
+
+    from triton._C.libtriton import nvidia
+
+    torch.manual_seed(123)
+    workspace_size = 32 * 1024 * 1024
+
+    def limited_rand(elements, shape):
+        total_elems = torch.prod(torch.tensor(shape)).item()
+        indices = torch.randint(0, len(elements), (total_elems, ), device=device)
+        return elements[indices].view(shape)
+
+    elements = torch.tensor([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=torch.float32, device=device)
+    a = limited_rand(elements, (m, k)).to(torch.float8_e4m3fn)
+    b = limited_rand(elements, (k, n)).to(torch.float8_e4m3fn)
+    c = torch.zeros((m, n), dtype=torch.float8_e4m3fn, device=device)
+
+    b = b.T.contiguous()
+
+    workspace = torch.empty(workspace_size, dtype=torch.int8, device=device)
+
+    cublas = nvidia.cublas.CublasLt(workspace)
+    cublas.fp8_matmul(a, b, c)
+
+    ref = torch.matmul(a.to(torch.float16), b.to(torch.float16).T)
+
+    assert torch.allclose(c.to(torch.float16), ref, atol=2.0)
