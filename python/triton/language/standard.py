4f902295a4def98383d91a0aad9c2febccf13744
谢双镱 <xieshuangyi@bytedance.com>
2024-06-11 15:39:06 +0800

    fix: `interleave` does not need to check shape

--- python/triton/language/standard.py
+++ python/triton/language/standard.py
@@ -437,7 +437,7 @@ def flip(x, dim=None):
 def interleave(a, b):
     """
     Interleaves the values of two tensors along their last dimension. The two tensors must have the same shape.
-    Equivalent to `tl.join(a, b).reshape(a.shape[-1:] + [2 * a.shape[-1]])`
+    Equivalent to `tl.join(a, b).reshape(a.shape[:-1] + [2 * a.shape[-1]])`
 
     :param a: The first input tensor.
     :type a: Tensor
@@ -446,7 +446,6 @@ def interleave(a, b):
     """
     c = core.join(a, b)
 
-    assert isinstance(c.shape, list)
     if len(c.shape) == 1:
         # We must have interleaved two scalars.
         return c
