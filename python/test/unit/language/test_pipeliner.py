b972811314f5fbeddc19e5df6041d752ea03bf43
Jokeren <robinho364@gmail.com>
2024-06-19 13:12:09 -0400

    Update

--- python/test/unit/language/test_pipeliner.py
+++ python/test/unit/language/test_pipeliner.py
@@ -4,10 +4,15 @@ import pytest
 import torch
 import triton
 import triton.language as tl
+import numpy as np
 
 
 def is_cuda():
     return triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+
+def is_cuda_tma_available():
+    return is_cuda() and torch.cuda.get_device_capability()[0] >= 9
 
 
 def is_hip():
@@ -33,32 +38,57 @@ def matmul_kernel(  #
         stride_am, stride_ak,  #
         stride_bk, stride_bn,  #
         stride_cm, stride_cn,  #
-        BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,  #
+        BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr,  #
         NUM_STAGES: tl.constexpr):
     pid = tl.program_id(axis=0)
-    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_m = tl.cdiv(M, BLOCK_M)
     pid_m = pid % num_pid_m
     pid_n = pid // num_pid_m
-    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
-    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
-    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    offs_am = (pid_m * BLOCK_M + tl.arange(0, BLOCK_M)) % M
+    offs_bn = (pid_n * BLOCK_N + tl.arange(0, BLOCK_N)) % N
+    offs_k = tl.arange(0, BLOCK_K)
     a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
     b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
-    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
-    for k in tl.range(0, tl.cdiv(K, BLOCK_SIZE_K), num_stages=NUM_STAGES):
-        mask_a = (offs_am[:, None] < M) & (offs_k[None, :] + k * BLOCK_SIZE_K < K)
-        mask_b = ((offs_k[:, None] + k * BLOCK_SIZE_K) < K) & (offs_bn[None, :] < N)
+    accumulator = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
+    for k in tl.range(0, tl.cdiv(K, BLOCK_K), num_stages=NUM_STAGES):
+        mask_a = (offs_am[:, None] < M) & (offs_k[None, :] + k * BLOCK_K < K)
+        mask_b = ((offs_k[:, None] + k * BLOCK_K) < K) & (offs_bn[None, :] < N)
         a = tl.load(a_ptrs, mask=mask_a, other=0)
         b = tl.load(b_ptrs, mask=mask_b, other=0)
         accumulator = tl.dot(a, b, acc=accumulator)
-        a_ptrs += BLOCK_SIZE_K * stride_ak
-        b_ptrs += BLOCK_SIZE_K * stride_bk
+        a_ptrs += BLOCK_K * stride_ak
+        b_ptrs += BLOCK_K * stride_bk
     accumulator = accumulator.to(tl.float16)
-    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    offs_cm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_cn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
     mask_c = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
     output_ptrs = output_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
     tl.store(output_ptrs, accumulator, mask=mask_c)
+
+
+@triton.jit
+def matmul_kernel_tma(  #
+        a_ptr, b_ptr, output_ptr,  #
+        M, N, K,  #
+        BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr,  #
+        NUM_STAGES: tl.constexpr):
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_M)
+    pid_m = pid % num_pid_m
+    pid_n = pid // num_pid_m
+    offs_am = (pid_m * BLOCK_M) % M
+    offs_bn = (pid_n * BLOCK_N) % N
+    offs_am = tl.multiple_of(offs_am, BLOCK_M)
+    offs_bn = tl.multiple_of(offs_bn, BLOCK_N)
+    offs_k = 0
+    accumulator = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
+    for _ in tl.range(0, tl.cdiv(K, BLOCK_K), num_stages=NUM_STAGES):
+        a = tl._experimental_descriptor_load(a_ptr, [offs_am, offs_k], [BLOCK_M, BLOCK_K], tl.float16)
+        b = tl._experimental_descriptor_load(b_ptr, [offs_k, offs_bn], [BLOCK_K, BLOCK_N], tl.float16)
+        accumulator = tl.dot(a, b, acc=accumulator)
+        offs_k += BLOCK_K
+    accumulator = accumulator.to(tl.float16)
+    tl._experimental_descriptor_store(output_ptr, accumulator, [offs_am, offs_bn])
 
 
 @triton.jit
@@ -84,8 +114,28 @@ def test_pipeline_matmul(device):
     b = torch.randn(K, N, device=device, dtype=torch.float16)
     output = torch.empty((M, N), dtype=torch.float16, device=device)
     grid = (triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N), 1)
-    handler = matmul_kernel[grid](a, b, output, M, N, K, a.stride(0), a.stride(1), b.stride(0), b.stride(1),
-                                  output.stride(0), output.stride(1), BLOCK_M, BLOCK_N, BLOCK_K, NUM_STAGES=NUM_STAGES)
+    if is_cuda_tma_available():
+        TMA_SIZE = 128
+
+        desc_a = np.empty(TMA_SIZE, dtype=np.int8)
+        desc_b = np.empty(TMA_SIZE, dtype=np.int8)
+        desc_output = np.empty(TMA_SIZE, dtype=np.int8)
+        triton.runtime.driver.active.utils.fill_2d_tma_descriptor(a.data_ptr(), M, K, BLOCK_M, BLOCK_K,
+                                                                  a.element_size(), desc_a)
+        triton.runtime.driver.active.utils.fill_2d_tma_descriptor(b.data_ptr(), K, N, BLOCK_K, BLOCK_N,
+                                                                  b.element_size(), desc_b)
+        triton.runtime.driver.active.utils.fill_2d_tma_descriptor(output.data_ptr(), M, N, BLOCK_M, BLOCK_N,
+                                                                  output.element_size(), desc_output)
+
+        a_tma = torch.tensor(desc_a, device=device)
+        b_tma = torch.tensor(desc_b, device=device)
+        output_tma = torch.tensor(desc_output, device=device)
+        handler = matmul_kernel_tma[grid](a_tma, b_tma, output_tma, M, N, K, BLOCK_M, BLOCK_N, BLOCK_K,
+                                          NUM_STAGES=NUM_STAGES)
+    else:
+        handler = matmul_kernel[grid](a, b, output, M, N, K, a.stride(0), a.stride(1), b.stride(0), b.stride(1),
+                                      output.stride(0), output.stride(1), BLOCK_M, BLOCK_N, BLOCK_K,
+                                      NUM_STAGES=NUM_STAGES)
     ref_out = torch.matmul(a, b)
     atol = 1e-2 if is_hip() else None
     # Bigger tolerance for AMD MI200 devices.
@@ -95,18 +145,27 @@ def test_pipeline_matmul(device):
     torch.testing.assert_close(ref_out, output, atol=atol, rtol=rtol)
     if is_cuda():
         ttgir = handler.asm["ttgir"]
-        # 1. check async
-        assert ttgir.count("triton_gpu.async_copy_global_to_local") != 0, "async copy not found"
-        # 2. check number of stages
-        assert ttgir.count(f"num = {NUM_STAGES} : i32") != 0, "num_stages not match"
-        # 3. check alloc
-        assert ttgir.count("triton_gpu.local_alloc") == 2, "alloc number not match"
-        # 4. check dot
-        cc = torch.cuda.get_device_capability()
-        if cc[0] >= 9:
-            ttgir.count("triton_nvidia_gpu.warp_group_dot") != 0, "warp_group_dot not found"
+        if is_cuda_tma_available():
+            assert ttgir.count("triton_nvidia_gpu.async_tma_copy_global_to_local") != 0, "async tma copy not found"
+            assert ttgir.count(f"num = {NUM_STAGES} : i32") == 0, "num_stages not match"
+            # a_tma, b_tma, output_tma, barriar
+            assert ttgir.count("triton_gpu.local_alloc") == 4, "alloc number not match"
+            assert ttgir.count("triton_nvidia_gpu.barrier_expect") != 0, "barrier_expect not found"
+            assert ttgir.count("triton_nvidia_gpu.wait_barrier") != 0, "wait_barrier not found"
+            assert ttgir.count("triton_nvidia_gpu.warp_group_dot") != 0, "warp_group_dot not found"
         else:
-            ttgir.count("triton_gpu.dot") != 0, "dot not found"
+            # 1. check async
+            assert ttgir.count("triton_gpu.async_copy_global_to_local") != 0, "async copy not found"
+            # 2. check number of stages
+            assert ttgir.count(f"num = {NUM_STAGES} : i32") != 0, "num_stages not match"
+            # 3. check alloc
+            assert ttgir.count("triton_gpu.local_alloc") == 2, "alloc number not match"
+            # 4. check dot
+            cc = torch.cuda.get_device_capability()
+            if cc[0] >= 9:
+                ttgir.count("triton_nvidia_gpu.warp_group_dot") != 0, "warp_group_dot not found"
+            else:
+                ttgir.count("triton_gpu.dot") != 0, "dot not found"
 
 
 def test_pipeline_vecadd(device):
