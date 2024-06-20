25062100cb0321d6093cb5a20a3183053e57afcd
Ilya Enkovich <ilya.enkovich@intel.com>
2024-06-20 18:51:53 -0500

    Support tt.split for CPU. (#30)
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -1995,6 +1995,7 @@ def test_interleave_scalars(device):
     np.testing.assert_equal([10, 20], to_numpy(z))
 
 
+@pytest.mark.cpu
 @pytest.mark.interpreter
 def test_split(device):
 
@@ -2017,6 +2018,7 @@ def test_split(device):
     np.testing.assert_equal(to_numpy(z2_ref), to_numpy(z2))
 
 
+@pytest.mark.cpu
 @pytest.mark.interpreter
 def test_split_to_scalar(device):
 
