f3093c311f01908bc7cef5fb85de06738d537363
Ilya Enkovich <ilya.enkovich@intel.com>
2024-06-20 14:09:54 -0500

    Move byte manipulation ops from elwise ops conversion. (#28)
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>

--- third_party/cpu/lib/TritonToTritonCPU/ConvertElementwiseOps.cpp
+++ third_party/cpu/lib/TritonToTritonCPU/ConvertElementwiseOps.cpp
@@ -51,16 +51,10 @@ public:
 
     addDynamicallyLegalOp<triton::BitcastOp>(
         [](triton::BitcastOp op) { return isa<PointerType>(op.getType()); });
-    addIllegalOp<triton::BroadcastOp>();
-    addIllegalOp<triton::ExpandDimsOp>();
     addIllegalOp<triton::PreciseDivFOp>();
     addIllegalOp<triton::PreciseSqrtOp>();
-    addIllegalOp<triton::ReshapeOp>();
     addIllegalOp<triton::MulhiUIOp>();
     addIllegalOp<triton::ClampFOp>();
-    addIllegalOp<triton::TransOp>();
-    addIllegalOp<triton::JoinOp>();
-    addIllegalOp<triton::CatOp>();
   }
 };
 
@@ -81,70 +75,6 @@ struct ConstantOpConversion : public OpConversionPattern<arith::ConstantOp> {
       llvm_unreachable("Unexpected constant attribute");
     }
     return success();
-  }
-};
-
-struct ReshapeOpConversion : public OpConversionPattern<triton::ReshapeOp> {
-  using OpConversionPattern::OpConversionPattern;
-
-  LogicalResult
-  matchAndRewrite(triton::ReshapeOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    assert(isa<RankedTensorType>(op.getType()));
-    auto loc = op.getLoc();
-    auto src = rewriter.getRemappedValue(op.getSrc());
-    auto srcShape = dyn_cast<VectorType>(src.getType()).getShape();
-    auto resTy =
-        dyn_cast<VectorType>(getTypeConverter()->convertType(op.getType()));
-    auto dstShape = resTy.getShape();
-    auto elemTy = resTy.getElementType();
-
-    // There are restrictions on how shape can be modified by ShapeCastOp
-    // when rank is changed. For now, we simply detect it and handle through
-    // a cast to 1D vector. Better solution may be required later.
-    if (canCastShape(srcShape, dstShape)) {
-      rewriter.replaceOpWithNewOp<vector::ShapeCastOp>(
-          op, VectorType::get(dstShape, elemTy), src);
-    } else {
-      SmallVector<int64_t> tmpShape({resTy.getNumElements()});
-      auto tmp = rewriter.create<vector::ShapeCastOp>(
-          loc, VectorType::get(tmpShape, elemTy), src);
-      rewriter.replaceOpWithNewOp<vector::ShapeCastOp>(
-          op, VectorType::get(dstShape, elemTy), tmp);
-    }
-    return success();
-  }
-
-private:
-  bool canCastShape(ArrayRef<int64_t> src, ArrayRef<int64_t> dst) const {
-    if (src.size() == dst.size())
-      return true;
-    if (src.size() > dst.size())
-      return canCastShape(dst, src);
-
-    size_t srcIdx = 0;
-    size_t dstIdx = 0;
-    while (srcIdx < src.size() && dstIdx < dst.size()) {
-      if (src[srcIdx] == 1) {
-        ++srcIdx;
-      } else {
-        // Source dim size should be a product of continuous dest dim sizes.
-        int64_t srcSize = src[srcIdx++];
-        int64_t dstSize = dst[dstIdx++];
-        while (dstSize < srcSize && dstIdx < dst.size())
-          dstSize *= dst[dstIdx++];
-        if (dstSize != srcSize)
-          return false;
-      }
-    }
-
-    // Skip trailing 1s.
-    while (srcIdx < src.size() && src[srcIdx] == 1)
-      ++srcIdx;
-    while (dstIdx < dst.size() && dst[dstIdx] == 1)
-      ++dstIdx;
-
-    return srcIdx == src.size() && dstIdx == dst.size();
   }
 };
 
@@ -200,57 +130,6 @@ struct ClampFOpConversion : public OpConversionPattern<triton::ClampFOp> {
       res = rewriter.create<arith::MinNumFOp>(loc, res, maxVal);
     }
     rewriter.replaceOp(op, res);
-    return success();
-  }
-};
-
-struct TransOpConversion : public OpConversionPattern<triton::TransOp> {
-  using OpConversionPattern::OpConversionPattern;
-
-  LogicalResult
-  matchAndRewrite(triton::TransOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    auto loc = op.getLoc();
-    auto val = rewriter.getRemappedValue(op.getSrc());
-    auto order = op.getOrder();
-    SmallVector<int64_t> permutation(order.begin(), order.end());
-    rewriter.replaceOpWithNewOp<vector::TransposeOp>(op, val, permutation);
-    return success();
-  }
-};
-
-struct JoinOpConversion : public OpConversionPattern<triton::JoinOp> {
-  using OpConversionPattern::OpConversionPattern;
-
-  LogicalResult
-  matchAndRewrite(triton::JoinOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    auto loc = op.getLoc();
-    auto lhs = rewriter.getRemappedValue(op.getLhs());
-    auto rhs = rewriter.getRemappedValue(op.getRhs());
-    auto interleave = rewriter.create<vector::InterleaveOp>(loc, lhs, rhs);
-    // JoinOp creates a new dimension, but InterleaveOp doubles the final one.
-    // Use ShapeCastOp to get the required shape.
-    auto resTy = getTypeConverter()->convertType(op.getType());
-    rewriter.replaceOpWithNewOp<vector::ShapeCastOp>(op, resTy, interleave);
-    return success();
-  }
-};
-
-struct CatOpConversion : public OpConversionPattern<triton::CatOp> {
-  using OpConversionPattern::OpConversionPattern;
-
-  LogicalResult
-  matchAndRewrite(triton::CatOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    auto loc = op.getLoc();
-    auto lhs = rewriter.getRemappedValue(op.getLhs());
-    auto rhs = rewriter.getRemappedValue(op.getRhs());
-    auto lhsTy = dyn_cast<VectorType>(lhs.getType());
-    auto rhsTy = dyn_cast<VectorType>(rhs.getType());
-    SmallVector<int64_t> indices(lhsTy.getShape()[0] + rhsTy.getShape()[0]);
-    std::iota(indices.begin(), indices.end(), 0);
-    rewriter.replaceOpWithNewOp<vector::ShuffleOp>(op, lhs, rhs, indices);
     return success();
   }
 };
@@ -326,20 +205,12 @@ struct ConvertElementwiseOps
 
     patterns.add<OpTypeConversion<triton::BitcastOp, arith::BitcastOp>>(
         typeConverter, context);
-    patterns.add<OpTypeConversion<triton::BroadcastOp, vector::BroadcastOp>>(
-        typeConverter, context);
-    patterns.add<OpTypeConversion<triton::ExpandDimsOp, vector::ShapeCastOp>>(
-        typeConverter, context);
     patterns.add<OpTypeConversion<triton::PreciseDivFOp, arith::DivFOp>>(
         typeConverter, context);
     patterns.add<OpTypeConversion<triton::PreciseSqrtOp, math::SqrtOp>>(
         typeConverter, context);
-    patterns.add<ReshapeOpConversion>(typeConverter, context);
     patterns.add<MulhiUIOpConversion>(typeConverter, context);
     patterns.add<ClampFOpConversion>(typeConverter, context);
-    patterns.add<TransOpConversion>(typeConverter, context);
-    patterns.add<JoinOpConversion>(typeConverter, context);
-    patterns.add<CatOpConversion>(typeConverter, context);
 
     if (failed(applyPartialConversion(mod, convTarget, std::move(patterns))))
       return signalPassFailure();
