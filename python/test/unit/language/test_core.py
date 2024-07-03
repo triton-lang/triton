77e64c212580d7610d3589bb3bfa51e8a7f93dd0
lancerts <tangshao28@gmail.com>
2024-07-03 13:33:34 -0700

    Update the 07-extern-functions tutorial and include a unit test for extern function (#4226)
    ## Description
    1 Updated the doc of 07-extern-functions tutorial
    
    tl.math.fn no longer works in the nightly (following change as a
    reference)
    
    
    https://github.com/triton-lang/triton/pull/3172/files#diff-8bb547c8082ddf083d49bdb5e6f126c1cebae899390c95afc0f713229982f516L35
    
    2 Added an example of extern_lib path (just use the default path
    explicitly to showcase the usage).
    
    3 Added a unit test. In the case of future backward breaking changes of
    extern math function usage, the unit test should break.
    Picked tanh since it is heavily used in gelu.
    
    ## Testing
    Executed in cuda.

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -16,6 +16,7 @@ from numpy.random import RandomState
 import triton
 import triton.language as tl
 from triton.runtime.jit import TensorWrapper, reinterpret
+from triton.language.extra import libdevice
 
 
 def is_interpreter():
@@ -5457,3 +5458,40 @@ def test_num_programs(device):
 
     kernel[grid](input)
     assert torch.all(input == torch.tensor(grid, device=device))
+
+
+# -----------------------
+# test extern functions
+# -----------------------
+
+
+@pytest.mark.parametrize("dtype_str", ['float32', 'float64'])
+def test_math_extern(dtype_str):
+    if is_interpreter():
+        pytest.skip('math_extern does not work in the interpreter mode')
+
+    @triton.jit
+    def kernel(
+        x_ptr,
+        y_ptr,
+        n_elements,
+        BLOCK_SIZE: tl.constexpr,
+    ):
+        pid = tl.program_id(axis=0)
+        block_start = pid * BLOCK_SIZE
+        offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        x = tl.load(x_ptr + offsets, mask=mask)
+        y = libdevice.tanh(x)
+        tl.store(y_ptr + offsets, y, mask=mask)
+
+    shape = (128, )
+    rs = RandomState(17)
+
+    x = numpy_random(shape, dtype_str=dtype_str, rs=rs)
+    y_ref = np.tanh(x)
+    x_tri = to_triton(x, device='cuda')
+    y_tri = to_triton(numpy_random(shape, dtype_str=dtype_str, rs=rs), device='cuda')
+    kernel[(1, )](x_tri, y_tri, shape[0], BLOCK_SIZE=shape[0])
+    # compare
+    np.testing.assert_allclose(y_ref, to_numpy(y_tri), rtol=0.01)
