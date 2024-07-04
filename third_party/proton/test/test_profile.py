1075d1be5da559eb44511efb9e971e532bfba002
Jokeren <robinho364@gmail.com>
2024-07-03 23:04:57 -0400

    Update

--- third_party/proton/test/test_profile.py
+++ third_party/proton/test/test_profile.py
@@ -218,5 +218,5 @@ def test_pcsampling():
         x.zero_()
         proton.finalize()
         data = json.load(f)
-        assert "foo" in data[0]["children"][0]["children"][0]["children"][0]["frame"]["name"]
-        assert data[0]["children"][0]["children"][0]["children"][0]["metrics"]["NumSamples"] > 0
+        assert "foo" in data[0]["children"][0]["children"][0]["frame"]["name"]
+        assert data[0]["children"][0]["children"][0]["NumSamples"] > 0
