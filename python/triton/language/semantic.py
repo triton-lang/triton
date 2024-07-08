b6aa1c6a2ae1ee0ce30f388da97da40dbf3623a6
Wang, Quintin <quintin.wang@intel.com>
2024-07-08 03:21:11 +0000

    Restrict tl.dot size >= 16x16x16 only for CUDA

--- python/triton/language/semantic.py
+++ python/triton/language/semantic.py
@@ -1370,9 +1370,10 @@ def dot(lhs: tl.tensor, rhs: tl.tensor, acc: tl.tensor, input_precision: Optiona
     assert lhs_rank == rhs_rank == 2 or lhs_rank == rhs_rank == 3, f"Both inputs must be either 2D or 3D; (lhs: {lhs.shape} vs rhs: {rhs.shape})"
     assert lhs.shape[-1].value == rhs.shape[
         -2].value, f"First input shape ({lhs.shape}) and second input shape {rhs.shape} are not compatible for matmul (second index of first shape ({lhs.shape[-1].value}) must be equal to first index of second shape ({rhs.shape[-2].value})"
-    assert lhs.shape[-2].value >= 16 and lhs.shape[-1].value >= 16 \
+    if builder.options.backend_name == "cuda":
+        assert lhs.shape[-2].value >= 16 and lhs.shape[-1].value >= 16 \
         and rhs.shape[-1].value >= 16, \
-        f"All non-batch values in both first input shape ({lhs.shape}) and second input shape ({rhs.shape}) must be >= 16!"
+        f"All non-batch values in both first input shape ({lhs.shape}) and second input shape ({rhs.shape}) must be >= 16 for CUDA backend!"
     if lhs.type.scalar.is_int():
         assert lhs.type.scalar == tl.int8, "only int8 supported!"
         # TODO: This is CUDA specific, check if ROCm has the same limitation
