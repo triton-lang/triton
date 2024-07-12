56f49bbc2622130139cab0bf3164ba066cb770ba
Zahi Moudallal <zahi@openai.com>
2024-07-12 13:05:56 -0700

    [FRONTEND] Fix the override mechanism (#4312)

--- python/triton/compiler/compiler.py
+++ python/triton/compiler/compiler.py
@@ -286,13 +286,13 @@ def compile(src, target=None, options=None):
     for ext, compile_ir in list(stages.items())[first_stage:]:
         next_module = compile_ir(module, metadata)
         ir_filename = f"{file_name}.{ext}"
-        metadata_group[ir_filename] = fn_cache_manager.put(next_module, ir_filename)
-        if fn_dump_manager is not None:
-            fn_dump_manager.put(next_module, ir_filename)
         if (fn_override_manager is not None and fn_override_manager.has_file(ir_filename)):
             print(f"\nOverriding kernel with file {ir_filename}")
             full_name = fn_override_manager.get_file(ir_filename)
             next_module = parse(full_name, ext, context)
+        metadata_group[ir_filename] = fn_cache_manager.put(next_module, ir_filename)
+        if fn_dump_manager is not None:
+            fn_dump_manager.put(next_module, ir_filename)
         # use an env variable to parse ttgir from file
         if use_ttgir_loc and ext == "ttgir":
             ttgir_full_name = fn_cache_manager.get_file(ir_filename)
