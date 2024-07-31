bc4fb5fbca6ea311281d96034440b465ce654c17
Yuanwei Fang <fywkevin@gmail.com>
2024-07-31 13:22:24 -0700

    [triton][tma] Check return value of cuTensorMapEncodeTiled() (#4419)
    Wrap the return value of `cuTensorMapEncodeTiled(...)` inside the
    `CUDA_CHECK_AND_RETURN_NULL` macro instead of `assert` to check the
    return result of TMA descriptor creation method. In this way, when the
    TMA descriptor is not set properly, we don't get a crash. Instead, a
    Python exception is raised.
    
    The core Triton is a small number of people, and we receive many PRs
    (thank
    you!).  To help us review your code more quickly, **if you are a new
    contributor (less than 3 PRs merged) we ask that you complete the
    following
    tasks and include the filled-out checklist in your PR description.**
    
    Complete the following tasks before sending your PR, and replace `[ ]`
    with
    `[x]` to indicate you have done them.
    
    - [x] I am not making a trivial change, such as fixing a typo in a
    comment.
    
    - [x] I have written a PR description following these
      [rules](https://cbea.ms/git-commit/#why-not-how).
    
    - [x] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    - Select one of the following.
      - [x] I have added tests.
        - `/test` for `lit` tests
        - `/unittest` for C++ tests
        - `/python/test` for end-to-end tests
      - [ ] This PR does not need a test because `FILL THIS IN`.
    
    - Select one of the following.
      - [x] I have not added any `lit` tests.
    - [ ] The `lit` tests I have added follow these [best
    practices](https://mlir.llvm.org/getting_started/TestingGuide/#filecheck-best-practices),
    including the "tests should be minimal" section. (Usually running Python
    code
        and using the instructions it generates is not minimal.)

---
+++ python/test/unit/hopper/test_tma_descriptor.py
@@ -0,0 +1,49 @@
+import pytest
+import torch
+from triton.tools.experimental_descriptor import create_1d_tma_descriptor, create_2d_tma_descriptor
+
+
+@pytest.mark.parametrize("M, BLOCK_M, expect_error", [(128, 32, False), (127, 32, False), (128, 31, True)])
+def test_1d_tma_descriptor_exception(M, BLOCK_M, expect_error):
+    if not torch.cuda.is_available() or not torch.cuda.get_device_capability()[0] == 9:
+        pytest.skip("Test requires Hopper target.")
+        return
+
+    device = "cuda"
+    x = torch.randn(M, dtype=torch.float32, device=device)
+    # globalAddress in the tma descriptor must be aligned to 16 bytes for CU_TENSOR_MAP_INTERLEAVE_NONE.
+    # https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__TENSOR__MEMORY.html#group__CUDA__TENSOR__MEMORY
+    assert x.data_ptr() % 16 == 0
+    is_error = False
+
+    try:
+        create_1d_tma_descriptor(x.data_ptr(), M, BLOCK_M, x.element_size())
+    except RuntimeError as e:
+        is_error = True
+        assert e.args[0] == "Triton Error [CUDA]: invalid argument"
+
+    assert is_error == expect_error
+
+
+@pytest.mark.parametrize("M, BLOCK_M", [(128, 32), (125, 33)])
+@pytest.mark.parametrize("N, BLOCK_N, expect_error", [(128, 32, False), (128, 30, True), (127, 32, True)])
+def test_2d_tma_descriptor_exception(M, N, BLOCK_M, BLOCK_N, expect_error):
+    if not torch.cuda.is_available() or not torch.cuda.get_device_capability()[0] == 9:
+        pytest.skip("Test requires Hopper target.")
+        return
+
+    device = "cuda"
+    torch.manual_seed(42)
+    A = torch.randn((M, N), dtype=torch.float16, device=device)
+    # globalAddress in the tma descriptor must be aligned to 16 bytes for CU_TENSOR_MAP_INTERLEAVE_NONE.
+    # https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__TENSOR__MEMORY.html#group__CUDA__TENSOR__MEMORY
+    assert A.data_ptr() % 16 == 0
+    is_error = False
+
+    try:
+        create_2d_tma_descriptor(A.data_ptr(), M, N, BLOCK_M, BLOCK_N, A.element_size())
+    except RuntimeError as e:
+        is_error = True
+        assert e.args[0] == "Triton Error [CUDA]: invalid argument"
+
+    assert is_error == expect_error
