851b02f55f6f2866ffe8ae0a337816d3e6e26d7e
Jokeren <robinho364@gmail.com>
2024-08-01 20:07:44 -0400

    Update

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -460,7 +460,6 @@ struct AtomicCASOpConversion
           // Extract the new_loaded value from the pair.
           Value newLoaded = extract_val(valueElemTy, cmpxchg, 0);
           Value atomPtr = getSharedMemoryBase(loc, rewriter, op.getOperation());
-          atomPtr = bitcast(atomPtr, ptr_ty(rewriter.getContext(), 3));
           store(newLoaded, atomPtr);
         }
 
