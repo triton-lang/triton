958e9a5a8b6c77c2ed77e3483030fe4de90f76b0
Christian Sigg <chsigg@users.noreply.github.com>
2024-08-05 15:10:24 +0200

    [BACKEND] Update LLVM version to https://github.com/llvm/llvm-project/commit/1a9acd786d493b00c08d1611f51420d421b74cf1 (#4468)

--- third_party/amd/lib/TritonAMDGPUToLLVM/BuiltinFuncToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/BuiltinFuncToLLVM.cpp
@@ -128,7 +128,7 @@ private:
     auto operands = callOp.getOperands();
     auto result = callOp.getResult();
 
-    LLVM::LLVMFunctionType calleeType = callOp.getVarCalleeType().value();
+    LLVM::LLVMFunctionType calleeType = callOp.getCalleeFunctionType();
     Type returnType = calleeType.getReturnType();
 
     auto loc = callOp.getLoc();
