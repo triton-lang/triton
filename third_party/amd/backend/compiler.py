bb441d1f2b68743e11524cb38f4942aab2d44fd9
Ognjen Plavsic <ognjen.plavsic@luxoft.com>
2024-08-07 18:55:23 +0200

    initial commit

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -214,6 +214,9 @@ class HIPBackend(BaseBackend):
         amd.passes.ttgpuir.add_accelerate_matmul(pm, options.arch, options.matrix_instr_nonkdim, options.kpack)
         passes.ttgpuir.add_remove_layout_conversions(pm)
         amd.passes.ttgpuir.add_optimize_epilogue(pm)
+
+        amd.passes.ttgpuir.add_tritongpu_bypass_lds_for_dot_layout_pass()
+        
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
         if amd.has_matrix_core_feature(options.arch):
             assert options.num_stages != 0, ("Triton AMD backend pipeliner has been updated. "
--- third_party/amd/include/TritonAMDGPUTransforms/Passes.h
+++ third_party/amd/include/TritonAMDGPUTransforms/Passes.h
@@ -20,6 +20,7 @@ std::unique_ptr<Pass> createTritonAMDGPUReorderInstructionsPass();
 std::unique_ptr<Pass> createTritonAMDGPUVerifier();
 
 std::unique_ptr<Pass> createTritonAMDGPUOptimizeEpiloguePass();
+std::unique_ptr<Pass> createTritonAMDGPUBypassLDSForDotLayout();
 
 std::unique_ptr<Pass> createTritonAMDGPUCanonicalizePointersPass();
 
