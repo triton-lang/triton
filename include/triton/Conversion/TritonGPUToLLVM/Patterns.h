35aa9a296e419c3712d6404468945b524dee3797
Lu, Chengjun <chengjun.lu@intel.com>
2024-08-14 14:48:07 +0000

    Use the MmaEncodingTraits instead of template type.

--- include/triton/Conversion/TritonGPUToLLVM/Patterns.h
+++ include/triton/Conversion/TritonGPUToLLVM/Patterns.h
@@ -17,48 +17,12 @@ void decomposeBlockedToDotLayoutConversion(ModuleOp module);
 /// |module| op.
 void decomposeSplatOpToSharedLayoutConversion(ModuleOp module);
 
-namespace {
-static void addAttrs(Operation *op, ArrayRef<mlir::NamedAttribute> attrs) {
-  for (const NamedAttribute attr : attrs)
-    op->setAttr(attr.getName(), attr.getValue());
-}
-} // namespace
-
 /// Replaces `mma/mfma -> dot_op` with `mma/mfma -> blocked -> dot_op` in the
 /// given |module| op, but bypass the decomposition if |shortcutFn| returns
 /// true.
 using ShortcutFn = std::function<bool(RankedTensorType, RankedTensorType)>;
-template <typename TensorCoreEncodingAttr>
 void decomposeTensorCoreToDotLayoutConversion(ModuleOp module,
-                                              ShortcutFn shortcutFn) {
-  int numWarps = triton::gpu::TritonGPUDialect::getNumWarps(module);
-  int numCTAs = triton::gpu::TritonGPUDialect::getNumCTAs(module);
-  int threadsPerWarp = triton::gpu::TritonGPUDialect::getThreadsPerWarp(module);
-
-  module.walk([&](triton::gpu::ConvertLayoutOp cvtOp) -> void {
-    OpBuilder builder(cvtOp);
-    auto srcType = cast<RankedTensorType>(cvtOp.getSrc().getType());
-    auto dstType = cast<RankedTensorType>(cvtOp.getType());
-    auto srcMma = dyn_cast<TensorCoreEncodingAttr>(srcType.getEncoding());
-    auto dstDotOp =
-        dyn_cast<triton::gpu::DotOperandEncodingAttr>(dstType.getEncoding());
-    if (srcMma && dstDotOp && !shortcutFn(srcType, dstType)) {
-      auto tmpType = RankedTensorType::get(
-          dstType.getShape(), dstType.getElementType(),
-          triton::gpu::BlockedEncodingAttr::get(
-              module.getContext(), srcType.getShape(), getSizePerThread(srcMma),
-              getOrder(srcMma), numWarps, threadsPerWarp, numCTAs));
-      auto tmp = builder.create<triton::gpu::ConvertLayoutOp>(
-          cvtOp.getLoc(), tmpType, cvtOp.getSrc());
-      addAttrs(tmp, cvtOp->getAttrs());
-      auto newConvert = builder.create<triton::gpu::ConvertLayoutOp>(
-          cvtOp.getLoc(), dstType, tmp);
-      addAttrs(newConvert, cvtOp->getAttrs());
-      cvtOp.replaceAllUsesWith(newConvert.getResult());
-      cvtOp.erase();
-    }
-  });
-}
+                                              ShortcutFn shortcutFn);
 
 } // namespace triton::gpu
 
--- lib/Conversion/TritonGPUToLLVM/DecomposeUnsupportedConversions.cpp
+++ lib/Conversion/TritonGPUToLLVM/DecomposeUnsupportedConversions.cpp
@@ -7,6 +7,15 @@
 using namespace mlir;
 using namespace mlir::triton;
 
+namespace {
+
+static void addAttrs(Operation *op, ArrayRef<mlir::NamedAttribute> attrs) {
+  for (const NamedAttribute attr : attrs)
+    op->setAttr(attr.getName(), attr.getValue());
+}
+
+} // namespace
+
 namespace mlir::triton::gpu {
 
 void decomposeSplatOpToSharedLayoutConversion(ModuleOp module) {
@@ -31,6 +40,37 @@ void decomposeSplatOpToSharedLayoutConversion(ModuleOp module) {
           splatOp.getLoc(), dstType, newSplat.getResult());
       splatOp.replaceAllUsesWith(newConvert.getResult());
       splatOp.erase();
+    }
+  });
+}
+
+void decomposeTensorCoreToDotLayoutConversion(ModuleOp module,
+                                              ShortcutFn shortcutFn) {
+  int numWarps = triton::gpu::TritonGPUDialect::getNumWarps(module);
+  int numCTAs = triton::gpu::TritonGPUDialect::getNumCTAs(module);
+  int threadsPerWarp = triton::gpu::TritonGPUDialect::getThreadsPerWarp(module);
+
+  module.walk([&](triton::gpu::ConvertLayoutOp cvtOp) -> void {
+    OpBuilder builder(cvtOp);
+    auto srcType = cast<RankedTensorType>(cvtOp.getSrc().getType());
+    auto dstType = cast<RankedTensorType>(cvtOp.getType());
+    auto srcMma = dyn_cast<MmaEncodingTrait>(srcType.getEncoding());
+    auto dstDotOp =
+        dyn_cast<triton::gpu::DotOperandEncodingAttr>(dstType.getEncoding());
+    if (srcMma && dstDotOp && !shortcutFn(srcType, dstType)) {
+      auto tmpType = RankedTensorType::get(
+          dstType.getShape(), dstType.getElementType(),
+          triton::gpu::BlockedEncodingAttr::get(
+              module.getContext(), srcType.getShape(), getSizePerThread(srcMma),
+              getOrder(srcMma), numWarps, threadsPerWarp, numCTAs));
+      auto tmp = builder.create<triton::gpu::ConvertLayoutOp>(
+          cvtOp.getLoc(), tmpType, cvtOp.getSrc());
+      addAttrs(tmp, cvtOp->getAttrs());
+      auto newConvert = builder.create<triton::gpu::ConvertLayoutOp>(
+          cvtOp.getLoc(), dstType, tmp);
+      addAttrs(newConvert, cvtOp->getAttrs());
+      cvtOp.replaceAllUsesWith(newConvert.getResult());
+      cvtOp.erase();
     }
   });
 }
