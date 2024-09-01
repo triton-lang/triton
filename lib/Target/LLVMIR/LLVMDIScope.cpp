634158b113e292685ddbf57e0b259eaa11655b64
Lei Zhang <antiagainst@gmail.com>
2024-09-01 07:38:30 -0700

    [BACKEND] Update llvm to llvm/llvm-project@56152fa377 (#4625)
    This bumps to llvm/llvm-project@56152fa377 to further
    include the following fix
    * https://github.com/llvm/llvm-project/pull/105871
    
    Which was fixing issues in a previous LLVM bump
    * https://github.com/triton-lang/triton/pull/4624

--- lib/Target/LLVMIR/LLVMDIScope.cpp
+++ lib/Target/LLVMIR/LLVMDIScope.cpp
@@ -103,9 +103,8 @@ struct LLVMDIScopePass : public LLVMDIScopeBase<LLVMDIScopePass> {
     // the column offset
     auto subprogramAttr = LLVM::DISubprogramAttr::get(
         context, distinctId, compileUnitAttr, fileAttr, funcNameAttr,
-        funcNameAttr, fileAttr,
-        /*line=*/line,
-        /*scopeline=*/line, subprogramFlags, subroutineTypeAttr);
+        funcNameAttr, fileAttr, /*line=*/line, /*scopeline=*/line,
+        subprogramFlags, subroutineTypeAttr, /*retainNodes=*/{});
     funcOp->setLoc(FusedLoc::get(context, {loc}, subprogramAttr));
   }
 
