ca3fb5f6fa6365445d2d982d137aa5cb2a80f547
Anatoly Myachev <anatoly.myachev@intel.com>
2024-09-05 00:00:05 +0200

    Use `device` fixture for `runtime/test_cache.py` and `runtime/test_launch.py` (#4641)
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/runtime/test_launch.py
+++ python/test/unit/runtime/test_launch.py
@@ -43,7 +43,7 @@ def test_metadata() -> None:
     assert used_hook
 
 
-def test_memory_leak() -> None:
+def test_memory_leak(device) -> None:
 
     @triton.jit
     def kernel(in_ptr0, out_ptr0, xnumel, XBLOCK: tl.constexpr):
@@ -57,8 +57,8 @@ def test_memory_leak() -> None:
 
     tracemalloc.start()
     try:
-        inp = torch.randn(10, device='cuda')
-        out = torch.randn(10, device='cuda')
+        inp = torch.randn(10, device=device)
+        out = torch.randn(10, device=device)
         kernel[(10, )](inp, out, 10, XBLOCK=16)
         gc.collect()
         begin, _ = tracemalloc.get_traced_memory()
