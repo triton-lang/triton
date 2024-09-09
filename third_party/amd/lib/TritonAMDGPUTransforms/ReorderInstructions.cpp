ef6103764b321adf4c4b5933dc856488e74b577c
Ognjen Plavsic <ognjen.plavsic@luxoft.com>
2024-09-09 15:36:18 +0200

    Address review comments

--- third_party/amd/lib/TritonAMDGPUTransforms/ReorderInstructions.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/ReorderInstructions.cpp
@@ -111,36 +111,51 @@ public:
     //    %2 = local_alloc
     //    %3 = local_store %1, %2
     //    %4 = local_load %2
-    m.walk([&](ttg::LocalLoadOp op) {
-      Operation *localLoad = (Operation *)op;
-      Operation *localAlloc = localLoad->getOperand(0).getDefiningOp();
-
-      if (!localAlloc || !isa<ttg::LocalAllocOp>(localAlloc)) {
-        return;
-      }
-
-      // Case when localAlloc has no operands
-      if (localAlloc->getNumOperands() < 1) {
-        Operation *localStore = getFirstUse(localAlloc);
-        assert(localStore && "localAlloc has no use");
-
-        // Check if the use is a LocalStoreOp
-        if (isa<ttg::LocalStoreOp>(localStore)) {
-          Operation *loadOp = localStore->getOperand(0).getDefiningOp();
-
-          if (isa<tt::LoadOp>(loadOp)) {
-            localAlloc->moveAfter(loadOp);
-            localStore->moveAfter(localAlloc);
-            localLoad->moveAfter(localStore);
-          }
-        }
+    m.walk([&](ttg::LocalLoadOp localLoad) {
+      auto localAlloc =
+          localLoad->getOperand(0).getDefiningOp<ttg::LocalAllocOp>();
+      if (!localAlloc) {
         return;
       }
 
       // Case when localAlloc has operands
-      Operation *loadOp = localAlloc->getOperand(0).getDefiningOp();
+      if (localAlloc->getNumOperands() == 1) {
+        if (!localAlloc->hasOneUse()) {
+          return;
+        }
+        Operation *loadOp =
+            localAlloc->getOperand(0).getDefiningOp<tt::LoadOp>();
+        if (!loadOp) {
+          return;
+        }
+        localAlloc->moveAfter(loadOp);
+        localLoad->moveAfter(localAlloc);
+        return;
+      }
+
+      // Case when localAlloc has no operands
+      assert(localAlloc->getNumOperands() < 1);
+      auto allocVal = localAlloc->getResult(0);
+
+      // Check if the localAlloc has exactly two uses (localStore and localLoad)
+      int numUses = std::distance(allocVal.use_begin(), allocVal.use_end());
+      if (numUses != 2) {
+        return;
+      }
+
+      // localStore comes before localLoad in block.
+      Operation *localStore = getFirstUse(localAlloc);
+      if (!isa<ttg::LocalStoreOp>(localStore)) {
+        return;
+      }
+
+      Operation *loadOp = localStore->getOperand(0).getDefiningOp<tt::LoadOp>();
+      if (!loadOp) {
+        return;
+      }
       localAlloc->moveAfter(loadOp);
-      localLoad->moveAfter(localAlloc);
+      localStore->moveAfter(localAlloc);
+      localLoad->moveAfter(localStore);
     });
 
     // Sink conversion after the last dealloc but before the first use ancestor
