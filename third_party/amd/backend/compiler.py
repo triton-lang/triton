1e06252a8ebad23648edb4d14ad37e4d74683c99
Hongtao Yu <hoy@meta.com>
2024-09-20 17:49:10 -0700

    [AMD] Enable the loop unroller (#4773)

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -142,6 +142,7 @@ class HIPBackend(BaseBackend):
         passes.common.add_cse(pm)
         passes.common.add_licm(pm)
         passes.common.add_symbol_dce(pm)
+        passes.ttir.add_loop_unroll(pm)
         pm.run(mod)
         return mod
 
