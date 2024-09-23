325a0cdd7899580f454f2f5d7c65ada29ab70feb
Jokeren <robinho364@gmail.com>
2024-09-23 10:22:37 -0400

    Update

--- lib/Dialect/TritonGPU/IR/LinearLayoutConversions.cpp
+++ lib/Dialect/TritonGPU/IR/LinearLayoutConversions.cpp
@@ -859,9 +859,8 @@ std::optional<LinearLayout> chooseStMatrixLayoutForRegToRegConversion(
   layout *=
       identityND(kWarp, mma.getWarpsPerCTA(), /*order=*/{0, 1}, {kRow, kCol})
           .transposeOuts(llvm::to_vector(layout.getOutDimNames()));
-  auto tensorShapePerCTA = getShapePerCTA(mma, tensorTy.getShape());
   auto ret =
-      combineCtaCgaWithShape(layout, mma.getCTALayout(), tensorShapePerCTA);
+      combineCtaCgaWithShape(layout, mma.getCTALayout(), tensorTy.getShape());
   return ret.transposeOuts(llvm::to_vector(layout.getOutDimNames()))
       .reshapeOuts(
           {{S("offset"), ret.getTotalOutDimSize()}, {S("iteration"), 1}});
