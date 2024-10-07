ee5bc31dc816ecc20fe8674969d4ea3c9fb6c063
lezcano <lezcano-93@hotmail.com>
2024-10-07 11:06:00 +0100

    Larger kWidth

--- lib/Dialect/TritonGPU/Transforms/ReduceDataDuplication.cpp
+++ lib/Dialect/TritonGPU/Transforms/ReduceDataDuplication.cpp
@@ -58,6 +58,12 @@ public:
             dstDotOp.getParent() == srcMfmaEncoding)
           return;
       }
+      // We support this one via LLs, as the LocalLoad path is buggy
+      bool largeKWidth =
+          dstDotOp.getKWidth() * dstType.getElementTypeBitWidth() > 64;
+      if (largeKWidth) {
+        return;
+      }
       auto srcOrder = triton::gpu::getOrder(srcEncoding);
       auto rank = srcOrder.size();
       SmallVector<unsigned> sharedOrder;
