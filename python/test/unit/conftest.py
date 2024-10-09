36049ed0202128081d181ac75d8bfdcb7955dbc7
Shuofei Zhu <shuofei@meta.com>
2024-10-09 14:11:39 -0700

    update context manager funcs

--- python/test/unit/conftest.py
+++ python/test/unit/conftest.py
@@ -1,10 +1,30 @@
-import pytest
 import os
 import tempfile
+from contextlib import contextmanager
+
+import pytest
+
+
+@contextmanager
+def enable_remark_context():
+    try:
+        os.environ["MLIR_ENABLE_REMARK"] = "1"
+        yield
+    finally:
+        os.environ["MLIR_ENABLE_REMARK"] = "0"
+
+
+@contextmanager
+def enable_dump_context(pass_name="1"):
+    try:
+        os.environ["MLIR_ENABLE_REMARK"] = pass_name
+        yield
+    finally:
+        os.environ["MLIR_ENABLE_REMARK"] = "0"
 
 
 def pytest_addoption(parser):
-    parser.addoption("--device", action="store", default='cuda')
+    parser.addoption("--device", action="store", default="cuda")
 
 
 @pytest.fixture
--- python/test/unit/test_debug_dump.py
+++ python/test/unit/test_debug_dump.py
@@ -1,14 +1,15 @@
+import os
+
+import torch
 import triton
 import triton.language as tl
-import os
-import torch
 
 
 def test_fn_dump(capfd, device):
     N = 1024
     src = torch.zeros(N, device=device)
 
-    grid = lambda META: (triton.cdiv(N, META['BLOCK_SIZE']), )
+    grid = lambda META: (triton.cdiv(N, META["BLOCK_SIZE"]),)
 
     @triton.jit
     def _kernel(src, N, BLOCK_SIZE: tl.constexpr):
@@ -16,24 +17,22 @@ def test_fn_dump(capfd, device):
         x = tl.load(src + offsets, mask=offsets < N) + 1
         tl.store(src + offsets, x, mask=offsets < N)
 
-    os.environ['MLIR_ENABLE_DUMP'] = '1'
-    BLOCK_SIZE = 16
-    _kernel[grid](src, N, BLOCK_SIZE)
+    with enable_dump_context():
+        BLOCK_SIZE = 16
+        _kernel[grid](src, N, BLOCK_SIZE)
     captured = capfd.readouterr()
     assert "IR Dump Before" in captured.err
     assert "tt.func public @_kernel" in captured.err
 
-    os.environ['MLIR_ENABLE_DUMP'] = '_kernel'
-    BLOCK_SIZE = 32
-    _kernel[grid](src, N, BLOCK_SIZE)
+    with enable_dump_context("_kernel"):
+        BLOCK_SIZE = 32
+        _kernel[grid](src, N, BLOCK_SIZE)
     captured = capfd.readouterr()
     assert "IR Dump Before" in captured.err
     assert "tt.func public @_kernel" in captured.err
 
-    os.environ['MLIR_ENABLE_DUMP'] = '_kernel2'
-    BLOCK_SIZE = 64
-    _kernel[grid](src, N, BLOCK_SIZE)
+    with enable_dump_context("_kernel2"):
+        BLOCK_SIZE = 64
+        _kernel[grid](src, N, BLOCK_SIZE)
     captured = capfd.readouterr()
     assert "IR Dump Before" not in captured.err
-
-    os.environ['MLIR_ENABLE_DUMP'] = '0'
