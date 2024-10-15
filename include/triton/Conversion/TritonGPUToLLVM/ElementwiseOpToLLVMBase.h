a60fa8c0bc90c7c90ba9bca9693e4e6a66912665
Alexander Weinrauch <alexander.weinrauch@amd.com>
2024-10-15 18:00:54 +0100

    [AMD] Fix gfx12 warp size and fix wmma in maybeDeduplicate (#4912)
    This adds a missing exception to the warp size and fixes dot test for m
    or n > 32 when using wmma.

--- include/triton/Conversion/TritonGPUToLLVM/ElementwiseOpToLLVMBase.h
+++ include/triton/Conversion/TritonGPUToLLVM/ElementwiseOpToLLVMBase.h
@@ -88,10 +88,11 @@ public:
       // encoding not available
       return resultVals;
     Attribute baseEncoding = encoding;
-    if (isa<AMDMfmaEncodingAttr>(baseEncoding))
-      // TODO: this logic seems incorrect for mfma layout. Skip for now.
-      // We saw mismatches for some flash-attention tests on AMD backend.
-      // Note that this logic works for sliced layout whose parent is
+    if (isa<AMDMfmaEncodingAttr>(baseEncoding) ||
+        isa<AMDWmmaEncodingAttr>(baseEncoding))
+      // TODO: this logic seems incorrect for mfma and wmma layout. Skip for
+      // now. We saw mismatches for some flash-attention and dot tests on AMD
+      // backend. Note that this logic works for sliced layout whose parent is
       // mfma layout. Therefore, this is not combined with the following check.
       return resultVals;
     while (auto sliced = dyn_cast<SliceEncodingAttr>(baseEncoding))
