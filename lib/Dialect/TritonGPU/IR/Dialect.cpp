dfa69e47052097d658bde105078a429f52c9a8e1
Jokeren <robinho364@gmail.com>
2024-10-14 21:17:32 -0700

    Update

--- lib/Dialect/TritonGPU/IR/Dialect.cpp
+++ lib/Dialect/TritonGPU/IR/Dialect.cpp
@@ -477,8 +477,7 @@ LogicalResult CTALayoutAttr::verify(
   }
 
   if (llvm::any_of(CTAsPerCGA, [](unsigned x) { return x == 0; })) {
-    return emitError()
-           << "Every element in CTAsPerCGA must be greater than 0.";
+    return emitError() << "Every element in CTAsPerCGA must be greater than 0.";
   }
 
   if (llvm::any_of(CTASplitNum, [](unsigned x) { return x == 0; })) {
