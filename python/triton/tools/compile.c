5adfb8c821fa8a31faae5f1a97427dd19b471214
Peter Bell <peterbell10@live.co.uk>
2024-10-17 14:09:22 +0100

    Fix aot compile

--- python/triton/tools/compile.c
+++ python/triton/tools/compile.c
@@ -60,6 +60,7 @@ CUresult {kernel_name}(CUstream stream, {signature}) {{
     unsigned int gX = {gridX};
     unsigned int gY = {gridY};
     unsigned int gZ = {gridZ};
+    CUdeviceptr global_scratch = NULL;
     void *args[{num_args}] = {{ {arg_pointers} }};
     // TODO: shared memory
     if(gX * gY * gZ > 0)
