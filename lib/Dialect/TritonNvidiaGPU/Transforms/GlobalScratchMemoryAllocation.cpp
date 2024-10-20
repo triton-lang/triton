3e3e60bbd7b058ddb231c641d97810842220a24a
Peter Bell <peterbell10@live.co.uk>
2024-10-21 00:54:21 +0100

    Support allocating memory in noinline functions

--- lib/Dialect/TritonNvidiaGPU/Transforms/GlobalScratchMemoryAllocation.cpp
+++ lib/Dialect/TritonNvidiaGPU/Transforms/GlobalScratchMemoryAllocation.cpp
@@ -24,7 +24,19 @@ static int32_t roundUp(int32_t val, int32_t step) {
   return t - (t % step);
 }
 
-static void allocateGMem(Operation *parentOp) {
+static void allocateGMem(Operation *parentOp,
+                         llvm::SetVector<Operation *> &callStack) {
+  // Recursively visit any dependency functions
+  parentOp->walk([&](triton::CallOp call) {
+    auto callable = call.resolveCallable();
+    if (!callable->hasAttr("triton_nvidia_gpu.global_scratch_memory_size")) {
+      auto inserted = callStack.insert(parentOp);
+      assert(inserted && "call cycle detected");
+      allocateGMem(callable, callStack);
+      callStack.remove(parentOp);
+    }
+  });
+
   MLIRContext *ctx = parentOp->getContext();
   OpBuilder builder(ctx);
   int32_t offset = 0;
@@ -33,16 +45,32 @@ static void allocateGMem(Operation *parentOp) {
   // Dumb allocation that ignores liveness and makes no attempt to minimize
   // padding
   // TODO: Use a real algorithm
-  parentOp->walk<WalkOrder::PostOrder>(
-      [&](triton::nvidia_gpu::GlobalScratchAllocOp alloc) {
-        auto nbytes = alloc.getNbytes();
-        auto align = alloc.getAlignment();
-        offset = roundUp(offset, align);
-        alloc->setAttr("triton_nvidia_gpu.global_scratch_memory_offset",
-                       builder.getI32IntegerAttr(offset));
-        offset += nbytes;
-        largestAlignment = std::max(largestAlignment, align);
-      });
+  parentOp->walk<WalkOrder::PostOrder>([&](Operation *op) {
+    uint32_t nbytes = 0;
+    uint32_t align = 0;
+    if (auto alloc = dyn_cast<triton::nvidia_gpu::GlobalScratchAllocOp>(op)) {
+      nbytes = alloc.getNbytes();
+      align = alloc.getAlignment();
+    } else if (auto callOp = dyn_cast<triton::CallOp>(op)) {
+      auto callable = callOp.resolveCallable();
+      auto nbytes_attr = callable->getAttrOfType<IntegerAttr>(
+          "triton_nvidia_gpu.global_scratch_memory_size");
+      auto align_attr = callable->getAttrOfType<IntegerAttr>(
+          "triton_nvidia_gpu.global_scratch_memory_alignment");
+      assert(nbytes_attr);
+      assert(align_attr);
+
+      nbytes = nbytes_attr.getValue().getZExtValue();
+      align = align_attr.getValue().getZExtValue();
+    }
+    if (nbytes > 0) {
+      offset = roundUp(offset, align);
+      op->setAttr("triton_nvidia_gpu.global_scratch_memory_offset",
+                  builder.getI32IntegerAttr(offset));
+      offset += nbytes;
+      largestAlignment = std::max(largestAlignment, align);
+    }
+  });
   int32_t totalMemorySize = roundUp(offset, largestAlignment);
   parentOp->setAttr("triton_nvidia_gpu.global_scratch_memory_size",
                     builder.getI32IntegerAttr(totalMemorySize));
@@ -57,8 +85,26 @@ public:
   void runOnOperation() override {
     ModuleOp mod = getOperation();
 
-    // TODO: handle cases with multiple function with GlobalScratchAllocOp.
-    allocateGMem(mod);
+    bool seenKernel = false;
+
+    SetVector<Operation *> callStack;
+    mod->walk([&](triton::FuncOp func) {
+      allocateGMem(func, callStack);
+
+      if (func.getVisibility() == SymbolTable::Visibility::Public) {
+        assert(!seenKernel);
+        seenKernel = true;
+        auto size = func->getAttrOfType<IntegerAttr>(
+            "triton_nvidia_gpu.global_scratch_memory_size");
+        auto align = func->getAttrOfType<IntegerAttr>(
+            "triton_nvidia_gpu.global_scratch_memory_alignment");
+        assert(size);
+        assert(align);
+        mod->setAttr("triton_nvidia_gpu.global_scratch_memory_size", size);
+        mod->setAttr("triton_nvidia_gpu.global_scratch_memory_alignment",
+                     align);
+      }
+    });
   }
 };
 
