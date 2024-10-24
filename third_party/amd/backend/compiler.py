3e53ed16051fab7016f7ed226349f37a5b7c2754
Lei Zhang <antiagainst@gmail.com>
2024-10-24 17:07:34 +0000

    Be explicit and check the env var is 1 to enable

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -229,7 +229,7 @@ class HIPBackend(BaseBackend):
         passes.ttgpuir.add_reduce_data_duplication(pm)
         if amd.has_matrix_core_feature(options.arch):
             amd.passes.ttgpuir.add_reorder_instructions(pm)
-        if os.environ.get("AMDGCN_USE_BUFFER_OPS"):
+        if os.environ.get("AMDGCN_USE_BUFFER_OPS", "0") == "1":
             amd.passes.ttgpuir.add_canonicalize_pointers(pm)
             passes.common.add_canonicalizer(pm)
             amd.passes.ttgpuir.add_convert_to_buffer_ops(pm)
