2abcf9c088b57a7e5b4c32612285cafce560bc27
Jungwook Park <Jungwook.Park@amd.com>
2024-10-29 06:29:39 -0500

    Fix bad merge

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -223,7 +223,7 @@ class HIPBackend(BaseBackend):
                                              "equivalent behavior in the past.")
             amd.passes.ttgpuir.add_stream_pipelinev2(pm, options.num_stages)
             passes.common.add_canonicalizer(pm)
-        #amd.passes.ttgpuir.insert_instruction_sched_hints(pm)
+        amd.passes.ttgpuir.insert_instruction_sched_hints(pm)
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
         passes.ttgpuir.add_remove_layout_conversions(pm)
         passes.ttgpuir.add_reduce_data_duplication(pm)
