b910590d5225781d4fa80441c560afffc88aefe7
Corbin Robeck <corbin.robeck@amd.com>
2024-10-31 11:11:49 +0000

    update test

--- python/test/unit/instrumentation/test_clone_kernel.py
+++ python/test/unit/instrumentation/test_clone_kernel.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import re
 
 import triton
 import triton.language as tl
@@ -17,16 +18,26 @@ def test_clone_kernel(device):
     pgm = _kernel[(1024 // 128, )](output, N=1024, BLOCK_N=128)
     kernel_signatures = []
     for line in pgm.asm['llir'].split("\n"):
-        if "amdgpu_kernel" in line:
-            kernel_signatures.append(line.strip())
+        if "define" in line:
+            stack = []
+            matches = []
+
+            for i, char in enumerate(line):
+                if char == '(':
+                    stack.append(i)
+                elif char == ')':
+                    if stack:
+                        start_index = stack.pop()
+                        matches.append((start_index, i))
+            kernel_signatures.append(line.strip()[matches[-1][0]+1:matches[-1][1]])
 
     if 'LLVM_PASS_PLUGIN_PATH' in os.environ and os.environ['LLVM_PASS_PLUGIN_PATH'].split(
             "/")[-1] == "libCloneKernelAndAugmentArgsLib.so":
         assert kernel_signatures == [
-            'define amdgpu_kernel void @_kernel(ptr addrspace(1) inreg nocapture writeonly %0) local_unnamed_addr #0 !dbg !4 {',
-            'define amdgpu_kernel void @_kernelPv(ptr addrspace(1) inreg nocapture writeonly %0, ptr %1) local_unnamed_addr #0 !dbg !15 {'
+            'ptr addrspace(1) inreg nocapture writeonly %0',
+            'ptr addrspace(1) inreg nocapture writeonly %0, ptr %1'
         ]
     else:
         assert kernel_signatures == [
-            "define amdgpu_kernel void @_kernel(ptr addrspace(1) inreg nocapture writeonly %0) local_unnamed_addr #0 !dbg !4 {"
+            "ptr addrspace(1) inreg nocapture writeonly %0"
         ]
