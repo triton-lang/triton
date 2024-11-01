triton-lang/triton

commit c54f9882aff504bf2ab62d0ba037fb042204dc90

[frontend] added overflow checks in `debug` mode (#4589)

python/triton/language/core.py
@@ -30,6 +30,7 @@ def builtin(fn: T) -> T:
     @wraps(fn)
     def wrapper(*args, **kwargs):
         if "_builder" not in kwargs or kwargs["_builder"] is None:
+            print(kwargs)
             raise ValueError("Did you forget to add @triton.jit ? "
                              "(`_builder` argument must be provided outside of JIT functions.)")
         return fn(*args, **kwargs)
@@ -440,6 +441,20 @@ class dtype:
             assert self.is_floating()
             return dtype.KIND.FLOATING
 
+    def get_int_max_value(self):
+        if self.is_int_signed():
+            return 2**(self.int_bitwidth - 1) - 1
+        if self.is_int_unsigned():
+            return 2**self.int_bitwidth - 1
+        assert False
+
+    def get_int_min_value(self):
+        if self.is_int_signed():
+            return -2**(self.int_bitwidth - 1)
+        if self.is_int_unsigned():
+            return 0
+        assert False
+
     @staticmethod
     def is_dtype(type_str):
         return type_str in dtype.SINT_TYPES + dtype.UINT_TYPES + dtype.FP_TYPES + dtype.OTHER_TYPES
@@ -755,31 +770,27 @@ class tensor(_value):
 
     @builtin
     def __add__(self, other, _builder=None):
-        other = _unwrap_if_constexpr(other)
-        return semantic.add(self, other, _builder)
+        return add(self, other, sanitize_overflow=True, _builder=_builder)
 
     @builtin
     def __radd__(self, other, _builder=None):
-        return self.__add__(other, _builder=_builder)
+        return add(other, self, sanitize_overflow=True, _builder=_builder)
 
     @builtin
     def __sub__(self, other, _builder=None):
-        other = _unwrap_if_constexpr(other)
-        return semantic.sub(self, other, _builder)
+        return sub(self, other, sanitize_overflow=True, _builder=_builder)
 
     @builtin
     def __rsub__(self, other, _builder=None):
-        other = _unwrap_if_constexpr(other)
-        return semantic.sub(other, self, _builder)
+        return sub(other, self, sanitize_overflow=True, _builder=_builder)
 
     @builtin
     def __mul__(self, other, _builder=None):
-        other = _unwrap_if_constexpr(other)
-        return semantic.mul(self, other, _builder)
+        return mul(self, other, sanitize_overflow=True, _builder=_builder)
 
     @builtin
     def __rmul__(self, other, _builder=None):
-        return self.__mul__(other, _builder=_builder)
+        return mul(other, self, sanitize_overflow=True, _builder=_builder)
 
     @builtin
     def __truediv__(self, other, _builder=None):
@@ -1869,6 +1880,33 @@ def where(condition, x, y, _builder=None):
 # -----------------------
 # Math
 # -----------------------
+
+
+@builtin
+def add(x, y, sanitize_overflow: constexpr = True, _builder=None):
+    x = _unwrap_if_constexpr(x)
+    y = _unwrap_if_constexpr(y)
+    x = semantic.to_tensor(x, _builder)
+    y = semantic.to_tensor(y, _builder)
+    return semantic.add(x, y, sanitize_overflow, _builder)
+
+
+@builtin
+def sub(x, y, sanitize_overflow: constexpr = True, _builder=None):
+    x = _unwrap_if_constexpr(x)
+    y = _unwrap_if_constexpr(y)
+    x = semantic.to_tensor(x, _builder)
+    y = semantic.to_tensor(y, _builder)
+    return semantic.sub(x, y, sanitize_overflow, _builder)
+
+
+@builtin
+def mul(x, y, sanitize_overflow: constexpr = True, _builder=None):
+    x = _unwrap_if_constexpr(x)
+    y = _unwrap_if_constexpr(y)
+    x = semantic.to_tensor(x, _builder)
+    y = semantic.to_tensor(y, _builder)
+    return semantic.mul(x, y, sanitize_overflow, _builder)
 
 
 @builtin

python/triton/runtime/jit.py
@@ -561,13 +561,14 @@ class JITFunction(KernelInterface[T]):
         ]
 
     def run(self, *args, grid, warmup, **kwargs):
+        kwargs["debug"] = kwargs.get("debug", False) or os.environ.get("TRITON_DEBUG", "0") == "1"
+
         # parse options
         from ..compiler import make_backend
         device = driver.active.get_current_device()
         stream = driver.active.get_current_stream(device)
         target = driver.active.get_current_target()
         backend = make_backend(target)
-        kwargs["debug"] = self.debug
 
         # Execute pre run hooks with args and kwargs
         for hook in self.pre_run_hooks:
@@ -697,7 +698,6 @@ class JITFunction(KernelInterface[T]):
         # JITFunction can be instantiated as kernel
         # when called with a grid using __getitem__
         self.kernel = None
-        self.debug = True if os.environ.get("TRITON_DEBUG", "0") == "1" else debug
         self.noinline = noinline
 
         # TODO(jlebar): Remove uses of these fields outside this file, then

python/test/unit/test_debug.py
@@ -1,0 +1,124 @@
+import os
+import pytest
+import torch
+import triton.language as tl
+import triton
+
+@pytest.mark.parametrize('cond, opt_flag, env_var', [
+    (cond, opt_flag, env_var) for cond in [True, False] \
+                              for opt_flag in [True, False] \
+                              for env_var in [True, False]\
+])
+@pytest.mark.forked
+def test_device_assert(cond, opt_flag, env_var, device="cuda"):
+    os.environ['TRITON_DEBUG'] = str(int(env_var))
+    torch.zeros([1], dtype=torch.int32, device=device)
+
+    @triton.jit
+    def _kernel(COND: tl.constexpr):
+        tl.device_assert(COND, 'test')
+
+    if not cond and (opt_flag or env_var):
+        with pytest.raises(RuntimeError):
+            _kernel[(1, )](cond, debug=opt_flag)
+            torch.cuda.synchronize()
+        return
+
+    _kernel[(1, )](cond, debug=opt_flag)
+    torch.cuda.synchronize()
+
+
+@pytest.mark.parametrize("cond", [False, True])
+def test_static_assert(cond):
+
+    @triton.jit
+    def _kernel(COND: tl.constexpr):
+        tl.static_assert(COND)
+
+    if not cond:
+        with pytest.raises(triton.compiler.errors.CompileTimeAssertionFailure):
+            _kernel[(1, )](cond)
+        return
+
+    _kernel[(1, )](cond)
+
+
+def _test_overflow(x, y, x_dtype, y_dtype, debug, should_overflow, tri_func, ref_func):
+    device = "cuda"
+    x = torch.tensor([x], dtype=getattr(torch, x_dtype), device=device)
+    y = torch.tensor([y], dtype=getattr(torch, y_dtype), device=device)
+    z = torch.empty_like(x)
+    if should_overflow and debug:
+        with pytest.raises(RuntimeError) as exc_info:
+            tri_func[(1, )](x, y, z, debug=debug)
+            torch.cuda.synchronize()
+        assert "device-side assert" in str(exc_info.value)
+    else:
+        tri_func[(1, )](x, y, z, debug=debug)
+        torch.cuda.synchronize()
+        assert int(z) == int(ref_func(x, y))
+
+
+# integer overflow sanitization
+
+
+@pytest.mark.parametrize("x, y, x_dtype, y_dtype, debug, should_overflow", [
+    (-2**31, -1, 'int32', 'int32', False, False),
+    (-2**31, -1, 'int32', 'int32', True, True),
+    (2**31 - 1, 1, 'int32', 'int32', True, True),
+    (2**31 - 1, 100, 'int32', 'int32', True, True),
+    (-2**31, 0, 'int32', 'int32', True, False),
+    (-2**31, 2, 'int32', 'int32', True, False),
+    (0, -1, 'int32', 'int32', True, False),
+    (-2**15, -1, 'int16', 'int16', True, True),
+    (2**15 - 1, 1, 'int16', 'int16', True, True),
+])
+@pytest.mark.forked
+def test_sanitize_int_add_overflow(x, y, x_dtype, y_dtype, debug, should_overflow):
+
+    @triton.jit
+    def _kernel_add(X, Y, Z):
+        tl.store(Z, tl.load(X) + tl.load(Y))
+
+    _test_overflow(x, y, x_dtype, y_dtype, debug, should_overflow, _kernel_add, lambda x, y: x + y)
+
+
+# mul overflow
+
+
+@pytest.mark.parametrize("x, y, x_dtype, y_dtype, debug, should_overflow", [
+    (2**30, 4, 'int32', 'int32', False, False),
+    (2**30, 4, 'int32', 'int32', True, True),
+    (2**30, 2, 'int32', 'int32', True, True),
+    (-2**30, -4, 'int32', 'int32', True, True),
+    (-2**31, 1, 'int32', 'int32', True, False),
+    (-2**30, 2, 'int32', 'int32', True, False),
+])
+@pytest.mark.forked
+def test_sanitize_int_mul_overflow(x, y, x_dtype, y_dtype, debug, should_overflow):
+
+    @triton.jit
+    def _kernel_mul(X, Y, Z):
+        tl.store(Z, tl.load(X) * tl.load(Y))
+
+    _test_overflow(x, y, x_dtype, y_dtype, debug, should_overflow, _kernel_mul, lambda x, y: x * y)
+
+
+# sub overflow
+
+
+@pytest.mark.parametrize("x, y, x_dtype, y_dtype, debug, should_overflow", [
+    (-2**31, 1, 'int32', 'int32', False, False),
+    (-2**31, 1, 'int32', 'int32', True, True),
+    (2**31 - 1, -1, 'int32', 'int32', True, True),
+    (2**31 - 1, 1, 'int32', 'int32', True, False),
+    (-2**31, -1, 'int32', 'int32', True, False),
+])
+@pytest.mark.forked
+def test_sanitize_int_sub_overflow(x, y, x_dtype, y_dtype, debug, should_overflow):
+
+    @triton.jit
+    def _kernel_sub(X, Y, Z):
+        tl.store(Z, tl.load(X) - tl.load(Y))
+
+    _test_overflow(x, y, x_dtype, y_dtype, should_overflow, debug, _kernel_sub, lambda x, y: x - y)

.github/workflows/integration-tests.yml
@@ -226,7 +226,7 @@ jobs:
       - name: Install pip dependencies
         run: |
           python3 -m pip install --upgrade pip
-          python3 -m pip install wheel cmake==3.24 ninja pytest-xdist lit
+          python3 -m pip install cython setuptools wheel cmake==3.24 ninja pytest-forked pytest-xdist lit
       - name: Install Triton
         env:
           TRITON_BUILD_WITH_CCACHE: "true"
@@ -250,8 +250,9 @@ jobs:
             echo "Coult not find '${SHARED_LIB_DIR}'" ; exit -1
           fi
           cd python/test/unit
-          python3 -m pytest -s -n 8 --ignore=hopper/test_flashattention.py --ignore=language/test_line_info.py --ignore=language/test_subprocess.py
+          python3 -m pytest -s -n 8 --ignore=hopper/test_flashattention.py --ignore=language/test_line_info.py --ignore=language/test_subprocess.py --ignore=test_debug.py
           python3 -m pytest -s -n 8 language/test_subprocess.py
+          python3 -m pytest -s -n 8 test_debug.py --forked
           # Run test_line_info.py separately with TRITON_DISABLE_LINE_INFO=0
           TRITON_DISABLE_LINE_INFO=0 python3 -m pytest -s language/test_line_info.py
           # Run hopper/test_flashattention.py separately to avoid out of gpu memory
@@ -407,7 +408,10 @@ jobs:
           pytest --capture=tee-sys -rfs python/tutorials/06-fused-attention.py
           cd python/test/unit
           pytest --capture=tee-sys -rfs -n 16 language runtime \
-                 --ignore=language/test_line_info.py
+                 --ignore=language/test_line_info.py \
+                 --ignore=test_debug.py
+          # TODO: uncomment
+          # pytest --capture=tee-sys -rfs test_debug.py
           TRITON_ALWAYS_COMPILE=1 TRITON_DISABLE_LINE_INFO=0 LLVM_PASS_PLUGIN_PATH=${SHARED_LIB_DIR}/libGPUHello.so \
           pytest --capture=tee-sys -rfs -vvv instrumentation/test_gpuhello.py
 

.github/workflows/integration-tests.yml.in
@@ -256,7 +256,7 @@ jobs:
       - name: Install pip dependencies
         run: |
           python3 -m pip install --upgrade pip
-          python3 -m pip install wheel cmake==3.24 ninja pytest-xdist lit
+          python3 -m pip install cython setuptools wheel cmake==3.24 ninja pytest-forked pytest-xdist lit
 
       - name: Install Triton
         env:
@@ -284,8 +284,9 @@ jobs:
             echo "Coult not find '${SHARED_LIB_DIR}'" ; exit -1
           fi
           cd python/test/unit
-          python3 -m pytest -s -n 8 --ignore=hopper/test_flashattention.py --ignore=language/test_line_info.py --ignore=language/test_subprocess.py
+          python3 -m pytest -s -n 8 --ignore=hopper/test_flashattention.py --ignore=language/test_line_info.py --ignore=language/test_subprocess.py --ignore=test_debug.py
           python3 -m pytest -s -n 8 language/test_subprocess.py
+          python3 -m pytest -s -n 8 test_debug.py --forked
           # Run test_line_info.py separately with TRITON_DISABLE_LINE_INFO=0
           TRITON_DISABLE_LINE_INFO=0 python3 -m pytest -s language/test_line_info.py
           # Run hopper/test_flashattention.py separately to avoid out of gpu memory
@@ -403,7 +404,10 @@ jobs:
           pytest --capture=tee-sys -rfs python/tutorials/06-fused-attention.py
           cd python/test/unit
           pytest --capture=tee-sys -rfs -n 16 language runtime \
-                 --ignore=language/test_line_info.py
+                 --ignore=language/test_line_info.py \
+                 --ignore=test_debug.py
+          # TODO: uncomment
+          # pytest --capture=tee-sys -rfs test_debug.py
           TRITON_ALWAYS_COMPILE=1 TRITON_DISABLE_LINE_INFO=0 LLVM_PASS_PLUGIN_PATH=${SHARED_LIB_DIR}/libGPUHello.so \
           pytest --capture=tee-sys -rfs -vvv instrumentation/test_gpuhello.py
 

python/test/unit/language/assert_helper.py
@@ -1,154 +1,0 @@
-import sys
-
-import torch
-from torch.testing import assert_close
-
-import triton
-import triton.language as tl
-
-
-def get_current_target_warp_size():
-    return triton.runtime.driver.active.get_current_target().warp_size
-
-
-@triton.jit
-def kernel_device_assert(X, Y, BLOCK: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    tl.device_assert(x == 0, "x != 0")
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-@triton.jit
-def kernel_assert_passes(X, Y, BLOCK: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    # Trivial assert, should not be an error.
-    tl.device_assert(0 == 0, "x != 0")
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-@triton.jit(debug=False)
-def kernel_device_assert_no_debug(X, Y, BLOCK: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    tl.device_assert(x == 0, "x != 0")
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-@triton.jit
-def kernel_assert(X, Y, BLOCK: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    assert x == 0, "x != 0"
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-@triton.jit
-def kernel_static_assert(X, Y, BLOCK: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    tl.static_assert(BLOCK == 128, "BLOCK != 128")
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-def test_assert(func: str, device: str):
-    N = 128  # This value should match with test_print in test_subprocess.py.
-    num_warps = N // get_current_target_warp_size()
-
-    x = torch.arange(0, N, dtype=torch.int32, device=device)
-    y = torch.zeros((N, ), dtype=x.dtype, device=device)
-    if func == "device_assert":
-        kernel_device_assert[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-    if func == "device_assert_passes":
-        # Assert passes; no error.
-        kernel_assert_passes[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-    elif func == "no_debug":
-        # TRITON_DEBUG=1 can override the debug flag
-        kernel_device_assert_no_debug[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-    elif func == "assert":
-        kernel_assert[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-    elif func == "static_assert":
-        kernel_static_assert[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-    elif func == "double_assert":
-        # Launching a different kernel after the first one asserted used to
-        # segfault.  What seems to have happened is:
-        #  - The first kernel is enqueued but doesn't run yet.
-        #  - We go to launch the second kernel.  Because this is the first time
-        #    we're running it, we have to load the kernel into the GPU.
-        #  - Loading the kernel takes some time, during which the first launch
-        #    completes.
-        #  - Now the GPU is in an error state.  We need to detect this inside
-        #    the kernel-launch/loading code and bail out properly.  If we don't,
-        #    we segfault.
-        kernel_device_assert[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-        kernel_assert_passes[(1, )](x, y, num_warps=num_warps, BLOCK=N)
-    assert_close(y, x)
-    # GPU/host synchronization before exiting the test.
-    getattr(torch, device).synchronize()
-
-
-@triton.jit
-def jit_device_assert_none(x):
-    tl.device_assert(x == 0, "x != 0")
-
-
-@triton.jit(debug=True)
-def jit_device_assert_true(x):
-    tl.device_assert(x == 0, "x != 0")
-
-
-@triton.jit(debug=False)
-def jit_device_assert_false(x):
-    tl.device_assert(x == 0, "x != 0")
-
-
-@triton.jit
-def kernel_device_assert_nested(X, Y, BLOCK: tl.constexpr, jit_debug: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    if jit_debug == "true":
-        jit_device_assert_true(x)
-    elif jit_debug == "false":
-        jit_device_assert_false(x)
-    else:
-        jit_device_assert_none(x)
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-@triton.jit(debug=True)
-def kernel_device_assert_nested_true(X, Y, BLOCK: tl.constexpr, jit_debug: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    if jit_debug == "true":
-        jit_device_assert_true(x)
-    elif jit_debug == "false":
-        jit_device_assert_false(x)
-    else:
-        jit_device_assert_none(x)
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-@triton.jit(debug=False)
-def kernel_device_assert_nested_false(X, Y, BLOCK: tl.constexpr, jit_debug: tl.constexpr):
-    x = tl.load(X + tl.arange(0, BLOCK))
-    if jit_debug == "true":
-        jit_device_assert_true(x)
-    elif jit_debug == "false":
-        jit_device_assert_false(x)
-    else:
-        jit_device_assert_none(x)
-    tl.store(Y + tl.arange(0, BLOCK), x)
-
-
-def test_assert_nested(caller: str, callee: str, device: str):
-    N = 128  # This value should match with test_print in test_subprocess.py.
-    num_warps = N // get_current_target_warp_size()
-
-    x = torch.arange(0, N, dtype=torch.int32, device=device)
-    y = torch.zeros((N, ), dtype=x.dtype, device=device)
-    if caller == "none":
-        kernel_device_assert_nested[(1, )](x, y, num_warps=num_warps, BLOCK=N, jit_debug=callee)
-    elif caller == "true":
-        kernel_device_assert_nested_true[(1, )](x, y, num_warps=num_warps, BLOCK=N, jit_debug=callee)
-    elif caller == "false":
-        kernel_device_assert_nested_false[(1, )](x, y, num_warps=num_warps, BLOCK=N, jit_debug=callee)
-    assert_close(y, x)
-
-
-if __name__ == "__main__":
-    fn = globals()[sys.argv[1]]
-    fn(*sys.argv[2:])

python/test/unit/language/test_subprocess.py
@@ -8,11 +8,6 @@ import pytest
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print_path = os.path.join(dir_path, "print_helper.py")
-assert_path = os.path.join(dir_path, "assert_helper.py")
-
-# TODO: bfloat16 after LLVM-15
-assert_types = ["device_assert", "device_assert_passes", "assert", "static_assert", "no_debug", "double_assert"]
-nested_types = [(caller, callee) for caller in ["true", "false", "none"] for callee in ["true", "false", "none"]]
 torch_types = ["int8", "uint8", "int16", "int32", "long", "float16", "float32", "float64"]
 
 
@@ -120,59 +115,3 @@ def test_print(func_type: str, data_type: str, device: str):
             continue
         print(f'Expected line "{line}" {expected_lines[line]} time(s), but saw {actual_lines[line]} time(s)')
     assert all(delta == 0 for delta in diff.values())
-
-
-@pytest.mark.parametrize("func_type", assert_types)
-def test_assert(func_type: str, device: str):
-    # The total number of elements in the 1-D tensor to assert on.
-    N = 128
-
-    proc = subprocess.run(
-        [sys.executable, assert_path, "test_assert", func_type, device],
-        capture_output=True,
-        env={**os.environ, "TRITON_DEBUG": "1"},
-    )
-    errs = proc.stderr.splitlines()
-    num_errs = 0
-    for err in errs:
-        if "x != 0" in err.decode("utf-8", errors="ignore"):
-            num_errs += 1
-
-    # Check for segfaults.
-    assert all("segmentation fault" not in line.decode("utf-8", errors="ignore").lower() for line in errs)
-
-    if func_type == "static_assert" or func_type == "device_assert_passes":
-        assert num_errs == 0
-    else:
-        assert num_errs == N - 1
-
-
-@pytest.mark.parametrize("caller_type, callee_type", nested_types)
-def test_assert_nested(caller_type, callee_type, device):
-    # The total number of elements in the 1-D tensor to assert on.
-    N = 128
-
-    proc = subprocess.run(
-        [sys.executable, assert_path, "test_assert_nested", caller_type, callee_type, device],
-        capture_output=True,
-    )
-    errs = proc.stderr.splitlines()
-    num_errs = 0
-    for err in errs:
-        if "x != 0" in err.decode("utf-8", errors="ignore"):
-            num_errs += 1
-    if caller_type == "none":
-        if callee_type == "true":
-            assert num_errs == N - 1
-        else:
-            assert num_errs == 0
-    elif caller_type == "true":
-        if callee_type == "false":
-            assert num_errs == 0
-        else:
-            assert num_errs == N - 1
-    elif caller_type == "false":
-        if callee_type == "true":
-            assert num_errs == N - 1
-        else:
-            assert num_errs == 0

python/test/unit/runtime/test_cache.py
@@ -427,23 +427,18 @@ def test_jit_warmup_cache(device) -> None:
 def test_jit_debug(device) -> None:
 
     @triton.jit
-    def kernel_add(a, b, o, N: tl.constexpr):
-        idx = tl.arange(0, N)
-        tl.device_assert(idx < 32, "idx < 32")
-        tl.store(o + idx, tl.load(a + idx) + tl.load(b + idx))
+    def kernel(tmp):
+        tl.device_assert(tl.load(tmp) == 1, "tmp == 1")
 
     device = getattr(torch, device).current_device()
-    assert len(kernel_add.cache[device]) == 0
-    kernel_add.warmup(torch.float32, torch.float32, torch.float32, 32, grid=(1, ))
-    assert len(kernel_add.cache[device]) == 1
-    kernel_add.debug = False
-    kernel_add.warmup(torch.float32, torch.float32, torch.float32, 32, grid=(1, ))
-    assert len(kernel_add.cache[device]) == 2
-    kernel_add.debug = True
-    kernel_add.warmup(torch.float32, torch.float32, torch.float32, 32, grid=(1, ))
-    assert len(kernel_add.cache[device]) == 3
-    bins = list(kernel_add.cache[device].values())
-    assert bins[2].asm['ttir'] != bins[1].asm['ttir']
+    tmp = torch.tensor([1], dtype=torch.int32, device="cuda")
+    assert len(kernel.cache[device]) == 0
+    kernel[(1, )](tmp, debug=False)
+    assert len(kernel.cache[device]) == 1
+    kernel[(1, )](tmp, debug=True)
+    assert len(kernel.cache[device]) == 2
+    bins = list(kernel.cache[device].values())
+    assert bins[0].asm['ttir'] != bins[1].asm['ttir']
 
 
 @triton.jit

commit d60da31776e2b9cc6801e365a95a8ca406e75b45

[FRONTEND] Fix @triton.jit(debug=True)
Reported in https://github.com/triton-lang/triton/pull/5033/files#r1825438371

This was broken by #4589

python/triton/language/core.py
@@ -29,7 +29,6 @@ def builtin(fn: T) -> T:
     @wraps(fn)
     def wrapper(*args, **kwargs):
         if "_builder" not in kwargs or kwargs["_builder"] is None:
-            print(kwargs)
             raise ValueError("Did you forget to add @triton.jit ? "
                              "(`_builder` argument must be provided outside of JIT functions.)")
         return fn(*args, **kwargs)

python/triton/runtime/jit.py
@@ -561,7 +561,7 @@ class JITFunction(KernelInterface[T]):
         ]
 
     def run(self, *args, grid, warmup, **kwargs):
-        kwargs["debug"] = kwargs.get("debug", False) or os.environ.get("TRITON_DEBUG", "0") == "1"
+        kwargs["debug"] = kwargs.get("debug", self.debug) or os.environ.get("TRITON_DEBUG", "0") == "1"
 
         # parse options
         from ..compiler import make_backend
@@ -698,6 +698,7 @@ class JITFunction(KernelInterface[T]):
         # JITFunction can be instantiated as kernel
         # when called with a grid using __getitem__
         self.kernel = None
+        self.debug = debug
         self.noinline = noinline
 
         # TODO(jlebar): Remove uses of these fields outside this file, then

python/test/unit/test_debug.py
@@ -4,27 +4,33 @@ import torch
 import triton.language as tl
 import triton
 
-@pytest.mark.parametrize('cond, opt_flag, env_var', [
-    (cond, opt_flag, env_var) for cond in [True, False] \
-                              for opt_flag in [True, False] \
-                              for env_var in [True, False]\
-])
+
+@pytest.mark.parametrize('cond', [True, False])
+@pytest.mark.parametrize('opt_flag', [True, False, None])
+@pytest.mark.parametrize('env_var', [True, False])
+@pytest.mark.parametrize('jit_flag', [True, False])
 @pytest.mark.forked
-def test_device_assert(cond, opt_flag, env_var, device):
+def test_device_assert(cond, opt_flag, env_var, jit_flag, device):
     os.environ['TRITON_DEBUG'] = str(int(env_var))
     torch.zeros([1], dtype=torch.int32, device=device)
 
-    @triton.jit
+    @triton.jit(debug=jit_flag)
     def _kernel(COND: tl.constexpr):
         tl.device_assert(COND, 'test')
 
-    if not cond and (opt_flag or env_var):
+    is_debug = env_var or (opt_flag if opt_flag is not None else jit_flag)
+
+    kwargs = {}
+    if opt_flag is not None:
+        kwargs["debug"] = opt_flag
+
+    if not cond and is_debug:
         with pytest.raises(RuntimeError):
-            _kernel[(1, )](cond, debug=opt_flag)
+            _kernel[(1, )](cond, **kwargs)
             getattr(torch, device).synchronize()
         return
 
-    _kernel[(1, )](cond, debug=opt_flag)
+    _kernel[(1, )](cond, **kwargs)
     getattr(torch, device).synchronize()
 
 