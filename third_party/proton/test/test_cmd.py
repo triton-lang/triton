9024c677817a2f5b6a2a94db4c614722cdee5665
Corbin Robeck <corbin.robeck@amd.com>
2024-11-06 03:34:37 +0000

    attempt to fix test after merge

--- third_party/proton/test/test_cmd.py
+++ third_party/proton/test/test_cmd.py
@@ -9,19 +9,6 @@ def test_help():
     ret = subprocess.check_call(["proton", "-h"], stdout=subprocess.DEVNULL)
     assert ret == 0
 
-
-def test_instrument_exec():
-
-    test_stderr = '0     matmul_kernel     instrument.py:43:20     SHARED     STORE\n'\
-                  '1     matmul_kernel     instrument.py:44:20     SHARED     STORE\n'\
-                  '2     matmul_kernel     instrument.py:43:20     SHARED     LOAD\n'\
-                  '3     matmul_kernel     instrument.py:44:20     SHARED     LOAD\n'
-
-    out = subprocess.Popen(["proton", "--instrument=print-mem-spaces", "instrument.py"], stderr=subprocess.PIPE,
-                           stdout=subprocess.PIPE)
-    assert test_stderr == out.stderr.read().decode()
-
-
 @pytest.mark.parametrize("mode", ["script", "python", "pytest"])
 def test_exec(mode, tmp_path: pathlib.Path):
     file_path = __file__
@@ -42,3 +29,14 @@ def test_exec(mode, tmp_path: pathlib.Path):
     kernels = data[0]["children"]
     assert len(kernels) == 2
     assert kernels[0]["frame"]["name"] == "test" or kernels[1]["frame"]["name"] == "test"
+def test_instrument_exec():
+
+    test_stderr = '0     matmul_kernel     instrument.py:43:20     SHARED     STORE\n'\
+                  '1     matmul_kernel     instrument.py:44:20     SHARED     STORE\n'\
+                  '2     matmul_kernel     instrument.py:43:20     SHARED     LOAD\n'\
+                  '3     matmul_kernel     instrument.py:44:20     SHARED     LOAD\n'
+
+    out = subprocess.Popen(["proton", "--instrument=print-mem-spaces", "instrument.py"], stderr=subprocess.PIPE,
+                           stdout=subprocess.PIPE)
+    assert test_stderr == out.stderr.read().decode()
+
