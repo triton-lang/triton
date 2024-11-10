d28f85a114bebbb74f59f75d3a0cfe5bfd663751
Alessandro Sangiorgi <asangior@redhat.com>
2024-11-10 15:37:05 -0600

    Use base32 instead of base64

--- python/triton/runtime/cache.py
+++ python/triton/runtime/cache.py
@@ -258,18 +258,7 @@ __cache_cls_nme = "DEFAULT"
 
 def _base64(key):
     # Assume key is a hex string.
-    encoded = base64.urlsafe_b64encode(bytes.fromhex(key)).decode("utf-8").rstrip("=")
-    return _ensure_safe_filename(encoded)
-
-
-def _ensure_safe_filename(encoded):
-    """
-    Prepends an underscore to the filename if it starts with a '-'
-    to prevent issues with command-line operations on Unix-like systems.
-    """
-    if encoded.startswith('-'):
-        return f"_{encoded}"
-    return encoded
+    return base64.b32encode(bytes.fromhex(key)).decode("utf-8").rstrip("=")
 
 
 def get_cache_manager(key) -> CacheManager:
