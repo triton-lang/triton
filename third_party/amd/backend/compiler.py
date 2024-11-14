5ab079b914613889ac139a238caf144f05e13199
Lei Zhang <antiagainst@gmail.com>
2024-11-14 18:03:20 +0000

    Fix lit test

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -228,14 +228,6 @@ class HIPBackend(BaseBackend):
         amd.passes.ttgpuir.add_optimize_epilogue(pm)
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
 
-        stream_prefetch = os.environ.get("TRITON_HIP_STREAM_PREFETCH", "0") == "1"
-        use_buffer_ops = os.environ.get("AMDGCN_USE_BUFFER_OPS", "0") == "1"
-
-        # The `local-prefetch` scheduling variant requires turning on stream prefetch and
-        # buffer ops.
-        if options.instruction_sched_variant == "local-prefetch":
-            stream_prefetch = use_buffer_ops = True
-
         if amd.has_matrix_core_feature(options.arch):
             assert options.num_stages != 0, ("Triton AMD backend pipeliner has been updated. "
                                              "We used to trigger software pipelining with "
@@ -250,6 +242,13 @@ class HIPBackend(BaseBackend):
         passes.ttgpuir.add_reduce_data_duplication(pm)
         if amd.has_matrix_core_feature(options.arch):
             amd.passes.ttgpuir.add_reorder_instructions(pm)
+
+        use_buffer_ops = os.environ.get("AMDGCN_USE_BUFFER_OPS", "0") == "1"
+
+        # The `local-prefetch` scheduling variant requires turning on buffer ops.
+        if options.instruction_sched_variant == "local-prefetch":
+            use_buffer_ops = True
+
         if use_buffer_ops:
             amd.passes.ttgpuir.add_canonicalize_pointers(pm)
             passes.common.add_canonicalizer(pm)
