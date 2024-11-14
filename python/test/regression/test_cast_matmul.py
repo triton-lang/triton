a9dda9e118c3a477c827f4d897af8176f0f38d05
Thomas Raoux <thomas.raoux@openai.com>
2024-11-14 14:11:33 -0800

    [TEST] Make mixed matmul test deterministic (#5151)
    This prevents surprises when some value may go above the tolerance
    threshold

--- python/test/regression/test_cast_matmul.py
+++ python/test/regression/test_cast_matmul.py
@@ -84,12 +84,13 @@ def test_cast_matmul(M, K, N, BLOCK_K, w_dtype, x_dtype, out_dtype):
 
     def init_tensor(dtype, shape):
         if dtype == torch.int8:
-            return torch.randint(0, 3, shape, device=device, dtype=dtype)
+            return torch.randint(0, 2, shape, device=device, dtype=dtype)
         elif dtype == torch.float8_e4m3fn or dtype == torch.float8_e5m2:
             return torch.randn(shape, device=device, dtype=torch.float16).to(dtype)
         else:
             return torch.randn(shape, device=device, dtype=dtype)
 
+    torch.manual_seed(42)
     a = init_tensor(w_dtype, (M, K))
     b = init_tensor(x_dtype, (K, N))
 
