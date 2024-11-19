2226cb68a1f5e3b95a667d4d39578245112d96be
Jungwook Park <Jungwook.Park@amd.com>
2024-11-19 09:21:44 -0600

    Add support for one block pingpong
    Currently support two mode:
    1. Small tile : Enabled when num_warps=4. Effective with the tile sizes
       {128x128x64, 256x128x32, 128x256x32}
    2. Large tile : Enabled when num_warps=8. Effective with 256x256x64 tile
       size and kpack=1 (kpack=2 spills)
    
    Added condBarrierOp in triton amdgpu dialect : functional but still
    considering design change.

--- third_party/amd/lib/TritonAMDGPUToLLVM/SPMDOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/SPMDOpToLLVM.cpp
@@ -1,5 +1,7 @@
+#include "Dialect/TritonAMDGPU/IR/Dialect.h"
 #include "PatternTritonGPUOpToLLVM.h"
 #include "Utility.h"
+#include "mlir/Dialect/LLVMIR/ROCDLDialect.h"
 
 using namespace mlir;
 
@@ -25,10 +27,50 @@ struct GetNumProgramsOpConversion
   }
 };
 
+struct CondBarrierOpConversion
+    : public ConvertOpToLLVMPattern<triton::amdgpu::condBarrierOp> {
+  using ConvertOpToLLVMPattern<
+      triton::amdgpu::condBarrierOp>::ConvertOpToLLVMPattern;
+
+  LogicalResult
+  matchAndRewrite(triton::amdgpu::condBarrierOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    Location loc = op->getLoc();
+    auto i32ty = rewriter.getIntegerType(32);
+    auto workIDX = rewriter.create<ROCDL::ThreadIdXOp>(loc, i32ty);
+    auto constZero = rewriter.create<LLVM::ConstantOp>(
+        loc, i32ty, IntegerAttr::get(i32ty, 0));
+    auto constWarpSize = rewriter.create<LLVM::ConstantOp>(
+        loc, i32ty, IntegerAttr::get(i32ty, 256));
+    auto warpIDX = rewriter.create<LLVM::SDivOp>(loc, workIDX, constWarpSize);
+    auto warpLow = rewriter.create<LLVM::ICmpOp>(loc, LLVM::ICmpPredicate::eq,
+                                                 warpIDX, constZero);
+    auto pred = rewriter.create<LLVM::ICmpOp>(loc, LLVM::ICmpPredicate::eq,
+                                              warpLow, op->getOperand(0));
+
+    Block *currentBlock = rewriter.getInsertionBlock();
+    Block *afterCondBarBlock =
+        rewriter.splitBlock(currentBlock, rewriter.getInsertionPoint());
+    Block *trueBlock = rewriter.createBlock(afterCondBarBlock);
+    rewriter.setInsertionPointToEnd(currentBlock);
+    rewriter.create<LLVM::CondBrOp>(loc, pred, trueBlock, afterCondBarBlock);
+
+    // conditional barrier
+    rewriter.setInsertionPointToStart(trueBlock);
+    rewriter.create<ROCDL::SBarrierOp>(loc);
+    rewriter.create<LLVM::BrOp>(loc, afterCondBarBlock);
+
+    rewriter.setInsertionPointToStart(afterCondBarBlock);
+    rewriter.eraseOp(op);
+    return success();
+  }
+};
+
 } // namespace
 
 void mlir::triton::AMD::populateSPMDOpToLLVMPattern(
     LLVMTypeConverter &typeConverter, RewritePatternSet &patterns,
     PatternBenefit benefit) {
   patterns.add<GetNumProgramsOpConversion>(typeConverter, benefit);
+  patterns.add<CondBarrierOpConversion>(typeConverter, benefit);
 }
--- third_party/amd/lib/TritonAMDGPUTransforms/BlockPingpong.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/BlockPingpong.cpp
@@ -3,8 +3,11 @@
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
+#include "third_party/amd/include/Dialect/TritonAMDGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
+
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"
 
 #define GEN_PASS_CLASSES
 #include "TritonAMDGPUTransforms/Passes.h"
@@ -17,9 +20,14 @@ class Pingponger {
   scf::ForOp forOp;
   SmallVector<Operation *> gLoadOps;
   SmallVector<Operation *> lLoadOps;
+  SmallVector<Operation *> lStoreOps;
   SmallVector<Operation *> dotOps;
-  SmallVector<SmallVector<Value>> loadSlices;
-  SmallVector<Operation *> dotSlices;
+  SmallVector<SmallVector<Operation *>> subViewOps;
+  SmallVector<SmallVector<Operation *>> loadSliceOps;
+  SmallVector<Operation *> dotSliceOps;
+  SmallVector<Value> constOffsets;
+
+  Operation *lastInsertedOp;
 
   IntegerAttr schedMaskAttr;
   IntegerAttr schedMaskAttr0;
@@ -27,19 +35,33 @@ class Pingponger {
   IntegerAttr highPrioAttr;
 
   int32_t kWidth;
+  int32_t numWarps;
 
 public:
-  Pingponger(scf::ForOp forOp) : forOp(forOp){};
+  Pingponger(scf::ForOp forOp, int32_t numWarps)
+      : forOp(forOp), numWarps(numWarps){};
   void getDotPingponged();
+  void genOffsetConstants(Location loc, OpBuilder &builder, unsigned numSlices,
+                          int64_t sliceWidth);
   LogicalResult genLocalSlice(OpBuilder &builder, Value v,
                               Attribute dotEncoding, unsigned opIdx,
-                              unsigned numSlices);
-  void transformOneWave(OpBuilder &builder, Location loc);
-  void transformTwoWave(OpBuilder &builder, Location loc);
+                              unsigned numSlices, int64_t sliceWidth);
+  void transformOneWarp(OpBuilder &builder, Location loc);
+  void transformTwoWarp(OpBuilder &builder, Location loc);
+  void initOpInsertion(Operation *Op);
+  void attachOp(Operation *Op);
 };
 
-// Transform each wave coming from the different blocks
-void Pingponger::transformOneWave(OpBuilder &builder, Location loc) {
+void Pingponger::initOpInsertion(Operation *op) { lastInsertedOp = op; }
+
+void Pingponger::attachOp(Operation *op) {
+  assert(lastInsertedOp != nullptr);
+  op->moveAfter(lastInsertedOp);
+  lastInsertedOp = op;
+}
+
+// Transform each warp coming from the different blocks
+void Pingponger::transformOneWarp(OpBuilder &builder, Location loc) {
   // Splitting loading A and B inorder to prevent global/local load units
   // from the congestion.
   // Locate global load at the end. Otherwise, local_load at the end of
@@ -64,42 +86,42 @@ void Pingponger::transformOneWave(OpBuilder &builder, Location loc) {
   setPrioBack0->moveAfter(dotOps[0]);
 }
 
+void Pingponger::genOffsetConstants(Location loc, OpBuilder &builder,
+                                    unsigned numSlices, int64_t sliceWidth) {
+  for (int i = 0; i < numSlices; i++) {
+    int64_t offset = sliceWidth * i;
+    constOffsets.push_back(
+        builder.create<arith::ConstantIntOp>(loc, offset, 32));
+  }
+}
+
 LogicalResult Pingponger::genLocalSlice(OpBuilder &builder, Value v,
                                         Attribute dotEncoding, unsigned opIdx,
-                                        unsigned numSlices) {
-  SmallVector<Value> slices;
-  //v.dump();
-  auto getIncomingOp = [this](Value v) -> Value {
-    if (auto arg = mlir::dyn_cast<BlockArgument>(v))
-      if (arg.getOwner()->getParentOp() == forOp.getOperation())
-        return forOp.getTiedLoopInit(arg)->get();
-    return Value();
-  };
-
-  auto memDesc = getIncomingOp(v.getDefiningOp()->getOperand(0));
-
+                                        unsigned numSlices,
+                                        int64_t sliceWidth) {
+  SmallVector<Operation *> slices;
+  SmallVector<Operation *> subviews;
+  auto memDesc = v.getDefiningOp()->getOperand(0);
   auto type = cast<triton::MemDescType>(memDesc.getType());
   auto encoding = cast<RankedTensorType>(v.getType()).getEncoding();
-  //auto encoding = cast<RankedTensorType>(type.getEncoding());
-  //auto encoding = type.getEncoding();
-  auto srcEncoding =
-      mlir::cast<ttg::DotOperandEncodingAttr>(encoding);
-  //auto srcEncoding = mlir::cast<ttg::DotOperandEncodingAttr>(dotEncoding);
+  auto srcEncoding = mlir::cast<ttg::DotOperandEncodingAttr>(encoding);
   SmallVector<int64_t> shape{type.getShape().begin(), type.getShape().end()};
   Type elementType = type.getElementType();
 
   int64_t kIdx = opIdx == 0 ? 1 : 0;
-  int64_t sliceWidth = shape[kIdx] / numSlices;
+  shape[kIdx] = sliceWidth;
   if (sliceWidth < 16)
     return failure();
 
   for (int i = 0; i < numSlices; i++) {
-    int64_t offset = sliceWidth * i;
+
     SmallVector<Value> offsetsVal;
-    offsetsVal.push_back(
-        builder.create<arith::ConstantIntOp>(v.getLoc(), 0, 32));
-    offsetsVal.push_back(
-        builder.create<arith::ConstantIntOp>(v.getLoc(), offset, 32));
+    SmallVector<int64_t> offsets = {0, 0};
+    offsets[kIdx] = i;
+    for (int64_t off : offsets) {
+      offsetsVal.push_back(constOffsets[off]);
+    }
+
     Value newSmem = builder.create<triton::gpu::MemDescSubviewOp>(
         v.getLoc(),
         triton::MemDescType::get(shape, elementType, type.getEncoding(),
@@ -110,16 +132,72 @@ LogicalResult Pingponger::genLocalSlice(OpBuilder &builder, Value v,
     Value prefetchSlice = builder.create<triton::gpu::LocalLoadOp>(
         v.getLoc(), RankedTensorType::get(shape, elementType, dotOperandEnc),
         newSmem);
-    slices.push_back(prefetchSlice);
+    subviews.push_back(newSmem.getDefiningOp());
+    slices.push_back(prefetchSlice.getDefiningOp());
   }
 
-  loadSlices.push_back(slices);
+  subViewOps.push_back(subviews);
+  loadSliceOps.push_back(slices);
   return success();
 }
 
-// Transform two waves coming from the same blocks
-void Pingponger::transformTwoWave(OpBuilder &builder, Location loc) {
-  // Schedule operations as below
+// Transform two warps coming from the same blocks
+void Pingponger::transformTwoWarp(OpBuilder &builder, Location loc) {
+
+  // First, slice local_loads and dot into 4 parts
+  unsigned numSlices = 4;
+  auto op = cast<triton::DotOp>(dotOps[0]);
+
+  builder.setInsertionPointToStart(forOp.getBody());
+  auto typeB = op.getB().getType();
+  auto shapeB = typeB.getShape();
+  int64_t sliceWidth = shapeB[0] / numSlices;
+  genOffsetConstants(loc, builder, numSlices, sliceWidth);
+
+  builder.setInsertionPointAfter(gLoadOps[0]);
+  auto dotEncoding = op.getType().getEncoding();
+  if (genLocalSlice(builder, op.getA(), dotEncoding, 0, numSlices, sliceWidth)
+          .failed() ||
+      genLocalSlice(builder, op.getB(), dotEncoding, 1, numSlices, sliceWidth)
+          .failed())
+    return;
+
+  // Clone dots four times to consume the slices
+  Operation *prevDot = op;
+  for (int i = 0; i < numSlices; i++) {
+    auto newOp = builder.clone(*op);
+    newOp->setOperand(0, loadSliceOps[0][i]->getResult(0));
+    newOp->setOperand(1, loadSliceOps[1][i]->getResult(0));
+    if (i > 0)
+      newOp->setOperand(2, prevDot->getResult(0));
+    prevDot = newOp;
+    dotSliceOps.push_back(newOp);
+  }
+  op->replaceAllUsesWith(prevDot);
+  op->erase();
+  lLoadOps[0]->erase();
+  lLoadOps[1]->erase();
+
+  SmallVector<Operation *> schedBarriers;
+  SmallVector<Operation *> barriers;
+  SmallVector<Operation *> setPrioHighs;
+  SmallVector<Operation *> setPrioLows;
+
+  for (int i = 0; i < 8; i++) {
+    schedBarriers.push_back(
+        builder.create<ROCDL::SchedBarrier>(loc, schedMaskAttr0));
+    // MembarAnalysis can recognize gpu::BarrierOp and skip inserting additional
+    // barrier
+    barriers.push_back(builder.create<gpu::BarrierOp>(loc));
+
+    if (i < 4) { // only required by dots
+      setPrioHighs.push_back(
+          builder.create<ROCDL::SetPrioOp>(loc, highPrioAttr));
+      setPrioLows.push_back(builder.create<ROCDL::SetPrioOp>(loc, lowPrioAttr));
+    }
+  }
+
+  // Reorder operations as below scheduling
   // mem1: global load A, local load A(1/4), local load B(1/4)
   // dot (1/4)
   // mem2: global load B, local load A(2/4), local load B(2/4)
@@ -129,50 +207,92 @@ void Pingponger::transformTwoWave(OpBuilder &builder, Location loc) {
   // mem3: local store A and B
   // dot (4/4)
 
-  // First, slice local_loads and dot into 4 parts
-  unsigned numSlices = 4;
-  auto op = cast<triton::DotOp>(dotOps[0]);
+  // mem1: global load A, local load A(1/4), local load B(1/4)
+  initOpInsertion(gLoadOps[1]);
+  attachOp(subViewOps[0][0]);
+  attachOp(subViewOps[1][0]);
+  attachOp(loadSliceOps[0][0]);
+  attachOp(loadSliceOps[1][0]);
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  schedBarriers.pop_back();
+  barriers.pop_back();
 
-  auto dotEncoding = op.getType().getEncoding();
-  if (genLocalSlice(builder, op.getA(), dotEncoding, 0, numSlices).failed() ||
-      genLocalSlice(builder, op.getB(), dotEncoding, 1, numSlices).failed())
-    return;
+  // dot (1/4)
+  attachOp(setPrioHighs.back());
+  attachOp(dotSliceOps[0]);
+  attachOp(setPrioLows.back());
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  setPrioHighs.pop_back();
+  setPrioLows.pop_back();
+  schedBarriers.pop_back();
+  barriers.pop_back();
 
-  // Clone dots four times and use slices
-  op->setOperand(0, loadSlices[0][0]);
-  op->setOperand(1, loadSlices[1][0]);
+  // mem2: global load B, local load A(2/4), local load B(2/4)
+  attachOp(gLoadOps[1]);
+  attachOp(subViewOps[0][1]);
+  attachOp(subViewOps[1][1]);
+  attachOp(loadSliceOps[0][1]);
+  attachOp(loadSliceOps[1][1]);
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  schedBarriers.pop_back();
+  barriers.pop_back();
 
-  Operation *prevDot = op;
-  for (int i=1; i<numSlices; i++){
-        auto newOp = builder.clone(*op);
-        newOp->setOperand(0, loadSlices[0][i]);
-        newOp->setOperand(1, loadSlices[1][i]);
-        newOp->setOperand(2, prevDot->getResult(0));
-        prevDot = newOp;
-  }
-  op->replaceAllUsesWith(prevDot);
-  lLoadOps[0]->erase();
-  lLoadOps[1]->erase();
-/*
-  lLoadOps[0]->moveBefore(gLoadOps[0]);
-  auto schedB0 = builder.create<ROCDL::SchedBarrier>(loc, schedMaskAttr);
-  schedB0->moveAfter(lLoadOps[0]);
-  auto schedB1 = builder.create<ROCDL::SchedBarrier>(loc, schedMaskAttr);
-  schedB1->moveAfter(gLoadOps[0]);
-  lLoadOps[1]->moveBefore(gLoadOps[1]);
-  auto schedB2 = builder.create<ROCDL::SchedBarrier>(loc, schedMaskAttr);
-  schedB2->moveAfter(lLoadOps[1]);
-  auto schedB3 = builder.create<ROCDL::SchedBarrier>(loc, schedMaskAttr0);
-  schedB3->moveAfter(gLoadOps[1]);
-  auto schedB4 = builder.create<ROCDL::SchedBarrier>(loc, schedMaskAttr0);
-  schedB4->moveAfter(dotOps[0]);
-  auto setPrio1 = builder.create<ROCDL::SetPrioOp>(loc, highPrioAttr);
-  auto setPrioBack0 = builder.create<ROCDL::SetPrioOp>(loc, lowPrioAttr);
-  setPrio1->moveBefore(dotOps[0]);
-  setPrioBack0->moveAfter(dotOps[0]);
+  // dot (2/4)
+  attachOp(setPrioHighs.back());
+  attachOp(dotSliceOps[1]);
+  attachOp(setPrioLows.back());
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  setPrioHighs.pop_back();
+  setPrioLows.pop_back();
+  schedBarriers.pop_back();
+  barriers.pop_back();
 
+  // mem3: local load A(3/4, 4/4), local load B(3/4, 4/4)
+  attachOp(subViewOps[0][2]);
+  attachOp(subViewOps[1][2]);
+  attachOp(loadSliceOps[0][2]);
+  attachOp(loadSliceOps[1][2]);
+  attachOp(subViewOps[0][3]);
+  attachOp(subViewOps[1][3]);
+  attachOp(loadSliceOps[0][3]);
+  attachOp(loadSliceOps[1][3]);
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  schedBarriers.pop_back();
+  barriers.pop_back();
 
-  */
+  // dot (3/4)
+  attachOp(setPrioHighs.back());
+  attachOp(dotSliceOps[2]);
+  attachOp(setPrioLows.back());
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  setPrioHighs.pop_back();
+  setPrioLows.pop_back();
+  schedBarriers.pop_back();
+  barriers.pop_back();
+
+  // mem3: local store A and B
+  initOpInsertion(lStoreOps[1]);
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  schedBarriers.pop_back();
+  barriers.pop_back();
+
+  // dot (4/4)
+  attachOp(setPrioHighs.back());
+  attachOp(dotSliceOps[3]);
+  attachOp(setPrioLows.back());
+  attachOp(schedBarriers.back());
+  attachOp(barriers.back());
+  setPrioHighs.pop_back();
+  setPrioLows.pop_back();
+  schedBarriers.pop_back();
+  barriers.pop_back();
 }
 
 void Pingponger::getDotPingponged() {
@@ -197,6 +317,8 @@ void Pingponger::getDotPingponged() {
         if (forOp.getTiedLoopInit(arg)->get())
           lLoadOps.push_back(op);
     }
+    if (isa<ttg::LocalStoreOp>(op))
+      lStoreOps.push_back(op);
     if (isa<triton::DotOp>(op))
       dotOps.push_back(op);
   });
@@ -204,9 +326,26 @@ void Pingponger::getDotPingponged() {
   if (gLoadOps.size() != 2 || lLoadOps.size() != 2 || dotOps.size() != 1)
     return;
 
-  
-  //transformOneWave(builder, loc);
-  transformTwoWave(builder, loc);
+  if (numWarps == 4) { // pingpong between warps from different blocks
+    transformOneWarp(builder, loc);
+  } else if (numWarps == 8) { // pingpong between warps from the same block
+    transformTwoWarp(builder, loc);
+    builder.setInsertionPointAfter(forOp);
+    // barrier before starting pingpong
+    auto preBarrier = builder.create<gpu::BarrierOp>(loc);
+
+    // condbarrier::second_half after forOp
+    auto constZero = builder.create<arith::ConstantIntOp>(loc, 0, 1);
+    auto condBarrierLow =
+        builder.create<triton::amdgpu::condBarrierOp>(loc, constZero);
+
+    // condbarrier::first_half after barrier
+    preBarrier->moveBefore(forOp);
+    builder.setInsertionPointAfter(preBarrier);
+    auto constOne = builder.create<arith::ConstantIntOp>(loc, 1, 1);
+    auto condBarrierHigh =
+        builder.create<triton::amdgpu::condBarrierOp>(loc, constOne);
+  }
 }
 
 class TritonAMDGPUBlockPingpongPass
@@ -216,15 +355,12 @@ public:
 
   void runOnOperation() override {
     ModuleOp m = getOperation();
-    int numWarps = ttg::TritonGPUDialect::getNumWarps(m);
-    //if (numWarps != 4)
-    //  return;
+    int32_t numWarps = ttg::TritonGPUDialect::getNumWarps(m);
 
     m.walk([&](scf::ForOp forOp) {
-      Pingponger pingponger(forOp);
+      Pingponger pingponger(forOp, numWarps);
       pingponger.getDotPingponged();
     });
-    m.dump();
   }
 };
 
