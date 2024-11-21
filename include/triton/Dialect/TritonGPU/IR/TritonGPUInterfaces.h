4cb22c32b38b6a0311d151bee287321e6c619e65
Jokeren <robinho364@gmail.com>
2024-11-20 19:47:36 -0500

    Update

--- include/triton/Dialect/TritonGPU/IR/TritonGPUInterfaces.h
+++ include/triton/Dialect/TritonGPU/IR/TritonGPUInterfaces.h
@@ -1,5 +1,6 @@
 #ifndef TRITON_GPU_DIALECT_INTERFACES_H
 #define TRITON_GPU_DIALECT_INTERFACES_H
+
 #include "triton/Dialect/TritonGPU/IR/AttrInterfaces.h.inc"
 #include "triton/Dialect/TritonGPU/IR/LinearLayoutConversions.h"
 
