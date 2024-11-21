5a2722a153b0e585696102f46b257e90f7aa6f40
Corbin Robeck <corbin.robeck@amd.com>
2024-11-21 19:50:09 +0000

    formatting

--- python/test/unit/address_sanitizer_helper.py
+++ python/test/unit/address_sanitizer_helper.py
@@ -1,10 +1,6 @@
-import os
-from contextlib import contextmanager
-
 import torch
 import triton
 import triton.language as tl
-
 
 size = 4096
 x = torch.rand(size, device='cuda')
@@ -13,20 +9,23 @@ output = torch.empty_like(x)
 n_elements = output.numel()
 grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
 
+
 @triton.jit
-def add_kernel(x_ptr,
-               y_ptr,
-               output_ptr,
-               n_elements,
-               BLOCK_SIZE: tl.constexpr,
-               ):
+def add_kernel(
+    x_ptr,
+    y_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
     pid = tl.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE) + 1
     x = tl.load(x_ptr + offsets)
     y = tl.load(y_ptr + offsets)
     output = x + y
-    tl.store(output_ptr + offsets, output)    
+    tl.store(output_ptr + offsets, output)
+
 
 pgm = add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
 amdgcn = pgm.asm['amdgcn']
