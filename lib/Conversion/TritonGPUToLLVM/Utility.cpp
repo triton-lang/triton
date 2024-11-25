9ddd2a1167762e7d5ee4da6b344d95be20f95e4b
Jokeren <robinho364@gmail.com>
2024-11-24 19:21:44 -0500

    Update

--- lib/Conversion/TritonGPUToLLVM/Utility.cpp
+++ lib/Conversion/TritonGPUToLLVM/Utility.cpp
@@ -427,7 +427,6 @@ loadSharedToDistributed(triton::gpu::LocalLoadOp op, RankedTensorType dstTy,
       allocShape =
           subview->getAttrOfType<DenseI64ArrayAttr>("allocation.shape");
       allocSharedEnc = subview->getAttrOfType<Attribute>("encoding");
-      llvm::errs() << "allocShape: " << allocShape << "\n";
       llvm::errs() << "allocSharedEnc: " << allocSharedEnc << "\n";
       break;
     }
