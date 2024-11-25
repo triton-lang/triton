4210274664ca70fd0ae88e05b89b5618891b0dda
Kyle Wang <ken.qrose@gmail.com>
2024-11-26 02:34:51 +0800

    [AMD] NFC: Drop v2 Suffix from Stream Pipeline (#5251)
    Since StreamPipelineV2 has been the default for a while, this
    commit promoted StreamPipelineV2 to the general
    StreamPipeline by removing 'v2' suffix.

--- third_party/amd/include/TritonAMDGPUTransforms/Passes.h
+++ third_party/amd/include/TritonAMDGPUTransforms/Passes.h
@@ -7,8 +7,8 @@
 
 namespace mlir {
 
-std::unique_ptr<Pass> createTritonAMDGPUStreamPipelineV2Pass(int numStages = 2,
-                                                             int prefetch = 0);
+std::unique_ptr<Pass> createTritonAMDGPUStreamPipelinePass(int numStages = 2,
+                                                           int prefetch = 0);
 
 std::unique_ptr<Pass>
 createTritonAMDGPUAccelerateMatmulPass(std::string archGenName = std::string(),
