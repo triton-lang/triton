53e9f38b8b59d914dbb76931d3f4ba812798923e
Jokeren <robinho364@gmail.com>
2024-11-27 17:26:28 -0500

    Update

--- lib/Dialect/TritonGPU/IR/LinearLayoutConversions.cpp
+++ lib/Dialect/TritonGPU/IR/LinearLayoutConversions.cpp
@@ -872,7 +872,8 @@ LinearLayout chooseStMatrixLayoutLeadingOffset(
   LinearLayout layout = LinearLayout::empty();
 
   if (instrN > numColsPerChunk) {
-    for (int logCol = 0; logCol < llvm::Log2_32(numColsPerChunk); logCol++) {
+    for (int logCol = 0; logCol < llvm::Log2_32(numColsPerChunk / 16);
+         logCol++) {
       int chunk = 1 << logCol;
       basesReg.push_back({16 * chunk, 0});
     }
@@ -893,7 +894,7 @@ LinearLayout chooseStMatrixLayoutLeadingOffset(
          LinearLayout::identity1D(instrN / numColsPerChunk, kReg, kOffset))
             .reshapeOuts({{kCol, instrN}, {kRow, numWarpRows}});
   } else {
-    for (int logCol = 0; logCol < llvm::Log2_32(instrN); logCol++) {
+    for (int logCol = 0; logCol < llvm::Log2_32(instrN / 16); logCol++) {
       int chunk = 1 << logCol;
       basesReg.push_back({16 * chunk, 0});
     }
