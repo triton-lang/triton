b7e060118af4352bc414df7ac404c396262ddcde
Anatoly Myachev <anatoly.myachev@intel.com>
2024-12-01 03:42:06 +0100

    Define `pytest-forked` and `pytest-xdist` as `tests` target deps (#5292)
    This way, the dependencies needed for testing are localized in one place
    - `setup.py` (instead of several), which makes maintenance easier.
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/setup.py
+++ python/setup.py
@@ -752,6 +752,8 @@ setup(
             "isort",
             "numpy",
             "pytest",
+            "pytest-forked",
+            "pytest-xdist",
             "scipy>=1.7.1",
             "llnl-hatchet",
         ],
