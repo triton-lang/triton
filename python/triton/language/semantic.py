5377eb89134fd3005bbb14dcfdc76447786222c9
Anatoly Myachev <anatoly.myachev@intel.com>
2024-12-06 16:07:40 +0100

    Mark dot' output bloat16 type as unsupported only for 'cuda' and 'hip' backend
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/triton/language/semantic.py
+++ python/triton/language/semantic.py
@@ -1496,8 +1496,13 @@ def dot(lhs: tl.tensor, rhs: tl.tensor, acc: tl.tensor, input_precision: Optiona
         _0 = builder.get_int32(0)
         ret_scalar_ty = tl.int32
     elif out_dtype.is_bf16():
-        raise ValueError(
-            "out_dtype=bfloat16 is unsupported. Please use out_dtype=float32/float16 and cast with `.to(tl.bfloat16)`")
+        if builder.options.backend_name in ('cuda', 'hip'):
+            raise ValueError(
+                "out_dtype=bfloat16 is unsupported. Please use out_dtype=float32/float16 and cast with `.to(tl.bfloat16)`"
+            )
+        else:
+            _0 = builder.get_bf16(0)
+            ret_scalar_ty = tl.bfloat16
     elif lhs.type.scalar.is_fp32() or lhs.type.scalar.is_bf16():
         _0 = builder.get_fp32(0)
         ret_scalar_ty = tl.float32
