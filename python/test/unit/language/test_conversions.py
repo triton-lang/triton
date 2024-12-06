148ebf5e1ae774efaa2d675a63ac5f49afcdee00
Ilya Enkovich <ilya.enkovich@intel.com>
2024-12-06 20:18:10 +0000

    Fix test_conversions.
    Signed-off-by: Ilya Enkovich <ilya.enkovich@intel.com>

--- python/test/unit/language/test_conversions.py
+++ python/test/unit/language/test_conversions.py
@@ -7,11 +7,8 @@ import pytest
 import triton
 import triton.language as tl
 
-from triton._internal_testing import is_cuda, is_hip, is_hip_mi300
+from triton._internal_testing import is_cuda, is_hip, is_hip_mi300, is_cpu
 
-
-def is_cpu():
-    return not is_interpreter() and triton.runtime.driver.active.get_current_target().backend == "cpu"
 
 def matching_int(dtype):
     if dtype.primitive_bitwidth == 8:
