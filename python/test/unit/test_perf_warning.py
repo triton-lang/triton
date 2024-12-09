11930a81cda30f9f4f5aae6f287303555e3f4395
Shuofei Zhu <shuofei@meta.com>
2024-12-09 00:50:46 -0800

    update
    add a unit test
    
    linter
    
    update
    
    update
    
    update
    
    update
    
    update
    
    clang-format
    
    rebase
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update

--- python/test/unit/test_perf_warning.py
+++ python/test/unit/test_perf_warning.py
@@ -186,3 +186,155 @@ def test_remark_swp_op_before_operands(capfd, fresh_triton_cache):
     i = torch.empty(64 * 64, dtype=torch.float32).cuda()
     o = torch.empty(64 * 64, dtype=torch.float32).cuda()
     kernel_pipe_error[(1, )](i, o)
+
+
+def test_remark_swp_op_before_operands_persistent_matmul(capfd, fresh_triton_cache):
+
+    # this example is from https://github.com/triton-lang/triton/issues/5172
+    @triton.jit
+    def matmul_kernel_persistent(a_ptr, b_ptr, c_ptr,  #
+                                 M, N, K,  #
+                                 stride_am, stride_ak,  #
+                                 stride_bk, stride_bn,  #
+                                 stride_cm, stride_cn,  #
+                                 BLOCK_SIZE_M: tl.constexpr,  #
+                                 BLOCK_SIZE_N: tl.constexpr,  #
+                                 BLOCK_SIZE_K: tl.constexpr,  #
+                                 GROUP_SIZE_M: tl.constexpr,  #
+                                 NUM_SMS: tl.constexpr,  #
+                                 ):
+        start_pid = tl.program_id(axis=0)
+        num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+        num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+        k_tiles = tl.cdiv(K, BLOCK_SIZE_K)
+        num_tiles = num_pid_m * num_pid_n
+
+        tiles_per_SM = num_tiles // NUM_SMS
+        if start_pid < num_tiles % NUM_SMS:
+            tiles_per_SM += 1
+
+        # tile_id = start_pid - NUM_SMS
+        tile_id = start_pid
+
+        ki = -1
+
+        offs_k_for_mask = tl.arange(0, BLOCK_SIZE_K)
+
+        num_pid_in_group = GROUP_SIZE_M * num_pid_n
+
+        pid_m = 0
+        pid_n = 0
+        offs_am = tl.arange(0, BLOCK_SIZE_M)
+        offs_bn = tl.arange(0, BLOCK_SIZE_N)
+
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+
+        for _ in range(0, k_tiles * tiles_per_SM):
+            ki = tl.where(ki == k_tiles - 1, 0, ki + 1)
+            if ki == 0:
+                # tile_id += NUM_SMS
+                group_id = tile_id // num_pid_in_group
+                first_pid_m = group_id * GROUP_SIZE_M
+                group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+                pid_m = first_pid_m + (tile_id % group_size_m)
+                pid_n = (tile_id % num_pid_in_group) // group_size_m
+
+                start_m = pid_m * BLOCK_SIZE_M
+                start_n = pid_n * BLOCK_SIZE_N
+                offs_am = start_m + tl.arange(0, BLOCK_SIZE_M)
+                offs_bn = start_n + tl.arange(0, BLOCK_SIZE_N)
+                offs_am = tl.where(offs_am < M, offs_am, 0)
+                offs_bn = tl.where(offs_bn < N, offs_bn, 0)
+                offs_am = tl.max_contiguous(tl.multiple_of(offs_am, BLOCK_SIZE_M), BLOCK_SIZE_M)
+                offs_bn = tl.max_contiguous(tl.multiple_of(offs_bn, BLOCK_SIZE_N), BLOCK_SIZE_N)
+            offs_k = ki * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
+            a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+            b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+            a = tl.load(a_ptrs, mask=offs_k_for_mask[None, :] < K - ki * BLOCK_SIZE_K, other=0.0)
+            b = tl.load(b_ptrs, mask=offs_k_for_mask[:, None] < K - ki * BLOCK_SIZE_K, other=0.0)
+            accumulator = tl.dot(a, b, accumulator)
+
+            if ki == k_tiles - 1:
+                offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+                offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+                c_ptrs = (c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :])
+                c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+                if c_ptr.dtype.element_ty == tl.float8e4nv:
+                    c = accumulator.to(tl.float8e4nv)
+                else:
+                    c = accumulator.to(tl.float16)
+                tl.store(c_ptrs, c, mask=c_mask)
+                accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+                tile_id += NUM_SMS  # this line is newly added
+
+    # We don't need any envirnoment variables to test this remark, as it reports as an error
+    with enable_diagnostics_context(''):
+        dtype = torch.float16
+
+        M = 8192
+        N = 8192
+        K = 512
+        a = torch.randn((M, K), device="cuda", dtype=torch.float16).to(dtype)
+        b = torch.randn((K, N), device="cuda", dtype=torch.float16).to(dtype)
+
+        b = b.T.contiguous().T
+
+        # Check constraints.
+        assert a.shape[1] == b.shape[0], "Incompatible dimensions"
+        assert a.dtype == b.dtype, "Incompatible dtypes"
+
+        # equals to 132 on H100
+        NUM_SMS = torch.cuda.get_device_properties("cuda").multi_processor_count
+
+        M, K = a.shape
+        K, N = b.shape
+        dtype = a.dtype
+        # Allocates output.
+        c = torch.empty((M, N), device=a.device, dtype=dtype)
+        # 1D launch kernel where each block gets its own program.
+        grid = lambda META: (min(NUM_SMS,
+                                 triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(N, META["BLOCK_SIZE_N"])), )
+        matmul_kernel_persistent[grid](
+            a, b, c,  #
+            M, N, K,  #
+            a.stride(0), a.stride(1),  #
+            b.stride(0), b.stride(1),  #
+            c.stride(0), c.stride(1),  #
+            BLOCK_SIZE_M=128,  #
+            BLOCK_SIZE_N=256,  #
+            BLOCK_SIZE_K=64,  #
+            GROUP_SIZE_M=8,  #
+            NUM_SMS=NUM_SMS,  #
+            num_stages=3,  #
+            num_warps=8,  #
+        )
+
+    _, err = capfd.readouterr()
+    # Split the output into lines for easier processing
+    lines = err.splitlines()
+    # Define the expected strings in order
+    expected_strings = [
+        "error: The software pipeliner failed due to a dependency conflict", "for _ in range",
+        "note: The loop body is divided into 3 stages to optimize GPU I/O and computation resources.",
+        "tile_id += NUM_SMS", "group_id = tile_id", "pid_m = first_pid_m + (tile_id % group_size_m)",
+        "pid_n = (tile_id % num_pid_in_group)"
+    ]
+    # Initialize an index to track the position in expected_strings
+    index = 0
+    # Iterate over each line in the output
+    for line in lines:
+        # Check if the current expected string is in the line
+        if expected_strings[index] in line:
+            # Move to the next expected string
+            index += 1
+            # If all expected strings have been found, break out of the loop
+            if index == len(expected_strings):
+                break
+        else:
+            print("line: ", line)
+            print("expected strings: ", expected_strings[index])
+    # Check if all expected strings were found
+    if index != len(expected_strings):
+        missing_string = expected_strings[index]
+        raise AssertionError(f"Missing expected string: '{missing_string}' from {err}")
