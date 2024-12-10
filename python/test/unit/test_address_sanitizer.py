3a8bb1549cf7cc486c0ecccddc3b2c618e88d3ff
Corbin Robeck <corbin.robeck@amd.com>
2024-12-10 15:06:01 +0000

    formatting

--- python/test/unit/test_address_sanitizer.py
+++ python/test/unit/test_address_sanitizer.py
@@ -1,9 +1,8 @@
 import os
 import subprocess
 
-
 # It is recommended to disable various memory caching strategies both within the ROCm stack and PyTorch
-# This will give the address sanitizer the best chance at finding the memory fault where it originates, 
+# This will give the address sanitizer the best chance at finding the memory fault where it originates,
 # otherwise it could be masked by writing past the end of a cached block within a larger allocation.
 os.environ["HSA_DISABLE_FRAGMENT_ALLOCATOR"] = "1"
 os.environ["AMD_PYTORCH_NO_CUDA_MEMORY_CACHING"] = "1"
@@ -14,6 +13,7 @@ os.environ["TRITON_ENABLE_ASAN"] = "1"
 # if this is not set and the xnack setting is configured be defult for xnack-
 # a runtime error some like "No kernel image found" will occur
 os.environ["HSA_XNACK"] = "1"
+
 
 def test_address_sanitizer():
     out = subprocess.Popen(["python", "address_sanitizer_helper.py"], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
