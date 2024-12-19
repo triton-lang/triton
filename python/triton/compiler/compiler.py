79efc584aba24f84d0ba07c245e925f9cd36e745
Corbin Robeck <corbin.robeck@amd.com>
2024-12-19 15:08:55 +0000

    add check for asan in disabling compiler multi-threading in MLIR PM

--- python/triton/compiler/compiler.py
+++ python/triton/compiler/compiler.py
@@ -304,7 +304,8 @@ def compile(src, target=None, options=None):
     # This is needed to safely finalize threads pool inside context: if current process forks before
     # python GC deletes context object, thread pool in child process will be invalid, which could
     # lead to child crash or hang.
-    context.disable_multithreading()
+    if not os.environ.get("TRITON_ENABLE_ASAN", "0") == "1":
+        context.disable_multithreading()
     # return handle to compiled kernel
     return CompiledKernel(src, metadata_group, hash)
 
