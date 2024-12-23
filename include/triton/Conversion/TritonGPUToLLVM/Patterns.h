d36f24f31dbf86aa7819b423c179c2a982dba2b4
Jokeren <robinho364@gmail.com>
2024-12-23 17:01:49 -0500

    Update

--- include/triton/Conversion/TritonGPUToLLVM/Patterns.h
+++ include/triton/Conversion/TritonGPUToLLVM/Patterns.h
@@ -13,13 +13,6 @@ namespace triton::gpu {
 /// |module| op because the codegen doesn't handle `blocked -> dot_op` directly.
 void decomposeBlockedToDotLayoutConversion(ModuleOp module);
 
-/// Replaces `mma/mfma -> dot_op` with `mma/mfma -> blocked -> dot_op` in the
-/// given |module| op, but bypass the decomposition if |shortcutFn| returns
-/// true.
-using ShortcutFn = std::function<bool(RankedTensorType, RankedTensorType)>;
-void decomposeTensorCoreToDotLayoutConversion(ModuleOp module,
-                                              ShortcutFn shortcutFn);
-
 } // namespace triton::gpu
 
 } // namespace mlir
