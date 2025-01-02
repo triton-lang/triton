e0e8f6883d7b5d15dd6fa6e281a33f8964ecf047
Peter Bell <peterbell10@openai.com>
2025-01-02 18:14:51 +0000

    [DEV] Add makefile to run common dev commands
    It's a bit annoying to type `ninja -C python/build/cmake-.../` to run
    build commands. This adds a top-level makefile that just forwards
    commands down to the ninja build system so you can just type `make`.
    
    Building and running the tests from a fresh install should now be as
    simple as `make dev-install && make test`.
    
    I also add `make golden-samples` to regenerate the golden sample tests,
    which should make it fairly painless to accept a failing sample.

--- python/setup.py
+++ python/setup.py
@@ -28,6 +28,8 @@ from wheel.bdist_wheel import bdist_wheel
 
 import pybind11
 
+from build_helpers import get_base_dir, get_cmake_dir
+
 
 @dataclass
 class Backend:
@@ -331,19 +333,6 @@ def download_and_copy(name, src_path, dst_path, variable, version, url_func):
 
 
 # ---- cmake extension ----
-
-
-def get_base_dir():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-
-
-def get_cmake_dir():
-    plat_name = sysconfig.get_platform()
-    python_version = sysconfig.get_python_version()
-    dir_name = f"cmake.{plat_name}-{sys.implementation.name}-{python_version}"
-    cmake_dir = Path(get_base_dir()) / "python" / "build" / dir_name
-    cmake_dir.mkdir(parents=True, exist_ok=True)
-    return cmake_dir
 
 
 class CMakeClean(clean):
