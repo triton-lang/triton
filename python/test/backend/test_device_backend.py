9ffd160db4804ff602e4e0b0bd6e3da85f45ceae
Anatoly Myachev <anatoly.myachev@intel.com>
2025-01-07 17:45:59 +0100

    Revert "use 'subprocess.run' with capture_output=True"
    This reverts commit de71ca040f372607eef8e6ea22b72a678b29d6b0.

--- python/test/backend/test_device_backend.py
+++ python/test/backend/test_device_backend.py
@@ -41,8 +41,7 @@ def build_for_backend(name, src, srcdir):
         scheme = 'posix_prefix'
     py_include_dir = sysconfig.get_paths(scheme=scheme)["include"]
 
-    cc_cmd = [cc, src, f"-I{py_include_dir}", f"-I{srcdir}", "-shared", "-fPIC", "-o", so]
-    subprocess.run(cc_cmd, capture_output=True, text=True, check=True)
+    subprocess.check_call([cc, src, f"-I{py_include_dir}", f"-I{srcdir}", "-shared", "-fPIC", "-o", so])
     return so
 
 
