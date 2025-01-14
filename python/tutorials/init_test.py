5e0b4acca32b287866aaca1b7b0cc404f8664c8a
Corbin Robeck <corbin.robeck@amd.com>
2025-01-14 16:42:11 +0000

    temp

--- python/tutorials/init_test.py
+++ python/tutorials/init_test.py
@@ -19,36 +19,26 @@ def is_cdna():
 @triton.jit
 def softmax_kernel(output_ptr, input_ptr, input_row_stride, output_row_stride, n_rows, n_cols, BLOCK_SIZE: tl.constexpr,
                    num_stages: tl.constexpr):
-    # starting row of the program
     row_start = tl.program_id(0)
     row_step = tl.num_programs(0)
 
     pl.record(True, 0)
     for row_idx in tl.range(row_start, n_rows, row_step, num_stages=num_stages):
-        # The stride represents how much we need to increase the pointer to advance 1 row
         row_start_ptr = input_ptr + row_idx * input_row_stride
-        # The block size is the next power of two greater than n_cols, so we can fit each
-        # row in a single block
         col_offsets = tl.arange(0, BLOCK_SIZE)
         input_ptrs = row_start_ptr + col_offsets
-        # Load the row into SRAM, using a mask since BLOCK_SIZE may be > than n_cols
         mask = col_offsets < n_cols
         row = tl.load(input_ptrs, mask=mask, other=-float('inf'))
-        # Subtract maximum for numerical stability
         row_minus_max = row - tl.max(row, axis=0)
-        # Note that exponentiation in Triton is fast but approximate (i.e., think __expf in CUDA)
         numerator = tl.exp(row_minus_max)
         denominator = tl.sum(numerator, axis=0)
         softmax_output = numerator / denominator
-        # Write back output to DRAM
         output_row_start_ptr = output_ptr + row_idx * output_row_stride
         output_ptrs = output_row_start_ptr + col_offsets
         tl.store(output_ptrs, softmax_output, mask=mask)
 
     pl.record(False, 0)
 
-# %%
-# We can create a helper function that enqueues the kernel and its (meta-)arguments for any given input tensor.
 
 properties = driver.active.utils.get_device_properties(DEVICE.index)
 NUM_SM = properties["multiprocessor_count"]
@@ -64,41 +54,21 @@ def test_proton_buff_init(tmp_path: pathlib.Path):
     
     n_rows, n_cols = x.shape
     
-    # The block size of each loop iteration is the smallest power of two greater than the number of columns in `x`
     BLOCK_SIZE = triton.next_power_of_2(n_cols)
     
-    # Another trick we can use is to ask the compiler to use more threads per row by
-    # increasing the number of warps (`num_warps`) over which each row is distributed.
-    # You will see in the next tutorial how to auto-tune this value in a more natural
-    # way so you don't have to come up with manual heuristics yourself.
     num_warps = 8
-    
-    # Number of software pipelining stages.
     num_stages = 4 if SIZE_SMEM > 200000 else 2
     
-    # Allocate output
     y = torch.empty_like(x)
     
-    # pre-compile kernel to get register usage and compute thread occupancy.
     kernel = softmax_kernel.warmup(y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE=BLOCK_SIZE,
                                    num_stages=num_stages, num_warps=num_warps, grid=(1, ))
     kernel._init_handles()
     n_regs = kernel.n_regs
     size_smem = kernel.metadata.shared
     if is_hip():
-        # NUM_REGS represents the number of regular purpose registers. On CDNA architectures this is half of all registers available.
-        # However, this is not always the case. In most cases all registers can be used as regular purpose registers.
-        # ISA SECTION (3.6.4 for CDNA3)
-        # VGPRs are allocated out of two pools: regular VGPRs and accumulation VGPRs. Accumulation VGPRs are used
-        # with matrix VALU instructions, and can also be loaded directly from memory. A wave may have up to 512 total
-        # VGPRs, 256 of each type. When a wave has fewer than 512 total VGPRs, the number of each type is flexible - it is
-        # not required to be equal numbers of both types.
         if is_cdna():
             NUM_GPRS = NUM_REGS * 2
-    
-        # MAX_NUM_THREADS represents maximum number of resident threads per multi-processor.
-        # When we divide this number with WARP_SIZE we get maximum number of waves that can
-        # execute on a CU (multi-processor)  in parallel.
         MAX_NUM_THREADS = properties["max_threads_per_sm"]
         max_num_waves = MAX_NUM_THREADS // WARP_SIZE
         occupancy = min(NUM_GPRS // WARP_SIZE // n_regs, max_num_waves) // num_warps
@@ -109,6 +79,5 @@ def test_proton_buff_init(tmp_path: pathlib.Path):
     
     num_programs = min(num_programs, n_rows)
     
-    # Create a number of persistent programs.
     llir = kernel.asm['llir']
     assert "@proton_smem = external local_unnamed_addr addrspace(3) global" in llir
