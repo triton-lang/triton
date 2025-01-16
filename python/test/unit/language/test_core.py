94f80f476b2c7e9a76c431454b87ceb7491bf51d
Jeff Niu <jeffniu22@gmail.com>
2025-01-16 15:49:46 -0800

    [Frontend] Validate minimum TMA block size in the frontend (#5611)
    The current TMA implementation requires the TMA block shape to have a
    minimum size, which is the swizzle tile size in smem. Add an error
    message for this in the frontend so users stop running into codegen
    crashes.

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -4733,6 +4733,34 @@ def test_reshape_err(device):
     assert "reshape" in str(exc_info.value)
 
 
+def test_tma_load_block_shape_err():
+
+    @triton.jit
+    def kernel(ptr):
+        desc = tl._experimental_make_tensor_descriptor(ptr, [128, 128], [128, 1], [1, 32])
+        desc.load([0, 0])
+
+    input = torch.empty((128, 128), dtype=torch.int32, device='cuda')
+    with pytest.raises(triton.CompilationError) as e:
+        kernel[(1, )](input)
+
+    assert "tensor descriptor block shape must have at least 8 rows" in str(e.value.__cause__)
+
+
+def test_tma_store_block_shape_err():
+
+    @triton.jit
+    def kernel(ptr):
+        desc = tl._experimental_make_tensor_descriptor(ptr, [128, 128], [128, 1], [8, 8])
+        desc.store([0, 0], tl.zeros((1, 32), dtype=tl.int16))
+
+    input = torch.empty((128, 128), dtype=torch.int16, device='cuda')
+    with pytest.raises(triton.CompilationError) as e:
+        kernel[(1, )](input)
+
+    assert "int16 tensor descriptor block shape must have at least 16 columns" in str(e.value.__cause__)
+
+
 def test_trans_reshape(device):
 
     @triton.jit
