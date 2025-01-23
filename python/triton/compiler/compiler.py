a00faed09b8d75113dc2c540cf4a84a5ef092154
fulvius31 <mail@alessandrosangiorgi.net>
2025-01-22 18:25:08 -0600

    Add the possibility to store only cubin/hsaco file on the cache directory

--- python/triton/compiler/compiler.py
+++ python/triton/compiler/compiler.py
@@ -233,6 +233,7 @@ def compile(src, target=None, options=None):
     # core changes to make it easier to track kernels by hash.
     enable_override = os.environ.get("TRITON_KERNEL_OVERRIDE", "0") == "1"
     enable_ir_dump = os.environ.get("TRITON_KERNEL_DUMP", "0") == "1"
+    enable_only_cubin_hsaco = os.environ.get("TRITON_STORE_CUBIN_HSACO_ONLY", "0") == "1"
     fn_override_manager = get_override_manager(src.hash()) if enable_override else None
     fn_dump_manager = get_dump_manager(src.hash()) if enable_ir_dump else None
     # Pre-truncate the file name here to avoid hitting the 255 character limit on common platforms.
@@ -283,7 +284,8 @@ def compile(src, target=None, options=None):
         if (fn_override_manager is not None and (full_name := fn_override_manager.get_file(ir_filename)) is not None):
             print(f"\nOverriding kernel with file {full_name}")
             next_module = parse(full_name, ext, context)
-        metadata_group[ir_filename] = fn_cache_manager.put(next_module, ir_filename)
+        if (ext == "cubin" or ext == "hsaco") or not enable_only_cubin_hsaco:
+            metadata_group[ir_filename] = fn_cache_manager.put(next_module, ir_filename)
         if fn_dump_manager is not None:
             fn_dump_manager.put(next_module, ir_filename)
         # use an env variable to parse ir from file
@@ -429,3 +431,4 @@ class CompiledKernel:
                      CompiledKernel.launch_enter_hook, CompiledKernel.launch_exit_hook, *args)
 
         return runner
+
