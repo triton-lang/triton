c3ed21d0c9b39ad7c7d666b515e800a8ca82ca30
Pawel Szczerbuk <pawel.szczerbuk@openai.com>
2025-01-26 15:18:41 -0800

    Revert "Improve thread locality for reduction ops (#5671)"
    This reverts commit 1bb8b8055c81f6bb85055645a20e0dbd27d5295f.

--- lib/Dialect/TritonGPU/IR/Ops.cpp
+++ lib/Dialect/TritonGPU/IR/Ops.cpp
@@ -62,7 +62,7 @@ struct CanonicalizeConvertFromReshape
 
     if (isExpensiveView(convert.getSrc().getType(), op.getType()))
       return failure();
-    if (!op.getAllowReorder())
+    if (!op.getAllowReorder() || op.getEfficientLayout())
       return failure();
 
     rewriter.replaceOpWithNewOp<triton::ReshapeOp>(
