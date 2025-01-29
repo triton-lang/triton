f7f5882e1489a782b6407b08f944e0b5c5d5ec66
Ilya Veselov <iveselov.nn@gmail.com>
2025-01-29 20:45:48 +0100

    [AMD][OPTIMIZER] Optimize NT GEMM shared memory
    This PR introduces shared memory optimization, which reduces bank conflicts and
    enables wide LDS stores in NT, TT and TN GEMM and similar cases
    (i.e. dot operand K dimension is not innermost).
    
    This is achieved by new shared memory swizzle pattern and
    transposition of elements inside of one thread.

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -68,6 +68,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::registerTritonAMDGPUStreamPipeline();
   mlir::registerTritonAMDGPUCanonicalizePointers();
   mlir::registerTritonAMDGPUConvertToBufferOps();
+  mlir::registerTritonAMDGPUInThreadTranspose();
   mlir::triton::registerTritonAMDGPUInsertInstructionSchedHints();
   mlir::triton::registerTritonAMDGPULowerInstructionSchedHints();
 
