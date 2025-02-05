d90f2aec6ee83a811bed2380178d5760d88fd93b
Pawel Szczerbuk <pawel.szczerbuk@openai.com>
2025-02-05 10:16:35 -0800

    Reintroducing assert for checking if loads are pipelined at all

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -4089,6 +4089,7 @@ def test_dot_mulbroadcasted(in_dtype, device):
     if not is_cuda():
         return
     assert "tt.dot" in h.asm['ttir']
+    assert re.search(r"ttg.async_wait %.* {num = 2 : i32}", h.asm["ttgir"]) is not None
 
 
 @pytest.mark.interpreter
