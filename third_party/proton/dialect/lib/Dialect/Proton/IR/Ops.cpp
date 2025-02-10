475abe2aef14aee74e813020b48299f5ab3cb3fa
Keren Zhou <kerenzhou@openai.com>
2025-02-10 14:37:03 -0500

    [PROTON-DEV] Restructure files (#5868)

--- third_party/proton/dialect/lib/Dialect/Proton/IR/Ops.cpp
+++ third_party/proton/dialect/lib/Dialect/Proton/IR/Ops.cpp
@@ -5,27 +5,8 @@
 #include "mlir/IR/OperationSupport.h"
 #include "mlir/Interfaces/FunctionImplementation.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
-#include "mlir/Support/LLVM.h"
-#include "triton/Dialect/Triton/IR/Dialect.h"
-#include "triton/Dialect/Triton/IR/Types.h"
-#include "triton/Dialect/Triton/IR/Utility.h"
 
 #define GET_OP_CLASSES
 #include "Dialect/Proton/IR/Ops.cpp.inc"
+
 #include "Dialect/Proton/IR/OpsEnums.cpp.inc"
-
-namespace mlir {
-namespace triton {
-namespace proton {
-
-// -- CircularRecordOp --
-LogicalResult CircularRecordOp::verify() {
-  // TODO(fywkevin): checks the following:
-  // 1. circular buffer size power of 2.
-  // 2. function's noinline is false.
-  return success();
-}
-
-} // namespace proton
-} // namespace triton
-} // namespace mlir
