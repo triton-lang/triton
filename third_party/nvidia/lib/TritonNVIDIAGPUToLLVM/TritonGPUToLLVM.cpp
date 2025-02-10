9e3517f8b4660800a45ecf5db0f1ac89c2d3b49b
Keren Zhou <kerenzhou@openai.com>
2025-02-10 18:39:19 -0500

    [PROTON-DEV] Initialize the conversion infrastructure (#5872)

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TritonGPUToLLVM.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TritonGPUToLLVM.cpp
@@ -22,7 +22,7 @@
 #include "triton/Conversion/TritonGPUToLLVM/PatternTritonGPUOpToLLVM.h"
 #include "triton/Conversion/TritonGPUToLLVM/TypeConverter.h"
 
-#include "third_party/proton/dialect/include/ProtonGPUToLLVM/PatternProtonOpToLLVM.h"
+#include "third_party/proton/dialect/include/Conversion/ProtonGPUToLLVM/PatternProtonOpToLLVM.h"
 
 namespace mlir {
 namespace triton {
@@ -138,10 +138,6 @@ struct ConvertTritonGPUToLLVM
                                                     targetInfo, benefit);
     mlir::triton::populatePrintOpToLLVMPattern(typeConverter, patterns,
                                                targetInfo, benefit);
-    mlir::triton::proton::populateInitScopeOpToLLVMPattern(typeConverter,
-                                                           patterns, benefit);
-    mlir::triton::proton::populateRecordOpToLLVMPattern(typeConverter, patterns,
-                                                        targetInfo, benefit);
     mlir::triton::populateControlFlowOpToLLVMPattern(typeConverter, patterns,
                                                      targetInfo, benefit);
     mlir::triton::NVIDIA::populateSPMDOpToLLVMPattern(typeConverter, patterns,
