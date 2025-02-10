a7f0f0ca173bb1d8f6c71cf9eea3bdfb70ff5351
Keren Zhou <kerenzhou@openai.com>
2025-02-10 14:37:03 -0500

    [PROTON-DEV] Restructure files (#5868)

--- third_party/proton/dialect/include/Dialect/Proton/IR/Dialect.h
+++ third_party/proton/dialect/include/Dialect/Proton/IR/Dialect.h
@@ -7,19 +7,8 @@
 #include "mlir/IR/PatternMatch.h"
 #include "proton/dialect/include/Dialect/Proton/IR/Dialect.h.inc"
 #include "proton/dialect/include/Dialect/Proton/IR/OpsEnums.h.inc"
-#include "triton/Dialect/Triton/IR/Dialect.h"
-#include "triton/Dialect/TritonGPU/IR/Dialect.h"
-
-#define GET_ATTRDEF_CLASSES
-#include "proton/dialect/include/Dialect/Proton/IR/ProtonAttrDefs.h.inc"
 
 #define GET_OP_CLASSES
 #include "proton/dialect/include/Dialect/Proton/IR/Ops.h.inc"
-
-namespace mlir {
-namespace triton {
-namespace proton {} // namespace proton
-} // namespace triton
-} // namespace mlir
 
 #endif // DIALECT_PROTON_IR_DIALECT_H_
