5ff36c350bcef804868c4edeba1431b48d377d1f
Jokeren <robinho364@gmail.com>
2025-02-10 13:25:23 -0500

    Update

--- third_party/proton/dialect/include/Dialect/ProtonGPU/IR/Dialect.h
+++ third_party/proton/dialect/include/Dialect/ProtonGPU/IR/Dialect.h
@@ -5,13 +5,11 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/PatternMatch.h"
-#include "proton/dialect/include/Dialect/Proton/IR/Dialect.h.inc"
+#include "proton/dialect/include/Dialect/Proton/IR/Dialect.h"
 #include "proton/dialect/include/Dialect/ProtonGPU/IR/Dialect.h.inc"
+#include "proton/dialect/include/Dialect/ProtonGPU/IR/OpsEnums.h.inc"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
-
-#define GET_ATTRDEF_CLASSES
-#include "proton/dialect/include/Dialect/ProtonGPU/IR/ProtonGPUAttrDefs.h.inc"
 
 #define GET_OP_CLASSES
 #include "proton/dialect/include/Dialect/ProtonGPU/IR/Ops.h.inc"
--- third_party/proton/dialect/lib/Dialect/ProtonGPU/IR/Dialect.cpp
+++ third_party/proton/dialect/lib/Dialect/ProtonGPU/IR/Dialect.cpp
@@ -5,20 +5,9 @@
 
 #include "Dialect/ProtonGPU/IR/Dialect.cpp.inc"
 
-using namespace mlir;
-using namespace mlir::triton::proton;
-
 void mlir::triton::proton::gpu::ProtonGPUDialect::initialize() {
-  addAttributes<
-#define GET_ATTRDEF_LIST
-#include "Dialect/ProtonGPU/IR/ProtonGPUAttrDefs.cpp.inc"
-      >();
-
   addOperations<
 #define GET_OP_LIST
 #include "Dialect/ProtonGPU/IR/Ops.cpp.inc"
       >();
 }
-
-#define GET_ATTRDEF_CLASSES
-#include "Dialect/ProtonGPU/IR/ProtonGPUAttrDefs.cpp.inc"
