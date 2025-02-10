9e06db3707a3f82e57739d74e50bb4a33aed35ef
Keren Zhou <kerenzhou@openai.com>
2025-02-10 18:39:19 -0500

    [PROTON-DEV] Initialize the conversion infrastructure (#5872)

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -3,7 +3,6 @@
 #include "amd/include/TritonAMDGPUTransforms/Passes.h"
 #include "third_party/nvidia/include/Dialect/NVGPU/IR/Dialect.h"
 #include "third_party/proton/dialect/include/Dialect/Proton/IR/Dialect.h"
-#include "third_party/proton/dialect/include/ProtonGPUToLLVM/Passes.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
