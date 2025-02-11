80d8ba9332c6ea759329f9f518b6a410f1740595
Lei Zhang <antiagainst@gmail.com>
2025-02-10 22:18:44 -0800

    [AMD] Enable automatic loop fusion

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -6732,9 +6732,6 @@ def test_tl_range_num_stages(device):
 
 
 def test_tl_range_fuse():
-    if is_hip():
-        pytest.skip("loop fusion is not enabled on AMD")
-
     @triton.jit
     def kernel(ub):
         for i in tl.range(0, ub, flatten=True):
