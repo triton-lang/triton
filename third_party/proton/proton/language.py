8ce7a710ee4ce7a25c38f5e69bba9f4f74c0358e
Keren Zhou <kerenzhou@openai.com>
2025-02-11 15:28:25 -0500

    [PROTON-DEV] Remove `proton.init_scope` (#5881)
    Scope id allocation and pass to the runtime are not implemented yet

--- third_party/proton/proton/language.py
+++ third_party/proton/proton/language.py
@@ -5,30 +5,20 @@ import warnings
 
 
 @builtin
-def record(is_start: tl.constexpr, scope_id: tl.tensor, _builder=None):
+def record(is_start: tl.constexpr, scope_name: tl.constexpr, _builder=None):
     warnings.warn(
         "\nWarning the proton language module within Proton contains under development features that are not intended to be used outside of the core development team"
     )
     is_start = tl._unwrap_if_constexpr(is_start)
-    return tl.tensor(_builder.create_proton_record(is_start, scope_id.handle), tl.void)
-
-
-@builtin
-def init_scope(name: tl.constexpr, _builder=None):
-    warnings.warn(
-        "\nWarning the proton language module within Proton contains under development features that are not intended to be used outside of the core development team"
-    )
-    name = tl._unwrap_if_constexpr(name)
-    return tl.tensor(_builder.create_proton_init_scope(name), tl.int32)
+    scope_name = tl._unwrap_if_constexpr(scope_name)
+    return tl.tensor(_builder.create_proton_record(is_start, scope_name), tl.void)
 
 
 @triton.jit
 def enter_scope(name: tl.constexpr):
-    scope_id = init_scope(name)
-    record(is_start=True, scope_id=scope_id)
-    return scope_id
+    record(is_start=True, scope_name=name)
 
 
 @triton.jit
-def exit_scope(scope_id: tl.tensor):
-    record(is_start=False, scope_id=scope_id)
+def exit_scope(name: tl.constexpr):
+    record(is_start=False, scope_name=name)
