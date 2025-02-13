b38aad42bdeb6074ce2bf32343718945a2c861c2
Keren Zhou <kerenzhou@openai.com>
2025-02-12 21:13:23 -0500

    [PROTON-DEV] Assign unique scope ids (#5901)

--- third_party/proton/dialect/include/Analysis/ScopeIdAllocation.h
+++ third_party/proton/dialect/include/Analysis/ScopeIdAllocation.h
@@ -4,76 +4,66 @@
 #include "mlir/IR/Operation.h"
 #include "third_party/proton/dialect/include/Dialect/Proton/IR/Dialect.h"
 #include "triton/Analysis/Utility.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/StringMap.h"
 #include <cstddef>
 #include <map>
 #include <string>
 #include <vector>
 
 namespace mlir {
-
 namespace triton::proton {
 
 class ScopeIdAllocation {
 public:
-  /// A unique identifier for scopes
   using ScopeId = size_t;
   using ScopeIdPairs = std::vector<std::pair<ScopeId, std::string>>;
 
-  ScopeIdAllocation(Operation *op) : funcOp(op) { run(); }
+  ScopeIdAllocation() = default;
+  explicit ScopeIdAllocation(Operation *op) : funcOp(op) { run(); }
 
   ScopeId getOpScopeId(Operation *op) const {
     if (auto recordOp = dyn_cast<RecordOp>(op)) {
-      auto name = recordOp.getName().str();
-      return nameToIdMap.at(name);
+      auto name = recordOp.getName();
+      return nameToIdMap.lookup(name);
     }
     llvm_unreachable("unexpected operation type");
   }
 
   ScopeIdPairs getScopeIdPairs() const {
     ScopeIdPairs pairs;
-    for (auto &pair : idToNameMap) {
-      pairs.push_back(pair);
+    for (const auto &pair : idToNameMap) {
+      pairs.push_back({pair.first, pair.second.str()});
     }
     return pairs;
   }
+
+  size_t getNumScopes() const { return idToNameMap.size(); }
 
 private:
   void run();
 
   Operation *funcOp;
-  std::map<std::string, ScopeId> nameToIdMap;
-  std::map<ScopeId, std::string> idToNameMap;
+  llvm::DenseMap<ScopeId, StringRef> idToNameMap;
+  llvm::StringMap<ScopeId> nameToIdMap;
 };
 
-class ModuleScopeIdAllocation : CallGraph<ScopeIdAllocation> {
+class ModuleScopeIdAllocation : public CallGraph<ScopeIdAllocation> {
 public:
-  ModuleScopeIdAllocation(ModuleOp moduleOp)
-      : CallGraph<ScopeIdAllocation>(moduleOp) {
-    ScopeIdAllocation::ScopeId scopeId = 0;
-    walk<WalkOrder::PreOrder, WalkOrder::PostOrder>(
-        // Pre-order edge walk callback
-        [](CallOpInterface callOp, FunctionOpInterface funcOp) {},
-        // Post-order node walk callback
-        [&](FunctionOpInterface funcOp) {
-          auto scopeIdPairs = ScopeIdAllocation(funcOp).getScopeIdPairs();
-          // Adjust offset
-          for (auto &pair : scopeIdPairs) {
-            pair.first += scopeId;
-          }
-          scopeId += scopeIdPairs.size();
-        });
-  }
+  using FuncOffsetMapT =
+      DenseMap<FunctionOpInterface, ScopeIdAllocation::ScopeId>;
 
-  ScopeIdAllocation::ScopeIdPairs getScopeIdPairs() const {
-    return moduleScopeIdPairs;
-  }
+  explicit ModuleScopeIdAllocation(ModuleOp moduleOp);
+
+  ScopeIdAllocation::ScopeId getOpScopeId(Operation *op) const;
+  ScopeIdAllocation::ScopeIdPairs getScopeIdPairs(triton::FuncOp funcOp) const;
+  ScopeIdAllocation::ScopeIdPairs getScopeIdPairs() const;
 
 private:
-  ScopeIdAllocation::ScopeIdPairs moduleScopeIdPairs;
+  FuncOffsetMapT funcScopeIdMap;
 };
 
 } // namespace triton::proton
-
 } // namespace mlir
 
 #endif // PROTON_ANALYSIS_SCOPE_ID_ALLOCATION_H
