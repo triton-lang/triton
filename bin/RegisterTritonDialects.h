eaa818e235cd9639a80e9ae0fb7541768a372611
Keren Zhou <kerenzhou@openai.com>
2025-02-12 21:13:23 -0500

    [PROTON-DEV] Assign unique scope ids (#5901)

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -41,6 +41,9 @@ void registerTestMembarPass();
 void registerTestAMDGPUMembarPass();
 void registerTestTritonAMDGPURangeAnalysis();
 void registerTestLoopPeelingPass();
+namespace proton {
+void registerTestScopeIdAllocationPass();
+} // namespace proton
 } // namespace test
 } // namespace mlir
 
@@ -91,6 +94,10 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
 
   // NVWS passes
   mlir::triton::registerNVWSTransformsPasses();
+  
+  // Proton passes
+  mlir::triton::registerProtonLoweringPass();
+  mlir::test::proton::registerTestScopeIdAllocationPass();
 
   // NVGPU transform passes
   mlir::registerNVHopperTransformsPasses();
