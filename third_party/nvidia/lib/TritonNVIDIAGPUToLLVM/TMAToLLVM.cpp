841057c752ba61b486049f3de37b498d6afb5a63
Mogball <jeffniu22@gmail.com>
2025-02-19 10:24:32 -0800

    cleanup

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TMAToLLVM.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TMAToLLVM.cpp
@@ -213,7 +213,7 @@ struct ExperimentalTensormapFenceproxyAcquireOpConversion
     // We run the fence on a single warp, then use a barrier to synchronize the
     // rest. This ends up being faster than running the fence on each warp.
     // TODO: Ideally we only emit one barrier after all fences are issued
-    rewriter.create<NVVM::Barrier0Op>(loc);
+    b.barrier();
 
     rewriter.eraseOp(op);
     return success();
