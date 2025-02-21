c9de0035d980bf2b8c5e18b8d4d3bc3775eac177
Yuanwei Fang <fywkevin@gmail.com>
2025-02-21 10:49:42 -0800

    [PROTON-DEV] proton dialect to protongpu dialect lowering (#5956)
    This is a WIP draft that contains the main lowering logic and approach
    that we convert `proton` to `proton_gpu`. Note that we changed back the
    buffer `index` to pass by reference (the address of `index`) instead of
    value. This is to simplify the data flow graph modification during
    `proton` -> `proton_gpu` lowering. Otherwise, when having control flow
    like `scf.if` or `scf.for` that contains `%new_index =
    circular_storeop(..., %old_index,...)`, passing the `index` by value is
    quite complicated since we need to change a lot of existing op
    inputs/outputs and make sure it is dataflow.
    
    ---------
    
    Co-authored-by: Yuanwei Fang <fywkevin@fb.com>

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -3,7 +3,9 @@
 #include "amd/include/TritonAMDGPUTransforms/Passes.h"
 #include "third_party/nvidia/include/Dialect/NVGPU/IR/Dialect.h"
 #include "third_party/nvidia/include/Dialect/NVWS/IR/Dialect.h"
+#include "third_party/proton/dialect/include/Conversion/ProtonToProtonGPU/Passes.h"
 #include "third_party/proton/dialect/include/Dialect/Proton/IR/Dialect.h"
+#include "third_party/proton/dialect/include/Dialect/ProtonGPU/IR/Dialect.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonInstrument/IR/Dialect.h"
@@ -99,6 +101,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   // Proton passes
   mlir::triton::registerProtonLoweringPass();
   mlir::test::proton::registerTestScopeIdAllocationPass();
+  mlir::triton::proton::registerConvertProtonToProtonGPU();
 
   // NVGPU transform passes
   mlir::registerNVHopperTransformsPasses();
@@ -112,5 +115,6 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
       mlir::gpu::GPUDialect, mlir::LLVM::LLVMDialect, mlir::NVVM::NVVMDialect,
       mlir::triton::nvgpu::NVGPUDialect, mlir::triton::nvws::NVWSDialect,
       mlir::triton::amdgpu::TritonAMDGPUDialect,
-      mlir::triton::proton::ProtonDialect, mlir::ROCDL::ROCDLDialect>();
+      mlir::triton::proton::ProtonDialect,
+      mlir::triton::proton::ProtonGPUDialect, mlir::ROCDL::ROCDLDialect>();
 }
