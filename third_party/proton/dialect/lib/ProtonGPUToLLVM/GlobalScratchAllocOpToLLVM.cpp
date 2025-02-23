c569e6512b6a74cb57be7d57d85a1dfa51d56784
Corbin Robeck <corbin.robeck@amd.com>
2025-02-23 20:57:31 +0000

    temp

--- third_party/proton/dialect/lib/ProtonGPUToLLVM/GlobalScratchAllocOpToLLVM.cpp
+++ third_party/proton/dialect/lib/ProtonGPUToLLVM/GlobalScratchAllocOpToLLVM.cpp
@@ -11,10 +11,61 @@
 #include "third_party/proton/dialect/include/Dialect/ProtonGPU/IR/Dialect.h"
 #include "third_party/proton/dialect/include/Conversion/ProtonGPUToLLVM/Utility.h"
 
+namespace mlir {
+FailureOr<LLVM::LLVMFuncOp>
+convertFuncOpToLLVMFuncOp(FunctionOpInterface funcOp,
+                          ConversionPatternRewriter &rewriter,
+                          const LLVMTypeConverter &converter);
+}
+
 namespace {
 using namespace mlir;
 using namespace mlir::triton;
 
+static void filterFuncAttributes(LLVM::LLVMFuncOp op, bool filterArgAttrs,
+                                 SmallVectorImpl<NamedAttribute> &result) {
+
+  for (const auto &attr : op->getAttrs()) {
+    if (attr.getName() == SymbolTable::getSymbolAttrName() ||
+        attr.getName() == op.getFunctionTypeAttrName() ||
+        attr.getName() == "std.varargs" ||
+        (filterArgAttrs && attr.getName() == op.getArgAttrsAttrName()))
+      continue;
+    result.push_back(attr);
+  }
+}
+
+triton::FuncOp amendFuncOp(LLVM::LLVMFuncOp funcOp,
+                           ConversionPatternRewriter &rewriter,
+                           const TargetInfoBase &targetInfo) {
+    auto loc = funcOp.getLoc();
+    auto ctx = funcOp->getContext();
+    auto globalPtrTy = LLVM::LLVMPointerType::get(ctx, 1);    
+    auto funcTy = funcOp.getFunctionType();
+    auto amendedInputTy = llvm::to_vector(funcOp.getArgumentTypes());
+//    amendedInputTy.push_back(globalPtrTy);
+    auto amendedFuncTy =
+        FunctionType::get(ctx, amendedInputTy, funcOp.getResultTypes());
+    SmallVector<NamedAttribute> amendedAttrs;    
+//    filterFuncAttributes(funcOp, /*filterArgAttrs=*/true, amendedAttrs);
+//    if (auto argAttrs = funcOp.getAllArgAttrs()) {
+//      llvm::SmallVector<mlir::Attribute> amendedArgAttrs(argAttrs.begin(),
+//                                                         argAttrs.end());
+//      while (amendedArgAttrs.size() < amendedInputTy.size()) {
+//        amendedArgAttrs.emplace_back(DictionaryAttr::get(ctx));
+//      }
+//      amendedAttrs.push_back(
+//          rewriter.getNamedAttr(funcOp.getArgAttrsAttrName(),
+//                                rewriter.getArrayAttr(amendedArgAttrs)));
+//    }
+    auto amendedFuncOp = rewriter.create<triton::FuncOp>(
+        funcOp.getLoc(), funcOp.getName(), amendedFuncTy, amendedAttrs);
+    auto &region = funcOp.getBody();
+//    region.addArgument(globalPtrTy, loc);
+    rewriter.inlineRegionBefore(region, amendedFuncOp.getBody(),
+                                amendedFuncOp.end());
+    return amendedFuncOp;
+  }
 struct GlobalScratchAllocOpConversion
    : public ConvertOpToLLVMPattern<proton::gpu::GlobalScratchAllocOp> {
 	   explicit GlobalScratchAllocOpConversion(LLVMTypeConverter &typeConverter,
@@ -28,20 +79,24 @@ struct GlobalScratchAllocOpConversion
   matchAndRewrite(proton::gpu::GlobalScratchAllocOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     auto func = op->getParentOfType<LLVM::LLVMFuncOp>();
-    auto funcTy = func.getFunctionType();
-//    auto loc = func.getLoc();
-//    auto ctx = func->getContext();
-//    auto globalPtrTy = LLVM::LLVMPointerType::get(ctx, 1);
-//    auto &region = func.getBody();
-//    region.addArgument(globalPtrTy, loc);
-//    auto b = TritonLLVMOpBuilder(loc, rewriter);
-//    Value bufferOffset = b.i32_val(0);
-//    Value ptr =
-//        triton::proton::gpu::getGlobalScratchPtr(loc, rewriter, func, bufferOffset);
-//
-//    rewriter.replaceOp(op, ptr);   
-    llvm::errs() << funcTy << "\n";
-    rewriter.eraseOp(op);
+    auto amendedFuncOp = amendFuncOp(func, rewriter, targetInfo);
+    FailureOr<LLVM::LLVMFuncOp> maybeNewFuncOp =
+        mlir::convertFuncOpToLLVMFuncOp(amendedFuncOp, rewriter,
+                                        *getTypeConverter());
+    if (failed(maybeNewFuncOp)) {
+      return failure();
+    }
+
+    LLVM::LLVMFuncOp newFuncOp = *maybeNewFuncOp;
+    auto ctx = func->getContext();    
+    newFuncOp->setAttr("nvvm.kernel",
+                       rewriter.getIntegerAttr(type::u1Ty(ctx), 1));
+    newFuncOp.setLinkage(LLVM::Linkage::External);    
+
+    rewriter.eraseOp(func);
+    IRRewriter newRewriter(newFuncOp->getContext());    
+    newRewriter.eraseOp(op);
+//    llvm::errs() << funcTy << "\n";
     return success();
   }
 protected:
