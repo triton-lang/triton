e48c77c8bea059f4ea8aac6cfd94fa52a4feead7
Shucai Xiao <shucai.xiao@amd.com>
2025-02-24 19:53:01 +0000

    backup changes

--- third_party/amd/lib/TritonAMDGPUToLLVM/Utility.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/Utility.cpp
@@ -287,8 +287,6 @@ Value llGetPid(Location loc, RewriterBase &rewriter, ModuleOp moduleOp,
 Value llLoad(RewriterBase &rewriter, Location loc, Value ptr, Type elemTy,
              Value pred, Value falseVal, triton::CacheModifier cm) {
   auto b = TritonLLVMOpBuilder(loc, rewriter);
-<<<<<<< HEAD
-=======
   // Try to emit llvm.intr.masked.load if we can. In theory the backend should
   // be happier because we emit less branchy code to optimize. The backend will
   // lower it down however it wants at some point.
@@ -307,7 +305,6 @@ Value llLoad(RewriterBase &rewriter, Location loc, Value ptr, Type elemTy,
   //   vecData = b.bitcast(vecData, elemTy);
   //   return vecData;
   // }
->>>>>>> 1a4fd9859 (minimal changes to fix regression)
 
   Type funcType = getFunctionType(elemTy, ValueRange({ptr, pred, falseVal}));
   auto parent = ptr.getParentRegion()->getParentOfType<LLVM::LLVMFuncOp>();
