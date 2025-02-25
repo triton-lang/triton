835c573279c73e5d0ed2c5f59e19f053c6c48dde
Mogball <jeffniu22@gmail.com>
2025-02-25 15:02:06 -0800

    tmp

--- lib/Dialect/TritonGPU/Transforms/Pipeliner/AutomaticWarpSpecialization.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeliner/AutomaticWarpSpecialization.cpp
@@ -2,6 +2,9 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Pass/Pass.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/Transforms/Passes.h"
+#include "triton/Dialect/TritonGPU/Transforms/PipeliningUtility.h"
+#include "triton/Dialect/TritonGPU/Transforms/Schedule.h"
 
 using namespace mlir;
 using namespace triton;
@@ -13,10 +16,35 @@ namespace mlir::triton::gpu {
 } // namespace mlir::triton::gpu
 
 namespace {
-struct AutomaticWarpSpecialization : triton::gpu::impl::TritonGPUAutomaticWarpSpecializationBase<AutomaticWarpSpecialization> {
+struct AutomaticWarpSpecialization
+    : triton::gpu::impl::TritonGPUAutomaticWarpSpecializationBase<
+          AutomaticWarpSpecialization> {
+  using TritonGPUAutomaticWarpSpecializationBase::
+      TritonGPUAutomaticWarpSpecializationBase;
+
   void runOnOperation() override;
 };
 } // namespace
 
 void AutomaticWarpSpecialization::runOnOperation() {
+  // Collect for loops to warp specialize. This pass expects the loop to already
+  // be scheduled.
+  SmallVector<scf::ForOp> loops;
+  getOperation().walk([&](scf::ForOp loop) {
+    if (llvm::any_of(loop.getOps(), [](Operation &op) {
+          return op.hasAttr(kLoopStageAttrName);
+        }))
+      loops.push_back(loop);
+  });
+
+  for (scf::ForOp loop : loops) {
+    int loopNumStages = numStages;
+    if (auto attr = loop->getAttrOfType<IntegerAttr>(kNumStagesAttrName))
+      loopNumStages = attr.getInt();
+
+    PipeliningOption result;
+    // Preprocess the loop by lowering async ops.
+    if (!preProcessLoopAndGetSchedule(loop, loopNumStages, result))
+      continue;
+  }
 }
