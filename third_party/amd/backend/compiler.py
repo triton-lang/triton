c6fa27b633dedf0b067fdd3fc100c84440e72471
Yi Qian <68618497+yiqian1@users.noreply.github.com>
2025-02-26 13:04:19 -0600

    [AMD] Enable OCP fp8/bf8 tests on MI350 (#6021)
    This patch enables tests for OCP fp8/bf8 MFMA ops on MI350.

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -102,8 +102,10 @@ class HIPBackend(BaseBackend):
 
         if "supported_fp8_dtypes" not in opts:
             supported_fp8_dtypes = set(HIPOptions.supported_fp8_dtypes)
-            if self.target.arch in ('gfx940', 'gfx941', 'gfx942', 'gfx950'):
+            if self.target.arch in ('gfx940', 'gfx941', 'gfx942'):
                 supported_fp8_dtypes.update({'fp8e4nv', 'fp8e4b8', 'fp8e5b16'})
+            elif self.target.arch in ('gfx950'):
+                supported_fp8_dtypes.update({'fp8e4nv', 'fp8e5'})
             args["supported_fp8_dtypes"] = tuple(sorted(supported_fp8_dtypes))
 
         if "enable_fp_fusion" not in opts:
