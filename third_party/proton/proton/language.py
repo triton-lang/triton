ac0248f27799ca1817bbf54ec747eb4e73bbdf21
Jokeren <robinho364@gmail.com>
2025-03-02 11:38:10 -0800

    Update

--- third_party/proton/proton/language.py
+++ third_party/proton/proton/language.py
@@ -10,14 +10,14 @@ def record(is_start: tl.constexpr, scope_name: tl.constexpr, builder=ir.builder)
     )
     is_start = tl._unwrap_if_constexpr(is_start)
     scope_name = tl._unwrap_if_constexpr(scope_name)
-    return tl.tensor(_builder.create_proton_record(is_start, scope_name), tl.void)
+    return tl.tensor(builder.create_proton_record(is_start, scope_name), tl.void)
 
 
 @builtin
 def enter_scope(name: tl.constexpr, _builder=None):
-    record(is_start=True, scope_name=name, _builder=_builder)
+    record(is_start=True, scope_name=name, builder=_builder)
 
 
 @builtin
 def exit_scope(name: tl.constexpr, _builder=None):
-    record(is_start=False, scope_name=name, _builder=_builder)
+    record(is_start=False, scope_name=name, builder=_builder)
