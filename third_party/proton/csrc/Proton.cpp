9a95b578a45bfc253d892e73fdcd0bfc6035a248
Jokeren <robinho364@gmail.com>
2025-03-02 11:20:28 -0800

    Update

--- third_party/proton/csrc/Proton.cpp
+++ third_party/proton/csrc/Proton.cpp
@@ -83,9 +83,10 @@ void initProton(pybind11::module &&m) {
           SessionManager::instance().addMetrics(scopeId, metrics);
         });
 
-  m.def("set_scope_id_pairs",
+  m.def("map_scope_ids",
         [](uint64_t functionHandle,
-           const std::vector<std::pair<size_t, std::string>> &scopeIdPairs) -> void {});
+           const std::vector<std::pair<size_t, std::string>> &scopeIdPairs)
+            -> void {});
 
   pybind11::bind_map<std::map<std::string, MetricValueType>>(m, "MetricMap");
 }
