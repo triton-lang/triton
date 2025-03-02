f7b798005a2d456dca205933bdc96a5211b6f554
Keren Zhou <kerenzhou@openai.com>
2025-03-02 13:53:09 -0800

    [PROTON-DEV] Extract scope id mappings from the IRs (#6083)

--- third_party/proton/proton/hook.py
+++ third_party/proton/proton/hook.py
@@ -4,6 +4,8 @@ from triton import knobs
 from triton.compiler import LazyDict
 
 from triton._C.libtriton import ir
+from triton._C.libtriton import proton as triton_proton
+from triton._C.libproton import proton as libproton
 
 COMPUTE_METADATA_SCOPE_NAME = "__proton_launch_metadata"
 
@@ -41,17 +43,16 @@ class TritonInitHandleHook:
     function_scope_ids: dict = {}
 
     @staticmethod
-    def map_scope_ids(function, module, metadata_group):
+    def map_scope_ids(function, module, metadata_group) -> None:
         if function and function not in TritonInitHandleHook.function_scope_ids:
-            ir_path = None
-            if "ttgir" in metadata_group:
-                ir_path = metadata_group["ttgir"]
-            elif "ttir" in metadata_group:
-                ir_path = metadata_group["ttir"]
+            ir_path = next((path for key, path in metadata_group.items() if key.endswith(("ttgir", "ttir"))), None)
             if ir_path:
                 context = ir.context()
+                ir.load_dialects(context)
                 module = ir.parse_mlir_module(ir_path, context)
                 module.context = context
+                scope_id_pairs = triton_proton.get_scope_id_pairs(module)
+                libproton.map_scope_ids(function, scope_id_pairs)
 
 
 def register_init_handle_hook() -> None:
--- third_party/proton/proton/language.py
+++ third_party/proton/proton/language.py
@@ -1,24 +1,23 @@
-import triton
 from triton.language import core as tl
 from triton.language.core import builtin
+from triton._C.libtriton import ir
 import warnings
 
 
-@builtin
-def record(is_start: tl.constexpr, scope_name: tl.constexpr, _builder=None):
+def record(is_start: tl.constexpr, scope_name: tl.constexpr, builder=ir.builder):
     warnings.warn(
         "\nWarning the proton language module within Proton contains under development features that are not intended to be used outside of the core development team"
     )
     is_start = tl._unwrap_if_constexpr(is_start)
     scope_name = tl._unwrap_if_constexpr(scope_name)
-    return tl.tensor(_builder.create_proton_record(is_start, scope_name), tl.void)
+    return tl.tensor(builder.create_proton_record(is_start, scope_name), tl.void)
 
 
-@triton.jit
-def enter_scope(name: tl.constexpr):
-    record(is_start=True, scope_name=name)
+@builtin
+def enter_scope(name: tl.constexpr, _builder=None):
+    record(is_start=True, scope_name=name, builder=_builder)
 
 
-@triton.jit
-def exit_scope(name: tl.constexpr):
-    record(is_start=False, scope_name=name)
+@builtin
+def exit_scope(name: tl.constexpr, _builder=None):
+    record(is_start=False, scope_name=name, builder=_builder)
