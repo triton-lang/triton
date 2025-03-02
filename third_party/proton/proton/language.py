4497ec7531b798b12dcb21569bf40e9841564d97
Jokeren <robinho364@gmail.com>
2025-03-02 11:36:24 -0800

    Update

--- third_party/proton/proton/language.py
+++ third_party/proton/proton/language.py
@@ -4,8 +4,7 @@ from triton._C.libtriton import ir
 import warnings
 
 
-@builtin
-def record(is_start: tl.constexpr, scope_name: tl.constexpr, _builder=None):
+def record(is_start: tl.constexpr, scope_name: tl.constexpr, builder=ir.builder):
     warnings.warn(
         "\nWarning the proton language module within Proton contains under development features that are not intended to be used outside of the core development team"
     )
@@ -14,9 +13,11 @@ def record(is_start: tl.constexpr, scope_name: tl.constexpr, _builder=None):
     return tl.tensor(_builder.create_proton_record(is_start, scope_name), tl.void)
 
 
-def enter_scope(name: tl.constexpr, builder: ir.builder):
-    record(is_start=True, scope_name=name)
+@builtin
+def enter_scope(name: tl.constexpr, _builder=None):
+    record(is_start=True, scope_name=name, _builder=_builder)
 
 
-def exit_scope(name: tl.constexpr, builder: ir.builder):
-    record(is_start=False, scope_name=name)
+@builtin
+def exit_scope(name: tl.constexpr, _builder=None):
+    record(is_start=False, scope_name=name, _builder=_builder)
