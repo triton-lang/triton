4cccd093aaaa20a8096dbe74d574f699c7616373
Corbin Robeck <corbin.robeck@amd.com>
2025-03-02 17:28:12 -0800

    [PROTON-DEV] Implement Proton Add Device Buffer Kernel Arg Pass (#6072)
    In this PR we add support for modifying the kernel argument to support
    Proton's scratch buffer. This pass happens after ProtonToProtonGPU
    lowering but before the lowering to LLVM.
    
    This pass looks for the presence of the global scratch alloc operation
    to rewrite the kernel argument region to add an additional ptr arg to be
    used for device to host transfers of performance data collected on the
    GPU

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -103,6 +103,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::test::proton::registerTestScopeIdAllocationPass();
   mlir::triton::proton::registerConvertProtonToProtonGPU();
   mlir::triton::proton::registerAllocateProtonSharedMemoryPass();
+  mlir::triton::proton::registerAddProtonKernelArgPass();
 
   // NVGPU transform passes
   mlir::registerNVHopperTransformsPasses();
