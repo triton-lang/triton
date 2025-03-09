5da90601195e679e3b5359984bd53ff5c4f5dde9
Jokeren <robinho364@gmail.com>
2025-03-08 21:06:32 -0500

    [PROTON-DEV] Resolve conflicts

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -87,7 +87,6 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::registerTritonAMDFoldTrueCmpI();
 
   // Proton passes
-  mlir::triton::registerProtonLoweringPass();
   mlir::test::proton::registerTestScopeIdAllocationPass();
   mlir::triton::proton::registerConvertProtonToProtonGPU();
   mlir::triton::proton::registerAllocateProtonSharedMemoryPass();
