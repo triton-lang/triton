2771f11d9cb1b75eac63334793153f4dc77f2485
Jokeren <robinho364@gmail.com>
2025-03-09 15:59:09 -0400

    Update

--- third_party/proton/proton/context.py
+++ third_party/proton/proton/context.py
@@ -1,6 +1,9 @@
 from typing import Optional
 from triton._C.libproton import proton as libproton
+from .flags import get_profiling_on
 
 
 def depth(session: Optional[int] = 0):
+    if not get_profiling_on():
+        return 0
     return libproton.get_context_depth(session)
