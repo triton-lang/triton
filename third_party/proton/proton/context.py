96f0c33ff3cfd1b8c91ea65f525d7ef967007313
Jokeren <robinho364@gmail.com>
2025-03-09 16:01:38 -0400

    Add comments

--- third_party/proton/proton/context.py
+++ third_party/proton/proton/context.py
@@ -4,6 +4,15 @@ from .flags import get_profiling_on
 
 
 def depth(session: Optional[int] = 0):
+    """
+    Get the depth of the context.
+
+    Args:
+        session (int): The session ID of the profiling session. Defaults to 0.
+
+    Returns:
+        int: The depth of the context of the session.
+    """
     if not get_profiling_on():
         return 0
     return libproton.get_context_depth(session)
