4e0c2a2ca8ee8bc4fd17395c4beaa82a9dd1a851
Pawel Szczerbuk <pawel.szczerbuk@openai.com>
2025-03-20 15:13:56 -0700

    Removing keep_tmem_in_acc

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -276,7 +276,6 @@ class CUDABackend(BaseBackend):
             passes.ttgpuir.add_pipeline(pm, opt.num_stages, dump_enabled)
             passes.ttgpuir.add_combine_tensor_select_and_if(pm)
             nvidia.passes.ttnvgpuir.add_promote_lhs_to_tmem(pm)
-            nvidia.passes.ttnvgpuir.add_keep_acc_in_tmem(pm)
             passes.common.add_canonicalizer(pm)
         else:
             passes.ttir.add_triton_licm(pm)
