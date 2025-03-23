6af2077accee8be64ef7e099c426f5aad27f8117
Keren Zhou <kerenzhou@openai.com>
2025-03-23 11:16:54 -0400

    [PROTON-DEV] Create instrumentation passes instead of hard coding (#6260)

--- third_party/proton/dialect/include/Conversion/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/TargetInfo.h
+++ third_party/proton/dialect/include/Conversion/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/TargetInfo.h
@@ -4,11 +4,11 @@
 #include "Conversion/ProtonGPUToLLVM/TargetInfoBase.h"
 #include "third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.h" // TODO(fywkevin): move nvidia TargetInfo.h to include/
 
-namespace mlir::triton::proton::NVIDIA {
-class TargetInfo : public mlir::triton::proton::TargetInfoBase {
+namespace mlir::triton::proton::gpu::NVIDIA {
+class TargetInfo : public mlir::triton::proton::gpu::TargetInfoBase {
 public:
   explicit TargetInfo(const mlir::triton::NVIDIA::TargetInfo &helper)
-      : mlir::triton::proton::TargetInfoBase(helper) {}
+      : mlir::triton::proton::gpu::TargetInfoBase(helper) {}
 
   const mlir::triton::NVIDIA::TargetInfo &getTritonTargetInfo() const override {
     return static_cast<const mlir::triton::NVIDIA::TargetInfo &>(helper);
@@ -19,6 +19,6 @@ public:
 
   ~TargetInfo() {}
 };
-} // namespace mlir::triton::proton::NVIDIA
+} // namespace mlir::triton::proton::gpu::NVIDIA
 
 #endif // PROTONGPU_TO_LLVM_TARGETINFO_NVIDIA_H
