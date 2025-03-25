9aa4c762fb50839a1443bedcd98fa1b0d91ac32d
Mogball <jeffniu22@gmail.com>
2025-03-25 12:04:11 -0700

    enable upstream scheduling

--- lib/Dialect/TritonGPU/Transforms/Pipeliner/ScheduleLoops.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeliner/ScheduleLoops.cpp
@@ -44,6 +44,8 @@ bool hasLatenciesAssigned(scf::ForOp forOp,
   for (auto &op : forOp.getBody()->without_terminator()) {
     if (opLatency.count(&op))
       return true;
+    if (op.hasAttr(kLoopStageAttrName))
+      return true;
   }
   return false;
 }
@@ -58,9 +60,11 @@ CoarseSchedule scheduleKeyOps(scf::ForOp forOp,
   for (auto &op : forOp.getBody()->without_terminator()) {
     if (opLatency.count(&op))
       latOps.push_back(&op);
+    if (auto stageAttr = op.getAttrOfType<IntegerAttr>(kLoopStageAttrName))
+      opToStage[&op] = stageAttr.getInt();
   }
   // If no latency ops, nothing to schedule
-  if (latOps.empty())
+  if (latOps.empty() && opToStage.empty())
     return CoarseSchedule(0);
 
   // Compute the longest path to the yield for each operation reachable
