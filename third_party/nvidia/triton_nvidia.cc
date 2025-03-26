b5eaa251164dc842bf80de2c2840f4af7ad80f33
Thomas Raoux <thomas.raoux@openai.com>
2025-03-26 05:55:30 -0700

    Fix indeterminism bug in LLVM datalayout (#6303)
    The data layout depends on the flag `nvptx-short-ptr` but this flag was
    being set after we set the data layout. Since this is a global this was
    only affecting the first kernel compiled.

--- third_party/nvidia/triton_nvidia.cc
+++ third_party/nvidia/triton_nvidia.cc
@@ -79,6 +79,16 @@ void init_triton_nvidia(py::module &&m) {
     context.loadAllAvailableDialects();
   });
 
+  // Set short point option, this needs to be set before setting the data
+  // layout.
+  m.def("set_short_ptr", []() {
+    auto options = llvm::cl::getRegisteredOptions();
+    const char *flag = "nvptx-short-ptr";
+    auto *shortPtr = static_cast<llvm::cl::opt<bool> *>(options[flag]);
+    assert(shortPtr);
+    shortPtr->setValue(true);
+  });
+
   // TODO: could be done in python if we had a generic interface to set metadata
   m.def("set_nvvm_reflect_ftz", [](llvm::Module *mod) {
     // please check https://llvm.org/docs/NVPTXUsage.html#reflection-parameters
