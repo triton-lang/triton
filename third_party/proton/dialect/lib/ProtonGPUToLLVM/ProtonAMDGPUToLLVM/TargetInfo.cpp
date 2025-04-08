ff319936f86d6fab8f00a450e74a9a075277ee06
Yuanwei Fang <fywkevin@gmail.com>
2025-04-08 12:36:21 -0700

    [PROTON-DEV] More LLVM conversion patterns (#6331)
    This PR provided the necessary code to make end-to-end running of the
    proton intra kernel profiler core functions. I added most of the llvm
    conversion patterns for `proton_gpu` -> `llvm`. We allow selective warps
    to be profiled by specifying the intent in the `proton_to_proton_gpu`
    pass. In additional, I added extra `proton_gpu` ops to do a code motion,
    reusing computed segment base offset. These computations are in the
    recoding critical path (performance critical), which directly impacts
    the profiler runtime overhead, and should be reused if possible. I also
    added `proton_gpu` address space for stack memory and heap memory so we
    could use `memdesc` type to describe the internal buffer.

--- third_party/proton/dialect/lib/ProtonGPUToLLVM/ProtonAMDGPUToLLVM/TargetInfo.cpp
+++ third_party/proton/dialect/lib/ProtonGPUToLLVM/ProtonAMDGPUToLLVM/TargetInfo.cpp
@@ -1,6 +1,9 @@
 #include "Conversion/ProtonGPUToLLVM/ProtonAMDGPUToLLVM/TargetInfo.h"
+#include "Dialect/ProtonGPU/IR/Dialect.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/LLVMIR/LLVMTypes.h"
+#include "third_party/amd/include/TritonAMDGPUToLLVM/GCNAsmFormat.h"
+#include "triton/Conversion/TritonGPUToLLVM/Utility.h"
 #include "llvm/Support/MathExtras.h"
 
 namespace mlir::triton::proton::gpu::AMD {
@@ -10,6 +13,32 @@ Value TargetInfo::clock(ConversionPatternRewriter &rewriter, Location loc,
 
   llvm_unreachable("AMDGPU clock not implemented yet");
   return Value();
+}
+
+Value TargetInfo::processorId(ConversionPatternRewriter &rewriter,
+                              Location loc) const {
+  GCNBuilder builder;
+  auto &gethwid = *builder.create("s_getreg_b32");
+  auto res = builder.newOperand("=s");
+  auto hwreg = builder.newConstantOperand("hwreg(HW_REG_HW_ID, 8, 4)");
+  gethwid(res, hwreg);
+  builder.create<>("s_waitcnt lgkmcnt(0)")->operator()();
+  return builder.launch(rewriter, loc, i32_ty, false);
+}
+
+int TargetInfo::getAddressSpace(Attribute addressSpace) const {
+  int spaceId = 0;
+  if (mlir::isa<triton::gpu::SharedMemorySpaceAttr>(addressSpace)) {
+    spaceId = 3;
+  } else if (mlir::isa<proton::gpu::StackMemorySpaceAttr>(addressSpace)) {
+    spaceId = 1;
+  } else if (mlir::isa<proton::gpu::HeapMemorySpaceAttr>(addressSpace)) {
+    spaceId = 1;
+  } else {
+    llvm::report_fatal_error("Only support SharedMemorySpace, "
+                             "StackMemorySpace, and HeapMemorySpace for now");
+  }
+  return spaceId;
 }
 
 } // namespace mlir::triton::proton::gpu::AMD
--- third_party/proton/dialect/lib/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/ConvertProtonGPUToLLVM.cpp
+++ third_party/proton/dialect/lib/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/ConvertProtonGPUToLLVM.cpp
@@ -1,7 +1,12 @@
 #include "Conversion/ProtonGPUToLLVM/PatternProtonGPUOpToLLVM.h"
+#include "Conversion/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/NvidiaPatternProtonGPUOpToLLVM.h"
 #include "Conversion/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/Passes.h"
 #include "Conversion/ProtonGPUToLLVM/ProtonNvidiaGPUToLLVM/TargetInfo.h"
 #include "Dialect/ProtonGPU/IR/Dialect.h"
+#include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/GPUToNVVM/GPUToNVVMPass.h"
+#include "mlir/Dialect/LLVMIR/NVVMDialect.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
@@ -24,6 +29,7 @@ public:
   explicit ProtonLLVMConversionTarget(MLIRContext &ctx)
       : ConversionTarget(ctx) {
     addLegalDialect<LLVM::LLVMDialect>();
+    addLegalDialect<NVVM::NVVMDialect>();
     addIllegalDialect<mlir::triton::proton::gpu::ProtonGPUDialect>();
     addIllegalDialect<mlir::triton::proton::ProtonDialect>();
     addLegalOp<mlir::UnrealizedConversionCastOp>();
@@ -50,8 +56,15 @@ struct ConvertProtonNvidiaGPUToLLVM
     mlir::LowerToLLVMOptions option(context);
     TritonGPUToLLVMTypeConverter typeConverter(context, option,
                                                tritonTargetInfo);
+    populateTypeConversions(typeConverter, protonTargetInfo);
     mlir::triton::proton::gpu::populateProtonGPUOpPatterns(
         typeConverter, patterns, protonTargetInfo, 1);
+    mlir::triton::proton::gpu::NVIDIA::populateProtonGPUOpNvidiaPatterns(
+        typeConverter, patterns, protonTargetInfo, 1);
+    mlir::arith::populateArithToLLVMConversionPatterns(typeConverter, patterns);
+    mlir::populateGpuToNVVMConversionPatterns(typeConverter, patterns);
+    mlir::cf::populateControlFlowToLLVMConversionPatterns(typeConverter,
+                                                          patterns);
     auto convTarget = ProtonLLVMConversionTarget(*context);
     if (failed(applyPartialConversion(mod, convTarget, std::move(patterns))))
       return signalPassFailure();
