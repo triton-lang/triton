dbf7ae0c5608a57c60603424a778b38f3bb6e10a
Yuanwei Fang <fywkevin@gmail.com>
2025-04-08 12:36:21 -0700

    [PROTON-DEV] More LLVM conversion patterns (#6331)
    This PR provided the necessary code to make end-to-end running of the
    proton intra kernel profiler core functions. I added most of the llvm
    conversion patterns for `proton_gpu` -> `llvm`. We allow selective warps
    to be profiled by specifying the intent in the `proton_to_proton_gpu`
    pass. In additional, I added extra `proton_gpu` ops to do a code motion,
    reusing computed segment base offset. These computations are in the
    recoding critical path (performance critical), which directly impacts
    the profiler runtime overhead, and should be reused if possible. I also
    added `proton_gpu` address space for stack memory and heap memory so we
    could use `memdesc` type to describe the internal buffer.

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -297,8 +297,7 @@ class CUDABackend(BaseBackend):
             nvidia.passes.ttnvgpuir.add_fence_insertion(pm)
         passes.common.add_sccp(pm)
         passes.common.add_canonicalizer(pm)
-        if CUDABackend.instrumentation:
-            CUDABackend.instrumentation.patch("ttir", pm, mod.context)
+
         pm.run(mod)
         metadata["cluster_dims"] = (cluster_info.clusterDimX, cluster_info.clusterDimY, cluster_info.clusterDimZ)
         tensordesc_meta = mod.get_tensordesc_metadata()
@@ -334,6 +333,9 @@ class CUDABackend(BaseBackend):
         passes.ttgpuir.add_allocate_shared_memory(pm)
         nvidia.passes.ttnvgpuir.add_allocate_tensor_memory(pm)
         passes.ttgpuir.add_allocate_global_scratch_memory(pm)
+        # instrumentation point here so we can override IRs above (e.g., ttir and ttgir)
+        if CUDABackend.instrumentation:
+            CUDABackend.instrumentation.patch("ttgpuir", pm, mod.context)
         nvidia.passes.ttgpuir.add_to_llvmir(pm, capability, ptx_version)
         passes.common.add_canonicalizer(pm)
         passes.common.add_cse(pm)
@@ -345,7 +347,7 @@ class CUDABackend(BaseBackend):
         if not knobs.compilation.disable_line_info:
             passes.llvmir.add_di_scope(pm)
         if CUDABackend.instrumentation:
-            CUDABackend.instrumentation.patch("ttgpuir", pm, mod.context)
+            CUDABackend.instrumentation.patch("llvmir", pm, mod.context)
         pm.run(mod)
         # LLVM-IR (MLIR) -> LLVM-IR (LLVM)
         llvm.init_targets()
