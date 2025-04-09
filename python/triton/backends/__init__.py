51fc3a0a7f9c7638ac7cf9d2e76416a90c15aa64
Dan Zimmerman <danzimm@meta.com>
2025-04-09 11:56:22 -0700

    wip3

--- python/triton/backends/__init__.py
+++ python/triton/backends/__init__.py
@@ -2,19 +2,28 @@ import os
 import importlib.util
 import inspect
 from dataclasses import dataclass
+from typing import cast, overload, Type, TypeVar
+from types import ModuleType
 from .driver import DriverBase
 from .compiler import BaseBackend
 
 
-def _load_module(name, path):
+def _load_module(name: str, path: str) -> ModuleType:
     spec = importlib.util.spec_from_file_location(name, path)
+    if not spec:
+        raise RuntimeError(f"Unable to load {name} from {path}, ModuleSpec could not be created")
     module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+    loader = spec.loader
+    if not loader:
+        raise RuntimeError(f"Unable to load {name} from {path}, no Loader on the ModuleSpec")
+    loader.exec_module(module)
     return module
 
 
-def _find_concrete_subclasses(module, base_class):
-    ret = []
+T = TypeVar("T", bound=BaseBackend | DriverBase)
+
+def _find_concrete_subclasses(module: ModuleType, base_class: Type[T]) -> Type[T]:
+    ret: list[Type[T]] = []
     for attr_name in dir(module):
         attr = getattr(module, attr_name)
         if isinstance(attr, type) and issubclass(attr, base_class) and not inspect.isabstract(attr):
@@ -28,11 +37,11 @@ def _find_concrete_subclasses(module, base_class):
 
 @dataclass(frozen=True)
 class Backend:
-    compiler: BaseBackend = None
-    driver: DriverBase = None
+    compiler: Type[BaseBackend]
+    driver: Type[DriverBase]
 
 
-def _discover_backends():
+def _discover_backends() -> dict[str, Backend]:
     backends = dict()
     root = os.path.dirname(__file__)
     for name in os.listdir(root):
@@ -42,9 +51,9 @@ def _discover_backends():
             continue
         compiler = _load_module(name, os.path.join(root, name, 'compiler.py'))
         driver = _load_module(name, os.path.join(root, name, 'driver.py'))
-        backends[name] = Backend(_find_concrete_subclasses(compiler, BaseBackend),
-                                 _find_concrete_subclasses(driver, DriverBase))
+        backends[name] = Backend(_find_concrete_subclasses(compiler, BaseBackend),  # type: ignore
+                                 _find_concrete_subclasses(driver, DriverBase))  # type: ignore
     return backends
 
 
-backends = _discover_backends()
+backends: dict[str, Backend] = _discover_backends()
