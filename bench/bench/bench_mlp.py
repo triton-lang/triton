52d512d2b90e46105684eb6960627f0edb294ead
Jokeren <robinho364@gmail.com>
2025-04-10 10:23:00 -0400

    Update

--- bench/bench/bench_mlp.py
+++ bench/bench/bench_mlp.py
@@ -22,8 +22,9 @@ def _query_gpu_specs():
     output = subprocess.check_output(cmd, stderr=subprocess.DEVNULL).decode().strip()
     name = output.splitlines()[0]
     return {
-        "NVIDIA H100 80GB HBM3": {"MAX_TFLOPS8": 1979, "MAX_TFLOPS16": 989, "MAX_TBPS": 3.35}, "HGX GB200":
-        {"MAX_TFLOPS8": 4500, "MAX_TFLOPS16": 2250, "MAX_TBPS": 8.0}
+        "NVIDIA H100 80GB HBM3": {"MAX_TFLOPS8": 1979, "MAX_TFLOPS16": 989, "MAX_TBPS": 3.35},
+        "NVIDIA GH200 480GB": {"MAX_TFLOPS8": 1979, "MAX_TFLOPS16": 989, "MAX_TBPS": 4.8},
+        "HGX GB200": {"MAX_TFLOPS8": 4500, "MAX_TFLOPS16": 2250, "MAX_TBPS": 8.0},
     }[name]
 
 
