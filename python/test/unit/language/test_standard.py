54de313e5d22344ed58ce22a71d80ddc94d68cf1
Phil Tillet <phil@openai.com>
2025-04-11 14:24:35 +0200

    [LANGUAGE] add support for topk

--- python/test/unit/language/test_standard.py
+++ python/test/unit/language/test_standard.py
@@ -27,24 +27,31 @@ def test_maximum_minium(dtype, op, device):
 
 @pytest.mark.interpreter
 @pytest.mark.parametrize("M, N", [[1, 512], [8, 64], [256, 16], [512, 8]])
+@pytest.mark.parametrize("K", [None, 16])
 @pytest.mark.parametrize("descending", [False, True])
 @pytest.mark.parametrize("dtype_str", ['int32', 'float16', 'float32', 'bfloat16'])
-def test_sort(M, N, descending, dtype_str, device):
+def test_sort(M, N, k, descending, dtype_str, device):
 
     @triton.jit
-    def sort_kernel(X, Z, N: tl.constexpr, M: tl.constexpr, descending: tl.constexpr):
+    def sort_kernel(X, Z, N: tl.constexpr, M: tl.constexpr, k: tl.constexpr, descending: tl.constexpr):
         offx = tl.arange(0, M)
         offy = tl.arange(0, N) * M
         off2d = offx[None, :] + offy[:, None]
         x = tl.load(X + off2d)
-        x = tl.sort(x, descending=descending)
+        if k is None:
+            x = tl.sort(x, descending=descending)
+        else:
+            x = tl.topk(x, k)
         tl.store(Z + off2d, x)
 
-    x = numpy_random((N, M), dtype_str=dtype_str)
+    x = numpy_random((N, k or M), dtype_str=dtype_str)
     x = torch.from_numpy(x).to(device)
-    y = torch.sort(x, descending=descending)[0]
+    if k is not None:
+        y = torch.topk(x, k=k)
+    else:
+        y = torch.sort(x, descending=descending)[0]
     z = torch.empty_like(x)
-    sort_kernel[(1, )](x, z, N, M, descending, num_warps=8)
+    sort_kernel[(1, )](x, z, N, M, k, descending, num_warps=8)
     assert (y == z).all(), (y, z)
 
 
