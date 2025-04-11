430b39d192e5d811618b48fb71f389d61f2e2a6a
Peter Bell <peterbell10@openai.com>
2025-04-11 15:35:19 +0100

    [TUTORIAL] Replace legacy host side TMA with TensorDescriptor
    git-pr-chain: tutorial_replace_legacy_host_side_tma_wi_8ccb

--- python/triton/tools/experimental_descriptor.py
+++ python/triton/tools/experimental_descriptor.py
@@ -46,3 +46,11 @@ class TensorDescriptor:
     shape: List[int]
     strides: List[int]
     block_shape: List[int]
+
+    def from_tensor(tensor: Any, block_shape: List[int]):
+        return TensorDescriptor(
+            tensor,
+            tensor.shape,
+            tensor.stride(),
+            block_shape,
+        )
