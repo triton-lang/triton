c69c98876e588d4fa155cb8c795a473186a8698e
Peter Bell <peterbell10@openai.com>
2025-04-15 00:13:49 +0100

    Really fix the tests

--- python/test/unit/cuda/test_tma_descriptor.py
+++ python/test/unit/cuda/test_tma_descriptor.py
@@ -23,14 +23,14 @@ def test_1d_tma_descriptor_exception(M, BLOCK_M, expect_error):
     assert x.data_ptr() % 16 == 0
 
     desc = TensorDescriptor.from_tensor(x, [BLOCK_M])
-    ctx = pytest.raises(ValueError, msg="Shape element 0 must be a power of 2") if expect_error else nullcontext()
+    ctx = pytest.raises(ValueError, match="Shape element 0 must be a power of 2") if expect_error else nullcontext()
     with ctx:
         dummy_kernel[(1, )](desc)
 
 
-@pytest.mark.parametrize("M, BLOCK_M", [(128, 32), (125, 33)])
-@pytest.mark.parametrize("N, BLOCK_N, expect_error", [(128, 32, False), (128, 30, True), (127, 32, True)])
-def test_2d_tma_descriptor_exception(M, N, BLOCK_M, BLOCK_N, expect_error):
+@pytest.mark.parametrize("M, BLOCK_M, expect_error_m", [(128, 32, False), (125, 33, True)])
+@pytest.mark.parametrize("N, BLOCK_N, expect_error_n", [(128, 32, False), (128, 30, True), (127, 32, False)])
+def test_2d_tma_descriptor_exception(M, N, BLOCK_M, BLOCK_N, expect_error_n, expect_error_m):
     if not torch.cuda.is_available() or not torch.cuda.get_device_capability()[0] >= 9:
         pytest.skip("Test requires Hopper or Blackwell target.")
         return
@@ -43,6 +43,13 @@ def test_2d_tma_descriptor_exception(M, N, BLOCK_M, BLOCK_N, expect_error):
     assert A.data_ptr() % 16 == 0
 
     desc = TensorDescriptor.from_tensor(A, [BLOCK_M, BLOCK_N])
-    ctx = pytest.raises(ValueError, msg="Shape element . must be a power of 2") if expect_error else nullcontext()
+
+    shape_error = expect_error_n or expect_error_m
+    error_alignment = (N % 16) != 0
+    expect_error = shape_error or error_alignment
+
+    exc_type = ValueError if shape_error else RuntimeError
+    match = "Shape element . must be a power of 2" if shape_error else "Triton Error \\[CUDA\\]: invalid argument"
+    ctx = pytest.raises(exc_type, match=match) if expect_error else nullcontext()
     with ctx:
         dummy_kernel[(1, )](desc)
