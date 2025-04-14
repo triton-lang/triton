fc558c37aeac142b197f451b8317eebe293b6ff7
Chris Sullivan <chrsullivan@nvidia.com>
2025-04-14 20:48:10 +0000

    [Blackwell] Support DescriptorLoadOp when deciding to use shared memory for scales
    This ensures scales produced by TMA are elligible for transfer to tensor
    memory in later lowering.
    
    git-pr-chain: csullivan/support_desc_load_tmem_copy

--- lib/Dialect/TritonNvidiaGPU/IR/Ops.cpp
+++ lib/Dialect/TritonNvidiaGPU/IR/Ops.cpp
@@ -464,16 +464,6 @@ void TMEMAllocOp::getEffects(
                          getOperation()->getOpResult(0), TensorMemory::get());
 }
 
-static bool isDescendingOrder(triton::gpu::MemDescType type) {
-  auto order = triton::gpu::getOrder(type);
-  auto rank = type.getRank();
-  for (int i = 0; i < rank; ++i) {
-    if (order[i] != rank - 1 - i)
-      return false;
-  }
-  return true;
-}
-
 // -- TMEMCopyOp --
 LogicalResult TMEMCopyOp::verify() {
   if (!isa<triton::gpu::SharedMemorySpaceAttr>(
@@ -499,7 +489,7 @@ LogicalResult TMEMCopyOp::verify() {
       sharedEnc.getVec() != 1)
     return emitOpError("The source should not have swizzling applied for now");
 
-  if (!isDescendingOrder(srcTy)) {
+  if (!triton::gpu::isInnermostContiguous(srcTy, 512)) {
     return emitOpError("The source must be in a row-major order.");
   }
 
