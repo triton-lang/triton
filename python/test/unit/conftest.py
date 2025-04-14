dfd101689974df0be0664b9b5b1b502bc1f10aec
Dan Zimmerman <danzimm@meta.com>
2025-04-14 08:53:17 -0700

    Create fresh_config pytest fixture

--- python/test/unit/conftest.py
+++ python/test/unit/conftest.py
@@ -24,3 +24,22 @@ def fresh_triton_cache():
             yield tmpdir
         finally:
             os.environ.pop("TRITON_CACHE_DIR", None)
+
+@pytest.fixture
+def fresh_config(request):
+    from triton import config
+    test_name = request.node.name
+    config_map = {
+        name: cls
+        for name, cls in config.__dict__.items()
+        if isinstance(cls, config._base) and cls != config._base
+    }
+    try:
+        for name, cls in config_map.items():
+            new_cls = type(f"{name}_{test_name}", cls.__bases__, dict(cls.__dict__))
+            new_cls.reset()
+            setattr(config, name, new_cls)
+        yield
+    finally:
+        for name, cls in config_map.items():
+            setattr(config, name, cls)
