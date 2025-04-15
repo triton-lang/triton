7cbc87b2c552cf83e79802c6f9d0b2b414e82e72
Dan Zimmerman <danzimm@meta.com>
2025-04-15 06:37:27 -0700

    Simplify find_paths_if and add typehints to _utils

--- python/triton/_utils.py
+++ python/triton/_utils.py
@@ -1,35 +1,35 @@
 from functools import reduce
+from typing import cast, Any, Callable, TYPE_CHECKING
 
-
-def get_iterable_path(iterable, path):
-    return reduce(lambda a, idx: a[idx], path, iterable)
-
-
-def set_iterable_path(iterable, path, val):
-    prev = iterable if len(path) == 1 else get_iterable_path(iterable, path[:-1])
-    prev[path[-1]] = val
-
-
-def find_paths_if(iterable, pred):
+if TYPE_CHECKING:
     from .language import core
-    is_iterable = lambda x: isinstance(x, (list, tuple, core.tuple, core.tuple_type))
-    ret = dict()
 
-    def _impl(current, path):
-        path = (path[0], ) if len(path) == 1 else tuple(path)
+IterableType = list[Any] | tuple[Any, ...] | core.tuple | core.tuple_type
+ObjPath = tuple[int, ...]
+
+
+def get_iterable_path(iterable: IterableType, path: ObjPath) -> Any:
+    return reduce(lambda a, idx: cast(IterableType, a[idx]), path, iterable)  # type: ignore[index]
+
+
+def set_iterable_path(iterable: IterableType, path: tuple[int, ...], val: Any):
+    assert len(path) != 0
+    prev = iterable if len(path) == 1 else get_iterable_path(iterable, path[:-1])
+    prev[path[-1]] = val  # type: ignore[index]
+
+
+def find_paths_if(iterable: IterableType | Any, pred: Callable[[ObjPath, Any], bool]) -> list[ObjPath]:
+    from .language import core
+    is_iterable: Callable[[Any], bool] = lambda x: isinstance(x, (list, tuple, core.tuple, core.tuple_type))
+    ret = set()
+
+    def _impl(path: tuple[int, ...], current: Any):
         if is_iterable(current):
             for idx, item in enumerate(current):
-                _impl(item, path + (idx, ))
+                _impl((*path, idx), item)
         elif pred(path, current):
-            if len(path) == 1:
-                ret[(path[0], )] = None
-            else:
-                ret[tuple(path)] = None
+            ret.add(path)
 
-    if is_iterable(iterable):
-        _impl(iterable, [])
-    elif pred(list(), iterable):
-        ret = {tuple(): None}
-    else:
-        ret = dict()
-    return list(ret.keys())
+    _impl((), iterable)
+
+    return list(ret)
