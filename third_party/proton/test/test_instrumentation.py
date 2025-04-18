887472765aeaa576f8b4b0f5a97d0ba9b3f42e45
Jokeren <robinho364@gmail.com>
2025-04-17 21:46:04 -0400

    Update

--- third_party/proton/test/test_instrumentation.py
+++ third_party/proton/test/test_instrumentation.py
@@ -11,15 +11,15 @@ def is_hip():
     return triton.runtime.driver.active.get_current_target().backend == "hip"
 
 
-@pytest.mark.parametrize("mode", ["default", "default:metric_type=cycle", "default:metric_type=cycle:buffer_size=4096" "mma"])
+@pytest.mark.parametrize("mode", ["default", "default:metric_type=cycle", "default:metric_type=cycle:buffer_size=4096", "mma"])
 def test_mode_str(mode, tmp_path: pathlib.Path):
     temp_file = tmp_path / "test_mode_str.hatchet"
     proton.start(str(temp_file.with_suffix("")), backend="instrumentation", mode=mode)
     proton.finalize()
 
 
-@pytest.mark.parametrize("mode", [proton.mode.Default(), proton.mode.Default(metric_type="cycles"), 
-                                  proton.mode.Default(metric_type="cycles", buffer_size=4096),
+@pytest.mark.parametrize("mode", [proton.mode.Default(), proton.mode.Default(metric_type="cycle"), 
+                                  proton.mode.Default(metric_type="cycle", buffer_size=4096),
                                   proton.mode.MMA()])
 def test_mode_obj(mode, tmp_path: pathlib.Path):
     temp_file = tmp_path / "test_mode_simple.hatchet"
