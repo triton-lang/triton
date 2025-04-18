5238ce3f7edd72de88b91900439920e45382656d
junjian.zhan <junjian.zhan@iluvatar.com>
2025-04-18 13:32:34 +0000

    Revert error message change due to potential misleading wording

--- python/triton/runtime/errors.py
+++ python/triton/runtime/errors.py
@@ -19,7 +19,7 @@ class OutOfResources(TritonError):
         self.name = name
 
     def __str__(self) -> str:
-        return f"out of resource: {self.name}, Required: {self.required}, Hardware limit: {self.limit}. Reducing block sizes, `num_stages` or `num_warps` may help."
+        return f"out of resource: {self.name}, Required: {self.required}, Hardware limit: {self.limit}. Reducing block sizes or `num_stages` may help."
 
     def __reduce__(self):
         # this is necessary to make CompilationError picklable
