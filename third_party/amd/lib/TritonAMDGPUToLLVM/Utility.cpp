5b266479e106d520dee1106e1473f50aa1cf8dce
Yi Qian <yi.qian@amd.com>
2025-04-18 03:06:10 +0000

    Clang format

--- third_party/amd/lib/TritonAMDGPUToLLVM/Utility.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/Utility.cpp
@@ -496,10 +496,10 @@ int32_t getCtrlBitsForCacheModifierOnTarget(
   }
 }
 
-Value cvtFp32ToFp16RTNE_oneValue(Location loc, RewriterBase &rewriter, const Value &v) {
+Value cvtFp32ToFp16RTNE_oneValue(Location loc, RewriterBase &rewriter,
+                                 const Value &v) {
   LLVM::RoundingMode rm = LLVM::RoundingMode::NearestTiesToEven;
-  return rewriter.create<LLVM::FPTruncOp>(
-      loc, f16_ty, v);
+  return rewriter.create<LLVM::FPTruncOp>(loc, f16_ty, v);
 }
 
 Type getPointerTypeWithShape(Value basePtr, Value offset) {
