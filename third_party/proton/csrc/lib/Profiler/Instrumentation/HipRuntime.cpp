32487bc77a0c89fbb7dd04b7e87004df42bcaf45
Keren Zhou <kerenzhou@openai.com>
2025-04-22 21:35:21 -0400

    [PROTON-DEV] Initialize the runtime of the instrumentation profiler (#6567)

---
+++ third_party/proton/csrc/lib/Profiler/Instrumentation/HipRuntime.cpp
@@ -0,0 +1,17 @@
+#include "Profiler/Instrumentation/HipRuntime.h"
+
+namespace proton {
+
+void HipRuntime::allocateHostBuffer(uint8_t **buffer, size_t size) {}
+
+void HipRuntime::freeHostBuffer(uint8_t *buffer) {}
+
+uint64_t HipRuntime::getDevice() { return 0; }
+
+void *HipRuntime::getPriorityStream() { return nullptr; }
+
+void HipRuntime::processHostBuffer(
+    uint8_t *hostBuffer, size_t hostBufferSize, uint8_t *deviceBuffer,
+    size_t deviceBufferSize, void *stream,
+    std::function<void(uint8_t *, size_t)> callback) {}
+} // namespace proton
