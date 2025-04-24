3c999f5454df4c0453b833822d08b3b95356b185
Tori <vwbaker@google.com>
2025-04-24 11:21:24 +0200

    Undo setup.py change

--- python/setup.py
+++ python/setup.py
@@ -195,12 +195,19 @@ def get_llvm_package_info():
     if system == "Darwin":
         system_suffix = f"macos-{arch}"
     elif system == "Linux":
-        if arch == 'arm64':
+        if arch == 'arm64' and is_linux_os('almalinux'):
             system_suffix = 'almalinux-arm64'
+        elif arch == 'arm64':
+            system_suffix = 'ubuntu-arm64'
         elif arch == 'x64':
             vglibc = tuple(map(int, platform.libc_ver()[1].split('.')))
             vglibc = vglibc[0] * 100 + vglibc[1]
-            if vglibc > 217:
+            if vglibc > 228:
+                # Ubuntu 24 LTS (v2.39)
+                # Ubuntu 22 LTS (v2.35)
+                # Ubuntu 20 LTS (v2.31)
+                system_suffix = "ubuntu-x64"
+            elif vglibc > 217:
                 # Manylinux_2.28 (v2.28)
                 # AlmaLinux 8 (v2.28)
                 system_suffix = "almalinux-x64"
