a57cc24fcc0e59677661bf12487f254e84d9d8d0
Keren Zhou <kerenzhou@openai.com>
2025-04-25 11:05:10 -0400

    [PROTON-DEV] Fix problems after rebase against main (#6608)

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -89,6 +89,9 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::triton::registerTritonAMDGPULowerInstructionSchedHints();
   mlir::registerTritonAMDFoldTrueCmpI();
 
+  // NVWS passes
+  mlir::registerNVWSTransformsPasses();
+
   // Proton passes
   mlir::test::proton::registerTestScopeIdAllocationPass();
   mlir::triton::proton::registerConvertProtonToProtonGPU();
@@ -109,5 +112,5 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
       mlir::triton::nvgpu::NVGPUDialect, mlir::triton::nvws::NVWSDialect,
       mlir::triton::amdgpu::TritonAMDGPUDialect,
       mlir::triton::proton::ProtonDialect,
-      mlir::triton::proton::ProtonGPUDialect, mlir::ROCDL::ROCDLDialect>();
+      mlir::triton::proton::gpu::ProtonGPUDialect, mlir::ROCDL::ROCDLDialect>();
 }
