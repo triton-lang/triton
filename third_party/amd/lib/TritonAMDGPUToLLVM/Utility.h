66052e38966e6be96f99c7944aba8f89048f6d95
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-04-28 17:15:46 +0000

    Address review

--- third_party/amd/lib/TritonAMDGPUToLLVM/Utility.h
+++ third_party/amd/lib/TritonAMDGPUToLLVM/Utility.h
@@ -14,14 +14,14 @@
 
 namespace mlir::LLVM::AMD {
 
-const char predicatedLoad[] = "__predicated_load";
-const char predicatedLoadCA[] = "__predicated_load_CA";
-const char predicatedLoadCG[] = "__predicated_load_CG";
-const char predicatedLoadCV[] = "__predicated_load_CV";
-const char predicatedStore[] = "__predicated_store";
-const char predicatedStoreCG[] = "__predicated_store_CG";
-const char predicatedStoreCS[] = "__predicated_store_CS";
-const char predicatedStoreWT[] = "__predicated_store_WT";
+const char predicatedLoad[] = "__triton_hip_predicated_load";
+const char predicatedLoadCA[] = "__triton_hip_predicated_load_CA";
+const char predicatedLoadCG[] = "__triton_hip_predicated_load_CG";
+const char predicatedLoadCV[] = "__triton_hip_predicated_load_CV";
+const char predicatedStore[] = "__triton_hip_predicated_store";
+const char predicatedStoreCG[] = "__triton_hip_predicated_store_CG";
+const char predicatedStoreCS[] = "__triton_hip_predicated_store_CS";
+const char predicatedStoreWT[] = "__triton_hip_predicated_store_WT";
 const char noAliasAsyncLoads[] = "__no_alias_async_loads";
 
 Value shuffleXor(Location loc, RewriterBase &rewriter, Value val, int i,
@@ -47,6 +47,9 @@ Value llLoad(RewriterBase &rewriter, Location loc, Value ptr, Type elemTy,
              triton::CacheModifier cm = triton::CacheModifier::NONE);
 
 // Stores to shared or global memory with predication.
+// forceNoAliasAsyncLoads=true adds alias information to the llvm.store to
+// signal its not aliasing with any AsyncCopyGlobalToLocal/BufferLoadToLocal to
+// avoid conservative waits. See `addLocalLoadNoAliasScope` for more details
 void llStore(RewriterBase &rewriter, Location loc, Value ptr, Value val,
              Value pred, triton::CacheModifier cm = triton::CacheModifier::NONE,
              bool forceNoAliasAsyncLoads = false);
