d214e7f6ee5658a502ff4dd96fb1d0250b62ff20
Dan Zimmerman <danzimm@meta.com>
2025-04-29 16:10:53 -0700

    Fix test again, post knobs rename

--- python/test/unit/runtime/test_compilation_listener.py
+++ python/test/unit/runtime/test_compilation_listener.py
@@ -2,7 +2,7 @@ import triton
 import triton.language as tl
 
 from triton.backends.compiler import GPUTarget
-from triton.config import CompileTimes
+from triton.knobs import CompileTimes
 from triton.compiler.compiler import ASTSource, IRSource
 
 from typing import Any, Union
@@ -17,7 +17,7 @@ def cumsum_kernel(ptr):
     tl.store(block, tl.cumsum(x, 0))
 
 
-def test_compile_stats(device: str, fresh_config: Any, fresh_triton_cache: str) -> None:
+def test_compile_stats(device: str, fresh_knobs: Any, fresh_triton_cache: str) -> None:
     captured: Union[tuple[Union[ASTSource, IRSource], dict[str, Any], CompileTimes, bool], None] = None
 
     def compile_listener(src: Union[ASTSource, IRSource], metadata: dict[str, Any], times: CompileTimes,
@@ -26,7 +26,7 @@ def test_compile_stats(device: str, fresh_config: Any, fresh_triton_cache: str) 
         assert captured is None
         captured = (src, metadata, times, cache_hit)
 
-    fresh_config.compilation.listener = compile_listener
+    fresh_knobs.compilation.listener = compile_listener
 
     x = torch.randn(4, device=device)
     cumsum_kernel[(1, )](x)
