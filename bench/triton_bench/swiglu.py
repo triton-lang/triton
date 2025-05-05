triton-lang/triton

commit 8e23157a15c777dc08fcbd44728bcdaf954d509f

[BENCH] `bench_mlp.py` now plots performance vs. roofline (#6703)

bench/triton_bench/swiglu.py
@@ -5,6 +5,7 @@ import triton
 from triton.tools.tensor_descriptor import TensorDescriptor
 from .swiglu_details._swiglu import _swiglu
 from triton_bench import target_info
+from .matmul_ogs_details.metadata import compute_metadata
 
 
 @dataclass(frozen=True)
@@ -23,7 +24,7 @@ class PrecisionConfig:
 class SwiGLU(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, a, alpha, precision_config, expt_data, num_experts):
+    def forward(ctx, a, alpha, precision_config, routing_data, num_experts):
         N = a.shape[-1]
         M = a.numel() // N
         assert a.stride()[-1] == 1
@@ -48,7 +49,7 @@ class SwiGLU(torch.autograd.Function):
         # launch semi-persistent kernel
         N_BLOCKS = triton.cdiv(N // 2, BLOCK_N)
         num_sms = target_info.num_sms()
-        if expt_data is not None:
+        if routing_data is not None:
             waves_per_sm = 32 if target_info.is_hip() else 128
             num_pid = num_sms * (waves_per_sm // num_warps)
             M_BLOCKS = max(1, triton.cdiv(num_pid, N_BLOCKS))
@@ -59,6 +60,9 @@ class SwiGLU(torch.autograd.Function):
                 grid = (8 * num_sms, )
             else:
                 grid = (min(M_BLOCKS * N_BLOCKS, 4 * num_sms), )
+        expt_data = None
+        if routing_data is not None:
+            expt_data = compute_metadata(routing_data, M, BLOCK_M).buffer
         _swiglu[grid](
             out_desc,
             flex_ctx.out_data.reinterpret(out),
@@ -91,8 +95,8 @@ class SwiGLU(torch.autograd.Function):
         return out
 
 
-def swiglu(a, alpha, precision_config, expt_data=None, num_experts=0):
-    return SwiGLU.apply(a, alpha, precision_config, expt_data, num_experts)
+def swiglu(a, alpha, precision_config, routing_data=None, num_experts=0):
+    return SwiGLU.apply(a, alpha, precision_config, routing_data, num_experts)
 
 
 def swiglu_torch(a, alpha, precision_config):

bench/bench/bench_mlp.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
 import json
 import triton.profiler as proton
 import torch
@@ -8,6 +9,7 @@ from triton_bench.matmul_ogs import MicroscalingCtx, matmul_ogs, PrecisionConfig
 from triton_bench.numerics import InFlexData
 from triton_bench.routing import routing
 from triton_bench.target_info import is_hip, get_cdna_version
+from dataclasses import dataclass
 
 if torch.cuda.is_available() and not is_hip():
     from triton._C.libtriton import nvidia
@@ -66,9 +68,38 @@ def quantize(w, dtype, dev, **opt):
                                                 actual_weight_scale_shape=weight_scale_shape)
 
 
-def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype,
-              # tensor / expert parallelism
-              TP=1, EP=1, name=""):
+@dataclass
+class PerfData:
+    time: float
+    flops: float
+    bytes: float
+
+    @property
+    def tflops(self):
+        return self.flops / self.time * 1e-3
+
+    @property
+    def tbps(self):
+        return self.bytes / self.time * 1e-3
+
+    @property
+    def opint(self):
+        # operational intensity
+        assert self.bytes > 0
+        return self.flops / self.bytes
+
+    @property
+    def util(self) -> float:
+        if SPECS is None:
+            return 0.0
+
+        peak_flops = max(SPECS["MAX_TFLOPS8"], SPECS.get("MAX_TFLOPS16", 0))
+        min_t_flop = self.flops / peak_flops * 1e-3  # ns → µs
+        min_t_bw = self.bytes / SPECS["MAX_TBPS"] * 1e-3
+        return max(min_t_flop, min_t_bw) / self.time
+
+
+def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, EP, name):
     assert n_expts_tot % EP == 0
     assert dim2 % TP == 0
     dev = "cuda"
@@ -96,7 +127,7 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype,
     pc2 = PrecisionConfig(mx_ctx=w2_mx, flex_ctx=FlexCtx(rhs_data=w2_flex))
 
     # -- benchmark --
-    fpath = Path(f"logs/{name}/{batch}-{dim1}-{dim2}-{n_expts_tot}-{n_expts_act}-{x_dtype}-{w_dtype}.hatchet")
+    fpath = Path(f"logs/{name}/{x_dtype}-{w_dtype}-TP{TP}-EP{EP}/profiles/batch-{batch}.hatchet")
     fpath.parent.mkdir(parents=True, exist_ok=True)
     x_dtype = {"fp16": torch.float16, "bf16": torch.bfloat16, "fp8": torch.float8_e4m3fn}[x_dtype]
     # special treatment of fp8_e4m3 on AMD CDNA3 because it uses fp8_e4m3fnuz
@@ -115,7 +146,7 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype,
         else:
             rdata, gather_indx, scatter_indx = None, None, None
         x = matmul_ogs(x, w1, b1, rdata, gather_indx=gather_indx, precision_config=pc1)
-        x = triton_bench.swiglu.swiglu(x, 1.0, pcs)
+        x = triton_bench.swiglu.swiglu(x, 1.0, pcs, routing_data=rdata)
         x = matmul_ogs(x, w2, b2, rdata, scatter_indx=scatter_indx, precision_config=pc2)
     proton.finalize()
 
@@ -127,42 +158,70 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype,
         matmuls = [
             x for x in data[0]["children"] if "_matmul" in x["frame"]["name"] and "metadata" not in x["frame"]["name"]
         ]
-        tot_bytes = sum([x["metrics"]["bytes"] for x in matmuls])
-        tot_flops = {w: sum([x["metrics"].get(f"flops{w}", 0) for x in matmuls]) for w in [8, 16]}
+        bytes = sum([x["metrics"]["bytes"] for x in matmuls])
+        flops = {w: sum([x["metrics"].get(f"flops{w}", 0) for x in matmuls]) for w in [8, 16]}
+        flops = sum([flops[w] for w in [8, 16]])
         # compute total time (incl. "not useful" work)
         # TODO: proton should really be recording that in the json instead of
         # relying on the user to aggregate
-        tot_time = sum(x["metrics"].get("time (ns)", 0) for x in data[0]["children"])
-        min_time_flops = min_time_bytes = 0
-        if SPECS is not None:
-            min_time_flops = sum([tot_flops[w] / SPECS[f"MAX_TFLOPS{w}"] for w in [8, 16]]) * 1e-3
-            min_time_bytes = tot_bytes / SPECS["MAX_TBPS"] * 1e-3
-            min_time = max(min_time_flops, min_time_bytes)
-            util = min_time / tot_time
-        else:
-            util = 0.0
-        tflops = sum([tot_flops[w] for w in [8, 16]]) / tot_time * 1e-3
-        tbps = tot_bytes / tot_time * 1e-3
-        print(f"Utilization: {util:.0%}; {tflops:>6.1f} TFLOPs, {tbps:.1f} TB/s")
+        time = sum(x["metrics"].get("time (ns)", 0) for x in data[0]["children"])
+    return PerfData(time, flops, bytes)
 
-    return util, tflops, tbps
+
+def roofline_mlp(batch_ranges, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP=1, EP=1, name="",
+                 verbose=True):
+    import numpy as np
+    from itertools import chain
+    from bisect import bisect_left
+    batches = list(chain(*[range(*r) for r in batch_ranges]))
+    # collect performance data
+    perfs = []
+    print(f"Benchmarking {name} ({x_dtype}x{w_dtype}, TP={TP}, EP={EP})...")
+    print("===============================================================")
+    for batch in batches:
+        perfs += [bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, EP, name)]
+        if verbose:
+            print(f"Batch: {batch}; Util: {perfs[-1].util}; TFLOPS: {perfs[-1].tflops}; TBPS: {perfs[-1].tbps}")
+    print("===============================================================")
+    # machine limits
+    fig, ax = plt.subplots(figsize=(7, 5), dpi=120)
+    ax.set_xlabel("batch size (toks/expt)")
+    ax.set_ylabel("performance  [TFLOP/s]")
+    ax.set_title("roofline")
+    # add a tiny margin so points are not flush with the frame
+    xs = [batch * n_expts_act / n_expts_tot for batch in batches]
+    perf = [p.tflops for p in perfs]
+    xmin, xmax = min(xs), max(xs)
+    dx = 0.05 * (xmax - xmin) if xmax > xmin else 1.0
+    ax.set_xlim(xmin - dx, xmax + dx)
+    ax.set_ylim(100, SPECS["MAX_TFLOPS8"] + 500)
+    # plot roofline
+    max_tbps = SPECS["MAX_TBPS"]
+    max_tflops = SPECS["MAX_TFLOPS8"]
+    opints = [p.opint for p in perfs]
+    knee = bisect_left(opints, max_tflops / max_tbps) - 1
+    x_bw, x_comp = xs[:knee], xs[knee:]
+    y_bw = [op * max_tbps for op in opints[:knee]]
+    y_comp = [max_tflops] * len(x_comp)
+    ax.plot(x_bw, y_bw, "--", label=f"BW-bound  ({max_tbps:.0f} TB/s)")
+    ax.plot(x_comp, y_comp, "--", label=f"Compute-bound  ({max_tflops:.0f} TFLOP/s)")
+    # plot data
+    ax.scatter(xs, perf, marker="+")
+    ax.legend(frameon=False, loc="lower right")
+    ax.grid(True, which="both", ls=":", lw=0.5)
+    fig.tight_layout()
+    fpath = Path(f"logs/{name}/{x_dtype}-{w_dtype}-TP{TP}-EP{EP}/roofline.png")
+    plt.savefig(fpath)
 
 
 if __name__ == "__main__":
     has_native_mx4 = torch.cuda.get_device_capability(0)[0] >= 10 or get_cdna_version() == 4
     if SPECS is None:
         print("Current GPU has no specs provided, utilization is N/A")
-    if has_native_mx4:
-        bench_mlp(8192, 8192, 8192, 1, 1, "fp8", "fp8", TP=1, EP=1, name="dense")
-        bench_mlp(8192, 8192, 8192, 1, 1, "fp8", "mx4", TP=1, EP=1, name="dense")
-        bench_mlp(2048, 5120, 8192, 128, 4, "fp8", "fp8", TP=4, EP=1, name="llama4")
-        bench_mlp(2048, 5120, 8192, 128, 4, "fp8", "mx4", TP=4, EP=1, name="llama4")
-    else:
-        # bf16/fp16 x fp8 is skipped because matmul_ogs requires x and w has the
-        # same type when not doing mxfp operation
-        bench_mlp(8192, 8192, 8192, 1, 1, "fp8", "fp8", TP=1, EP=1, name="dense")
-        bench_mlp(8192, 8192, 8192, 1, 1, "fp16", "mx4", TP=1, EP=1, name="dense")
-        bench_mlp(8192, 8192, 8192, 1, 1, "bf16", "mx4", TP=1, EP=1, name="dense")
-        bench_mlp(2048, 5120, 8192, 128, 4, "fp8", "fp8", TP=4, EP=1, name="llama4")
-        bench_mlp(2048, 5120, 8192, 128, 4, "bf16", "mx4", TP=4, EP=1, name="llama4")
-        bench_mlp(2048, 5120, 8192, 128, 4, "fp16", "mx4", TP=4, EP=1, name="llama4")
+    batch_ranges = [(1024, 32768, 1024)]
+    dense_dtypes = ["fp8", "fp8"]
+    quantized_dtypes = ["fp8", "mx4"] if has_native_mx4 else ["bf16", "mx4"]
+    roofline_mlp(batch_ranges, 8192, 8192, 1, 1, *dense_dtypes, TP=1, EP=1, name="dense")
+    roofline_mlp(batch_ranges, 8192, 8192, 1, 1, *quantized_dtypes, TP=1, EP=1, name="dense")
+    roofline_mlp(batch_ranges, 5120, 8192, 128, 4, *dense_dtypes, TP=1, EP=1, name="llama4-maverick")
+    roofline_mlp(batch_ranges, 5120, 8192, 128, 4, *quantized_dtypes, TP=1, EP=1, name="llama4-maverick")

bench/triton_bench/matmul_ogs_details/_common.py
@@ -87,9 +87,10 @@ def matmul_launch_metadata(grid, kernel, args):
     fM = M if M is not None else n_tokens
     fK = K if K is not None else n_tokens
     ret[f"flops{nbits}"] = 2.0 * fM * N * fK
-    skipped = 0
+    gindx = args.get("GatherIndx", None)
     sindx = args.get("WriteBackIndx", None)
-    if sindx is not None:
-        skipped = (sindx == -1).sum() / sindx.numel()
-    ret["bytes"] = int((1 - skipped) * Y.numel() * Y.element_size() + X.numel() * X.element_size() + n_w_bytes)
+    sskipped = 0. if sindx is None else (sindx == -1).sum() / sindx.shape[0]
+    gskipped = 0. if gindx is None else (gindx == -1).sum() / gindx.shape[0]
+    ret["bytes"] = int((1 - sskipped) * Y.numel() * Y.element_size() + (1 - gskipped) * X.numel() * X.element_size() +
+                       n_w_bytes)
     return ret

bench/triton_bench/matmul_ogs_details/_ptma_matmul_ogs.py
@@ -58,8 +58,6 @@ def _make_tensor_desc(ptr, shape, strides, block_shape, transpose: tl.constexpr 
     tl.static_assert(len(shape) == len(strides))
     tl.static_assert(len(strides) == len(block_shape))
     if transpose:
-        # Pass constexpr(1) to workaround torchflow tracer changing values of 1 to 2 during compile.
-        # We check that the stride is actually 1 before launching the kernel.
         return tl.make_tensor_descriptor(
             ptr,
             shape=shape[:-2] + [shape[-1], shape[-2]],
@@ -67,8 +65,6 @@ def _make_tensor_desc(ptr, shape, strides, block_shape, transpose: tl.constexpr 
             block_shape=block_shape[:-2] + [block_shape[-1], block_shape[-2]],
         )
     else:
-        # Pass constexpr(1) to workaround torchflow tracer changing values of 1 to 2 during compile.
-        # We check that the stride is actually 1 before launching the kernel.
         return tl.make_tensor_descriptor(
             ptr,
             shape=shape,

bench/triton_bench/matmul_ogs_details/opt_flags.py
@@ -59,7 +59,6 @@ def make_default_opt_flags_amd(
         block_m = 128
     elif tokens_per_expt >= 512 and n >= 2048:
         block_m = 128
-
     else:
         block_m = max(32, min(triton.next_power_of_2(tokens_per_expt), 64))
     if routing_data is not None:
@@ -139,7 +138,7 @@ def make_default_opt_flags_nvidia(
     elif enforce_bitwise_invariance:
         block_m = 128
     else:
-        block_m = max(16, min(triton.next_power_of_2(tokens_per_expt), 128))
+        block_m = max(64, min(triton.next_power_of_2(tokens_per_expt), 128))
     # TODO: remove when triton is more optimized for H100 MXFP4
     arch = None
     if (

commit 8a154d62e8b7c0e9e5a89351f75be5ab53aee518

Remove TMA workaround in swiglu
It is no longer needed after the convert layout cost model
added in https://github.com/triton-lang/triton/pull/6699

Also fix test_swiglu.py. It broke after https://github.com/triton-lang/triton/pull/6703/files.

bench/triton_bench/swiglu.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from triton_bench.numerics import InFlexData, OutFlexData
 import torch
 import triton
-from triton.tools.tensor_descriptor import TensorDescriptor
 from .swiglu_details._swiglu import _swiglu
 from triton_bench import target_info
 from .matmul_ogs_details.metadata import compute_metadata
@@ -35,17 +34,6 @@ class SwiGLU(torch.autograd.Function):
         BLOCK_M, BLOCK_N = 32 // a.itemsize, 128
         num_warps = 4
         kwargs = {'maxnreg': 64} if not target_info.is_hip() else {}
-        # TMA descriptors
-        out_desc = None
-        a_desc = None
-        if target_info.cuda_capability_geq(9, 0) and flex_ctx.out_data.actual_scale is not None:
-            # We need TMA to store the outputs otherwise Triton will aggressively removing layout conversions at
-            # the cost of duplicating too much compute. With TMA, the layout conversion gets folded into the TMA store,
-            # and the duplication doesn't occur.
-            assert out.shape[-1] * out.element_size() % 16 == 0
-            out_desc = TensorDescriptor.from_tensor(out, (BLOCK_M, BLOCK_N))
-            assert a.shape[-1] * a.element_size() % 16 == 0
-            a_desc = TensorDescriptor.from_tensor(a, (BLOCK_M, 2 * BLOCK_N))
         # launch semi-persistent kernel
         N_BLOCKS = triton.cdiv(N // 2, BLOCK_N)
         num_sms = target_info.num_sms()
@@ -64,12 +52,10 @@ class SwiGLU(torch.autograd.Function):
         if routing_data is not None:
             expt_data = compute_metadata(routing_data, M, BLOCK_M).buffer
         _swiglu[grid](
-            out_desc,
             flex_ctx.out_data.reinterpret(out),
             flex_ctx.out_data.expected_scale,
             flex_ctx.out_data.actual_scale,
             flex_ctx.out_data.checksum_scale,
-            a_desc,
             flex_ctx.inp_data.reinterpret(a),
             flex_ctx.inp_data.scale,
             alpha,

bench/triton_bench/swiglu_details/_swiglu.py
@@ -35,10 +35,9 @@ def swiglu_launch_metadata(grid, kernel, args):
 
 
 @triton.jit(repr=swiglu_repr, launch_metadata=swiglu_launch_metadata)
-def _swiglu(out_desc, Out, OutExpectedScale, OutActualScale, OutChecksumScale, a_desc, A, AScale, alpha, M, N,
-            stride_am, stride_an, stride_outm, stride_outn, limit: tl.constexpr, ExptData, NUM_EXPERTS: tl.constexpr,
-            BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, EVEN_N: tl.constexpr, M_BLOCKS, N_BLOCKS,
-            flexpoint_saturate_inf: tl.constexpr):
+def _swiglu(Out, OutExpectedScale, OutActualScale, OutChecksumScale, A, AScale, alpha, M, N, stride_am, stride_an,
+            stride_outm, stride_outn, limit: tl.constexpr, ExptData, NUM_EXPERTS: tl.constexpr, BLOCK_M: tl.constexpr,
+            BLOCK_N: tl.constexpr, EVEN_N: tl.constexpr, M_BLOCKS, N_BLOCKS, flexpoint_saturate_inf: tl.constexpr):
     if ExptData is not None:
         M = tl.load(ExptData + 2 * NUM_EXPERTS)
         M_BLOCKS = (M + BLOCK_M - 1) // BLOCK_M
@@ -61,8 +60,6 @@ def _swiglu(out_desc, Out, OutExpectedScale, OutActualScale, OutChecksumScale, a
         # load a
         packed_off_n = pid_n * 2 * BLOCK_N + tl.arange(0, 2 * BLOCK_N)
         packed_offs = off_m[:, None] * stride_am + packed_off_n[None, :] * stride_an
-        if a_desc is not None:
-            a_packed = a_desc.load([pid_m * BLOCK_M, pid_n * 2 * BLOCK_N])
         if EVEN_N:
             a_packed = tl.load(A + packed_offs, mask=mask_m[:, None], other=0.)
         else:
@@ -91,11 +88,7 @@ def _swiglu(out_desc, Out, OutExpectedScale, OutActualScale, OutChecksumScale, a
         out = float_to_flex(out, out_expected_scale,
                             None,  # ActualScale: local absmax is tracked and updated after the loop
                             OutChecksumScale, None, Out, flexpoint_saturate_inf)
-
-        if out_desc is not None:
-            out_desc.store([pid_m * BLOCK_M, pid_n * BLOCK_N], out.to(Out.dtype.element_ty))
-        else:
-            mask = mask_m[:, None] if EVEN_N else mask_m[:, None] and mask_n[None, :]
-            tl.store(Out + off_m[:, None] * stride_outm + off_n[None, :] * stride_outn, out, mask)
+        mask = mask_m[:, None] if EVEN_N else mask_m[:, None] and mask_n[None, :]
+        tl.store(Out + off_m[:, None] * stride_outm + off_n[None, :] * stride_outn, out, mask)
 
     update_scale(local_max, OutActualScale, Out)

bench/tests/test_swiglu.py
@@ -5,7 +5,6 @@ import torch
 import pytest
 
 from .test_routing import init_data as init_routing_data
-from .test_routing import ref_expt_data
 
 # ---------------
 # initialize data
@@ -33,8 +32,7 @@ def test_op(M, N, limit, device, alpha=0.5):
     n_expts_act = 2
     logits = init_routing_data(M, n_expts_tot).detach()
     routing_data, _, _ = routing_torch(logits, n_expts_act)
-    expt_data = ref_expt_data(routing_data, M * n_expts_act, block_m=128)
-    n_tokens = expt_data[2 * n_expts_tot].sum()
+    n_tokens = routing_data.expt_hist.sum()
 
     # initialize data
     x = alloc_rand([n_tokens, N], device=device, dtype=torch.bfloat16)