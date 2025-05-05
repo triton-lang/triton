851b5bf12ba83bfd460721fec8cadd39187f76ac
Jokeren <robinho364@gmail.com>
2025-05-04 20:40:16 -0400

    [PROTON-DEV] Fix Lint

--- third_party/nvidia/backend/driver.py
+++ third_party/nvidia/backend/driver.py
@@ -724,8 +724,8 @@ class CudaLauncher(object):
         global_scratch = allocate_scratch(self.global_scratch_size, self.global_scratch_align, _allocation._allocator)
         profile_scratch = allocate_scratch(self.profile_scratch_size, self.profile_scratch_align,
                                            _allocation._profile_allocator)
-        self.launch(gridX, gridY, gridZ, stream, function, self.launch_cooperative_grid, self.launch_pdk, global_scratch,
-                    profile_scratch, *args)
+        self.launch(gridX, gridY, gridZ, stream, function, self.launch_cooperative_grid, self.launch_pdk,
+                    global_scratch, profile_scratch, *args)
 
 
 class CudaDriver(GPUDriver):
