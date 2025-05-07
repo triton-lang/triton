6caf54011df39ee616346f66c5f996f62fdd4577
Yueming Hao <yhao@meta.com>
2025-05-06 19:53:11 -0700

    take metadata back

--- python/test/unit/runtime/test_compilation_listener.py
+++ python/test/unit/runtime/test_compilation_listener.py
@@ -18,13 +18,13 @@ def cumsum_kernel(ptr):
 
 
 def test_compile_stats(device: str, fresh_knobs_except_libraries: Any, fresh_triton_cache: str) -> None:
-    captured: Union[tuple[Union[ASTSource, IRSource], dict[str, Any], CompileTimes, bool], None] = None
+    captured: Union[tuple[Union[ASTSource, IRSource], dict[str, Any], dict[str, Any], CompileTimes, bool], None] = None
 
-    def compile_listener(src: Union[ASTSource, IRSource], metadata_group: dict[str, Any], times: CompileTimes,
+    def compile_listener(src: Union[ASTSource, IRSource], metadata: dict[str, Any], metadata_group: dict[str, Any], times: CompileTimes,
                          cache_hit: bool) -> None:
         nonlocal captured
         assert captured is None
-        captured = (src, metadata_group, times, cache_hit)
+        captured = (src, metadata, metadata_group, times, cache_hit)
 
     fresh_knobs_except_libraries.compilation.listener = compile_listener
 
@@ -34,7 +34,7 @@ def test_compile_stats(device: str, fresh_knobs_except_libraries: Any, fresh_tri
     assert captured is not None
 
     # No cache hit at first
-    assert not captured[3]
+    assert not captured[4]
 
     # Expected metadata
     assert len(captured[1]["hash"]) > 0
@@ -53,7 +53,7 @@ def test_compile_stats(device: str, fresh_knobs_except_libraries: Any, fresh_tri
 
     assert captured is not None
     # Cache hit!
-    assert captured[3]
+    assert captured[4]
 
     # Expected metadata
     assert len(captured[1]["hash"]) > 0
