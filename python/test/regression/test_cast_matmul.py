a43f4f479eb84bbd9d3d349af9597d16f41c9da2
lezcano <lezcano-93@hotmail.com>
2025-05-14 12:28:00 +0100

    Revert "[PIPELINER] Pipeline RS WGMMA (#6804)"
    This reverts commit 21fd9ebbf248a3da48c5dbfa51b486ab4476f327.

--- python/test/regression/test_cast_matmul.py
+++ python/test/regression/test_cast_matmul.py
@@ -35,7 +35,8 @@ def matmul_kernel(A, B, C, M, N, K,  #
                   stride_am, stride_ak,  #
                   stride_bk, stride_bn,  #
                   stride_cm, stride_cn,  #
-                  compute_dtype: tl.constexpr, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr,  #
+                  dot_out_dtype: tl.constexpr,  #
+                  BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr,  #
                   BLOCK_K: tl.constexpr, GROUP_M: tl.constexpr):
     # matrix multiplication
     pid = tl.program_id(0)
@@ -56,14 +57,15 @@ def matmul_kernel(A, B, C, M, N, K,  #
     # pointers
     A = A + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
     B = B + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
-    acc_dtype = tl.float16 if compute_dtype == tl.float16 and C.dtype.element_ty == tl.float16 else tl.float32
-    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=acc_dtype)
+    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=dot_out_dtype)
     for k in range(0, tl.cdiv(K, BLOCK_K)):
         k_remaining = K - k * BLOCK_K
-        _0 = tl.zeros((1, 1), dtype=compute_dtype)
+        _0 = tl.zeros((1, 1), dtype=C.dtype.element_ty)
         a = tl.load(A, mask=rk[None, :] < k_remaining, other=_0)
         b = tl.load(B, mask=rk[:, None] < k_remaining, other=_0)
-        acc += tl.dot(a.to(compute_dtype), b.to(compute_dtype), out_dtype=acc_dtype)
+        a = a.to(C.dtype.element_ty)
+        b = b.to(C.dtype.element_ty)
+        acc += tl.dot(a, b, out_dtype=dot_out_dtype)
         A += BLOCK_K * stride_ak
         B += BLOCK_K * stride_bk
     acc = acc.to(C.dtype.element_ty)
@@ -98,31 +100,14 @@ def test_cast_matmul(M, K, N, BLOCK_K, BLOCK_M, BLOCK_N, w_dtype, x_dtype, out_d
         else:
             return torch.randn(shape, device=device, dtype=dtype)
 
-    def compute_dtype(a_dtype, b_dtype):
-        # a holds the larger dtype
-        if a_dtype.itemsize < b_dtype.itemsize:
-            a_dtype, b_dtype = b_dtype, a_dtype
-        # float64 matmul is not supported by triton
-        if a_dtype == torch.float64:
-            return torch.float32
-        # If they are both 1 byte or float16 and (1 byte or float16)
-        if a_dtype.itemsize == 1 or (a_dtype == torch.float16 and b_dtype != torch.bfloat16):
-            return torch.float16
-        else:
-            return torch.float32
-
-    # nasty hack
-    def get_triton_dtype(dtype):
-        return getattr(tl, str(dtype).removeprefix("torch."))
-
     torch.manual_seed(42)
     a = init_tensor(w_dtype, (M, K))
     b = init_tensor(x_dtype, (K, N))
 
     torch_dtype = getattr(torch, out_dtype)
+    triton_dtype = getattr(tl, out_dtype)  # <- here force dot_out_dtype
     out_torch = torch.matmul(a.to(torch_dtype), b.to(torch_dtype))
     out_triton = torch.empty((M, N), device=device, dtype=torch_dtype)
-    compute_triton = get_triton_dtype(compute_dtype(w_dtype, x_dtype))
 
     # launch kernel
     block_m, block_n, block_k = BLOCK_M, BLOCK_N, BLOCK_K
@@ -132,8 +117,8 @@ def test_cast_matmul(M, K, N, BLOCK_K, BLOCK_M, BLOCK_N, w_dtype, x_dtype, out_d
         a, b, out_triton, M, N, K,  #
         a.stride(0), a.stride(1),  #
         b.stride(0), b.stride(1),  #
-        out_triton.stride(0), out_triton.stride(1),  #
-        compute_triton, GROUP_M=8,  #
+        out_triton.stride(0), out_triton.stride(1), dot_out_dtype=triton_dtype,  #
+        GROUP_M=8,  #
         BLOCK_M=block_m,  #
         BLOCK_N=block_n,  #
         BLOCK_K=block_k)
