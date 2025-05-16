4b95c9c7984eb54b4a7b2db60a7e6b40c14c2fde
Corbin Robeck <robeck@meta.com>
2025-05-16 15:26:21 -0400

    [PROTON-DEV] Get final remaining Proton Dialect instrumentation tests passing on AMD (#6843)
    This PR gets the final remaining Proton Dialect instrumentation tests
    passing on AMD

--- third_party/proton/csrc/include/Driver/GPU/HipApi.h
+++ third_party/proton/csrc/include/Driver/GPU/HipApi.h
@@ -8,6 +8,8 @@ namespace proton {
 
 namespace hip {
 
+template <bool CheckSuccess> hipError_t ctxGetDevice(hipDevice_t *device);
+
 template <bool CheckSuccess> hipError_t deviceSynchronize();
 
 template <bool CheckSuccess>
@@ -21,10 +23,27 @@ hipError_t getDeviceProperties(hipDeviceProp_t *prop, int deviceId);
 
 Device getDevice(uint64_t index);
 
+template <bool CheckSuccess>
+hipError_t ctxGetStreamPriorityRange(int *leastPriority, int *greatestPriority);
+
+template <bool CheckSuccess>
+hipError_t streamCreateWithPriority(hipStream_t *pStream, unsigned int flags,
+                                    int priority);
+
+template <bool CheckSuccess> hipError_t streamSynchronize(hipStream_t stream);
+
+template <bool CheckSuccess>
+hipError_t memcpyDToHAsync(void *dst, hipDeviceptr_t src, size_t count,
+                           hipStream_t stream);
+
 const std::string getHipArchName(uint64_t index);
 
 const char *getKernelNameRef(const hipFunction_t f);
 const char *getKernelNameRefByPtr(const void *hostFunction, hipStream_t stream);
+
+template <bool CheckSuccess>
+hipError_t memAllocHost(void **pp, size_t bytesize);
+template <bool CheckSuccess> hipError_t memFreeHost(void *p);
 
 } // namespace hip
 
