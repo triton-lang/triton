502891492265916603e337a8cfd04606035345c2
Mogball <jeffniu22@gmail.com>
2025-05-19 14:05:50 -0700

    use analyiss

--- lib/Dialect/TritonGPU/Transforms/BufferReuse.cpp
+++ lib/Dialect/TritonGPU/Transforms/BufferReuse.cpp
@@ -2,6 +2,7 @@
 #include "mlir/IR/Dominance.h"
 #include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/Pass/Pass.h"
+#include "triton/Analysis/LiveBuffers.h"
 #include "triton/Dialect/Triton/IR/Utility.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
@@ -12,293 +13,6 @@ using namespace mlir;
 using namespace triton;
 using namespace triton::gpu;
 namespace ttng = triton::nvidia_gpu;
-
-//===----------------------------------------------------------------------===//
-// RegionPredecessorAnalysis
-//===----------------------------------------------------------------------===//
-
-using VirtualBlock = std::pair<Block *, Block::iterator>;
-
-struct RegionPredecessorAnalysis {
-  RegionPredecessorAnalysis(Operation *op);
-
-  DenseMap<llvm::PointerUnion<Operation *, Region *>, SetVector<VirtualBlock>>
-      predecessors;
-};
-
-RegionPredecessorAnalysis::RegionPredecessorAnalysis(Operation *op) {
-  op->walk([&](Operation *op) {
-    if (auto br = dyn_cast<RegionBranchOpInterface>(op)) {
-      SmallVector<RegionSuccessor> successors;
-      br.getSuccessorRegions(RegionBranchPoint::parent(), successors);
-      VirtualBlock it(op->getBlock(), op->getIterator());
-      for (RegionSuccessor &successor : successors) {
-        if (successor.isParent())
-          predecessors[op].insert(it);
-        else
-          predecessors[successor.getSuccessor()].insert(it);
-      }
-      return WalkResult::advance();
-    }
-
-    // FIXME: `ReturnLike` adds `RegionBranchTerminatorOpInterface` for some
-    // reason. Check that the parent is actually a `RegionBranchOpInterface`.
-    auto br = dyn_cast<RegionBranchTerminatorOpInterface>(op);
-    if (br && isa<RegionBranchOpInterface>(br->getParentOp())) {
-      SmallVector<Attribute> operands(br->getNumOperands());
-      SmallVector<RegionSuccessor> regions;
-      br.getSuccessorRegions(operands, regions);
-      VirtualBlock it(br->getBlock(), br->getBlock()->end());
-      for (RegionSuccessor &successor : regions) {
-        if (successor.isParent())
-          predecessors[br->getParentOp()].insert(it);
-        else
-          predecessors[successor.getSuccessor()].insert(it);
-      }
-      return WalkResult::advance();
-    }
-
-    return WalkResult::advance();
-  });
-}
-
-//===----------------------------------------------------------------------===//
-// BufferRange
-//===----------------------------------------------------------------------===//
-
-namespace {
-struct BufferRange {
-  Value alloc;
-  bool isFullRange;
-};
-} // namespace
-
-static BufferRange getBufferAccess(Value value) {
-  if (auto trans = value.getDefiningOp<MemDescTransOp>()) {
-    // The full memdesc is accessed.
-    return getBufferAccess(trans.getSrc());
-  }
-
-  if (auto reshape = value.getDefiningOp<MemDescReshapeOp>()) {
-    // The full memdesc is accessed.
-    return getBufferAccess(reshape.getSrc());
-  }
-
-  if (auto subview = value.getDefiningOp<MemDescSubviewOp>()) {
-    BufferRange srcAccess = getBufferAccess(subview.getSrc());
-    // If the access range is unknown, propagate that.
-    if (!srcAccess.isFullRange)
-      return srcAccess;
-    // FIXME: Only full buffer accesses are supported for now.
-    for (Value offset : subview.getOffsets()) {
-      APInt value;
-      if (!matchPattern(offset, m_ConstantInt(&value)) || !value.isZero())
-        return {srcAccess.alloc, /*isFullRange=*/false};
-    }
-    if (subview.getType().getNumElements() !=
-        subview.getSrc().getType().getNumElements())
-      return {srcAccess.alloc, /*isFullRange=*/false};
-    // The full buffer is accessed.
-    return {srcAccess.alloc, /*isFullRange=*/true};
-  }
-
-  if (auto alloc = value.getDefiningOp<LocalAllocOp>()) {
-    uint64_t accessSize = product(alloc.getType().getShape());
-    return BufferRange{value, /*isFullRange=*/true};
-  }
-  if (auto alloc = value.getDefiningOp<ttng::TMEMAllocOp>()) {
-    uint64_t accessSize = product(alloc.getType().getShape());
-    return BufferRange{value, /*isFullRange=*/true};
-  }
-
-  // TODO: Could handle joins, like `scf.if` or `arith.select`, if necessary.
-  return {Value(), /*isFullRange=*/false};
-}
-
-// Get tracked buffers accessed by this operation and the memory effect on them.
-static SmallVector<std::pair<BufferRange, MemoryEffects::Effect *>>
-getAccessedBuffersAndEffects(Operation *op) {
-  SmallVector<std::pair<BufferRange, MemoryEffects::Effect *>> result;
-  auto itf = dyn_cast<MemoryEffectOpInterface>(op);
-  if (!itf)
-    return result;
-
-  SmallVector<MemoryEffects::EffectInstance> effects;
-  itf.getEffects(effects);
-  // Consider the effects in reverse order because this is a backwards
-  // analysis. E.g. a read then write should be treated as a write then read
-  // by the analysis.
-  for (const MemoryEffects::EffectInstance &effect : llvm::reverse(effects)) {
-    Value mem = effect.getValue();
-    if (!mem)
-      continue;
-    BufferRange access = getBufferAccess(mem);
-    if (!access.alloc)
-      continue;
-    result.push_back({access, effect.getEffect()});
-  }
-  return result;
-}
-
-// Determine whether all accesses to the buffer can be tracked by the analysis.
-// This function implements the inverse of `getBufferAccess` and
-// `getAccessedBuffersAndEffects`.
-static bool canTrackBufferAccesses(Value value) {
-  for (Operation *user : value.getUsers()) {
-    // Look through views.
-    if (user->hasTrait<OpTrait::MemDescViewTrait>()) {
-      if (!canTrackBufferAccesses(user->getResult(0)))
-        return false;
-      continue;
-    }
-    // Consider any known memory-effecting operation as a sink.
-    if (isa<MemoryEffectOpInterface>(user))
-      continue;
-    return false;
-  }
-  return true;
-}
-
-//===----------------------------------------------------------------------===//
-// BufferLiveRangeAnalysis
-//===----------------------------------------------------------------------===//
-
-using llvm::BitVector;
-using BufferStates = std::optional<BitVector>;
-
-namespace {
-struct BufferLiveRangeAnalysis {
-  static bool join(BufferStates &lhs, const BitVector &rhs);
-  void initialize(FuncOp func);
-  void run(FuncOp func, RegionPredecessorAnalysis &preds);
-
-  SmallVector<Operation *> buffers;
-  llvm::MapVector<Operation *, size_t> bufferIds;
-  llvm::MapVector<VirtualBlock, BufferStates> bufferStates;
-};
-} // namespace
-
-bool BufferLiveRangeAnalysis::join(BufferStates &lhs, const BitVector &rhs) {
-  if (!lhs) {
-    lhs = rhs;
-    return true;
-  }
-  assert(lhs->size() == rhs.size());
-  if (*lhs == rhs)
-    return false;
-  *lhs |= rhs;
-  return true;
-}
-
-void BufferLiveRangeAnalysis::initialize(FuncOp func) {
-  // First find all the buffers to track.
-  func.walk([&](Operation *op) {
-    if (!isa<ttng::TMEMAllocOp, LocalAllocOp>(op) ||
-        !canTrackBufferAccesses(op->getResult(0)))
-      return;
-    auto memdesc = cast<MemDescType>(op->getResult(0).getType());
-    // Don't bother tracking buffers that are too small, such as mbarriers.
-    if (memdesc.getNumElements() < 16)
-      return;
-    bufferIds.insert({op, bufferIds.size()});
-    buffers.push_back(op);
-  });
-}
-
-void BufferLiveRangeAnalysis::run(FuncOp func,
-                                  RegionPredecessorAnalysis &preds) {
-  SmallVector<VirtualBlock> worklist;
-
-  // Root the analysis at function exits.
-  for (auto root : func.getOps<ReturnOp>()) {
-    Block *block = root->getBlock();
-    worklist.push_back({block, block->end()});
-    // All buffers are initially dead.
-    bufferStates.insert({worklist.back(), BitVector(bufferIds.size(), false)});
-  }
-
-  // Fixed-point propagation.
-  while (!worklist.empty()) {
-    auto [block, iter] = worklist.pop_back_val();
-    VirtualBlock afterIt(block, iter);
-    BitVector stateAfter = *bufferStates.find(afterIt)->second;
-
-    // Check for block entry.
-    if (iter == block->begin()) {
-      // Check for region entry.
-      if (block->isEntryBlock()) {
-        // Check for function entry.
-        if (block->getParentOp() == func) {
-          assert(stateAfter.none() && "all buffers should be dead on entry");
-          continue;
-        }
-
-        // The parent must be a region branch op.
-        assert(isa<RegionBranchOpInterface>(block->getParentOp()));
-        for (VirtualBlock pred : preds.predecessors.at(block->getParent())) {
-          if (join(bufferStates[pred], stateAfter))
-            worklist.push_back(pred);
-        }
-        continue;
-      }
-      // Check for predecessors.
-      for (Block *pred : block->getPredecessors()) {
-        VirtualBlock beforeIt(pred, pred->end());
-        if (join(bufferStates[beforeIt], stateAfter))
-          worklist.push_back(beforeIt);
-      }
-      continue;
-    }
-
-    // We are visiting an operation.
-    Operation &op = *std::prev(iter);
-
-    // Check for a region branch op.
-    if (isa<RegionBranchOpInterface>(op)) {
-      for (VirtualBlock pred : preds.predecessors.at(&op)) {
-        if (join(bufferStates[pred], stateAfter))
-          worklist.push_back(pred);
-      }
-      continue;
-    }
-
-    // HACK: Because `ttng.tmem_alloc` and `ttg.local_alloc` don't indicate an
-    // `Allocate` effect before memory allocation, we have to check them
-    // separately.
-    BitVector curState = stateAfter;
-    if (auto it = bufferIds.find(&op); it != bufferIds.end()) {
-      // Buffers are definitely dead before they are allocated.
-      curState.reset(it->second);
-    }
-
-    // Check the operation for effects on tracked buffers.
-    for (auto [access, effect] : getAccessedBuffersAndEffects(&op)) {
-      auto it = bufferIds.find(access.alloc.getDefiningOp());
-      if (it == bufferIds.end())
-        continue;
-      size_t bufferId = it->second;
-      if (isa<MemoryEffects::Read>(effect)) {
-        // Read always require the buffer to be live.
-        curState.set(bufferId);
-      } else if (isa<MemoryEffects::Write>(effect)) {
-        // Writes mark the buffer as dead only if we know it writes the whole
-        // buffer.
-        if (access.isFullRange)
-          curState.reset(bufferId);
-      } else if (isa<MemoryEffects::Allocate>(effect)) {
-        // Buffers are definitely dead before they are allocated.
-        curState.reset(bufferId);
-      } else {
-        assert(isa<MemoryEffects::Free>(effect));
-      }
-    }
-
-    // Enqueue the next virtual block if anything changed.
-    VirtualBlock beforeIt(block, op.getIterator());
-    if (join(bufferStates[beforeIt], curState))
-      worklist.push_back(beforeIt);
-  }
-}
 
 //===----------------------------------------------------------------------===//
 // reuseBuffers
@@ -388,15 +102,7 @@ static bool shouldMergeBuffers(Value a, Value b) {
 static void reuseBuffers(FuncOp func) {
   // First determine when buffers are live at every point in the function.
   RegionPredecessorAnalysis preds(func);
-  BufferLiveRangeAnalysis analysis;
-  analysis.initialize(func);
-  analysis.run(func, preds);
-
-  // There are likely a bunch of identical states, so compress them.
-  SetVector<BitVector> uniqueStates;
-  for (const BufferStates &state :
-       llvm::make_second_range(analysis.bufferStates))
-    uniqueStates.insert(*state);
+  LiveBuffersAnalysis analysis(func, preds);
 
   // Idealy, buffer merging runs after warp specialization and pipelining,
   // because the goal of both of these passes is to overlap execution of code.
@@ -423,7 +129,7 @@ static void reuseBuffers(FuncOp func) {
   };
   llvm::SpecificBumpPtrAllocator<MergedBuffer> allocator;
   SmallVector<MergedBuffer *> mergedBuffers;
-  for (auto [buffer, id] : analysis.bufferIds) {
+  for (auto [id, buffer] : llvm::enumerate(analysis.getBuffers())) {
     MergedBuffer *mergedBuffer = allocator.Allocate();
     ::new (mergedBuffer) MergedBuffer{{buffer}, {id}};
     mergedBuffers.push_back(mergedBuffer);
@@ -451,7 +157,7 @@ static void reuseBuffers(FuncOp func) {
   // For a given buffer, find all the buffers that at any point are live at the
   // same time as the given buffer. We can do this by joining all the masks for
   // when the buffer is live.
-  size_t numBuffers = analysis.buffers.size();
+  size_t numBuffers = analysis.getBuffers().size();
   bool changed;
   do {
     changed = false;
@@ -459,13 +165,7 @@ static void reuseBuffers(FuncOp func) {
     for (MergedBuffer *mergedBuffer : mergedBuffers) {
       if (mergedBuffer->ops.empty())
         continue;
-      BitVector liveMask(numBuffers, false);
-      for (const BitVector &state : uniqueStates) {
-        for (size_t id : mergedBuffer->ids) {
-          if (state.test(id))
-            liveMask |= state;
-        }
-      }
+      BitVector liveMask = analysis.getLiveBufferMask(mergedBuffer->ids);
       // Greedily merge buffers.
       for (MergedBuffer *otherBuffer : mergedBuffers) {
         if (mergedBuffer->ops.empty())
