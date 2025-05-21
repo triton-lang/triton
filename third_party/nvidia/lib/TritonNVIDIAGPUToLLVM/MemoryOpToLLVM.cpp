c23efff0f85811ddedd23da0c1af95ddc04f0015
Maksim Levental <maksim.levental@gmail.com>
2025-05-21 14:26:27 -0400

    fix LocalStoreOpConversion

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/MemoryOpToLLVM.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/MemoryOpToLLVM.cpp
@@ -265,6 +265,8 @@ struct LocalAllocOpConversion
                   ConversionPatternRewriter &rewriter) const override {
     if (!op.getSrc())
       return failure();
+    if (!targetInfo.supportLdStMatrix())
+      return failure();
     MemDescType memDescType = op.getType();
     RankedTensorType srcTy = op.getSrc().getType();
     Type llvmElemTy = typeConverter->convertType(srcTy.getElementType());
@@ -302,6 +304,8 @@ struct LocalStoreOpConversion
   LogicalResult
   matchAndRewrite(triton::gpu::LocalStoreOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
+    if (!targetInfo.supportLdStMatrix())
+      return failure();
     Type llvmElemTy =
         getTypeConverter()->convertType(op.getDst().getType().getElementType());
     SharedMemoryObject smemObj = LLVM::getSharedMemoryObjectFromStruct(
