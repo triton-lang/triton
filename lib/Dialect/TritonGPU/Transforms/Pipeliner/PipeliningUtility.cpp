8daa349e5c29cce9fa1f58d9925bb6c843e34261
pawelszczerbuk <153013546+pawelszczerbuk@users.noreply.github.com>
2025-05-23 16:47:14 -0700

    [PIPELINE] Peel single epilogue iteration after loop expansion (#6893)
    Attempt to improve performance of loops after mmav5 pipelining - it is
    often the case that last iteration of such loops has only one
    `barrier_wait`, while everything else is masked out.
    This implementation has only super simple handling of peeling single
    iteration. In the future we will most likely generalize upstream
    expander to control amount of prologue and epilogue peeling, so I
    decided against completely generic implementation for the moment.

--- lib/Dialect/TritonGPU/Transforms/Pipeliner/PipeliningUtility.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeliner/PipeliningUtility.cpp
@@ -1,5 +1,4 @@
 #include "triton/Dialect/TritonGPU/Transforms/PipeliningUtility.h"
-
 #include "mlir/Analysis/TopologicalSortUtils.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
@@ -160,6 +159,8 @@ Operation *mlir::triton::predicateOp(RewriterBase &rewriter, Operation *op,
                                      Value pred) {
   OpBuilder::InsertionGuard guard(rewriter);
   if (mlir::isMemoryEffectFree(op))
+    return op;
+  if (isConstantIntValue(pred, 1))
     return op;
   if (isa<LLVM::AssumeOp, ttng::FenceAsyncSharedOp>(op))
     return op;
