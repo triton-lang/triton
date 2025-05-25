aa6d6b3c80db2d2adb1577c89d52cb2b3cc3d0e9
Phil Tillet <phil@openai.com>
2025-05-25 13:01:40 -0700

    .

--- python/triton_kernels/triton_kernels/topk.py
+++ python/triton_kernels/triton_kernels/topk.py
@@ -3,7 +3,7 @@ from .topk_details._topk import _topk
 from .bitmatrix import Bitmatrix
 
 
-def topk(x, k, dim=1, return_bitmatrix=True):
+def topk(x, k, dim=1, return_bitmatrix=True, y_indx=None):
     cdiv = lambda a, b: (a + b - 1) // b
     BLOCK_M = 32
     BLOCK_N = 32
@@ -19,21 +19,22 @@ def topk(x, k, dim=1, return_bitmatrix=True):
     # scratchpad tensors
     # NOTE: these are not returned
     y_vals = torch.empty((n_rows, k), dtype=x.dtype, device=dev)
-    y_indx = torch.empty((n_rows, k), dtype=torch.int16, device=dev)
-
+    if y_indx is not None:
+        use_provided_indx = True
+    else:
+        y_indx = torch.empty((n_rows, k), dtype=torch.int16, device=dev)
+        use_provided_indx = False
     # create bitmatrix in transposed memory layout:
     bitmatrix = torch.empty((n_cols_words, cdiv(n_rows, 32) * 32), dtype=torch.uint32, device=dev)
     bitmatrix = torch.transpose(bitmatrix, 0, 1)[:n_rows]
     s_blocks = cdiv(n_cols, BLOCK_S)
     s_cols = s_blocks * BLOCK_S
     S = torch.empty((s_cols, ), dtype=torch.int32, device=dev)
-
     pids = max(cdiv(n_rows, BLOCK_M), s_blocks)
-
     _topk[(pids, )](
         x, x.stride(0),  # inputs
         y_vals, y_indx, y_vals.stride(0),  # output [topk]
-        bitmatrix, bitmatrix.stride(0), bitmatrix.stride(1),  # output [bitmatrix]
+        use_provided_indx, bitmatrix, bitmatrix.stride(0), bitmatrix.stride(1),  # output [bitmatrix]
         n_rows, n_cols,  # shapes
         S, BLOCK_S, s_blocks,  # thing to memset to zero
         BLOCK_M=BLOCK_M, BLOCK_N=BLOCK_N,  # tunable parameter
