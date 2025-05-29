b316d88261bc38d90db1c8fcf02120c015474a78
pawelszczerbuk <153013546+pawelszczerbuk@users.noreply.github.com>
2025-05-29 10:28:14 -0700

    [PIPELINER] Reintroduce epilogue peeling (#6962)
    Reintroduce "[PIPELINE] Peel single epilogue iteration after loop
    expansion #6893"
    Fixed the issue that was exposing IMA by using poison values as buffer
    indices used by speculatively executed async copy.

--- lib/Dialect/TritonGPU/Transforms/Pipeliner/SoftwarePipeliner.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeliner/SoftwarePipeliner.cpp
@@ -1,11 +1,13 @@
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
-#include "mlir/IR/IRMapping.h"
+#include "mlir/Dialect/UB/IR/UBOps.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Support/LLVM.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "triton/Analysis/AxisInfo.h"
 #include "triton/Analysis/Utility.h"
+#include "triton/Dialect/Triton/IR/Utility.h"
+#include "triton/Dialect/Triton/Transforms/LoopPeeling.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Passes.h"
 #include "triton/Dialect/TritonGPU/Transforms/PipelineExpander.h"
@@ -13,9 +15,7 @@
 #include "triton/Dialect/TritonGPU/Transforms/Schedule.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
-#include "triton/Tools/Sys/GetEnv.hpp"
 #include "llvm/Support/Debug.h"
-
 //===----------------------------------------------------------------------===//
 // This file will create a schedule that will be handed over to the pipeline
 // expander.
@@ -42,7 +42,115 @@ static void pipelineWgmma(ModuleOp moduleOp) {
   }
 }
 
+static Operation *wrapInMaskOp(RewriterBase &rewriter, Operation *op,
+                               Value pred) {
+  auto mask = rewriter.create<MaskOp>(op->getLoc(), op->getResultTypes(), pred);
+  rewriter.createBlock(&mask->getRegion(0));
+  rewriter.setInsertionPointToStart(&mask->getRegion(0).front());
+  auto newOp = rewriter.clone(*op);
+  rewriter.create<MaskReturnOp>(op->getLoc(), newOp->getResults());
+  op->replaceAllUsesWith(mask->getResults());
+  rewriter.eraseOp(op);
+  return mask;
+}
+
+static void resolveMaskOp(ModuleOp moduleOp, DenseSet<MaskOp> &peeledMaskOps) {
+  IRRewriter rewriter(moduleOp);
+
+  // Canonicalize the IR to simplify the arithmetic ops defining the mask
+  auto arithDialect =
+      moduleOp.getContext()->getLoadedDialect<arith::ArithDialect>();
+  RewritePatternSet patterns(moduleOp.getContext());
+  arithDialect->getCanonicalizationPatterns(patterns);
+  if (applyPatternsGreedily(moduleOp, std::move(patterns)).failed())
+    return llvm::report_fatal_error("Failed to canonicalize the IR");
+
+  // Prune all the statically dead mask ops in the epilogue. This is a
+  // hack, ideally we should do it for all the mask ops, but it is incorrect if
+  // we have speculatively executed async cp operations that will store to shmem
+  // even if the mask is false.
+  for (auto maskOp : peeledMaskOps) {
+    rewriter.setInsertionPoint(maskOp);
+    while (&maskOp.getBody()->front() != maskOp.getBody()->getTerminator()) {
+      Operation *op = &maskOp.getBody()->front();
+      if (isConstantIntValue(maskOp.getPred(), 0)) {
+        if (op->getNumResults() > 0) {
+          SmallVector<Value> results;
+          for (auto result : op->getResults()) {
+            auto poisonOp =
+                rewriter.create<ub::PoisonOp>(op->getLoc(), result.getType());
+            results.push_back(poisonOp);
+          }
+          op->replaceAllUsesWith(results);
+        }
+        op->erase();
+      }
+    }
+  }
+
+  SmallVector<MaskOp> maskOps;
+  moduleOp->walk([&](MaskOp maskOp) { maskOps.push_back(maskOp); });
+  for (auto maskOp : maskOps) {
+    rewriter.setInsertionPoint(maskOp);
+    while (&maskOp.getBody()->front() != maskOp.getBody()->getTerminator()) {
+      Operation *op = &maskOp.getBody()->front();
+      rewriter.moveOpBefore(op, maskOp);
+      op = triton::predicateOp(rewriter, op, maskOp.getPred());
+    }
+    maskOp->replaceAllUsesWith(
+        maskOp.getBody()->getTerminator()->getOperands());
+    maskOp->erase();
+  }
+}
+
+static bool hasMMAv5WaitsInLastStage(scf::ForOp forOp,
+                                     CoarseSchedule &schedule) {
+  int maxStage = schedule.getNumStages() - 1;
+  bool hasMMAv5 = false;
+  bool hasWaitInLastStage = false;
+  for (auto &op : forOp.getBody()->without_terminator()) {
+    if (isa<triton::nvidia_gpu::WaitBarrierOp>(op) &&
+        schedule[&op].first == maxStage) {
+      hasWaitInLastStage = true;
+    }
+    if (isa<triton::nvidia_gpu::MMAv5OpInterface>(op)) {
+      hasMMAv5 = true;
+    }
+  }
+  return hasMMAv5 && hasWaitInLastStage;
+}
+
 static void expandLoops(ModuleOp moduleOp) {
+  DenseSet<MaskOp> peeledMaskOps;
+  auto processPeeledEpilogueOp = [&](RewriterBase &rewriter, Operation *op,
+                                     bool isEpilogue) -> Operation * {
+    if (auto predOp = dyn_cast<triton::gpu::PredicateStageOp>(op)) {
+      if (isEpilogue) {
+        // Return false for the predicate of the peeled iteration
+        return rewriter.create<mlir::arith::ConstantIntOp>(
+            predOp.getLoc(), 0, predOp.getResult().getType());
+      } else {
+        if (predOp.getStage() == predOp.getMaxStage() - 1) {
+          return rewriter.create<mlir::arith::ConstantIntOp>(
+              predOp.getLoc(), 1, predOp.getResult().getType());
+        } else {
+          OpBuilder::InsertionGuard guard(rewriter);
+          rewriter.setInsertionPoint(op);
+          return triton::emitPredicateForStage(
+                     rewriter, predOp.getIv(), predOp.getUb(), predOp.getStep(),
+                     predOp.getMaxStage(), predOp.getStage())
+              .getDefiningOp();
+        }
+      }
+    }
+    if (auto maskOp = dyn_cast<triton::gpu::MaskOp>(op)) {
+      if (isEpilogue) {
+        peeledMaskOps.insert(maskOp);
+      }
+    }
+    return op;
+  };
+
   SmallVector<scf::ForOp> loops;
   moduleOp->walk([&](scf::ForOp forOp) { loops.push_back(forOp); });
   for (scf::ForOp forOp : loops) {
@@ -56,15 +164,26 @@ static void expandLoops(ModuleOp moduleOp) {
     triton::PipeliningOption options;
     options.supportDynamicLoops = true;
     options.peelEpilogue = false;
-    options.predicateFn = triton::predicateOp;
+    options.predicateFn = wrapInMaskOp;
     options.getScheduleFn =
         [&](scf::ForOp forOp,
             std::vector<std::pair<Operation *, unsigned>> &schedule) {
           schedule = finalSchedule;
         };
+
     // Testing feature: allow for unresolved predicate stage ops
     // in the loop body.
-    if (forOp->hasAttr("__test_keep_predicate_stage")) {
+    bool keepPredicateStage = forOp->hasAttr("__test_keep_predicate_stage");
+    // TODO: Enable epilogue peeling for warp specialized loops
+    // Heuristic: only peel epilogue for MMAv5 loops with waits in the last
+    // stage
+    bool customEpiloguePeeling =
+        hasMMAv5WaitsInLastStage(forOp, schedule) &&
+        !forOp->getParentOfType<triton::gpu::WarpSpecializeOp>() &&
+        !keepPredicateStage; // do not peel if we are testing the stage
+                             // predication
+
+    if (keepPredicateStage || customEpiloguePeeling) {
       options.emitPredicateStageFn =
           [](RewriterBase &rewriter, Value inductionVar, Value upperBound,
              Value step, uint64_t maxStage, uint64_t stage) {
@@ -76,7 +195,18 @@ static void expandLoops(ModuleOp moduleOp) {
     IRRewriter rewriter(forOp);
     FailureOr<scf::ForOp> newForOp =
         triton::pipelineForLoop(rewriter, forOp, options);
+
+    if (failed(newForOp)) {
+      continue;
+    }
+    forOp = *newForOp;
+    if (customEpiloguePeeling) {
+      mlir::triton::peelLoopEpilogue(forOp, processPeeledEpilogueOp);
+    }
   }
+  assert(moduleOp.getOps<triton::gpu::PredicateStageOp>().empty() &&
+         "PredicateStageOp should be resolved after the pipeline expansion");
+  resolveMaskOp(moduleOp, peeledMaskOps);
 }
 
 static void removeAttributes(ModuleOp moduleOp) {
