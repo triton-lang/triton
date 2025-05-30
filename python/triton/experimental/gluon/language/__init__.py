9f88c7f8ba9b7c56f2a1aff55163ecacf820df36
peterbell10 <peterbell10@openai.com>
2025-05-30 03:44:27 +0100

    [Gluon] Implement tensor memory (#6985)
    This implements:
    - `ttgl.nvidia.blackwell.allocate_tensor`
    - `ttgl.nvidia.blackwell.TensorMemoryLayout`
    - `tensor_memory_descriptor.load`
    - `tensor_memory_descriptor.store`
    - `tensor_memory_descriptor.subslice`

--- python/triton/experimental/gluon/language/__init__.py
+++ python/triton/experimental/gluon/language/__init__.py
@@ -3,4 +3,10 @@ from ._core import __all__ as __core_all
 from ._layouts import *  # NOQA: F403
 from ._layouts import __all__ as __layouts_all
 
-__all__ = [*__core_all, *__layouts_all]
+from . import nvidia
+
+__all__ = [
+    *__core_all,
+    *__layouts_all,
+    "nvidia",
+]
