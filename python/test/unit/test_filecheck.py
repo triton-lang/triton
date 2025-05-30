a1c688f9f87dd80e42cbd011dfd37ae6939f3d77
Thomas Raoux <thomas.raoux@openai.com>
2025-05-30 15:34:30 -0700

    move to checker test

--- python/test/unit/test_filecheck.py
+++ python/test/unit/test_filecheck.py
@@ -34,3 +34,23 @@ def test_filecheck_negative():
 
     with pytest.raises(ValueError, match="Couldn't match \"%c42_i32\""):
         run_filecheck_test(test_kernel)
+
+
+@triton.jit
+def accumulate(a, b):
+    return a + b
+
+
+# Check that we can call a function returning a value from a loop.
+def test_fall_in_loop():
+
+    @triton.jit
+    def kernel():
+        # CHECK-LABEL: kernel
+        acc = 0
+        # CHECK: scf.for
+        # CHECK:   call @accumulate
+        for i in range(10):
+            acc = accumulate(acc, i)
+
+    run_filecheck_test(kernel)
