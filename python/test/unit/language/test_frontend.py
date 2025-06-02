5b50c7f7deb8bb15ca31b8d159f23ede549c635b
Jeff Niu <jeffniu22@gmail.com>
2025-06-02 16:20:03 -0700

    [Frontend] Allow `@_aggregate` types to contain constexprs (#7024)
    Constexpr fields don't have an IR representation and are carried through
    the type. Also ensure that they are mangled into the type name.
    
    This PR achieves this by adding a `constexpt_type` that contains the
    constexpr value itself and makes this the type of `constexpr`. Shoutout
    to @peterbell10 for the suggestion

--- python/test/unit/language/test_frontend.py
+++ python/test/unit/language/test_frontend.py
@@ -141,3 +141,35 @@ def test_call_in_loop():
     # CHECK:   call @accumulate
     for i in range(10):
         acc = accumulate(acc, i)
+
+
+@tl.core._aggregate
+class AggregateWithConstexpr:
+    a: tl.tensor
+    b: tl.constexpr
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @staticmethod
+    def create(a):
+        return AggregateWithConstexpr(a, tl.constexpr(42))
+
+
+@triton.jit
+def add_rhs_constexpr(agg):
+    _ = agg.a + agg.b
+
+
+@filecheck_test
+@triton.jit
+def test_aggregate_with_constexpr():
+    # CHECK-LABEL: test_aggregate_with_constexpr
+    # CHECK: tt.call @"add_rhs_constexpr__test_frontend.AggregateWithConstexpr<i32S4S, constexpr[42]>
+    agg = AggregateWithConstexpr.create(tl.arange(0, 4))
+    add_rhs_constexpr(agg)
+
+    # CHECK: tt.func private @"add_rhs_constexpr__test_frontend.AggregateWithConstexpr<i32S4S, constexpr[42]>
+    # CHECK: %cst = arith.constant dense<42> : tensor<4xi32>
+    # CHECK: arith.addi %arg0, %cst : tensor<4xi32>
