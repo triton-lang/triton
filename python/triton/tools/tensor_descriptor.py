6b04d6523818d3358b108a953533bf4c188d2c2c
Peter Bell <peterbell10@openai.com>
2025-06-03 01:07:43 +0100

    [TensorDescriptor] Improve error from creating invalid descriptor
    This adds validation when creating a `TensorDescriptor` object in
    python, before it's been passed to the kernel. This not only improves
    the error message from the generic "invalid argument", it also means the
    stack trace will point to the code causing the error rather than the
    bowels of the kernel launch function.

--- python/triton/tools/tensor_descriptor.py
+++ python/triton/tools/tensor_descriptor.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List, Any
+from triton._utils import validate_block_shape, canonicalize_dtype, get_primitive_bitwidth
 
 
 @dataclass
@@ -13,6 +14,15 @@ class TensorDescriptor:
         rank = len(self.shape)
         assert len(self.strides) == rank, f"rank mismatch: {self}"
         assert len(self.block_shape) == rank, f"rank mismatch: {self}"
+        assert rank > 0, "rank must not be zero"
+        assert rank <= 5, "rank cannot be more than 5"
+        assert self.base.data_ptr() % 16 == 0, "base must be 16-byte aligned"
+        validate_block_shape(self.block_shape)
+        dtype_str = canonicalize_dtype(self.base.dtype)
+        elem_bytes = get_primitive_bitwidth(dtype_str) // 8
+        for stride in self.strides[:-1]:
+            assert (stride * elem_bytes) % 16 == 0, "strides must be 16-byte aligned"
+        assert self.strides[-1] == 1, "Last dimension must be contiguous"
 
     @staticmethod
     def from_tensor(tensor: Any, block_shape: List[int]):
