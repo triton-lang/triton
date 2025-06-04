bddeec98a512f4fdbacf49832446c1f993c66b6f
Peter Bell <peterbell10@openai.com>
2025-06-04 18:50:01 +0100

    [Frontend][NFC] Make semantic a language-specific class
    This changes the builtin calling convention to pass a `_semantic`
    value instead of `_builder`. So instead of:
    ```python
    def add(x, y, _builder=None):
        return semantic.add(x, y, _builder)
    ```
    we now have
    ```python
    def add(x, y, _semantic=None):
        return _semantic.add(x, y)
    ```
    The advantage being that `GluonSemantic` can inherit from `TritonSemantic`
    and share most of the implementation, but override some specifics e.g. broadcasting.

--- python/triton/experimental/gluon/language/nvidia/hopper/tma.py
+++ python/triton/experimental/gluon/language/nvidia/hopper/tma.py
@@ -1,25 +1,23 @@
-from triton.language.semantic import _convert_to_ir_values
 from triton.experimental.gluon.language._core import builtin, _unwrap_if_constexpr
-import triton.experimental.gluon.language._core as ttgl
 
 __all__ = ["async_copy_global_to_shared", "async_copy_shared_to_global", "store_wait"]
 
 
 @builtin
-def async_copy_global_to_shared(tensor_desc, coord, barrier, result, pred=True, _builder=None):
-    coord = _convert_to_ir_values(_builder, coord, require_i64=False)
-    pred = ttgl.to_tensor(pred, _builder=_builder)
-    _builder.create_async_tma_copy_global_to_local(tensor_desc.handle, coord, barrier.handle, result.handle,
-                                                   pred.handle)
+def async_copy_global_to_shared(tensor_desc, coord, barrier, result, pred=True, _semantic=None):
+    coord = _semantic._convert_to_ir_values(coord, require_i64=False)
+    pred = _semantic.to_tensor(pred)
+    _semantic.builder.create_async_tma_copy_global_to_local(tensor_desc.handle, coord, barrier.handle, result.handle,
+                                                            pred.handle)
 
 
 @builtin
-def async_copy_shared_to_global(tensor_desc, coord, src, _builder=None):
-    coord = _convert_to_ir_values(_builder, coord, require_i64=False)
-    _builder.create_async_tma_copy_local_to_global(tensor_desc.handle, coord, src.handle)
+def async_copy_shared_to_global(tensor_desc, coord, src, _semantic=None):
+    coord = _semantic._convert_to_ir_values(coord, require_i64=False)
+    _semantic.builder.create_async_tma_copy_local_to_global(tensor_desc.handle, coord, src.handle)
 
 
 @builtin
-def store_wait(pendings, _builder=None):
+def store_wait(pendings, _semantic=None):
     pendings = _unwrap_if_constexpr(pendings)
-    _builder.create_async_tma_store_wait(pendings)
+    _semantic.builder.create_async_tma_store_wait(pendings)
