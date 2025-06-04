5be9a979d14fd52629a4a712bc76dc650e06f804
Corbin Robeck <robeck@meta.com>
2025-06-04 10:28:32 -0700

    get test working

--- third_party/proton/test/test_override.py
+++ third_party/proton/test/test_override.py
@@ -1,60 +1,101 @@
+"""
+Vector Addition
+===============
+
+In this tutorial, you will write a simple vector addition using Triton.
+
+In doing so, you will learn about:
+
+* The basic programming model of Triton.
+
+* The `triton.jit` decorator, which is used to define Triton kernels.
+
+* The best practices for validating and benchmarking your custom ops against native reference implementations.
+
+"""
+
+# %%
+# Compute Kernel
+# --------------
+
 import torch
-import pathlib
-import pytest
-import json
-import os
 
 import triton
 import triton.language as tl
 import triton.profiler.language as pl
 import triton.profiler as proton
+import pathlib
+import os
+import sys
 
-from typing import NamedTuple
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def metadata_fn(grid: tuple, metadata: NamedTuple, args: dict):
-    BLOCK_SIZE = args["BLOCK_SIZE"]
-    return {"name": f"add_{BLOCK_SIZE}"}
-@triton.jit(launch_metadata=metadata_fn)
-def add_kernel(
-    x_ptr,
-    y_ptr,
-    output_ptr,
-    n_elements,
-    BLOCK_SIZE: tl.constexpr,
-):
-    pid = tl.program_id(axis=0)
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    mask = offsets < n_elements
-    x = tl.load(x_ptr + offsets, mask=mask)
-    y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
-    tl.store(output_ptr + offsets, output, mask=mask)
-torch.manual_seed(0)
-size = 256
-x = torch.rand(size, device='cuda')
-y = torch.rand(size, device='cuda')
-tmp_path = pathlib.Path(dir_path + '/tmp')
-temp_file = tmp_path / "test_tree.hatchet"
-output = torch.empty_like(x)
-n_elements = output.numel()
-grid = (1, 1, 1)
-proton.start(str(temp_file.with_suffix("")), backend="instrumentation", hook="hook")
-add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024, num_warps=1)
-proton.finalize()
 
-# with open(temp_file, "rb") as f:
-#     data = json.load(f)
-#     if hook:
-#         assert "add_1024" == data[0]["children"][0]["frame"]["name"]
-#     kernel_frame = data[0]["children"][0]["children"][0]
-#     load_ops = kernel_frame["children"][0]
-#     assert "load_ops" in load_ops["frame"]["name"]
-#     assert ("load_x" in load_ops["children"][0]["frame"]["name"]
-#             or "load_x" in load_ops["children"][1]["frame"]["name"])
-#     assert ("load_y" in load_ops["children"][0]["frame"]["name"]
-#             or "load_y" in load_ops["children"][1]["frame"]["name"])
-#     assert load_ops["children"][0]["metrics"]["cycles"] > 0
-#     assert load_ops["children"][1]["metrics"]["cycles"] > 0
+
+@triton.jit
+def add_kernel(x_ptr,  # *Pointer* to first input vector.
+               y_ptr,  # *Pointer* to second input vector.
+               output_ptr,  # *Pointer* to output vector.
+               n_elements,  # Size of the vector.
+               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+               # NOTE: `constexpr` so it can be used as a shape value.
+               ):
+    with pl.scope("kernel"):
+        pid = tl.program_id(axis=0)
+        block_start = pid * BLOCK_SIZE
+        offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        with pl.scope("load_ops"):
+            with pl.scope("load_x"):
+                x = tl.load(x_ptr + offsets, mask=mask)
+            with pl.scope("load_y"):
+                y = tl.load(y_ptr + offsets, mask=mask)
+        output = x + y
+        tl.store(output_ptr + offsets, output, mask=mask)
+
+
+# %%
+# Let's also declare a helper function to (1) allocate the `z` tensor
+# and (2) enqueue the above kernel with appropriate grid/block sizes:
+
+
+def add(x: torch.Tensor, y: torch.Tensor):
+    # We need to preallocate the output.
+    output = torch.empty_like(x)
+    assert x.device == DEVICE and y.device == DEVICE and output.device == DEVICE
+    n_elements = output.numel()
+    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
+    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
+    # In this case, we use a 1D grid where the size is the number of blocks:
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    # NOTE:
+    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+    #  - Don't forget to pass meta-parameters as keywords arguments.
+    tmp_path = pathlib.Path(os.getcwd() + '/tmp')
+    temp_file = tmp_path / "test_tree.hatchet"
+    if sys.argv[-1] == "on":
+        proton.start(str(temp_file.with_suffix("")), backend="instrumentation")
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024, num_warps=1)
+    if sys.argv[-1] == "on":
+        proton.finalize()
+    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
+    # running asynchronously at this point.
+    return output
+
+
+# %%
+# We can now use the above function to compute the element-wise sum of two `torch.tensor` objects and test its correctness:
+
+torch.manual_seed(0)
+size = 98432
+x = torch.rand(size, device=DEVICE)
+y = torch.rand(size, device=DEVICE)
+output_torch = x + y
+output_triton = add(x, y)
+# print(output_torch)
+# print(output_triton)
+# print(f'The maximum difference between torch and triton is '
+#       f'{torch.max(torch.abs(output_torch - output_triton))}')
--- third_party/proton/test/test_override_helper.py
+++ third_party/proton/test/test_override_helper.py
@@ -2,26 +2,43 @@ import os
 import subprocess
 import shutil
 import pathlib
+import json
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-tmp_dir = dir_path + '/tmp'
-os.environ["TRITON_ALWAYS_COMPILE"] = "1"
-os.environ["TRITON_KERNEL_DUMP"] = "1"
-os.environ["TRITON_DUMP_DIR"] = tmp_dir
-os.environ["TRITON_KERNEL_OVERRIDE"] = "1"
-os.environ["TRITON_OVERRIDE_DIR"] = tmp_dir
+tmp_dir = os.getcwd() + '/tmp'
 
 if os.path.exists(tmp_dir):
     shutil.rmtree(tmp_dir)
 os.makedirs(tmp_dir)
 
-
 # Run once to get the file dumps
-subprocess.run(["python", dir_path + "/test_override.py"])
+first_env = os.environ.copy()
+first_env["TRITON_ALWAYS_COMPILE"] = "1"
+first_env["TRITON_KERNEL_DUMP"]= "1"
+first_env["TRITON_DUMP_DIR"]=tmp_dir
+
+subprocess.run(["python", dir_path + "/01-vector-add.py", "off"], env=first_env)
+
 path = pathlib.Path(tmp_dir)
-asm_files = list(path.rglob(f"*.ttgir"))
-assert len(asm_files) == 1
+
+ttir_files = list(path.rglob(f"*.ttir"))
+ttgir_files = list(path.rglob(f"*.ttgir"))
+llir_files = list(path.rglob(f"*.llir"))
+ptx_files = list(path.rglob(f"*.ptx"))
+cubin_files = list(path.rglob(f"*.cubin"))
+
+assert len(ttir_files) == 1
+assert len(ttgir_files) == 1
+assert len(llir_files) == 1
+assert len(ptx_files) == 1
+assert len(cubin_files) == 1
+
+os.remove(ttir_files[0])
+os.remove(llir_files[0])
+os.remove(ptx_files[0])
+os.remove(cubin_files[0])
+
 print(str(list(path.rglob(f"*.ttgir"))[0]))
 
 filename = str(list(path.rglob(f"*.ttgir"))[0])
@@ -32,35 +49,47 @@ with open(filename, "r") as infile:
 # Add ttgir instrumentation
 with open(filename, "w") as outfile:
     for line in file_str:
-        # if "tt.get_program_id x" in line and "loc(#loc2)" in line:
-        #     line = '    proton.record start "kernel" loc(#loc1)\n' + line
-        # if "arith.cmpi slt" in line and "loc(#loc6)" in line:
-        #     line = '    proton.record start "load_ops loc(#loc1)"\n' + line
-        #     line = '    proton.record start "load_x loc(#loc1)"\n' + line
-        # if "tt.load" in line and "loc(#loc8)" in line:
-        #     line = '    proton.record end "load_x loc(#loc1)"\n' + line
-        #     line = '    proton.record start "load_y loc(#loc1)"\n' + line
-        # if "tt.load" in line and "loc(#loc10)" in line:
-        #     line = '    proton.record end "load_y loc(#loc1)"\n' + line
-        #     line = '    proton.record end "load_ops loc(#loc1)"\n' + line
-        # if "tt.return" in line and "loc(#loc14)" in line:
-        #     line = '    proton.record end "kernel" loc(#loc1)\n' + line
+        if "tt.get_program_id x" in line and "loc(#loc2)" in line:
+            #insert before the line
+            line = '    proton.record start "kernel" loc(#loc1)\n' + line
+        if "arith.cmpi slt" in line and "loc(#loc6)" in line:
+            #insert after the line
+            line = line + '    proton.record start "load_ops loc(#loc1)"\n'
+            line = line + '    proton.record start "load_x loc(#loc1)"\n'
+        if "tt.load" in line and "loc(#loc8)" in line:
+            #insert after the line
+            line = line + '    proton.record end "load_x loc(#loc1)"\n'
+            line = line + '    proton.record start "load_y loc(#loc1)"\n'
+        if "tt.load" in line and "loc(#loc10)" in line:
+            #insert after the line
+            line = line + '    proton.record end "load_y loc(#loc1)"\n'
+            line = line + '    proton.record end "load_ops loc(#loc1)"\n'
+        if "tt.return" in line and "loc(#loc14)" in line:
+            #insert before the line
+            line = '    proton.record end "kernel" loc(#loc1)\n' + line
         outfile.write(line)
 
 # Run again with kernel override
-subprocess.run(["python", dir_path + "/test_override.py"])
+second_env = os.environ.copy()
+second_env["TRITON_ALWAYS_COMPILE"]="1"
+second_env["TRITON_KERNEL_OVERRIDE"]="1"
+second_env["TRITON_OVERRIDE_DIR"]=tmp_dir
+subprocess.run(["python", dir_path + "/01-vector-add.py", "on"], env=second_env)
 
-# temp_file = tmp_dir / "test_tree.hatchet"
-# with open(temp_file, "rb") as f:
-#     data = json.load(f)
-#     if hook:
-#         assert "add_1024" == data[0]["children"][0]["frame"]["name"]
-#     kernel_frame = data[0]["children"][0]["children"][0]
-#     load_ops = kernel_frame["children"][0]
-#     assert "load_ops" in load_ops["frame"]["name"]
-#     assert ("load_x" in load_ops["children"][0]["frame"]["name"]
-#             or "load_x" in load_ops["children"][1]["frame"]["name"])
-#     assert ("load_y" in load_ops["children"][0]["frame"]["name"]
-#             or "load_y" in load_ops["children"][1]["frame"]["name"])
-#     assert load_ops["children"][0]["metrics"]["cycles"] > 0
-#     assert load_ops["children"][1]["metrics"]["cycles"] > 0
+tmp_path = pathlib.Path(dir_path + '/tmp')
+temp_file = tmp_path / "test_tree.hatchet"
+
+with open(temp_file, "rb") as f:
+    data = json.load(f)
+    kernel_frame = data[0]["children"][0]["children"][0]
+    load_ops = kernel_frame["children"][0]
+    assert "load_ops" in load_ops["frame"]["name"]
+    assert ("load_x" in load_ops["children"][0]["frame"]["name"]
+            or "load_x" in load_ops["children"][1]["frame"]["name"])
+    assert ("load_y" in load_ops["children"][0]["frame"]["name"]
+            or "load_y" in load_ops["children"][1]["frame"]["name"])
+    assert load_ops["children"][0]["metrics"]["cycles"] > 0
+    assert load_ops["children"][1]["metrics"]["cycles"] > 0
+
+# Clean up
+shutil.rmtree(tmp_dir)
