4f25d19ed5d46209d5764bb10d2281a2ecff19ae
Yuanwei Fang <fywkevin@gmail.com>
2025-06-06 14:03:05 -0700

    [PROTON-DEV] Remove redundant lowering code and suppress parser print (#7098)
    Move the `circular_store` AMD and NV common code into the `utility` and
    add an ENV for Proton to print debug info.

--- third_party/proton/dialect/lib/ProtonGPUToLLVM/ProtonAMDGPUToLLVM/AMDPatternProtonGPUOpToLLVM.cpp
+++ third_party/proton/dialect/lib/ProtonGPUToLLVM/ProtonAMDGPUToLLVM/AMDPatternProtonGPUOpToLLVM.cpp
@@ -27,39 +27,12 @@ struct CircularStoreOpConversion
                   OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     auto loc = op.getLoc();
-    auto mod = op.getOperation()->getParentOfType<ModuleOp>();
     auto b = TritonLLVMOpBuilder(loc, rewriter);
 
-    const int bytesPerEntry = proton::gpu::getBytesPerClockEntry();
-    const int wordsPerEntry = bytesPerEntry / 4; // 1 word = 4 bytes
-    auto segment = adaptor.getSegment();
-    auto segmentObj = LLVM::SegmentObject::fromStruct(loc, segment, rewriter);
-    Value indexPtr = segmentObj.indexPtr;
-    Value bufferBase = segmentObj.base;
-    Value segmentBase = segmentObj.segmentBase;
-    auto bufferBaseType = bufferBase.getType();
+    auto dataPack =
+        lowerCircularStoreOpHelper(op, adaptor.getSegment(), rewriter);
 
-    Value curIdx = b.load(i32_ty, indexPtr);
-    Value newIdx = b.add(curIdx, b.i32_val(wordsPerEntry));
-    b.store(newIdx, indexPtr);
-
-    int selectedWarpNum = mlir::triton::gpu::lookupNumWarps(mod);
-    auto segmentType = op.getSegment().getType();
-    auto selectedIds = segmentType.getSelectIds();
-    if (!selectedIds.empty())
-      selectedWarpNum = selectedIds.size();
-    const int bufferSizeInBytes = segmentType.getNBytes();
-    const int segmentWordSize = bufferSizeInBytes / selectedWarpNum / 4;
-    Value tagOffset =
-        b.add(segmentBase, b.urem(curIdx, b.i32_val(segmentWordSize)));
-    Value vecPtr = b.gep(bufferBaseType, i32_ty, bufferBase, tagOffset);
-    Value tag = op.getIsStart() ? b.i32_val(op.getScopeId())
-                                : b.i32_val(1 << 31 | op.getScopeId());
-    Value clock = op.getCounter();
-    Value valsVec = packLLVector(loc, {tag, clock}, rewriter);
-
-    uint32_t addrSpace =
-        cast<LLVM::LLVMPointerType>(bufferBaseType).getAddressSpace();
+    uint32_t addrSpace = dataPack.addrSpace;
     if (addrSpace == 1) {
       // TODO(crobeck): see what buffer ops performance looks like here for
       // stack mem (address space 1) compared to predicated ops to shared memory
@@ -67,9 +40,9 @@ struct CircularStoreOpConversion
     } else if (addrSpace == 3) {
       // Setting predicate always true has bank conflicts but it is
       // expected and stable.
-      targetInfo.getTritonTargetInfo().storeDShared(rewriter, loc, vecPtr,
-                                                    std::nullopt, valsVec,
-                                                    /*predicate=*/b.true_val());
+      targetInfo.getTritonTargetInfo().storeDShared(
+          rewriter, loc, dataPack.ptr, std::nullopt, dataPack.record,
+          /*predicate=*/b.true_val());
     } else {
       llvm::report_fatal_error("unsupported address space in circular store");
     }
