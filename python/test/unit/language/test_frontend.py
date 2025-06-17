114944318ba2164f0b80b26b57dee33d7a81867e
David Watson <davidw@graphcore.ai>
2025-06-17 15:56:46 +0000

    Check output value of loop carries

--- python/test/unit/language/test_frontend.py
+++ python/test/unit/language/test_frontend.py
@@ -110,11 +110,14 @@ def test_aggregate_modification_in_for_loop():
     value = TypeWithBuiltinInitializer()
     # CHECK: [[RANGE:%.*]] = tt.make_range {end = 4 : i32, start = 0 : i32}
     for i in range(0, 2):
-        # CHECK: for
+        # CHECK: [[RET:%.*]] = scf.for
         # CHECK-SAME: iter_args([[ITER:%.*]] = [[RANGE]])
         value.modify(tl.arange(4, 8))
         # CHECK: [[RANGE:%.*]] = tt.make_range {end = 8 : i32, start = 4 : i32}
         # CHECK: yield [[RANGE]]
+
+    anchor(value)
+    # CHECK: call @{{.*}}anchor{{.*}}([[RET]])
 
 
 @filecheck_test
@@ -126,13 +129,16 @@ def test_aggregate_modification_in_while_loop():
     i = 0
     # CHECK: [[C0:%.*]] = arith.constant 0 :
     while i < 1:
-        # CHECK: while ([[ITER:%.*]] = [[RANGE]], [[IV:%.*]] = [[C0]])
+        # CHECK: [[RET:%.*]]:2 = scf.while ([[ITER:%.*]] = [[RANGE]], [[IV:%.*]] = [[C0]])
         # CHECK: do
         i = 1
         # CHECK: [[C1:%.*]] = arith.constant 1 :
         value.modify(tl.arange(4, 8))
         # CHECK: [[RANGE:%.*]] = tt.make_range {end = 8 : i32, start = 4 : i32}
         # CHECK: yield [[RANGE]], [[C1]]
+
+    anchor(value)
+    # CHECK: call @{{.*}}anchor{{.*}}([[RET]]#0)
 
 
 @triton.jit
