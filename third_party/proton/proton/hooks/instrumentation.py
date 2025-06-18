85f5aa94fa2a317d2d5e50779d264f11f0103794
Yuanwei Fang <fywkevin@gmail.com>
2025-06-18 11:35:51 -0700

    lint

--- third_party/proton/proton/hooks/instrumentation.py
+++ third_party/proton/proton/hooks/instrumentation.py
@@ -162,7 +162,8 @@ class InstrumentationHook(Hook):
                                                           self.mode.sampling_options, self.mode.granularity,
                                                           self.mode.buffer_strategy, self.mode.buffer_type,
                                                           self.mode.buffer_size, max_shared_mem,
-                                                          self.profile_buffer_size, self.profile_buffer_alignment, is_clk64)
+                                                          self.profile_buffer_size, self.profile_buffer_alignment,
+                                                          is_clk64)
 
             if mode.Optimize.SCHED_STORES in self.mode.optimizations:
                 triton_proton.add_schedule_buffer_store(pm)
