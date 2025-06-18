685d3091b171720698efa55aca93cc24a83c99a8
Ziteng Yang <youngzt@fb.com>
2025-06-18 11:18:03 -0700

    successfully preserves the var name properly
    Summary: (cherry picked from commit 129bec7bbc45dbafc91a29bb3ef39df2708ab5a0)
    
    Test Plan:
    Write a lit test
    
    Reviewers:
    plotfi, hoy
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Differential Revision: https://phabricator.intern.facebook.com/D77154922

--- python/triton/compiler/code_generator.py
+++ python/triton/compiler/code_generator.py
@@ -609,7 +609,6 @@ class CodeGenerator(ast.NodeVisitor):
         # we temporarily store the name of defined globally, and delete it after finishing the
         self.defined_name = node.targets[0].id
         values = _sanitize_value(self.visit(node.value))
-        self.defined_name = None
 
         assert len(targets) == 1
         self.assignTarget(targets[0], values)
@@ -1420,6 +1419,12 @@ class CodeGenerator(ast.NodeVisitor):
                     self.builder.set_loc_def_name(self.defined_name)
                 last_loc = self.builder.get_loc()
 
+            defined_name = self.defined_name
+
+            # if the value of this syntax will be assigned to a name
+            if self.defined_name is not None:
+                self.builder.set_loc_def_name(self.defined_name)
+                self.defined_name = None    # sub-syntax didn't define this value
             try:
                 ret = super().visit(node)
             except CompilationError:
