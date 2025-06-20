56c785009141a47d1cf1c5226e89a25000d8eff3
Corbin Robeck <robeck@meta.com>
2025-06-20 12:48:04 -0400

    [PROTON-DEV] Add Sched Barrier Pass To Prevent Instruction Reordering Outside Proton Record Regions  (#7180)
    This PR adds a TTGIR level pass to insert sched barrier intrinsics to prevent the AMDGPU backend from reordering the Proton profiling region instructions to outside the record ops. The pass is added as a option to the mode optimizations and currently disabled by default.

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -113,6 +113,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::triton::proton::gpu::registerAllocateProtonSharedMemoryPass();
   mlir::triton::proton::gpu::registerAllocateProtonGlobalScratchBufferPass();
   mlir::triton::proton::gpu::registerScheduleBufferStorePass();
+  mlir::triton::proton::gpu::registerAddSchedBarriersPass();
 
   registry.insert<
       mlir::triton::TritonDialect, mlir::cf::ControlFlowDialect,
