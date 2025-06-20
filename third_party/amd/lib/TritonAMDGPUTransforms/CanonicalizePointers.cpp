f78939ec3d48b2aa9b23307da2b2cb1dffbcd327
Nicola Zaghen <nicola.zaghen@amd.com>
2025-06-20 09:03:04 +0000

    Move common code to a lambda

--- third_party/amd/lib/TritonAMDGPUTransforms/CanonicalizePointers.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/CanonicalizePointers.cpp
@@ -569,6 +569,12 @@ public:
         tt::filterDiscardableAttrs(addPtrOp.getOperation(), propagateList);
     auto currPtrTy = llvm::dyn_cast<RankedTensorType>(addPtrOp.getType());
     int currPtrRank = currPtrTy ? currPtrTy.getRank() : 1;
+    auto doSetDiscardableAttrs = [&](tt::AddPtrOp newAddPtrOp) {
+      auto newPtrTy = llvm::dyn_cast<RankedTensorType>(newAddPtrOp.getType());
+      int newPtrRank = newPtrTy ? newPtrTy.getRank() : 1;
+      if (newPtrRank == currPtrRank)
+        newAddPtrOp->setDiscardableAttrs(propagatedAttrs);
+    };
 
     // If it is a scalar pointer update, simply bump the base pointer
     if (llvm::isa<tt::PointerType>(addPtrOp.getPtr().getType())) {
@@ -576,10 +582,7 @@ public:
              "expected offset to be integer type");
       auto newAddPtrOp = rewriter.create<tt::AddPtrOp>(
           curLoc, fatPtrBase.getType(), fatPtrBase, origOffset);
-      auto newPtrTy = llvm::dyn_cast<RankedTensorType>(newAddPtrOp.getType());
-      int newPtrRank = newPtrTy ? newPtrTy.getRank() : 1;
-      if (newPtrRank == currPtrRank)
-        newAddPtrOp->setDiscardableAttrs(propagatedAttrs);
+      doSetDiscardableAttrs(newAddPtrOp);
 
       rewriter.replaceOpWithMultiple(addPtrOp, {{newAddPtrOp, fatPtrOffset}});
       fatPtrs[{newAddPtrOp, fatPtrOffset}] =
@@ -595,10 +598,7 @@ public:
             maybeGetOrCreateScalarConstant(rewriter, curLoc, origOffset)) {
       tt::AddPtrOp newAddPtrOp = rewriter.create<tt::AddPtrOp>(
           curLoc, fatPtrBase.getType(), fatPtrBase, *scalarConst);
-      auto newPtrTy = llvm::dyn_cast<RankedTensorType>(newAddPtrOp.getType());
-      int newPtrRank = newPtrTy ? newPtrTy.getRank() : 1;
-      if (newPtrRank == currPtrRank)
-        newAddPtrOp->setDiscardableAttrs(propagatedAttrs);
+      doSetDiscardableAttrs(newAddPtrOp);
 
       rewriter.replaceOpWithMultiple(addPtrOp, {{newAddPtrOp, fatPtrOffset}});
       // If we are updating the tensor pointer with a constant value, we can
@@ -615,10 +615,7 @@ public:
 
     auto newAddPtrOp = rewriter.create<tt::AddPtrOp>(
         curLoc, fatPtrBase.getType(), fatPtrBase, uniformOffset);
-    auto newPtrTy = llvm::dyn_cast<RankedTensorType>(newAddPtrOp.getType());
-    int newPtrRank = newPtrTy ? newPtrTy.getRank() : 1;
-    if (newPtrRank == currPtrRank)
-      newAddPtrOp->setDiscardableAttrs(propagatedAttrs);
+    doSetDiscardableAttrs(newAddPtrOp);
 
     // Vector offset update (if any): bump the tensor offset
     bool canNarrow = fatPtrs.at({fatPtrBase, fatPtrOffset}).canNarrow;
