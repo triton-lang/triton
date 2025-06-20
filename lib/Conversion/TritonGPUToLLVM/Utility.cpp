baaac9e208506dbfd447a91625f2062d4e1425a4
Adam P. Goucher <apgoucher@openai.com>
2025-06-20 12:02:38 +0000

    use bitwise-AND instead of AssumeOp to aid optimisation

--- lib/Conversion/TritonGPUToLLVM/Utility.cpp
+++ lib/Conversion/TritonGPUToLLVM/Utility.cpp
@@ -264,11 +264,10 @@ Value getThreadId(OpBuilder &rewriter, Location loc) {
     tid = rewriter.create<arith::SubIOp>(loc, tid, b.i32_val(*startId));
   }
 
-  // Insert the llvm.assume intrinsic: tid < upperBound (unsigned comparison)
-  Value upperBoundVal = b.i32_val(upperBound);
-  Value cmp = rewriter.create<arith::CmpIOp>(loc, arith::CmpIPredicate::ult,
-                                             tid, upperBoundVal);
-  rewriter.create<LLVM::AssumeOp>(loc, cmp);
+  if (llvm::isPowerOf2_32(upperBound)) {
+    // help LLVM's known bits analysis:
+    tid = b.and_(tid, b.i32_val(upperBound - 1));
+  }
 
   return tid;
 }
