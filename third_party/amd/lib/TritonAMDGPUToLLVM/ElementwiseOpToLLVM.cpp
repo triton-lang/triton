ddacd46ada9fd8561c01fbe178069c18229c92be
Kyle Wang <ec1wng@gmail.com>
2025-06-27 14:39:42 -0700

    [AMD] Clamp Results in Downcasting to FP8E4M3 and FP8E5M2 (#7337)
    There are several conversion ops on the NV side using `satfinite` mode,
    but on the AMD side, some of those are in non-saturation mode. We need
    to align AMD ops with NV.
    
    For example, fp32 to OCP fp8 on mi350 is lowered to
    `ROCDL::CvtScaleF32PkFp8F32Op`, and is eventually lowered to
    `v_cvt_scalef32_pk_fp8_f32`, which, according to ISA, is in
    non-saturation mode. But on the NV side, it's lowered to
    `cvt.rn.satfinite.e4m3x2.f32`, which is in saturation mode.
    
    Other examples including:
    
    | Conversion | ROCDL dialect | Instruction |
    | ----------------- | ----------------------------- |
    -------------------------- |
    | fp32 to fp8e4m3fn | ROCDL::CvtScaleF32PkFp8F32Op |
    v_cvt_scalef32_pk_fp8_f32 |
    | fp32 to fp8e5m2 | ROCDL::CvtScaleF32PkBf8F32Op |
    v_cvt_scalef32_pk_bf8_f32 |
    | fp16 to fp8e4m3fn | ROCDL::CvtScaleF32PkFp8F16Op |
    v_cvt_scalef32_pk_fp8_f16 |
    | fp16 to fp8e5m2 | ROCDL::CvtScaleF32PkBf8F16Op |
    v_cvt_scalef32_pk_bf8_f16 |
    | bf16 to fp8e4m3fn | ROCDL::CvtScaleF32PkFp8Bf16Op |
    v_cvt_scalef32_pk_fp8_bf16 |
    | bf16 to fp8e5m2 | ROCDL::CvtScaleF32PkBf8Bf16Op |
    v_cvt_scalef32_pk_bf8_bf16 |
    
    This PR fixed this issue by enabling the `FP16_OVFL` flag in the Mode
    register before these conversion instrs.
    
    ---------
    
    Co-authored-by: ravil-mobile <ravil.aviva.com@gmail.com>

--- third_party/amd/lib/TritonAMDGPUToLLVM/ElementwiseOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/ElementwiseOpToLLVM.cpp
@@ -65,6 +65,17 @@ static SmallVector<Value>
 cvtScalePkDowncastToFp8(Location loc, ConversionPatternRewriter &rewriter,
                         Value v0, Value v1) {
   auto b = TritonLLVMOpBuilder(loc, rewriter);
+
+  // This is the location of the fp16_ovfl flag in the Mode register. It's
+  // calculated following this formula:
+  //     (mode register ID = 1) | (Offset << 6) | ((Width - 1) << 11)
+  // In this case, Offset = 23 and Width = 1.
+  // When the bit is 0/1, the conversion from fp32/fp16/bf16 to fp8/bf8 is in
+  // non-saturation/saturation mode.
+  Value fp16OVFLModeRegLoc = b.i32_val(1473);
+  LLVM::createLLVMIntrinsicCallOp(rewriter, loc, "llvm.amdgcn.s.setreg", {},
+                                  {fp16OVFLModeRegLoc, b.i32_val(1)});
+
   Type v2I16Ty = vec_ty(i16_ty, 2);
   Value v2I16Vec = b.undef(v2I16Ty);
   Value scale = b.f32_val(1);
@@ -84,6 +95,7 @@ cvtScalePkDowncastToFp8(Location loc, ConversionPatternRewriter &rewriter,
     result = rewriter.create<ConvertOp>(loc, v2I16Ty, v2I16Vec, srcVec, scale,
                                         /*dstLoHiSel=*/false);
   }
+
   auto fp8x4VecTy = vec_ty(i8_ty, 4);
   auto fp8x4Vec = b.bitcast(result, fp8x4VecTy);
   SmallVector<Value> ret(2);
