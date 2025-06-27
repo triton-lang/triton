a41ee975e598198fe1e9c3d62af1a52345246293
Peter Bell <peterbell10@openai.com>
2025-06-27 19:10:25 +0100

    Revert "Include constexprs in cache keys, fixes #7322 (#7332)"
    This reverts commit 54a0b815450652e4bd2a9be73e187ed7871720ed.

--- python/test/unit/runtime/test_cache.py
+++ python/test/unit/runtime/test_cache.py
@@ -400,23 +400,6 @@ def test_no_cache_callable():
     assert not kernel.used_global_vals
 
 
-def test_constexpr_cache_invalidation_recreated(device):
-
-    def test_run(val):
-        VAL = tl.constexpr(val)
-
-        @triton.jit
-        def kernel(out):
-            tl.store(out, VAL)
-
-        out = torch.zeros(1, device=device)
-        kernel[(1, )](out)
-        return out.item()
-
-    assert test_run(123) == 123
-    assert test_run(1234) == 1234
-
-
 def test_jit_warmup_cache(device) -> None:
 
     @triton.jit
