b90b9dafba26a6cb2ef8bffc85dbb7ce19250f16
Peter Bell <peterbell10@openai.com>
2025-06-27 16:41:19 +0100

    [Build] create `compile_comands.json` symlink even if the build fails

--- setup.py
+++ setup.py
@@ -522,10 +522,9 @@ class CMakeBuild(build_ext):
         env = os.environ.copy()
         cmake_dir = get_cmake_dir()
         subprocess.check_call(["cmake", self.base_dir] + cmake_args, cwd=cmake_dir, env=env)
+        update_symlink(Path(self.base_dir) / "compile_commands.json", cmake_dir / "compile_commands.json")
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=cmake_dir)
         subprocess.check_call(["cmake", "--build", ".", "--target", "mlir-doc"], cwd=cmake_dir)
-
-        update_symlink(Path(self.base_dir) / "compile_commands.json", cmake_dir / "compile_commands.json")
 
 
 def download_and_copy_dependencies():
