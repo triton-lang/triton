e3098a753088cc21def712474c65dac683601c06
Phil Tillet <phil@openai.com>
2025-06-28 15:22:21 -0700

    .

--- python/triton_kernels/triton_kernels/matmul_ogs.py
+++ python/triton_kernels/triton_kernels/matmul_ogs.py
@@ -499,31 +499,33 @@ def matmul_ogs(x, w, bias,
     has_gather = gather_indx is not None
     x = flex.lhs_data.reinterpret(x)
     x_has_tma = ((not has_gather) or (has_gather and target_info.has_tma_gather())) and opt_flags.is_persistent
+    x_tensor = x
     if x_has_tma:
-        x = torch.squeeze(x) if has_gather else x
+        x_tensor = torch.squeeze(x) if has_gather else x
         block_m = [1] if has_gather else [opt_flags.block_m]
         block_z = [] if has_gather else [1] * (x.ndim - 2)
-        x = make_tma(x, block_z + block_m + [opt_flags.block_k])
+        x_tensor = make_tma(x_tensor, block_z + block_m + [opt_flags.block_k])
     # initialize w
-    w = flex.rhs_data.reinterpret(w)
+    w_tensor = flex.rhs_data.reinterpret(w)
     w_tma_transpose = False
     w_has_tma = opt_flags.is_persistent
     if w_has_tma:
         w_tma_transpose = w.stride(2) != 1
-        w = make_tma(w, [1, opt_flags.block_k, opt_flags.block_n], transpose=w_tma_transpose)
+        w_tensor = make_tma(w_tensor, [1, opt_flags.block_k, opt_flags.block_n], transpose=w_tma_transpose)
     # initialize w_scale
-    w_scale_has_tma = w_scale is not None
+    w_scale_has_tma = opt_flags.is_persistent and w_scale is not None
+    mx_tensor = mx_ctx.weight_scale
     if w_scale_has_tma:
-        w_scale = make_tma(w_scale, [opt_flags.block_n, opt_flags.block_k])
+        mx_tensor = make_tma(mx_tensor, [opt_flags.block_n, opt_flags.block_k])
     kernels = get_kernels(epilogue.specs, fused_activation.specs)
     (kernels._p_matmul_ogs if opt_flags.is_persistent else kernels._matmul_ogs)[(grid,)](
                    flex.out_data.reinterpret(memory["output"]),
                    flex.out_data.reinterpret(out0), *out0.stride(), *out0_flex,
-                   x, x, x.stride(0), x.stride(1), x.stride(2),
+                   x_tensor, x, x.stride(0), x.stride(1), x.stride(2),
                    flex.lhs_data.scale,
-                   w, w.stride(0), w.stride(1), w.stride(2), w_tma_transpose,
+                   w_tensor, w.stride(0), w.stride(1), w.stride(2), w_tma_transpose,
                    flex.rhs_data.scale,
-                   w_scale, mx_scale_stride_e, mx_scale_stride_k, mx_scale_stride_n,
+                   mx_tensor, mx_scale_stride_e, mx_scale_stride_k, mx_scale_stride_n,
                    bias, bias_stride,
                    x.shape[1] if routing_data.expt_hist is None else None,
                    N, K,
