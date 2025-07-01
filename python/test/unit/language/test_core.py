afb09f08289b17aba08e919fdef875d22bdfd12c
Ognjen Plavsic <plognjen@amd.com>
2025-07-01 12:22:58 +0000

    Fix typos

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -3686,7 +3686,7 @@ def convert_fp8_to_fp32(x, device, dtype_str):
 # M, N, K, num_warps, col_a, col_b, epilogue, input_precision, in_dtype, out_dtype, kpack, mma_nonk_size
 def get_test_dot_base_cases():
     return [(*shape, 4, False, False, epilogue, input_precision, in_dtype, out_dtype, 1, None)
-            for shape in [(64, 64, 64), (64, 64, 64), (32, 32, 32), (16, 16, 16)]
+            for shape in [(64, 64, 64), (32, 32, 32), (16, 16, 16)]
             for epilogue in ['none', 'trans', 'add-matrix', 'add-rows', 'add-cols', 'softmax', 'chain-dot']
             for input_precision in ['tf32', 'tf32x3', 'ieee']
             for in_dtype, out_dtype in [('float16', 'float16'), ('float16', 'float32'), ('float32', 'float32')]
