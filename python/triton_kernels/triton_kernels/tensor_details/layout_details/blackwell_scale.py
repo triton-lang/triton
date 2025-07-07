6170bfec9ae0fecf5fed7e7938963f7b53fa660d
Phil Tillet <phil@openai.com>
2025-07-07 09:30:26 -0700

    .

--- python/triton_kernels/triton_kernels/tensor_details/layout_details/blackwell_scale.py
+++ python/triton_kernels/triton_kernels/tensor_details/layout_details/blackwell_scale.py
@@ -1,30 +1,9 @@
-import torch
 import triton
 import triton.language as tl
 
 SWIZZLE_ALIGN_INNER = 8
 SWIZZLE_SIZE_INNER = 4
 SWIZZLE_SIZE_OUTER = 128
-
-
-def swizzle_mx_scale_bw(tensor: torch.Tensor, allow_pad=True):
-    """
-    Swizzle the input tensor of shape (A, B, ... N, K) to (A, B, ... N // 128, K // 4, 32, 4, 4).
-    Padding is applied if N and K are not multiples of 128 and 4 respectively.
-    Returns the swizzled tensor repacked as (A, B, ... N, K), with padding.
-    """
-    *leading_shape, N, K, = tensor.shape
-    pad_k = (SWIZZLE_ALIGN_INNER - (K % SWIZZLE_ALIGN_INNER)) % SWIZZLE_ALIGN_INNER
-    pad_n = (SWIZZLE_SIZE_OUTER - (N % SWIZZLE_SIZE_OUTER)) % SWIZZLE_SIZE_OUTER
-    if pad_k or pad_n > 0:
-        assert allow_pad, "Padding is required for swizzling, but it was explicitly disabled."
-        tensor = torch.nn.functional.pad(tensor, (0, pad_k, 0, pad_n))
-    padded_shape = tensor.shape
-    tensor = tensor.reshape(*leading_shape, padded_shape[-2] // SWIZZLE_SIZE_OUTER, SWIZZLE_SIZE_OUTER // 32, 32,
-                            padded_shape[-1] // SWIZZLE_SIZE_INNER, SWIZZLE_SIZE_INNER)
-    permute_order = list(range(len(tensor.shape)))
-    permute_order[-2], permute_order[-4] = permute_order[-4], permute_order[-2]
-    return tensor.permute(permute_order).reshape(*padded_shape)
 
 
 @triton.jit
@@ -38,17 +17,3 @@ def unswizzle_mx_scale_bw(x, SIZE_OUTER: tl.constexpr = SWIZZLE_SIZE_OUTER,
     x = x.reshape(shape_0, (shape_1 // SIZE_OUTER) // SIZE_INNER, 32, SIZE_OUTER // 32, SIZE_INNER)
     x = x.trans(0, 3, 2, 1, 4).reshape(shape_0 * SIZE_OUTER, shape_1 // SIZE_OUTER)
     return x
-
-
-def unswizzle_mx_scale_bw_torch(tensor: torch.Tensor):
-    """
-    Unswizzle the input tensor of shape (A, B, ... N // 128, K // 4, 32, 4, 4) packed as (A, B, ... N, K). (Testing only)
-    """
-    assert tensor.shape[-1] % SWIZZLE_SIZE_INNER == 0, f"{tensor.shape[-1]=} must be a multiple of {SWIZZLE_SIZE_INNER}"
-    assert tensor.shape[-2] % SWIZZLE_SIZE_OUTER == 0, f"{tensor.shape[-2]=} must be a multiple of {SWIZZLE_SIZE_OUTER}"
-    *leading_shape, N, K, = tensor.shape
-    tensor = tensor.reshape(*leading_shape, N // SWIZZLE_SIZE_OUTER, K // SWIZZLE_SIZE_INNER, 32,
-                            SWIZZLE_SIZE_OUTER // 32, SWIZZLE_SIZE_INNER)
-    permute_order = list(range(len(tensor.shape)))
-    permute_order[-2], permute_order[-4] = permute_order[-4], permute_order[-2]
-    return tensor.permute(permute_order).reshape(*leading_shape, N, K)
--- python/triton_kernels/triton_kernels/tensor_details/layout_details/hopper_scale.py
+++ python/triton_kernels/triton_kernels/tensor_details/layout_details/hopper_scale.py
@@ -1,35 +1,5 @@
-import torch
 import triton
 import triton.language as tl
-
-
-def swizzle_mxfp4_scale_hopper(x: torch.Tensor, num_warps: int, allow_pad: bool = True):
-    """
-    Make the 64x2 tile of scales of a 64x64 tile of mxfp4 values contiguous.
-    """
-    *batch, M, K = x.shape
-    SWIZZLE_ALIGN_M = 2 * num_warps * 2 * 8
-    SWIZZLE_ALIGN_K = 2
-    pad_m = (SWIZZLE_ALIGN_M - (M % SWIZZLE_ALIGN_M)) % SWIZZLE_ALIGN_M
-    pad_k = (SWIZZLE_ALIGN_K - (K % SWIZZLE_ALIGN_K)) % SWIZZLE_ALIGN_K
-    if pad_m or pad_k > 0:
-        assert allow_pad, "Padding is required for swizzling, but it was explicitly disabled."
-    x = torch.nn.functional.pad(x, (0, pad_k, 0, pad_m))
-    *batch, M, K = x.shape
-    assert x.is_contiguous()
-    assert num_warps & (num_warps - 1) == 0, "warps_n must be a power of 2"
-    assert M % (2 * num_warps * 2 *
-                8) == 0 and K % 2 == 0, f"Input tensor must have a subtile of shape (..., {2 * num_warps * 2 * 8}, 2)"
-    b = len(batch)
-    x = x.reshape(*batch, M // (2 * num_warps * 2 * 8), 2, num_warps, 2, 8, K // 2, 2)
-    perm = [0, 2, 5, 1, 4, 6, 3]
-    perm = list(range(b)) + [b + p for p in perm]
-    x = x.permute(*perm)
-    x = x.flatten(-5, -1)
-    x = x.flatten(-3, -2)
-    assert x.shape[-2] == M // 32
-    assert x.shape[-1] == K * 32
-    return x
 
 
 @triton.jit
@@ -45,19 +15,4 @@ def unswizzle_mxfp4_scale_hopper(x, num_warps: tl.constexpr):
     x = x.reshape(M // num_warps, num_warps, K // 64, 2, 8, 2, 2)
     x = x.trans(0, 3, 1, 6, 4, 2, 5)
     x = x.reshape(M * 32, K // 32)
-    return x
-
-
-def unswizzle_mxfp4_scale_hopper_torch(x: torch.Tensor, num_warps: int):
-    """
-    PyTorch inverse of unswizzle_mxfp4_scale_hopper
-    """
-    assert num_warps & (num_warps - 1) == 0, "num_warps must be a power of 2"
-    *batch, M, K = x.shape
-    b = len(batch)
-    x = x.reshape(*batch, M // num_warps, num_warps, K // 64, 2, 8, 2, 2)
-    perm = [0, 3, 1, 6, 4, 2, 5]
-    perm = list(range(b)) + [b + p for p in perm]
-    x = x.permute(*perm)
-    x = x.reshape(*batch, M * 32, K // 32)
     return x
