f05dcfab45e619f17521f2d48f7280d1b013179d
PMylon <pmylonas@amd.com>
2025-07-07 08:14:15 +0000

    Add sw emulation for Bf16_to_Fp8E4M3FNUZ conversion on CDNA4

--- python/test/unit/language/test_conversions.py
+++ python/test/unit/language/test_conversions.py
@@ -347,9 +347,6 @@ def test_typeconvert_downcast(src_dtype, dst_dtype, rounding, max_repr, device):
             if not is_hip_cdna4() and src_dtype == 'bfloat16':
                 pytest.skip("float8e4nv downcast tests from bfloat16 only supported on AMDGPU CDNA4")
 
-        if dst_dtype in ('float8e4b8') and rounding == 'rtne' and not is_hip_cdna3():
-            pytest.skip(f"{dst_dtype} downcast with RTNE rounding tests only supported on AMDGPU CDNA3")
-
     # dtype : (exponent_bits, mantissa_bits, exponent_bias)
     stuff = {
         'float16': (5, 10, 15),
