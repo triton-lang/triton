f450cfd8051cbe464b9dd41cbfdae82ca4bdceec
Tori <vwbaker@google.com>
2025-07-08 13:28:23 +0200

    Remove orthogonal optimizations

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -333,9 +333,6 @@ class constexpr(base_value):
         args = (_unwrap_if_constexpr(x) for x in _normalize_tuple(args))
         return self.value.__getitem__(*args)
 
-    def __hash__(self):
-        return hash(self.value)
-
 
 def constexpr_function(f):
     """
