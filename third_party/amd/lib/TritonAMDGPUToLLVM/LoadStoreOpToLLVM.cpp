de730e1e1eb8b68a46df789d0a6263de35ec430a
Corbin Robeck <robeck@meta.com>
2025-07-12 10:39:04 -0700

    optimize single ttgir level op that lowers to two llvm ops

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -1535,6 +1535,26 @@ struct AtomicRMWOpConversion
                                       b.i32_val(ii));
           valElement = vecVal;
         }
+        // If we have a single tl.atomic_rmw that is lowered into multiple
+        // llvm.atomic_rmw, and we set the ordering for each to aql_rel the llvm
+        // backend will insert extra buffer invalidates and L2 write backs
+        // causing a perforance degration. To avoid this we set the ordering to
+        // release for the first, acquire for the last, and relaxed for anything
+        // in between so that only a single set of buffer_inv and buffer_wbl2
+        // instructions are inserted by the backend for any "cluster" of atomic
+        // ops.
+        if (vec > 1 && op.getSem() == MemSemantic::ACQUIRE_RELEASE) {
+          if (i == 0) {
+            // First
+            emitter.setAtomicOrdering(LLVM::AtomicOrdering::release);
+          } else if (i == elemsPerThread - vec) {
+            // Last
+            emitter.setAtomicOrdering(LLVM::AtomicOrdering::acquire);
+          } else {
+            // Middle
+            emitter.setAtomicOrdering(LLVM::AtomicOrdering::monotonic);
+          }
+        }
 
         Value retVal =
             emitter.emitAtomicRMW(rewriter, ptrElements[i], valElement, rmwMask,
@@ -1555,6 +1575,7 @@ struct AtomicRMWOpConversion
           Value atomPtr = *atomicSharedMemBase;
           b.barrier();
           Value ret = b.load(valueElemTy, atomPtr);
+
           rewriter.replaceOp(op, {ret});
         }
       }
