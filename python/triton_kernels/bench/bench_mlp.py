fc8c05ce0c10908dc3c9e0a2def5c58e98c611a8
Kyle Wang <ec1wng@gmail.com>
2025-07-17 12:47:07 -0500

    resolve comments

--- python/triton_kernels/bench/bench_mlp.py
+++ python/triton_kernels/bench/bench_mlp.py
@@ -10,7 +10,7 @@ from triton_kernels.numerics_details.mxfp import downcast_to_mxfp
 from triton_kernels.matmul_ogs import matmul_ogs, PrecisionConfig, FlexCtx, FnSpecs, FusedActivation
 from triton_kernels.numerics import InFlexData
 from triton_kernels.routing import routing
-from triton_kernels.target_info import is_hip, get_cdna_version
+from triton_kernels.target_info import is_hip, get_cdna_version, is_cuda
 from triton_kernels.tensor import convert_layout
 from triton_kernels.tensor_details.layout import StridedLayout, BlackwellMXScaleLayout, HopperMXScaleLayout, HopperMXValueLayout
 from triton_kernels.tensor import wrap_torch_tensor, FP4
@@ -104,7 +104,7 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP,
     if w_dtype == "mx4":
         value_layout = StridedLayout
         scale_layout = StridedLayout
-        if not is_hip():
+        if is_cuda():
             if torch.cuda.get_device_capability()[0] == 9:
                 value_layout = HopperMXValueLayout
                 scale_layout = HopperMXScaleLayout
