a0a9565f6c0f886bd7b902b1262d3a2da98c0739
Jokeren <robinho364@gmail.com>
2025-07-17 15:12:54 -0400

    [PROTON-DEV] Address some comments

--- third_party/proton/Dialect/lib/Dialect/Proton/IR/Dialect.cpp
+++ third_party/proton/Dialect/lib/Dialect/Proton/IR/Dialect.cpp
@@ -1,15 +1,33 @@
 #include "Dialect/Proton/IR/Dialect.h"
-#include "Dialect/Proton/IR/Interfaces.h"
 
 #include "mlir/IR/DialectImplementation.h"
 #include "mlir/IR/OpImplementation.h"
 
 #include "Dialect/Proton/IR/Dialect.cpp.inc"
 
-void mlir::triton::proton::ProtonDialect::initialize() {
+namespace mlir::triton::proton {
+struct ProtonInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+
+  bool isLegalToInline(Operation *call, Operation *callable,
+                       bool wouldBeCloned) const final {
+    return true;
+  }
+  bool isLegalToInline(Region *dest, Region *src, bool wouldBeCloned,
+                       IRMapping &valueMapping) const final {
+    return true;
+  }
+  bool isLegalToInline(Operation *, Region *, bool wouldBeCloned,
+                       IRMapping &) const final {
+    return true;
+  }
+};
+
+void ProtonDialect::initialize() {
   addOperations<
 #define GET_OP_LIST
 #include "Dialect/Proton/IR/Ops.cpp.inc"
       >();
   addInterfaces<ProtonInlinerInterface>();
 }
+} // namespace mlir::triton::proton
