f179c546273eba9520d9c80e945e4e58af9ed806
Jokeren <robinho364@gmail.com>
2025-07-29 18:06:08 -0400

    Merge bench_mlp and bench_mlp_dist

--- python/triton_kernels/bench/bench_mlp.py
+++ python/triton_kernels/bench/bench_mlp.py
@@ -1,15 +1,18 @@
 from pathlib import Path
 from copy import deepcopy
+import matplotlib.pyplot as plt
 import triton.profiler as proton
 from triton.profiler import viewer
 import torch
+import argparse
 import triton_kernels
 import triton_kernels.swiglu
 from triton_kernels.matmul_ogs import matmul_ogs, PrecisionConfig, FlexCtx, FnSpecs, FusedActivation
-from triton_kernels.routing import routing
 from triton_kernels.target_info import is_hip, get_cdna_version
+from dataclasses import dataclass
+import distributed as triton_dist
 from triton_kernels.tensor_details import layout
-from bench_utils import PerfData, quantize_weight, roofline_mlp
+from bench_utils import quantize_weight
 
 if torch.cuda.is_available() and not is_hip():
     from triton._C.libtriton import nvidia
@@ -20,23 +23,85 @@ else:
     cublas = None
 
 
+@dataclass
+class PerfData:
+    time: float
+    flops: float
+    bytes: float
+    bitwidth: int
+    device_type: str
+    device_info: dict
+
+    @property
+    def tflops(self):
+        return self.flops / self.time * 1e-3
+
+    @property
+    def tbps(self):
+        return self.bytes / self.time * 1e-3
+
+    @property
+    def opint(self):
+        # operational intensity
+        assert self.bytes > 0
+        return self.flops / self.bytes
+
+    @property
+    def max_tbps(self):
+        return (proton.specs.max_bps(
+            self.device_type,
+            self.device_info["arch"],
+            self.device_info["bus_width"],
+            self.device_info["memory_clock_rate"],
+        ) * 1e-12)
+
+    @property
+    def max_tflops(self):
+        return (proton.specs.max_flops(
+            self.device_type,
+            self.device_info["arch"],
+            self.bitwidth,
+            self.device_info["num_sms"],
+            self.device_info["clock_rate"],
+        ) * 1e-12)
+
+    @property
+    def util(self) -> float:
+        assert self.bitwidth in (8, 16)
+        min_t_flop = self.flops / self.max_tflops * 1e-3
+        min_t_bw = self.bytes / self.max_tbps * 1e-3
+        return max(min_t_flop, min_t_bw) / self.time
+
+
+def get_bench_path(name, rank, x_dtype, w_dtype, TP, EP):
+    return Path(f"logs/{name}/{rank}/{x_dtype}-{w_dtype}-TP{TP}-EP{EP}/")
+
+
 def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, EP, name):
     assert n_expts_tot % EP == 0
     assert dim2 % TP == 0
-    dev = "cuda"
+    rank, world_size = triton_dist.setup()
+    dev = f"cuda:{rank}"
+    DP = world_size
+
+    assert n_expts_tot % EP == 0, f"{n_expts_tot=}, {EP=}, n_expts_tot must be divisible by EP"
+    assert dim2 % TP == 0, f"{dim2=}, {TP=}, dim2 must be divisible by TP"
 
     # input
     # weights
-    wg = torch.randn((dim1, n_expts_tot), device=dev)
+    wg = triton_dist.broadcast(torch.randn((dim1, n_expts_tot), device=dev))
     w1 = torch.randn((n_expts_tot // EP, dim1, dim2 // TP), device=dev)
     w2 = torch.randn((n_expts_tot // EP, dim2 // TP // 2, dim1), device=dev)
+
     # biases
-    bg = torch.randn((n_expts_tot, ), device=dev)
+    bg = triton_dist.broadcast(torch.randn((n_expts_tot, ), device=dev))
     b1 = torch.randn((n_expts_tot // EP, dim2 // TP), device=dev)
     b2 = torch.randn((n_expts_tot // EP, dim1), device=dev)
+    ep_indx = (rank // TP) % EP
+    groups = [list(range(ep * TP, (ep + 1) * TP)) for ep in range(EP)]
+    b2 = triton_dist.broadcast(b2, src=ep_indx * TP, groups=groups, group_idx=ep_indx)
 
     # -- numerics --
-    optg = dict()
     opt1 = dict()
     opt2 = dict()
     if w_dtype == "mx4" and not is_hip():
@@ -51,7 +116,7 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP,
             "scale_layout_opts": scale_layout_opts,
         }
         opt2 = deepcopy(opt1)
-    wg, wg_flex, wg_scale = quantize_weight(wg, "bf16", **optg)
+    wg, wg_flex, wg_scale = quantize_weight(wg, "bf16")
     w1, w1_flex, w1_scale = quantize_weight(w1, w_dtype, **opt1)
     w2, w2_flex, w2_scale = quantize_weight(w2, w_dtype, **opt2)
     pcg = PrecisionConfig(flex_ctx=FlexCtx(rhs_data=wg_flex), weight_scale=wg_scale)
@@ -60,26 +125,31 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP,
     pc2 = PrecisionConfig(flex_ctx=FlexCtx(rhs_data=w2_flex), weight_scale=w2_scale)
 
     # -- benchmark --
-    fpath = Path(f"logs/{name}/{x_dtype}-{w_dtype}-TP{TP}-EP{EP}/profiles/batch-{batch}.hatchet")
+    fpath = get_bench_path(name, rank, x_dtype, w_dtype, TP, EP) / f"profiles/batch-{batch}.hatchet"
     fpath.parent.mkdir(parents=True, exist_ok=True)
     x_dtype = {"fp16": torch.float16, "bf16": torch.bfloat16, "fp8": torch.float8_e4m3fn}[x_dtype]
     # special treatment of fp8_e4m3 on AMD CDNA3 because it uses fp8_e4m3fnuz
     if x_dtype == torch.float8_e4m3fn and get_cdna_version() == 3:
         x_dtype = torch.float8_e4m3fnuz
 
-    x = torch.randn((batch, dim1), device=dev)
-    xg = x.to(wg.dtype if n_expts_tot > 1 else x_dtype)
-    x = x.to(x_dtype)
+    input_x = torch.randn((batch // DP, dim1), device=dev)
     # run layer
     proton.start(str(fpath.with_suffix("")), hook="triton")
+    input_x = input_x.to(x_dtype)
+    xg = input_x.to(wg.dtype if n_expts_tot > 1 else input_x.dtype)
     for i in range(100):
-        if n_expts_tot > 1:
+        if n_expts_tot > 1:  # sparse
             logits = matmul_ogs(xg, wg, bg, precision_config=pcg)
-            rdata, gather_indx, scatter_indx = routing(logits, n_expts_act, simulated_ep=EP)
-        else:
-            rdata, gather_indx, scatter_indx = None, None, None
-        x = matmul_ogs(x, w1, b1, rdata, gather_indx=gather_indx, precision_config=pc1, fused_activation=act)
-        x = matmul_ogs(x, w2, b2, rdata, scatter_indx=scatter_indx, precision_config=pc2)
+            x, rdata, gather_indx, scatter_indx, metadata = triton_dist.routing(input_x, logits, n_expts_act, EP=EP,
+                                                                                TP=TP)
+        else:  # dense
+            x = triton_dist.all_gather(input_x, dim=0)
+            rdata, gather_indx, scatter_indx, metadata = None, None, None, None
+        if x.nelement() > 0:
+            x = matmul_ogs(x, w1, b1, rdata, gather_indx=gather_indx, precision_config=pc1, fused_activation=act)
+            x = matmul_ogs(x, w2, b2 if rank % TP == 0 else None, rdata, scatter_indx=scatter_indx,
+                           precision_config=pc2)
+        x = triton_dist.reduce_scatter(x, metadata=metadata, dim=0)
     proton.finalize()
 
     # -- analyze --
@@ -102,13 +172,92 @@ def bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP,
     )
 
 
+def roofline_mlp(batch_ranges, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP=1, EP=1, name="",
+                 verbose=True):
+    from itertools import chain
+    from bisect import bisect_left
+
+    batches = list(chain(*[range(*r) for r in batch_ranges]))
+    # collect performance data
+    perfs = []
+    bench_case = f"{name} ({x_dtype}x{w_dtype}, TP={TP}, EP={EP})"
+    print(f"Benchmarking {bench_case}...")
+    print("===============================================================")
+    for batch in batches:
+        perfs += [bench_mlp(batch, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_dtype, TP, EP, name)]
+        if verbose:
+            print(f"Batch: {batch}; Util: {perfs[-1].util}; TFLOPS: {perfs[-1].tflops}; TBPS: {perfs[-1].tbps}")
+    print("===============================================================")
+    # machine limits
+    max_tbps = perfs[0].max_tbps
+    max_tflops = perfs[0].max_tflops
+    fig, ax = plt.subplots(figsize=(7, 5), dpi=120)
+    ax.set_xlabel("batch size (toks/expt)")
+    ax.set_ylabel("performance  [TFLOP/s]")
+    ax.set_title(f"{bench_case} roofline")
+    # add a tiny margin so points are not flush with the frame
+    xs = [batch * n_expts_act / n_expts_tot for batch in batches]
+    perf = [p.tflops for p in perfs]
+    xmin, xmax = min(xs), max(xs)
+    dx = 0.05 * (xmax - xmin) if xmax > xmin else 1.0
+    ax.set_xlim(xmin - dx, xmax + dx)
+    ax.set_ylim(100, max_tflops + 500)
+    # plot roofline
+    opints = [p.opint for p in perfs]
+    knee = bisect_left(opints, max_tflops / max_tbps)
+    if knee > 0:  # has a bandwidth-bound knee
+        x_bw = [xs[0], xs[knee - 1]]
+        y_bw = [opints[0] * max_tbps, max_tflops]
+    else:  # no knee found, compute-bound only
+        x_bw = y_bw = []
+    x_comp = xs[knee:]
+    y_comp = [max_tflops] * len(x_comp)
+    ax.plot(x_bw, y_bw, "--", label=f"BW-bound  ({max_tbps:.1f} TB/s)", color="blue")
+    ax.plot(x_comp, y_comp, "--", label=f"Compute-bound  ({max_tflops:.0f} TFLOP/s)", color="orange")
+    # plot data
+    ax.scatter(xs, perf, marker="+")
+    ax.legend(frameon=False, loc="lower right")
+    ax.grid(True, which="both", ls=":", lw=0.5)
+    fig.tight_layout()
+    rank, _ = triton_dist.setup()
+    fpath = get_bench_path(name, rank, x_dtype, w_dtype, TP, EP) / "roofline.png"
+    plt.savefig(fpath)
+
+
 if __name__ == "__main__":
     has_native_mx4 = torch.cuda.get_device_capability(0)[0] >= 10 or get_cdna_version() == 4
     batch_ranges_dense = [(1024, 32768, 1024)]
     batch_ranges_moe = [(128, 512, 32), (512, 32000, 128)]
     dense_dtypes = ["fp8", "fp8"]
     quantized_dtypes = ["fp8", "mx4"] if has_native_mx4 else ["bf16", "mx4"]
-    roofline_mlp(batch_ranges_dense, 8192, 8192, 1, 1, *dense_dtypes, bench_mlp, TP=1, EP=1, name="dense")
-    roofline_mlp(batch_ranges_dense, 8192, 8192, 1, 1, *quantized_dtypes, bench_mlp, TP=1, EP=1, name="dense")
-    roofline_mlp(batch_ranges_moe, 5120, 8192, 128, 4, *dense_dtypes, bench_mlp, TP=1, EP=1, name="llama4-maverick")
-    roofline_mlp(batch_ranges_moe, 5120, 8192, 128, 4, *quantized_dtypes, bench_mlp, TP=1, EP=1, name="llama4-maverick")
+    rank, world_size = triton_dist.setup()
+    if world_size > 1:
+        # Running all workloads at once may cause OOM on some GPUs such as H100 80GB.
+        # Thus we request users to run each workload separately.
+        # For example, all eligible combinations of options are listed below when four GPUs are used:
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 2 --ep 2 --name llama4-maverick
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 1 --ep 4 --name llama4-maverick
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 4 --ep 1 --name llama4-maverick
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 4 --ep 1 --name dense
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 2 --ep 2 --name llama4-maverick --quantized
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 1 --ep 4 --name llama4-maverick --quantized
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 4 --ep 1 --name llama4-maverick --quantized
+        # torchrun --nproc-per-node=4 ./bench_mlp_distributed.py --tp 4 --ep 1 --name dense --quantized
+        argparse = argparse.ArgumentParser()
+        argparse.add_argument("--tp", type=int, default=1)
+        argparse.add_argument("--ep", type=int, default=1)
+        argparse.add_argument("--name", type=str, choices=["dense", "llama4-maverick"])
+        argparse.add_argument("--quantized", type=bool, action="store_true", default=False)
+        args = argparse.parse_args()
+        dtypes = dense_dtypes if args.quantized else quantized_dtypes
+        if args.name == "dense":
+            assert args.ep == 1, "EP must be 1 for dense"
+            roofline_mlp(batch_ranges_dense, 8192, 8192, 1, 1, *dtypes, TP=args.tp, EP=args.ep, name="dense")
+        else:
+            roofline_mlp(batch_ranges_moe, 5120, 8192, 128, 4, *dtypes, TP=args.tp, EP=args.ep, name="llama4-maverick")
+        triton_dist.cleanup()
+    else:
+        roofline_mlp(batch_ranges_dense, 8192, 8192, 1, 1, *dense_dtypes, TP=1, EP=1, name="dense")
+        roofline_mlp(batch_ranges_dense, 8192, 8192, 1, 1, *quantized_dtypes, TP=1, EP=1, name="dense")
+        roofline_mlp(batch_ranges_moe, 5120, 8192, 128, 4, *dense_dtypes, TP=1, EP=1, name="llama4-maverick")
+        roofline_mlp(batch_ranges_moe, 5120, 8192, 128, 4, *quantized_dtypes, TP=1, EP=1, name="llama4-maverick")
