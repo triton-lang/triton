14c6b110aa1a0f680730d89d4c323d410a6d5c90
Manman Ren <mren@meta.com>
2025-08-03 21:26:50 -0700

    support warp_specialize
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/PingPong.cpp
+++ third_party/nvidia/hopper/lib/Transforms/WarpSpecialization/PingPong.cpp
@@ -13,6 +13,7 @@
 #define LDBG(X) LLVM_DEBUG(DBGS() << X << "\n")
 
 namespace tt = mlir::triton;
+namespace ttg = mlir::triton::gpu;
 namespace ttng = ::mlir::triton::nvidia_gpu;
 namespace mlir {
 
@@ -62,9 +63,9 @@ static unsigned getLoopDepth(Operation *op) {
   return depth;
 }
 
-void getNestedFor(scf::IfOp ifOp,
+void getNestedFor(Region *partition,
                   DenseMap<unsigned, SmallVector<Operation *>> &loopDepthMap) {
-  ifOp->walk<WalkOrder::PreOrder>([&](Operation *subOp) {
+  partition->walk([&](Operation *subOp) {
     if (dyn_cast<scf::ForOp>(subOp)) {
       unsigned tDepth = getLoopDepth(subOp);
       loopDepthMap[tDepth].push_back(subOp);
@@ -116,158 +117,154 @@ bool categorizeIf(scf::IfOp ifOp, bool &hasDot, bool &hasExpCudaOp) {
   return hasFor;
 }
 
-void doPingPongSync(triton::FuncOp &funcOp, unsigned numWarpGroups) {
-  // Insert sync points in ForOp for consumer warp groups. Enable this pass
-  // when number of consumer warp groups == 2.
-  if (numWarpGroups < 3)
-    return;
-
-  SmallVector<scf::ForOp> loops;
-  // Identify ForOps for consumer warp groups. Here we assume taskId 0 is for
-  // producer. This pass handles the case of a single forOp for two consumer
-  // warp groups.
-  // Find top-most IfOps, and find the top level ForOp, assuming only one top
-  // level ForOp. A few use cases: 1> Persistent with ForOp containing both
-  // cuda and tensor 2> Persistent with ForOp containing tensor, then epilogue
-  // with cuda
-  DenseMap<unsigned, SmallVector<Operation *>> loopDepthMap1;
-  DenseMap<unsigned, SmallVector<Operation *>> loopDepthMap2;
-  for (auto &block : funcOp.getBody().getBlocks()) {
-    for (Operation &bodyOp : block.getOperations()) {
-      Operation *op = &bodyOp;
-      if (auto ifOp = dyn_cast<scf::IfOp>(op)) {
-        int wgId = getSingleTaskId(op);
-        // Assume taskId 0 is for producer. Assume we will visit taskId 1
-        // first.
-        if (wgId == 1) {
-          getNestedFor(ifOp, loopDepthMap1);
-        } else if (wgId == 2) {
-          getNestedFor(ifOp, loopDepthMap2);
-        }
-      }
+static void handleWarpSpec(ttg::WarpSpecializeOp wsOp) {
+  // ForOps in partition 0, map from loop depth to the ForOp.
+  DenseMap<unsigned, SmallVector<Operation *>> loopDepthPartition0;
+  // ForOps in partition 1, map from loop depth to the ForOp.
+  DenseMap<unsigned, SmallVector<Operation *>> loopDepthPartition1;
+  unsigned partitionId = 0;
+  for (Region *region : wsOp.getPartitionRegions()) {
+    LDBG("-- region " << region->getNumArguments());
+    // Assume default for producer.
+    if (partitionId == 0) {
+      getNestedFor(region, loopDepthPartition0);
+    } else if (partitionId == 1) {
+      getNestedFor(region, loopDepthPartition1);
     }
+    ++partitionId;
   }
-  // Verify loopDepthMap1 and loopDepthMap2: a single ForOp at depth of 0
-  // and a single ForOp at depth of 1.
-  LDBG("Found loops: " << loopDepthMap1.size());
-  if (loopDepthMap1.empty())
-    return;
-  if (loopDepthMap1[0].size() != 1)
-    return;
-  bool hasPersistent = loopDepthMap1.find(1) != loopDepthMap1.end();
 
-  // Assume two loops have the same ops. Check innermost loop.
+  // Verify a single ForOp at depth of 0, i.e a single outer ForOp.
+  LDBG("Found loops: " << loopDepthPartition0.size());
+  if (loopDepthPartition0.empty() || loopDepthPartition0[0].size() != 1)
+    return;
+  // Have a ForOp at depth of 1.
+  bool hasPersistent = loopDepthPartition0.find(1) != loopDepthPartition0.end();
+
+  // Find the region of gemms via [startOfGemm, endOfGemm)
   SmallVector<Operation *> starts, ends;
   for (unsigned iter = 0; iter < 2; ++iter) {
-    Operation *op = iter == 0 ? loopDepthMap1[hasPersistent ? 1 : 0][0]
-                              : loopDepthMap2[hasPersistent ? 1 : 0][0];
+    // Check partition 0 first, then partition 1.
+    // Find the innermost ForOp (i.e depth of 1 for persistent).
+    Operation *op = iter == 0 ? loopDepthPartition0[hasPersistent ? 1 : 0][0]
+                              : loopDepthPartition1[hasPersistent ? 1 : 0][0];
     auto forOp = dyn_cast<scf::ForOp>(op);
-    Operation *startOfGemm = nullptr;
-    Operation *endOfGemm = nullptr;
     // A simple heuristic for now:
     //   Mark the start of a gemm section when hitting a DotLike op.
     //   Mark the end of a gemm section once hitting an expensive non-dot
     //   computation op.
+    Operation *startOfGemm = nullptr;
+    Operation *endOfGemm = nullptr;
     for (auto &op : forOp.getBody()->without_terminator()) {
       if (startOfGemm && endOfGemm)
         break;
       bool hasDot, isCudaCore;
-      bool hasError = false;
       if (auto ifOp = dyn_cast<scf::IfOp>(op)) {
-        // if containing expensive cuda core op
-        hasError = categorizeIf(ifOp, hasDot, isCudaCore);
+        // Categorize IfOp to see if it contains dot or expensive cuda core op.
+        bool hasError = categorizeIf(ifOp, hasDot, isCudaCore);
+        // Bail out if IfOp contains both Dot and cuda core ops or contains
+        // ForOp.
+        if (hasError || (hasDot && isCudaCore))
+          break;
       } else {
-        LLVM_DEBUG({
-          LDBG("walk for");
-          op.dump();
-        });
         hasDot = isa<mlir::triton::DotOpInterface>(op);
-        // hasDot = isa<nvidia_gpu::WarpGroupDotOp>(&op);
         isCudaCore = isExpensiveComp(&op);
-        LDBG("walk for " << hasDot << " " << isCudaCore);
+        if (hasDot || isCudaCore) {
+          LLVM_DEBUG({
+            LDBG("walk op in for");
+            op.dump();
+          });
+          LDBG("-- hasDot " << hasDot << " cudaCoreOp " << isCudaCore);
+        }
       }
-      if (hasError || (hasDot && isCudaCore))
-        break;
-      if (hasDot && !isCudaCore && startOfGemm == nullptr) {
+      if (hasDot && startOfGemm == nullptr) {
+        // This is a dotop and we are trying to set startOfGemm.
         startOfGemm = &op;
+        LLVM_DEBUG({
+          LDBG("set start of Gemm region");
+          startOfGemm->dump();
+        });
         continue;
       }
-      if (!hasDot && isCudaCore && startOfGemm) {
+      if (isCudaCore && startOfGemm) {
+        // Already found startOfGemm, set endOfGemm prior to an expensive cuda
+        // core op.
         endOfGemm = &op;
+        LLVM_DEBUG({
+          LDBG("set end of Gemm region");
+          endOfGemm->dump();
+        });
         break;
       }
     }
-    if (startOfGemm) {
-      LLVM_DEBUG({
-        LDBG("found start of tensor core ops");
-        startOfGemm->dump();
-      });
-    }
-    if (endOfGemm) {
-      LLVM_DEBUG({
-        LDBG("found end of tensor core ops");
-        endOfGemm->dump();
-      });
-    }
-
     if (!startOfGemm || !endOfGemm)
       return;
     starts.push_back(startOfGemm);
     ends.push_back(endOfGemm);
   }
+
   // TODO: epilogue overlapping.
-  {
-    // "bar.arrive 9, 256" only when task Id is 2.
-    Operation *outerLoopTask2 = loopDepthMap2[0][0];
-    OpBuilder builder(outerLoopTask2);
-    builder.setInsertionPoint(outerLoopTask2);
-    auto forLoc = outerLoopTask2->getLoc();
-    Value pingBarrier =
-        builder.create<arith::ConstantIntOp>(forLoc, PING_BARRIER, 32);
-    Value numThreads = builder.create<arith::ConstantIntOp>(forLoc, 256, 32);
-    builder.create<ttng::NamedBarrierArriveOp>(forLoc, pingBarrier, numThreads);
-  }
+  // "bar.arrive PING, 256" prior to outer loop for partition 1.
+  Operation *outerLoopPartition1 = loopDepthPartition1[0][0];
+  OpBuilder builder(outerLoopPartition1);
+  builder.setInsertionPoint(outerLoopPartition1);
+  auto forLoc = outerLoopPartition1->getLoc();
+  Value pingBarrier =
+      builder.create<arith::ConstantIntOp>(forLoc, PING_BARRIER, 32);
+  // 256 threads for one partition of 4 warps.
+  Value numThreads = builder.create<arith::ConstantIntOp>(forLoc, 256, 32);
+  builder.create<ttng::NamedBarrierArriveOp>(forLoc, pingBarrier, numThreads);
+
   for (unsigned idx = 0; idx < 2; ++idx) {
-    Operation *op = idx == 0 ? loopDepthMap1[hasPersistent ? 1 : 0][0]
-                             : loopDepthMap2[hasPersistent ? 1 : 0][0];
+    // Find the innermost ForOp (i.e depth of 1 for persistent).
+    Operation *op = idx == 0 ? loopDepthPartition0[hasPersistent ? 1 : 0][0]
+                             : loopDepthPartition1[hasPersistent ? 1 : 0][0];
     auto forOp = dyn_cast<scf::ForOp>(op);
     OpBuilder builder(forOp);
     Operation *startOfGemm = starts[idx];
     Operation *endOfGemm = ends[idx];
 
-    // FIXME: hard-code using named barrier 9 and 10 in this pass.
-    // Prior to the forOp, add "bar.arrive 9, 256" only when task Id is 2.
-    // At startOfGemm, insert "bar.sync 8+taskId, 256"
-    // At endOfGemm, insert "bar.arrive 11-taskId, 256"
+    // At startOfGemm, insert "bar.sync PING, 256" for partition 0 or "bar.sync
+    // PONG" for partition 1 At endOfGemm, insert "bar.arrive PONG, 256" for
+    // partition 0 or "bar.arrive PING" for partition 1
     builder.setInsertionPoint(forOp);
     auto forLoc = forOp->getLoc();
-
-    // FIXME: hard-code total number of threads to be 256 when
-    // numConsumerGroups is 2.
+    // Hard-code number of threads to be 256 for each partition.
     Value numThreads = builder.create<arith::ConstantIntOp>(forLoc, 256, 32);
-    // for taskId of 1, generate: bar.sync pingBarrier; bar.arrive pongBarrier
-    // for taskId of 2, outside of the loop, generate bar.arrive pingBarrier
-    //   inside the loop, generate bar.sync pongBarrier; bar.arrive
-    //   pingBarrier
-    Value pingBarrier =
-        builder.create<arith::ConstantIntOp>(forLoc, PING_BARRIER, 32);
 
-    int wgId = getSingleTaskId(forOp);
-    // At startOfGemm, insert "bar.sync 9 or 10, 256"
     builder.setInsertionPoint(startOfGemm);
     auto loc = startOfGemm->getLoc();
     Value syncBarrier = builder.create<arith::ConstantIntOp>(
-        loc, wgId == 1 ? PING_BARRIER : PONG_BARRIER, 32);
+        loc, idx == 0 ? PING_BARRIER : PONG_BARRIER, 32);
     builder.create<ttng::NamedBarrierWaitOp>(loc, syncBarrier, numThreads);
 
-    // At endOfGemm, insert "bar.arrive 10 or 9, 256"
-    Operation *insertBefore = endOfGemm;
-    insertBefore = moveBackward(endOfGemm, forOp);
+    Operation *insertBefore = moveBackward(endOfGemm, forOp);
     builder.setInsertionPoint(insertBefore);
     auto loc2 = endOfGemm->getLoc();
     Value arriveBarrier = builder.create<arith::ConstantIntOp>(
-        loc2, wgId == 1 ? PONG_BARRIER : PING_BARRIER, 32);
+        loc2, idx == 0 ? PONG_BARRIER : PING_BARRIER, 32);
     builder.create<ttng::NamedBarrierArriveOp>(loc2, arriveBarrier, numThreads);
+  }
+}
+
+void doPingPongSync(triton::FuncOp &funcOp, unsigned numWarpGroups) {
+  // Insert sync points in ForOp for consumer warp groups. Enable this pass
+  // when number of consumer warp groups == 2.
+  if (numWarpGroups != 3)
+    return;
+
+  SmallVector<scf::ForOp> loops;
+  // Identify ForOps for consumer warp groups. Check partitions to find regions
+  // of gemms.
+  DenseMap<unsigned, SmallVector<Operation *>> loopDepthPartition0;
+  DenseMap<unsigned, SmallVector<Operation *>> loopDepthPartition1;
+  for (auto &block : funcOp.getBody().getBlocks()) {
+    for (Operation &bodyOp : block.getOperations()) {
+      Operation *op = &bodyOp;
+      if (auto wsOp = dyn_cast<ttg::WarpSpecializeOp>(op)) {
+        handleWarpSpec(wsOp);
+      }
+    }
   }
 }
 
