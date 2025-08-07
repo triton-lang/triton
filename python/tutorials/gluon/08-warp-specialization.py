55f08f75ff2a970ec406875f3640762a120e0648
Mogball <jeffniu22@gmail.com>
2025-08-07 13:35:46 -0700

    done warp spec

--- python/tutorials/gluon/08-warp-specialization.py
+++ python/tutorials/gluon/08-warp-specialization.py
@@ -24,11 +24,28 @@ import pytest
 import torch
 import triton
 import importlib
+from functools import partial
 from triton.experimental import gluon
 from triton.experimental.gluon import language as gl
 
+from triton.language.core import _aggregate as aggregate
 from triton.experimental.gluon.nvidia.hopper import TensorDescriptor
 from triton.experimental.gluon.language.nvidia.hopper import tma, mbarrier, fence_async_shared
+from triton.experimental.gluon.language.nvidia.blackwell import (
+    TensorMemoryLayout,
+    tensor_memory_descriptor,
+    allocate_tensor_memory,
+    get_tmem_32x32b_reg_layout,
+    tcgen05_mma,
+    tcgen05_commit,
+)
+
+if torch.cuda.is_available():
+    from triton._C.libtriton import nvidia
+    cublas_workspace = torch.empty(32 * 1024 * 1024, device="cuda", dtype=torch.uint8)
+    cublas = nvidia.cublas.CublasLt(cublas_workspace)
+else:
+    cublas = None
 
 # Re-use utilities from the previous tutorial.
 t4 = importlib.import_module("04-tma")
@@ -37,6 +54,11 @@ t4 = importlib.import_module("04-tma")
 def is_hopper_or_newer():
     target = triton.runtime.driver.active.get_current_target()
     return target.backend == "cuda" and torch.cuda.get_device_capability()[0] >= 9
+
+
+def is_blackwell():
+    target = triton.runtime.driver.active.get_current_target()
+    return target.backend == "cuda" and torch.cuda.get_device_capability()[0] == 10
 
 
 if __name__ == "__main__" and not is_hopper_or_newer():
@@ -310,6 +332,7 @@ if __name__ == "__main__":
     ms = triton.testing.do_bench(lambda: elementwise_add_warp_specialized(  #
         A, B, C, XBLOCK, YBLOCK, num_load_buffers, num_store_buffers, num_warps))
     print(f"elementwise_add_warp_specialized: {ms:.2f} ms")
+    print()
 
 # %%
 # Results on GB200:
@@ -335,6 +358,298 @@ if __name__ == "__main__":
 # that minimize the critical path and maximize hardware utilization. In other
 # words, warp specialization allows us to fuse multiple programs into
 # one kernel.
+
+# %%
+# Since we have unfinished business with Blackwell matmul from the last
+# tutorial, let's demonstrate a warp-specialized persistent matmul with tcgen05.
 #
-# In the next tutorial, we will explore implement an efficient persistent GEMM
-# and demonstrate more advanced warp-specialized kernels.
+# - Use the same block sizes BLOCK_{M,N,K} = (128, 256, 64)
+# - Aim for 4 buffers using techniques to reduce epilogue smem.
+# - Double-buffer the accumulator to fully overlap the epilogue.
+#
+# Because the epilogue is overlapped, we can subtile by a factor of 4 to allow
+# 4 buffers. However, for tiny K, it might still be better to steal B.
+
+
+# Helper class for passing arguments around partitions.
+@aggregate
+class PartitionArgs:
+    a_desc: tma.tensor_descriptor
+    b_desc: tma.tensor_descriptor
+    c_desc: tma.tensor_descriptor
+    a_bufs: gl.shared_memory_descriptor
+    b_bufs: gl.shared_memory_descriptor
+    load_empty_bars: gl.shared_memory_descriptor
+    load_ready_bars: gl.shared_memory_descriptor
+    acc_bufs: tensor_memory_descriptor
+    acc_empty_bars: gl.shared_memory_descriptor
+    acc_ready_bars: gl.shared_memory_descriptor
+    SUBTILE_FACTOR: gl.constexpr
+    num_warps: gl.constexpr
+
+    def __init__(self, a_desc, b_desc, c_desc, a_bufs, b_bufs, load_empty_bars, load_ready_bars, acc_bufs,
+                 acc_empty_bars, acc_ready_bars, SUBTILE_FACTOR, num_warps):
+        self.a_desc = a_desc
+        self.b_desc = b_desc
+        self.c_desc = c_desc
+        self.a_bufs = a_bufs
+        self.b_bufs = b_bufs
+        self.load_empty_bars = load_empty_bars
+        self.load_ready_bars = load_ready_bars
+        self.acc_bufs = acc_bufs
+        self.acc_empty_bars = acc_empty_bars
+        self.acc_ready_bars = acc_ready_bars
+        self.SUBTILE_FACTOR = SUBTILE_FACTOR
+        self.num_warps = num_warps
+
+
+# Counter abstraction for tracking barrier index and phase.
+@aggregate
+class Counter:
+    index: gl.tensor
+    phase: gl.tensor
+    num_barriers: gl.constexpr
+
+    def __init__(self, index, phase, num_barriers):
+        self.index = index
+        self.phase = phase
+        self.num_barriers = num_barriers
+
+    @gluon.jit
+    def create(phase, num_barriers: gl.constexpr):
+        return Counter(gl.to_tensor(0), gl.to_tensor(phase), num_barriers)
+
+    @gluon.must_use_result
+    @gluon.jit
+    def next(self):
+        incr = self.index + 1
+        rollover = incr == self.num_barriers
+        index = gl.where(rollover, 0, incr)
+        phase = gl.where(rollover, self.phase ^ 1, self.phase)
+        return Counter(index, phase, self.num_barriers)
+
+
+@gluon.jit
+def matmul_load_partition(p, SchedulerImpl: gl.constexpr):
+    BLOCK_M: gl.constexpr = p.c_desc.block_type.shape[0]
+    BLOCK_N: gl.constexpr = p.c_desc.block_type.shape[1]
+    BLOCK_K: gl.constexpr = p.a_desc.block_type.shape[1]
+    K = p.a_desc.shape[1]
+
+    empty_bars = p.load_empty_bars
+    ready_bars = p.load_ready_bars
+    state = Counter.create(1, empty_bars.shape[0])
+
+    # Just loop over all tiles and issue loads.
+    scheduler = SchedulerImpl.initialize(p.c_desc.shape[0], p.c_desc.shape[1], BLOCK_M, BLOCK_N)
+    for idx in range(scheduler.get_num_tiles()):
+        pid_m, pid_n = scheduler.get_tile(idx)
+        off_m = pid_m * BLOCK_M
+        off_n = pid_n * BLOCK_N
+        for k in range(0, K, BLOCK_K):
+            # Acquire buffers, issue loads, and complete them asynchronously.
+            bar = ready_bars.index(state.index)
+            mbarrier.wait(empty_bars.index(state.index), state.phase)
+            mbarrier.expect(bar, p.a_desc.block_type.nbytes + p.b_desc.block_type.nbytes)
+            tma.async_copy_global_to_shared(p.a_desc, [off_m, k], bar, p.a_bufs.index(state.index))
+            tma.async_copy_global_to_shared(p.b_desc, [k, off_n], bar, p.b_bufs.index(state.index))
+            state = state.next()
+
+
+@gluon.jit
+def matmul_mma_partition(p, SchedulerImpl: gl.constexpr):
+    BLOCK_M: gl.constexpr = p.c_desc.block_type.shape[0]
+    BLOCK_N: gl.constexpr = p.c_desc.block_type.shape[1]
+    BLOCK_K: gl.constexpr = p.a_desc.block_type.shape[1]
+    K = p.a_desc.shape[1]
+
+    load_empty_bars = p.load_empty_bars
+    load_ready_bars = p.load_ready_bars
+    load_state = Counter.create(0, load_empty_bars.shape[0])
+
+    acc_empty_bars = p.acc_empty_bars
+    acc_ready_bars = p.acc_ready_bars
+    acc_state = Counter.create(1, p.acc_empty_bars.shape[0])
+
+    scheduler = SchedulerImpl.initialize(p.c_desc.shape[0], p.c_desc.shape[1], BLOCK_M, BLOCK_N)
+    for _ in range(scheduler.get_num_tiles()):
+        # Acquire the accumulator for the entire inner loop.
+        mbarrier.wait(acc_empty_bars.index(acc_state.index), acc_state.phase)
+        acc_buf = p.acc_bufs.index(acc_state.index)
+        use_acc = False
+        for k in range(0, K, BLOCK_K):
+            # Acquire opreands, issue MMA, and complete asynchronously.
+            mbarrier.wait(load_ready_bars.index(load_state.index), load_state.phase)
+            tcgen05_mma(p.a_bufs.index(load_state.index), p.b_bufs.index(load_state.index), acc_buf, use_acc=use_acc)
+            tcgen05_commit(load_empty_bars.index(load_state.index))
+            load_state = load_state.next()
+            use_acc = True
+        # Complete the accumulator asynchronously.
+        tcgen05_commit(acc_ready_bars.index(acc_state.index))
+        acc_state = acc_state.next()
+
+
+# Helper for splitting a tensor along N. This only works for certain layouts.
+# For our kernel, this only works for BLOCK_M=128 and num_warps=4.
+@gluon.jit
+def _split_n(x, SUBTILE_FACTOR: gl.constexpr):
+    split_count: gl.constexpr = SUBTILE_FACTOR.bit_length() - 1  # log2
+    xs = (x, )
+    for _ in gl.static_range(split_count):
+        next_xs = ()
+        for j in gl.static_range(len(xs)):
+            x = xs[j]
+            # Reshape to (M, 2, N//2) then permute so that tensor elements
+            # remain contiguous along N.
+            next_xs += x.reshape(x.shape[0], 2, x.shape[1] // 2).permute(0, 2, 1).split()
+        xs = next_xs
+    return xs
+
+
+@gluon.jit
+def matmul_epilogue_partition(p, SchedulerImpl: gl.constexpr):
+    BLOCK_M: gl.constexpr = p.c_desc.block_type.shape[0]
+    BLOCK_N: gl.constexpr = p.c_desc.block_type.shape[1]
+    dtype: gl.constexpr = p.c_desc.dtype
+
+    acc_empty_bars = p.acc_empty_bars
+    acc_ready_bars = p.acc_ready_bars
+    acc_state = Counter.create(0, p.acc_empty_bars.shape[0])
+    acc_layout: gl.constexpr = get_tmem_32x32b_reg_layout(BLOCK_M, BLOCK_N, [BLOCK_M, BLOCK_N], p.num_warps)
+    SPLIT_N: gl.constexpr = BLOCK_N // p.SUBTILE_FACTOR
+    acc_smem = gl.allocate_shared_memory(dtype, [BLOCK_M, SPLIT_N], p.c_desc.layout)
+
+    scheduler = SchedulerImpl.initialize(p.c_desc.shape[0], p.c_desc.shape[1], BLOCK_M, BLOCK_N)
+    for idx in range(scheduler.get_num_tiles()):
+        pid_m, pid_n = scheduler.get_tile(idx)
+        off_m = pid_m * BLOCK_M
+        off_n = pid_n * BLOCK_N
+
+        # Wait for the accumulator. Since BLOCK_N=256, we need to interleave
+        # the TMEM loads with the SMEM stores to avoid spilling.
+        mbarrier.wait(acc_ready_bars.index(acc_state.index), acc_state.phase)
+        acc = p.acc_bufs.index(acc_state.index).load(acc_layout)
+        acc_state = acc_state.next()
+
+        accs = _split_n(acc, p.SUBTILE_FACTOR)
+        for i in gl.static_range(len(accs)):
+            acc = accs[i].to(dtype)
+            tma.store_wait(pendings=0)  # overlap with downcast
+            acc_smem.store(acc.to(dtype))
+            # Arrive after the first SMEM store and rely on ptxas to interleave.
+            if i == 0:
+                mbarrier.arrive(acc_empty_bars.index(acc_state.index), count=1)
+            fence_async_shared()
+            tma.async_copy_shared_to_global(p.c_desc, [off_m, off_n + SPLIT_N * i], acc_smem)
+        tma.store_wait(pendings=0)
+
+
+@gluon.jit
+def matmul_warp_specialized_kernel(a_desc, b_desc, c_desc, SchedulerImpl: gl.constexpr, num_buffers: gl.constexpr,
+                                   SUBTILE_FACTOR: gl.constexpr, num_warps: gl.constexpr):
+    BLOCK_M: gl.constexpr = c_desc.block_type.shape[0]
+    BLOCK_N: gl.constexpr = c_desc.block_type.shape[1]
+    dtype: gl.constexpr = a_desc.dtype
+
+    a_bufs = gl.allocate_shared_memory(dtype, [num_buffers] + a_desc.block_type.shape, a_desc.layout)
+    b_bufs = gl.allocate_shared_memory(dtype, [num_buffers] + b_desc.block_type.shape, b_desc.layout)
+    load_empty_bars = gl.allocate_shared_memory(gl.int64, [num_buffers, 1], mbarrier.MBarrierLayout())
+    load_ready_bars = gl.allocate_shared_memory(gl.int64, [num_buffers, 1], mbarrier.MBarrierLayout())
+    for i in gl.static_range(num_buffers):
+        mbarrier.init(load_empty_bars.index(i), count=1)
+        mbarrier.init(load_ready_bars.index(i), count=1)
+
+    tmem_layout: gl.constexpr = TensorMemoryLayout([BLOCK_M, BLOCK_N], unpacked=True)
+    acc_bufs = allocate_tensor_memory(gl.float32, [2, BLOCK_M, BLOCK_N], tmem_layout)
+    acc_empty_bars = gl.allocate_shared_memory(gl.int64, [2, 1], mbarrier.MBarrierLayout())
+    acc_ready_bars = gl.allocate_shared_memory(gl.int64, [2, 1], mbarrier.MBarrierLayout())
+    for i in gl.static_range(2):
+        mbarrier.init(acc_empty_bars.index(i), count=1)
+        mbarrier.init(acc_ready_bars.index(i), count=1)
+
+    p = PartitionArgs(a_desc, b_desc, c_desc, a_bufs, b_bufs, load_empty_bars, load_ready_bars, acc_bufs,
+                      acc_empty_bars, acc_ready_bars, SUBTILE_FACTOR, num_warps)
+    gl.warp_specialize(
+        default_args=(p, SchedulerImpl),
+        default_partition=matmul_epilogue_partition,
+        worker_args=(p, SchedulerImpl),
+        worker_partitions=[matmul_load_partition, matmul_mma_partition],
+        worker_num_warps=[1, 1],
+        worker_num_regs=[24, 24],
+    )
+
+
+def matmul_warp_specialized(A, B, C, BLOCK_M, BLOCK_N, BLOCK_K, num_buffers, SUBTILE_FACTOR, num_warps, SchedulerImpl):
+    M, N = C.shape
+
+    a_layout = gl.NVMMASharedLayout.get_default_for([BLOCK_M, BLOCK_K], gl.float16)
+    b_layout = gl.NVMMASharedLayout.get_default_for([BLOCK_K, BLOCK_N], gl.float16)
+    c_layout = gl.NVMMASharedLayout.get_default_for([BLOCK_M, BLOCK_N], gl.float16)
+
+    a_desc = TensorDescriptor(A, A.shape, A.stride(), [BLOCK_M, BLOCK_K], a_layout)
+    b_desc = TensorDescriptor(B, B.shape, B.stride(), [BLOCK_K, BLOCK_N], b_layout)
+    c_desc = TensorDescriptor(C, C.shape, C.stride(), [BLOCK_M, BLOCK_N], c_layout)
+
+    num_sms = torch.cuda.get_device_properties("cuda").multi_processor_count
+    num_pid = triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N)
+    grid = (min(num_sms, num_pid), )
+    matmul_warp_specialized_kernel[grid](a_desc, b_desc, c_desc, SchedulerImpl, num_buffers, SUBTILE_FACTOR,
+                                         num_warps=num_warps)
+
+
+t7 = importlib.import_module("07-persistence")
+
+
+@pytest.mark.parametrize("M, N, K", [(208, 416, 304), (2000, 1000, 2000)])
+@pytest.mark.parametrize("BLOCK_M, BLOCK_N, BLOCK_K", [(128, 256, 64)])
+@pytest.mark.parametrize("num_buffers", [2, 3, 4])
+@pytest.mark.parametrize("SUBTILE_FACTOR", [4])
+@pytest.mark.parametrize("num_warps", [4])
+@pytest.mark.parametrize("SchedulerImpl", t7.schedulers)
+@pytest.mark.skipif(not is_blackwell(), reason="Requires Blackwell")
+def test_matmul_warp_specialized(M, N, K, BLOCK_M, BLOCK_N, BLOCK_K, num_buffers, SUBTILE_FACTOR, num_warps,
+                                 SchedulerImpl):
+    torch.manual_seed(0)
+    A = torch.randn(M, K, device="cuda", dtype=torch.float16)
+    B = torch.randn(K, N, device="cuda", dtype=torch.float16)
+    C = torch.empty(M, N, device="cuda", dtype=torch.float16)
+    matmul_warp_specialized(A, B, C, BLOCK_M, BLOCK_N, BLOCK_K, num_buffers, SUBTILE_FACTOR, num_warps, SchedulerImpl)
+    torch.testing.assert_close(A @ B, C, rtol=1e-3, atol=1e-1)
+
+
+if __name__ == "__main__" and is_blackwell():
+    print("Benchmarking matmul_warp_specialized")
+    print("====================================")
+    args = {
+        "BLOCK_M": 128,
+        "BLOCK_N": 256,
+        "BLOCK_K": 64,
+        "num_buffers": 4,
+        "SUBTILE_FACTOR": 4,
+        "num_warps": 4,
+        "SchedulerImpl": t7.GroupedPersistentTileScheduler(8),
+    }
+
+    M, N = 8192, 8192
+    C = torch.empty(M, N, device="cuda", dtype=torch.float16)
+    print("    K  warp-specialized    cublas")
+    for K in [2**i for i in range(9, 15)]:
+        as_flops = partial(t7.get_flops, M=M, N=N, K=K)
+        A = torch.randn(M, K, device="cuda", dtype=torch.float16)
+        B = torch.randn(K, N, device="cuda", dtype=torch.float16)
+        BT = B.T.contiguous()
+        r0 = as_flops(triton.testing.do_bench_cudagraph(lambda: matmul_warp_specialized(A, B, C, **args)))
+        r1 = as_flops(triton.testing.do_bench(lambda: cublas.matmul(A, BT, C)))
+        print(f"{K:>5} {r0:>17.2f} {r1:>9.2f}")
+
+# %%
+#     K  warp-specialized    cublas
+#   512           1160.28   1130.67
+#  1024           1249.69   1148.52
+#  2048           1347.18   1261.59
+#  4096           1390.95   1299.38
+#  8192           1350.01   1401.10
+# 16384           1448.14   1508.76
+#
+# Much better! There is still lots of tuning we can do to improve performance.
+# On Blackwell, warp specialization is critical for achieving peak performance.
