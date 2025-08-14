098e9e6e56aa99c9d298071eee5405bf42a5d90b
Jokeren <robinho364@gmail.com>
2025-08-13 21:08:50 -0400

    Skip mypy check

--- python/triton/knobs.py
+++ python/triton/knobs.py
@@ -11,8 +11,7 @@ from dataclasses import dataclass
 from contextlib import contextmanager
 from typing import cast, Any, Callable, Generator, Generic, Optional, Protocol, Type, TypeVar, TypedDict, TYPE_CHECKING, Union
 
-# comment
-from triton._C.libtriton import getenv, getenv_bool
+from triton._C.libtriton import getenv, getenv_bool  # type: ignore[import]
 
 if TYPE_CHECKING:
     from .runtime.cache import CacheManager, RemoteCacheBackend
