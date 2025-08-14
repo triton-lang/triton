2606920d766b6b1a3ed9ddc9bbffb59754f182e0
Pengzhan Zhao <borontion@gmail.com>
2025-08-14 09:35:50 -0700

    fix

--- python/test/gluon/test_frontend.py
+++ python/test/gluon/test_frontend.py
@@ -1759,7 +1759,7 @@ module attributes {"ttg.num-ctas" = 1 : i32, "ttg.num-warps" = 4 : i32, ttg.targ
   ^bb1:  // no predecessors
     %0 = ub.poison : tensor<64x64xf32, #mma>
     tt.return %0 : tensor<64x64xf32, #mma>
-  } origin/main
+  }
 }
 """)
 
