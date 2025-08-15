f5673a2b1f776eb639808ba263a96d9db70e2a0f
Xinya Zhang <Xinya.Zhang@amd.com>
2025-08-15 00:53:14 +0000

    Fix lint

--- python/build_helpers.py
+++ python/build_helpers.py
@@ -7,9 +7,11 @@ from pathlib import Path
 def get_base_dir():
     return Path(__file__).parent.parent
 
+
 def _get_build_base():
     build_base = os.getenv("TRITON_BUILD_DIR", default=(get_base_dir() / "build"))
     return Path(build_base)
+
 
 def _get_dir_common(prefix):
     plat_name = sysconfig.get_platform()
@@ -19,14 +21,18 @@ def _get_dir_common(prefix):
     path.mkdir(parents=True, exist_ok=True)
     return path.as_posix()
 
+
 def get_cmake_dir():
     return _get_dir_common("cmake")
+
 
 def get_build_lib():
     return _get_dir_common("lib")
 
+
 def get_build_temp():
     return _get_dir_common("temp")
+
 
 def get_bdist_dir():
     return _get_dir_common("bdist")
