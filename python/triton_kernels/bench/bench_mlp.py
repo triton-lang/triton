6e8d7be3835593ed264a27dd3f7360c6140788e6
Keren Zhou <kerenzhou@openai.com>
2025-08-27 15:16:57 -0400

    [BENCH] Various fixes on `bench_mlp.py` (#7926)

--- python/triton_kernels/bench/bench_mlp.py
+++ python/triton_kernels/bench/bench_mlp.py
@@ -112,7 +112,7 @@ def roofline_mlp(batch_sizes, dim1, dim2, n_expts_tot, n_expts_act, x_dtype, w_d
 
 if __name__ == "__main__":
     has_native_mx4 = torch.cuda.get_device_capability(0)[0] >= 10 or get_cdna_version() == 4
-    batch_sizes_dense = [(128, 8192, 128)]
+    batch_sizes_dense = [*range(128, 8192, 128)]
     batch_ranges_moe = [(2**(2 + k), 2**(3 + k), min(2**k, 32)) for k in range(8)]
     batch_sizes_moe = list(chain(*[range(*r) for r in batch_ranges_moe]))
     dense_dtypes = ["fp8", "fp8"]
@@ -130,24 +130,31 @@ if __name__ == "__main__":
         # torchrun --nproc-per-node=4 ./bench_mlp.py --tp 1 --ep 4 --name gpt-oss-x2 --quantized
         # torchrun --nproc-per-node=4 ./bench_mlp.py --tp 4 --ep 1 --name gpt-oss-x2 --quantized
         # torchrun --nproc-per-node=4 ./bench_mlp.py --tp 4 --ep 1 --name dense --quantized
-        argparse = argparse.ArgumentParser()
-        argparse.add_argument("--tp", type=int, default=1)
-        argparse.add_argument("--ep", type=int, default=1)
-        argparse.add_argument("--name", type=str, choices=["dense", "gpt-oss"])
-        argparse.add_argument("--quantized", action="store_true", default=False)
-        args = argparse.parse_args()
-        dtypes = dense_dtypes if args.quantized else quantized_dtypes
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--tp", type=int, default=1)
+        parser.add_argument("--ep", type=int, default=1)
+        parser.add_argument("--name", type=str, choices=["dense", "gpt-oss-x2"])
+        parser.add_argument("--quantized", action="store_true", default=False)
+        args = parser.parse_args()
+        dtypes = quantized_dtypes if args.quantized else dense_dtypes
         if args.name == "dense":
             assert args.ep == 1, "EP must be 1 for dense"
-            roofline_mlp(batch_sizes_dense, 8192, 8192, 1, 1, *dtypes, TP=args.tp, EP=args.ep, name="dense")
+            roofline_mlp(batch_sizes_dense, 8192, 8192, 1, 1, dtypes[0], dtypes[1], TP=args.tp, EP=args.ep,
+                         name="dense")
         else:
-            roofline_mlp(batch_sizes_moe, 2880, 2880, 128, 4, *dtypes, TP=args.tp, EP=args.ep, name="gpt-oss")
+            roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, dtypes[0], dtypes[1], TP=args.tp, EP=args.ep,
+                         name="gpt-oss-x2")
         triton_dist.cleanup()
     else:
-        pass
-        # roofline_mlp(batch_sizes_dense, 8192, 8192, 1, 1, *quantized_dtypes, TP=1, EP=1, name="dense")
-        # roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, *dense_dtypes, TP=1, EP=1, name="gpt-oss-x2")
-        roofline_mlp(batch_sizes_moe, 2880, 2880, 128, 4, *quantized_dtypes, TP=1, EP=1, name="gpt-oss")
-        # roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, *quantized_dtypes, TP=2, EP=1, name="gpt-oss-x2")
-        # roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, *quantized_dtypes, TP=4, EP=1, name="gpt-oss-x2")
-        # roofline_mlp(batch_ranges_moe, 5760, 5760, 128, 4, *quantized_dtypes, TP=8, EP=1, name="gpt-oss-x2")
+        roofline_mlp(batch_sizes_dense, 8192, 8192, 1, 1, quantized_dtypes[0], quantized_dtypes[1], TP=1, EP=1,
+                     name="dense")
+        roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, dense_dtypes[0], dense_dtypes[1], TP=1, EP=1,
+                     name="gpt-oss-x2")
+        roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=1, EP=1,
+                     name="gpt-oss-x2")
+        roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=2, EP=1,
+                     name="gpt-oss-x2")
+        roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=4, EP=1,
+                     name="gpt-oss-x2")
+        roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=8, EP=1,
+                     name="gpt-oss-x2")
