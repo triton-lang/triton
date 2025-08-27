c0319e631c46a39da4518bb6de1b5802fced05d8
Kyle Wang <ec1wng@gmail.com>
2025-08-27 13:45:34 -0500

    fix lit tests

--- lib/Dialect/TritonGPU/Transforms/RemoveLayoutConversions.cpp
+++ lib/Dialect/TritonGPU/Transforms/RemoveLayoutConversions.cpp
@@ -1310,7 +1310,8 @@ void LayoutRematerialization::hoistConvertDotOperand(
   // threads We do views and elementwise pure ops for now
   auto noDataMovement = [](Operation *op) {
     return (op->hasTrait<OpTrait::Elementwise>() && isMemoryEffectFree(op)) ||
-           isa<BroadcastOp, ConvertLayoutOp, UpcastFpOpInterface>(op) ||
+           isa<BroadcastOp, Fp4ToFpOp, ConvertLayoutOp, UpcastFpOpInterface>(
+               op) ||
            isView(op);
   };
   // Stop the slice as soon as we find an operation that cannot be done without
