f4667215370adc9e41c77b23b24a020ebafaf82b
Pengzhan Zhao <borontion@gmail.com>
2025-09-08 18:00:24 -0700

    support wmma

---
+++ python/triton/experimental/gluon/language/amd/rdna3/__init__.py
@@ -0,0 +1,23 @@
+from triton import knobs
+from triton.experimental.gluon.language import _core as ttgl
+from triton.experimental.gluon.language._semantic import _check
+
+from .._layouts import AMDWMMALayout
+from ..._core import builtin
+
+__all__ = ["wmma"]
+
+
+@builtin
+def wmma(a, b, acc, _semantic=None):
+    """
+    """
+    _check(acc is not None, lambda: "acc is required")
+    layout = acc.type.layout
+    _check(
+        isinstance(layout, AMDWMMALayout) and layout.version == 1,
+        lambda: "Expected layout to be an instance of AMDWMMALayout with version 1")
+
+    handle = _semantic.dot(a, b, acc, input_precision=knobs.language.fp32_default, max_num_imprecise_acc=None,
+                           out_dtype=acc.dtype).handle
+    return ttgl.tensor(handle, acc.type)
