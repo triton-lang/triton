0e14f8e9dae946d6eb26c2d86c2aab68c2ec1884
Pengzhan Zhao <borontion@gmail.com>
2025-09-08 18:10:54 -0700

    add operand layout check

--- python/triton/experimental/gluon/language/amd/rdna3/__init__.py
+++ python/triton/experimental/gluon/language/amd/rdna3/__init__.py
@@ -3,6 +3,7 @@ from triton.experimental.gluon.language import _core as ttgl
 from triton.experimental.gluon.language._semantic import _check
 
 from .._layouts import AMDWMMALayout
+from ..._layouts import DotOperandLayout
 from ..._core import builtin
 
 __all__ = ["wmma"]
@@ -23,6 +24,12 @@ def wmma(a, b, acc, _semantic=None):
     _check(
         isinstance(layout, AMDWMMALayout) and layout.version == 1,
         lambda: "Expected layout to be an instance of AMDWMMALayout with version 1")
+    _check(
+        isinstance(a.type.layout, DotOperandLayout) and a.type.layout.parent == layout,
+        lambda: "Expected a's layout to be a DotOperandLayout with parent matching AMDWMMALayout")
+    _check(
+        isinstance(b.type.layout, DotOperandLayout) and b.type.layout.parent == layout,
+        lambda: "Expected b's layout to be a DotOperandLayout with parent matching AMDWMMALayout")
 
     handle = _semantic.dot(a, b, acc, input_precision=knobs.language.fp32_default, max_num_imprecise_acc=None,
                            out_dtype=acc.dtype).handle
