625c8cb4545c78c3ee753e5bb68198551c72b24d
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2025-09-10 10:13:11 -0700

    [BACKEND] Retain mlir reproducer temporaries from prior run pass pipelines (#8113)
    Currently MLIR reproducers for each pass pipeline run overrides the
    previous `TRITON_REPRODUCER_PATH` path. This change allows for including
    a reproducer suffix when calling pm.run() to allow for retaining all
    previously run pipeline reproducers prior to the most recently run pass
    pipeline.
    
    This is important to add because with multiple pipelines, it is
    necessary to retain all previous pipelines reproducers to reproduce the
    full compilation sequence.
    
    - [X] I am not making a trivial change, such as fixing a typo in a
    comment.
    
    - [X] I have written a PR description following these
      [rules](https://cbea.ms/git-commit/#why-not-how).
    
    - [X] I have run `pre-commit run --from-ref origin/main --to-ref HEAD`.
    
    - Select one of the following.
      - [X] I have added tests.
        - `/python/test` for end-to-end tests
    
    - Select one of the following.
      - [X] I have not added any `lit` tests.

--- third_party/nvidia/backend/compiler.py
+++ third_party/nvidia/backend/compiler.py
@@ -239,7 +239,7 @@ class CUDABackend(BaseBackend):
         passes.common.add_cse(pm)
         passes.common.add_symbol_dce(pm)
         passes.ttir.add_loop_unroll(pm)
-        pm.run(mod)
+        pm.run(mod, 'make_ttir')
         return mod
 
     @staticmethod
@@ -316,7 +316,7 @@ class CUDABackend(BaseBackend):
         passes.common.add_cse(pm)
         passes.common.add_canonicalizer(pm)
 
-        pm.run(mod)
+        pm.run(mod, 'make_ttgir')
         metadata["cluster_dims"] = (cluster_info.clusterDimX, cluster_info.clusterDimY, cluster_info.clusterDimZ)
         tensordesc_meta = mod.get_tensordesc_metadata()
         metadata["tensordesc_meta"] = tensordesc_meta
@@ -334,7 +334,7 @@ class CUDABackend(BaseBackend):
         passes.gluon.add_canonicalizer(pm)
         passes.ttgpuir.add_combine_tensor_select_and_if(pm)
 
-        pm.run(mod)
+        pm.run(mod, 'gluon_to_ttgir')
         metadata["tensordesc_meta"] = mod.get_tensordesc_metadata()
         return mod
 
@@ -374,7 +374,7 @@ class CUDABackend(BaseBackend):
         if CUDABackend.instrumentation:
             CUDABackend.instrumentation.patch("llvmir_to_llvm", pm, mod.context)
 
-        pm.run(mod)
+        pm.run(mod, 'make_llir')
         # LLVM-IR (MLIR) -> LLVM-IR (LLVM)
         llvm.init_targets()
         context = llvm.context()
