a0a4b2a633b93e288da2f9d74893ca37cbba6bd0
Thomas Raoux <thomas.raoux@openai.com>
2025-09-10 13:03:36 -0700

    dummy PR

--- python/triton_kernels/bench/bench_mlp.py
+++ python/triton_kernels/bench/bench_mlp.py
@@ -156,5 +156,5 @@ if __name__ == "__main__":
                      name="gpt-oss-x2")
         roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=4, EP=1,
                      name="gpt-oss-x2")
-        roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=8, EP=1,
-                     name="gpt-oss-x2")
+    # roofline_mlp(batch_sizes_moe, 5760, 5760, 128, 4, quantized_dtypes[0], quantized_dtypes[1], TP=8, EP=1,
+    #              name="gpt-oss-x2")
