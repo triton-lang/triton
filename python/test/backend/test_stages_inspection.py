a584fdfa28840724a909e00d4d806fcb3e7a0f19
Puyan Lotfi <puyan@puyan.org>
2025-09-13 19:48:55 -0700

    more test cleanup

--- python/test/backend/test_stages_inspection.py
+++ python/test/backend/test_stages_inspection.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import pathlib
 
-def test_override(tmp_path: pathlib.Path):
+def test_inspection(tmp_path: pathlib.Path):
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     test_env = os.environ.copy()
@@ -19,7 +19,7 @@ def test_override(tmp_path: pathlib.Path):
     assert file_str == ["# This is generated from Triton add_stages_inspection_hook"]
 
 
-def test_override_integrity(tmp_path: pathlib.Path):
+def test_inspection_integrity(tmp_path: pathlib.Path):
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
     # Run once to get the clean/golden repro dump
@@ -30,14 +30,14 @@ def test_override_integrity(tmp_path: pathlib.Path):
     minimal_kernel = "/inspection_helper.py"
     subprocess.run(["python3", dir_path + minimal_kernel], env=golden_env)
 
-    curr_repro_path = tmp_path / ("../test_override_integrity0." + "make_ttir" + ".repro.mlir")
+    curr_repro_path = tmp_path / ("../test_inspection_integrity0." + "make_ttir" + ".repro.mlir")
     golden_ttir_repro = curr_repro_path.read_text()
     curr_repro_path.unlink()
-    curr_repro_path = tmp_path / ("../test_override_integrity0." + "make_ttgir" + ".repro.mlir")
+    curr_repro_path = tmp_path / ("../test_inspection_integrity0." + "make_ttgir" + ".repro.mlir")
     golden_ttgir_repro = curr_repro_path.read_text()
     curr_repro_path.unlink()
 
-    # # Run again with pipeline override
+    # # Run again with pipeline inspection
     test_env = os.environ.copy()
     test_env["TRITON_ALWAYS_COMPILE"] = "1"
     test_env["TRITON_MOCK_OVERRIDE_STAGES"] = "1"
@@ -47,9 +47,9 @@ def test_override_integrity(tmp_path: pathlib.Path):
 
     subprocess.run(["python3", dir_path + "/inspection_helper.py"], env=test_env)
 
-    curr_repro_path = tmp_path / ("../test_override_integrity0." + "make_ttir" + ".repro.mlir")
-    override_ttir_repro = curr_repro_path.read_text()
-    curr_repro_path = tmp_path / ("../test_override_integrity0." + "make_ttgir" + ".repro.mlir")
-    override_ttgir_repro = curr_repro_path.read_text()
+    curr_repro_path = tmp_path / ("../test_inspection_integrity0." + "make_ttir" + ".repro.mlir")
+    inspection_ttir_repro = curr_repro_path.read_text()
+    curr_repro_path = tmp_path / ("../test_inspection_integrity0." + "make_ttgir" + ".repro.mlir")
+    inspection_ttgir_repro = curr_repro_path.read_text()
 
-    assert golden_ttir_repro == override_ttir_repro and golden_ttgir_repro == override_ttgir_repro
+    assert golden_ttir_repro == inspection_ttir_repro and golden_ttgir_repro == inspection_ttgir_repro
