ffede04a9bb2a468933ca3c121f40f0daf4cec90
root <root@banff-ccs-aus-g04-05.cs-aus.dcgpu>
2025-09-15 15:29:32 +0000

    Remove getShapePerCTATile from slice/concat ops

--- third_party/amd/lib/TritonAMDGPUDialectToLLVM/Utility.h
+++ third_party/amd/lib/TritonAMDGPUDialectToLLVM/Utility.h
@@ -3,12 +3,16 @@
 
 #include "triton/Tools/LinearLayout.h"
 
-namespace mlir::LLVM::AMD {
+namespace tt = mlir::triton;
 
-// Determine the order in which CTA tiles are laid out across the tensor.
-// That is, create vector of dimensions from fastest to slowest varying.
-SmallVector<unsigned> getCTATileOrder(MLIRContext *ctx,
-                                      const mlir::triton::LinearLayout &layout);
+namespace mlir::LLVM::AMD {
+using ElemLocationKey = SmallVector<std::pair<StringAttr, int32_t>>;
+
+ElemLocationKey getElemCoordsFromReg(tt::LinearLayout ll, unsigned regId,
+                                     MLIRContext *ctx);
+
+llvm::MapVector<ElemLocationKey, unsigned>
+mapRegToCoordinates(tt::LinearLayout ll, MLIRContext *ctx);
 
 } // namespace mlir::LLVM::AMD
 #endif // TRITON_THIRD_PARTY_AMD_LIB_TRITONAMDGPUDIALECTTOLLVM_UTILITY_H_
