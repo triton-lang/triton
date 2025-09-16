72d354317c543334709f159c1a2c90044bea3586
Puyan Lotfi <puyan@puyan.org>
2025-09-16 11:38:41 -0700

    dropping env var and subprocess forking in test

--- python/test/backend/test_stages_inspection.py
+++ python/test/backend/test_stages_inspection.py
@@ -1,25 +1,52 @@
+import triton
+import triton.language as tl
+from triton import knobs
+from triton._internal_testing import is_cuda, is_hip
+
 import os
-import subprocess
 import pathlib
 
-def test_inspection(tmp_path: pathlib.Path):
-    _run_test = lambda env, dir_path, filename: subprocess.run(["python3", dir_path + "/" + filename],
-                                                     env=env, capture_output=True, text=True)
-    curr_repro_path = tmp_path / ("../test_inspection0." + "make_ttgir" + ".repro.mlir")
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+def test_inspection(monkeypatch, tmp_path: pathlib.Path):
+    stage_name = 'make_ttgir'
+    curr_repro_path = tmp_path / ("repro_prefix." + stage_name + ".repro.mlir")
+    repro_path = tmp_path / "repro_prefix"
 
-    golden_env = os.environ.copy()
-    golden_env["TRITON_ALWAYS_COMPILE"] = "1"
-    golden_env["TRITON_DUMP_DIR"] = str(tmp_path)
-    golden_env["TRITON_REPRODUCER_PATH"] = str(tmp_path)
-    test_env = golden_env.copy()
-    test_env["TRITON_OVERRIDE_DIR"] = str(tmp_path)
+    monkeypatch.setenv("TRITON_ALWAYS_COMPILE", "1")
+    monkeypatch.setenv("TRITON_REPRODUCER_PATH", str(repro_path))
+
+    inspect_stages_hook_called = False
+    make_ttgir_wrapper_called = False
+
+    def inspect_stages_hook(self, stages, options, language, capability):
+        nonlocal inspect_stages_hook_called
+        inspect_stages_hook_called = True
+        def make_ttgir_wrapper(src, metadata, options, capability):
+            nonlocal make_ttgir_wrapper_called
+            make_ttgir_wrapper_called = True
+            return self.make_ttgir(src, metadata, options, capability)
+        stages["ttgir"] = lambda src, metadata: make_ttgir_wrapper(src, metadata, options, capability)
+
+    @triton.jit
+    def k1():
+        return
+
+    @triton.jit
+    def k2():
+        return
 
     # Run once to get the clean/golden repro dump
-    assert "Called make_ttgir_wrapper" not in str(_run_test(golden_env, dir_path, "inspection_helper_golden.py").stdout)
-    golden_ttgir_repro = curr_repro_path.read_text()
+    k1[(1, )]()
+    assert not inspect_stages_hook_called and not make_ttgir_wrapper_called
+    assert os.path.exists(curr_repro_path)
+    golden_repro = curr_repro_path.read_text()
     curr_repro_path.unlink()
 
-    # Run again with pipeline inspection
-    assert "Called make_ttgir_wrapper" in str(_run_test(test_env, dir_path, "inspection_helper.py").stdout)
-    assert golden_ttgir_repro == curr_repro_path.read_text()
+    # Setup hook and call again, check if hooks got called
+    knobs.runtime.add_stages_inspection_hook = inspect_stages_hook
+    k2[(1, )]()
+    assert inspect_stages_hook_called and make_ttgir_wrapper_called
+    assert os.path.exists(curr_repro_path)
+    hook_repro = curr_repro_path.read_text()
+
+    # Check that repros match
+    assert golden_repro.replace('k1', 'dummy') == hook_repro.replace('k2', 'dummy')
