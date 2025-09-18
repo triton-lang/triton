baa41efe6104a5eb44b2dfd9cd0d679dfc256d96
Kyle Wang <ec1wng@gmail.com>
2025-09-18 17:01:23 -0500

    address comments

--- python/triton_kernels/triton_kernels/matmul_ogs_details/opt_flags.py
+++ python/triton_kernels/triton_kernels/matmul_ogs_details/opt_flags.py
@@ -107,7 +107,7 @@ def make_default_opt_flags_amd(
         epilogue_subtile = 1
 
     # specific configs for F16 x MXFP4 on MI355
-    # Notice these configs will exceed LDS usage with async copy enabled
+    # Note that these configs will exceed LDS usage with async copy enabled
     if is_cdna4 and bitwidth(lhs_dtype) == 16 and bitwidth(rhs_dtype) == 4 and precision_config.weight_scale is not None:
         split_k = 1
         if m <= 1024:
