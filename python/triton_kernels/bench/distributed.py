e50be43bb1443555e85ff525cd4ce8228ac29f82
Jokeren <robinho364@gmail.com>
2025-09-24 16:36:32 -0400

    Update

--- python/triton_kernels/bench/distributed.py
+++ python/triton_kernels/bench/distributed.py
@@ -171,6 +171,7 @@ def _accumulate_ep_triton_kernel(ep_positions_ptr, output_tensor_ptr, input_ptrs
     for tp_idx in range(TP):
         rank_index = ep_rank * TP + tp_idx
         input_tensor_ptr = tl.load(input_ptrs + rank_index).to(tl.pointer_type(original_dtype))
+        input_tensor_ptr = tl.multiple_of(input_tensor_ptr, 16)
         input = tl.load(
             input_tensor_ptr + row_offsets + offs_n[None, :],
             mask=input_output_mask & has_token[:, None],
@@ -223,6 +224,7 @@ def _reduce_ep_triton(metadata: ReduceScatterMetadata, input_tensor: torch.Tenso
     n_expts = metadata.ep_indx.size(1 - dim)
     other_dims = input_tensor.shape[1:]
     hidden_size = math.prod(other_dims)
+    assert hidden_size * original_dtype().element_size() >= 16, "hidden_size * element_size must be >= 16"
     output_tensor = input_tensor.new_zeros((n_tokens, ) + other_dims, dtype=original_dtype)
     triton_original_dtype = TRITON_DTYPE_MAP.get(original_dtype, tl.float32)
     triton_intermediate_dtype = TRITON_DTYPE_MAP.get(intermediate_dtype, tl.float32)
