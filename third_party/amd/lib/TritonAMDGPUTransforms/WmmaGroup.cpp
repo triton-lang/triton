c733bf7ed360cd99b0b40a2f513c1adbb96cbc1f
Pengzhan Zhao <borontion@gmail.com>
2025-09-28 10:59:47 -0700

    [AMD] Add more gfx1250 wmma data types (#8312)
    Previously we only have 2 wmma v3 instructions:
    
    - bf16 * bf16 -> fp32
    - bf8 * bf8 -> fp32 k=64
    
    This PR extends to cover the following wmma v3 instructions:
    
    - fp32 * fp32 -> fp32
    - fp16 * fp16 -> fp32
    - fp8 * fp8 -> fp32 k=64/128
    - fp8 * bf8 -> fp32 k=64/128
    - bf8 * fp8 -> fp32 k=64/128
    - bf8 * bf8 -> fp32 k=128

--- third_party/amd/lib/TritonAMDGPUTransforms/WmmaGroup.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/WmmaGroup.cpp
@@ -83,68 +83,86 @@ WmmaDatabase::WmmaDatabase(MLIRContext *context) {
   auto ocpBf8T = b.getType<Float8E5M2Type>();
 
   wmmaMap = {
+      // f32 inputs
+      // wmma_f32_16x16x4_f32
+      TRITON_WMMA_v(3, 16, 16, f32T, f32T, 32, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x4.f32", 4, 2),
+
+      // f16 inputs
+      // wmma_f32_16x16x16_f16
+      TRITON_WMMA_v(1, 16, 16, f16T, f16T, 16, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x16.f16", 16, 16),
+      TRITON_WMMA_v(2, 16, 16, f16T, f16T, 16, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x16.f16", 16, 8),
+      // wmma_f32_16x16x32_f16
+      TRITON_WMMA_v(3, 16, 16, f16T, f16T, 16, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x32.f16", 32, 16),
       // wmma_f16_16x16x16_f16
       TRITON_WMMA_v(1, 16, 16, f16T, f16T, 16, f16T,
                     "llvm.amdgcn.wmma.f16.16x16x16.f16", 16, 16),
       TRITON_WMMA_v(2, 16, 16, f16T, f16T, 16, f16T,
                     "llvm.amdgcn.wmma.f16.16x16x16.f16", 16, 8),
 
+      // bf16 inputs
       // wmma_f32_16x16x16_bf16
       TRITON_WMMA_v(1, 16, 16, bf16T, bf16T, 16, f32T,
                     "llvm.amdgcn.wmma.f32.16x16x16.bf16", 16, 16),
       TRITON_WMMA_v(2, 16, 16, bf16T, bf16T, 16, f32T,
                     "llvm.amdgcn.wmma.f32.16x16x16.bf16", 16, 8),
-
       // wmma_f32_16x16x32_bf16
       TRITON_WMMA_v(3, 16, 16, bf16T, bf16T, 16, f32T,
                     "llvm.amdgcn.wmma.f32.16x16x32.bf16", 32, 16),
-
-      // wmma_f32_16x16x16_f16
-      TRITON_WMMA_v(1, 16, 16, f16T, f16T, 16, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x16.f16", 16, 16),
-      TRITON_WMMA_v(2, 16, 16, f16T, f16T, 16, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x16.f16", 16, 8),
-
       // wmma_bf16_16x16x16_bf16
       TRITON_WMMA_v(1, 16, 16, bf16T, bf16T, 16, bf16T,
                     "llvm.amdgcn.wmma.bf16.16x16x16.bf16", 16, 16),
       TRITON_WMMA_v(2, 16, 16, bf16T, bf16T, 16, bf16T,
                     "llvm.amdgcn.wmma.bf16.16x16x16.bf16", 16, 8),
 
-      // wmma_i32_16x16x16_iu4
-      TRITON_WMMA_v(1, 16, 16, i4T, i4T, 4, i32T,
-                    "llvm.amdgcn.wmma.i32.16x16x16.iu4", 16, 16),
+      // fp8/bf8 inputs
+      // wmma_f32_16x16x16_fp8_fp8
+      TRITON_WMMA_v(2, 16, 16, ocpFp8T, ocpFp8T, 8, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x16.fp8.fp8", 16, 8),
+      // wmma_f32_16x16x128_fp8_fp8 & wmma_f32_16x16x64_fp8_fp8
+      TRITON_WMMA_v_2case(3, 16, 16, ocpFp8T, ocpFp8T, 8, f32T,
+                          "llvm.amdgcn.wmma.f32.16x16x128.fp8.fp8", 128, 64,
+                          "llvm.amdgcn.wmma.f32.16x16x64.fp8.fp8", 64, 32),
+      // wmma_f32_16x16x16_fp8_bf8
+      TRITON_WMMA_v(2, 16, 16, ocpFp8T, ocpBf8T, 8, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x16.fp8.bf8", 16, 8),
+      // wmma_f32_16x16x128_fp8_bf8 & wmma_f32_16x16x64_fp8_bf8
+      TRITON_WMMA_v_2case(3, 16, 16, ocpFp8T, ocpBf8T, 8, f32T,
+                          "llvm.amdgcn.wmma.f32.16x16x128.fp8.bf8", 128, 64,
+                          "llvm.amdgcn.wmma.f32.16x16x64.fp8.bf8", 64, 32),
+      // wmma_f32_16x16x16_bf8_fp8
+      TRITON_WMMA_v(2, 16, 16, ocpBf8T, ocpFp8T, 8, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x16.bf8.fp8", 16, 8),
+      // wmma_f32_16x16x128_bf8_fp8 & wmma_f32_16x16x64_bf8_fp8
+      TRITON_WMMA_v_2case(3, 16, 16, ocpBf8T, ocpFp8T, 8, f32T,
+                          "llvm.amdgcn.wmma.f32.16x16x128.bf8.fp8", 128, 64,
+                          "llvm.amdgcn.wmma.f32.16x16x64.bf8.fp8", 64, 32),
+      // wmma_f32_16x16x16_bf8_bf8
+      TRITON_WMMA_v(2, 16, 16, ocpBf8T, ocpBf8T, 8, f32T,
+                    "llvm.amdgcn.wmma.f32.16x16x16.bf8.bf8", 16, 8),
+      // wmma_f32_16x16x128_bf8_bf8 & wmma_f32_16x16x64_bf8_bf8
+      TRITON_WMMA_v_2case(3, 16, 16, ocpBf8T, ocpBf8T, 8, f32T,
+                          "llvm.amdgcn.wmma.f32.16x16x128.bf8.bf8", 128, 64,
+                          "llvm.amdgcn.wmma.f32.16x16x64.bf8.bf8", 64, 32),
 
-      // wmma_i32_16x16x32_iu4 && wmma_i32_16x16x16_iu4
-      TRITON_WMMA_v2_2case(16, 16, i4T, i4T, 4, i32T,
-                           "llvm.amdgcn.wmma.i32.16x16x32.iu4", 32, 16,
-                           "llvm.amdgcn.wmma.i32.16x16x16.iu4", 16, 8),
-
+      // iu8 inputs
       // wmma_i32_16x16x16_iu8
       TRITON_WMMA_v(1, 16, 16, i8T, i8T, 8, i32T,
                     "llvm.amdgcn.wmma.i32.16x16x16.iu8", 16, 16),
       TRITON_WMMA_v(2, 16, 16, i8T, i8T, 8, i32T,
                     "llvm.amdgcn.wmma.i32.16x16x16.iu8", 16, 8),
 
-      // wmma_f32_16x16x16_fp8_fp8
-      TRITON_WMMA_v(2, 16, 16, ocpFp8T, ocpFp8T, 8, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x16.fp8.fp8", 16, 8),
-
-      // wmma_f32_16x16x16_fp8_bf8
-      TRITON_WMMA_v(2, 16, 16, ocpFp8T, ocpBf8T, 8, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x16.fp8.bf8", 16, 8),
-
-      // wmma_f32_16x16x16_bf8_fp8
-      TRITON_WMMA_v(2, 16, 16, ocpBf8T, ocpFp8T, 8, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x16.bf8.fp8", 16, 8),
-
-      // wmma_f32_16x16x16_bf8_bf8
-      TRITON_WMMA_v(2, 16, 16, ocpBf8T, ocpBf8T, 8, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x16.bf8.bf8", 16, 8),
-
-      // wmma_f32_16x16x64_bf8_bf8
-      TRITON_WMMA_v(3, 16, 16, ocpBf8T, ocpBf8T, 8, f32T,
-                    "llvm.amdgcn.wmma.f32.16x16x64.bf8.bf8", 64, 32),
+      // iu4 inputs
+      // wmma_i32_16x16x16_iu4
+      TRITON_WMMA_v(1, 16, 16, i4T, i4T, 4, i32T,
+                    "llvm.amdgcn.wmma.i32.16x16x16.iu4", 16, 16),
+      // wmma_i32_16x16x32_iu4 && wmma_i32_16x16x16_iu4
+      TRITON_WMMA_v2_2case(16, 16, i4T, i4T, 4, i32T,
+                           "llvm.amdgcn.wmma.i32.16x16x32.iu4", 32, 16,
+                           "llvm.amdgcn.wmma.i32.16x16x16.iu4", 16, 8),
   };
 }
 
@@ -184,6 +202,35 @@ WmmaIntrinsic::selectFor(int version, unsigned mDim, unsigned nDim,
   // We always have one choice--the only / smallest-K intrinsic.
   auto [symbol, k, kBase] = values.back();
 
+  return WmmaIntrinsic(symbol, mDim, nDim, k, kBase, aElemType, bElemType,
+                       dElemType);
+}
+
+FailureOr<WmmaIntrinsic> WmmaIntrinsic::get(int version, unsigned mDim,
+                                            unsigned nDim, unsigned kDim,
+                                            Type aElemType, Type bElemType,
+                                            Type dElemType) {
+  const WmmaMap &wmmaMap = WmmaDatabase::get(aElemType.getContext());
+  WmmaKey key = {version,
+                 mDim,
+                 nDim,
+                 aElemType.getTypeID(),
+                 bElemType.getTypeID(),
+                 aElemType.getIntOrFloatBitWidth(),
+                 dElemType.getTypeID()};
+
+  auto it = wmmaMap.find(key);
+  if (it == wmmaMap.end())
+    return failure();
+
+  const SmallVector<WmmaMapValue, 2> &values = it->second;
+  auto match = llvm::find_if(values, [&](const WmmaMapValue &val) {
+    return std::get<1>(val) == kDim;
+  });
+  if (match == values.end())
+    return failure();
+
+  auto [symbol, k, kBase] = *match;
   return WmmaIntrinsic(symbol, mDim, nDim, k, kBase, aElemType, bElemType,
                        dElemType);
 }
