dc6818ec1f76999725bd40401a708f26aeedf8ff
Zeng Wu <zengwu13@amd.com>
2025-09-30 14:29:12 -0500

    [AMD][Gluon] Refactor buffer_atomic_rmw APIs

--- python/triton/experimental/gluon/language/amd/cdna3/__init__.py
+++ python/triton/experimental/gluon/language/amd/cdna3/__init__.py
@@ -9,7 +9,10 @@ from ..._core import builtin, _unwrap_if_constexpr
 if TYPE_CHECKING:
     from ..._semantic import GluonSemantic
 
-__all__ = ["buffer_atomic_rmw", "buffer_load", "buffer_store", "mfma"]
+__all__ = [
+    "buffer_atomic_add", "buffer_atomic_and", "buffer_atomic_min", "buffer_atomic_max", "buffer_atomic_or",
+    "buffer_atomic_xor", "buffer_atomic_xor", "buffer_load", "buffer_store", "mfma"
+]
 
 _atomic_op_str_to_op = {
     "smax": ir.ATOMIC_OP.MAX, "smin": ir.ATOMIC_OP.MIN, "umax": ir.ATOMIC_OP.UMAX, "umin": ir.ATOMIC_OP.UMIN, "fadd":
@@ -28,32 +31,47 @@ def _verify_buffer_ops(ptr, offsets, mask=None, other=None):
         assert mask is not None, "when other is not None, mask should not be None"
 
 
-def _verify_element_type_in_buffer_atomic(op, elem_type, arch):
+def _verify_element_type_and_dispatch_op(op, elem_type, arch):
     supported_types = [
         ttgl.float16, ttgl.float32, ttgl.bfloat16, ttgl.float64, ttgl.int32, ttgl.int64, ttgl.uint32, ttgl.uint64
     ]
     assert elem_type in supported_types, f"{elem_type} is not supported in buffer atomic on {arch}."
 
-    op = _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
-    if op in [ir.ATOMIC_OP.AND, ir.ATOMIC_OP.OR, ir.ATOMIC_OP.XOR, ir.ATOMIC_OP.XCHG]:
-        assert elem_type in [ttgl.int32, ttgl.int64], f"{op} with {elem_type} is not supported on CDNA3 and CDNA4"
+    if op in ['and', 'or', 'xor', 'xchg']:
+        assert elem_type in [ttgl.int32, ttgl.int64], f"{op} with {elem_type} is not supported on CDNA3 or CDNA4"
+        return _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
 
-    if op in [ir.ATOMIC_OP.UMAX, ir.ATOMIC_OP.UMIN, ir.ATOMIC_OP.ADD]:
-        assert elem_type in [ttgl.uint32, ttgl.uint64], f"{op} with {elem_type} is not supported on CDNA3 and CDNA4"
-
-    if op in [ir.ATOMIC_OP.MAX, ir.ATOMIC_OP.MIN]:
-        assert elem_type in [ttgl.int32, ttgl.int64,
-                             ttgl.float64], "smax only support int32, int64 and fp64 on CDNA3 and CDNA4"
-
-    if op == ir.ATOMIC_OP.FADD:
-        if elem_type is ttgl.bfloat16:
-            assert arch == "cdna4", "Buffer atomic fadd with bf16 is only supported on CDNA4 for now."
+    if op in ['max', 'min']:
+        if elem_type in [ttgl.int32, ttgl.int64, ttgl.float64]:
+            op = 's' + op
+            return _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
+        elif elem_type in [ttgl.uint32, ttgl.uint64]:
+            op = 'u' + op
+            return _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
         else:
-            assert elem_type in [ttgl.float16, ttgl.float32, ttgl.float64]
+            raise ValueError(f"{op} with {elem_type} is not supported on CDNA3 and CDNA4")
+
+    if op == 'add':
+        if elem_type in [ttgl.uint32, ttgl.uint64]:
+            op = 'i' + op
+            return _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
+        elif elem_type in [ttgl.float16, ttgl.float32, ttgl.float64]:
+            op = 'f' + op
+            return _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
+        elif elem_type is ttgl.bfloat16:
+            assert arch == "cdna4", "Buffer atomic fadd with bf16 is only supported on CDNA4 for now."
+            op = 'f' + op
+            return _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
+        else:
+            raise ValueError(f"{op} with {elem_type} is not supported on CDNA3 and CDNA4")
+
+    raise ValueError(f"Unknown {op} on CDNA3 or CDNA4")
 
 
 def _buffer_atomic_rmw_impl(op, ptr, offsets, value, arch, mask, sem, scope, _semantic):
     _verify_buffer_ops(ptr, offsets, mask)
+
+    op = _verify_element_type_and_dispatch_op(op, ptr.type.scalar.element_ty, arch)
 
     mask = _unwrap_if_constexpr(mask)
     if mask is not None:
@@ -66,11 +84,8 @@ def _buffer_atomic_rmw_impl(op, ptr, offsets, value, arch, mask, sem, scope, _se
     value = _semantic.to_tensor(value)
     _, value = _semantic.broadcast_impl_value(offsets, value)
 
-    _verify_element_type_in_buffer_atomic(op, value.dtype, arch)
-
     sem = _semantic._str_to_sem(sem)
     scope = _semantic._str_to_scope(scope)
-    op = _atomic_op_str_to_op[_unwrap_if_constexpr(op)]
     return _semantic.tensor(
         _semantic.builder.create_buffer_atomic_rmw(op, ptr.handle, offsets.handle, value.handle, sem, scope, mask),
         value.type)
@@ -153,28 +168,71 @@ def mfma(a, b, acc, _semantic: GluonSemantic = None):
     return ttgl.tensor(handle, ret_type)
 
 
+"""
+AMD Buffer Atomic RMW operations.
+The supported operatios are max, min, add, and, or, xor, xchg.
+Similar to normal atomic ops: it loads data at ptr plus offsets, do `op` with `value`, and store result to `ptr` plus `offsets` with
+the specified memory semantics and scope.
+
+Buffer atomics access global memory via a scalar base pointer and a tensor of offsets instead of a tensor of pointers.
+Similar to other buffer ops, the `mask` is a boolean vector that determines if a given element should be processed with
+the atomic RMW op. Elements with `mask[i] == 0` are dropped (i.e., the atomic is not executed).
+
+Buffer Atomic RMW ops return the pre-op value in the global memory.
+
+Args:
+    ptr (pointer to scalar): Global memory scalar base pointer to load from.
+    offsets (tensor): Offsets tensor for the load operation.
+    value (tensor): Another operand of `op`.
+    mask (tensor, optional): Mask tensor for predicated loads. Defaults to None.
+    sem (str, optional): Memory Semantic Descriptor. Default is None which means acq_rel memory semantic.
+    scope (str, optional): Memory Sync Scope for atomic accesses. Default is None and it will be mapped to `gpu`, which is called `agent` for AMDGPU. Please ref https://llvm.org/docs/AMDGPUUsage.html#memory-model-gfx942 for details.
+"""
+
+
 @builtin
-def buffer_atomic_rmw(op, ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
-    """
-    AMD Buffer Atomic RMW operation.
-    Similar to normal atomic ops: it loads data at ptr plus offsets, do `op` with `value`, and store result to `ptr` plus `offsets` with
-    the specified memory semantics and scope.
+def buffer_atomic_max(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
+    return _buffer_atomic_rmw_impl('max', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+                                   _semantic=_semantic)
 
-    Buffer atomics access global memory via a scalar base pointer and a tensor of offsets instead of a tensor of pointers.
-    Similar to other buffer ops, the `mask` is a boolean vector that determines if a given element should be processed with
-    the atomic RMW op. Elements with `mask[i] == 0` are dropped (i.e., the atomic is not executed).
 
-    Buffer Atomic RMW ops return the pre-op value in the global memory.
+@builtin
+def buffer_atomic_min(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
 
-    Args:
-        op (str) : The operator to be executed atomically.
-        ptr (pointer to scalar): Global memory scalar base pointer to load from.
-        offsets (tensor): Offsets tensor for the load operation.
-        value (tensor): Another operand of `op`.
-        mask (tensor, optional): Mask tensor for predicated loads. Defaults to None.
-        sem (str, optional): Memory Semantic Descriptor. Default is None which means acq_rel memory semantic.
-        scope (str, optional): Memory Sync Scope for atomic accesses. Default is None and it will be mapped to `gpu`, which is called `agent` for AMDGPU. Please ref https://llvm.org/docs/AMDGPUUsage.html#memory-model-gfx942 for details.
-    """
+    return _buffer_atomic_rmw_impl('min', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+                                   _semantic=_semantic)
 
-    return _buffer_atomic_rmw_impl(op, ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+
+@builtin
+def buffer_atomic_add(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
+
+    return _buffer_atomic_rmw_impl('add', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+                                   _semantic=_semantic)
+
+
+@builtin
+def buffer_atomic_and(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
+
+    return _buffer_atomic_rmw_impl('and', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+                                   _semantic=_semantic)
+
+
+@builtin
+def buffer_atomic_or(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
+
+    return _buffer_atomic_rmw_impl('or', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+                                   _semantic=_semantic)
+
+
+@builtin
+def buffer_atomic_xor(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
+
+    return _buffer_atomic_rmw_impl('xor', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
+                                   _semantic=_semantic)
+
+
+@builtin
+def buffer_atomic_xchg(ptr, offsets, value, mask=None, sem=None, scope=None, _semantic=None):
+
+    return _buffer_atomic_rmw_impl('xchg', ptr, offsets, value, "cdna3", mask=mask, sem=sem, scope=scope,
                                    _semantic=_semantic)
