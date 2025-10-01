1328b6a7b990e094860a4c15c4e5f4e0b0e37a53
Alexander Efimov <efimov.alexander@gmail.com>
2025-10-01 12:28:31 +0000

    make test more robust

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -6667,10 +6667,13 @@ def test_schedule_hint(device):
         off_k = tl.arange(0, BLOCK_K)
         Xs = X + off_m[:, None] * BLOCK_K + off_k[None, :] * 1
         Ys = Y + off_k[:, None] * 1 + off_n[None, :] * BLOCK_K
-        Zs = Z + off_m[:, None] * BLOCK_N + off_n[None, :] * 1
+        z_offset = off_m[:, None] * BLOCK_N + off_n[None, :] * 1
+        Zs = Z + z_offset
         x = tl.load(Xs)
         y = tl.load(Ys)
         z = tl.dot(x, y)
+        # additional computations to give more diverse context to backend scheduler
+        z += z_offset
         tl.store(Zs, z)
 
     M = 128
@@ -6681,11 +6684,6 @@ def test_schedule_hint(device):
     pgm_ilp = kernel.warmup(torch.float32, torch.float32, torch.float32, M, N, K,
                             schedule_hint="iterative-ilp-scheduler", grid=(1, ))
 
-    def get_num_vgprs(text):
-        for line in text.split("\n"):
-            if ".vgpr_count" in line:
-                return int(line.split(" ")[-1])
-
-    default_vgprs = get_num_vgprs(pgm_default.asm['amdgcn'])
-    ilp_vgprs = get_num_vgprs(pgm_ilp.asm['amdgcn'])
-    assert ilp_vgprs != default_vgprs
+    # check that option affects only llvm backend
+    assert pgm_default.asm["llir"] == pgm_ilp.asm["llir"]
+    assert pgm_default.asm["amdgcn"] != pgm_ilp.asm["amdgcn"]
