1d74879ec4190a527b2ad3a4fe65e47566266787
pawelszczerbuk <153013546+pawelszczerbuk@users.noreply.github.com>
2025-10-02 16:37:46 -0700

    [ConSan] Make sure kernel is recompiled when consan state changes (#8342)
    Include ConSan state in the compilation options and kwargs of the kernel
    to force jit cache miss. Also, preparing the knobs to unify this
    behavior with proton by introducing `instrumentation_mode` compiltion
    knob that can be set to "consan" or "proton" by proton runtime.

--- python/triton/knobs.py
+++ python/triton/knobs.py
@@ -364,7 +364,9 @@ class compilation_knobs(base_knobs):
     disable_line_info: env_bool = env_bool("TRITON_DISABLE_LINE_INFO")
     front_end_debugging: env_bool = env_bool("TRITON_FRONT_END_DEBUGGING")
     allow_non_constexpr_globals: env_bool = env_bool("TRITON_ALLOW_NON_CONSTEXPR_GLOBALS")
-    enable_experimental_consan: env_bool = env_bool("TRITON_ENABLE_EXPERIMENTAL_CONSAN")
+    # Instrumentation mode is checked on every run, which is expensive.
+    # We cache the value here to avoid the expensive check on every run.
+    instrumentation_mode: str = env_str("TRITON_INSTRUMENTATION_MODE", "").get()
     listener: Union[CompilationListener, None] = None
 
 
@@ -533,3 +535,4 @@ proton = proton_knobs()
 
 def refresh_knobs():
     runtime.debug = env_bool("TRITON_DEBUG").get()
+    compilation.instrumentation_mode = env_str("TRITON_INSTRUMENTATION_MODE", "").get()
