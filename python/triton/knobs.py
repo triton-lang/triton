da5bca7ed1ff8db4fc555c6ecc5c9123d52ab56e
Pawel Szczerbuk <pawel.szczerbuk@openai.com>
2025-10-02 14:26:17 -0700

    Making instrumentation_mode cached

--- python/triton/knobs.py
+++ python/triton/knobs.py
@@ -366,7 +366,7 @@ class compilation_knobs(base_knobs):
     allow_non_constexpr_globals: env_bool = env_bool("TRITON_ALLOW_NON_CONSTEXPR_GLOBALS")
     # Instrumentation mode is checked on every run, which is expensive.
     # We cache the value here to avoid the expensive check on every run.
-    instrumentation_mode: str | None = env_opt_str("TRITON_INSTRUMENTATION_MODE").get()
+    instrumentation_mode: Union[str, None] = env_opt_str("TRITON_INSTRUMENTATION_MODE").get()
     listener: Union[CompilationListener, None] = None
 
 
