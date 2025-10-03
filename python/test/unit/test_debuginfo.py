c6993c0dfe22c608bdcdcdef8c2f89f4ba27279d
Anatoly Myachev <anatoly.myachev@intel.com>
2025-10-03 10:22:28 +0200

    use 'kernel.device_caches.clear()'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/test_debuginfo.py
+++ python/test/unit/test_debuginfo.py
@@ -7,26 +7,22 @@ import triton
 import triton.language as tl
 
 
-def kernel_factory():
-    # to avoid reusing the cached kernel
-    @triton.jit
-    def add_kernel(
-        x_ptr,
-        y_ptr,
-        output_ptr,
-        n_elements,
-        BLOCK_SIZE: tl.constexpr,
-    ):
-        pid = tl.program_id(axis=0)
-        block_start = pid * BLOCK_SIZE
-        offsets = block_start + tl.arange(0, BLOCK_SIZE)
-        mask = offsets < n_elements
-        x = tl.load(x_ptr + offsets, mask=mask)
-        y = tl.load(y_ptr + offsets, mask=mask)
-        output = x + y
-        tl.store(output_ptr + offsets, output, mask=mask)
-
-    return add_kernel
+@triton.jit
+def add_kernel(
+    x_ptr,
+    y_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
 
 
 def checkDbgInfo(llir, hasDbgInfo):
@@ -64,5 +60,6 @@ def test_triton_debuginfo_on(lineInfoKey, diLocalVarKey, hasDbgInfo, device, mon
     output = torch.empty_like(x)
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
-    h = kernel_factory()[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    add_kernel.device_caches.clear()
+    h = add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
     checkDbgInfo(h.asm['llir'], hasDbgInfo)
