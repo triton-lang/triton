be6a688b3849e3d9004184e7510a8665da73f3aa
Anatoly Myachev <anatoly.myachev@intel.com>
2025-10-03 15:43:45 +0200

    [Tests] Remove subprocess usage from `test_triton_debuginfo_on` (#8350)
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- python/test/unit/test_debuginfo.py
+++ python/test/unit/test_debuginfo.py
@@ -1,40 +1,65 @@
 import os
-import subprocess
 
-all_names = ["offsets", "pid", "block_start", "mask", "x", "y", "output"]
+import pytest
+import torch
+
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def add_kernel(
+    x_ptr,
+    y_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
 
 
 def checkDbgInfo(llir, hasDbgInfo):
     assert hasDbgInfo == ('dbg_value' in llir)
-    for name in all_names:
+    for name in ["offsets", "pid", "block_start", "mask", "x", "y", "output"]:
         assert hasDbgInfo == ('!DILocalVariable(name: \"' + name + '\"' in llir)
 
 
-def test_triton_debuginfo_on():
-    lineInfoKey = "TRITON_DISABLE_LINE_INFO"
-    diLocalVarKey = "LLVM_EXTRACT_DI_LOCAL_VARIABLES"
+@pytest.mark.parametrize("lineInfoKey, diLocalVarKey, hasDbgInfo", [
+    (None, None, False),
+    # expect dbginfo based on parent proccess' TRITON_DISABLE_LINE_INFO
+    (None, "1", "infer"),
+    ("0", "1", True),
+    ("1", "1", False),
+    ("0", "0", False),
+    ("1", "0", False),
+])
+def test_triton_debuginfo_on(lineInfoKey, diLocalVarKey, hasDbgInfo, device, monkeypatch):
+    lineInfoKeyName = "TRITON_DISABLE_LINE_INFO"
+    diLocalVarKeyName = "LLVM_EXTRACT_DI_LOCAL_VARIABLES"
+    if lineInfoKey is not None:
+        monkeypatch.setenv(lineInfoKeyName, lineInfoKey)
+    if diLocalVarKey is not None:
+        monkeypatch.setenv(diLocalVarKeyName, diLocalVarKey)
 
     isEnvSet = lambda env, str: env.get(str, None) is not None
-    hasOrigLineInfo = (not isEnvSet(os.environ, lineInfoKey)
-                       or os.environ[lineInfoKey].lower() not in ["on", "true", "1"])
-    envs = [
-        # expect no dbginfo if unset
-        {lineInfoKey: None, diLocalVarKey: None, "hasDbgInfo": False},
-        # expect dbginfo based on parent proccess' TRITON_DISABLE_LINE_INFO
-        {lineInfoKey: None, diLocalVarKey: "1", "hasDbgInfo": hasOrigLineInfo},
-        {lineInfoKey: "0", diLocalVarKey: "1", "hasDbgInfo": True},
-        {lineInfoKey: "1", diLocalVarKey: "1", "hasDbgInfo": False},
-        {lineInfoKey: "0", diLocalVarKey: "0", "hasDbgInfo": False},
-        {lineInfoKey: "1", diLocalVarKey: "0", "hasDbgInfo": False},
-    ]
+    if hasDbgInfo == "infer":
+        hasDbgInfo = (not isEnvSet(os.environ, lineInfoKeyName)
+                      or os.environ[lineInfoKeyName].lower() not in ["on", "true", "1"])
 
-    _run_test = lambda test_env: subprocess.run([
-        "python3", os.path.dirname(os.path.realpath(__file__)) + "/test_debuginfo_helper.py"
-    ], env=test_env, capture_output=True, text=True)
-    for env in envs:
-        test_env = os.environ.copy()
-        test_env["TRITON_ALWAYS_COMPILE"] = "1"
-        for entry in env:
-            if not isEnvSet(env, entry): continue
-            test_env[entry] = str(env[entry])
-        checkDbgInfo(str(_run_test(test_env).stdout), hasDbgInfo=env["hasDbgInfo"])
+    size = 98432
+    torch.manual_seed(0)
+    x = torch.rand(size, device=device)
+    y = torch.rand(size, device=device)
+    output = torch.empty_like(x)
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    add_kernel.device_caches.clear()
+    h = add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    checkDbgInfo(h.asm['llir'], hasDbgInfo)
