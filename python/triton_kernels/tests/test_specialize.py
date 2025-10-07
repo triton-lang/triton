38844a6f6b8f26924f86a350c956236e7bf06ba9
Jokeren <robinho364@gmail.com>
2025-10-07 10:35:58 -0400

    Update

--- python/triton_kernels/tests/test_specialize.py
+++ python/triton_kernels/tests/test_specialize.py
@@ -1,5 +1,4 @@
 import importlib
-import re
 
 import torch
 from triton_kernels.specialize import cacheable, specialize
@@ -75,7 +74,7 @@ def test_cacheable(device, fresh_triton_cache):
     for line in ttir.split("\n"):
         if loc and loc in line:
             assert "test_specialize.py" in line
-            assert ":19" in line
+            assert ":18" in line
         if "store" in line:
             loc = line.split("(", 1)[1].split(")", 1)[0]
     assert loc is not None, f"Expected to find a store instruction with location info, got: {ttir}"
