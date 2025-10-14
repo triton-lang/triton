71852632cb7de906462bda4c67a8c4910a3be734
Corbin Robeck <robeck@meta.com>
2025-10-13 17:58:03 -0700

    update naming

---
+++ test/lib/Plugins/TritonPlugin.cpp
@@ -0,0 +1,45 @@
+#include "mlir/Analysis/SliceAnalysis.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
+#include "mlir/Transforms/Passes.h"
+#include "triton/Dialect/Triton/IR/Utility.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
+#include <Python.h>
+
+
+namespace mlir {
+namespace triton {
+namespace plugin {
+
+
+#define GEN_PASS_DEF_TRITONGPUMLIRPLUGIN
+#include "Passes.h.inc"
+
+struct MLIRPluginPass :
+  public impl::TritonGPUMLIRPluginBase<MLIRPluginPass> {
+  void runOnOperation() override {
+
+    MLIRContext *context = &getContext();
+    ModuleOp mod = getOperation();
+    mod.walk([&](FunctionOpInterface funcOp) {
+      StringAttr funcNameAttr = funcOp.getNameAttr();
+      funcOp.setName("foo");
+    }
+    );
+
+  }
+};
+
+} // namespace plugin
+} // namespace triton
+} // namespace mlir
+
+extern "C" void addTritonPluginPass(mlir::PassManager* pm) {
+  pm->addPass(mlir::triton::plugin::createTritonGPUMLIRPlugin());
+}
+
+extern "C" void registerTritonPluginPass() {
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return mlir::triton::plugin::createTritonGPUMLIRPlugin();
+  });
+}
