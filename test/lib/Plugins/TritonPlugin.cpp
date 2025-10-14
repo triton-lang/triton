14fafc94d30a10f3e08f6429c3fa9e051e00482a
Corbin Robeck <robeck@meta.com>
2025-10-13 19:53:52 -0700

    lint

--- test/lib/Plugins/TritonPlugin.cpp
+++ test/lib/Plugins/TritonPlugin.cpp
@@ -7,17 +7,14 @@
 #include <Python.h>
 #include <unordered_map>
 
-
 namespace mlir {
 namespace triton {
 namespace plugin {
 
-
 #define GEN_PASS_DEF_TRITONGPUMLIRPLUGIN
 #include "Passes.h.inc"
 
-struct MLIRPluginPass :
-  public impl::TritonGPUMLIRPluginBase<MLIRPluginPass> {
+struct MLIRPluginPass : public impl::TritonGPUMLIRPluginBase<MLIRPluginPass> {
   void runOnOperation() override {
 
     MLIRContext *context = &getContext();
@@ -25,9 +22,7 @@ struct MLIRPluginPass :
     mod.walk([&](FunctionOpInterface funcOp) {
       StringAttr funcNameAttr = funcOp.getNameAttr();
       funcOp.setName("foo");
-    }
-    );
-
+    });
   }
 };
 
@@ -35,7 +30,7 @@ struct MLIRPluginPass :
 } // namespace triton
 } // namespace mlir
 
-static void addTritonPluginPass(mlir::PassManager* pm) {
+static void addTritonPluginPass(mlir::PassManager *pm) {
   pm->addPass(mlir::triton::plugin::createTritonGPUMLIRPlugin());
 }
 
@@ -46,29 +41,28 @@ static void registerTritonPluginPass() {
 }
 
 static const char *ADD_PLUGIN_PASS_NAME = "add_plugin";
-static std::unordered_map<std::string, void (*)(mlir::PassManager *)> passMap = {
-  {ADD_PLUGIN_PASS_NAME, addTritonPluginPass}
-};
+static std::unordered_map<std::string, void (*)(mlir::PassManager *)> passMap =
+    {{ADD_PLUGIN_PASS_NAME, addTritonPluginPass}};
 static std::unordered_map<std::string, void (*)()> registryMap = {
-  {ADD_PLUGIN_PASS_NAME, registerTritonPluginPass}
-};
-static std::vector<const char *> passNamesTable = {
-  ADD_PLUGIN_PASS_NAME
-};
+    {ADD_PLUGIN_PASS_NAME, registerTritonPluginPass}};
+static std::vector<const char *> passNamesTable = {ADD_PLUGIN_PASS_NAME};
 
 // Key APIs:
 
-extern "C" __attribute__((visibility("default"))) void tritonAddPluginPass(mlir::PassManager* pm, const char *passName) {
+extern "C" __attribute__((visibility("default"))) void
+tritonAddPluginPass(mlir::PassManager *pm, const char *passName) {
   std::string passNameStr(passName);
   passMap[passNameStr](pm);
 }
 
-extern "C" __attribute__((visibility("default"))) void tritonRegisterPluginPass(const char *passName) {
+extern "C" __attribute__((visibility("default"))) void
+tritonRegisterPluginPass(const char *passName) {
   std::string passNameStr(passName);
   registryMap[passNameStr]();
 }
 
-extern "C" __attribute__((visibility("default"))) void tritonEnumeratePluginPasses(uint32_t *passCount, const char **passNames) {
+extern "C" __attribute__((visibility("default"))) void
+tritonEnumeratePluginPasses(uint32_t *passCount, const char **passNames) {
   *passCount = passMap.size();
   if (!passNames)
     return;
