8f41ae3271c4e2588711bfb25f5ef3c013ab5b8d
Jokeren <robinho364@gmail.com>
2025-10-16 15:25:14 -0400

    Fix

--- python/triton_kernels/triton_kernels/topk.py
+++ python/triton_kernels/triton_kernels/topk.py
@@ -61,7 +61,8 @@ def topk_forward(x, k, apply_softmax=True, dim=1, y_indx=None, n_rows=None, all_
     )
     if all_gather:
         y_vals_hdl.barrier(channel=0)
-        y_indx_hdl.barrier(channel=0)
+        if y_indx_hdl is not None:
+            y_indx_hdl.barrier(channel=0)
         bitmatrix_hdl.barrier(channel=0)
     bitmatrix_shape = [n_rows * dist.get_world_size() if all_gather else n_rows, n_cols_words * 32]
     bitmatrix_shape_max = [n_rows_out_max, None]
