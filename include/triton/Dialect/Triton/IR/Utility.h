07c8b2fba88638b92576c2720a0245b4f2f45541
Puyan Lotfi <puyan@puyan.org>
2025-10-16 20:45:24 -0700

    lint

--- include/triton/Dialect/Triton/IR/Utility.h
+++ include/triton/Dialect/Triton/IR/Utility.h
@@ -7,14 +7,14 @@
 #include <algorithm>
 #include <numeric>
 
-
 extern "C" {
-  enum TritonPluginResult {
-    TP_SUCESS = 0,
-    TP_GENERIC_FAILURE = 1,
-  };
+enum TritonPluginResult {
+  TP_SUCESS = 0,
+  TP_GENERIC_FAILURE = 1,
 };
-#define TRITON_PLUGIN_API extern "C" __attribute__((visibility("default"))) TritonPluginResult
+};
+#define TRITON_PLUGIN_API                                                      \
+  extern "C" __attribute__((visibility("default"))) TritonPluginResult
 
 namespace mlir {
 
