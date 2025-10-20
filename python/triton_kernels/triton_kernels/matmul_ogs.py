069318b61eeff32e35f5f7e310b7d44b642cf547
Phil Tillet <phil@openai.com>
2025-10-19 23:36:01 -0700

    .

--- python/triton_kernels/triton_kernels/matmul_ogs.py
+++ python/triton_kernels/triton_kernels/matmul_ogs.py
@@ -425,6 +425,9 @@ def matmul_ogs(x, w, bias,
         x_transpose, y_acc_in is not None,
         inner_routing_data.block_k if inner_routing_data is not None else None,
     )
+    # TODO: track down why this is needed
+    if (fused_activation.specs.fn is not None) or (epilogue.specs.fn is not None):
+        opt_flags.split_k = 1
     if inner_routing_data is not None:
         assert opt_flags.block_k == inner_routing_data.block_k
         assert opt_flags.split_k == 1
