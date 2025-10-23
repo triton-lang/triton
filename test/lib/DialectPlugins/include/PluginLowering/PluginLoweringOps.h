7c12b81539a9664f78d91d6144cfcd3818fddad0
Puyan Lotfi <puyan@puyan.org>
2025-10-23 16:27:25 -0700

    Triton Dialect Plugins, first attempt

---
+++ test/lib/DialectPlugins/include/PluginLowering/PluginLoweringOps.h
@@ -0,0 +1,21 @@
+//===- PluginLoweringOps.h - PluginLowering dialect ops -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef PLUGINLOWERING_PLUGINLOWERINGOPS_H
+#define PLUGINLOWERING_PLUGINLOWERINGOPS_H
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#define GET_OP_CLASSES
+#include "PluginLowering/PluginLoweringOps.h.inc"
+
+#endif // PLUGINLOWERING_PLUGINLOWERINGOPS_H
---
+++ test/lib/DialectPlugins/lib/PluginLowering/PluginLoweringOps.cpp
@@ -0,0 +1,13 @@
+//===- PluginLoweringOps.cpp - PluginLowering dialect ops ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "PluginLowering/PluginLoweringOps.h"
+#include "PluginLowering/PluginLoweringDialect.h"
+
+#define GET_OP_CLASSES
+#include "PluginLowering/PluginLoweringOps.cpp.inc"
