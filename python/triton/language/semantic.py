b4c5356f0480c654f1ad2d77eab67f5084552179
YOUR NAME <gh512@openai.com>
2025-10-30 16:31:17 +0000

    fix both

--- python/triton/language/semantic.py
+++ python/triton/language/semantic.py
@@ -1256,9 +1256,13 @@ class TritonSemantic(Generic[TensorTy]):
 
         # Make `mask` and `val` into the same shape as `ptr`
         if ptr.type.is_block():
+            # if mask is not None:
+            #     ptr, mask = self.broadcast_impl_value(ptr, mask)
+            # val = self.broadcast_impl_shape(val, ptr.type.get_block_shapes())
+            if val is not None:
+                ptr, val = self.broadcast_impl_value(ptr, val)
             if mask is not None:
                 ptr, mask = self.broadcast_impl_value(ptr, mask)
-            val = self.broadcast_impl_shape(val, ptr.type.get_block_shapes())
 
         ptr_ty = ptr.type.scalar
         elt_ty = ptr_ty.element_ty
