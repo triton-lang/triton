c7411ed26933e236eeeb9f7eb27ccaaf489cb79a
Evghenii <1485713+3gx@users.noreply.github.com>
2025-11-07 15:12:30 -0800

    [WS] support ops annotations outside tt.ws loops (#8651)
    - annotates tmem .exit ops with partitions that matches .enter ops
    inside tt.ws loop
    - fix collectPartitions in insert-tmem-aref pass.
    - fix partition-loops to split code that is also outside tt.ws loop
    - update lit test
    
    these are changes necessary to support upcoming tt.ws nested loop
    feature

--- include/triton/Dialect/TritonGPU/IR/Dialect.h
+++ include/triton/Dialect/TritonGPU/IR/Dialect.h
@@ -303,6 +303,8 @@ SetVector<int> getPartitionIds(Operation *op);
 SmallVector<SetVector<int>, 4> getPartitionOutputs(Operation *op);
 SetVector<int> getPartitionIds(OpOperand *use);
 bool hasPartition(Operation *op);
+bool hasWarpSpecializeTag(Operation *op);
+std::optional<int> getWarpSpecializeTag(Operation *op);
 
 } // namespace mlir::triton::gpu
 
