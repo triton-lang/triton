de764abc7460818d15c377e87bb664ec4530a516
Jungwook Park <jungwook.park@amd.com>
2025-11-13 12:58:04 -0600

    Fix bad merge.

--- python/src/gluon_ir.cc
+++ python/src/gluon_ir.cc
@@ -860,13 +860,11 @@ void init_gluon_ir(py::module &&m) {
       .def("create_lds_barrier_arrive",
            [](GluonOpBuilder &self, Value memDesc, int count) -> Value {
              return self.create<ttag::ArriveBarrierOp>(memDesc, count);
+           })
+      .def("create_warp_pipeline_border", [](GluonOpBuilder &self) {
+           auto border = self.create<ROCDL::SchedBarrier>(0);
+           border->setAttr("triton.warp_pipeline.border", self.getBuilder().getUnitAttr());
            });
-  .def("create_warp_pipeline_border", [](GluonOpBuilder &self) {
-    auto border = self.create<ROCDL::SchedBarrier>(0);
-    border->setAttr("triton.warp_pipeline.border",
-                    self.getBuilder().getUnitAttr());
-  });
-  ;
 
   m.def(
       "compute_tmem_reg_layout",
