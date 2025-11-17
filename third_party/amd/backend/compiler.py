49e174c6856aed1d36b85fb2b398ffaa32a80aa8
Pengzhan Zhao <borontion@gmail.com>
2025-11-17 14:19:55 -0800

    [AMD][GLUON] Initial support for host-side tdm descriptor  (#8722)
    This PR adds initial support for host-side tdm descriptor.
    Implementation follows host-side tma descriptor. Currently only support
    in gluon and limited top 2D tdm descriptor.

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -262,6 +262,7 @@ class HIPBackend(BaseBackend):
         passes.common.add_cse(pm)
         passes.common.add_symbol_dce(pm)
         pm.run(mod, 'make_ttgir')
+        metadata["tensordesc_meta"] = mod.get_tensordesc_metadata()
         return mod
 
     @staticmethod
@@ -278,6 +279,7 @@ class HIPBackend(BaseBackend):
         passes.ttgpuir.add_combine_tensor_select_and_if(pm)
 
         pm.run(mod, 'gluon_to_ttgir')
+        metadata["tensordesc_meta"] = mod.get_tensordesc_metadata()
         return mod
 
     @staticmethod
