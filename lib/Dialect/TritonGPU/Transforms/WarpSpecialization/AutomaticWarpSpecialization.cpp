61491ebf03f67412e256c3fdd153f76ee5235cd5
Masahiro Masuda <masahi129@gmail.com>
2025-11-18 18:54:25 +0900

    move hoist tmem alloc code into its own pass

--- lib/Dialect/TritonGPU/Transforms/WarpSpecialization/AutomaticWarpSpecialization.cpp
+++ lib/Dialect/TritonGPU/Transforms/WarpSpecialization/AutomaticWarpSpecialization.cpp
@@ -63,6 +63,7 @@ void AutomaticWarpSpecialization::runOnOperation() {
   OpPassManager pm;
   pm.addPass(createTritonGPUPartitionScheduling());
   pm.addPass(createNVWSInsertAref());
+  pm.addPass(createNVWSHoistTmemStore());
   pm.addPass(createNVWSInsertTmemAref());
   // `int-range-optimizations` and SCCP are good at cleaning up loop arithmetic.
   // FIXME: Re-enable integer range analysis once it is fixed.
