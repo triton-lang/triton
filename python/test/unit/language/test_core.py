e8bec00917e5e75ca8fd7f456f4c8f33a3997ad3
Saeid Rostami <srostami@amd.com>
2025-11-19 23:49:05 +0000

    Fix multidim failure on RDNA

--- python/test/unit/language/test_core.py
+++ python/test/unit/language/test_core.py
@@ -6693,4 +6693,4 @@ def test_dot_multidim(rank, trans_a, trans_b, device):
 
     d = a.to(torch.float32) @ b.to(torch.float32)
 
-    assert torch.equal(c, d)
+    assert torch.allclose(c, d, rtol=1e-3, atol=1e-2)
