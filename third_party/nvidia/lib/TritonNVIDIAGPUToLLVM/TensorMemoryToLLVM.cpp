81ce8a298fc5376313ea732d9ad076be2df7e3b3
Masahiro Masuda <mmasuda@nvidia.com>
2025-11-20 08:33:47 +0000

    working

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TensorMemoryToLLVM.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TensorMemoryToLLVM.cpp
@@ -654,7 +654,8 @@ struct MemDescIndexOpConversion
     auto dstTy = op.getResult().getType();
     auto llvmElemTy = getTypeConverter()->convertType(srcTy.getElementType());
 
-    if (!isa<triton::nvidia_gpu::TensorMemoryEncodingAttr>(
+    if (!isa<triton::nvidia_gpu::TensorMemoryEncodingAttr,
+             triton::nvidia_gpu::TensorMemoryScalesEncodingAttr>(
             srcTy.getEncoding())) {
       return failure();
     }
