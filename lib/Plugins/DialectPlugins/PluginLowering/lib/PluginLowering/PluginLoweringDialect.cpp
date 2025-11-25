1839cedfff77fab2163b435655f3e6f7a37fa8dd
Puyan Lotfi <puyan@puyan.org>
2025-11-25 15:29:44 -0800

    populate pass plugin api into this library for dialect plugins

--- lib/Plugins/DialectPlugins/PluginLowering/lib/PluginLowering/PluginLoweringDialect.cpp
+++ lib/Plugins/DialectPlugins/PluginLowering/lib/PluginLowering/PluginLoweringDialect.cpp
@@ -31,21 +31,57 @@ void PluginLoweringDialect::initialize() {
 
 using namespace mlir;
 
+static void addTritonPluginPass(mlir::PassManager *pm) {
+  pm->addPass(mlir::pluginlowering::createPluginLoweringSwitchBarFoo());
+}
+
+static void registerTritonPluginPass() {
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return mlir::pluginlowering::createPluginLoweringSwitchBarFoo();
+  });
+}
+
+static const char *ADD_PLUGIN_PASS_NAME = "pluginlowering_fooop_inserter";
+static std::unordered_map<std::string, void (*)(mlir::PassManager *)> passMap =
+    {{ADD_PLUGIN_PASS_NAME, addTritonPluginPass}};
+static std::unordered_map<std::string, void (*)()> registryMap = {
+    {ADD_PLUGIN_PASS_NAME, registerTritonPluginPass}};
+static std::vector<const char *> passNamesTable = {ADD_PLUGIN_PASS_NAME};
+
 // Key APIs:
 
 TRITON_PLUGIN_API
 tritonAddPluginPass(mlir::PassManager *pm, const char *passName) {
-  return TP_GENERIC_FAILURE;
+  std::string passNameStr(passName);
+  if (passMap.find(passNameStr) == passMap.end())
+    return TP_GENERIC_FAILURE;
+  passMap[passNameStr](pm);
+  return TP_SUCCESS;
 }
 
 TRITON_PLUGIN_API
 tritonRegisterPluginPass(const char *passName) {
-  return TP_GENERIC_FAILURE;
+  std::string passNameStr(passName);
+  if (registryMap.find(passNameStr) == registryMap.end())
+    return TP_GENERIC_FAILURE;
+  registryMap[passNameStr]();
+  return TP_SUCCESS;
 }
 
 TRITON_PLUGIN_API
 tritonEnumeratePluginPasses(uint32_t *passCount, const char **passNames) {
-  *passCount = 0;
+  if (!passCount)
+    return TP_GENERIC_FAILURE;
+  auto count = passMap.size();
+  assert(count == registryMap.size() &&
+         "Expected register and add passes map size to match");
+  *passCount = count;
+  if (!passNames)
+    return TP_SUCCESS;
+  unsigned i = 0;
+  for (auto passName : passNamesTable) {
+    passNames[i] = passName;
+  }
   return TP_SUCCESS;
 }
 
