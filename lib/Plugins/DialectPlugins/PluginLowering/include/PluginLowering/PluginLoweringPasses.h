843d4433de1caeb6a23c91e65449d76a374ae6b4
Puyan Lotfi <puyan@puyan.org>
2025-11-25 13:58:48 -0800

    Move dialect plugin under lib/Plugins

--- lib/Plugins/DialectPlugins/PluginLowering/include/PluginLowering/PluginLoweringPasses.h
+++ lib/Plugins/DialectPlugins/PluginLowering/include/PluginLowering/PluginLoweringPasses.h
@@ -1,11 +1,3 @@
-//===- PluginLoweringPasses.h - PluginLowering passes  ------------------*- C++
-//-*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
 #ifndef PLUGINLOWERING_PLUGINLOWERINGPASSES_H
 #define PLUGINLOWERING_PLUGINLOWERINGPASSES_H
 
