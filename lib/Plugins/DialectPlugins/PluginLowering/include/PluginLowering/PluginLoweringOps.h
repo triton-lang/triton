2342558c6286160db36b9f436368a24919bc5e18
Puyan Lotfi <puyan@puyan.org>
2025-11-25 13:58:48 -0800

    Move dialect plugin under lib/Plugins

--- lib/Plugins/DialectPlugins/PluginLowering/include/PluginLowering/PluginLoweringOps.h
+++ lib/Plugins/DialectPlugins/PluginLowering/include/PluginLowering/PluginLoweringOps.h
@@ -1,12 +1,3 @@
-//===- PluginLoweringOps.h - PluginLowering dialect ops -----------------*- C++
-//-*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #ifndef PLUGINLOWERING_PLUGINLOWERINGOPS_H
 #define PLUGINLOWERING_PLUGINLOWERINGOPS_H
 
--- lib/Plugins/DialectPlugins/PluginLowering/lib/PluginLowering/PluginLoweringPasses.cpp
+++ lib/Plugins/DialectPlugins/PluginLowering/lib/PluginLowering/PluginLoweringPasses.cpp
@@ -1,11 +1,3 @@
-//===- PluginLoweringPasses.cpp - PluginLowering passes -----------------*- C++
-//-*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
 #include "PluginLowering/PluginLoweringDialect.h"
 #include "PluginLowering/PluginLoweringOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
