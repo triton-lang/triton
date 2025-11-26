a2b00dcccdd0d4f64dd100adb4f646c48d6d1aa1
Peter Bell <peterbell10@openai.com>
2025-11-26 12:23:59 +0000

    [Frontend] Avoid using find_paths_if

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -188,7 +188,11 @@ class constexpr_type(base_type):
         return hash(self.value)
 
     def mangle(self) -> str:
-        return repr(self)
+        if hasattr(self.value, "mangle"):
+            val = self.value.mangle()
+        else:
+            val = repr(self.value)
+        return f"c{val}"
 
     def _flatten_ir_types(self, builder: ir.builder, out: List[ir.type]) -> None:
         return
