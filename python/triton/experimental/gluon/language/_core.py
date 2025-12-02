454ec9eea6cf6deab971ed66dad45d957c561fe4
peterbell10 <peterbell10@openai.com>
2025-12-02 21:50:56 +0000

    [Frontend] Avoid using find_paths_if (#8846)
    This reduces the overhead from calling `find_paths_if` several times
    whenever we call a jit function.
    
    Along the way it also fixes a few issues with name mangling as we now go
    through the `constexpr.mangle()` path instead of converting the value to
    a string.

--- python/triton/experimental/gluon/language/_core.py
+++ python/triton/experimental/gluon/language/_core.py
@@ -216,7 +216,8 @@ class shared_memory_descriptor_type(base_type):
 
     def mangle(self) -> str:
         shape_str = "_".join([str(s) for s in self.shape])
-        return f"MD{self.element_ty.mangle()}S{shape_str}SL{self.layout.mangle()}LAS{self.alloc_shape}ASMD"
+        alloc_shape_str = "_".join([str(s) for s in self.alloc_shape])
+        return f"MD{self.element_ty.mangle()}S{shape_str}SL{self.layout.mangle()}LAS{alloc_shape_str}ASMD"
 
 
 class shared_memory_descriptor(base_value):
