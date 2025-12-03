0ff7a35cb35a6799376564c1f73e30252cb75df4
Joshua James Venter <venter.joshua@gmail.com>
2025-12-03 17:16:56 +0200

    [FRONTEND] Add get_int_attr to Operation for out-of-tree walk
    Enables extracting integer attributes from MLIR operations during
    out-of-tree walks. Useful for extracting constant values as well
    as attributes like start and end from tt.make_range, for example.
    
    Follows the pattern established in PR #3191.
    
    Signed-off-by: Joshua James Venter <venter.joshua@gmail.com>

--- python/src/ir.cc
+++ python/src/ir.cc
@@ -635,6 +635,13 @@ void init_triton_ir(py::module &&m) {
                return py::none();
              return py::str(ret.getValue().str());
            })
+      .def("get_int_attr",
+           [](Operation &self, const std::string &name) -> py::object {
+             auto ret = self.getAttrOfType<IntegerAttr>(name);
+             if (!ret)
+               return py::none();
+             return py::int_(ret.getInt());
+           })
       .def("get_bool_attr",
            [](Operation &self, const std::string &name) -> py::object {
              auto ret = self.getAttrOfType<BoolAttr>(name);
