fce1d54f9647aecd4f3d5067daa05a4ce56a0b7b
Corbin Robeck <robeck@meta.com>
2025-12-02 17:59:25 -0800

    remote inserter rewrite pass

--- lib/Plugins/DialectPlugins/LoweringDialectPlugin/lib/LoweringDialectPlugin/LoweringDialectPluginDialect.cpp
+++ lib/Plugins/DialectPlugins/LoweringDialectPlugin/lib/LoweringDialectPlugin/LoweringDialectPluginDialect.cpp
@@ -33,18 +33,18 @@ using namespace mlir;
 
 static void addTritonPluginPass(mlir::PassManager *pm) {
   pm->addPass(mlir::triton::loweringdialectplugin::
-                  createLoweringDialectPluginMagicOpInserter());
+                  createLoweringDialectPluginMagicOp());
 }
 
 static void registerTritonPluginPass() {
   ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
     return mlir::triton::loweringdialectplugin::
-        createLoweringDialectPluginMagicOpInserter();
+        createLoweringDialectPluginMagicOp();
   });
 }
 
 static const char *ADD_PLUGIN_PASS_NAME =
-    "loweringdialectplugin_magicop_inserter";
+    "loweringdialectplugin_magicop";
 static std::unordered_map<std::string, void (*)(mlir::PassManager *)> passMap =
     {{ADD_PLUGIN_PASS_NAME, addTritonPluginPass}};
 static std::unordered_map<std::string, void (*)()> registryMap = {
