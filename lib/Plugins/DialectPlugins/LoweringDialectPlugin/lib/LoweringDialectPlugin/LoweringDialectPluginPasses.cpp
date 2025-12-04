56fd7af7fd6f6115cb0bef10ffa754ed376b681f
Corbin Robeck <robeck@meta.com>
2025-12-03 16:19:59 -0800

    general clean up

--- lib/Plugins/DialectPlugins/LoweringDialectPlugin/lib/LoweringDialectPlugin/LoweringDialectPluginPasses.cpp
+++ lib/Plugins/DialectPlugins/LoweringDialectPlugin/lib/LoweringDialectPlugin/LoweringDialectPluginPasses.cpp
@@ -5,76 +5,49 @@
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "LoweringDialectPlugin/LoweringDialectPluginPasses.h"
 
 namespace mlir::triton::loweringdialectplugin {
-#define GEN_PASS_DEF_LOWERINGDIALECTPLUGINMAGICOPINSERTER
 #define GEN_PASS_DEF_LOWERINGDIALECTPLUGINMAGICOP
 #include "LoweringDialectPlugin/LoweringDialectPluginPasses.h.inc"
 
 namespace {
-// class LoweringDialectPluginMagicOpInserterRewriter
-//     : public OpRewritePattern<func::FuncOp> {
-// public:
-//   using OpRewritePattern<func::FuncOp>::OpRewritePattern;
-//   LogicalResult matchAndRewrite(func::FuncOp op,
-//                                 PatternRewriter &rewriter) const final {
-//     // Note: This is what I want this pass to do, no luck so far:
-//     // auto magicOp = mlir::triton::loweringdialectplugin::MagicOp::create(
-//     //     builder, op.getLoc(), op.getOpResult(0));
-//     // op.replaceAllUsesWith(magicOp);
-//     if (op.getSymName() == "bar") {
-//       rewriter.modifyOpInPlace(op, [&op]() { op.setSymName("foo"); });
-//       return success();
-//     }
-//     return failure();
-//   }
-// };
 
-class LoweringDialectPluginMagicOpRewriter
-    : public OpRewritePattern<mlir::triton::loweringdialectplugin::MagicOp> {
-public:
-  using OpRewritePattern<
-      mlir::triton::loweringdialectplugin::MagicOp>::OpRewritePattern;
-  LogicalResult matchAndRewrite(mlir::triton::loweringdialectplugin::MagicOp op,
-                                PatternRewriter &rewriter) const final {
-    auto a = op.getInput();
-    auto newOp = arith::AddIOp::create(rewriter, op.getLoc(), a, a);
-    op->replaceAllUsesWith(newOp);
-    return success();
-  }
-};
 
-// class LoweringDialectPluginMagicOpInserter
-//     : public impl::LoweringDialectPluginMagicOpInserterBase<
-//           LoweringDialectPluginMagicOpInserter> {
-// public:
-//   using impl::LoweringDialectPluginMagicOpInserterBase<
-//       LoweringDialectPluginMagicOpInserter>::LoweringDialectPluginMagicOpInserterBase;
-//   void runOnOperation() final {
-//     RewritePatternSet patterns(&getContext());
-//     patterns.add<LoweringDialectPluginMagicOpInserterRewriter>(&getContext());
-//     FrozenRewritePatternSet patternSet(std::move(patterns));
-//     if (failed(applyPatternsGreedily(getOperation(), patternSet)))
-//       signalPassFailure();
-//   }
-// };
+LogicalResult PluginMagicOpLowering(FunctionOpInterface func) {
+  MLIRContext *context = func.getContext();
+  Location loc = func->getLoc();
+  OpBuilder builder(context);
+  func.walk([&](mlir::triton::loweringdialectplugin::MagicOp op) {
+      builder.setInsertionPoint(op);
+      auto a = op.getInput();
+      auto newOp = arith::AddIOp::create(builder, loc, a, a);
+      op->erase();
+  });
+  return success();
+}
 
-class LoweringDialectPluginMagicOp
-    : public impl::LoweringDialectPluginMagicOpBase<
-          LoweringDialectPluginMagicOp> {
-public:
-  using impl::LoweringDialectPluginMagicOpBase<
-      LoweringDialectPluginMagicOp>::LoweringDialectPluginMagicOpBase;
-  void runOnOperation() final {
-    RewritePatternSet patterns(&getContext());
-    patterns.add<LoweringDialectPluginMagicOpRewriter>(&getContext());
-    FrozenRewritePatternSet patternSet(std::move(patterns));
-    if (failed(applyPatternsGreedily(getOperation(), patternSet)))
-      signalPassFailure();
-  }
-};
 } // namespace
+
+class LoweringDialectPluginMagicOpPass
+    : public impl::LoweringDialectPluginMagicOpBase<
+          LoweringDialectPluginMagicOpPass> {
+public:
+  void runOnOperation() override {
+    ModuleOp m = getOperation();
+    Location loc = m->getLoc();
+    FunctionOpInterface func = *m.getOps<FunctionOpInterface>().begin();
+    if (failed(PluginMagicOpLowering(func))) {
+      signalPassFailure();
+    }
+  }
+};
+
+std::unique_ptr<OperationPass<ModuleOp>> createLoweringDialectPluginMagicOpPass() {
+  return std::make_unique<LoweringDialectPluginMagicOpPass>();
+}
+
 } // namespace mlir::triton::loweringdialectplugin
