afec0f75f8230bb4734ee5901c28a7f338efcacd
Corbin Robeck <robeck@meta.com>
2025-12-03 16:39:59 -0800

    more clean up

--- lib/Plugins/DialectPlugins/LoweringDialectPlugin/lib/LoweringDialectPlugin/LoweringDialectPluginOps.cpp
+++
@@ -1,5 +0,0 @@
-#include "LoweringDialectPlugin/LoweringDialectPluginOps.h"
-#include "LoweringDialectPlugin/LoweringDialectPluginDialect.h"
-
-#define GET_OP_CLASSES
-#include "LoweringDialectPlugin/LoweringDialectPluginOps.cpp.inc"
