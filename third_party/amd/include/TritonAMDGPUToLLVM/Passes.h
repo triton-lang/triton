dae8de8513f6ffda2355062c4357f943e00a8f8d
PMylon <pmylon20@gmail.com>
2025-12-09 20:07:59 +0000

    [AMD] Support warp specialization on gfx1250

--- third_party/amd/include/TritonAMDGPUToLLVM/Passes.h
+++ third_party/amd/include/TritonAMDGPUToLLVM/Passes.h
@@ -24,6 +24,8 @@ namespace mlir::triton {
 } // namespace mlir::triton
 
 namespace mlir::triton::AMD {
+std::unique_ptr<OperationPass<ModuleOp>>
+createTritonAMDGPUConvertWarpSpecializeToLLVMPass(StringRef arch);
 void runScalarizePackedFOpsPass(llvm::Function &F);
 
 } // namespace mlir::triton::AMD
