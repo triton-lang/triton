df55eb7210082f7973f6e0b5d5a3caba4df73064
Thomas Raoux <thomas.raoux@openai.com>
2025-12-09 23:44:43 -0800

    Revert "[FRONTEND] Add optional device arg to preload (#8951)"
    This reverts commit b9e6768541c07bf1060d1d014f3fa0be02e7b78b.

--- python/triton/runtime/jit.py
+++ python/triton/runtime/jit.py
@@ -795,11 +795,10 @@ class JITFunction(JITCallable, KernelInterface[T]):
         # Hooks that will be called prior to executing "run"
         self.pre_run_hooks = []
 
-    def preload(self, specialization_data, device=None):
+    def preload(self, specialization_data):
         import json
         import triton.language as tl
-        if device is None:
-            device = driver.active.get_current_device()
+        device = driver.active.get_current_device()
         deserialized_obj = json.loads(specialization_data)
         if deserialized_obj['name'] != self._fn_name:
             raise RuntimeError(
