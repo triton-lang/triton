f1d06ccac9ca218baa1b09e73ef7a6d8c75d71e5
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2025-12-12 14:00:22 -0800

    Disable SGPR preload for gfx1250

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -394,7 +394,8 @@ class HIPBackend(BaseBackend):
         # Hint the compiler that we'd like the firmware to set the kernel arguments
         # to user SGPRs so that the kernel does not need to s_load its arguments
         # from memory.
-        amd.set_all_fn_arg_inreg(fns[0])
+        if "gfx1250" not in options.arch:
+            amd.set_all_fn_arg_inreg(fns[0])
 
         if knobs.compilation.enable_asan:
             default_libdir = Path(__file__).parent / 'lib'
