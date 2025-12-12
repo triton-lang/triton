9a8f7fa5f7b30b51fbb48851b4d644418b059f14
Thomas Raoux <thomas.raoux@openai.com>
2025-12-12 11:45:57 -0800

    Revert "[AMD] Add Warp-Pipeline Support, Gluon and LLVM lowering. (#8586)"
    This reverts commit 4899090f2f6d87e981bffc4507da3e20c99d47ab.

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -99,7 +99,6 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::triton::registerAllocateAMDGPUSharedMemory();
   mlir::triton::registerConvertTritonAMDGPUToLLVM();
   mlir::triton::registerConvertBuiltinFuncToLLVM();
-  mlir::triton::registerConvertWarpPipeline();
 
   mlir::ub::registerConvertUBToLLVMInterface(registry);
   mlir::registerConvertNVVMToLLVMInterface(registry);
@@ -120,7 +119,6 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::registerTritonAMDGPUInThreadTranspose();
   mlir::registerTritonAMDGPUCoalesceAsyncCopy();
   mlir::registerTritonAMDGPUUpdateAsyncWaitCount();
-  mlir::registerTritonAMDGPUWarpPipeline();
   mlir::triton::registerTritonAMDGPUInsertInstructionSchedHints();
   mlir::triton::registerTritonAMDGPULowerInstructionSchedHints();
   mlir::registerTritonAMDFoldTrueCmpI();
