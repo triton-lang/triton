9868ce0ac7ef792716a3d143607841853dea68cc
Peter Bell <peterbell10@openai.com>
2025-12-16 19:55:58 +0000

    Use argument location

--- python/src/ir.cc
+++ python/src/ir.cc
@@ -205,8 +205,8 @@ py::list getTensorDescMetadata(ModuleOp &mod) {
   });
   assert(kernelFunc);
 
-  for (auto [i, argTy] : llvm::enumerate(kernelFunc.getArgumentTypes())) {
-    auto descTy = dyn_cast<TensorDescType>(argTy);
+  for (auto [i, arg] : llvm::enumerate(kernelFunc.getArguments())) {
+    auto descTy = dyn_cast<TensorDescType>(arg.getType());
     if (!descTy)
       continue;
 
@@ -216,8 +216,8 @@ py::list getTensorDescMetadata(ModuleOp &mod) {
     py::dict metadata;
     if (isa<ttg::NVMMASharedEncodingAttr>(encoding)) {
       auto mmaEncoding = dyn_cast<ttg::NVMMASharedEncodingAttr>(encoding);
-      auto swizzle = ttng::getTMASwizzleMode(mod.getOperation(), descTy);
-      auto elemType = ttng::getTMAElementType(mod.getOperation(), descTy);
+      auto swizzle = ttng::getTMASwizzleMode(arg.getLoc(), descTy);
+      auto elemType = ttng::getTMAElementType(arg.getLoc(), descTy);
       if (!swizzle.has_value() || !elemType.has_value())
         throw py::type_error("Invalid descriptor type");
       auto blockSize = ttng::getTMABlockShape(blockType, /*packedSize=*/false);
