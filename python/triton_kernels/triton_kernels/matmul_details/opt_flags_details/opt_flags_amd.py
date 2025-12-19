87a4f0cefffb0bbe26624775dc8d3044c917276d
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-12-19 11:23:54 +0000

    Fix block sizes

--- python/triton_kernels/triton_kernels/matmul_details/opt_flags_details/opt_flags_amd.py
+++ python/triton_kernels/triton_kernels/matmul_details/opt_flags_details/opt_flags_amd.py
@@ -14,14 +14,12 @@ def compute_block_nk(n, block_m, grid_m, num_xcds, lhs_dtype, rhs_dtype, precisi
         if n <= 128 and (n & (n - 1)) == 0:
             block_n = n
         else:
-            block_n = max(32, min(256, triton.next_power_of_2(grid_m * n * num_xcds // n_cu)))
+            max_n = 128 if get_cdna_version() == 4 else 256
+            block_n = max(32, min(max_n, triton.next_power_of_2(grid_m * n * num_xcds // n_cu)))
     elif block_m > 64:
         block_n = 256
     else:
         block_n = 128
-
-    if get_cdna_version() == 4 and block_m == 128:
-        block_n = 512
 
     # block_k needs to match the cacheline size (128B)
     block_k = int(128 // min(lhs_width, rhs_width))
