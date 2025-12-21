cfabf98689b653116d4e7427f4bcc3d875a5287f
lezcano <lezcano-93@hotmail.com>
2025-12-21 02:46:25 +0100

    fix diff

--- python/triton/experimental/gluon/__init__.py
+++ python/triton/experimental/gluon/__init__.py
@@ -1,6 +1,6 @@
-from ._runtime import constexpr_function, jit
-from triton.language.core import must_use_result
 from . import nvidia
 from . import amd
+from ._runtime import constexpr_function, jit
+from triton.language.core import must_use_result
 
 __all__ = ["constexpr_function", "jit", "must_use_result", "nvidia", "amd"]
