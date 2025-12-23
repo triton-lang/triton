cc9a14b6536150576ed17b8181f7e53a89ca8f7a
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-12-23 09:55:01 +0000

    Adjust more block sizes

--- python/triton_kernels/triton_kernels/matmul_details/opt_flags_details/opt_flags_amd.py
+++ python/triton_kernels/triton_kernels/matmul_details/opt_flags_details/opt_flags_amd.py
@@ -14,7 +14,7 @@ def compute_block_nk(n, block_m, grid_m, num_xcds, lhs_dtype, rhs_dtype, precisi
         if n <= 128 and (n & (n - 1)) == 0:
             block_n = n
         else:
-            max_n = 128 if get_cdna_version() == 4 else 256
+            max_n = 64 if get_cdna_version() == 4 else 256
             block_n = max(32, min(max_n, triton.next_power_of_2(grid_m * n * num_xcds // n_cu)))
     elif block_m > 64:
         block_n = 256
