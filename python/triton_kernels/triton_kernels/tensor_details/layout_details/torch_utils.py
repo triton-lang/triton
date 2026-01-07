4641010829bfb7dee8725a2e35efb69a18742d44
Phil Tillet <phil@openai.com>
2026-01-06 22:44:54 -0800

    .

--- python/triton_kernels/triton_kernels/tensor_details/layout_details/torch_utils.py
+++ python/triton_kernels/triton_kernels/tensor_details/layout_details/torch_utils.py
@@ -6,8 +6,8 @@ def unpack(data: torch.Tensor, dim: int, is_fp4: bool):
         return data
     if data.shape[dim] == 1:
         return data
-    data_lo = (data >> 0) & 0x0F
-    data_hi = (data >> 4) & 0x0F
+    data_lo = data & 0x0F
+    data_hi = data & 0xF0
     return torch.cat([data_lo, data_hi], dim=dim)
 
 
@@ -21,15 +21,13 @@ def pack(data: torch.Tensor, dim: int, is_fp4: bool):
     idx_hi = [slice(None)] * data.ndim
     idx_lo[dim] = slice(0, size)
     idx_hi[dim] = slice(size, 2 * size)
-    return data[tuple(idx_lo)] | (data[tuple(idx_hi)] << 4)
+    return data[tuple(idx_lo)] | data[tuple(idx_hi)]
 
 
 def repack(data: torch.Tensor, old_dim: int, new_dim: int, is_fp4: bool):
-    if not is_fp4:
-        return data
     old_dim %= data.ndim
     new_dim %= data.ndim
-    if old_dim == new_dim:
+    if not is_fp4 or old_dim == new_dim:
         return data
     tmp = unpack(data, old_dim, is_fp4)
     ret = pack(tmp, new_dim, is_fp4)
