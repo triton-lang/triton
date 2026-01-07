3f10c1d8ed6ef5822688047f29e6518f754ac7a4
Phil Tillet <phil@openai.com>
2026-01-06 16:23:20 -0800

    .

--- python/triton_kernels/triton_kernels/tensor_details/layout_details/torch_utils.py
+++ python/triton_kernels/triton_kernels/tensor_details/layout_details/torch_utils.py
@@ -1,61 +1,71 @@
 import torch
 import gc
 
-# def unpack(data: torch.Tensor, dim: int, is_fp4: bool):
-#     if not is_fp4:
-#         return data
-#     if data.shape[dim] == 1:
-#         return data
-#     data_lo = (data >> 0) & 0x0F
-#     data_hi = (data >> 4) & 0x0F
-#     return torch.cat([data_lo, data_hi], dim=dim)
 
-# def pack(data: torch.Tensor, dim: int, is_fp4: bool):
-#     if not is_fp4:
-#         return data
-#     if data.shape[dim] == 1:
-#         return data
-#     size = data.shape[dim] // 2
-#     idx_lo = [slice(None)] * data.ndim
-#     idx_hi = [slice(None)] * data.ndim
-#     idx_lo[dim] = slice(0, size)
-#     idx_hi[dim] = slice(size, 2 * size)
-#     data[tuple(idx_lo)] |= (data[tuple(idx_hi)] << 4)
-#     return data[tuple(idx_lo)]
+def unpack(data: torch.Tensor, dim: int, is_fp4: bool):
+    if not is_fp4:
+        return data
+    if data.shape[dim] == 1:
+        return data
+    data_lo = (data >> 0) & 0x0F
+    data_hi = (data >> 4) & 0x0F
+    return torch.cat([data_lo, data_hi], dim=dim)
 
-# def repack(data: torch.Tensor, old_dim: int, new_dim: int, is_fp4: bool):
-#     if not is_fp4:
-#         return data
-#     tmp = unpack(data, old_dim, is_fp4)
-#     ret = pack(tmp, new_dim, is_fp4)
-#     del tmp
-#     gc.collect()
-#     torch.cuda.empty_cache()
-#     return ret
+
+def pack(data: torch.Tensor, dim: int, is_fp4: bool):
+    if not is_fp4:
+        return data
+    if data.shape[dim] == 1:
+        return data
+    size = data.shape[dim] // 2
+    idx_lo = [slice(None)] * data.ndim
+    idx_hi = [slice(None)] * data.ndim
+    idx_lo[dim] = slice(0, size)
+    idx_hi[dim] = slice(size, 2 * size)
+    return data[tuple(idx_lo)] | (data[tuple(idx_hi)] << 4)
 
 
 def repack(data: torch.Tensor, old_dim: int, new_dim: int, is_fp4: bool):
     if not is_fp4:
         return data
-    if data.shape[old_dim] == 1 or data.shape[new_dim] == 1:
-        return data
-    # unpack data
-    data_lo = (data >> 0) & 0x0F
-    data_hi = (data >> 4) & 0x0F
-    tmp = torch.cat([data_lo, data_hi], dim=old_dim)
-    # free memory
-    del data_lo, data_hi
-    gc.collect()
-    torch.cuda.empty_cache()
-    # repack data
-    size = tmp.shape[new_dim] // 2
-    idx_lo = [slice(None)] * data.ndim
-    idx_hi = [slice(None)] * data.ndim
-    idx_lo[new_dim] = slice(0, size)
-    idx_hi[new_dim] = slice(size, 2 * size)
-    tmp[tuple(idx_lo)] |= (tmp[tuple(idx_hi)] << 4)
-    ret = tmp[tuple(idx_lo)].clone()
-    del tmp
-    gc.collect()
-    torch.cuda.empty_cache()
+    if data.ndim == 3:
+        ret = torch.empty_like(data)
+        assert old_dim % data.ndim != 0
+        assert new_dim % data.ndim != 0
+        for i in range(data.shape[0]):
+            tmp = unpack(data[i], old_dim, is_fp4)
+            ret[i] = pack(tmp, new_dim, is_fp4)
+    else:
+        tmp = unpack(data, old_dim, is_fp4)
+        ret = pack(tmp, new_dim, is_fp4)
+        del tmp
+        gc.collect()
+        torch.cuda.empty_cache()
     return ret
+
+
+# def repack(data: torch.Tensor, old_dim: int, new_dim: int, is_fp4: bool):
+#     if not is_fp4:
+#         return data
+#     if data.shape[old_dim] == 1 or data.shape[new_dim] == 1:
+#         return data
+#     # unpack data
+#     data_lo = (data >> 0) & 0x0F
+#     data_hi = (data >> 4) & 0x0F
+#     tmp = torch.cat([data_lo, data_hi], dim=old_dim)
+#     # free memory
+#     del data_lo, data_hi
+#     gc.collect()
+#     torch.cuda.empty_cache()
+#     # repack data
+#     size = tmp.shape[new_dim] // 2
+#     idx_lo = [slice(None)] * data.ndim
+#     idx_hi = [slice(None)] * data.ndim
+#     idx_lo[new_dim] = slice(0, size)
+#     idx_hi[new_dim] = slice(size, 2 * size)
+#     tmp[tuple(idx_lo)] |= (tmp[tuple(idx_hi)] << 4)
+#     ret = tmp[tuple(idx_lo)].clone()
+#     del tmp
+#     gc.collect()
+#     torch.cuda.empty_cache()
+#     return ret
