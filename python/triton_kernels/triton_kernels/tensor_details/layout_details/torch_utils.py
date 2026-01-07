4dd20fd2f84b5db6ff0cb458b75eb6fa7e65eff0
Phil Tillet <phil@openai.com>
2026-01-07 00:30:27 -0800

    .

--- python/triton_kernels/triton_kernels/tensor_details/layout_details/torch_utils.py
+++ python/triton_kernels/triton_kernels/tensor_details/layout_details/torch_utils.py
@@ -6,10 +6,17 @@ def unpack(data: torch.Tensor, dim: int, is_fp4: bool):
         return data
     if data.shape[dim] == 1:
         return data
-    data_lo = data & 0x0F
-    data_hi = data & 0xF0
-    data_hi >>= 4
-    return torch.cat([data_lo, data_hi], dim=dim)
+    ret_shape = list(data.shape)
+    ret_shape[dim] *= 2
+    ret = torch.empty(ret_shape, dtype=data.dtype, device=data.device)
+    idx_lo = [slice(None)] * data.ndim
+    idx_hi = [slice(None)] * data.ndim
+    idx_lo[dim] = slice(0, data.shape[dim])
+    idx_hi[dim] = slice(data.shape[dim], 2 * data.shape[dim])
+    ret[tuple(idx_lo)] = data & 0x0F
+    ret[tuple(idx_hi)] = data & 0xF0
+    ret[tuple(idx_hi)] >>= 4
+    return ret
 
 
 def pack(data: torch.Tensor, dim: int, is_fp4: bool):
@@ -22,7 +29,9 @@ def pack(data: torch.Tensor, dim: int, is_fp4: bool):
     idx_hi = [slice(None)] * data.ndim
     idx_lo[dim] = slice(0, size)
     idx_hi[dim] = slice(size, 2 * size)
-    return data[tuple(idx_lo)] | (data[tuple(idx_hi)] << 4)
+    out = (data[tuple(idx_hi)] << 4)
+    out |= data[tuple(idx_lo)]
+    return out
 
 
 def repack(data: torch.Tensor, old_dim: int, new_dim: int, is_fp4: bool):
