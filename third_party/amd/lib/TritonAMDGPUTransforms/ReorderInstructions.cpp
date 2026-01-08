d10b90c071fc09acc26a24ec961ced6a3ca71e9a
Volkan Keles <volkan.keles@amd.com>
2026-01-08 13:21:57 +0300

    [AMD] Add TritonAMDGPUSinkLayoutConversions pass
    Add a new pass to sink LayoutConversion operations instead of using
    ReorderInstructions pass to do that. Eventually, we want each pass
    to have a clear purpose and ability to run/test separately.

--- third_party/amd/lib/TritonAMDGPUTransforms/ReorderInstructions.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/ReorderInstructions.cpp
@@ -56,40 +56,9 @@ findEarlyInsertionPoint(Block *block, triton::LoadOp move) {
   return ipnt;
 }
 
-// Return the first user in the same block of the given op. If the user is in a
-// nested block then return the op owning the block. Return nullptr if not
-// existing.
-static Operation *getFirstUseInSameBlock(Operation *op) {
-  SmallVector<Operation *> usersInSameBlock;
-  for (auto user : op->getUsers()) {
-    if (Operation *ancestor = op->getBlock()->findAncestorOpInBlock(*user))
-      usersInSameBlock.push_back(ancestor);
-  }
-  auto minOpIt =
-      llvm::min_element(usersInSameBlock, [](Operation *a, Operation *b) {
-        return a->isBeforeInBlock(b);
-      });
-  return minOpIt != usersInSameBlock.end() ? *minOpIt : nullptr;
-}
-
 //===----------------------------------------------------------------------===//
 // Reorder mechanisms
 //===----------------------------------------------------------------------===//
-
-// Sink conversion after the last dealloc but before the first use in its block.
-// This helps to avoid unnecessary shared memory allocation.
-static void moveDownCoversion(triton::FuncOp funcOp) {
-  SmallVector<ttg::ConvertLayoutOp> convertOps;
-  funcOp.walk([&](ttg::ConvertLayoutOp op) { convertOps.push_back(op); });
-
-  for (auto op : convertOps) {
-    Operation *user = getFirstUseInSameBlock(op);
-    for (auto it = Block::iterator(op), ie = op->getBlock()->end();
-         it != ie && &*it != user; ++it)
-      if (isa<ttg::LocalDeallocOp>(&*it))
-        op->moveAfter(&*it);
-  }
-}
 
 // Move transpositions just after their definition.
 static void moveUpTranspose(triton::FuncOp funcOp) {
@@ -167,7 +136,6 @@ struct TritonAMDGPUReorderInstructionsPass
   void runOnOperation() override {
     ModuleOp m = getOperation();
     for (auto funcOp : m.getOps<triton::FuncOp>()) {
-      moveDownCoversion(funcOp);
       moveUpTranspose(funcOp);
       moveUpGlobalLoadInPrologue(funcOp);
     }
