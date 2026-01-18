19d8d8873368f90cc2b04c819d67a8494b248d87
lezcano <lezcano-93@hotmail.com>
2026-01-18 12:53:13 -0300

    [BACKEND] Support generic multi-cta convert_layouts
    We generalise the swizzling algorithm to work with blocks and generalise
    the most of the memory lowerings to support layouts with blocks.
    
    We remove the legacy lowering.
    
    The generic swizzling algorithm for blocks might be fine, but we didn't
    try to be super clever. There might be some perf left on the table. We
    can look into this at a later point if it becomes relevant.
    
    We also activate multi-cta reductions in the process and test both
    there.
    
    TODO: Add some funky tests that just test the `convert_layout`, not the
    `convert_layout` within the reduction.
    TODO: Check how to perform multiCTA barriers in AMD and perhaps merge
    cluster barriers into ttg.barrier, predicate broadcasting blocks, etc.
    
    stack-info: PR: https://github.com/triton-lang/triton/pull/9317, branch: lezcano/stack/9

--- include/triton/Conversion/TritonGPUToLLVM/TargetInfoBase.h
+++ include/triton/Conversion/TritonGPUToLLVM/TargetInfoBase.h
@@ -20,6 +20,8 @@ public:
   // target address space
   virtual void barrier(Location loc, RewriterBase &rewriter,
                        triton::gpu::AddrSpace targets) const = 0;
+  // Emit a cluster-level barrier when supported. Defaults to CTA barrier.
+  virtual void clusterBarrier(Location loc, RewriterBase &rewriter) const = 0;
   // Insert a warp syncronization barrier that also guarantees local address
   // space visibility at warp level when supported by the backend.
   // Backends that do not support warp-level barriers should conservatively
--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.h
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/TargetInfo.h
@@ -19,6 +19,7 @@ public:
 
   void barrier(Location loc, RewriterBase &rewriter,
                triton::gpu::AddrSpace targets) const override;
+  void clusterBarrier(Location loc, RewriterBase &rewriter) const override;
 
   void warpSync(Location loc, RewriterBase &rewriter) const override;
 
