c07ef8aff778d973c0d327068d1abbf617e778bf
Ravil Dorozhinskii <ravil.aviva.com@gmail.com>
2026-01-24 02:01:42 +0100

    Revert "[AMD][gfx1250] Added missing f64.16x16x4.f64 to wmma database" (#9291)
    This reverts https://github.com/triton-lang/triton/commit/53b0eafd76debe074965a5d751dd21c593097eb2.

--- third_party/amd/lib/TritonAMDGPUTransforms/WmmaGroup.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/WmmaGroup.cpp
@@ -72,7 +72,6 @@ WmmaDatabase::WmmaDatabase(MLIRContext *context) {
                       symbol2, k2, kBase2)
 
   Builder b(context);
-  auto f64T = b.getF64Type();
   auto f32T = b.getF32Type();
   auto f16T = b.getF16Type();
   auto bf16T = b.getBF16Type();
@@ -84,10 +83,6 @@ WmmaDatabase::WmmaDatabase(MLIRContext *context) {
   auto ocpBf8T = b.getType<Float8E5M2Type>();
 
   wmmaMap = {
-      // f64 inputs
-      TRITON_WMMA_v(3, 16, 16, f64T, f64T, 64, f64T,
-                    "llvm.amdgcn.wmma.f64.16x16x4.f64", 4, 1),
-
       // f32 inputs
       // wmma_f32_16x16x4_f32
       TRITON_WMMA_v(3, 16, 16, f32T, f32T, 32, f32T,
