adceb2b02a147d5fac6c3d90ef6a622b2e1818dc
Lei Zhang <antiagainst@gmail.com>
2026-02-01 18:06:34 +0000

    Disable test_hooks for CDNA2 for now

--- python/test/unit/runtime/test_autotuner.py
+++ python/test/unit/runtime/test_autotuner.py
@@ -6,7 +6,7 @@ import pytest
 
 import pathlib
 import uuid
-from triton._internal_testing import is_cuda
+from triton._internal_testing import is_cuda, is_hip_cdna2
 
 
 def do_bench(kernel_call, quantiles, use_cuda_graph=False):
@@ -84,6 +84,7 @@ def test_restore(pass_kwargs_to_kernel, device):
     triton.testing.assert_close(src, torch.ones_like(src))
 
 
+@pytest.mark.skipif(is_hip_cdna2(), reason="Hit LLVM assertion in splitLiveThroughBlock")
 def test_hooks(device):
     # Autotuner's pre- and post- hooks should be called the same number of times
     N = 4096
