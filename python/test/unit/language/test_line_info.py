8d3712b2ebfe6dc9bd26fbafbfe6fe9e63edb32e
Simon Waters <simon@kernelize.ai>
2026-02-09 19:41:20 -0600

    - revert line changes - self-contained tests for tensordesc (only on cuda) and namedtuples

--- python/test/unit/language/test_line_info.py
+++ python/test/unit/language/test_line_info.py
@@ -1,14 +1,13 @@
 import inspect
 import subprocess
 import tempfile
-from typing import NamedTuple
 
 import pytest
 import torch
 
 import triton
 import triton.language as tl
-from triton._internal_testing import is_interpreter
+from triton._internal_testing import is_interpreter, is_cuda
 from triton._filecheck import run_filecheck
 
 
@@ -159,24 +158,24 @@ def test_line_info(func: str):
 
     file_lines = extract_file_lines(command, anchor, separator, kernel_info.asm[obj_kind])
     if func == "single":
+        assert (check_file_lines(file_lines, "test_line_info.py", 16))
         assert (check_file_lines(file_lines, "test_line_info.py", 17))
-        assert (check_file_lines(file_lines, "test_line_info.py", 18))
     elif func == "call":
-        assert (check_file_lines(file_lines, "test_line_info.py", 28))
+        assert (check_file_lines(file_lines, "test_line_info.py", 27))
         assert (check_file_lines(file_lines, "test_line_info.py", 29))
     elif func == "call_noinline":
-        assert (check_file_lines(file_lines, "test_line_info.py", 42))
-        assert (check_file_lines(file_lines, "test_line_info.py", 35))
-        assert (check_file_lines(file_lines, "test_line_info.py", 35))
+        assert (check_file_lines(file_lines, "test_line_info.py", 41))
+        assert (check_file_lines(file_lines, "test_line_info.py", 34))
+        assert (check_file_lines(file_lines, "test_line_info.py", 34))
     elif func == "autotune":
+        assert (check_file_lines(file_lines, "test_line_info.py", 52))
         assert (check_file_lines(file_lines, "test_line_info.py", 53))
         assert (check_file_lines(file_lines, "test_line_info.py", 54))
-        assert (check_file_lines(file_lines, "test_line_info.py", 55))
     elif func == "dot_combine":
-        assert (check_file_lines(file_lines, "test_line_info.py", 65))
-        assert (check_file_lines(file_lines, "test_line_info.py", 66, should_contain=False))
+        assert (check_file_lines(file_lines, "test_line_info.py", 64))
+        assert (check_file_lines(file_lines, "test_line_info.py", 65, should_contain=False))
     elif func == "cdiv":
-        assert (check_file_lines(file_lines, "test_line_info.py", 75))
+        assert (check_file_lines(file_lines, "test_line_info.py", 74))
 
 
 @pytest.mark.interpreter
@@ -260,7 +259,7 @@ def test_use_name_loc_as_prefix(fresh_triton_cache):
 
     @triton.jit
     def kernel_basic(src, N, BLOCK_SIZE: tl.constexpr):
-        # CHECK: #loc = loc("{{.*}}":262:0)
+        # CHECK: #loc = loc("{{.*}}":261:0)
         # CHECK-LABEL:  tt.func public @kernel_basic(
         # CHECK-SAME:                                %src: !tt.ptr<f32> loc("src"(#loc)), %N: i32 loc("N"(#loc)))
         # CHECK:          %x_plus_1 = arith.constant dense<1.000000e+00> : tensor<16xf32> loc(#loc14)
@@ -326,12 +325,16 @@ def test_use_name_loc_as_prefix(fresh_triton_cache):
         # CHECK-SAME: %foo.stride.1: i64
         foo
 
-    h = triton.compile(
-        triton.compiler.ASTSource(fn=kernel_tensordesc_param, signature={"foo": "tensordesc<fp16[32,64]>"},
+    if is_cuda():
+        # CUDA backend creates aggregate tensor descriptor parameters
+        h = triton.compile(
+           triton.compiler.ASTSource(fn=kernel_tensordesc_param, signature={"foo": "tensordesc<fp16[32,64]>"},
                                   constexprs={}))
 
-    check_template = inspect.getsource(kernel_tensordesc_param.fn)
-    run_filecheck("placeholder", h.asm["ttir"], check_template)
+        check_template = inspect.getsource(kernel_tensordesc_param.fn)
+        run_filecheck("placeholder", h.asm["ttir"], check_template)
+
+    from typing import NamedTuple
 
     @triton.jit
     def kernel_tuple_param(foo):
