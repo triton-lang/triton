fdd4be19c5fc11a7ab38d1cf40fcc9deff692e70
Jungwook Park <jungwook.park@amd.com>
2026-02-10 16:08:57 +0000

    format

--- python/triton/experimental/gluon/language/amd/gfx1250/async_copy.py
+++ python/triton/experimental/gluon/language/amd/gfx1250/async_copy.py
@@ -4,8 +4,7 @@ from triton.experimental.gluon.language._layouts import DistributedLayout
 from ..cdna4.async_copy import commit_group, wait_group, load_shared_relaxed
 
 __all__ = [
-    "global_to_shared", "shared_to_global", "commit_group", "wait_group", "mbarrier_arrive",
-    "load_shared_relaxed"
+    "global_to_shared", "shared_to_global", "commit_group", "wait_group", "mbarrier_arrive", "load_shared_relaxed"
 ]
 
 
