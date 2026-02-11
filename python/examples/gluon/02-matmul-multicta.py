cfe2d866b4e0faa4a3def646b9a8ce8d8a93d094
lezcano <lezcano-93@hotmail.com>
2026-02-11 15:20:49 +0000

    implement CLC

--- python/examples/gluon/02-matmul-multicta.py
+++ python/examples/gluon/02-matmul-multicta.py
@@ -10,6 +10,7 @@ from triton.experimental.gluon import language as gl
 from triton.experimental.gluon.language.nvidia.blackwell import (
     TensorMemoryLayout,
     allocate_tensor_memory,
+    clc,
     get_tmem_reg_layout,
     tcgen05_commit,
     tcgen05_mma,
@@ -132,94 +133,6 @@ def _planar_snake(lin_idx, m_tiles, n_tiles, minor_dim: gl.constexpr, tile_width
 
 
 @aggregate
-class PersistentTileScheduler:
-    pid_start: gl.tensor
-    num_pid: gl.tensor
-    num_pid_m: gl.tensor
-    num_pid_n: gl.tensor
-    TILE_M: gl.constexpr
-    TILE_N: gl.constexpr
-    MINOR_DIM: gl.constexpr
-    GRID_TILE_WIDTH: gl.constexpr
-
-    @gluon.constexpr_function
-    def __init__(self, pid_start, num_pid, num_pid_m, num_pid_n, TILE_M, TILE_N, MINOR_DIM, GRID_TILE_WIDTH):
-        self.pid_start = pid_start
-        self.num_pid = num_pid
-        self.num_pid_m = num_pid_m
-        self.num_pid_n = num_pid_n
-        self.TILE_M = gl.constexpr(TILE_M)
-        self.TILE_N = gl.constexpr(TILE_N)
-        self.MINOR_DIM = gl.constexpr(MINOR_DIM)
-        self.GRID_TILE_WIDTH = gl.constexpr(GRID_TILE_WIDTH)
-
-    @gluon.jit
-    def initialize(M, N, TILE_M: gl.constexpr, TILE_N: gl.constexpr, MINOR_DIM: gl.constexpr,
-                   GRID_TILE_WIDTH: gl.constexpr):
-        pid_start = gl.program_id(axis=0)
-        num_pid_m = gl.cdiv(M, TILE_M)
-        num_pid_n = gl.cdiv(N, TILE_N)
-        num_pid = num_pid_m * num_pid_n
-        return PersistentTileScheduler(pid_start, num_pid, num_pid_m, num_pid_n, TILE_M, TILE_N, MINOR_DIM,
-                                       GRID_TILE_WIDTH)
-
-    @gluon.jit
-    def get_num_tiles(self):
-        return gl.cdiv(self.num_pid - self.pid_start, gl.num_programs(axis=0))
-
-    @gluon.jit
-    def get_offsets(self, idx):
-        tile_id = self.pid_start + idx * gl.num_programs(axis=0)
-        pid_m, pid_n = _planar_snake(tile_id, self.num_pid_m, self.num_pid_n, self.MINOR_DIM, self.GRID_TILE_WIDTH)
-        return pid_m * self.TILE_M, pid_n * self.TILE_N
-
-
-@aggregate
-class PartitionArgs:
-    a_desc: tma.tensor_descriptor
-    b_desc: tma.tensor_descriptor
-    c_desc: tma.tensor_descriptor
-    a_bufs: gl.shared_memory_descriptor
-    b_bufs: gl.shared_memory_descriptor
-    load_empty_bars: gl.shared_memory_descriptor
-    load_ready_bars: gl.shared_memory_descriptor
-    acc_bufs: tensor_memory_descriptor
-    acc_empty_bars: gl.shared_memory_descriptor
-    acc_ready_bars: gl.shared_memory_descriptor
-    MINOR_DIM: gl.constexpr
-    GRID_TILE_WIDTH: gl.constexpr
-    TWO_CTAS: gl.constexpr
-
-    @gluon.constexpr_function
-    def __init__(self, a_desc, b_desc, c_desc, a_bufs, b_bufs, load_empty_bars, load_ready_bars, acc_bufs,
-                 acc_empty_bars, acc_ready_bars, MINOR_DIM, GRID_TILE_WIDTH, TWO_CTAS):
-        self.a_desc = a_desc
-        self.b_desc = b_desc
-        self.c_desc = c_desc
-        self.a_bufs = a_bufs
-        self.b_bufs = b_bufs
-        self.load_empty_bars = load_empty_bars
-        self.load_ready_bars = load_ready_bars
-        self.acc_bufs = acc_bufs
-        self.acc_empty_bars = acc_empty_bars
-        self.acc_ready_bars = acc_ready_bars
-        self.MINOR_DIM = gl.constexpr(MINOR_DIM)
-        self.GRID_TILE_WIDTH = gl.constexpr(GRID_TILE_WIDTH)
-        self.TWO_CTAS = gl.constexpr(TWO_CTAS)
-
-    @gluon.jit
-    def get_scheduler(self):
-        return PersistentTileScheduler.initialize(
-            self.c_desc.shape[0],
-            self.c_desc.shape[1],
-            self.a_desc.block_shape[0],
-            self.b_desc.block_shape[1],
-            self.MINOR_DIM,
-            self.GRID_TILE_WIDTH,
-        )
-
-
-@aggregate
 class Counter:
     index: gl.tensor
     phase: gl.tensor
@@ -245,43 +158,215 @@ class Counter:
         return Counter(index, phase, self.num_barriers)
 
 
+@aggregate
+class ClcTileSchedulerConsumer:
+    has_work: gl.tensor
+    tile_id: gl.tensor
+    num_pid_m: gl.tensor
+    num_pid_n: gl.tensor
+    TILE_M: gl.constexpr
+    TILE_N: gl.constexpr
+    MINOR_DIM: gl.constexpr
+    GRID_TILE_WIDTH: gl.constexpr
+    clc_result_buffers: gl.shared_memory_descriptor
+    clc_barriers: gl.shared_memory_descriptor
+    clc_consumed_bars: gl.shared_memory_descriptor
+    counter: Counter
+    consumed_counter: Counter
+
+    @gluon.constexpr_function
+    def __init__(self, has_work, tile_id, num_pid_m, num_pid_n, TILE_M, TILE_N, MINOR_DIM, GRID_TILE_WIDTH,
+                 clc_result_buffers, clc_barriers, clc_consumed_bars, counter, consumed_counter):
+        self.has_work = has_work
+        self.tile_id = tile_id
+        self.num_pid_m = num_pid_m
+        self.num_pid_n = num_pid_n
+        self.TILE_M = gl.constexpr(TILE_M)
+        self.TILE_N = gl.constexpr(TILE_N)
+        self.MINOR_DIM = gl.constexpr(MINOR_DIM)
+        self.GRID_TILE_WIDTH = gl.constexpr(GRID_TILE_WIDTH)
+        self.clc_result_buffers = clc_result_buffers
+        self.clc_barriers = clc_barriers
+        self.clc_consumed_bars = clc_consumed_bars
+        self.counter = counter
+        self.consumed_counter = consumed_counter
+
+    @gluon.jit
+    def initialize(M, N, TILE_M: gl.constexpr, TILE_N: gl.constexpr, MINOR_DIM: gl.constexpr,
+                   GRID_TILE_WIDTH: gl.constexpr, clc_result_buffers, clc_barriers, clc_consumed_bars):
+        tile_id = gl.program_id(axis=0)
+        num_pid_m = gl.cdiv(M, TILE_M)
+        num_pid_n = gl.cdiv(N, TILE_N)
+        has_work = gl.to_tensor(True)
+        counter = Counter.create(0, clc_barriers.shape[0])
+        consumed_counter = Counter.create(0, clc_barriers.shape[0])
+        return ClcTileSchedulerConsumer(
+            has_work,
+            tile_id,
+            num_pid_m,
+            num_pid_n,
+            TILE_M,
+            TILE_N,
+            MINOR_DIM,
+            GRID_TILE_WIDTH,
+            clc_result_buffers,
+            clc_barriers,
+            clc_consumed_bars,
+            counter,
+            consumed_counter,
+        )
+
+    @gluon.jit
+    def get_offsets(self):
+        pid_m, pid_n = _planar_snake(self.tile_id, self.num_pid_m, self.num_pid_n, self.MINOR_DIM, self.GRID_TILE_WIDTH)
+        return pid_m * self.TILE_M, pid_n * self.TILE_N
+
+    @gluon.jit
+    def step(self, iteration):
+        # The 0-th iteration uses the program_id as the tile_id.
+        # At the end of each iteration we prefetch the next tile.
+        # As such we must signal the consumed slot at the end of
+        # each iteration skipping the first one.
+        consumed_counter = self.consumed_counter
+        if iteration > 0:
+            mbarrier.arrive(self.clc_consumed_bars.index(consumed_counter.index))
+            consumed_counter = consumed_counter.next()
+        counter = self.counter
+        barrier = self.clc_barriers.index(counter.index)
+        result = self.clc_result_buffers.index(counter.index)
+        mbarrier.wait(barrier, counter.phase)
+        clc_res = clc.load_result(result)
+        has_work = clc_res.is_canceled()
+        tile_id = self.tile_id
+        if has_work:
+            tile_id = clc_res.program_id(0)
+        return ClcTileSchedulerConsumer(
+            has_work,
+            tile_id,
+            self.num_pid_m,
+            self.num_pid_n,
+            self.TILE_M,
+            self.TILE_N,
+            self.MINOR_DIM,
+            self.GRID_TILE_WIDTH,
+            self.clc_result_buffers,
+            self.clc_barriers,
+            self.clc_consumed_bars,
+            counter.next(),
+            consumed_counter,
+        )
+
+
+@aggregate
+class PartitionArgs:
+    a_desc: tma.tensor_descriptor
+    b_desc: tma.tensor_descriptor
+    c_desc: tma.tensor_descriptor
+    a_bufs: gl.shared_memory_descriptor
+    b_bufs: gl.shared_memory_descriptor
+    load_empty_bars: gl.shared_memory_descriptor
+    load_ready_bars: gl.shared_memory_descriptor
+    acc_bufs: tensor_memory_descriptor
+    acc_empty_bars: gl.shared_memory_descriptor
+    acc_ready_bars: gl.shared_memory_descriptor
+    clc_result_buffers: gl.shared_memory_descriptor
+    clc_barriers: gl.shared_memory_descriptor
+    clc_consumed_bars: gl.shared_memory_descriptor
+    MINOR_DIM: gl.constexpr
+    GRID_TILE_WIDTH: gl.constexpr
+    TWO_CTAS: gl.constexpr
+
+    @gluon.constexpr_function
+    def __init__(self, a_desc, b_desc, c_desc, a_bufs, b_bufs, load_empty_bars, load_ready_bars, acc_bufs,
+                 acc_empty_bars, acc_ready_bars, clc_result_buffers, clc_barriers, clc_consumed_bars, MINOR_DIM,
+                 GRID_TILE_WIDTH, TWO_CTAS):
+        self.a_desc = a_desc
+        self.b_desc = b_desc
+        self.c_desc = c_desc
+        self.a_bufs = a_bufs
+        self.b_bufs = b_bufs
+        self.load_empty_bars = load_empty_bars
+        self.load_ready_bars = load_ready_bars
+        self.acc_bufs = acc_bufs
+        self.acc_empty_bars = acc_empty_bars
+        self.acc_ready_bars = acc_ready_bars
+        self.clc_result_buffers = clc_result_buffers
+        self.clc_barriers = clc_barriers
+        self.clc_consumed_bars = clc_consumed_bars
+        self.MINOR_DIM = gl.constexpr(MINOR_DIM)
+        self.GRID_TILE_WIDTH = gl.constexpr(GRID_TILE_WIDTH)
+        self.TWO_CTAS = gl.constexpr(TWO_CTAS)
+
+    @gluon.jit
+    def get_clc_consumer(self):
+        return ClcTileSchedulerConsumer.initialize(
+            self.c_desc.shape[0],
+            self.c_desc.shape[1],
+            self.a_desc.block_shape[0],
+            self.b_desc.block_shape[1],
+            self.MINOR_DIM,
+            self.GRID_TILE_WIDTH,
+            self.clc_result_buffers,
+            self.clc_barriers,
+            self.clc_consumed_bars,
+        )
+
+
+@gluon.jit
+def matmul_clc_partition(p):
+    has_work = gl.to_tensor(True)
+    state = Counter.create(0, p.clc_barriers.shape[0])
+    consumed_state = Counter.create(1, p.clc_barriers.shape[0])
+    i = 0
+    while has_work:
+        # Reuse the slot only after all consumer partitions signaled consumed.
+        mbarrier.wait(p.clc_consumed_bars.index(consumed_state.index), consumed_state.phase, pred=(i > 1))
+        barrier = p.clc_barriers.index(state.index)
+        result = p.clc_result_buffers.index(state.index)
+        mbarrier.expect(barrier, 16)
+        clc.try_cancel(result, barrier, multicast=True)
+        mbarrier.wait(barrier, state.phase)
+        has_work = clc.load_result(result).is_canceled()
+        state = state.next()
+        consumed_state = consumed_state.next()
+        i += 1
+
+
 @gluon.jit
 def matmul_load_partition(p):
-    if p.TWO_CTAS:
-        mbarrier.sync_cluster_init()
-
     BLOCK_K: gl.constexpr = p.a_desc.block_shape[1]
     K = p.a_desc.shape[1]
 
     concurrent_loads: gl.constexpr = p.load_ready_bars.shape[0]
     state = Counter.create(1, concurrent_loads)
-    scheduler = p.get_scheduler()
+    scheduler = p.get_clc_consumer()
 
-    for idx in range(scheduler.get_num_tiles()):
-        off_m, off_n = scheduler.get_offsets(idx)
+    i = 0
+    while scheduler.has_work:
+        off_m, off_n = scheduler.get_offsets()
         for k in range(0, K, BLOCK_K):
-            pred = (idx > 0) or (k >= BLOCK_K * concurrent_loads)
+            pred = (i > 0) or (k >= BLOCK_K * concurrent_loads)
             mbarrier.wait(p.load_empty_bars.index(state.index), state.phase, pred=pred)
             bar = p.load_ready_bars.index(state.index)
             mbarrier.expect(bar, p.a_desc.nbytes_per_cta + p.b_desc.nbytes_per_cta)
             tma.async_copy_global_to_shared(p.a_desc, [off_m, k], bar, p.a_bufs.index(state.index))
             tma.async_copy_global_to_shared(p.b_desc, [k, off_n], bar, p.b_bufs.index(state.index))
             state = state.next()
+        scheduler = scheduler.step(i)
+        i += 1
 
 
 @gluon.jit
 def matmul_mma_partition(p):
-    if p.TWO_CTAS:
-        mbarrier.sync_cluster_init()
-
     BLOCK_K: gl.constexpr = p.a_desc.block_shape[1]
     K = p.a_desc.shape[1]
 
     load_state = Counter.create(0, p.load_empty_bars.shape[0])
     acc_state = Counter.create(1, p.acc_empty_bars.shape[0])
-    scheduler = p.get_scheduler()
+    scheduler = p.get_clc_consumer()
 
-    for i in range(scheduler.get_num_tiles()):
+    i = 0
+    while scheduler.has_work:
         acc_buf = p.acc_bufs.index(acc_state.index)
         mbarrier.wait(p.acc_empty_bars.index(acc_state.index), acc_state.phase, pred=(i > 1))
         use_acc = False
@@ -293,13 +378,12 @@ def matmul_mma_partition(p):
             use_acc = True
         tcgen05_commit(p.acc_ready_bars.index(acc_state.index))
         acc_state = acc_state.next()
+        scheduler = scheduler.step(i)
+        i += 1
 
 
 @gluon.jit
 def matmul_epilogue_partition(p):
-    if p.TWO_CTAS:
-        mbarrier.sync_cluster_init()
-
     TILE_M: gl.constexpr = p.a_desc.block_shape[0]
     TILE_N: gl.constexpr = p.b_desc.block_shape[1]
     SPLIT_TILE_N: gl.constexpr = p.c_desc.block_shape[1]
@@ -309,18 +393,19 @@ def matmul_epilogue_partition(p):
     acc_state = Counter.create(0, p.acc_empty_bars.shape[0])
     acc_smems = gl.allocate_shared_memory(dtype, [2, TILE_M, SPLIT_TILE_N], p.c_desc.layout)
     sub_acc_state = Counter.create(0, p.acc_empty_bars.shape[0])
-    scheduler = p.get_scheduler()
+    scheduler = p.get_clc_consumer()
 
-    for idx in range(scheduler.get_num_tiles()):
-        off_m, off_n = scheduler.get_offsets(idx)
+    i = 0
+    while scheduler.has_work:
+        off_m, off_n = scheduler.get_offsets()
 
         # TODO: we are not emitting read=True
         tma.store_wait(pendings=0)
         mbarrier.wait(p.acc_ready_bars.index(acc_state.index), acc_state.phase)
         acc_buf = p.acc_bufs.index(acc_state.index)
 
-        for i in gl.static_range(SUBTILE_FACTOR):
-            acc_sub = acc_buf.slice(SPLIT_TILE_N * i, SPLIT_TILE_N)
+        for s in gl.static_range(SUBTILE_FACTOR):
+            acc_sub = acc_buf.slice(SPLIT_TILE_N * s, SPLIT_TILE_N)
             acc_smem = acc_smems.index(sub_acc_state.index)
             acc_smem.store(
                 acc_sub.load(
@@ -332,19 +417,15 @@ def matmul_epilogue_partition(p):
                         cga_layout=p.c_desc.layout.cga_layout,
                     )).to(dtype))
             fence_async_shared()
-            tma.async_copy_shared_to_global(p.c_desc, [off_m, off_n + SPLIT_TILE_N * i], acc_smem)
+            tma.async_copy_shared_to_global(p.c_desc, [off_m, off_n + SPLIT_TILE_N * s], acc_smem)
             # TODO: we are not emitting read=True
             tma.store_wait(pendings=1)
             sub_acc_state = sub_acc_state.next()
         # Signal that the accumulator slot can be reused only after all stores are done.
         mbarrier.arrive(p.acc_empty_bars.index(acc_state.index))
         acc_state = acc_state.next()
-
-
-@gluon.jit
-def matmul_sync_partition(p):
-    if p.TWO_CTAS:
-        mbarrier.sync_cluster_init()
+        scheduler = scheduler.step(i)
+        i += 1
 
 
 @gluon.jit
@@ -364,8 +445,6 @@ def _matmul_kernel(
     TWO_CTAS: gl.constexpr,
     EPILOGUE_SIZE_N: gl.constexpr,
 ):
-    gl.static_assert(STAGES >= 2, "Expected at least 2 stages")
-    gl.static_assert(gl.num_ctas() == (2 if TWO_CTAS else 1), "num_ctas mismatch with TWO_CTAS")
     BLOCK_M: gl.constexpr = a_desc.block_shape[0]
     BLOCK_N: gl.constexpr = b_desc.block_shape[1]
 
@@ -398,6 +477,17 @@ def _matmul_kernel(
         # For multicast we could use tcgen05_mma_barrier_count
         mbarrier.init(acc_ready_bars.index(i), count=1)
 
+    clc_barriers = mbarrier.allocate_mbarrier(batch=2)
+    clc_consumed_bars = mbarrier.allocate_mbarrier(batch=2, two_ctas=TWO_CTAS)
+    for i in gl.static_range(2):
+        mbarrier.init(clc_barriers.index(i), count=1)
+        mbarrier.init(clc_consumed_bars.index(i), count=3)
+
+    clc_result_shape: gl.constexpr = [clc_barriers.shape[0], 2 * clc_barriers.shape[1]]
+    clc_result_buffers = gl.allocate_shared_memory(gl.int64, clc_result_shape, clc_barriers.layout)
+
+    if TWO_CTAS:
+        mbarrier.sync_cluster_init()
     p = PartitionArgs(
         a_desc,
         b_desc,
@@ -409,6 +499,9 @@ def _matmul_kernel(
         acc_bufs,
         acc_empty_bars,
         acc_ready_bars,
+        clc_result_buffers,
+        clc_barriers,
+        clc_consumed_bars,
         GRID_MINOR_DIM,
         GRID_TILE_WIDTH,
         TWO_CTAS,
@@ -418,8 +511,8 @@ def _matmul_kernel(
         (matmul_epilogue_partition, (p, )),
         (matmul_load_partition, (p, )),
         (matmul_mma_partition, (p, )),
-        (matmul_sync_partition, (p, )),
-    ], [1, 1, 2], [24, 24, 24])
+        (matmul_clc_partition, (p, )),
+    ], [1, 1, 1], [24, 24, 24])
 
 
 matmul_kernel = triton.autotune(
@@ -484,8 +577,6 @@ def matmul_with_config(
         "EPILOGUE_SIZE_N": epilogue_size_n,
     })
 
-    num_sms = torch.cuda.get_device_properties("cuda").multi_processor_count
-
     def grid(meta):
         tile_m = meta["BLOCK_SIZE_M"] * (2 if bool(meta["TWO_CTAS"]) else 1)
         tile_n = meta["BLOCK_SIZE_N"] * (2 if bool(meta["TWO_CTAS"]) else 1)
@@ -493,7 +584,7 @@ def matmul_with_config(
         if M % tile_m != 0 or N % tile_n != 0 or K % block_k != 0:
             raise ValueError(f"Shape {(M, N, K)} incompatible with tile {(tile_m, tile_n, block_k)}")
         num_tiles = triton.cdiv(M, tile_m) * triton.cdiv(N, tile_n)
-        return (min(num_sms, num_tiles), )
+        return (num_tiles, )
 
     _matmul_kernel[grid](
         a_desc,
@@ -531,8 +622,6 @@ def matmul(a, b):
     b_desc = TensorDescriptor.from_tensor(b, dummy_block, dummy_layout)
     c_desc = TensorDescriptor.from_tensor(c, dummy_block, dummy_layout)
 
-    num_sms = torch.cuda.get_device_properties(a.device).multi_processor_count
-
     def grid(meta):
         tile_m = meta["BLOCK_SIZE_M"] * (2 if bool(meta["TWO_CTAS"]) else 1)
         tile_n = meta["BLOCK_SIZE_N"] * (2 if bool(meta["TWO_CTAS"]) else 1)
@@ -540,7 +629,7 @@ def matmul(a, b):
         if M % tile_m != 0 or N % tile_n != 0 or K % block_k != 0:
             raise ValueError(f"Shape {(M, N, K)} incompatible with tile {(tile_m, tile_n, block_k)}")
         num_tiles = triton.cdiv(M, tile_m) * triton.cdiv(N, tile_n)
-        return (min(num_sms, num_tiles), )
+        return (num_tiles, )
 
     matmul_kernel[grid](a_desc, b_desc, c_desc, M, N, K)
     return c
@@ -688,6 +777,7 @@ def benchmark(*, profile=True):
 
     pallas_available = False
     try:
+        raise RuntimeError("Pallas is not available")
         import jax
         import jax.numpy as jnp
         pallas_mod = importlib.import_module("jax.experimental.pallas.ops.gpu.blackwell_matmul_mgpu")
