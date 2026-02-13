4a68bea15a9af6664715ca2d92872fe9b5ae8f6b
lezcano <lezcano-93@hotmail.com>
2026-02-13 14:12:12 +0000

    [BACKEND] Make cluster barriers be executed by all the threads
    This is a requisite, as cluster barriers do not come with a mask as
    `bar.sync` does. This meant that in warp specialised code emitting a
    cluster barrier would deadlock.
    
    We put the op in NVGPU as the `freeTMAlloc` pass is there. If we want to
    move it to `nvidia_gpu` we'd need to move that pass as well.
    
    This has the interesting corollary that you cannot have a
    `convert_layout` (or a `reduce`)  that crosses CTAs within a partition
    in a warp-specialised kernel.

--- lib/Analysis/Membar.cpp
+++ lib/Analysis/Membar.cpp
@@ -1,4 +1,5 @@
 #include "triton/Analysis/Membar.h"
+#include "third_party/nvidia/include/Dialect/NVGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/IR/LinearLayoutConversions.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
@@ -6,6 +7,10 @@
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include <deque>
+
+namespace ttg = ::mlir::triton::gpu;
+namespace ttng = ::mlir::triton::nvidia_gpu;
+namespace ttn = ::mlir::triton::nvgpu;
 
 namespace mlir {
 
@@ -247,6 +252,8 @@ void MembarAnalysis::update(Operation *op, BlockInfo *blockInfo,
                             OpBuilder *builder) {
   auto containsLocalBarrier = [](Operation *op) {
     if (isa<gpu::BarrierOp>(op))
+      return true;
+    if (isa<triton::nvgpu::ClusterBarrierOp>(op))
       return true;
     if (isa<triton::nvidia_gpu::ClusterWaitOp>(op))
       return true;
