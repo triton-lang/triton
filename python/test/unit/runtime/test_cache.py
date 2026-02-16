fe741704ce89f05f0cc310b13fd2b5d7ef7bb57c
Shucai Xiao <shucai.xiao@amd.com>
2026-02-16 14:25:34 -0600

    refine test and format script

--- python/test/unit/runtime/test_cache.py
+++ python/test/unit/runtime/test_cache.py
@@ -899,10 +899,22 @@ def test_module_load_unload():
     def kernel(out_ptr, val) -> None:
         tl.store(out_ptr, val)
 
+    # we should hit the module unload call to decrese the counter from 1 to 0
+    counter = 1
+
+    def module_unload(*args, **kwargs):
+        nonlocal counter
+        counter -= 1
+
+    triton.knobs.runtime.module_unload_hook.add(module_unload)
+
     out = torch.randn(1, dtype=torch.float32, device='cuda')
     pre_compile = kernel.warmup(out, 1, grid=(1, ))
     pre_compile._init_handles()
 
+    assert counter == 1
     assert pre_compile.module is not None
     pre_compile.__del__()
+
+    assert counter == 0
     assert pre_compile.module is None
