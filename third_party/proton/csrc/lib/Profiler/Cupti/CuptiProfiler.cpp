cb7bf4c1266e4f1df94b147022010de13f3cadc1
Jokeren <robinho364@gmail.com>
2026-03-12 21:46:53 -0400

    Fix typo in memory leak warning message in CuptiProfiler

--- third_party/proton/csrc/lib/Profiler/Cupti/CuptiProfiler.cpp
+++ third_party/proton/csrc/lib/Profiler/Cupti/CuptiProfiler.cpp
@@ -642,7 +642,7 @@ void CuptiProfiler::CuptiProfilerPimpl::handleApiEnterLaunchCallbacks(
     if (!findGraph && !graphStates[graphExecId].captureStatusChecked) {
       graphStates[graphExecId].captureStatusChecked = true;
       std::cerr << "[PROTON] Cannot find graph for graphExecId: " << graphExecId
-                << ", and t may cause memory leak. To avoid this problem, "
+                << ", and it may cause memory leak. To avoid this problem, "
                    "please start profiling before the graph is created."
                 << std::endl;
     } else if (findGraph && !graphStates[graphExecId].captureStatusChecked) {
