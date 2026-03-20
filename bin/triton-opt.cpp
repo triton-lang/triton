376de79979ea166497f3b4e99c105022aa7712ea
Puyan Lotfi <puyan@puyan.org>
2026-03-19 23:48:14 -0700

    more cleanup

--- bin/triton-opt.cpp
+++ bin/triton-opt.cpp
@@ -1,12 +1,8 @@
 #include "./RegisterTritonDialects.h"
 
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-#include "llvm/Support/DynamicLibrary.h"
-#include "llvm/Support/FileSystem.h"
-#include "llvm/Support/Path.h"
 
 int main(int argc, char **argv) {
-
   mlir::DialectRegistry registry;
   registerTritonDialects(registry);
 
