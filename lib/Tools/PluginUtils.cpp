69259478599b1ffef88c4513344f087af85705c7
Puyan Lotfi <puyan@puyan.org>
2026-03-23 12:10:23 -0700

    lint

--- lib/Tools/PluginUtils.cpp
+++ lib/Tools/PluginUtils.cpp
@@ -39,22 +39,21 @@ std::runtime_error TritonPlugin::err2exp(llvm::Error Err) {
 }
 
 llvm::Error TritonPlugin::loadPlugin() {
-  #if TRITON_EXT_ENABLED == 0
+#if TRITON_EXT_ENABLED == 0
   // Right now we only support one extension, bump this up if that changes
   static llvm::SmallVector<std::string, 1> printedWarning;
   if (llvm::find(printedWarning, filename) == printedWarning.end()) {
-    llvm::errs()
-      << "\n"
-      << "\n=================== WARNING =====================\n"
-      << "Triton will not load the following extension\n"
-      << "because it is not built with TRITON_EXT_ENABLED:\n"
-      << filename
-      << "\n=================================================\n"
-      << "\n";
+    llvm::errs() << "\n"
+                 << "\n=================== WARNING =====================\n"
+                 << "Triton will not load the following extension\n"
+                 << "because it is not built with TRITON_EXT_ENABLED:\n"
+                 << filename
+                 << "\n=================================================\n"
+                 << "\n";
     printedWarning.push_back(filename);
   }
   return llvm::Error::success();
-  #endif
+#endif
   if (isLoaded)
     return llvm::Error::success();
 
