278068d2e26a856d6c59392240bb9f520f90ae83
Neil Dhar <neildhar16@gmail.com>
2026-04-15 09:42:03 -0700

    Use TargetInfo for shared memory accesses

--- lib/Conversion/TritonGPUToLLVM/Utility.cpp
+++ lib/Conversion/TritonGPUToLLVM/Utility.cpp
@@ -646,7 +646,7 @@ lowerLdSt(Location loc, MLIRContext *ctx, LinearLayout cvt,
   auto vals = to_vector(valsArray);
   bool isStore = !vals.empty();
   auto b = TritonLLVMOpBuilder(loc, rewriter);
-  auto smemPtrTy = ptr_ty(ctx, 3);
+  auto smemPtrTy = ptr_ty(ctx, targetInfo.getSharedAddressSpace());
   auto kReg = str_attr("register");
   auto kLane = str_attr("lane");
   auto kWarp = str_attr("warp");
