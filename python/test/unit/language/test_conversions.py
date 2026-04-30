a9c5a0995355e174ad9a33feec932d91b608cce2
Lei Zhang <antiagainst@gmail.com>
2026-04-30 02:58:56 +0000

    [AMD][gfx1250] Fix f32 to fp8*fnuz conversion and test skips

--- python/test/unit/language/test_conversions.py
+++ python/test/unit/language/test_conversions.py
@@ -7,7 +7,7 @@ import pytest
 import triton
 import triton.language as tl
 
-from triton._internal_testing import is_cuda, is_hip, is_hip_cdna2, is_hip_cdna3, is_hip_cdna4, is_hip_rdna3, is_hip_rdna4
+from triton._internal_testing import is_cuda, is_hip, is_hip_cdna2, is_hip_cdna3, is_hip_cdna4, is_hip_rdna3, is_hip_rdna4, is_hip_gfx1250
 
 FP8_DTYPES = ('float8e5', 'float8e4b15', 'float8e4nv', 'float8e4b8', 'float8e5b16')
 
@@ -294,8 +294,8 @@ def test_typeconvert_upcast(src_dtype, dst_dtype, device):
             with pytest.raises(triton.CompilationError, match="not supported in this architecture"):
                 launch_exhaustive_populate(getattr(tl, src_dtype), 0, 65536, False, 8, 0x7f, device=device)
             return
-        if src_dtype in ('float8e4b8', 'float8e5b16') and (is_hip_cdna2() or is_hip_rdna4()):
-            pytest.skip(f"{src_dtype} is not supported on AMDGPU CDNA2 and RDNA4")
+        if src_dtype in ('float8e4b8', 'float8e5b16') and (is_hip_cdna2() or is_hip_rdna4() or is_hip_gfx1250()):
+            pytest.skip(f"{src_dtype} is not supported on current AMD GPU")
 
     # dtype : (exponent_bits, mantissa_bits, exponent_bias, max_repr)
     stuff = {
@@ -349,8 +349,8 @@ def test_typeconvert_downcast(src_dtype, dst_dtype, rounding, max_repr, device):
     if is_hip():
         if dst_dtype in FP8_DTYPES and is_hip_rdna3():
             pytest.skip(f"{dst_dtype} is not supported on AMDGPU RDNA3")
-        if dst_dtype in ('float8e4b8', 'float8e5b16') and (is_hip_cdna2() or is_hip_rdna4()):
-            pytest.skip(f"{dst_dtype} is not supported on AMDGPU CDNA2 and RDNA4")
+        if dst_dtype in ('float8e4b8', 'float8e5b16') and (is_hip_cdna2() or is_hip_rdna4() or is_hip_gfx1250()):
+            pytest.skip(f"{dst_dtype} is not supported on current AMD GPU")
 
     # dtype : (exponent_bits, mantissa_bits, exponent_bias)
     stuff = {
@@ -382,7 +382,7 @@ def test_typeconvert_downcast_clamping(src_dtype, dst_dtype, mode, device, round
     if dst_dtype in FP8_DTYPES and is_hip_rdna3():
         pytest.skip(f"{dst_dtype} is not supported on AMDGPU RDNA3")
 
-    if mode in ('inf', '-inf') and is_hip_rdna4():
+    if mode in ('inf', '-inf') and (is_hip_rdna4() or is_hip_gfx1250()):
         pytest.skip(f"clamping from `{mode}` is not supported on AMDGPU GFX12")
 
     converter = {
