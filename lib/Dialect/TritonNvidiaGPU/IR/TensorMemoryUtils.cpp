0914a04af03c8345d15fa41af6f2fab618f07b43
masahi <masahi129@gmail.com>
2026-05-21 15:08:16 +0900

    Fix alignment error in unpacked tmem store with `16x32bx2` message (#10321)
    When copying a small scale tensor of shape `16x2xi8` from `BLOCK_N = 16`
    and `BLOCK_K = 64`, currently we end up using unpacked store with
    [secondHalfOffset =
    1](https://github.com/triton-lang/triton/blob/main/lib/Dialect/TritonNvidiaGPU/IR/TensorMemoryUtils.cpp#L250C9-L250C23).
    In my understanding, this results in the following TMEM access:
    
    ```
      For 16x2 with .unpack::16b:                                                                                                                                                                
                                                                                                                                                                                                 
      real half:   cols 0,1                                                                                                                                                                      
      dummy half:  cols 1,2   // secondHalfOffset = 1   
    ```
    
    Apparently, the second message causes misaligned access - running a
    block-scaled test with `BLOCK_N = 16` and `BLOCK_K = 64` results in the
    `cudaErrorMisalignedAddress` error. Setting `secondHalfOffset = 2` fixes
    the issue.
    
    The surrounding comment indicates that padding columns are allocated to
    accommodate such use of `secondHalfOffset`. Setting it to 2 requires
    more padding. The only evidence I found, that we allocate extra columns
    for scales, is the [columnAlignment =
    4](https://github.com/triton-lang/triton/blob/main/lib/Dialect/TritonNvidiaGPU/Transforms/TensorMemoryAllocation.cpp#L365)
    convention which is already compatible with `secondHalfOffset = 2`. To
    make this extra allocation for scales explicit, I modified
    `getTmemAllocSizes` to always allocates 4 columns for scales at minimum.

--- lib/Dialect/TritonNvidiaGPU/IR/TensorMemoryUtils.cpp
+++ lib/Dialect/TritonNvidiaGPU/IR/TensorMemoryUtils.cpp
@@ -236,19 +236,6 @@ lowerTMemLdSt(const LinearLayout &cvt, int maxnreg, int bitwidth, bool isScales,
       auto row = reps.getBasis(kLane, 4, kRow);
       auto col = reps.getBasis(kLane, 4, kCol);
       secondHalfOffset = (row << 16) | col;
-      if (*secondHalfOffset == 0) {
-        // Workaround for ptxas bug, we cannot use secondHalfOffset = 0 to write
-        // only 16 elements. We use secondHalfOffset = 1 instead and we pad the
-        // allocation.
-        if (!isScales) {
-          if (emitError) {
-            emitError()
-                << "Only supported for scales as we pad the allocation.";
-          }
-          return failure();
-        }
-        secondHalfOffset = 1;
-      }
       // We "quotient it out", meaning we remove the last basis from reps
       auto basis = reps.getBases();
       basis[kLane][4] = {0, 0};
