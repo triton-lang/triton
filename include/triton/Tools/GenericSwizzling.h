cbdaedcc0bd90433dd497a82a5d6b760f1583b90
Jian Li <Jian.Li@amd.com>
2026-05-29 15:04:24 +0000

    [AMD][LAYOUTS] Refine segment computation to support DS_READ_B128
    DS_READ_B128 instruction has swizzled thread grouping in each phase
    reading from LDS.
    
    For MI300:
    Phase 0: T0-T3, T20-T23
    Phase 1: T32-T35, T52-T55
    Phase 2: T4-T7, T16-T19
    Phase 3: T36-T39, T48-T51
    Phase 4: T8-T11, T28-T31
    Phase 5: T40-T43, T60-T63
    Phase 6: T12-T15, T24-T27
    Phase 7: T44-T47, T56-T59
    
    For MI350:
    Phase 0: T0-T3, T12-T15, T20-T23, T24-T27
    Phase 1: T32-T35, T44-T47, T52-T55, T56-T59
    Phase 2: T4-T7, T8-T11, T16-T19, T28-T31
    Phase 3: T36-T39, T40-T43, T48-T51, T60-T63
    
    As Frederick Vu pointed out: Lane basis vectors used for tracking
    concurrent memory accesses during bank conflict accounting are chosen to
    ensure their span defines the phase 0 lane ID subspace. Lane ID subspace
    of other phases is an affine shift of that of phase 0.
    
    For MI300 we choose:
    V0 = span(l0, l1, l2 \oplus l4)
    V1 = V0 \oplus l5
    V2 = V0 \oplus l2
    V3 = V0 \opuls l5 \oplus l2
    V4 = V0 \oplus l3
    V5 = V0 \opuls l5 \oplus l3
    V6 = V0 \oplus l3 \oplus l2
    V7 = V0 \oplus l5 \oplus l3 \oplus l2
    
    For MI350 we choose:
    V0 = span(l0, l1, l2 \oplus l3, l2 \oplus l4)
    V1 = V0 \oplus l5
    V2 = V0 \oplus l2
    V3 = V0 \oplus l5 \oplus l2
    
    To support the mixed/XORed basis, we refine computeSegment() by
    replacing raw integer lookups with proper linear algebra operations over
    F2 to cleanly handle both power-of-two inputs and mixed XOR patterns.
    
    Co-authored-by: Frederick Vu <100011202+FrederickVu@users.noreply.github.com>

--- include/triton/Tools/GenericSwizzling.h
+++ include/triton/Tools/GenericSwizzling.h
@@ -26,8 +26,20 @@ struct LocalMemOpTile {
   // If laneAddr.size() < 3, we assume that the first
   // 3 - laneAddr.size() bases are registers
   llvm::SmallVector<int32_t> laneAddr;
+  // A set of bitmasks representing the basis vectors whose span defines the
+  // phase 0 lane ID subspace. This is used to track which parallel lane
+  // addresses are processed together in the same hardware execution phase for
+  // shared memory bank conflict accounting.
+  // - CDNA3 operates in 8 phases of 8 threads each for ds_read_b128
+  //   (e.g., phase 0: T0-T3, T20-T23).
+  //   V0 = span{l0, l1, l2 ^ l4}, represented as {1, 2, 20}.
+  // - CDNA4 operates in 4 phases of 16 threads each for ds_read_b128
+  //   (e.g., phase 0: T0-T3, T12-T15, T20-T23, T24-T27).
+  //   V0 = span{l0, l1, l2 ^ l3, l2 ^ l4}, represented as {1, 2, 12, 20}.
+  llvm::SmallVector<int32_t> laneMask;
 
   llvm::SmallVector<int32_t> getLaneAddr(llvm::ArrayRef<int32_t> lane) const;
+  llvm::SmallVector<int32_t> getLaneMask(llvm::ArrayRef<int32_t> lane) const;
 };
 
 // Given a set of possible instructions given by
