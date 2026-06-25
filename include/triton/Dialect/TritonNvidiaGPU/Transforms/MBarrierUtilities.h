2e20dfb0b38ad52321212e0603f3d63a8ef5e8bc
Keren Zhou <kerenzhou@openai.com>
2026-06-25 18:04:58 +0000

    Use alias analysis for mbarrier lifecycle hoisting

--- include/triton/Dialect/TritonNvidiaGPU/Transforms/MBarrierUtilities.h
+++ include/triton/Dialect/TritonNvidiaGPU/Transforms/MBarrierUtilities.h
@@ -2,20 +2,16 @@
 
 #include "mlir/IR/Operation.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
-#include "llvm/ADT/STLFunctionalExtras.h"
+#include "llvm/ADT/SmallVector.h"
 
 namespace mlir::triton::nvidia_gpu {
 
 bool isCrossCTAMBarrier(Value barrier, int numCTAs);
 
-bool isCrossCTAConsumer(Operation *op,
-                        llvm::function_ref<bool(Value)> aliasesBarrier);
+void getCrossCTAConsumerBarriers(Operation *op,
+                                 SmallVectorImpl<Value> &barriers);
 
 bool isCrossCTAConsumer(Operation *op, Value barrier);
-
-bool requiresCrossCTAMBarrierInitSync(
-    FunctionOpInterface funcOp, Value barrier, int numCTAs,
-    llvm::function_ref<bool(Value)> aliasesBarrier);
 
 bool requiresCrossCTAMBarrierInitSync(FunctionOpInterface funcOp,
                                       Value barrier, int numCTAs);
