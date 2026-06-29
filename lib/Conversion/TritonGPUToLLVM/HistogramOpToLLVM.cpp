0610212563339926b80c528c705f2c9db7649d68
Neil Dhar <neildhar16@gmail.com>
2026-06-29 14:26:07 -0700

    Use unique elements in simple lowerings
    Rewrite more lowerings to operate only on unique elements.

--- lib/Conversion/TritonGPUToLLVM/HistogramOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/HistogramOpToLLVM.cpp
@@ -80,6 +80,7 @@ public:
   matchAndRewrite(triton::HistogramOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     Location loc = op.getLoc();
+    auto *ctx = op.getContext();
     Value input = adaptor.getSrc();
     auto typeConverter = getTypeConverter();
     SmallVector<Value> srcValues =
@@ -105,8 +106,9 @@ public:
     Value baseSharedMemPtr =
         LLVM::getSharedMemoryBase(loc, rewriter, targetInfo, op.getOperation());
     auto dstType = op.getType();
-    Attribute dstEncoding = dstType.getEncoding();
-    auto indices = emitIndices(op.getLoc(), rewriter, targetInfo, dstEncoding,
+    auto dstLayout =
+        toLinearLayout(dstType).removeZeroBasesAlongDim(str_attr("register"));
+    auto indices = emitIndices(op.getLoc(), rewriter, targetInfo, dstLayout,
                                dstType, true);
     SmallVector<Value> innerDimIndices;
     for (int i = 0; i < indices.size(); ++i)
@@ -133,8 +135,8 @@ public:
           b.sdiv(histogramValue[i], b.i32_val(replicationFactor));
     }
 
-    Value results = packTensorElements(loc, typeConverter, histogramValue,
-                                       rewriter, op.getType());
+    Value results = packUniqueTensorElements(loc, typeConverter, histogramValue,
+                                             rewriter, op.getType());
     rewriter.replaceOp(op, results);
     return success();
   }
