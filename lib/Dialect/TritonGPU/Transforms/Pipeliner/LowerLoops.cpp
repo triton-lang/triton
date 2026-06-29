04af37581c6f324673f6a658ef89c79168210c64
Keren Zhou <kerenzhou@openai.com>
2026-06-29 19:01:18 +0000

    Simplify MMAv5 feed helper API

--- lib/Dialect/TritonGPU/Transforms/Pipeliner/LowerLoops.cpp
+++ lib/Dialect/TritonGPU/Transforms/Pipeliner/LowerLoops.cpp
@@ -396,7 +396,8 @@ void createTMABarrierAndWait(
       int loadSize = product(getShapePerCTA(tensorTy));
       sizeInBytes += loadSize * tensorTy.getElementTypeBitWidth() / 8;
       hasTwoCTAMMAUser |= asyncLoads[op].useMulticast ||
-                          mlir::triton::loadFeedsTwoCTAMMA(op);
+                          llvm::any_of(op->getResults(),
+                                       mlir::triton::valueFeedsTwoCTAMMA);
     }
 
     Value barrierAlloc = triton::createBarrierAlloc(
@@ -538,8 +539,10 @@ scf::ForOp lowerLoads(scf::ForOp forOp, CoarseSchedule &schedule,
     Value alloc = createAlloc(forOp, loadOp, asyncLoad.sharedEncoding,
                               asyncLoad.stageDiff);
     asyncLoad.alloc = alloc;
-    asyncLoad.useMulticast = loadUsesTMAMulticast(loadOp, alloc) &&
-                             mlir::triton::loadFeedsMulticastMMAv5MMA(loadOp);
+    asyncLoad.useMulticast =
+        loadUsesTMAMulticast(loadOp, alloc) &&
+        llvm::any_of(loadOp->getResults(),
+                     mlir::triton::valueFeedsMulticastMMAv5MMA);
     loadGroups.insert({asyncLoad.stageDiff, {}});
     if (isTMALoad(loadOp)) {
       loadGroups[asyncLoad.stageDiff].hasTMALoad = true;
