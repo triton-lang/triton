c2db81a995afa57cbd6071487b3e0d9d38ec9ac6
Jokeren <robinho364@gmail.com>
2026-07-22 02:33:04 -0400

    Fix preprocessor directive for HIP_GRAPH in RocprofSDKProfiler to ensure proper compilation

--- third_party/proton/csrc/lib/Profiler/RocprofSDK/RocprofSDKProfiler.cpp
+++ third_party/proton/csrc/lib/Profiler/RocprofSDK/RocprofSDKProfiler.cpp
@@ -1427,7 +1427,7 @@ void RocprofSDKProfiler::RocprofSDKProfilerPimpl::doStop() {
   corrIdToStreamId.clear();
   kernelPhaseTracker.clear();
 
-#ifdef PROTON_ROCPROFILER_SDK_HAS_HIP_GRAPH
+#if PROTON_ROCPROFILER_SDK_HAS_HIP_GRAPH
   graphToState.clear();
   graphExecToGraph.clear();
   graphExecToGraphExecId.clear();
