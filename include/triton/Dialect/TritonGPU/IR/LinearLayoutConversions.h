c3aa0c5ff0d38c789c5614033e631e0744616252
Frederick Vu <100011202+FrederickVu@users.noreply.github.com>
2026-08-01 17:33:00 -0700

    [AMD][NFC][Backend] Refactor ds_read_tr lowerings (#11144)

--- include/triton/Dialect/TritonGPU/IR/LinearLayoutConversions.h
+++ include/triton/Dialect/TritonGPU/IR/LinearLayoutConversions.h
@@ -133,13 +133,6 @@ LinearLayout chooseShemLayoutForRegToRegConversion(
     MLIRContext *ctx, ArrayRef<unsigned> tensorShape,
     ArrayRef<unsigned> repShape, ArrayRef<unsigned> order);
 
-// The primary goal of this function is to efficiently load 2D tiles of a
-// tensor from shared memory using the `ds_read_tr` instruction for AMD GPUs.
-std::optional<LinearLayout>
-chooseDsReadTrLayout(Attribute enc, ArrayRef<int64_t> shape,
-                     int32_t elemBitWidth, unsigned instBitWidth,
-                     unsigned numLanesInShuffleGroup);
-
 // Create LinearLayout for scale in scaled mfma.
 LinearLayout chooseScaledMfmaScaleLayout(MLIRContext *ctx, int dotOperandIdx,
                                          ArrayRef<int64_t> dotOperandShape,
