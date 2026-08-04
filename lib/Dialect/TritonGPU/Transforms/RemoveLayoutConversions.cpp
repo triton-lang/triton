89de4d877a2c8b733fe32ea3956bf06269ecf480
Jokeren <robinho364@gmail.com>
2026-08-04 09:14:02 +0800

    [TRANSFORM] Prevent modification of input arguments in LayoutRematerialization

--- lib/Dialect/TritonGPU/Transforms/RemoveLayoutConversions.cpp
+++ lib/Dialect/TritonGPU/Transforms/RemoveLayoutConversions.cpp
@@ -910,6 +910,10 @@ LogicalResult LayoutRematerialization::getRematerializableSlice(
     DenseMap<Value, Attribute> &layoutArg,
     DenseMap<std::pair<Value, Attribute>, Value> &existingRematsArg,
     std::function<bool(Operation *)> stopPropagation) {
+  // Operate on copies of the input, we do not want to modify them unless we
+  // have succeeded.
+  auto slice = sliceArg;
+  auto layout = layoutArg;
   auto existingRemats = existingRematsArg;
   auto getExistingConversion = std::bind(
       &LayoutRematerialization::getExistingConversion, this,
