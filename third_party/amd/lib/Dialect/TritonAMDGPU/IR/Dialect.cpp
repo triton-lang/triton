fc72319bb4ce593689895b8c0857acc098a463f6
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2026-08-29 14:00:01 -0700

    feat: Implementing DeclareOpInterfaceMethods for cas

--- third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
+++ third_party/amd/lib/Dialect/TritonAMDGPU/IR/Dialect.cpp
@@ -1687,6 +1687,14 @@ Type BufferAtomicRMWOp::getPredicateOperandTypeLike() {
   return getOffsets().getType();
 }
 
+Value BufferAtomicCASOp::getPredicateOperand() { return getMask(); }
+void BufferAtomicCASOp::setPredicateOperand(Value pred) {
+  getMaskMutable().assign(pred);
+}
+Type BufferAtomicCASOp::getPredicateOperandTypeLike() {
+  return getOffsets().getType();
+}
+
 Value BufferStoreOp::getPredicateOperand() { return getMask(); }
 void BufferStoreOp::setPredicateOperand(Value pred) {
   getMaskMutable().assign(pred);
