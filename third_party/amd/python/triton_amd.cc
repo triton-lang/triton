c048b865003906630d1d1e88134c30fa85f47fc5
Anatoly Myachev <anatoly.myachev@intel.com>
2026-09-08 18:14:54 +0200

    Use stable python abi API in `triton_amd.cc`

--- third_party/amd/python/triton_amd.cc
+++ third_party/amd/python/triton_amd.cc
@@ -579,7 +579,8 @@ void init_triton_amd(py::module_ &m) {
                                .attr("find_libraries")("hipblaslt");
           auto path = libraries.attr("__getitem__")(0);
           auto pathString = path.attr("__str__")();
-          const char *pathChars = PyUnicode_AsUTF8(pathString.ptr());
+          Py_ssize_t name_size;
+          const char *pathChars = PyUnicode_AsUTF8AndSize(pathString.ptr(), &name_size);
           if (pathChars == nullptr)
             throw py::python_error();
           hipblasLtPath = pathChars;
