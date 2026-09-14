bcd8cc863938eb9f96dbf49928a099a825ca60f2
Keren Zhou <kerenzhou@openai.com>
2026-09-14 15:39:30 +0000

    [TEST] Simplify multi-CTA histogram cases

--- python/test/gluon/test_lowerings.py
+++ python/test/gluon/test_lowerings.py
@@ -1041,10 +1041,7 @@ def _histogram_cases():
 
     for src_cga, dst_cga in [
         ([[1]], [[1]]),
-        ([[1], [2]], [[1], [2]]),
-        ([[1], [0]], [[1], [2]]),
         ([[0], [1]], [[0], [0]]),
-        ([[0], [0]], [[1], [2]]),
     ]:
         src = ttgl.BlockedLayout([1], [THREADS_PER_WARP], [4], [0], cga_layout=src_cga)
         dst = ttgl.BlockedLayout([1], [THREADS_PER_WARP], [4], [0], cga_layout=dst_cga)
